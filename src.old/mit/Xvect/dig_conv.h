41f1a53caf7bf87f0170a71b1b8e1b7f8f108f80
bot <grass-svn2git@osgeo.org>
1990-01-26 18:03:24 +0100

    added

---
+++ src.old/mit/Xvect/dig_conv.h
@@ -0,0 +1,16 @@
+/* %W%   %G% */
+
+double dig_D_west ;
+double dig_D_east ;
+double dig_D_north ;
+double dig_D_south ;
+
+double dig_U_west ;
+double dig_U_east ;
+double dig_U_north ;
+double dig_U_south ;
+
+double dig_U_to_D_xconv ;
+double dig_U_to_D_yconv ;
+
+double xl, xr, yt, yb;
