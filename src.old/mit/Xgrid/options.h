616c269ab8527f5b81376867601fee24a2a96774
bot <grass-svn2git@osgeo.org>
1990-01-26 18:03:25 +0100

    added

---
+++ src.old/mit/Xgrid/options.h
@@ -0,0 +1,24 @@
+/*  %W%  %G%  */
+
+#define SIZE	1
+#define COLOR	2
+
+#ifdef MAIN
+	struct variables
+	{
+		char *alias ;
+		int position ;
+	} variables[] = {
+	    "size", SIZE,
+	    "s", SIZE,
+	    "color", COLOR,
+	    "c", COLOR
+	} ;
+	static int n_variables = 4 ;
+
+	char color[64] ;
+	int size ;
+#else
+	extern char color[64] ;
+	extern int size ;
+#endif
