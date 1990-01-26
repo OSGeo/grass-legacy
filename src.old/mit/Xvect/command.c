d5d587d7bf7ccac7d3a0fbe7a5c34c33c522165d
bot <grass-svn2git@osgeo.org>
1990-01-26 18:03:24 +0100

    added

---
+++ src.old/mit/Xvect/command.c
@@ -0,0 +1,34 @@
+/* @(#)command.c	2.2   7/24/87 */
+
+#include "options.h"
+
+set_default_options()
+{
+	strcpy(map_name, "") ;
+	strcpy(color, "white");
+}
+
+stash_away(pos, option)
+	int pos ;
+	char *option ;
+{
+	int new_colr ;
+
+	switch(pos)
+	{
+	case NAME:
+		strcpy (map_name, option);
+		G_strip (map_name);
+		if (*map_name == 0)
+			return -1;
+		break ;
+	case COLOR:
+		strcpy(color, option);
+		break ;
+	default:
+		printf("Unknown option\n") ;
+		return(-1) ;
+		break ;
+	}
+	return(0) ;
+}
