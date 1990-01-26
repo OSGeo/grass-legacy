8bdd07baa70749f32a73f6b12b39c8b102cbae6d
bot <grass-svn2git@osgeo.org>
1990-01-26 18:03:19 +0100

    added

---
+++ src.old/mit/latest/quit.c
@@ -0,0 +1,19 @@
+
+
+#include <X11/Intrinsic.h>
+#include <X11/StringDefs.h>
+
+
+extern Widget top_level;
+
+void quit(qt, client_data, call_data)
+	Widget qt;
+	caddr_t client_data;
+	caddr_t call_data;
+{
+
+	XtDestroyWidget((Widget) top_level);
+	
+	exit(1);
+}
+
