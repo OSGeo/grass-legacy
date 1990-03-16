276b1141d7291a3e5709dd43dc85e94d9548b96f
bot <grass-svn2git@osgeo.org>
1990-03-16 21:08:11 +0100

    added

---
+++ src.X/mit/Xproto/Xcolormode/options.h
@@ -0,0 +1,25 @@
+/*  @(#)options.h   2.1    6/26/87  */
+
+#define MODE    1
+
+#define UNSET   0
+#define FLOAT   1
+#define FIXED   2
+
+#ifdef MAIN
+struct variables {
+    char *alias;
+    int position;
+}   variables[] = {
+
+    "mode", MODE,
+    "m", MODE
+};
+static int n_variables = 2;
+
+int mode;
+
+#else
+extern int mode;
+
+#endif
