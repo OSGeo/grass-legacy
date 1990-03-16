63dba7c838ee5e6a31829da88262d78d2cacae1f
bot <grass-svn2git@osgeo.org>
1990-03-16 21:08:37 +0100

    added

---
+++ src.X/mit/Xproto/Xwhat/show.c
@@ -0,0 +1,33 @@
+/*  %W%  %G%  */
+
+#include <stdio.h>
+static int nlines = 100;
+
+show_cat(width, name, cat, label)
+char *name, *label;
+{
+    if (!isatty(fileno(stdout)))
+        fprintf(stdout, "%-*s (%d)%s\n", width, name, cat, label);
+    fprintf(stderr, "%-*s (%d)%s\n", width, name, cat, label);
+    nlines += 1;
+}
+
+show_utm(north, east)
+double north, east;
+{
+    if (!isatty(fileno(stdout)))
+        fprintf(stdout, "\n%.2lf(N) %.2lf(E)\n", north, east);
+    fprintf(stderr, "\n%.2lf(N) %.2lf(E)\n", north, east);
+    nlines += 2;
+}
+
+show_mouse()
+{
+    if (nlines >= 18) {
+        fprintf(stderr, "\n");
+        fprintf(stderr, "Buttons\n");
+        fprintf(stderr, " Left:  what's here\n");
+        fprintf(stderr, " Right: quit\n");
+        nlines = 4;
+    }
+}
