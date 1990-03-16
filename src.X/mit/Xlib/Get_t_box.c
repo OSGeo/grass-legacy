6c8f5b7d743d78387b5465d472411ea076b872e9
bot <grass-svn2git@osgeo.org>
1990-03-16 21:08:04 +0100

    added

---
+++ src.X/mit/Xlib/Get_t_box.c
@@ -0,0 +1,14 @@
+/* %W% %G% */
+
+extern int cur_x, cur_y;
+extern double _text_size_x, _text_size_y, _text_rotation;
+
+
+Get_text_box(text, t, b, l, r)
+char *text;
+int *t, *b, *l, *r;
+{
+    soft_text_ext(cur_x, cur_y,
+        _text_size_x, _text_size_y, _text_rotation, text);
+    get_text_ext(t, b, l, r);
+}
