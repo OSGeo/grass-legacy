2b688caa738713fe6d6720ac050bc5286ce9b188
bot <grass-svn2git@osgeo.org>
1990-04-28 23:08:15 +0200

    added

---
+++ src.new/celledit/lib/Xutils.h
@@ -0,0 +1,24 @@
+/* busy.c */
+Window CreateBusy();
+void HideBudy();
+void ShowBusy();
+/* c_pop_shell.c */
+Widget CreatePopupShell();
+void MovePopup();
+/* c_label.c */
+Widget CreateLabel();
+void SetLabelStr();
+/* c_bsb.c */
+Widget CreateBSB();
+/* c_view.c */
+Widget CreateViewport();
+/* c_form.c */
+Widget CreateForm();
+/* c_button.c */
+Widget CreateButton();
+/* c_dialog.c */
+Widget CreateDialogBox();
+/* c_menu_btn.c */
+Widget CreateMenuButton();
+/* c_line.c */
+Widget CreateLine();
