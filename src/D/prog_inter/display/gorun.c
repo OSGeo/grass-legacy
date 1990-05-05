6681999665440a5a79e7ba0e34ca1efd7910840b
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:01 +0200

    added

---
+++ src/D/prog_inter/display/gorun.c
@@ -0,0 +1,40 @@
+#include	<signal.h>
+#include	<stdio.h>
+
+gorun(cmd, args)
+	char *cmd ;
+	char *args ;
+{
+	int status, pid, w;
+	register int (*istat)(), (*qstat)();
+	char full_cmd_name[128] ;
+	char command[256] ;
+
+	sprintf(full_cmd_name, "%s/bin/%s", G_gisbase(), cmd) ;
+	if (-1 == access(full_cmd_name, 1 /*execute OK*/))
+	{
+		sprintf(command, "%s/bin/%s not available", G_gisbase(), cmd) ;
+		G_warning(command) ;
+		return(-1) ;
+	}
+		
+	sprintf(command, "%s/bin/%s %s", G_gisbase(), cmd, args) ;
+/*
+fprintf(stderr,"Calling: %s\n", command) ;
+*/
+
+	if ((pid = fork()) == 0) {
+		execl("/bin/sh", "sh", "-c", command, 0);
+		_exit(127);
+	}
+	istat = signal(SIGINT, SIG_IGN);
+	qstat = signal(SIGQUIT, SIG_IGN);
+
+	while ((w = wait(&status)) != pid && w != -1)
+		;
+	if (w == -1)
+		status = -1;
+	signal(SIGINT, istat);
+	signal(SIGQUIT, qstat);
+	return(status);
+}
