f1c05808186f765459fdc978fc8c39e24d92b001
bot <grass-svn2git@osgeo.org>
1990-05-05 05:07:37 +0200

    added

---
+++ src/mapdev/unsupported/dig_to_dlg/tty.c
@@ -0,0 +1,37 @@
+#ifdef USE_TERMIO
+
+#include <termio.h>
+
+static struct termio old, new;
+#define get(x) ioctl (0, TCGETA, x)
+#define set(x) ioctl (0, TCSETAF, x)
+
+#else
+
+#include <sgtty.h>
+
+static struct sgttyb old, new;
+#define get(x) gtty (0, x)
+#define set(x) stty (0, x)
+
+#endif
+
+Get_old_tty()
+{
+    get (&old);
+}
+
+Get_new_tty()
+{
+    get (&new);
+}
+
+Old_tty()
+{
+    set (&old);
+}
+
+New_tty()
+{
+    set (&new);
+}
