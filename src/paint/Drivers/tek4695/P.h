66fc02dfb22548fd43719e41b30aaac289844e0f
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:43 +0200

    added

---
+++ src/paint/Drivers/tek4695/P.h
@@ -0,0 +1,17 @@
+/* %W%  %G% */
+
+#include <stdio.h>
+
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int nrows, ncols ;
+GLOBAL int nbytes ;
+GLOBAL int ras_row;
+GLOBAL int ras_nrows;
+GLOBAL unsigned char BLACK[128];
+GLOBAL unsigned char YELLOW[128];
+GLOBAL unsigned char CYAN[128];
+GLOBAL unsigned char MAGENTA[128];
