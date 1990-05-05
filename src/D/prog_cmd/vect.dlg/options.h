34865aad08ed4d0401a660410ab5af59f8ebacd4
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:09 +0200

    added

---
+++ src/D/prog_cmd/vect.dlg/options.h
@@ -0,0 +1,28 @@
+/*  %W%  %G%  */
+
+/* Command argument positions */
+#define NAME	1
+#define COLOR	2
+
+#ifdef MAIN
+	struct variables
+	{
+		char *alias ;
+		int position ;
+	} variables[] = {
+		"name", NAME,
+		"n", NAME,
+		"color", COLOR,
+		"c", COLOR
+	} ;
+	static int n_variables = 4 ;
+
+	char map_name[64] ;
+	int color ;
+#else
+	extern char map_name[] ;
+	extern int color ;
+#endif
+
+#define NORMAL	1
+#define FANCY	2
