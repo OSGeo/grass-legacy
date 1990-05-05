dd59920da7baf648e40df2675d636394011d4da9
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:46 +0200

    added

---
+++ src/paint/Drivers/NULL/open.c
@@ -0,0 +1,20 @@
+/* %W% %G% */
+#define GLOBAL
+#include "P.h"
+
+Popen (port)
+    char *port;
+{
+    int i;
+    if (port == NULL || *port == 0)
+	out = stderr ;
+    else
+	out = fopen (port, "w");
+    if (out == NULL)
+    {
+	char msg[100];
+	sprintf (msg, "unable to open %s", port);
+	error (msg);
+    }
+    fprintf (out, "Popen(%s)\n", out == stderr ? "" : port);
+}
