0a8c958d28000b70d538b6364fd1b9e68e16cd4b
bot <grass-svn2git@osgeo.org>
1990-05-05 04:58:38 +0200

    added

---
+++ src/mapsets/externs.h
@@ -0,0 +1,12 @@
+/* %W% %G% */
+#ifdef MAIN
+	char *mapset_name[100];
+	int nmapsets ;
+	int choice[100];
+	int nchoices;
+#else
+	extern char *mapset_name[];
+	extern int nmapsets;
+	extern int choice[];
+	extern int nchoices;
+#endif
