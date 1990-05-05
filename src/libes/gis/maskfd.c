241a661d7bf8bfc53be09bced3690943c2a5e613
bot <grass-svn2git@osgeo.org>
1990-05-05 04:54:39 +0200

    added

---
+++ src/libes/gis/maskfd.c
@@ -0,0 +1,21 @@
+/*
+ **********************************************************************
+ *
+ *   G_maskfd()
+ *
+ *   Returns the file descriptor opened for the current mask
+ *
+ *   returns:    fd number if mask is in use
+ *               -1        if no mask is in use
+ **********************************************************************/
+
+#include "G.h"
+/*
+ * open mask file and return file descriptor
+ */
+G_maskfd ()
+{
+	G__check_for_auto_masking () ;
+
+	return G__.auto_mask > 0 ? G__.mask_fd : -1;
+}
