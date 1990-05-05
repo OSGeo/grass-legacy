74bd81776bef7f7254a3a3a24929818160203c76
bot <grass-svn2git@osgeo.org>
1990-05-05 04:58:00 +0200

    added

---
+++ src/sites/Lib/ask_quad.c
@@ -0,0 +1,46 @@
+static char *quad_explanation[] = {
+
+"Please set the site quad mask size.",
+"This is the number of cells away from the site to be included in the analysis",
+"eg, enter",
+"  0 if you just want to include the site itself",
+"  1 if you want to include the 8 cells surrounding the site",
+"  2 if you want to include both the above 8 cells and the 14 cells",
+"    surrounding those 8, etc.",
+"",
+"enter <stop> if you want to stop",
+
+0};
+
+ask_quad (quadsize)
+
+	int *quadsize;
+{
+	char **s;
+	char buf[300];
+
+/*
+ * ask user for quad mask size. This is the number of cells away
+ * from the actual site to include in the analysis
+ */
+	new_report_screen();
+
+	for (s = quad_explanation; *s; s++)
+		printf("%s\n", *s);
+
+	while (1)
+	{
+		printf("\nsite quad mask size> ");
+		if (!G_gets(buf)) continue;
+		printf("<<%s>>\n", buf);
+		if (strcmp(buf,"stop") == 0)
+			return 0;
+		if(!scan_int(buf, quadsize))
+			continue;
+		if (*quadsize >= 0)
+			break;
+		printf("can't be negative\n");
+	}
+
+	return 1;
+}
