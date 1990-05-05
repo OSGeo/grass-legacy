10ffbc71cec2e33812b2153e1bc171dcfe106645
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:31 +0200

    added

---
+++ src/libes/imagery/OLD/ncolors.c
@@ -0,0 +1,14 @@
+/* %W%  %G% */
+/*************************************************************
+* I_ncolors()
+*
+* returns the total number of colors based on the
+* current color levels
+*************************************************************/
+I_ncolors()
+{
+    int r,g,b;
+
+    I_get_color_levels (&r,&g,&b);
+    return r*g*b;
+}
