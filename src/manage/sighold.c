f3ab2ab4ecceba8fe0361016b98c727a22aa1543
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:00 +0200

    added

---
+++ src/manage/sighold.c
@@ -0,0 +1,15 @@
+/* %W% %G% */
+
+#include "signal.h"
+hold_signals (hold)
+{
+    int (*sig)();
+
+    sig = hold ? SIG_IGN : SIG_DFL;
+
+    signal (SIGINT, sig);
+    signal (SIGQUIT, sig);
+#ifdef SIGTSTP
+    signal (SIGTSTP, sig);
+#endif
+}
