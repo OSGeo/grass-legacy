b726c71dbd71218b72149839c51bdd57ca4c5fd4
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:45 +0200

    added

---
+++ src/paint/drivers.junk/epson.24.180/outc.c
@@ -0,0 +1,17 @@
+/* %W% %G% */
+#
+/********************************************************
+NAME:		Poutc (c)
+
+FUNCTION:	output 1 character
+**********************************************************/
+#include "P.h"
+
+Poutc ( c)
+    unsigned char c;
+{
+    if (printer.b >= printer.bufsize )
+	Pflush ();
+
+    printer.buf[printer.b++] = c;
+}
