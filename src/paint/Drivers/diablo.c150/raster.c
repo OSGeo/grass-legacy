5ecc0b2c08bdfd9f56593c23bcb1baebd2ad7698
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:06 +0200

    added

---
+++ src/paint/Drivers/diablo.c150/raster.c
@@ -0,0 +1,20 @@
+/* %W% %G% */
+#include "P.h"
+
+Praster ()
+{
+    ras_row = 0;
+    flush_raster();
+}
+
+end_raster ()
+{
+    flush_raster();
+}
+
+flush_raster()
+{
+    if (ras_row)
+	esc ("k1"); /* output the micro line */
+    ras_row = 0;
+}
