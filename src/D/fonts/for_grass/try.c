8d4317b540e7c7412a80877a5fce68632d1cf227
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:00 +0200

    added

---
+++ src/D/fonts/for_grass/try.c
@@ -0,0 +1,36 @@
+main()
+{
+	char buf[200];
+	int map[1000];
+	int nmap;
+	long offset;
+	long *index;
+	int nchars;
+	int n;
+	int size;
+
+	int font;
+
+	while (1)
+	{
+		printf ("select font (ls for list): ");
+		if (!gets(buf)) exit(0);
+		if (*buf == 0) exit(0);
+		if (strcmp (buf, "ls") == 0)
+		{
+			system ("cd fonts; ls *.hmp | sed 's/.hmp//' | fmt");
+			continue ;
+		}
+		if (-1 == init_font(buf))
+			exit(-1) ;
+
+		nmap = 0176 - 040 ;
+		while (1)
+		{
+			printf ("enter character: ");
+			if (!gets(buf)) exit(0);
+			if (*buf == 0) break;
+			showchar (*buf);
+		}
+	}
+}
