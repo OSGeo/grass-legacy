277ede17ef88765f8c1b426fc64474c881ef6fc6
bot <grass-svn2git@osgeo.org>
1990-05-05 04:53:38 +0200

    added

---
+++ src/libes/gis/TESTS/try9.c
@@ -0,0 +1,23 @@
+#include "gis.h"
+
+main()
+{
+    char name[30], *mapset;
+    char *G_get_cat();
+    struct Categories cats;
+
+    G_gisinit("try");
+    while (mapset = G_ask_cell_in_mapset ("", name))
+    {
+	if (G_read_cats (name, mapset, &cats) < 0)
+	{
+	    printf ("OOPS\n");
+	    continue;
+	}
+	if(G_edit_cats (&cats) > 0)
+	{
+	    printf ("updating cats for [%s]\n", name);
+	    G_write_cats (name, &cats);
+	}
+    }
+}
