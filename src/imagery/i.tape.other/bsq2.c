046c29fdcd197dd4aa7ea80002be900379b02382
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:04 +0200

    added

---
+++ src/imagery/i.tape.other/bsq2.c
@@ -0,0 +1,29 @@
+/* %W% %G% */
+/* read each band from current file */
+#include "tape.h"
+
+bsq2()
+{
+    int band;
+    int tail;
+    int lastband;
+
+    tail = bandsize - lastrow ;
+
+    I_tape_advance (tapefd, skiprecords);
+    for (band=0; band < nbands; band++)
+	if (wantband[band])
+		lastband = band;
+    for (band=0; band <= lastband; band++)
+    {
+	if (wantband[band])
+	{
+	    if(!readbsq (band))
+		break;
+	}
+	else if (band < lastband)
+	    I_tape_advance(tapefd, nrows);
+	if (band < lastband)
+	    I_tape_advance (tapefd, tail);
+    }
+}
