0c2637faf1e5cd4c7d54d160c12f1369ad133efc
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:59 +0200

    added

---
+++ src/paint/Drivers/epson.lq2500/rle.c
@@ -0,0 +1,23 @@
+/* %W% %G% */
+#include "P.h"
+Prle (buf, n)
+    unsigned char *buf;
+{
+    int col;
+    int cyan, yellow, magenta ;
+    unsigned char value, repeat;
+
+    col = 0;
+    while (n-- > 0)
+    {
+	repeat = *buf++;
+	value = *buf++;
+
+	cym (value, &cyan, &yellow, &magenta);
+	while (repeat-- > 0)
+	    dither (col++, cyan, yellow, magenta);
+    }
+    ras_row++;
+    if (ras_row == ras_nrows)
+	flush_raster();
+}
