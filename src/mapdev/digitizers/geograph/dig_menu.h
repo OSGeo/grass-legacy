112e5fb4f8d7937b26750a85df29da9861b7233e
bot <grass-svn2git@osgeo.org>
1990-05-05 05:06:56 +0200

    added

---
+++ src/mapdev/digitizers/geograph/dig_menu.h
@@ -0,0 +1,23 @@
+/*  %W%  %G%  */
+#define dig_menu_lines	16
+
+char *dig_menu[] = {
+" GRASS-DIGIT Version 3.0                                     Digitizing menu",
+"-----------------------------------------------------------------------------",
+"   Geographics                                  | AMOUNT DIGITIZED",
+"                     <ftswitch> for point       |   # Lines:",
+"                     u  update MONITOR          |   # Area edges:",
+"                     m  toggle digitizing mode  |   # Sites:",
+"                                                | - - - - - - - - - - - - - -",
+"                                                |   Total points:",
+"-----------------------------------------------------------------------------",
+"                                                | CURRENT DIGITIZER PARAMS.",
+"                                                |",
+"                                                |",
+"                                                | MODE       TYPE",
+"                                                |   point      line",
+"                                                |   stream     area edge",
+"                                                |"
+} ;
+
+
