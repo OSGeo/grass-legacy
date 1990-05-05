ea035110f8367dc1e46a6477b7511d100595e607
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:17 +0200

    added

---
+++ src/paint/Drivers/ppm/pictsize.c
@@ -0,0 +1,12 @@
+Ppictsize (rows, cols)
+{
+    char buf[128];
+
+/* P6 is code for ppm format */
+    Pouts("P6\n");
+/* next line is width height */
+    sprintf (buf, "%d %d\n", cols, rows);
+    Pouts(buf);
+/* maximum color level. */
+    Pouts("255\n");
+}
