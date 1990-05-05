b2d61f2ef1b1eea3be638f5f65e787b92c0c0c23
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:41 +0200

    added

---
+++ src/D/libes/Dlib/Dclear.c
@@ -0,0 +1,29 @@
+
+/*  %W%  %G%  */
+
+Dclearscreen()
+{
+	char **pads;
+	int npads;
+	int count;
+	int p;
+
+	R_pad_list (&pads, &npads);
+	for (p = -1; p < npads; p++)
+	{
+		if (p < 0)
+		{
+			R_pad_select ("");
+			R_pad_delete_item("time") ;
+			R_pad_delete_item("cur_w") ;
+		}
+		else
+		{
+			R_pad_select (pads[p]);
+			R_pad_delete ();
+		}
+	}
+
+	R_standard_color(D_translate_color("black")) ;
+	R_erase() ;
+}
