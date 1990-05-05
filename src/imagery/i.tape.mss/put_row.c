c9a6589abfcf8f1d659a89457ef9e9e62059019e
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:25 +0200

    added

---
+++ src/imagery/i.tape.mss/put_row.c
@@ -0,0 +1,15 @@
+
+#include "tape.h"
+
+put_row (fd, buf, row)
+    unsigned char *buf;
+{
+    CELL *c;
+    int ncols;
+
+    ncols = G_window_cols();
+    c = cellbuf;
+    while(ncols-- > 0)
+	*c++ = *buf++;
+    G_put_map_row (fd, cellbuf, row);
+}
