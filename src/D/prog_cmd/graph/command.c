7fb54c0b33d00852c6e20655ea08292905d28004
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:09 +0200

    added

---
+++ src/D/prog_cmd/graph/command.c
@@ -0,0 +1,58 @@
+/*  %W%  %G%  */
+
+#include "options.h"
+
+set_default_options()
+{
+	color = D_translate_color("white") ;
+	hsize = 17.0 ;
+	vsize = 17.0 ;
+	infile = stdin ;
+}
+
+stash_away(pos, option)
+	int pos ;
+	char *option ;
+{
+	int new_colr ;
+	FILE *G_fopen_old() ;
+
+	switch(pos)
+	{
+	case INPUT:
+		infile = G_fopen_old ("graph", option, "");
+		if (infile == NULL)
+		{
+			printf("Graph file <%s> not found\n", option) ;
+			return(-1) ;
+		}
+		break ;
+	case VSIZE:
+		if (! sscanf(option,"%f",&vsize) )
+			return(-1) ;
+		break ;
+	case HSIZE:
+		if (! sscanf(option,"%f",&hsize) )
+			return(-1) ;
+		break ;
+	case COLOR:
+		new_colr = D_translate_color(option) ;
+		if (new_colr == 0)
+		{
+			printf("Don't know the color %s\n", option) ;
+			printf("Available colors:\n") ;
+			printf("  red      orange      yellow      green\n") ;
+			printf("  blue     indigo      violet      gray\n") ;
+			printf("  white    black\n") ;
+			return(-1) ;
+		}
+		else
+			color = new_colr ;
+		break ;
+	default:
+		printf("Unknown option\n") ;
+		return(-1) ;
+		break ;
+	}
+	return(0) ;
+}
