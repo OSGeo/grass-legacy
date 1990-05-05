0214752dd1127647c31ec9473cdc07adc8f8f45c
bot <grass-svn2git@osgeo.org>
1990-05-05 04:54:32 +0200

    added

---
+++ src/libes/gis/openvect.c
@@ -0,0 +1,32 @@
+/*************************************************************
+ *
+ *  G_fopen_vector_old (name, mapset)
+ *      char *name            map file name
+ *      char *mapset          mapset containing map "name"
+ *
+ *  opens the existing vector file 'name' in the 'mapset'
+ *
+ *************************************************************
+ *
+ *  G_fopen_vector_new (name)
+ *      char *name            map file name
+ *
+ *  opens a new vector file 'name' in the current mapset
+ *
+ *************************************************************/
+
+#include "G.h"
+FILE *
+G_fopen_vector_old (name, mapset)
+    char *name;
+    char *mapset;
+{
+    return G_fopen_old ("dig", name, mapset);
+}
+
+FILE *
+G_fopen_vector_new (name)
+    char *name;
+{
+    return G_fopen_new ("dig", name);
+}
