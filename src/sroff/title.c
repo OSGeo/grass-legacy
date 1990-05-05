1db504bfc0dfc70b609fa97069b4715569139c21
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:19 +0200

    added

---
+++ src/sroff/title.c
@@ -0,0 +1,15 @@
+/* %W% %G% */
+#include "glob.h"
+
+begin_title()
+{
+	free_text_list (&title_text);
+	initialize_text_list (&title_text);
+}
+
+add_title (buf)
+
+	char *buf;
+{
+	add_text (&title_text, buf);
+}
