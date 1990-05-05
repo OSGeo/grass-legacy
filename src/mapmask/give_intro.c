eb99588aa280fb1e2a646deb31f7cca34573cd6b
bot <grass-svn2git@osgeo.org>
1990-05-05 05:06:09 +0200

    added

---
+++ src/mapmask/give_intro.c
@@ -0,0 +1,9 @@
+/* %W% %G% */
+give_intro()
+{
+    G_clear_screen () ;
+    printf("MAPMASK\n\n");
+    printf("This utility will allow you to define polygonal areas of interest\n");
+    printf("which will be converted to a cell file.  This data layer may then be used to\n");
+printf( "view and work with other data layers, focusing in on these areas of interest.\n\n");
+}
