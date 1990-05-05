be53c47d394df3c773aee81e7bc5455e73a64b79
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:12 +0200

    added

---
+++ src/D/prog_cmd/where/options.h
@@ -0,0 +1,26 @@
+/* Command argument positions */
+#define SPHEROID    1
+#define MODE    2
+
+#define SILENT  0
+#define LOUD    1
+
+#ifdef MAIN
+    struct variables
+    {
+        char *alias ;
+        int position ;
+    } variables[] = {
+        "s", SPHEROID,
+        "spheroid", SPHEROID,
+        "m", MODE,
+        "mode", MODE
+    } ;
+    static int n_variables = 4 ;
+
+    int have_spheroid;
+    int mode;
+#else
+    extern int have_spheroid;
+    extern int mode;
+#endif
