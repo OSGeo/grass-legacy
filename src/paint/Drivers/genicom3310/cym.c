0e75357d239caa0611116d71a0745e26419e8a3c
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:01 +0200

    added

---
+++ src/paint/Drivers/genicom3310/cym.c
@@ -0,0 +1,17 @@
+/* %W% %G% */
+cym (n, cyan, yellow, magenta)
+    unsigned char n;
+    int *cyan, *yellow, *magenta;
+{
+    int red, grn, blu;
+
+    if (n > 124) n = 0;
+
+    red = n / 25 ;
+    grn = (n % 25) / 5;
+    blu = n % 5;
+
+    *cyan    = 4 - red;
+    *yellow  = 4 - blu;
+    *magenta = 4 - grn;
+}
