c54bb21e8ed88c27c0325e9431c44bbe73b0ea28
bot <grass-svn2git@osgeo.org>
1990-05-05 05:04:58 +0200

    added

---
+++ src/cell.stats/conv.h
@@ -0,0 +1,5 @@
+/* X is area in square meters */
+#define KILOMETERS(X) X/1000000.0
+#define MILES(X)      KILOMETERS(X) *   0.3861
+#define ACRES(X)      KILOMETERS(X) * 247.1000
+#define HECTACRES(X)  KILOMETERS(X) * 100.0000
