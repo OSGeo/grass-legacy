a0f179fa57bca700679819efe87ec9dd598f5d36
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:04 +0200

    added

---
+++ src/libes/icon/release.c
@@ -0,0 +1,16 @@
+/* %W% %G% */
+#include "icon.h"
+
+release_icon (icon)
+    ICON *icon;
+{
+    int row;
+
+    if (icon->nrows > 0)
+    {
+	for (row = 0; row < icon->nrows; row++)
+	    free (icon->map[row]);
+	free (icon->map);
+    }
+    icon->nrows = 0;
+}
