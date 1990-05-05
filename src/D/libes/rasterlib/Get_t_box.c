28c87396b14609b3e8fe9c180ac3714a1247b9c0
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:23 +0200

    added

---
+++ src/D/libes/rasterlib/Get_t_box.c
@@ -0,0 +1,15 @@
+/*  %W%  %G%  */
+
+#include "graph.h"
+
+R_get_text_box(sometext, t, b, l, r)
+	char *sometext ;
+	int *t, *b, *l, *r ;
+{
+	_send_ident(GET_TEXT_BOX) ;
+	_send_text(sometext) ;
+	_get_int(t) ;
+	_get_int(b) ;
+	_get_int(l) ;
+	_get_int(r) ;
+}
