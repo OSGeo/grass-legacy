709c781a3cc6c11782a68d691d085dc9410aba50
bot <grass-svn2git@osgeo.org>
1990-05-05 04:54:42 +0200

    added

---
+++ src/libes/gis/strip.c
@@ -0,0 +1,30 @@
+/*******************************************************************
+ *  G_strip(buf)
+ *     char *buf         buffer to be worked on
+ *
+ *  'buf' is rewritten in place with leading and trailing white
+ *  space removed.
+ ******************************************************************/
+
+G_strip (buf)
+    register char *buf;
+{
+    register char *a, *b;
+
+/* remove leading white space */
+    for (a = b = buf; *a == ' ' || *a == '\t'; a++)
+	    ;
+    if (a != b)
+	while (*b++ = *a++)
+	    ;
+/* remove trailing white space */
+    for (a = buf; *a; a++)
+	    ;
+    if (a != buf)
+    {
+	for (a--; *a == ' ' || *a == '\t'; a--)
+		;
+	a++;
+	*a = 0;
+    }
+}
