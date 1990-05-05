bb74e9d63b2e0b2dcbaeaca447e85210aac4d457
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:35 +0200

    added

---
+++ src/libes/imagery/c_nclasses.c
@@ -0,0 +1,12 @@
+/* %W% %G% */
+#include "imagery.h"
+I_cluster_nclasses (C,minsize)
+    struct Cluster *C;
+{
+    int i,n;
+    n = 0;
+    for (i = 0; i < C->nclasses; i++)
+	if (C->count[i] >= minsize)
+	    n++;
+    return n;
+}
