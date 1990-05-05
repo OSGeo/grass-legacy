6c704dbe8ac41ed289be4b69fbc19f1bbd669bb7
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:49 +0200

    added

---
+++ src/paint/Drivers/preview/P.h
@@ -0,0 +1,12 @@
+/* %W% %G% */
+#include <stdio.h>
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int top, bottom, left, right;
+GLOBAL int window_nrows, window_ncols;
+GLOBAL int picture_nrows, picture_ncols;
+GLOBAL int *data;
+GLOBAL int current_row, left_edge;
