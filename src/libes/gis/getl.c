96717d075c6975560060e40553ebc4cbe3a250e6
bot <grass-svn2git@osgeo.org>
1990-05-05 04:54:36 +0200

    added

---
+++ src/libes/gis/getl.c
@@ -0,0 +1,26 @@
+/*
+ ***********************************************************
+ *  G_getl(buf, n, fd)
+ *     char *buf         buffer to receive read data
+ *     int n             max num of bytes to read
+ *     FILE *fd          file descriptor structure
+ *
+ *  does fgets() and removes trailing newline
+ *  
+ *  returns: 1 ok, 0 eof
+ ************************************************************/
+
+#include <stdio.h>
+G_getl (buf, n, fd)
+    char *buf;
+    FILE *fd;
+{
+    if (!fgets (buf, n, fd))
+	return 0;
+
+    for (; *buf && *buf != '\n'; buf++)
+	    ;
+    *buf = 0;
+
+    return 1;
+}
