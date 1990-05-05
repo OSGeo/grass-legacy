46062ac868e9fcb516838908dc74bfc5f31a88ae
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:04 +0200

    added

---
+++ src/libes/icon/put.c
@@ -0,0 +1,23 @@
+/* %W% %G% */
+#include "icon.h"
+#include "gis.h"
+
+put_icon (name, icon)
+    char *name;
+    ICON *icon;
+{
+    FILE *fd;
+    char msg[100];
+
+    if (fd = G_fopen_new ("icons", name))
+    {
+	write_icon (fd, icon, 0);
+	fclose (fd);
+	return 1;
+    }
+
+    sprintf (msg, "can't write icon file [%s]", name);
+    G_warning (msg);
+
+    return -1;
+}
