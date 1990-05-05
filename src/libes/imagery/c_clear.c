2ec8c051e099da77a5d1af6063c785294a1ab221
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:34 +0200

    added

---
+++ src/libes/imagery/c_clear.c
@@ -0,0 +1,19 @@
+/* %W% %G% */
+#include "imagery.h"
+I_cluster_clear (C)
+    struct Cluster *C;
+{
+    C->points = NULL;
+    C->band_sum = NULL;
+    C->band_sum2 = NULL;
+    C->class = NULL;
+    C->reclass = NULL;
+    C->count = NULL;
+    C->countdiff = NULL;
+    C->sum = NULL;
+    C->sumdiff = NULL;
+    C->sum2 = NULL;
+    C->mean = NULL;
+    C->nbands = 0;
+    I_init_signatures (&C->S, 0);
+}
