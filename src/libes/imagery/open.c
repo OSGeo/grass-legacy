55efd24d7f878b3681905327e841bbe7ccab287c
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:38 +0200

    added

---
+++ src/libes/imagery/open.c
@@ -0,0 +1,59 @@
+/* %W%  %G% */
+#include "gis.h"
+/******************************************************
+* I_open_group_file_new()
+* I_open_group_file_old()
+*
+* open new and old imagery group files in the current mapset
+*******************************************************/
+
+I_open_group_file_new (group, file)
+    char *group;
+    char *file;
+{
+    int fd;
+    char element[100];
+
+/* get group element name */
+    sprintf (element, "group/%s", group);
+
+    fd = G_open_new (element, file);
+    if (fd < 0)
+	error (group, file, "can't create ", "");
+    return fd;
+}
+
+I_open_group_file_old (group, file)
+    char *group;
+    char *file;
+{
+    int fd;
+    char element[100];
+
+/* find the file first */
+    if (!I_find_group_file (group, file))
+    {
+	error (group, file, "", " not found");
+	return -1;
+    }
+
+/* get group element name */
+    sprintf (element, "group/%s", group);
+
+    fd = G_open_old (element, file, G_mapset());
+    if (fd < 0)
+	error (group, file, "can't open ", "");
+    return fd;
+}
+
+static error (group, file, msga, msgb)
+    char *group;
+    char *file;
+    char *msga;
+    char *msgb;
+{
+    char buf[100];
+    sprintf (buf, "%sfile [%s] of group [%s in %s]%s",
+	msga, file, group, G_mapset(), msgb);
+    G_warning (buf);
+}
