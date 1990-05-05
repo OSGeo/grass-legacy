c9e2e6b169e12edf51826edfa767fc866f4454eb
bot <grass-svn2git@osgeo.org>
1990-05-05 04:54:45 +0200

    added

---
+++ src/libes/gis/gishelp.c
@@ -0,0 +1,40 @@
+/*
+ **********************************************************************
+ *
+ *  G_gishelp(helpfile, request)
+ *      char *helpfile           help directory where "request" is found
+ *      char *request            help file desired
+ *
+ *   Prints a helpfile to the screen.  Helpfiles are stored in directories
+ *   associated with different GRID programs.  A given file will be
+ *   found in   $GISBASE/txt/"helpfile"/"request".
+ *
+ **********************************************************************/
+
+#include "gis.h"
+#define GEN_HELP	"gen_help"
+
+G_gishelp(helpfile, request)
+    char *helpfile ;
+    char *request ;
+{
+    char file[512] ;
+    char buffer[512] ;
+
+    if (request == NULL)
+	request = GEN_HELP ;
+
+    sprintf(file,"%s/txt/%s/%s",
+	    G_getenv("GISBASE"), helpfile, request) ;
+
+    if (! access(file, 04))
+    {
+	printf("one moment...\n") ;
+	sprintf(buffer, "more -d %s", file) ;
+	system(buffer) ;
+    }
+    else
+    {
+	printf("No help available for command [%s]\n", request) ;
+    }
+}
