3a3d82e2949b74fcd4ad036d7566a96136830526
bot <grass-svn2git@osgeo.org>
1990-05-05 04:58:01 +0200

    added

---
+++ src/sites/Lib/count_site.c
@@ -0,0 +1,25 @@
+#include "site.h"
+#include "gis.h"
+count_sites (site_list, clipped)
+
+	SITE_LIST *site_list;
+{
+	int count;
+	struct Cell_head window;
+	int north;
+	int east;
+	char *desc;
+
+	if (clipped)
+		if (G_get_window (&window) == -1)
+			clipped = 0;
+
+	rewind_site_list (site_list);
+
+	count = 0;
+	while (next_site (site_list, &north, &east, &desc))
+		if (!clipped || within_window (north, east, &window))
+			count++;
+
+	return count;
+}
