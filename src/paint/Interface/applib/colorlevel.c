08ea12e00a42867fb9b8b4f2e8f375063375aa4f
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:26 +0200

    added

---
+++ src/paint/Interface/applib/colorlevel.c
@@ -0,0 +1,11 @@
+/* %W% %G% */
+#include "interface.h"
+
+Pcolorlevels (red, grn, blu)
+    int *red, *grn, *blu;
+{
+    P__opcode (COLORLEVELS) ;
+    *red = P__geti();
+    *grn = P__geti();
+    *blu = P__geti();
+}
