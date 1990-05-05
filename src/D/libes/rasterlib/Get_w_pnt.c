f0ce99b78fb83dc8aa3a77cdf2197ad43fe36365
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:24 +0200

    added

---
+++ src/D/libes/rasterlib/Get_w_pnt.c
@@ -0,0 +1,18 @@
+/*  %W%  %G%  */
+
+#include "graph.h"
+
+R_get_location_with_pointer(wx, wy, button)
+	int *wx, *wy ;
+	int *button ;
+{
+	int z ;
+	_send_ident(GET_LOCATION_WITH_POINTER) ;
+	z = *wx ;
+	_send_int(&z) ;
+	z = *wy ;
+	_send_int(&z) ;
+	_get_int(wx) ;
+	_get_int(wy) ;
+	_get_int(button) ;
+}
