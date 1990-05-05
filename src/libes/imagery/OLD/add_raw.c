88a2676a22cf46ea48a2e4b6f64737ba5ad3c4d4
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:31 +0200

    added

---
+++ src/libes/imagery/OLD/add_raw.c
@@ -0,0 +1,24 @@
+/* %W%  %G% */
+/*********************************************************
+* I_add_raw_to_image (image, raw, table, map, n)
+*
+*   unsigned int *image     image to be modified
+*   unsigned char *raw      raw data to be added to image
+*   int *table              returned by I_color_conversion_table()
+*   int *map                computed by I_histo_eq()
+*   int n                   number of columns of data
+*
+* adds color to the image based on the color conversion
+* table and map using raw data
+**********************************************************/
+
+I_add_raw_to_image (image, raw, table, map, n)
+    register unsigned int *image;
+    register unsigned char *raw;
+    int *table;
+    int *map;
+    register int n;
+{
+    while (n-- > 0)
+	*image++ += table[map[*raw++]];
+}
