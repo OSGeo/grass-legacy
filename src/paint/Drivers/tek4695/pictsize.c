abdc67e89dde0d11704813bccd0e58eb4e68af94
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:42 +0200

    added

---
+++ src/paint/Drivers/tek4695/pictsize.c
@@ -0,0 +1,13 @@
+/* %W% %G% */
+#include "P.h"
+static int first = 1;
+Ppictsize (nr, nc)
+{
+    nrows = nr;
+    ncols = nc;
+    nbytes = (nc+7)/8;
+    if (first)
+	first = 0;
+    else
+	formfeed();
+}
