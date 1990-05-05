0ab7683c389d788218ec4ae774c4dc02cefa182d
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:00 +0200

    added

---
+++ src/D/prog_inter/display/site.c
@@ -0,0 +1,16 @@
+/* %W% %G% */
+#include "windows.h"
+#include "gis.h"
+#include "variables.h"
+
+site()
+{
+/*
+	printf("Sorry, not available yet\n") ;
+	do_pause() ;
+*/
+	R_open_driver ();
+	Dchoose (MAP.name);
+	R_close_driver ();
+	gorun ("d.sites", "");
+}
