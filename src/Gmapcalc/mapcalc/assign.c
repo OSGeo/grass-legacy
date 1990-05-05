7d92ce2b57aa6635c6fe1490c594e4ea918f0795
bot <grass-svn2git@osgeo.org>
1990-05-05 04:56:01 +0200

    added

---
+++ src/Gmapcalc/mapcalc/assign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+assignment (fd, name)
+    FILE *fd;
+    char *name;
+{
+    char *nm;
+    int c;
+
+/* assumes that pre-parsing has put something into fd */
+
+    fseek (fd, 0L, 0);
+    nm = name;
+    while ((c = fgetc(fd)) > 0 && c != '=' && c != '\n')
+	*nm++ = c;
+    *nm = 0;
+
+    if (c != '=')
+    {
+	fprintf ("You must say map = expression\n");
+	return 0;
+    }
+
+    G_strip (name);
+    if (G_legal_filename (name) < 0)
+    {
+	fprintf (stderr, "%s - illegal name\n", name);
+	return 0;
+    }
+
+    return 1;
+}
