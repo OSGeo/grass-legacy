7878c47ab70491a879b6e0c4b5a6fb7759636bc4
bot <grass-svn2git@osgeo.org>
1990-05-05 05:04:53 +0200

    added

---
+++ src/weight/add_mult.c
@@ -0,0 +1,12 @@
+#include "include.h"
+
+set_to_add()
+{
+	analysis_type = ADD ;
+	printf("    Analysis will be done by ADDING weights.\n") ;
+}
+set_to_mult()
+{
+	analysis_type = MULT ;
+	printf("    Analysis will be done by MULTIPLYING weights.\n") ;
+}
