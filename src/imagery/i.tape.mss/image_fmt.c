490abb5d32082e902a7458ffd24ad79b42ab5d02
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:23 +0200

    added

---
+++ src/imagery/i.tape.mss/image_fmt.c
@@ -0,0 +1,27 @@
+/* %W% %G% */
+/***************************************************************
+
+NAME:		image_fmt()
+
+FUNCTION:	compute left fill, pixels, right fill for fully
+		corrected data image
+
+USAGE:		image_fmt (buf, lfill, pixels, rfill)
+
+		unsigned char buf[];
+		int *lfill
+		int *pixels
+		int *rfill
+*****************************************************************/
+
+image_fmt (buf, lfill, pixels, rfill)
+    unsigned char buf[];
+    int *lfill;
+    int *pixels;
+    int *rfill;
+{
+    *lfill = ((int)buf[9] * 16) + ((int)buf[10] >> 4);
+    *rfill = ((int)(buf[10] & 017) * 256) + ((int)buf[11]);
+
+    *pixels = 3560 - 12 - *lfill - *rfill;
+}
