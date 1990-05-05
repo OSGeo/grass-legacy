033761fbd876a75a563a0396335c3ea13f41a138
bot <grass-svn2git@osgeo.org>
1990-05-05 04:59:30 +0200

    added

---
+++ src/D/devices/XDRIVER/Graph_Clse.c
@@ -0,0 +1,16 @@
+/*
+ * Close down the graphics processing.  This gets called only at driver
+ * termination time.
+ */
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+Graph_Close()
+{
+	extern Display *dpy;
+	extern Window  grwin;
+	
+	XDestroyWindow(dpy,grwin);
+	return(0) ;
+}
