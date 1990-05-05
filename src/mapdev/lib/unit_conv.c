68d9ebdc25a940ca62128982e840bfb19e502974
bot <grass-svn2git@osgeo.org>
1990-05-05 05:09:11 +0200

    added

---
+++ src/mapdev/lib/unit_conv.c
@@ -0,0 +1,37 @@
+/* %W% %G% */
+#include "digit.h"
+#include "gis.h"
+
+/* 
+**  HACK.  this is just to get us up this round.
+**   in the future we would like to try to support different
+**   coordinate systems, and we need a conversion factor from
+**   meters to whatever units are needed
+**
+**  What this is tho, is conversions from digitizer units (inches)
+**  to another given unit
+*/
+double
+dig_unit_conversion ()
+{
+    int units;
+
+    units = G_projection_units(G_projection()); 
+    switch (units) {
+	case -1:
+	    fprintf (stderr, "Unknown Projection.\n");
+	    exit (-1);
+	    break;
+	case 0:
+	    return (1.0);
+	    break;
+	case FEET:
+	    return (0.083);
+	    break;
+	case METERS:
+	    return (0.0254);
+	    break;
+    }
+    return (0.0254);
+}
+
