95bdbe17093418b2420bb50028c8a79d6b706800
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:10 +0200

    added

---
+++ src/neighbors/c_min.c
@@ -0,0 +1,19 @@
+/* %W% %G% */
+#include "gis.h"
+
+CELL
+c_min (values, n)
+    register int n;
+    register CELL *values;
+{
+    register CELL min;
+
+    min = *values++;
+    while (--n > 0)
+    {
+	if (min > *values)
+	    min = *values;
+	values++;
+    }
+    return min;
+}
