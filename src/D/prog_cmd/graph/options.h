03a1468ed23721d4d00f893900adec620537ae41
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:10 +0200

    added

---
+++ src/D/prog_cmd/graph/options.h
@@ -0,0 +1,38 @@
+/*  %W%  %G%  */
+
+#include <stdio.h>
+
+#define INPUT	1
+#define COLOR	2
+#define HSIZE	3
+#define VSIZE	4
+
+#ifdef MAIN
+	struct variables
+	{
+		char *alias ;
+		int position ;
+	} variables[] = {
+	    "input", INPUT,
+	    "in", INPUT,
+	    "vsize", VSIZE,
+	    "hsize", HSIZE,
+	    "vs", VSIZE,
+	    "hs", HSIZE,
+	    "color", COLOR,
+	    "c", COLOR,
+	} ;
+	static int n_variables = 8 ;
+
+	int color ;
+	float hsize ;
+	float vsize ;
+	int t, b, l, r ;
+	FILE *infile ;
+#else
+	extern int color ;
+	extern float hsize ;
+	extern float vsize ;
+	extern int t, b, l, r ;
+	extern FILE *infile ;
+#endif
