b862fc077e59d26c88d7ea2a0be664f8b8df5f76
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:29 +0200

    added

---
+++ src/imagery/i.tape.tm/mnt_tape.c
@@ -0,0 +1,17 @@
+/***************************************************
+
+NAME:		mount_tape ()
+
+FUNCTION:	open tape drive for reading
+
+USAGE:		mount_tape ()
+
+****************************************************/
+
+#include "tape.h"
+mount_tape ()
+{
+    tape.eof = 0;
+    while((tape.fd = open(tape.name, 0)) < 0)
+	I_ask("\nMount tape and put on-line. Then hit RETURN-->", 0, 1) ;
+}
