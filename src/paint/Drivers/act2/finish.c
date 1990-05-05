42b6939aa400cd09875a4c1cc78aad718af233db
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:37 +0200

    added

---
+++ src/paint/Drivers/act2/finish.c
@@ -0,0 +1,17 @@
+/* %W% %G% */
+#include "P.h"
+
+Pfinish()
+{
+
+/*
+ * ACT printer paints a line in 2 passes. This means that a line
+ * doesn't get finished until the next one comes in.  This implies
+ * that the very last line before the close will not get painted
+ * So, we force it to be painted by printing a new line
+ */
+
+    Palpha() ;
+    Pout ("\n\n",2);
+    Pflush();
+}
