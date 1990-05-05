bac5938a179593d6ca8df1dfc8f76d5f6522cf72
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:57 +0200

    added

---
+++ src/paint/Drivers/epson.lq2500/data.c
@@ -0,0 +1,21 @@
+/* %W% %G% */
+#include "P.h"
+Pdata (buf, n)
+    unsigned char *buf;
+{
+    int col;
+    int cyan, yellow, magenta ;
+    unsigned char value;
+
+    col = 0;
+    while (n-- > 0)
+    {
+	value = *buf++;
+
+	cym (value, &cyan, &yellow, &magenta);
+	dither (col++, cyan, yellow, magenta);
+    }
+    ras_row++;
+    if (ras_row == ras_nrows)
+	flush_raster();
+}
