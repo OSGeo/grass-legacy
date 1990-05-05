de8e7c16947b099582b919d88708668ecd783db8
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:37 +0200

    added

---
+++ src/D/libes/displaylib/scan_dbl.c
@@ -0,0 +1,11 @@
+/*  %W%  %G%  */
+
+D_scan_double (buf, f)
+    char *buf;
+    double *f;
+{
+    char dummy[2];
+
+    *dummy = 0;
+    return sscanf (buf, "%lf%1s", f, dummy) == 1 && *dummy == 0 ;
+}
