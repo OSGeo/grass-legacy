57e78b7de18d4d405015573db6e74dd49a6a6169
bot <grass-svn2git@osgeo.org>
1990-05-05 05:06:36 +0200

    added

---
+++ src/mapdev/dig_front/bin_digit.h
@@ -0,0 +1,14 @@
+/* %W% %G% */
+
+#define		TMP_FILE	"/tmp/DIGIT."
+#define		DIGITIZER_CAP	"digitcap"
+#define		DRIVER_DIR	"dig_drivers"
+
+struct driver_desc
+{
+    char  name[128] ;
+    char  device[128] ;
+    char  dig_program[128] ;
+    char  dig_desc[128] ;
+};
+
