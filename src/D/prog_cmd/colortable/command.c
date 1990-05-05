f97d022387df342d757450c4216bde7bbcbae456
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:34 +0200

    added

---
+++ src/D/prog_cmd/colortable/command.c
@@ -0,0 +1,53 @@
+/*  %W%  %G%  */
+
+#include "options.h"
+
+set_default_options()
+{
+	strcpy(map_name, "") ;
+	color = D_translate_color("white") ;
+	lines = 0 ;
+	cols = 0 ;
+}
+
+stash_away(pos, option)
+	int pos ;
+	char *option ;
+{
+	int new_colr ;
+
+	switch(pos)
+	{
+	case NAME:
+		if (! sscanf(option,"%s",map_name) )
+			return(-1) ;
+		break ;
+	case COLOR:
+		new_colr = D_translate_color(option) ;
+		if (new_colr == 0)
+		{
+			printf("Don't know the color %s\n", option) ;
+			printf("Available colors:\n") ;
+			printf("  red      orange      yellow      green\n") ;
+			printf("  blue     indigo      violet      gray\n") ;
+			printf("  white    black\n") ;
+			return(-1) ;
+		}
+		else
+			color = new_colr ;
+		break ;
+	case LINES:
+		if (! sscanf(option,"%d",&lines) )
+			return(-1) ;
+		break ;
+	case COLS:
+		if (! sscanf(option,"%d",&cols) )
+			return(-1) ;
+		break ;
+	default:
+		printf("Unknown option\n") ;
+		return(-1) ;
+		break ;
+	}
+	return(0) ;
+}
