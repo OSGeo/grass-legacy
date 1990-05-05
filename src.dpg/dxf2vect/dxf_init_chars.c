737bfa776fa0e9eb825d3f01be51739913fe5cf7
bot <grass-svn2git@osgeo.org>
1990-05-05 05:15:23 +0200

    added

---
+++ src.dpg/dxf2vect/dxf_init_chars.c
@@ -0,0 +1,18 @@
+#include "dxf2vect.h"
+
+dxf_init_chars ()
+{
+	strcpy (zzero, "  0\n");
+	strcpy (eeight, "  8\n");
+	strcpy (tten, " 10\n");
+	strcpy (ttwenty, " 20\n");
+	strcpy (eelev, " 11\n");
+	strcpy (ttwentyone, " 21\n");
+	strcpy (entitie, "ENTITIES\n");
+	strcpy (polyline, "POLYLINE\n");
+	strcpy (line, "LINE\n");
+	strcpy (text, "TEXT\n");
+	strcpy (point, "POINT\n");
+	strcpy (vertex, "VERTEX\n");
+	strcpy (seqend, "SEQEND\n");
+}
