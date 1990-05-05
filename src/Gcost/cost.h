3c9ed1ee256ab740d8b3bc5068b0840a246eee32
bot <grass-svn2git@osgeo.org>
1990-05-05 05:06:32 +0200

    added

---
+++ src/Gcost/cost.h
@@ -0,0 +1,21 @@
+/* %W% %G% */
+/***************************************************************/
+/*                                                             */
+/*        cost.h    in   ~/src/Gcost                           */  
+/*                                                             */
+/*      This header file defines the data structure of a       */  
+/*      point structure containing various attributes of       */
+/*      a grid cell.                                           */
+/*                                                             */
+/***************************************************************/
+
+struct cost{
+
+    float  min_cost;
+    int row;
+    int col;
+    struct cost *next;
+    struct cost *prev;
+};
+
+/***************************************************************/
