67859758259cd36f211d7a62dbd06d41d4f2b928
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:12 +0200

    added

---
+++ src/neighbors/readcell.c
@@ -0,0 +1,13 @@
+/* %W% %G% */
+#include "gis.h"
+#include "ncb.h"
+
+readcell (fd, row, nrows, ncols)
+{
+    rotate_bufs();
+
+    if (row < nrows)
+	G_get_map_row (fd, ncb.buf[ncb.nsize-1] + ncb.dist, row);
+    else
+	G_zero(ncb.buf[ncb.nsize-1] + ncb.dist, ncols * sizeof(CELL));
+}
