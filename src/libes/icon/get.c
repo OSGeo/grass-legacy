4f12a7cbf1153082f05775af5c3dc8df44cba63d
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:04 +0200

    added

---
+++ src/libes/icon/get.c
@@ -0,0 +1,24 @@
+/* %W% %G% */
+#include "icon.h"
+#include "gis.h"
+
+get_icon (name, mapset, icon)
+    char *name;
+    char *mapset;
+    ICON *icon;
+{
+    FILE *fd;
+    char msg[100];
+
+    if (fd = G_fopen_old ("icons", name, mapset))
+    {
+	read_icon (fd, icon);
+	fclose (fd);
+	return 1;
+    }
+
+    sprintf (msg, "can't read icon file [%s] in [%s]", name, mapset);
+    G_warning (msg);
+
+    return -1;
+}
