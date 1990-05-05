7de50130f7f35a70a2133d64871b17afec60dded
bot <grass-svn2git@osgeo.org>
1990-05-05 05:07:01 +0200

    added

---
+++ src/mapdev/digitizers/altek2/dig_menu.h
@@ -0,0 +1,22 @@
+/*  %W%  %G%  */
+#define dig_menu_lines	16
+
+char *dig_menu[] = {
+" GRASS-DIGIT Version 3.0                                     Digitizing menu",
+"-----------------------------------------------------------------------------",
+"  ALTEK digitizer                               | AMOUNT DIGITIZED",
+"         Key:      Action:                      |   # Lines:",
+"         <p>         digitize point             |   # Area edges:",
+"         <space>     quit digitizing            |   # Sites:",
+"         <u>         update monitor             | - - - - - - - - - - - - - -",
+"                                                |   Total points:",
+"-----------------------------------------------------------------------------",
+"                                                | CURRENT DIGITIZER PARAMS.",
+"                                                |",
+"                                                |",
+"                                                | MODE       TYPE",
+"                                                |   point      line",
+"                                                |   stream     area edge",
+"                                                |"
+} ;
+
