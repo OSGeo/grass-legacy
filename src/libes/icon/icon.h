1bde67abd7bafdd780f2370d2202dc4460166c89
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:03 +0200

    added

---
+++ src/libes/icon/icon.h
@@ -0,0 +1,14 @@
+/* %W% %G% */
+#define ICON struct _icon
+struct _icon
+{
+    int nrows;
+    int ncols;
+    int xref;
+    int yref;
+    char **map;
+} ;
+
+char *ask_icon_old();
+char *ask_icon_new();
+char *ask_icon_any();
