e43a908ce9662c5d9d65e3e255562aec6781159c
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:49 +0200

    added

---
+++ src/paint/Drivers/preview/data.c
@@ -0,0 +1,18 @@
+/* %W% %G% */
+#include "P.h"
+
+Pdata (buf, n) 
+    unsigned char *buf;
+{
+    int *d;
+    int i;
+
+    for (d = data, i = 0; i < n; i++)
+	*d++ = *buf++;
+    send_data (n);
+}
+send_data (n)
+{
+    R_move_abs (left_edge, current_row++);
+    R_raster (n, 1, 1, data);
+}
