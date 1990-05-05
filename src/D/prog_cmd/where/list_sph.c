fa5e86fae38fd3ea8c8683efb486749262509c43
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:11 +0200

    added

---
+++ src/D/prog_cmd/where/list_sph.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+list_spheroids()
+{
+    int i;
+    char *CC_spheroid_name();
+
+    for (i = 0; CC_spheroid_name(i); i++)
+    {
+	if (i%3 == 0) fprintf (stderr,"  ");
+	fprintf (stderr,"%-20s ", CC_spheroid_name(i));
+	if (i%3 == 2) fprintf (stderr,"\n");
+    }
+    fprintf (stderr,"\n");
+}
