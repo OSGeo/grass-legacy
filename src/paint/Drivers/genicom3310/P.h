7a212c515cd61433dd1f8a80cc32a9d318952076
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:00 +0200

    added

---
+++ src/paint/Drivers/genicom3310/P.h
@@ -0,0 +1,16 @@
+/* %W% %G% */
+
+#include <stdio.h>
+
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int nrows, ncols ;
+GLOBAL int quality ;
+GLOBAL int ras_row;
+GLOBAL int ras_nrows;
+GLOBAL unsigned char YELLOW[4][1000];
+GLOBAL unsigned char CYAN[4][1000];
+GLOBAL unsigned char MAGENTA[4][1000];
