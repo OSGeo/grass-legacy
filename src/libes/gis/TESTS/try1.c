56743340c840d1f8e445b8930fbf81867ee80743
bot <grass-svn2git@osgeo.org>
1990-05-05 04:53:36 +0200

    added

---
+++ src/libes/gis/TESTS/try1.c
@@ -0,0 +1,36 @@
+/* %W% %G% */
+#include "gis.h"
+main()
+{
+    char name[30], *mapset;
+    int fd;
+    CELL *cell;
+    int nrows, ncols, row, col;
+
+    /*
+    G_gisinit ("try");
+    */
+
+    mapset = G_ask_cell_old ("Enter cell to dump", name);
+    if (mapset == NULL) exit(0);
+    fd = G_open_cell_old (name, mapset);
+    if (fd < 0)
+    {
+	printf ("OOPS");
+	exit(1);
+    }
+    cell = G_allocate_cell_buf();
+    nrows = G_window_rows();
+    ncols = G_window_cols();
+    for (row = 0; row < nrows; row++)
+    {
+	printf ("%3d: ", row);
+	G_get_map_row (fd, cell, row);
+	for (col = 0; col < ncols; col++)
+		printf ("%ld ", (long) cell[col]);
+	printf ("\n>");
+	/*
+	G_gets(cell);
+	*/
+    }
+}
