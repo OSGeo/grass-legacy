10ff1d551fb7e7d85a3674d227c42bcd2dd362f4
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:56 +0200

    added

---
+++ src/paint/Drivers/epson.lq2500/P.h
@@ -0,0 +1,22 @@
+/* %W% %G% */
+
+#include <stdio.h>
+
+typedef unsigned char COLOR[2][3][1024];
+
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int nrows, ncols ;
+GLOBAL int ras_row;
+GLOBAL int ras_nrows;
+GLOBAL COLOR YELLOW;
+GLOBAL COLOR CYAN;
+GLOBAL COLOR MAGENTA;
+
+#define COLOR_BLACK 0
+#define COLOR_MAGENTA 1
+#define COLOR_CYAN 2
+#define COLOR_YELLOW 4
