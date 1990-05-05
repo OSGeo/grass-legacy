59cfb25296af7f74d307288a6f195a1843a3f40e
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:40 +0200

    added

---
+++ src/libes/imagery/var.c
@@ -0,0 +1,27 @@
+/* %W% %G% */
+/* sum: sum of x
+ * sum2: sum of x squared
+ * n:    number of points
+ */
+
+double
+I_variance (sum, sum2, n)
+    double sum, sum2;
+{
+    if (n < 2)
+	return ( (double) 0.0);
+    else
+	return ((sum2 - sum * sum / n) / (n - 1));
+}
+
+double
+I_stddev (sum, sum2, n)
+    double sum, sum2;
+{
+    double sqrt(), I_variance();
+
+    if (n < 2)
+	return ( (double) -99.0);
+    else
+	return sqrt (I_variance (sum, sum2, n));
+}
