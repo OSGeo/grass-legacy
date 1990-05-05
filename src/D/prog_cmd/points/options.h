ebe3867f7afee912c9ce0fe627f4d5a6a2f94f70
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:42 +0200

    added

---
+++ src/D/prog_cmd/points/options.h
@@ -0,0 +1,39 @@
+/*  %W%  %G%  */
+
+#define COLOR	1
+#define SIZE	2
+#define TYPE	3
+#define INFILE	4
+
+#define TYPE_X	1
+#define TYPE_PLUS	2
+#define TYPE_BOX	3
+#define TYPE_DIAMOND	4
+
+#ifdef MAIN
+	struct variables
+	{
+		char *alias ;
+		int position ;
+	} variables[] = {
+		"size", SIZE,
+		"s", SIZE,
+		"color", COLOR,
+		"c", COLOR,
+		"type", TYPE,
+		"t", TYPE,
+		"file", INFILE,
+		"f", INFILE
+	} ;
+	static int n_variables = 8 ;
+
+	int color ;
+	int size ;
+	int type ;
+	FILE *infile ;
+#else
+	extern int color ;
+	extern int size ;
+	extern int type ;
+	extern FILE *infile ;
+#endif
