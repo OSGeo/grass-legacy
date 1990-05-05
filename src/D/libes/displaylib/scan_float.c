71c36edb23387e0074ba7285e4609dd5eb657fb0
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:37 +0200

    added

---
+++ src/D/libes/displaylib/scan_float.c
@@ -0,0 +1,11 @@
+/*  %W%  %G%  */
+
+D_scan_float (buf, f)
+    char *buf;
+    float *f;
+{
+    char dummy[2];
+
+    *dummy = 0;
+    return sscanf (buf, "%f%1s", f, dummy) == 1 && *dummy == 0 ;
+}
