5ff4714100692af1e5f3c623d5718667afdd7eb5
bot <grass-svn2git@osgeo.org>
1990-05-05 04:56:28 +0200

    added

---
+++ src/gisenv/main.c
@@ -0,0 +1,31 @@
+/* %W%   %G% */
+main(argc,argv) char *argv[];
+{
+    int n;
+    int tty;
+    char *name;
+    char *value;
+    char *G__env_name();
+    char *G__getenv();
+    char xname[30], xvalue[70];
+
+    for (n = 1; n < argc; n++)
+    {
+	if (sscanf (argv[n], "%[^= \t]=%s", xname, xvalue) == 2)
+	    G_setenv (xname, xvalue);
+	else if (sscanf (argv[n], "%[^=]%s", xname, xvalue) == 2)
+	    G_unsetenv (xname);
+	else if (value = G__getenv (argv[n]))
+	    printf ("%s\n", value);
+    }
+    if (argc < 2)
+    {
+	tty = isatty(1);
+	for (n=0; name = G__env_name (n); n++)
+	    if (value = G__getenv(name))
+		if (tty)
+		    printf ("%s=%s\n", name, value);
+		else
+		    printf ("%s='%s'; export %s;\n", name, value, name);
+    }
+}
