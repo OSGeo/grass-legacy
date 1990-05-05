69f439be4eb69e4820d31adcdec4502b94b859a2
bot <grass-svn2git@osgeo.org>
1990-05-05 05:01:01 +0200

    added

---
+++ src/D/fonts/for_grass/fontmap.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+fontmap(mapfile, map)
+    char *mapfile;
+    int map[];
+{
+    FILE *fd;
+    int n;
+    int a,b;
+    char buf[200];
+
+
+    if (strcmp (mapfile, "ls") == 0)
+    {
+	system ("cd ../fonts; ls *.hmp | sed 's/.hmp//' | fmt");
+	return -1;
+    }
+    sprintf (buf, "../fonts/%s.hmp", mapfile);
+	printf("%s\n", buf) ;
+    n = 0;
+    fd = fopen (buf, "r");
+    if (!fd)
+    {
+	perror (buf);
+	return -1;
+    }
+
+    while (fscanf (fd, "%s", buf) == 1)
+    {
+	if (sscanf (buf, "%d-%d", &a, &b) == 2)
+	    while (a <= b)
+		map[n++] = a++;
+	else if (sscanf (buf, "%d", &a) == 1)
+	    map[n++] = a;
+    }
+    fclose (fd);
+    return n;
+}
