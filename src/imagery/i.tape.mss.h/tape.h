9df7f8b0acb007f5c8c1f1d7db79b0c35e0ba790
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:27 +0200

    added

---
+++ src/imagery/i.tape.mss.h/tape.h
@@ -0,0 +1,34 @@
+/* %W% %G% */
+
+#define BIL		1
+#define BSQ		2
+#define UNKNOWN		0
+#define CORRECTED	1
+#define UNCORRECTED	2
+
+#define REC_SIZE	4096
+#define HAVE_IMAGE	0
+#define NO_HEADER	-1
+
+#define TAPE_DIR	011
+#define HEADER		022
+#define ANCILLARY	044
+#define ANNOTATION	0333
+#define IMAGE		0355
+#define TRAILER		0366
+#define REC_SIZE	4096
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int record_size;
+GLOBAL int tape_type;
+GLOBAL int correction;
+GLOBAL int vol;
+GLOBAL int num_bands;
+GLOBAL char date[30];
+GLOBAL char sun_angle[30];
+GLOBAL char mission[3];
+
+#include <stdio.h>
