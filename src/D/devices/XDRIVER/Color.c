5ce6114c1bc5228278ec7399a3d73679bc44e3ec
bot <grass-svn2git@osgeo.org>
1990-05-05 04:59:29 +0200

    added

---
+++ src/D/devices/XDRIVER/Color.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * Set the GC foreground value to the number passed to color. All
+ * subsequent graphics calls will use this number, hence they will be 
+ * drawn in that color's number.
+ *
+ * Called by:
+ *      Color() in ../lib/Color.c
+ */
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+extern int NCOLORS ;
+extern Display *dpy;
+extern GC gc;
+
+color(number)
+	int number ;
+{
+	if (number >= NCOLORS)
+		return ;
+	XSetForeground(dpy,gc, (unsigned long) number);
+}
