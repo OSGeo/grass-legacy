40edab5cc2eea692cfa66b7aae0a7ee1596ad611
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:41 +0200

    added

---
+++ src/paint/Drivers/tek4695/init.c
@@ -0,0 +1,12 @@
+/* %W% %G% */
+#define GLOBAL
+#include "P.h"
+Pinit()
+{
+    ras_row = 0;
+    ras_nrows = 4;
+    esc("");	/* cancel any characters in printer buffer */
+    Poutc ((char)24);
+    esc("4");	/* clear-parameters */
+    esc("C10");	/* foreground black, no background */
+}
