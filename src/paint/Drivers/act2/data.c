aeb12f2c39337e37c4fadec66cb810df89a46f66
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:37 +0200

    added

---
+++ src/paint/Drivers/act2/data.c
@@ -0,0 +1,15 @@
+/* %W% %G% */
+#include "P.h"
+
+Pdata (buf,n)
+    char *buf;
+{
+    int i ;
+
+    Poutc (DATA);
+    Pout (buf, n);
+
+/* Ppictsize() will adjust ncols to multiple of 8. pad with white */
+    for (i = 0; i < padding; i++)
+	Poutc (WHITE);
+}
