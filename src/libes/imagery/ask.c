5e29051b904009f6a98f70e802b35c9f844b4e28
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:33 +0200

    added

---
+++ src/libes/imagery/ask.c
@@ -0,0 +1,84 @@
+/* %W% %G% */
+/******************************************************
+
+NAME:		I_ask()
+
+FUNCTION:	print a prompt, input user response
+
+USAGE:		I_ask (prompt, answer, nullok)
+
+ACTION:		ctrl-d will cause exit
+		leading and trailing white space is removed
+		tabs are replaced with blanks.
+		non-printing chars are removed.
+
+		if (!nullok)
+			null entry causes ask to try again
+
+RETURNS:	0 if NULL answer, 1 otherwise
+
+PARMS:		prompt: prompt to print
+		answer: buffer to read answer into
+			if NULL just wait for c/r
+		nullok: allow RETURN alone
+******************************************************/
+#include <stdio.h>
+I_ask (prompt, answer, nullok)
+    char *prompt;
+    char *answer;
+{
+    char *a;
+    char *b;
+    char buf[200];
+
+    fflush (stdout);
+    fflush (stderr);
+    if (!answer)
+    {
+	answer = buf;
+	nullok = 1;
+    }
+
+    do
+    {
+	fprintf (stderr, "%s", prompt);
+	if (!gets(answer))
+	{
+	    fprintf(stderr, "\n");
+	    exit(1);
+	}
+
+/* collapse white space to single blank */
+/* remove non-printing chars */
+
+	for (a = b = answer; *a = *b++; )
+	{
+	    if (*a == ' ' || *a == '\t')
+	    {
+		*a = ' ';
+		while (*b == ' ' || *b == '\t')
+		    b++;
+	    }
+
+	    if (*a >= ' ' && *a < 0177)
+		a++;
+	}
+
+/* remove leading blank */
+	if (*(a=answer) == ' ')
+	    for (b = a+1; *a++ = *b++; )
+		    ;
+
+/* remove trailing blank */
+	for (b = 0, a = answer; *a; a++)
+	    if (*a != ' ')
+		b = a;
+	if (b++) *b = 0;
+
+	if (strcmp (answer,"exit") == 0)
+	    exit(1);
+
+    } while (!nullok && *answer == 0);
+
+    return (*answer != 0);
+}
