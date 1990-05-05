06c4d3fc8426e264eb89ff14fd8073efd448c9a0
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:03 +0200

    added

---
+++ src/imagery/i.tape.other/bil.c
@@ -0,0 +1,35 @@
+/* %W% %G% */
+/* tape only has 1 file, band interleaved */
+#include "tape.h"
+
+bil()
+{
+    int band;
+    int row;
+
+    bandfd = (int *) G_malloc (nbands * sizeof(int));
+    for (band=0; band < nbands; band++)
+    {
+	if (wantband[band])
+	    bandfd[band] = I_open_band_new (band);
+    }
+
+    printf ("\nadvancing to row %d ...", firstrow); fflush (stdout);
+    I_tape_advance (tapefd, (firstrow-1)*nbands+skiprecords);
+
+    printf ("\nextracting ... "); fflush (stdout);
+    for (row = 0; row < nrows; row++)
+    {
+	G_percent (row, nrows, 10);
+	for (band = 0; band < nbands; band++)
+	{
+	    if(!readbil (row, band)) goto done;
+	}
+    }
+    G_percent (row, nrows, 10);
+done:
+    printf ("\n");
+    for (band=0; band < nbands; band++)
+	if (wantband[band])
+	    I_close_band (bandfd[band], &tape_info, band);
+}
