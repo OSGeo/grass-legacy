418f91445a41eabc48ee9d55c1806afafd05d623
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:26 +0200

    added

---
+++ src/imagery/i.tape.mss.h/rec_type.c
@@ -0,0 +1,35 @@
+/* %W% %G% */
+/*********************************************************************
+
+NAME:		record_type ()
+
+FUNCTION:       print record type
+
+USAGE:		record_type(type)
+		unsigned char type
+
+**********************************************************************/
+#include "tape.h"
+
+record_type(type)
+    unsigned char type;
+{
+    char *name;
+
+    printf ( "\n");
+    switch ((int)type)
+    {
+    case TAPE_DIR:	name = "TAPE_DIR:"; break;
+    case HEADER:	name = "HEADER:"; break;
+    case ANCILLARY:	name = "ANCILLARY:"; break;
+    case ANNOTATION:	name = "ANNOTATION:"; break;
+    case IMAGE:		name = "IMAGE:"; break;
+    case TRAILER:	name = "TRAILER:"; break;
+
+    default:	printf("0%o: ", (int)type);
+		name = "** unknown record type **";
+		break;
+    }
+
+    printf ("%s\n\n", name);
+}
