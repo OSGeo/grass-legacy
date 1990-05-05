17486d72c45cf3c64b6c8f0157b6d2f3f590877f
bot <grass-svn2git@osgeo.org>
1990-05-05 05:07:04 +0200

    added

---
+++ src/mapdev/digitizers/kurta2/dig_menu.h
@@ -0,0 +1,24 @@
+/*  %W%  %G%  */
+#define dig_menu_lines	16
+
+char *dig_menu[] = {
+" GRASS-DIGIT Version 3.0                                       Digitize menu",
+"-----------------------------------------------------------------------------",
+"  KURTA digitizer                               | AMOUNT DIGITIZED",
+"         Key:      Action:                      |   # Lines:",
+"         <p>         digitize point             |   # Area edges:",
+"         <space>     quit digitizing            |   # Sites:",
+"         <u>         update monitor             | - - - - - - - - - - - - - -",
+"                                                |   Total points:",
+"-----------------------------------------------------------------------------",
+"                                                | CURRENT DIGITIZER PARAMS.",
+"                                                |",
+"                                                |",
+"                                                | MODE       TYPE",
+"                                                |    point      line",
+"                                                |    stream     area edge",
+"                                                |",
+"                                                |"
+} ;
+
+
