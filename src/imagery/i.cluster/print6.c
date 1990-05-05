549b32308f0028c047e8f7d90bc9d3d42656996d
bot <grass-svn2git@osgeo.org>
1990-05-05 05:05:34 +0200

    added

---
+++ src/imagery/i.cluster/print6.c
@@ -0,0 +1,15 @@
+/* %W% %G% */
+#include "imagery.h"
+print_distribution (fd,C)
+    FILE *fd;
+    struct Cluster *C;
+{
+    int cat;
+
+    fprintf (fd, "class distribution");
+    for (cat = 0; cat < C->nclasses; cat++)
+    {
+	fprintf (fd, "%s %10ld",cat%5?"":"\n",(long) C->count[cat]);
+    }
+    fprintf (fd, "\n");
+}
