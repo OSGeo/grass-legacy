f4bf7a0b5520387d271a9fa139ef40c4c523fca3
bot <grass-svn2git@osgeo.org>
1990-05-05 04:55:32 +0200

    added

---
+++ src/libes/imagery/OLD/list_group.c
@@ -0,0 +1,42 @@
+#include "imagery.h"
+
+I_list_group (group, ref, fd)
+    char *group;
+    struct Ref *ref;
+    FILE *fd;
+{
+    char buf[80];
+    int n,i,k;
+    int len, tot_len;
+    int max;
+
+    if (ref->nfiles <= 0)
+    {
+	fprintf (fd, "group [%s] is empty\n", group);
+	return;
+    }
+    max = 0;
+    for (i=0; i < ref->nfiles; i++)
+    {
+	sprintf (buf, "%s in %s", ref->file[i].name, ref->file[i].mapset);
+	len = strlen(buf)+4;
+	if (len > max) max = len;
+    }
+    fprintf (fd, "group [%s] references the following cell files\n", group);
+    fprintf (fd, "-------------\n");
+    tot_len = 0;
+    for (i=0; i < ref->nfiles; i++)
+    {
+	sprintf (buf, "%s in %s", ref->file[i].name, ref->file[i].mapset);
+	tot_len += max;
+	if (tot_len > 78)
+	{
+	    fprintf (fd, "\n");
+	    tot_len = max;
+	}
+	fprintf (fd, "%-*s", max, buf);
+    }
+    if (tot_len)
+	fprintf (fd, "\n");
+    fprintf (fd, "-------------\n");
+}
