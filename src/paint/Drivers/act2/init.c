f593a7acbbffc6589a967558f63d72fdf734a6bd
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:37 +0200

    added

---
+++ src/paint/Drivers/act2/init.c
@@ -0,0 +1,16 @@
+/* %W% %G% */
+#define GLOBAL
+#include "P.h"
+Pinit()
+{
+/* set forground text color to blue, background to white */
+    Palpha();
+    Poutc (COLORF);
+    Poutc (COLOR_BLUE);
+    Poutc (COLORB);
+    Poutc (COLOR_WHITE);
+
+/* load default color lookup table */
+    Praster();
+    Poutc (DLOOKUP);
+}
