591408bd30dbca49144b74bdd037738f0934ce1a
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:18 +0200

    added

---
+++ src/sroff/divider.c
@@ -0,0 +1,15 @@
+/* %W% %G% */
+#include "glob.h"
+
+begin_divider()
+{
+	free_text_list (&divider_text);
+	initialize_text_list (&divider_text);
+}
+
+add_divider (buf)
+
+	char *buf;
+{
+	add_text (&divider_text, buf);
+}
