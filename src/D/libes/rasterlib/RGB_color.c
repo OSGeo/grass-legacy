c251d85b55b5727ba0de75da2322cea37968d883
bot <grass-svn2git@osgeo.org>
1990-05-05 05:00:34 +0200

    added

---
+++ src/D/libes/rasterlib/RGB_color.c
@@ -0,0 +1,15 @@
+/*  %W%  %G%  */
+
+#include "graph.h"
+
+R_RGB_color(red, grn, blu)
+	unsigned char red, grn, blu ;
+{
+	int i ;
+	unsigned char f ;
+
+	_send_ident(RGB_COLOR) ;
+	f = red ; _send_char(&f) ;
+	f = grn ; _send_char(&f) ;
+	f = blu ; _send_char(&f) ;
+}
