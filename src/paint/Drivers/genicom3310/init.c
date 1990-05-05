7519c634e52948931a960ebefb068ef74ed0a307
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:02 +0200

    added

---
+++ src/paint/Drivers/genicom3310/init.c
@@ -0,0 +1,24 @@
+/* %W% %G% */
+#define GLOBAL
+#include "P.h"
+Pinit()
+{
+    char *getenv();
+    char *q;
+
+    quality = 1;
+
+    if (q = getenv ("QUALITY"))
+    {
+	if (strcmp (q,"2") == 0) quality = 2;
+	else if (strcmp (q,"3") == 0) quality = 3;
+    }
+
+    ras_row = 0;
+    if (quality == 3)
+	ras_nrows = 6;
+    else
+	ras_nrows = 3;
+    formfeed();
+    esc("[r");	/* reset forms */
+}
