4a86367a5111155ddd632a1b3a52c5615ed39581
bot <grass-svn2git@osgeo.org>
1990-05-05 05:02:38 +0200

    added

---
+++ src/paint/Drivers/act2/rle.c
@@ -0,0 +1,17 @@
+/* %W% %G% */
+#include "P.h"
+
+Prle (buf, n)
+    unsigned char *buf;
+{
+    unsigned char i;
+
+    Poutc (RLE);
+    Pout (buf, n + n);
+    if (padding > 0)
+    {
+	i = padding;
+	Poutc (i);
+	Poutc (WHITE);
+    }
+}
