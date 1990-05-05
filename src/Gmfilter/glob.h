b43d8b5272e49b0523d2bb40081e7fa18048bad1
bot <grass-svn2git@osgeo.org>
1990-05-05 04:57:28 +0200

    added

---
+++ src/Gmfilter/glob.h
@@ -0,0 +1,13 @@
+/* %W% %G% */
+#ifdef MAIN
+#define GLOBAL
+#else
+#define GLOBAL extern
+#endif
+
+GLOBAL int nrows, ncols;
+GLOBAL int buflen;
+GLOBAL int direction;
+GLOBAL int silent;
+GLOBAL int zero_only;
+GLOBAL int preserve_edges;
