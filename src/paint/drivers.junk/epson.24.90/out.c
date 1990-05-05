efd5b720492717b961ec4d334475cc4b2c3099ea
bot <grass-svn2git@osgeo.org>
1990-05-05 05:03:51 +0200

    added

---
+++ src/paint/drivers.junk/epson.24.90/out.c
@@ -0,0 +1,31 @@
+/* %W%   %G% */
+#
+/********************************************************
+
+NAME:		Pout ()
+
+FUNCTION:	output a buffer
+
+USAGE:		Pout (buf, len)
+
+		char buf[]
+
+PARMS:		buf:	buffer of chars to output
+		len:	number of chars to output
+
+NOTES:		
+**********************************************************/
+
+#include "P.h"
+
+#undef Pout
+
+Pout (buf, len)
+    char *buf ;
+{
+    while (len-- > 0)
+    {
+	Poutc (*buf);
+	buf++;
+    }
+}
