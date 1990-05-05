e2847537b37f6684b90e9e302d022ff6933f170a
bot <grass-svn2git@osgeo.org>
1990-05-05 04:59:15 +0200

    added

---
+++ src/D/devices/lib/Raster_int.c
@@ -0,0 +1,9 @@
+Raster_int(num, nrows, array, withzeros, color_type)
+	int num ;
+	int nrows ;
+	unsigned int *array ;
+	int withzeros ;
+	int color_type ;
+{
+	Raster_int_def(num, nrows, array, withzeros, color_type) ;
+}
