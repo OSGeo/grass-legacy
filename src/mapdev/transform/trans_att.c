df1dc1e9aac2607890cb0ef0fcb09910104880b0
bot <grass-svn2git@osgeo.org>
1990-05-14 21:09:41 +0200

    added

---
+++ src/mapdev/transform/trans_att.c
@@ -0,0 +1,52 @@
+
+/*
+*  Transform the current attribute file.
+*
+*
+*  Written by the GRASS Team, 04/13/90, -mh.
+*/
+
+#include <stdio.h>
+#include "dig_atts.h"
+
+int
+transform_att_file(fp_current, fp_new)
+	FILE *fp_current, *fp_new ;
+{
+	char type ;
+	int  cat ;
+	long offset ;
+	double x, y;
+	int  return_value ;
+
+	rewind(fp_current) ;
+
+
+	while( 1 )
+	{
+
+		return_value = read_att (fp_current, &type, &x, &y, &cat, &offset ) ;
+
+		/*  EOF condition  */
+		if (return_value == 1)
+			break ;
+
+		if (return_value < 0)
+		{
+			/*
+			*  Deleted atts 'a' will cause this to occur.
+			*
+			* fprintf( stderr, "Attribute file has unreadable lines in it\n") ;
+			*/
+			continue ;
+		}
+
+		transform_a_into_b( x, y, &(x), &(y) ) ;
+
+		write_att (fp_new, type, x, y, cat) ;
+	}
+
+	return(0) ;
+}
+
+
