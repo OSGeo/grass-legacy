d640554c24b044e096a7d41b43322dcbfb934bc5
bot <grass-svn2git@osgeo.org>
1990-05-15 00:52:21 +0200

    added

---
+++ src/Gstats/global.h
@@ -0,0 +1,16 @@
+#include "gis.h"
+
+#ifndef GLOBAL
+# define GLOBAL extern
+# define INIT(x)
+#else
+# define INIT(x) =(x)
+#endif
+
+GLOBAL int nfiles;
+GLOBAL int nrows;
+GLOBAL int ncols;
+
+GLOBAL int maskfd;
+GLOBAL CELL *mask;
+GLOBAL int (*get_row)();
