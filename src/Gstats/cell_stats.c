ac4eb3ffc1db0fb807fff31595f318468a070451
bot <grass-svn2git@osgeo.org>
1990-05-15 00:52:20 +0200

    added

---
+++ src/Gstats/cell_stats.c
@@ -0,0 +1,69 @@
+#include "global.h"
+
+cell_stats (fd, verbose, non_zero, cell_counts, fmt)
+    int fd[];
+    char *fmt;
+{
+    CELL **cell;
+    int i;
+    int row;
+    double unit_area;
+    int planimetric;
+    int compute_areas;
+    double G_area_of_cell_at_row();
+
+/* allocate i/o buffers for each cell file */
+    cell = (CELL **) G_calloc (nfiles, sizeof (CELL *));
+    for (i = 0; i < nfiles; i++)
+	cell[i] = G_allocate_cell_buf();
+
+/* if we want area totals, set this up.
+ * distinguish projections which are planimetric (all cells same size)
+ * from those which are not (e.g., lat-long)
+ */
+    unit_area = 0.0;
+    if (cell_counts != 1)
+    {
+	switch (G_begin_cell_area_calculations())
+	{
+	case 0: /* areas don't make sense, but ignore this for now */
+	case 1:
+	    planimetric = 1;
+	    unit_area = G_area_of_cell_at_row (0);
+	    break;
+	default:
+	    planimetric = 0;
+	    break;
+	}
+    }
+    compute_areas = (cell_counts != 1) && !planimetric;
+
+/* here we go */
+    initialize_cell_stats(nfiles);
+    if (verbose)
+	fprintf (stderr, "%s:  complete ... ", G_program_name());
+
+    for (row = 0; row < nrows; row++)
+    {
+	if (compute_areas)
+	    unit_area = G_area_of_cell_at_row (row);
+
+	if (verbose)
+	    G_percent (row, nrows, 2);
+
+	for (i = 0; i < nfiles; i++)
+	    if (get_row (fd[i], cell[i], row) < 0)
+		exit(1);
+
+/* read the mask file */
+	if (maskfd >= 0)
+	    G_get_map_row_nomask (maskfd, mask, row);
+
+	update_cell_stats (cell, mask, ncols, unit_area);
+    }
+    if (verbose)
+	G_percent (nrows, nrows, 2);
+
+    sort_cell_stats();
+    print_cell_stats (fmt, non_zero, cell_counts, planimetric);
+}
