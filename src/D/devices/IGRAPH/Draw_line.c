71ee757b9a5941b1c40f3989423f50ed891e93f9
bot <grass-svn2git@osgeo.org>
1990-06-26 21:59:10 +0200

    added

---
+++ src/D/devices/IGRAPH/Draw_line.c
@@ -0,0 +1,18 @@
+
+/*
+ * draw a line between two given points in the current color.
+ *
+ * Called by:
+ *     Cont_abs() in ../lib/Cont_abs.c
+ *
+ *  Written by the GRASS Team in the Winter of 88.
+ *
+ */
+
+ extern  int  WNO ;
+
+draw_line(cur_x, cur_y, x, y)
+{
+	move(WNO, cur_x, cur_y) ;
+	draw(WNO, x, y) ;
+}
