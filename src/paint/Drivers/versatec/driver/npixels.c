bb8f2ada6c6a116cb398377816a1ade82adb41ca
bot <grass-svn2git@osgeo.org>
1990-06-27 22:46:25 +0200

    added

---
+++ src/paint/Drivers/versatec/driver/npixels.c
@@ -0,0 +1,13 @@
+#include "P.h"
+Pnpixels (rows, cols)
+    int *rows, *cols;
+{
+    char *s, *getenv();
+
+    s = getenv ("NPIXELS");
+    if (s == NULL)
+	error ("NPIXELS not set");
+    if (sscanf (s, "%d", cols) != 1 || *cols <= 0)
+	error ("NPIXELS not valid");
+    *rows = 0;
+}
