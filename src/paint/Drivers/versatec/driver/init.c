71dc07bc32dbffefc09097ea75038d56b5d316ab
bot <grass-svn2git@osgeo.org>
1990-07-05 19:03:16 +0200

    added

---
+++ src/paint/Drivers/versatec/driver/init.c
@@ -0,0 +1,32 @@
+#define GLOBAL
+#include "P.h"
+
+Pinit()
+{
+    char *getenv();
+    rasterfile = getenv("RASTERFILE");
+    if (rasterfile == NULL)
+	error ("RASTERFILE not set");
+    sprint_command = getenv ("SPRINT_COMMAND");
+    if (sprint_command == NULL)
+	error ("SPRINT_COMMAND not set");
+
+    rasterfd = creat (rasterfile, 0666);
+    if (rasterfd < 0)
+    {
+	char msg[1024];
+	perror (rasterfile);
+	sprintf (msg, "Pinit: can't create rasterfile %s", rasterfile);
+	error (msg);
+	exit(1);
+    }
+    initialize_rasterfile(rasterfd);
+
+
+    WHITE = Pcolornum (1.0, 1.0, 1.0);
+    BLACK = Pcolornum (0.0, 0.0, 0.0);
+
+
+    nrows = 0;
+    data_buf = NULL;
+}
