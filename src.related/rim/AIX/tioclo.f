0e91bdbbc23a09ace5f3347599240ddb2352b4b2
bot <grass-svn2git@osgeo.org>
1990-08-21 16:23:01 +0200

    added

---
+++ src.related/rim/AIX/tioclo.f
@@ -0,0 +1,16 @@
+      SUBROUTINE TIOCLO(FILE,UNIT,MODE)
+      INCLUDE 'syspar.d'
+C
+C     ***UNIX SYSTEM DEPENDENT ROUTINE ***
+C
+C     CLOSE A TEXT FILE
+C         FILE -- FILE NAME
+C         UNIT -- UNIT NUMBER
+C         MODE -- 'INPUT' OR 'OUTPUT'
+C
+      CHARACTER*(*) FILE
+      CHARACTER*(*) MODE
+C
+      CLOSE(UNIT)
+      RETURN
+      END
