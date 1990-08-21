2c900202659269513013c45026207fffcaf517d4
bot <grass-svn2git@osgeo.org>
1990-08-21 16:26:17 +0200

    added

---
+++ src.related/rim/src/getloc.f
@@ -0,0 +1,21 @@
+      SUBROUTINE GETLOC (INDPTR,NAMREL,LOCID)
+      INCLUDE 'syspar.d'
+C     PURPOSE: GETS THE RSTART VALUE FOR THE INDICATED RELATION
+C              EQUIVALENT TO A RMFIND FOR A RANDOM READ OF A RELATION
+C
+C     INPUT:   INDPTR --   INTERFACE POINTER NUMBER (1 THRU 5)
+C              NAMREL -- RELATION NAME
+C     OUTPUT:  LOCID --  LOCATION POINTER TO FIRST ROW OF THIS
+C                        RELATION
+C
+C      ====>   RIC JOHNSTON ACC UW 85/01/04
+C
+C==================================================
+      INCLUDE 'rimcom.d'
+      INCLUDE 'tupler.d'
+      CALL RMFIND(INDPTR,NAMREL)
+      LOCID = 0
+      IF(RMSTAT.NE.0)RETURN
+      LOCID = RSTART
+      RETURN
+      END
