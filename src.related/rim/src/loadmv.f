90a0d845516e92bed7cd26ed0f46215f67b2b8d3
bot <grass-svn2git@osgeo.org>
1990-08-21 16:26:34 +0200

    added

---
+++ src.related/rim/src/loadmv.f
@@ -0,0 +1,48 @@
+      LOGICAL FUNCTION LOADMV(CCOL,CLEN)
+      INCLUDE 'syspar.d'
+C
+C     CHECK FORMATTED LOADING DATA FOR MISSING VALUES
+C
+C  PARAMETERS:
+C         CCOL----STARTING COLUMN
+C         CLEN----LENGTH
+C
+      INCLUDE 'ascpar.d'
+      INCLUDE 'tokens.d'
+      INCLUDE 'cards.d'
+      INCLUDE 'flags.d'
+C
+C  DECLARATION STATEMENTS
+C
+      LOGICAL EQ
+      INTEGER NULLBF(Z)
+ 
+      LOADMV = .FALSE.
+C
+C     -- CHECK FOR NULL VALUE FLAGS
+C
+      LNB = ASCAN(CRDREC,CCOL,0-CLEN,ABLANK,.FALSE.)
+      IF (LNB.GT.0) LNB = LNB - CCOL + 1
+      IF (LNB.GT.ZC) GOTO 500
+      CALL ZMOVE(NULLBF,BLANK)
+      CALL STRMOV(CRDREC,CCOL,LNB,NULLBF,1)
+ 
+      IF (LNB.EQ.0 .OR. LNB.EQ.KMSSVL) THEN
+         IF (EQ(NULLBF,KMSSVT)) THEN
+           IDI(ITEMS) = ZIMISS
+           IDT(ITEMS) = KXTEXT + KXINT + KXREAL
+           LOADMV = .TRUE.
+           GOTO 500
+         ENDIF
+      ENDIF
+      IF (LNB.EQ.0 .OR. LNB.EQ.KNAPVL) THEN
+         IF (EQ(NULLBF,KNAPVT)) THEN
+           IDI(ITEMS) = ZINAPP
+           IDT(ITEMS) = KXTEXT + KXINT + KXREAL
+           LOADMV = .TRUE.
+         ENDIF
+      ENDIF
+C
+500   CONTINUE
+      RETURN
+      END
