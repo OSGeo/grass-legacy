0940f80e6a3eb0d888f53b99bd21d1ac4cc4a4e8
bot <grass-svn2git@osgeo.org>
1990-08-21 16:23:02 +0200

    added

---
+++ src.related/rim/AIX/tioout.f
@@ -0,0 +1,49 @@
+      SUBROUTINE TIOOUT(FILE,TEXT,LEN,IERR)
+      INCLUDE 'syspar.d'
+C
+C     **UNIX SYSTEM DEPENDENT ROUTINE **
+C
+C  ROUTINE TO WRITE A LINE OF TEXT
+C
+C  PARAMETERS
+C
+C         FILE----UNIT TO WRITE
+C         TEXT----OUTPUT RECORD (PACKED ASCII-TEXT)
+C         LEN-----NUMBER OF CHARACTERS IN TEXT
+C         IERR----ERROR FLAG
+C
+      INTEGER FILE
+      INTEGER TEXT(1)
+      INCLUDE 'files.d'
+ 
+      CHARACTER*1 CHRASC, FFCHAR
+      CHARACTER*(ZPRINL) OULINE
+C
+      FFCHAR = ' '
+      IF (FFFLAG.NE.0) THEN
+         FFCHAR = '1'
+         FFFLAG = 0
+      ENDIF
+C
+C  CONVERT OUTPUT TO TEXT CHARS
+C
+100   OULINE = ' '
+      IF (LEN.LE.0) THEN
+         WRITE(FILE,'(A1)') FFCHAR
+      ELSE
+         L = LEN
+         IF (L.GT.ZPRINL) L = ZPRINL
+         DO 200 I = 1,L
+         CALL GETT(TEXT,I,CH)
+         OULINE(I:I) = CHRASC(CH)
+200      CONTINUE
+C        NO FORMS CONTROL ON TERMINAL
+         IF (NINT.EQ.ZNINT .OR. ULPP.EQ.0) THEN
+            WRITE(FILE,'(A)') OULINE(1:L)
+         ELSE
+            WRITE(FILE,'(A1,A)') FFCHAR, OULINE(1:L)
+         ENDIF
+      ENDIF
+      IERR = 0
+      RETURN
+      END
