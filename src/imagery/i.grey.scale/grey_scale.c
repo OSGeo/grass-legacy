8084ba7c0ee933464e9626039d10bbe61f931eb1
bot <grass-svn2git@osgeo.org>
1990-09-13 12:45:54 +0200

    added

---
+++ src/imagery/i.grey.scale/grey_scale.c
@@ -0,0 +1,16 @@
+#include "gis.h"
+grey_scale(name,mapset)
+    char *name;
+    char *mapset;
+{
+    struct Colors colr;
+    struct Histogram histogram ;
+
+    if(I_get_histogram (name, mapset, &histogram) < 0)
+	return;
+    G_make_histo_grey_scale (&colr, &histogram);
+    G_write_colors (name, mapset, &colr);
+    G_free_histogram (&histogram);
+    G_free_colors (&colr);
+    printf ("[%s in %s] now has a grey scale color table\n", name, mapset);
+}
