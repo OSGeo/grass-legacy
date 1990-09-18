f637de2b4321555be40535fc4dc18b92e6b4ed17
bot <grass-svn2git@osgeo.org>
1990-09-18 18:25:07 +0200

    added

---
+++ src.new/Mdem.extract2/get_prof.c
@@ -0,0 +1,21 @@
+/* %W% %G% */
+#include "usgs.h"
+
+get_profile()
+{
+	double junk;
+	buffer += get_int(&bas_n);
+	if(filestat != blocksize)
+		return(0);
+	buffer += get_int(&bas_e);
+	buffer += get_int(&rows);
+	buffer += get_int(&cols);
+	buffer += get_dfloat(&P_col);
+	buffer += get_dfloat(&P_row);
+	buffer += get_dfloat(&bas_elev);
+
+	buffer += get_dfloat(&junk);
+	buffer += get_dfloat(&junk);
+
+	return(1);
+}
