e425c0b4d8c26e3034b9ede83a01380b5a5ee755
bot <grass-svn2git@osgeo.org>
1990-12-04 00:25:09 +0100

    added

---
+++ src/general/manage/lib/empty.c
@@ -0,0 +1,23 @@
+/* look for at least one file in the element */
+#include "gis.h"
+empty (elem)
+    char *elem;
+{
+    FILE *ls, *popen();
+    char command[1024];
+    char *dir;
+    int any;
+
+    dir = command;
+    sprintf (dir, "ls ");
+    dir += strlen (dir);
+    G__file_name (dir, elem, "", G_mapset());
+
+    any = 0;
+    if (access(dir,0) == 0 && (ls = popen(command, "r")))
+    {
+	any = fgets(command, 10, ls) != 0;
+	pclose (ls);
+    }
+    return any == 0;
+}
