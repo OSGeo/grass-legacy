2de52533f2bf477fdf286fa15a1533aabf0d8faf
bot <grass-svn2git@osgeo.org>
1990-12-04 00:25:25 +0100

    added

---
+++ src/raster/r.report/inter/ask_layers.c
@@ -0,0 +1,37 @@
+#include "global.h"
+ask_layers()
+{
+    char *prompt;
+    char name[256];
+    char *mapset;
+
+    G_clear_screen();
+    printf ("You may run a report on a single raster map, or on many maps.\n");
+    printf ("A single map will produce a simple tabulation report.\n");
+    printf ("Multiple maps will produce a multi-level coincidence tabluation report.\n");
+
+    layer = NULL;
+
+    for (nlayers = 0;;nlayers++)
+    {
+	if (nlayers==0)
+	    prompt="";
+	else if (nlayers==1)
+	{
+	    prompt="Enter a second map, if you want a coincidence tabluation";
+	    G_set_ask_return_msg("if you only want a simple report");
+	}
+	else
+	{
+	    prompt="Enter another map";
+	    G_set_ask_return_msg("if you have selected all the maps for the report");
+	}
+	mapset = G_ask_cell_old (prompt, name);
+	if (mapset == NULL) break;
+
+	layer = (struct layer *) G_realloc (layer, (nlayers+1) * sizeof(struct layer));
+	layer[nlayers].name = G_store(name);
+	layer[nlayers].mapset = G_store(mapset);
+    }
+    if (nlayers == 0) exit(0);
+}
