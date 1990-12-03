e633899ddc0db104193fdeb938cdf1d06702fa7f
bot <grass-svn2git@osgeo.org>
1990-12-04 00:24:28 +0100

    added

---
+++ src/general/g.region/inter/glob.h
@@ -0,0 +1,13 @@
+#include "gis.h"
+
+#ifdef MAIN
+
+    char *window_dir = "windows" ;
+    struct Cell_head cur_window  ;
+
+#endif
+
+extern char *window_dir ;
+extern struct Cell_head cur_window  ;
+
+int lister();
