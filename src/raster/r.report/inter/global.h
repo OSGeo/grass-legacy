6e904a7be44a541c184e16a5a433a30ad4b4a97e
bot <grass-svn2git@osgeo.org>
1990-12-04 00:25:24 +0100

    added

---
+++ src/raster/r.report/inter/global.h
@@ -0,0 +1,33 @@
+#include "gis.h"
+
+struct units
+{
+    char *name;
+    char *code;
+    char marked[2];
+};
+#ifdef GLOBAL
+struct units units[] = {
+	{"square meters","me"},
+	{"square kilometers","k"},
+	{"square miles","mi"},
+	{"acres","a"},
+	{"hectacres","h"},
+	{"cell counts","c"},
+	{"percent cover","p"},
+        {NULL,NULL}};
+#else
+extern struct units units[];
+#endif
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#else
+#define GLOBAL
+#endif
+
+
+GLOBAL char *stats_file;
+GLOBAL char *report_file;
+GLOBAL int nlayers;
+GLOBAL struct layer { char *name; char *mapset;} *layer;
