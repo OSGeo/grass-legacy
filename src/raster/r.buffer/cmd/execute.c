f1e68fc592dd383b902ed39bd9c6b7096446d9e4
bot <grass-svn2git@osgeo.org>
1990-12-04 00:24:41 +0100

    added

---
+++ src/raster/r.buffer/cmd/execute.c
@@ -0,0 +1,25 @@
+#include "distance.h"
+execute_distance()
+{
+    int row, col, nrows;
+    MAPTYPE *ptr;
+
+	/* find the first 1 in each row, and process that row */
+
+    fprintf (stderr, "Finding buffer zones ... ");
+    nrows = 0;
+    for (row = minrow; row <= maxrow; row++)
+    {
+	ptr = map + MAPINDEX(row,mincol);
+	for (col = mincol; col <= maxcol; col++)
+	{
+	    if (*ptr++ == 1)
+	    {
+		G_percent (nrows++, count_rows_with_data, 2);
+		process_row (row, col);
+		break;
+	    }
+	}
+    }
+    G_percent (nrows, count_rows_with_data, 2);
+}
