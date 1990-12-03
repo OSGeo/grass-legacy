36807459791634d2253d1ddeb4a2db817a81f34b
bot <grass-svn2git@osgeo.org>
1990-12-04 00:25:08 +0100

    added

---
+++ src/general/manage/lib/add_elem.c
@@ -0,0 +1,17 @@
+#include "list.h"
+add_element (elem,desc)
+    char *elem;
+    char *desc;
+{
+    int n;
+    int nelem;
+
+    if (*desc == 0) desc = elem;
+
+    n = nlist - 1;
+    nelem = list[n].nelem++;
+    list[n].element = (char **) G_realloc (list[n].element, (nelem+1) * sizeof (char *));
+    list[n].element[nelem] = G_store (elem);
+    list[n].desc = (char **) G_realloc (list[n].desc, (nelem+1) * sizeof (char *));
+    list[n].desc[nelem] = G_store (desc);
+}
