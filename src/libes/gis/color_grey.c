28339d8d8e1f71a5fcda53cea88bd36eb71a8145
bot <grass-svn2git@osgeo.org>
1990-12-28 20:12:21 +0100

    added

--- src/libes/gis/color_grey.c
+++ src/libes/gis/color_grey.c
@@ -1,39 +1,38 @@
 /**********************************************************************
  *
- *  G_make_grey_scale (pcolr, min, max)
+ *  G_make_grey_scale_colors (colors, min, max)
  *
- *   struct Colors *pcolr    struct to hold colors
- *   CELL min,max            min,max color numbers
+ *   struct Colors *colors    struct to hold colors
+ *   CELL min,max             min,max color numbers
  *
- *  Generates grey scale colors that are stored in the pcolr structure. 
+ *  Generates grey scale colors that are stored in the colors structure. 
  *
  **********************************************************************/
 
 #include "gis.h"
 
-G_make_grey_scale (pcolr,min,max)
-    struct Colors *pcolr ;
+G_make_grey_scale_colors (colors,min,max)
+    struct Colors *colors ;
     CELL min,max;
 {
-    int i ;
-    int num;
-    int red, grn, blu;
+    G_init_colors (colors);
+    return G_add_grey_scale_colors (colors,min,max);
+}
 
-    G_init_colors (pcolr);
-    if (max < min)
-	return -1;
+G_make_grey_scale  (colors,min,max) /* for 3.0 compatibility */
+    struct Colors *colors ;
+    CELL min,max;
+{
+    return G_make_grey_scale_colors(colors,min,max);
+}
 
+G_add_grey_scale_colors (colors,min,max)
+    struct Colors *colors ;
+    CELL min,max;
+{
+    if(min > max) return -1;
     if (min == 1) min = 0;
     if (max == -1) max = 0;
-    num = max - min;
-
-    if (num == 0)
-	G_set_color (min, 256/2, 256/2, 256/2, pcolr);
-    else
-	for(i=0; i <= num; i++)
-	{
-	    red = grn = blu = (i / (float)num) * 256 ;
-	    G_set_color (min++, red, grn, blu, pcolr);
-	}
+    G_add_color_rule (min,0,0,0, max,255,255,255, colors) ;
     return 1;
 }
