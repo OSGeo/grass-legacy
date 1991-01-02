df9f88bc4bdba738c363c98762f67eb4f7879b43
bot <grass-svn2git@osgeo.org>
1991-01-02 21:06:15 +0100

    added

---
+++ src/display/d.sites/cmd/options.h
@@ -0,0 +1,13 @@
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int color ;
+GLOBAL int size ;
+GLOBAL int type ;
+GLOBAL FILE *infile ;
+
+#define TYPE_X  1
+#define TYPE_PLUS   2
+#define TYPE_BOX    3
+#define TYPE_DIAMOND    4
