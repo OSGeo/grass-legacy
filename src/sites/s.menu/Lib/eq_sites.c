f550d65a6504cfcdb693502ff7b9d4d60dac8262
bot <grass-svn2git@osgeo.org>
1991-01-02 16:56:21 +0100

    added

---
+++ src/sites/s.menu/Lib/eq_sites.c
@@ -0,0 +1,42 @@
+#include "site.h"
+#include "gis.h"
+equal_sites (a, b)
+    SITE_LIST *a, *b;
+{
+    int equal;
+
+
+    rewind_site_list (a);
+    rewind_site_list (b);
+
+    equal = cmp_sites (a, b);
+
+    rewind_site_list (a);
+    rewind_site_list (b);
+
+    return equal;
+}
+
+cmp_sites (a, b)
+    SITE_LIST *a, *b;
+{
+    int a_north, a_east;
+    int b_north, b_east;
+    char *a_desc;
+    char *b_desc;
+
+    if (strcmp(b->name, a->name) != 0)
+	return 0;
+    if (strcmp(b->desc, a->desc) != 0)
+	return 0;
+    while (next_site (a, &a_north, &a_east, &a_desc))
+    {
+	if (!next_site (b, &b_north, &b_east, &b_desc))
+	    return 0;
+	if (a_north != b_north) return 0;
+	if (a_east  != b_east ) return 0;
+	if (strcmp (a_desc, b_desc) != 0)
+		return 0;
+    }
+    return next_site (b, &b_north, &b_east, &b_desc) == 0;
+}
