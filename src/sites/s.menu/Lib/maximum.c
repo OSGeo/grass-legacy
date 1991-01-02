cd50d86a4568de24b51ad89c365d43d13675ecf7
bot <grass-svn2git@osgeo.org>
1991-01-02 16:56:21 +0100

    added

---
+++ src/sites/s.menu/Lib/maximum.c
@@ -0,0 +1,23 @@
+maximum (array, size)
+
+	register int *array;
+{
+	register int n;
+	register int max;
+	register int a;
+	int x;
+
+	max = *array++;
+	x = 0;
+
+	for (n = 1; n < size; n++)
+	{
+		a = *array++;
+		if (a > max)
+		{
+			max = a;
+			x = n;
+		}
+	}
+	return x;
+}
