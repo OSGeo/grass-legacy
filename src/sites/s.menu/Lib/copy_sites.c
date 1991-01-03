0bec050bebf13d4822bd2e0e67cbeb6c14ce826a
bot <grass-svn2git@osgeo.org>
1991-01-03 17:37:28 +0100

    added

---
+++ src/sites/s.menu/Lib/copy_sites.c
@@ -0,0 +1,26 @@
+#include "site.h"
+#include "gis.h"
+copy_sites (a, b, clipped)
+
+	SITE_LIST *a, *b;
+{
+	int north, east;
+	char *desc;
+	struct Cell_head window;
+
+	if (clipped)
+		if (G_get_window(&window) == -1)
+		{
+			fprintf(stderr,"warning - can't read current region\n");
+			clipped = 0;
+		}
+
+	rewind_site_list (a);
+	initialize_site_list (b);
+
+	strcpy (b->name, a->name);
+	strcpy (b->desc, a->desc);
+	while (next_site (a, &north, &east, &desc))
+		if (!clipped || within_window (north, east, &window))
+			add_site (b, north, east, desc);
+}
