67d24bf8d83d2ed23bc3a2359a3c27d086456303
bot <grass-svn2git@osgeo.org>
1991-01-04 16:01:47 +0100

    added

---
+++ src/mapdev/v.mkquads/sites_quads.c
@@ -0,0 +1,59 @@
+
+
+/*
+*   Creates a sites file of the quad points.
+*   Written by Grass Team, Fall of 88, -mh.
+*/
+
+#include	<stdio.h>
+#include	"quad_structs.h"
+#include	"dig_defines.h"
+
+sites_quads (fp_sites, Q)
+	FILE  *fp_sites ;
+	struct  quads_description  *Q ;
+{
+
+	int  i, k ;
+	int  num_v_rows, num_v_cols ;
+	double  x,  y ;
+	double  lon, lat ;
+	double  lon_shift,  lat_shift ;
+
+	num_v_rows = Q->num_vect_rows ;
+	num_v_cols = Q->num_vect_cols ;
+
+
+	lat_shift =  Q->lat_shift ;
+	lon_shift =  Q->lon_shift ;
+
+
+/*  write out all the vector lengths (x vectors) of the entire grid  */
+
+
+	lat = Q->origin_lat ;
+	for ( i = 0; i < num_v_rows; ++i)
+	{
+		lon = Q->origin_lon ;
+		for (k = 0; k < num_v_cols; ++k)
+		{
+			convert_ll_to_utm( lon, lat, &x, &y, Q) ;
+			write_sites_line( fp_sites, x, y) ;
+			lon += lon_shift ;
+		}
+		lat += lat_shift ;
+	}
+
+	return (0) ;
+
+}
+
+
+write_sites_line( fp_sites, x, y )
+	FILE  *fp_sites ;
+	double   x, y ;
+{
+
+	fprintf( fp_sites, "%d|%d|%d\n", (int)x, (int)y, 1) ;
+}
+
