979a0ed676f2810b4a531a5f77c8f19b30ff477e
bot <grass-svn2git@osgeo.org>
1991-01-04 22:54:35 +0100

    added

---
+++ src/raster/r.neighbors/inter/ask_title.c
@@ -0,0 +1,20 @@
+#include "gis.h"
+
+ask_title (input, output, method, nsize, title)
+    char *input, *output, *method, *title;
+{
+    char default_title[1024];
+
+    sprintf (default_title,"%dx%d neighborhood: %s of %s",
+	    nsize, nsize, method, input);
+
+    do
+    {
+	printf("enter title for <%s> [%s]  ", output, default_title);
+    }
+    while (!G_gets(title));
+
+    G_squeeze (title);
+    if (*title == 0)
+	strcpy (title, default_title);
+}
