1ca73c911cb94f03d5312d587cce892efc8e7bed
bot <grass-svn2git@osgeo.org>
1991-01-07 22:23:04 +0100

    added

---
+++ src/imagery/i.rgb.his/cmd/putoutput.c
@@ -0,0 +1,33 @@
+
+#include "gis.h"
+#include "globals.h"
+
+
+putoutput()
+{
+
+  int i,j;
+  CELL *rowbuf;
+  int band;
+  unsigned char *tempbuf;
+  char tempstr[100];
+
+  if ((rowbuf=G_allocate_cell_buf()) == NULL)
+    G_fatal_error("Unable to allocate cell map buffer.");
+
+  for (band=0; band<Nbands; band++) {
+    tempbuf = offsetO[band];
+    for (i=0; i<Dim.row; i++) {
+      for (j=0; j<Dim.column; j++) {
+	rowbuf[j] = (CELL) *tempbuf;
+	tempbuf++;
+      }
+      G_put_map_row(fd_output[band], rowbuf);
+    }
+    G_close_cell(fd_output[band]);
+    sprintf(tempstr, "Gcell.colors %s grey", outputfiles[band]);
+    system(tempstr);
+  }
+  free(rowbuf);
+
+}
