b34826d69af268919ceaa619efc9b423df949895
bot <grass-svn2git@osgeo.org>
1991-01-16 19:12:20 +0100

    added

---
+++ src/display/d.display/menu_info.c
@@ -0,0 +1,48 @@
+#include "popup.h"
+#include <stdio.h>
+
+information()
+{
+	int answer ;
+	int background_color ;
+	int text_color ;
+	int div_color ;
+	static char *options[] = {
+		"INFORMATION MENU",
+		"histogram of displayed raster map",
+		"RETURN",
+		NULL } ;
+
+	background_color = D_translate_color(BC_INFO) ;
+	text_color       = D_translate_color(TC_INFO) ;
+	div_color        = D_translate_color(DC_INFO) ;
+
+	for(;;)
+	{
+		R_open_driver();
+		tell_em_to_use_mouse() ;
+		answer = D_popup(
+			background_color,
+			text_color,
+			div_color,
+			TOP,
+			LEFT,
+			SIZE,
+			options
+			) ;
+		G_clear_screen() ;
+		R_close_driver();
+		
+		switch(answer)
+		{
+		case 1:
+			histo_plot() ;
+			break ;
+		case 2:
+			return(0) ;
+			break ;
+		default:
+			break ;
+		}
+	}
+}
