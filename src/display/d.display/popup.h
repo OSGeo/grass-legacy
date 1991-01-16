ae8ee1df51df9986857af97d33fdb969eda02fa2
bot <grass-svn2git@osgeo.org>
1991-01-16 17:41:02 +0100

    added

---
+++ src/display/d.display/popup.h
@@ -0,0 +1,27 @@
+#define  TOP	10
+#define  LEFT	10
+#define  SIZE	3
+
+#define BC_EXIT	"red"
+#define TC_EXIT	"white"
+#define DC_EXIT	"white"
+
+#define BC_LEG	"black"
+#define TC_LEG	"white"
+#define DC_LEG	"white"
+
+#define BC_DISP	"black"
+#define TC_DISP	"white"
+#define DC_DISP	"white"
+
+#define BC_INFO	"black"
+#define TC_INFO	"white"
+#define DC_INFO	"white"
+
+#define BC_MAIN	"black"
+#define TC_MAIN	"white"
+#define DC_MAIN	"white"
+
+#define BC_WIND	"black"
+#define TC_WIND	"white"
+#define DC_WIND	"white"
