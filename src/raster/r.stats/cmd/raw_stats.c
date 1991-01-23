26705973878ec574eea071bb3956ac37013670e2
bot <grass-svn2git@osgeo.org>
1991-01-23 21:48:20 +0100

    added

---
+++ src/raster/r.stats/cmd/raw_stats.c
@@ -0,0 +1,65 @@
+#include "global.h"
+
+raw_stats (fd, verbose, non_zero, with_coordinates, with_rowcol)
+    int fd[];
+{
+    CELL **cell;
+    register int i;
+    int row, col;
+    double G_row_to_northing(), G_col_to_easting();
+    struct Cell_head window;
+    char nbuf[100], ebuf[100];
+
+/* allocate i/o buffers for each cell file */
+    cell = (CELL **) G_calloc (nfiles, sizeof (CELL *));
+    for (i = 0; i < nfiles; i++)
+	cell[i] = G_allocate_cell_buf();
+
+/* get window */
+    if (with_coordinates)
+	G_get_set_window (&window);
+
+/* here we go */
+    if (verbose)
+	fprintf (stderr, "%s:  complete ... ", G_program_name());
+
+    for (row = 0; row < nrows; row++)
+    {
+	if (verbose)
+	    G_percent (row, nrows, 2);
+	for (i = 0; i < nfiles; i++)
+	    if (get_row (fd[i], cell[i], row) < 0)
+		exit(1);
+	if (maskfd >= 0)
+	    G_get_map_row_nomask (maskfd, mask, row);
+
+	if (with_coordinates)
+	    G_format_northing (G_row_to_northing(row+.5, &window), nbuf, -1);
+
+	for (col = 0; col < ncols; col++)
+	{
+	    if (!mask[col]) continue;
+	    if (non_zero)
+	    {
+		for (i = 0; i < nfiles; i++)
+		    if (cell[i][col])
+			break;
+		if (i == nfiles)
+		    continue;
+	    }
+	    if (with_coordinates)
+	    {
+		G_format_easting (G_col_to_easting(col+.5, &window), ebuf, -1);
+		printf ("%s%s%s%s", ebuf,fs,nbuf,fs);
+	    }
+	    if (with_rowcol)
+		printf ("%d%s%d%s", col+1,fs,row+1,fs);
+	    printf ("%ld", (long)cell[0][col]);
+	    for (i = 1; i < nfiles; i++)
+		printf ("%s%ld", fs, (long)cell[i][col]);
+	    printf ("\n");
+	}
+    }
+    if (verbose)
+	G_percent (nrows, nrows, 2);
+}
