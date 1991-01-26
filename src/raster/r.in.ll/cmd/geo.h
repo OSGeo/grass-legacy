96b6f509c1f38dd9d082ffe5b2256a78e39578b3
bot <grass-svn2git@osgeo.org>
1991-01-26 18:36:08 +0100

    added

---
+++ src/raster/r.in.ll/cmd/geo.h
@@ -0,0 +1,13 @@
+#include "gis.h"
+struct GEO
+{
+    double lon;
+    double lat;
+    double lat_res;
+    double lon_res;
+    int ncols;
+    int nrows;
+    int bpc;
+    int sflag;
+    double a,e;	/* spheroid parameters */
+};
