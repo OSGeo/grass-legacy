5bad0c01204435bdf90b105b2c5d2f239e278763
bot <grass-svn2git@osgeo.org>
1991-02-20 16:03:37 +0100

    added

---
+++ src/raster/r.reclass/inter/do_reclass.c
@@ -0,0 +1,56 @@
+#include "gis.h"
+
+do_reclass (old_name, old_mapset, new_name, cats, new_cats, table)
+    char *old_name, *old_mapset, *new_name;
+    struct Categories *cats, *new_cats;
+    long *table;
+{
+    FILE *fd;
+    char *tempfile;
+    long n;
+    int stat;
+    char command[1024];
+    char *c,*t;
+
+/* write reclass rules to a temp file */
+    tempfile = G_tempfile();
+    fd = fopen (tempfile, "w");
+    if (fd == NULL)
+	G_fatal_error ("can't open any tempfiles");
+    for (n = 0; n <= cats->num; n++)
+	if (table[n])
+	    fprintf (fd, "%ld = %ld\n", (long) n, (long) table[n]);
+    fclose (fd);
+
+/* build the reclass command */
+    sprintf (command, "r.reclass input='%s' 'output=%s'",
+	G_fully_qualified_name(old_name, old_mapset), new_name);
+    if (new_cats->title[0])
+    {
+	strcat (command, " title='");
+	for(c = command; *c; c++)
+		;
+	for (t = new_cats->title; *t; t++)
+	{
+	    if (*t == '\'')
+	    {
+		*c++ = '\'';
+		*c++ = '"';
+		*c++ = '\'';
+		*c++ = '"';
+	    }
+	    *c++ = *t;
+	}
+	*c++ = '\'';
+	*c = 0;
+    }
+    strcat (command, " < ");
+    strcat (command, tempfile);
+
+/* run reclass */
+    stat = system (command);
+    unlink (tempfile);
+    if (stat == 0)
+	G_write_cats (new_name, new_cats);
+    return stat;
+}
