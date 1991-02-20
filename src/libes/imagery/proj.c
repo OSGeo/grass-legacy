2721e9d1872e1f76660e7e4304b5de44b2cd59fd
bot <grass-svn2git@osgeo.org>
1991-02-20 21:57:27 +0100

    added

--- src/libes/imagery/proj.c
+++ src/libes/imagery/proj.c
@@ -3,13 +3,13 @@ I_must_be_imagery_projection()
 {
     int proj;
     char *G_projection_name();
-    if ((proj = G_projection()) == 0)
+    if ((proj = G_projection()) == PROJECTION_XY)
 	return;
 
     printf ("ERROR: LOCATION %s has a projection of %d (%s)\n",
-	G_location(), proj, G_projection_name(proj));
+	G_location(), proj, G_database_projection_name());
     printf ("\nImagery data must be extracted into a location\n");
-    printf ("which has a projection of 0 (row-column only)\n");
+    printf ("which has a projection of %d (row-column only)\n", PROJECTION_XY);
     printf ("\nUnable to proceed\n");
     exit(1);
 }
