666d8c5c8c2414cba0d1ea1e03b7355366810241
bot <grass-svn2git@osgeo.org>
1991-02-22 16:50:28 +0100

    added

--- src/libes/gis/color_hist.c
+++ src/libes/gis/color_hist.c
@@ -1,29 +1,68 @@
-#include "gis.h"
-/***************************************************************
- * G_make_histo_grey_scale (colr, histo)
+/**********************************************************************
  *
- *  struct Histogram *histo;
- *  struct Colors *colr;
+ *  G_make_histogram_eq_colors (colors, statf)
  *
- *  given the histogram (histo),  the colr table
- *  is filled with a contrast stretched grey scale
- ***************************************************************/
-G_make_histo_grey_scale (colr, histo)
-    struct Colors *colr;
-    struct Histogram *histo;
-{
-    unsigned char *map, *xm;
-    CELL min,max;
-    CELL i;
-    int grey;
+ *   struct Colors *colors      struct to hold colors
+ *   struct Cell_stats *statf   cell stats info
+ *
+ *  Generates histogram equalized grey scale from
+ *  cell stats structure info.
+ *  Color range is 0-255.
+ *
+ **********************************************************************/
 
-    G_histogram_eq (histo, &map, &min, &max);
-    G_init_colors (colr);
-    xm = map;
-    for (i = min; i <= max; i++)
+#include "gis.h"
+G_make_histogram_eq_colors (colors, statf)
+    struct Colors *colors;
+    struct Cell_stats *statf;
+{
+    long count, total;
+    CELL prev,cat;
+    double span, sum;
+    int first;
+    int x, grey;
+
+    G_init_colors (colors);
+    G_add_color_rule ((CELL) 0, 0, 0, 0, (CELL) 0, 0, 0, 0, colors);
+
+    total = 0;
+
+    G_rewind_cell_stats (statf);
+    while (G_next_cell_stat (&cat, &count, statf))
+	if (cat && count > 0)
+	    total += count;
+    if (total <= 0)
+	return ;
+
+    span = total/256.0;
+    first = 1;
+    grey = 0;
+    sum = 0.0;
+
+    G_rewind_cell_stats (statf);
+    while (G_next_cell_stat (&cat, &count, statf))
     {
-	grey = *xm++;
-	G_set_color (i, grey, grey, grey, colr);
+	if (cat == 0 || count <= 0)
+	    continue;
+	x = (sum + (count/2.0))/span;
+	if (x < 0) x = 0;
+	else if (x > 255) x = 255;
+	sum += count;
+	if (first)
+	{
+	    prev = cat;
+	    grey = x;
+	    first = 0;
+	}
+	else if (grey != x)
+	{
+	    G_add_color_rule (prev, grey, grey, grey, cat-1, grey, grey, grey, colors);
+	    grey = x;
+	    prev = cat;
+	}
     }
-    free (map);
+    if (!first)
+    {
+	G_add_color_rule (prev, grey, grey, grey, cat, grey, grey, grey, colors);
+    }
 }
