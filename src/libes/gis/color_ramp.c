4c64b2ab128ffc993e7b51064a896b784bf8ceb0
bot <grass-svn2git@osgeo.org>
1991-02-27 18:13:35 +0100

    added

--- src/libes/gis/color_ramp.c
+++ src/libes/gis/color_ramp.c
@@ -1,79 +1,58 @@
 /**********************************************************************
  *
- *  G_make_color_ramp (pcolr, min, max)
+ *  G_make_ramp_colors (colors, min, max)
  *
- *   struct Colors *pcolr    struct to hold colors
+ *   struct Colors *colors   struct to hold colors
  *   CELL min,max            min,max color numbers
  *
- *  Generates color ramp that is stored in the pcolr structure. 
+ *  Generates color ramp that is stored in the colors structure. 
  *
  **********************************************************************/
 
 #include "gis.h"
 
-G_make_color_ramp  (pcolr,min,max)
-    struct Colors *pcolr ;
+G_make_ramp_colors  (colors,min,max)
+    struct Colors *colors ;
     CELL min,max;
 {
-    float incr ;
-    int i ;
-    int red_end, grn_end, blu_end ;
-    float intens ;
-    int num;
-    int red, grn, blu;
+    G_init_colors (colors);
+    return G_add_ramp_colors  (colors,min,max) ;
+}
 
-    G_init_colors (pcolr);
+G_make_color_ramp  (colors,min,max) /* for 3.0 compatibility */
+    struct Colors *colors ;
+    CELL min,max;
+{
+    return G_make_ramp_colors(colors,min,max);
+}
+
+G_add_ramp_colors  (colors,min,max)
+    struct Colors *colors ;
+    CELL min,max;
+{
+    CELL blu1, blu2, grn1, grn2, red1, red2;
+    double delta;
+
     if (max < min)
 	return -1;
 
     if (min == 1) min = 0;
     if (max == -1) max = 0;
-    num = max - min + 1;
+    delta = (double)max - (double)min;
 
-    blu_end = num / 3 ;
-    grn_end = 2 * blu_end ;
-    red_end = num-1 ;
-    incr = 3.0 / (float)num;
+    red1 = min;
+    red2 = min + delta/3;
+    grn1 = red2 + 1;
+    grn2 = min + 2*delta/3;
+    blu1 = grn2 + 1;
+    blu2 = max;
 
-    intens = 0.0 ;
-    i = 0 ;
-    while(i<=blu_end)
-    {
-	intens += incr ;
-	intens = (intens < 1.0) ? intens : 1.0 ;
+    if (red1 <= red2)
+	G_add_color_rule (red1, 0, 0, 0, red2, 0, 0, 255, colors);
+    if (grn1 <= grn2)
+	G_add_color_rule (grn1, 0, 0, 0, grn2, 0, 255, 0, colors);
+    if (blu1 <= blu2)
+	G_add_color_rule (blu1, 0, 0, 0, blu2, 255, 0, 0, colors);
 
-	blu = intens * 256;
-	grn = 0;
-	red = 0;
-
-	G_set_color ((CELL)(i+min), red, grn, blu, pcolr);
-	i++ ;
-    }
-    intens = 0.0 ;
-    while(i<=grn_end)
-    {
-	intens += incr ;
-	intens = (intens < 1.0) ? intens : 1.0 ;
-
-	grn = intens * 256;
-	blu = 0;
-	red = 0;
-
-	G_set_color ((CELL)(i+min), red, grn, blu, pcolr);
-	i++ ;
-    }
-    intens = 0.0 ;
-    while(i<=red_end)
-    {
-	intens += incr ;
-	intens = (intens < 1.0) ? intens : 1.0 ;
-
-	red = intens * 256;
-	blu = 0;
-	grn = 0;
-
-	G_set_color ((CELL)(i+min), red, grn, blu, pcolr);
-	i++ ;
-    }
     return 1;
 }
