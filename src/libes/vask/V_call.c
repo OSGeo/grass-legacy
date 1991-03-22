391066578affd1216e18d10d10266ab040befbae
bot <grass-svn2git@osgeo.org>
1991-03-22 22:35:56 +0100

    added

--- src/libes/vask/V_call.c
+++ src/libes/vask/V_call.c
@@ -1,83 +1,82 @@
-/* %W% %G% */
 #define TELLME(x)	fprintf(stderr,"V_call %d\n",x) ;
 #
 /***********************************************************************
 
-NAME:		V_call()
+NAME:       V_call()
 
-FUNCTION:	Interactively allow the user to enter answers into all
-			available fields (as previously defined).  
-			Answer fields have been created with calls to V_ques()
-			Information fields have been created using V_const()
-			General text has been created with calls to V_line()
+FUNCTION:   Interactively allow the user to enter answers into all
+            available fields (as previously defined).  
+            Answer fields have been created with calls to V_ques()
+            Information fields have been created using V_const()
+            General text has been created with calls to V_line()
 
-USAGE:		V_call()
+USAGE:      V_call()
 
 PARAMETERS:
 
-RETURNS:	1 user entered ESC to continue
-		0 user entered ctrl-C to cancel
+RETURNS:    1 user entered ESC to continue
+        0 user entered ctrl-C to cancel
 
-ALGORITHM:	
-		|	Zero out screen answer locations
-		|	Initial curses screens
-		|	Display text, constants, and answer fields
-		|		Write text      (results from V_line() calls)  to curses window
-		|		Write constants (results from V_const() calls) to curses window
-		|		Write answers   (results from V_ques() calls)  to curses window
-		|	Take commands from the keyboard
-		|		switch on commands:
-		|			case CR: case NL: case UP: case ESC:
-		|				switch on answer type
-		|					case string
-		|						remove trailing non-alphanumeric characters
-		|						copy answer to target denoted in V_ques() call
-		|						blank out screen line
-		|						copy target to curses window
-		|					case integer
-		|						run atoi on answer, putting results in target 
-		|							denoted in V_ques() call
-		|						blank out screen line
-		|						printf target to curses window
-		|					case long
-		|						run atol on answer, putting results in target 
-		|							denoted in V_ques() call
-		|						blank out screen line
-		|						printf target to curses window
-		|					case float, double
-		|						run sscanf on answer, putting results in target 
-		|							denoted in V_ques() call
-		|						blank out screen line
-		|						printf target to curses window
-		|					default:
-		|						do nothing
-		|				if ESC return from V_call()
-		|				if UP  shift to previous question
-		|				if CR or NL  shift to next question
-		|			case BS:   Move cursor back one column in current question
-		|			case FS:   Move cursor forward one column in current 
-		|				question
-		|			case RPLT: Replot the current screen image
-		|			case DUMP: Dump (append) the current window to the user's 
-		|				home dir.
-		|			default:   If an alphanumeric, put that char on the screen 
-		|				and in the current answer field
-		|	call V_exit  (erase screen and exit curses)
+ALGORITHM:  
+        |   Zero out screen answer locations
+        |   Initial curses screens
+        |   Display text, constants, and answer fields
+        |       Write text      (results from V_line() calls)  to curses window
+        |       Write constants (results from V_const() calls) to curses window
+        |       Write answers   (results from V_ques() calls)  to curses window
+        |   Take commands from the keyboard
+        |       switch on commands:
+        |           case CR: case NL: case UP: case ESC:
+        |               switch on answer type
+        |                   case string
+        |                       remove trailing non-alphanumeric characters
+        |                       copy answer to target denoted in V_ques() call
+        |                       blank out screen line
+        |                       copy target to curses window
+        |                   case integer
+        |                       run atoi on answer, putting results in target 
+        |                           denoted in V_ques() call
+        |                       blank out screen line
+        |                       printf target to curses window
+        |                   case long
+        |                       run atol on answer, putting results in target 
+        |                           denoted in V_ques() call
+        |                       blank out screen line
+        |                       printf target to curses window
+        |                   case float, double
+        |                       run sscanf on answer, putting results in target 
+        |                           denoted in V_ques() call
+        |                       blank out screen line
+        |                       printf target to curses window
+        |                   default:
+        |                       do nothing
+        |               if ESC return from V_call()
+        |               if UP  shift to previous question
+        |               if CR or NL  shift to next question
+        |           case BS:   Move cursor back one column in current question
+        |           case FS:   Move cursor forward one column in current 
+        |               question
+        |           case RPLT: Replot the current screen image
+        |           case DUMP: Dump (append) the current window to the user's 
+        |               home dir.
+        |           default:   If an alphanumeric, put that char on the screen 
+        |               and in the current answer field
+        |   call V_exit  (erase screen and exit curses)
 
-CALLS:		
-		V_init()        Vask routine to initialize curses
-		V_exit()        Vask routine to exit curses
-		V__dump_window()   V_ask
-		V__remove_trail()   V_ask
-		addch()         curses routine
-		addstr()        curses routine
-		move()          curses routine
-		putc()          curses routine
-		refresh()       curses routine
-		getch()         curses routine
-		wrefresh()      curses routine
-		sprintf()       UNIX
-		strcpy()        UNIX
+CALLS:      
+        V_init()        Vask routine to initialize curses
+        V_exit()        Vask routine to exit curses
+        V__dump_window()   V_ask
+        V__remove_trail()   V_ask
+        addch()         curses routine
+        addstr()        curses routine
+        move()          curses routine
+        putc()          curses routine
+        refresh()       curses routine
+        getch()         curses routine
+        wrefresh()      curses routine
+        sprintf()       UNIX
+        strcpy()        UNIX
 
 ***********************************************************************/
 
@@ -126,12 +125,10 @@ V_call()
     long atol() ;
     int y,x;		/* shapiro */
     char temp[100];
-    char *fmt();
-    char *f;
 
 
 /* Zero out screen answer locations */
-    for(incr=0; incr<24; incr++)
+    for(incr=0; incr<MAX_ANSW; incr++)
 	    for(incr2=0; incr2<80; incr2++)
 		    scr_answ[incr].position[incr2] = 000 ;
 
@@ -163,13 +160,13 @@ V_call()
 		addstr (temp) ;
 		break ;
 	case 'f':
-		f = fmt ("f", V__.constant[at_constant].decimal_places);
-		sprintf(temp, f, *V__.constant[at_constant].targetptr.f) ;
+		fmt (temp, V__.constant[at_constant].decimal_places,
+		    (double)*V__.constant[at_constant].targetptr.f) ;
 		addstr (temp) ;
 		break ;
 	case 'd':
-		f = fmt ("lf", V__.constant[at_constant].decimal_places);
-		sprintf(temp, f, *V__.constant[at_constant].targetptr.d) ;
+		fmt (temp, V__.constant[at_constant].decimal_places,
+		    (double)*V__.constant[at_constant].targetptr.d) ;
 		addstr (temp) ;
 		break ;
 	default:
@@ -208,14 +205,12 @@ V_call()
 		addstr (ANSWER);
 		break ;
 	case 'f':
-		f = fmt ("f", V__.usr_answ[at_answer].decimal_places);
-		sprintf(ANSWER,f,*TARGET.f) ;
+		fmt (ANSWER, V__.usr_answ[at_answer].decimal_places, (double)*TARGET.f);
 		RELINE ;
 		addstr (ANSWER);
 		break ;
 	case 'd':
-		f = fmt ("lf", V__.usr_answ[at_answer].decimal_places);
-		sprintf(ANSWER,f,*TARGET.d) ;
+		fmt (ANSWER, V__.usr_answ[at_answer].decimal_places, (double)*TARGET.d);
 		RELINE ;
 		addstr (ANSWER);
 		break ;
@@ -275,13 +270,18 @@ V_call()
 	switch (newchar)  
 	{
 	case CR: case NL: case UP: case ESC: case CTRLC:
+		V__remove_trail(LENGTH, ANSWER) ;
 		switch (TYPE) 
 		{
 		case 's':
+		    /*
 			ans_col = (LENGTH) ; 
 			while((ANSWER[--ans_col] < 041) || (ANSWER[ans_col] > 0175));
+			ANSWER[++ans_col] = 000 ;
+		    */
+		    /*
 			V__remove_trail(LENGTH, ANSWER) ;
-/*					ANSWER[++ans_col] = 000 ; */
+		    */
 			strcpy(TARGET.c, ANSWER) ;
 			RELINE ;
 			addstr(TARGET.c) ;
@@ -301,16 +301,16 @@ V_call()
 		case 'f':
 			sscanf (ANSWER,"%f",TARGET.f);
 			RELINE ;
-			f = fmt ("f", V__.usr_answ[at_answer].decimal_places);
-			sprintf(ANSWER, f, *TARGET.f) ;
+			fmt (ANSWER, V__.usr_answ[at_answer].decimal_places,
+				(double)*TARGET.f);
 			sscanf (ANSWER,"%f",TARGET.f);
 			addstr (ANSWER) ;
 			break ;
 		case 'd':
 			sscanf (ANSWER,"%lf",TARGET.d);
 			RELINE ;
-			f = fmt ("lf", V__.usr_answ[at_answer].decimal_places);
-			sprintf(ANSWER, f, *TARGET.d) ;
+			fmt (ANSWER, V__.usr_answ[at_answer].decimal_places,
+				(double)*TARGET.d);
 			sscanf (ANSWER,"%lf",TARGET.d);
 			addstr (ANSWER) ;
 			break ;
@@ -385,17 +385,20 @@ V_intrpt_msg (msg)
 }
 
 
-static char *
-fmt(s,n)
+static
+fmt(s,n,x)
     char *s;
+    double x;
 {
-    static char buf[20];
+    char buf[20];
 
-    if (n > 0)
-	sprintf (buf, "%%.%d%s", n, s);
+    if (n >= 0)
+	sprintf (buf, "%%.%dlf", n);
     else
-	sprintf (buf, "%%.0%s", s);
-    return buf;
+	strcpy (buf, "%lf");
+    sprintf (s, buf, x);
+    if (n < 0)
+	V__trim_decimal(s);
 }
 
 static
