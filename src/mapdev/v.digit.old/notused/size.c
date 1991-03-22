2786b126857bc2331c19b58681c5c1895d4937ed
bot <grass-svn2git@osgeo.org>
1991-03-22 21:32:56 +0100

    added

---
+++ src/mapdev/v.digit.old/notused/size.c
@@ -0,0 +1,9 @@
+#include "digit.h"
+
+main ()
+{
+    fprintf (stderr, "P_LINE: %d\n", sizeof (P_LINE));
+    fprintf (stderr, "P_AREA: %d\n", sizeof (P_AREA));
+    fprintf (stderr, "P_NODE: %d\n", sizeof (P_NODE));
+    fprintf (stderr, "P_ATT: %d\n", sizeof (P_ATT));
+}
