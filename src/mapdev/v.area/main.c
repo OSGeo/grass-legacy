f782bd0cf0a13b08638e2e3beccae17f0efc9497
bot <grass-svn2git@osgeo.org>
1991-03-23 21:22:49 +0100

    added

---
+++ src/mapdev/v.area/main.c
@@ -0,0 +1,136 @@
+/*
+ *   Darea
+ *
+ *   Usage:  Darea [-fill] vector-file-name [color]
+ *
+ */
+
+#include "gis.h"
+struct Cell_head window ;
+
+main(argc, argv)
+    int argc ;
+    char **argv ;
+{
+    char window_name[64] ;
+    int t, b, l, r ;
+    char *mapset;
+    char name[20];
+    char color[8];
+    int fill;
+    int length;
+    static int cflag = {0};
+    char gisbase[256];
+    char command[512];
+    char Dvect_color[8];
+    int where_am_i();
+    struct Flag *flag1 ;
+	struct Option *opt1 ;
+	struct Option *opt2 ;
+
+	/* Define the different options */
+
+	opt1 = G_define_option() ;
+	opt1->key        = "map";
+	opt1->type       = TYPE_STRING;
+	opt1->required   = YES;
+	opt1->description= "input vector filename" ;
+
+    opt2 = G_define_option() ;
+	opt2->key        = "color";
+	opt2->type       = TYPE_STRING;
+	opt2->required   = NO;
+	opt2->answer     = "red";
+	opt2->description= "set the color";
+
+/* Define the different flags */
+
+	flag1 = G_define_flag() ;
+	flag1->key         = 'f' ;
+	flag1->description = "fill" ;
+
+
+
+/* Initialize the GIS calls */
+    G_gisinit(argv[0]) ;
+
+	fill = 0;
+
+	if (G_parser(argc, argv) < 0)
+		exit(-1);
+
+/* Check the length of the "name" to ensure it is not too long */
+	length = strlen(opt1->answer);
+	if (length > 19)
+	{
+fprintf(stderr,"\nFile name:'%s' is LONGER than 19 chars\n\n",opt1->answer);
+		 exit(-1);
+	}
+	strcpy(name,opt1->answer);
+
+/* Check the length of the "color" to ensure it is not too long */
+	length = strlen(opt2->answer);
+	if (length > 7)
+	{
+		fprintf(stderr,"\nColor:  '%s' is LONGER than the allowable 7 characters.\n\n", opt2->answer);
+		cflag = 1;
+    }
+	strcpy(color,opt2->answer);
+
+
+	fill = flag1->answer;
+
+/* Determine the "mapset" based on your order of mapsets */
+    if ( ! (mapset = G_find_vector2(name,"")) )
+     {
+      fprintf(stderr,"\nVector file name:  '%s' NOT found.\n\n",name);
+      exit(-1);
+     }
+      
+    if (cflag)
+     {
+      fprintf(stderr,"\nThe default color 'red' will be used.\n\n");
+     }
+
+/* Check if color is "white" and fill option is "off" */
+    if ( (strcmp(color,"white")==0) && (fill!=1) )
+      strcpy(Dvect_color,"red");
+    else
+      strcpy(Dvect_color,"white");
+
+/* Perform "d.vect" for vector file "name" */
+    strcpy(gisbase,G_gisbase() );
+    sprintf(command,"d.vect %s color=%s",name,Dvect_color);
+    system(command);
+
+    R_open_driver();
+
+    if (D_get_cur_wind(window_name))
+	G_fatal_error("No current graphics window") ;
+
+    if (D_set_cur_wind(window_name))
+	G_fatal_error("Current graphics window not available") ;
+
+/* Read in the map window associated with window */
+    G_get_window(&window) ;
+
+    if (D_check_map_window(&window))
+	G_fatal_error("Setting map window") ;
+
+    if (G_set_window(&window) == -1) 
+	G_fatal_error("Current graphics window not settable") ;
+
+/* Determine conversion factors */
+    if (D_get_screen_window(&t, &b, &l, &r))
+	G_fatal_error("Getting screen window") ;
+    if (D_do_conversions(&window, t, b, l, r))
+	G_fatal_error("Error in calculating conversions") ;
+    
+    where_am_i(name,mapset,color,fill,Dvect_color) ;
+
+    fprintf(stderr,"\n") ;
+
+    R_close_driver();
+
+    exit(1);
+}
