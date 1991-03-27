929653432cca357b537e40fbc76b2126d60a8232
bot <grass-svn2git@osgeo.org>
1991-03-27 18:12:58 +0100

    added

---
+++ src/libes/raster/Plylne_abs.c
@@ -0,0 +1,14 @@
+
+#include "graph.h"
+
+R_polyline_abs(xarray, yarray, number)
+	int *xarray, *yarray ;
+	int number ;
+{
+	int n ;
+	_send_ident(POLYLINE_ABS) ;
+	n = number ;
+	_send_int(&n) ;
+	_send_int_array(number,xarray) ;
+	_send_int_array(number,yarray) ;
+}
