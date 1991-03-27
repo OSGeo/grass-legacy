89f78c3243116815dfa6a9be0f48204e1691bce0
bot <grass-svn2git@osgeo.org>
1991-03-27 18:17:07 +0100

    added

---
+++ src/sites/s.menu/occur_rpt/title.c
@@ -0,0 +1,32 @@
+#include "report.h"
+
+title (out,report)
+    FILE *out;
+    REPORT *report;
+{
+    fprintf (out, ".title\n");
+    fprintf (out, ".center\n");
+    fprintf (out, "SITE OCCURENCE REPORT\n");
+    fprintf (out, ".center\n");
+    fprintf (out, "%s\n", report->fullname);
+
+    fprintf (out, "\n\n");
+    fprintf (out, "Location:  %s\n", report->location);
+    fprintf (out, "Mapset:    %s\n", report->mapset);
+    fprintf (out, "Site List: ");
+    fprintf (out,"%s", report->site_list_name);
+    if (report->site_list_desc[0])
+    {
+	if (report->site_list_name[0])
+		fprintf(out," - ");
+	fprintf(out,"%s", report->site_list_desc);
+    }
+    fprintf(out, " (%d site%s)\n",
+	    report->npoints, report->npoints == 1 ? "" : "s");
+    fprintf(out,"\n");
+    fprintf (out, "Analysis Region:\n");
+    fprintf (out, "                    north: %10.2lf\n",report->north);
+    fprintf (out, "   west: %10.2lf                       east: %10.2lf\n", report->west,report->east);
+    fprintf (out, "                    south: %10.2lf\n\n",report->south);
+    fprintf (out, ".end\n");
+}
