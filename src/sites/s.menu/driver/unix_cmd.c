411653bf0ff8d506e2774b896e50c15d71e7e5c2
bot <grass-svn2git@osgeo.org>
1991-03-27 18:16:58 +0100

    added

---
+++ src/sites/s.menu/driver/unix_cmd.c
@@ -0,0 +1,15 @@
+#include "site.h"
+#include "run.h"
+
+unix_cmd (pgm, site_list)
+
+	SITE_LIST *site_list;
+{
+	char temp[3];
+
+	if (sscanf (pgm, "%1s", temp) != 1)
+		return 0;
+
+	printf("%s\n", pgm);
+	run (site_list, pgm, RUN_PIPED, 0, 0);
+}
