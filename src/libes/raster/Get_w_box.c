2362d541dae00ea2132848ce42e02b53cbf1ba92
bot <grass-svn2git@osgeo.org>
1991-03-27 18:12:55 +0100

    added

---
+++ src/libes/raster/Get_w_box.c
@@ -0,0 +1,22 @@
+
+#include "graph.h"
+
+R_get_location_with_box(cx, cy, wx, wy, button)
+	int cx, cy ;
+	int *wx, *wy ;
+	int *button ;
+{
+	int z ;
+	_send_ident(GET_LOCATION_WITH_BOX) ;
+	z = cx ;
+	_send_int(&z) ;
+	z = cy ;
+	_send_int(&z) ;
+	z = *wx ;
+	_send_int(&z) ;
+	z = *wy ;
+	_send_int(&z) ;
+	_get_int(wx) ;
+	_get_int(wy) ;
+	_get_int(button) ;
+}
