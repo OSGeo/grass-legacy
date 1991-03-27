4368105767fc671de3c943a0e2fd70436c7386d9
bot <grass-svn2git@osgeo.org>
1991-03-27 18:16:52 +0100

    added

---
+++ src/sites/s.menu/char_rpt/divider.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+divider(out)
+    FILE *out;
+{
+    int i;
+
+    fprintf (out, ".divider\n");
+    fprintf (out, "\n");
+    for (i = 0; i < 79; i++)
+	fprintf (out, "=");
+    fprintf (out, "\n\n");
+    fprintf (out, ".end\n");
+}
