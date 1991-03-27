a974c1ea97611a7a4cd62a87e235c8dba0f9a132
bot <grass-svn2git@osgeo.org>
1991-03-27 19:11:27 +0100

    added

---
+++ src/display/d.vect.dlg/cmd/dlg_conv.h
@@ -0,0 +1,13 @@
+
+double dlg_D_west ;
+double dlg_D_east ;
+double dlg_D_north ;
+double dlg_D_south ;
+
+double dlg_U_west ;
+double dlg_U_east ;
+double dlg_U_north ;
+double dlg_U_south ;
+
+double dlg_U_to_D_xconv ;
+double dlg_U_to_D_yconv ;
