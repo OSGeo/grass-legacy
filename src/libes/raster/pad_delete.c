9a849fd2e4609d1c2e0a1fd7d3feb3ee3bfc12b6
bot <grass-svn2git@osgeo.org>
1991-03-27 18:13:11 +0100

    added

---
+++ src/libes/raster/pad_delete.c
@@ -0,0 +1,16 @@
+
+#include "graph.h"
+
+R_pad_delete ()
+{
+    char result;
+
+    _hold_signals(1);
+
+    _send_ident (PAD_DELETE);
+    _get_char (&result);
+
+    _hold_signals(0);
+
+    return result ;
+}
