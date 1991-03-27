3e31a3636f2d894881f225bd4aab8a020774282a
bot <grass-svn2git@osgeo.org>
1991-03-27 18:17:17 +0100

    added

---
+++ src/sites/s.menu/site_reports/unix_cmd.c
@@ -0,0 +1,12 @@
+unix_cmd (pgm, raw_report, report)
+    char *raw_report;
+    char *report;
+{
+    char temp[3];
+
+    if (sscanf (pgm, "%1s", temp) != 1)
+	    return 0;
+
+    printf("%s\n", pgm);
+    return run_report (pgm, raw_report, report);
+}
