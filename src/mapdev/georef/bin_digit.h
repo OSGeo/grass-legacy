19943eae0767a0f36baead0fbb48a6eaff94e6a2
bot <grass-svn2git@osgeo.org>
1991-03-27 19:11:51 +0100

    added

---
+++ src/mapdev/georef/bin_digit.h
@@ -0,0 +1,13 @@
+
+#define		TMP_FILE	"/tmp/DIGIT."
+#define		DIGITIZER_CAP	"digitcap"
+#define		DRIVER_DIR	"dig_drivers"
+
+struct driver_desc
+{
+    char  name[128] ;
+    char  device[128] ;
+    char  dig_program[128] ;
+    char  dig_desc[128] ;
+};
+
