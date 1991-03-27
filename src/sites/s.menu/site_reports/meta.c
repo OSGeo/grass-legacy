5a8d1816c6f90b76ede790fe16b7e020e63f42a7
bot <grass-svn2git@osgeo.org>
1991-03-27 18:17:16 +0100

    added

---
+++ src/sites/s.menu/site_reports/meta.c
@@ -0,0 +1,24 @@
+meta (raw_report, sites, layers, quadsize, with_stats)
+    char *raw_report;
+    char *sites;
+    char *layers;
+{
+
+    static int have_report = 0;
+    static int have_stats  = 0;
+
+    if (with_stats && have_stats)
+	    return 0;
+    
+    if (!with_stats && have_report)
+	    return 0;
+
+    have_report = have_stats = 0;
+    if(meta_report (raw_report, sites, layers, quadsize, with_stats) != 0)
+	    return -1;
+
+    have_report = 1;
+    have_stats = with_stats;
+
+    return 0;
+}
