dc3f27cf73ad902d5c7a60f6386fe63cbe049e2f
bot <grass-svn2git@osgeo.org>
1991-03-27 18:17:14 +0100

    added

---
+++ src/sites/s.menu/site_reports/ask_layers.c
@@ -0,0 +1,41 @@
+#include "gis.h"
+
+ask_layers (file)
+    char *file;
+{
+    FILE *fd;
+    int i;
+    char list[400];
+    char name[100];
+    char *mapset;
+    char prompt[1000];
+
+    fd = fopen (file, "w");
+    if (!fd)
+    {
+	perror (file);
+	exit (-1);
+    }
+
+    printf("You may select one or more map layers to be analyzed\n\n");
+
+    *list = 0;
+    while(1)
+    {
+	if (*list)
+	{
+	    strcat (list, " ");
+	    sprintf (prompt,"selected layers: %s\n\nselect another map layer", list);
+	}
+	else
+	    sprintf (prompt,"select first map layer");
+
+	mapset = G_ask_cell_old (prompt, name);
+	if (!mapset)
+		break;
+	fprintf (fd, "%s %s\n", name, mapset);
+	strcat (list, name);
+    }
+
+    fclose (fd);
+}
