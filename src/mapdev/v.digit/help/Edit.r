aeb4d94913c5b391d9c44bc1df1d3ec8c2c67927
bot <grass-svn2git@osgeo.org>
1991-04-16 21:30:49 +0200

    added

---
+++ src/mapdev/v.digit/help/Edit.r
@@ -0,0 +1,4 @@
+ REMOVE LINE   Selectively remove an existing line
+   Use the mouse to select a line to be removed.  Left button to select
+ a line, Right button to accept chosen line, Middle button to abort or 
+ return when finished removing lines.
