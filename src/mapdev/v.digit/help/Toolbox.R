43f813bab146dbfc381939d6b1828bcc440c8f57
bot <grass-svn2git@osgeo.org>
1991-04-16 21:30:52 +0200

    added

---
+++ src/mapdev/v.digit/help/Toolbox.R
@@ -0,0 +1,4 @@
+ RE-REGISTER MAP    Register this or another map to the digitizer.
+   This function can be used to register another map (e.g. quad sheet) to 
+ the digitizer without leaving digit.  This does not affect the digitizing
+ window or allow you to edit a different file.
