875184b649248c55afa3205b8331922693677ab9
bot <grass-svn2git@osgeo.org>
1991-04-18 17:48:13 +0200

    added

---
+++ src/display/d.3d/draw_face.c
@@ -0,0 +1,122 @@
+#include <gis.h>
+#include "options.h"
+
+draw_north_face(min, max)
+	CELL min, max ;
+{
+	int scr_x, scr_y ;
+	point_calc(window.west+window.ew_res, window.north-window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_move_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.north-window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.north-window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.north-window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.north-window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	if (min < 0 && max > 0)
+	{
+		point_calc(window.west+window.ew_res, window.north-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_move_abs(scr_x, scr_y) ;
+		point_calc(window.east-window.ew_res, window.north-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_cont_abs(scr_x, scr_y) ;
+	}
+}
+
+draw_south_face(min, max)
+	CELL min, max ;
+{
+	int scr_x, scr_y ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_move_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.south+window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	if (min < 0 && max > 0)
+	{
+		point_calc(window.west+window.ew_res, window.south-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_move_abs(scr_x, scr_y) ;
+		point_calc(window.east-window.ew_res, window.south-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_cont_abs(scr_x, scr_y) ;
+	}
+}
+
+draw_east_face(min, max)
+	CELL min, max ;
+{
+	int scr_x, scr_y ;
+	point_calc(window.east-window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_move_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.north-window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.north-window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.south+window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.east-window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	if (min < 0 && max > 0)
+	{
+		point_calc(window.east-window.ew_res, window.south+window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_move_abs(scr_x, scr_y) ;
+		point_calc(window.east-window.ew_res, window.north-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_cont_abs(scr_x, scr_y) ;
+	}
+}
+
+draw_west_face(min, max)
+	CELL min, max ;
+{
+	int scr_x, scr_y ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_move_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.north-window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.north-window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		max * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	point_calc(window.west+window.ew_res, window.south+window.ns_res,
+		min * exag, &scr_x, &scr_y) ;
+	R_cont_abs(scr_x, scr_y) ;
+	if (min < 0 && max > 0)
+	{
+		point_calc(window.west-window.ew_res, window.south+window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_move_abs(scr_x, scr_y) ;
+		point_calc(window.west-window.ew_res, window.north-window.ns_res,
+			0., &scr_x, &scr_y) ;
+		R_cont_abs(scr_x, scr_y) ;
+	}
+}
