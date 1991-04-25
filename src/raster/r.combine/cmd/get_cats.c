66caf0c38ae3e8a5cfa4292af6a91458c912749f
bot <grass-svn2git@osgeo.org>
1991-04-25 21:17:44 +0200

    added

---
+++ src/raster/r.combine/cmd/get_cats.c
@@ -0,0 +1,58 @@
+#include "gis.h"
+
+get_cats(name) char *name ;
+{
+    char *mapset ;
+    struct Categories cats ;
+    CELL cat;
+    FILE *fd;
+    static char *tempfile = NULL;
+
+
+    mapset = G_find_file("cell", name, "") ;
+    if (!mapset)
+    {
+	fprintf (stderr, "Cell file [%s] not found\n", name) ;
+	return -1 ;
+    }
+
+/* read the category data */
+    if(G_read_cats(name, mapset, &cats) < 0)
+	return 0;
+
+/* run this thru "more" if input is the keyboard */
+    fd = NULL;
+    if (isatty(0))
+    {
+	if (tempfile == NULL)
+	    tempfile = G_tempfile();
+	fd = fopen (tempfile, "w");
+	if (fd == NULL)
+	    G_warning ("No temp files available");
+    }
+    if (fd == NULL)
+	fd = stdout;
+
+/* write the cat data structure */
+    fprintf (fd, "Categories for raster file [%s in %s]\n", name, mapset) ;
+
+    fprintf (fd, "TITLE: %s\n", G_get_cats_title (&cats));
+
+    for (cat = 0; cat <= cats.num; cat++)
+	fprintf (fd, "%4ld %s\n", (long)cat, G_get_cat (cat, &cats));
+
+    G_free_cats (&cats);
+
+    if (fd != stdout)
+    {
+	char command[1024];
+
+	fclose (fd);
+	sprintf (command, "more %s", tempfile);
+	system(command);
+	unlink (tempfile);
+    }
+
+/* everything went okay if it got this far */
+    return 1 ;
+}
