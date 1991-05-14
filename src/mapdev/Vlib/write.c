03bcef54356927fa74c07540b60dd92af42c3715
bot <grass-svn2git@osgeo.org>
1991-05-15 00:21:23 +0200

    added

---
+++ src/mapdev/Vlib/write.c
@@ -0,0 +1,16 @@
+#include "V_.h"
+
+/*
+**  Writes line info to current positioni(should be EOF) in output map
+**
+**  returns offset in file that (start of) line is written at.
+*/
+
+long
+Vect_write_line (Map, type, points)
+    struct Map_info *Map;
+    int type;
+    struct line_pnts *points;
+{
+    return Vect__Write_line (Map, (char) type, points);
+}
