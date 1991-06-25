211ea8c8886468dc4fb70822ab5da013e075437c
bot <grass-svn2git@osgeo.org>
1991-06-25 20:24:36 +0200

    added

---
+++ src/display/devices/XDRIVER/XDRIVER/alloc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <X11/Xos.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+
+char *malloc(), *realloc();
+static num_alloc = 0;
+static XPoint *pnts = NULL;
+
+XPoint *AllocXPoints(count)
+int count;
+{
+    if (num_alloc < count) {
+        if (num_alloc == 0)
+            pnts = (XPoint *) malloc((unsigned) (count * sizeof(XPoint)));
+        else
+            pnts = (XPoint *) realloc((char *) pnts, (unsigned) (count *
+                            sizeof(XPoint)));
+        if (pnts == NULL) {
+            fprintf(stderr, "AllocXPoints: can't alloc %d XPoints.\n",
+                    count);
+            num_alloc = 0;
+            return NULL;
+        }
+        num_alloc = count;
+    }
+    return (pnts);
+}
