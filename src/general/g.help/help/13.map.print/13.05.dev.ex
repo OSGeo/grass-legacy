40ac0209ba578a1d8c590caff217baa8d51d339b
bot <grass-svn2git@osgeo.org>
1991-06-27 22:57:13 +0200

    added

---
+++ src/general/g.help/help/13.map.print/13.05.dev.ex
@@ -0,0 +1,15 @@
+                          SUPPORTED PRINTERS
+
+This section lists the printers that have been currently tested and
+are supported.  In future releases of GRASS, plans are to add any
+drivers that users have written for specific printers and include
+them in an unsuppported printers section (if they are not able to be
+fully tested). If you have written a device driver or are using a
+printer that is not listed here, please contact Doug Brooks
+(217-373-6752).  He would like to include your drivers in this section.
+
+	\Tektronix 4696\
+	\Epson LQ-2500\
+	\Shinko CHC-635\
+
+For information on a specific printer, select a printer and press ESC.
