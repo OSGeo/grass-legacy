625aefe20a231c98974911cbda0d2f1c068f4e37
bot <grass-svn2git@osgeo.org>
1991-06-28 20:23:55 +0200

    added

---
+++ src/libes/D/Derase.c
@@ -0,0 +1,18 @@
+Derase(color)
+{
+	int t, b, l, r ;
+	int line ;
+
+	if (D_get_screen_window(&t, &b, &l, &r))
+		G_fatal_error("getting graphics window") ;
+
+	if (D_clear_window())
+		G_fatal_error("clearing current graphics window") ;
+
+	/* Do the plotting */
+	R_standard_color(D_translate_color(color)) ;
+	R_box_abs (l, t, r, b);
+
+	/* Add erase item to the pad */
+	D_set_erase_color(color);
+}
