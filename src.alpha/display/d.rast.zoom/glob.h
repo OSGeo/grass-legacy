683d7681095bcdbefee1ec2bb4e9bb6c74ed8d37
bot <grass-svn2git@osgeo.org>
1991-07-01 23:01:24 +0200

    added

---
+++ src.alpha/display/d.rast.zoom/glob.h
@@ -0,0 +1,19 @@
+/* %W% %G% */
+
+/*
+#ifdef MAIN
+
+    char *window_dir = "windows" ;
+*/
+    struct Cell_head cur_window  ;
+
+/*
+#endif
+*/
+
+/*
+extern char *window_dir ;
+extern struct Cell_head cur_window  ;
+
+int lister();
+*/
