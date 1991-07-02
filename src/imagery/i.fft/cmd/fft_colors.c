d4041bb3627915999b9ca34549ecfb711b063be4
bot <grass-svn2git@osgeo.org>
1991-07-02 21:40:13 +0200

    added

---
+++ src/imagery/i.fft/cmd/fft_colors.c
@@ -0,0 +1,22 @@
+#include "gis.h"
+#include "globals.h"
+
+fft_colors()
+{
+        struct Colors colors;
+        struct Range range ;
+        CELL min, max ;
+
+        /* make a real component color table */
+        G_read_range(Cellmap_real, G_mapset(), &range) ;
+	G_get_range_min_max (&range, &min, &max);
+        G_make_wave_colors(&colors, min, max) ;
+        G_write_colors(Cellmap_real, G_mapset(), &colors) ;
+	G_free_colors (&colors);
+
+        /* make a imag component color table */
+        G_read_range(Cellmap_imag, G_mapset(), &range) ;
+	G_get_range_min_max (&range, &min, &max);
+        G_make_wave_colors(&colors, min, max) ;
+        G_write_colors(Cellmap_imag, G_mapset(), &colors) ;
+}
