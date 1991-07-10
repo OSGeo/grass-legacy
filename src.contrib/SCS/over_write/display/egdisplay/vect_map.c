df7a2146fcf3e5fae501577bd54c786e33601a0b
bot <grass-svn2git@osgeo.org>
1991-07-10 17:40:44 +0200

    added

---
+++ src.contrib/SCS/over_write/display/egdisplay/vect_map.c
@@ -0,0 +1,73 @@
+#include "gis.h"
+#include "windows.h"
+#include "variables.h"
+
+vect_map()
+{
+	char *vect_mapset ;
+	char vect_name[128] ;
+	char command[256] ;
+	char buff[128] ;
+	char *color ;
+	int option ;
+	FILE *popen() ;
+	FILE *fptr ;
+
+	vect_mapset = G_ask_old("", vect_name, "dig", "vect")  ;
+	if (vect_mapset == NULL)
+		return ;
+	
+	for(;;)
+	{
+		fprintf(stderr,"\nEnter number of color desired:\n") ;
+		fprintf(stderr," 1 - red        7 - brown\n") ;
+		fprintf(stderr," 2 - orange     8 - gray\n") ;
+		fprintf(stderr," 3 - yellow     9 - white\n") ;
+		fprintf(stderr," 4 - green     10 - black\n") ;
+		fprintf(stderr," 5 - blue\n") ;
+		fprintf(stderr," 6 - violet\n") ;
+		fprintf(stderr," > ") ;
+		if(gets(buff) == NULL) return ;
+		if(sscanf(buff, "%d", &option) != 1)
+			continue ;
+		fprintf(stderr,"%d\n", option) ;
+		if(option < 1 || option > 10)
+			continue ;
+		switch(option)
+		{
+		case 1: color = "red";    break ;
+		case 2: color = "orange"; break ;
+		case 3: color = "yellow"; break ;
+		case 4: color = "green";  break ;
+		case 5: color = "blue";   break ;
+		case 6: color = "violet"; break ;
+		case 7: color = "brown";  break ;
+		case 8: color = "gray";   break ;
+		case 9: color = "white";  break ;
+		case 10: color = "black"; break ;
+		default: color = "black"; break ;
+		}
+		break ;
+	}
+
+/* Show the map name in name window */
+	R_open_driver();
+	Dchoose(NAM.name) ;
+	Derase(D_translate_color("black")) ;
+	R_close_driver();
+
+	if (NULL != (fptr = popen("Dtext", "w")))
+	{
+		fprintf(fptr, ".S 60\n.C white\n.B\n") ;
+		fprintf(fptr, "@%s\n", vect_name) ;
+		pclose(fptr) ;
+	}
+
+	R_open_driver();
+	Dchoose(MAP.name) ;
+	R_close_driver();
+
+	sprintf(command,"'%s in %s' %s", vect_name, vect_mapset, color) ;
+	maptyp = 2 ;
+	gorun("Dvect", command) ;
+}
