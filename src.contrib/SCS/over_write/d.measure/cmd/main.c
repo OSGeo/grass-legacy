de57fe4903f5c9d63cbcf8d987408228ad52c200
bot <grass-svn2git@osgeo.org>
1991-07-10 17:41:11 +0200

    added

---
+++ src.contrib/SCS/over_write/d.measure/cmd/main.c
@@ -0,0 +1,60 @@
+#include "gis.h"
+
+main(argc, argv)
+	int argc ;
+	char **argv ;
+{
+	char frame[64] ;
+	struct
+	{
+	    struct Option *c1;
+	    struct Option *c2;
+	    struct Option *out;
+	} parm;
+	int color1, color2;
+	char *color_list = "red,orange,yellow,green,blue,indigo,violet,gray,white,black";
+
+/* Initialize the GIS calls */
+	G_gisinit(argv[0]) ;
+
+	parm.c1 = G_define_option();
+	parm.c1->key = "c1";
+	parm.c1->description = "line color 1";
+	parm.c1->type = TYPE_STRING;
+	parm.c1->required = NO;
+	parm.c1->options=color_list;
+	parm.c1->answer = "black";
+
+	parm.c2 = G_define_option();
+	parm.c2->key = "c2";
+	parm.c2->description = "line color 2";
+	parm.c2->type = TYPE_STRING;
+	parm.c2->required = NO;
+	parm.c2->options=color_list;
+	parm.c2->answer = "white";
+
+	parm.out = G_define_option();
+	parm.out->key = "output";
+	parm.out->description = "output file name";
+	parm.out->type = TYPE_STRING;
+	parm.out->required = NO;
+
+
+	if (G_parser(argc,argv))
+	    exit(1);
+
+	R_open_driver();
+
+	if (D_get_cur_wind(frame))
+		G_fatal_error("No current frame") ;
+
+	if (D_set_cur_wind(frame))
+		G_fatal_error("Current frame not available") ;
+
+	color1 = D_translate_color (parm.c1->answer);
+	color2 = D_translate_color (parm.c2->answer);
+
+	measurements(color1, color2, parm.out->answer) ;
+
+	R_close_driver();
+}
