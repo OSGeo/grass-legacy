75fc81c518b8938837a15441ee01fbc105f51472
bot <grass-svn2git@osgeo.org>
1991-07-10 17:41:00 +0200

    added

---
+++ src.contrib/SCS/over_write/display/vgdisplay/popup.h
@@ -0,0 +1,27 @@
+#define  TOP	90
+#define  LEFT	10
+#define  SIZE	4
+
+#define BC_EXIT	"red"
+#define TC_EXIT	"yellow"
+#define DC_EXIT	"yellow"
+
+#define BC_LEG	"blue"
+#define TC_LEG	"white"
+#define DC_LEG	"black"
+
+#define BC_DISP	"blue"
+#define TC_DISP	"white"
+#define DC_DISP	"black"
+
+#define BC_INFO	"blue"
+#define TC_INFO	"white"
+#define DC_INFO	"black"
+
+#define BC_MAIN	"blue"
+#define TC_MAIN	"white"
+#define DC_MAIN	"black"
+
+#define BC_WIND	"blue"
+#define TC_WIND	"white"
+#define DC_WIND	"black"
