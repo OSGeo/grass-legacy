831c6b3497ee2032b9fd08401e3b9ccd35dba4e7
bot <grass-svn2git@osgeo.org>
1991-07-10 17:40:30 +0200

    added

---
+++ src.contrib/SCS/over_write/d.labels/cmd/where.h
@@ -0,0 +1,14 @@
+/*  %W%  %G%  */
+
+#include "gis.h"
+#define MAX_LAYERS 15
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL struct Categories cats[MAX_LAYERS];
+GLOBAL int fd[MAX_LAYERS];
+GLOBAL int nlayers;
+GLOBAL char name[MAX_LAYERS][40];
+GLOBAL char mapset[MAX_LAYERS][40];
