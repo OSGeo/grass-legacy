991b7af5431c57979894d72ea2d9d94fe22fc602
bot <grass-svn2git@osgeo.org>
1991-07-10 17:43:37 +0200

    added

---
+++ src.contrib/SCS/vector/v.report/inter/units.h
@@ -0,0 +1,18 @@
+struct units
+{
+    char *name;
+    char *code;
+};
+#ifndef GLOBAL
+struct units units[] = {
+	{"meters","me"},
+	{"kilometers","k"},
+	{"acres","a"},
+	{"hectacres","h"},
+	{"miles","mi"},
+	{"cell counts","c"},
+	{"percent cover","p"},
+        {NULL,NULL}};
+#else
+extern struct units units[];
+#endif
