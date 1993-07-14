f7b7ee6044ebb0da98c093b392de3e556edbf190
bot <grass-svn2git@osgeo.org>
1993-07-14 17:09:09 +0200

    added

---
+++ src.alpha/raster/r.water.fea/src/programs/fea/fea.h
@@ -0,0 +1,10 @@
+extern char timefile[64] ;
+extern int max,nsn,cbw,bw,noe;
+extern int basin_no,no_stream,basinhydrograph,animate,infilmap;
+extern int duration, mode,delta_t,out_node,monit_time;
+extern int *nin,*nout,*sn,*stream_node;
+extern double *easting,*northing,*le,*rhf,*capacitance,*manning,*slope,*exponent;
+extern double width,nslope,base,rain_max,area;
+extern short int **stiffness,*A;
+extern char manningsmap[100],sat_cond_map[100],cap_suc_map[100],porosity_map[100],deg_of_s_map[100],slopemap[100];
+extern  char element[64],filename[200];
