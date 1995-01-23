39b83827c7d0e08647a1722ee5a2668fadec0e6d
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.contrib/CERL/SGI/SG3d/trans.c
+++ src.contrib/CERL/SGI/SG3d/trans.c
@@ -1,17 +1,33 @@
-
 /*
-**  Written by Dave Gerdes  Summer 1990
-**  US Army Construction Engineering Research Lab
+**  Written by Dave Gerdes Jan 1990
+**  Copyright  Dave Gerdes 1990    All rights reserved
+**
+**
+**  Matrix Transformation library.
+**  
+**   P_pushmatrix ()
+**   P_popmatrix ()
+**   P_scale ()		
+**   P_translate ()
+**   P_rot ()
+**   P_rotate ()
+**   P_transform ()      transform array of vectors using current T matrix
+**      		 This routine should be available in GL!
+**
+**  Arguments are same as GL counterparts
+**
+**  I threw this code together in January at the beginning of this
+**  class.  I was still learning about GL at the time.
+**  There are many places where the code could be improved.
+**
 */
-
 #include <stdio.h>
 #include <math.h>
 
 #define MAX_STACK 20
-static float c_stack[MAX_STACK][4][4];
-static int stack_ptr = -1;
-static float d[4][4];
-static float pnts[1][4];
+static float c_stack[MAX_STACK][4][4];	/* matrix stack */
+static int stack_ptr = -1;		/* index of curr matrix depth */
+static float d[4][4];			/* tmp matrix */
 
 #define PI  3.14159265358979323846
 
@@ -42,8 +58,12 @@ P_scale (x, y, z)
     d[2][0] = 0.; d[2][1] = 0.; d[2][2] = z;  d[2][3] = 0.;
     d[3][0] = 0.; d[3][1] = 0.; d[3][2] = 0.; d[3][3] = 1.; 
 
+    /*
+    **  will write into 1 down on matrix stack
+    **  and then the popmatrix() will place it as the current T matrix
+    */
     P_pushmatrix ();
-    P__transform (4, trans_mat, c_stack[stack_ptr], d);
+    P__transform (4, d, c_stack[stack_ptr], trans_mat);
     P_popmatrix ();
 }
 
@@ -56,7 +76,7 @@ P_translate (x, y, z)
     d[3][0] = x;  d[3][1] = y;  d[3][2] = z;  d[3][3] = 1.;
 
     P_pushmatrix ();
-    P__transform (4, trans_mat, c_stack[stack_ptr], d);
+    P__transform (4, d, c_stack[stack_ptr], trans_mat);
     P_popmatrix ();
 }
 
@@ -74,7 +94,6 @@ P_transform (num_vert, in, out)
     P__transform (num_vert, in, out, trans_mat);
 }
 
-static
 P__transform (num_vert, in, out, c)
     int num_vert;
     float in[][4];
@@ -88,7 +107,7 @@ P__transform (num_vert, in, out, c)
 	{
 	    out[i][j] = 0.;
 	    for (k = 0 ; k < 4 ; k++)
-		out[i][j] += c[k][j] * in[i][k];
+		out[i][j] += in[i][k] * c[k][j] ;
 	}
 }
 
@@ -104,32 +123,6 @@ P_matrix_copy (from, to, size)
 	    to[i][j] = from[i][j];
 }
     
-P_display_matrix (in, size)
-    float in[][4];
-{
-    register int j, i;
-    for (i = 0 ; i < size ; i++)
-    {
-	for (j = 0 ; j < 4 ; j++)
-	    printf (" %4.2f", in[i][j]);
-	printf ( "\n");
-    }
-    printf ( "\n");
-}
-
-P_display_matrix_err (in, size)
-    float in[][4];
-{
-    register int j, i;
-    for (i = 0 ; i < size ; i++)
-    {
-	for (j = 0 ; j < 4 ; j++)
-	    fprintf (stderr, " %4.2f", in[i][j]);
-	fprintf (stderr,  "\n");
-    }
-    fprintf (stderr,  "\n");
-}
-
 /*
 ** push current transformation matrix onto matrix stack
 */
@@ -160,46 +153,61 @@ P_popmatrix ()
     return (0);
 }
 
-/* plot point w/ current transformation matrix 
-*/
-P_pnt (x, y, z)
-    float x, y, z;
-{
-    float out[1][4];
-    pnts[0][0] = x;
-    pnts[0][1] = y;
-    pnts[0][2] = z;
-    pnts[0][3] = 1.;
-
-    P__transform (1, pnts, out, trans_mat);
-
-    printf ( "%f %f %f %f\n", out[0][0], out[0][1], out[0][2], out[0][3]);
-}
-
-/* rotate 90 deg around Z */
-/* I.E. transpose x/y */
-P_rot_90z ()
-{
-    d[0][0] = 0.; d[0][1] = 1.; d[0][2] = 0.; d[0][3] = 0.;
-    d[1][0] = 1.; d[1][1] = 0.; d[1][2] = 0.; d[1][3] = 0.;
-    d[2][0] = 0.; d[2][1] = 0.; d[2][2] = 1.; d[2][3] = 0.;
-    d[3][0] = 0.; d[3][1] = 0.; d[3][2] = 0.; d[3][3] = 1.;
-    
-    P_pushmatrix ();
-    P__transform (4, trans_mat, c_stack[stack_ptr], d);
-    P_popmatrix ();
-}
-
 /*  angle is expressed in tenths of degrees */
 P_rotate (angle, axis)
     int angle;
+    char axis;
+{
+    return P_rot (angle/10., axis);
+}
+
+P_rot (angle, axis)
+    float angle;
     char axis;
 {
     double theta;
 
     P_matrix_copy (ident, d, 4);
 
-    theta = (PI / 1800.) * (float) angle; /* convert to radians */
+    theta = (PI / 180.) * angle; /* convert to radians */
+
+    /* optimize to handle rotations of mutliples of 90 deg */
+    switch (axis) {
+	case 'X':
+	case 'x':
+	    d[1][1] =  cos (theta);
+	    d[1][2] =  sin (theta);
+	    d[2][1] = -sin (theta);
+	    d[2][2] =  cos (theta);
+	    break;
+	case 'Y':
+	case 'y':
+	    d[0][0] =  cos (theta);
+	    d[0][2] = -sin (theta);
+	    d[2][0] =  sin (theta);
+	    d[2][2] =  cos (theta);
+	    break;
+	case 'Z':
+	case 'z':
+	    d[0][0] =  cos (theta);
+	    d[0][1] =  sin (theta);
+	    d[1][0] = -sin (theta);
+	    d[1][1] =  cos (theta);
+	    break;
+    }
+
+    P_pushmatrix ();
+    P__transform (4, d, c_stack[stack_ptr], trans_mat);
+    P_popmatrix ();
+}
+
+/*  angle is expressed in radians  */
+P_rad_rotate (theta, axis)
+    double theta;
+    char axis;
+{
+
+    P_matrix_copy (ident, d, 4);
 
     /* optimize to handle rotations of mutliples of 90 deg */
     switch (axis) {
@@ -224,6 +232,6 @@ P_rotate (angle, axis)
     }
 
     P_pushmatrix ();
-    P__transform (4, trans_mat, c_stack[stack_ptr], d);
+    P__transform (4, d, c_stack[stack_ptr], trans_mat);
     P_popmatrix ();
 }
