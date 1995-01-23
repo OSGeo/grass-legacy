9bf0d5bbad75d72ecfe2b78fc5bcffff90dffbec
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.alpha/imagery/i.smap/cmd/shapiro/read_sig.c
@@ -0,0 +1,39 @@
+#include "imagery.h"
+#include "parms.h"
+
+read_signatures (parms, S)
+    struct parms *parms;
+    struct SigSet *S;
+{
+    FILE *fd;
+    struct Ref Ref;
+
+    if (!I_get_subgroup_ref (parms->group, parms->subgroup, &Ref))
+    {
+        fprintf (stderr,
+             "ERROR: unable to read REF file for subgroup [%s] in group [%s]\n",
+                parms->subgroup, parms->group);
+        exit(1);
+    }
+    if (Ref.nfiles <= 0)
+    {
+        fprintf (stderr, "ERROR: subgroup [%s] in group [%s] contains no files\n",
+                parms->subgroup, parms->group);
+        exit(1);
+    }
+
+    fd = I_fopen_sigset_file_old (parms->group, parms->subgroup, parms->sigfile);
+    if (fd == NULL)
+    {
+	fprintf (stderr, "ERROR: signature file [%s] missing or not readable\n",
+		parms->sigfile);
+	exit(1);
+    }
+    if(I_ReadSigSet (fd, S) < 0 || Ref.nfiles != S->nbands)
+    {
+	fprintf (stderr, "ERROR: signature file [%s] invalid\n",
+		parms->sigfile);
+	exit(1);
+    }
+    fclose (fd);
+}
