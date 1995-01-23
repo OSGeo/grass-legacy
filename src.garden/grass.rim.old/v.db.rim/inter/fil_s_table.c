05597e0fda3919f15e3876d9a16e281921d2acff
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.garden/grass.rim.old/v.db.rim/inter/fil_s_table.c
@@ -0,0 +1,11 @@
+#define FIL_S_TABLE
+
+
+/* This routine fills the buffer with a row for the Screenlayout table. */
+fil_s_table(buffer, line_num, line_text)
+   int *buffer, line_num;
+   char *line_text;
+{
+   fill_buf_i(buffer, &line_num);
+   fill_buf_t(buffer, line_text, 80);
+}
