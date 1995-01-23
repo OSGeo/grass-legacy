720c18bb7fb6131bbbf4b77069616ab8ab4217d8
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Programs/grey.map/cmd/labels.h
@@ -0,0 +1,15 @@
+#define MAXLABELS 10
+
+struct labels
+{
+    int count;		/* number of labels files */
+    char *name[MAXLABELS];
+    char *mapset[MAXLABELS];
+    char *other;
+} ;
+
+#ifdef MAIN
+    struct labels labels;
+#else
+    extern struct labels labels;
+#endif
