025b2167391f88df432852e5344a8e6a1064e8cc
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/CERL/raster/r.traj/cmd/radians.h
@@ -0,0 +1,17 @@
+/****************************************************************/	
+/*								*/
+/*	radians.h	in ~/src/Gtraj				*/
+/*								*/
+/* 	This header file defines the syntactic sugar to be	*/
+/*	used for some prominent radian measures.                */
+/*                                                              */
+/****************************************************************/
+
+#define		PI		3.141592654
+#define		PIBYFOUR 	(PI / 4.0)
+#define		PIBYTWO		(PI / 2.0)
+#define		TWOPI		(PI * 2.0)	
+#define 	THREEPIBYTWO	(PI * 3.0 / 2.0)
+#define         D_TO_RAD_CONV   (PI / 180.0)
+
+/****************************************************************/
