37b3db3e97bbbfa79a78917905c217a9d0015201
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/raster/r.watershed4.0/front/main.c
@@ -0,0 +1,314 @@
+#include "gis.h"
+
+main(argc, argv)
+	int argc ;
+	char *argv[] ;
+{
+	int err ;
+	char command[512] ;
+	struct Option *opt1 ;
+	struct Option *opt2 ;
+	struct Option *opt3 ;
+	struct Option *opt4 ;
+	struct Option *opt5 ;
+	struct Option *opt6 ;
+	struct Option *opt7 ;
+	struct Option *opt8 ;
+	struct Option *opt9 ;
+	struct Option *opt10 ;
+	struct Option *opt11 ;
+	struct Option *opt12 ;
+	struct Option *opt13 ;
+	struct Option *opt14 ;
+	struct Option *opt15 ;
+	struct Option *opt16 ;
+	struct Flag *flag1 ;
+	struct Flag *flag2 ;
+
+	G_gisinit(argv[0]) ;
+	
+	opt1 = G_define_option() ; 
+	opt1->key            = "elevation" ; 
+	opt1->description    = "Input map: elevation on which entire analysis is based"; 
+	opt1->required       = YES ; 
+	opt1->type           = TYPE_STRING ; 
+	opt1->gisprompt      = "old,cell,raster" ;
+
+	opt2 = G_define_option() ; 
+	opt2->key            = "depression" ; 
+	opt2->description    = "Input map: locations of real depressions" ; 
+	opt2->required       = NO ; 
+	opt2->type           = TYPE_STRING ;
+	opt2->gisprompt      = "old,cell,raster" ;
+
+	opt3 = G_define_option() ; 
+	opt3->key            = "flow" ; 
+	opt3->description    = "Input map: amount of overland flow per cell" ;
+	opt3->required       = NO ; 
+	opt3->type           = TYPE_STRING ; 
+	opt3->gisprompt      = "old,cell,raster" ;
+
+	opt4 = G_define_option() ; 
+	opt4->key            = "disturbed.land" ; 
+	opt4->description    = "Input map or value: percent of disturbed land, for USLE" ; 
+	opt4->required       = NO ; 
+	opt4->type           = TYPE_STRING ; 
+	opt4->gisprompt      = "old,cell,raster" ;
+
+	opt5 = G_define_option() ; 
+	opt5->key            = "blocking" ; 
+	opt5->description    = "Input map: terrain blocking overland surface flow, for USLE" ; 
+	opt5->required       = NO ; 
+	opt5->type           = TYPE_STRING ; 
+	opt5->gisprompt      = "old,cell,raster" ;
+
+	opt6 = G_define_option() ; 
+	opt6->key            = "threshold" ; 
+	opt6->description    = "Input value: minimum size of exterior watershed basin" ; 
+	opt6->required       = NO ; 
+	opt6->type           = TYPE_INTEGER ; 
+	opt6->gisprompt      = "new,cell,raster" ;
+
+	opt7 = G_define_option() ; 
+	opt7->key            = "max.slope.length" ; 
+	opt7->description    = "Input value: maximum length of surface flow, for USLE" ; 
+	opt7->required       = NO ; 
+	opt7->type           = TYPE_DOUBLE ; 
+	opt7->gisprompt      = "new,cell,raster" ;
+
+	opt8 = G_define_option() ; 
+	opt8->key            = "accumulation" ; 
+	opt8->description    = "Output map: number of cells that drain through each cell"; 
+	opt8->required       = NO ; 
+	opt8->type           = TYPE_STRING ; 
+	opt8->gisprompt      = "new,cell,raster" ;
+
+	opt9 = G_define_option() ; 
+	opt9->key            = "drainage" ; 
+	opt9->description    = "Output map: drainage direction" ; 
+	opt9->required       = NO ; 
+	opt9->type           = TYPE_STRING ; 
+	opt9->gisprompt      = "new,cell,raster" ;
+
+	opt10 = G_define_option() ; 
+	opt10->key            = "basin" ; 
+	opt10->description    = "Output map: unique label for each watershed basin" ; 
+	opt10->required       = NO ; 
+	opt10->type           = TYPE_STRING ; 
+	opt10->gisprompt      = "new,cell,raster" ;
+
+	opt11 = G_define_option() ; 
+	opt11->key            = "stream" ; 
+	opt11->description    = "Output map: stream segments" ; 
+	opt11->required       = NO ; 
+	opt11->type           = TYPE_STRING ; 
+	opt11->gisprompt      = "new,cell,raster" ;
+
+	opt12 = G_define_option() ; 
+	opt12->key            = "half.basin" ; 
+	opt12->description    = "Output map: each half-basin is given a unique value"; 
+	opt12->required       = NO ; 
+	opt12->type           = TYPE_STRING ; 
+	opt12->gisprompt      = "new,cell,raster" ;
+
+	opt13 = G_define_option() ; 
+	opt13->key            = "visual" ; 
+	opt13->description    = "Output map: useful for visual display of results" ; 
+	opt13->required       = NO ; 
+	opt13->type           = TYPE_STRING ; 
+	opt13->gisprompt      = "new,cell,raster" ;
+
+	opt14 = G_define_option() ; 
+	opt14->key            = "length.slope" ; 
+	opt14->description    = "Output map: slope length and steepness (LS) factor for USLE"; 
+	opt14->required       = NO ; 
+	opt14->type           = TYPE_STRING ; 
+	opt14->gisprompt      = "new,cell,raster" ;
+
+	opt15 = G_define_option() ; 
+	opt15->key            = "slope.steepness" ; 
+	opt15->description    = "Output map: slope steepness (S) factor for USLE" ; 
+	opt15->required       = NO ; 
+	opt15->type           = TYPE_STRING ; 
+	opt15->gisprompt      = "new,cell,raster" ;
+
+	opt16 = G_define_option() ; 
+	opt16->key            = "armsed" ; 
+	opt16->description    = "Output file: input to r.grass.armsed program" ; 
+	opt16->required       = NO ; 
+	opt16->type           = TYPE_STRING ; 
+
+	flag1 = G_define_flag() ;
+	flag1->key            = 'm' ;
+	flag1->description    = "Enable extend memory option: Operation is slow" ;
+
+	flag2 = G_define_flag() ;
+	flag2->key            = '4' ;
+	flag2->description    = "Allow only horizontal and vertical flow of water" ;
+
+	if (G_parser(argc, argv))
+		exit(1) ;
+
+/* Check option combinations */
+
+	/* Check for some output map */
+	if (   (opt8->answer == NULL)
+		&& (opt9->answer == NULL)
+		&& (opt10->answer == NULL)
+		&& (opt11->answer == NULL)
+		&& (opt12->answer == NULL)
+		&& (opt13->answer == NULL)
+		&& (opt14->answer == NULL)
+		&& (opt15->answer == NULL)
+		&& (opt16->answer == NULL))
+	{
+		fprintf(stderr,"\nSorry, you must choose some output map\n") ;
+		exit(1) ;
+	}
+
+	/* (arm_flag == 1) && ((bas_thres <= 0) || (haf_flag != 1)) */
+	/*  ARMSED              basin.threshold     half.basin      */
+
+	if ( (opt16->answer != NULL)
+	  && ((opt6->answer == NULL) 
+	       || ((opt12->answer == NULL) && (opt10->answer == NULL))))
+	{
+		fprintf(stderr,"\nSorry, if you chose to have ARMSED output (%s), you must also\n",
+			opt16->key) ;
+        fprintf(stderr,"    specify a basin threshold (%s) and an output map to \n",
+			opt6->key) ;
+        fprintf(stderr,"    contain the basins (%s or %s)\n", opt10->key, opt12->key) ;
+		exit(1) ;
+	}
+	
+	err = 0 ;
+	/* basin and basin.thresh */
+		err += (opt10->answer != NULL && opt6->answer == NULL) ;
+	/* stream and basin.thresh */
+		err += (opt11->answer != NULL && opt6->answer == NULL) ;
+	/* half.basin and basin.thresh */
+		err += (opt12->answer != NULL && opt6->answer == NULL) ;
+	/* slope and basin.thresh */
+		err += (opt15->answer != NULL && opt6->answer == NULL) ;
+	/* LS and basin.thresh */
+		err += (opt16->answer != NULL && opt6->answer == NULL) ;
+	
+	if (err)
+	{
+		fprintf(stderr,"\nSorry, if any of the following options are set:\n") ;
+		fprintf(stderr,"    basin, stream, half.basin, slope, or lS\n") ;
+		fprintf(stderr,"    you MUST provide a value for the basin threshold (basin.threshold)\n") ;
+		G_usage() ;
+		exit(1) ;
+	}
+
+	/* Build command line */
+	sprintf (command, "%s/etc/water/", G_gisbase()) ;
+
+	if (flag1->answer)
+		strcat(command,"seg4.0") ;
+	else
+		strcat(command,"ram4.0") ;
+
+	if (flag2->answer)
+		strcat(command," -4") ;
+	
+	if (opt1->answer)
+	{
+		strcat(command, " el=") ; strcat(command, "\"") ;
+		strcat(command, opt1->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt2->answer)
+	{
+		strcat(command, " de=") ; strcat(command, "\"") ;
+		strcat(command, opt2->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt3->answer)
+	{
+		strcat(command, " ov=") ; strcat(command, "\"") ;
+		strcat(command, opt3->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt4->answer)
+	{
+		strcat(command, " r=") ; strcat(command, "\"") ;
+		strcat(command, opt4->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt5->answer)
+	{
+		strcat(command, " ob=") ; strcat(command, "\"") ;
+		strcat(command, opt5->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt6->answer)
+	{
+		strcat(command, " t=") ;
+		strcat(command, opt6->answer) ;
+	}
+	
+	if (opt7->answer)
+	{
+		strcat(command, " ms=") ;
+		strcat(command, opt7->answer) ;
+	}
+	
+	if (opt8->answer)
+	{
+		strcat(command, " ac=") ; strcat(command, "\"") ;
+		strcat(command, opt8->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt9->answer)
+	{
+		strcat(command, " dr=") ; strcat(command, "\"") ;
+		strcat(command, opt9->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt10->answer)
+	{
+		strcat(command, " ba=") ; strcat(command, "\"") ;
+		strcat(command, opt10->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt11->answer)
+	{
+		strcat(command, " se=") ; strcat(command, "\"") ;
+		strcat(command, opt11->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt12->answer)
+	{
+		strcat(command, " ha=") ; strcat(command, "\"") ;
+		strcat(command, opt12->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt13->answer)
+	{
+		strcat(command, " di=") ; strcat(command, "\"") ;
+		strcat(command, opt13->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt14->answer)
+	{
+		strcat(command, " LS=") ; strcat(command, "\"") ;
+		strcat(command, opt14->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt15->answer)
+	{
+		strcat(command, " S=") ; strcat(command, "\"") ;
+		strcat(command, opt15->answer) ; strcat(command, "\"") ;
+	}
+	
+	if (opt16->answer)
+	{
+		strcat(command, " ar=") ; strcat(command, "\"") ;
+		strcat(command, opt16->answer) ; strcat(command, "\"") ;
+	}
+
+	fprintf(stderr,"Running: %s\n", command) ;
+	exit(system(command)) ;
+}
