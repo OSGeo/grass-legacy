e6ccaf427e12aed7c095f5f6f82cb3e327ea1b3d
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/mapdev/vs.digit/popup.h
@@ -0,0 +1,40 @@
+#include "wind.h"
+#include "graphics.h"
+
+#define ESC	033
+#define C_C	003
+#define CR	015
+#define NL	012
+#define BS	010
+
+#define  MEN_TOP	96
+#define  MEN_LEFT	10
+#define  MEN_SIZE	4
+
+#define BC_STOP	"red"
+#define TC_STOP	"white"
+#define DC_STOP	"black"
+
+#define BC_MAIN	"blue"
+#define TC_MAIN	"white"
+#define DC_MAIN	"black"
+
+#define BC_BUTN	"white"
+#define TC_BUTN	"blue"
+#define DC_BUTN	"white"
+
+#define BC_INFO	"orange"
+#define TC_INFO	"black"
+#define DC_INFO	"orange"
+
+#define BC_WARN	"yellow"
+#define TC_WARN	"black"
+#define DC_WARN	"yellow"
+
+#define BC_HELP	"white"
+#define TC_HELP	"black"
+#define DC_HELP	"black"
+
+#define BC_QUES	"white"
+#define TC_QUES	"black"
+#define DC_QUES	"black"
