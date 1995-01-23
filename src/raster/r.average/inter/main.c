f61a35551eef225ceafa461e53255af4581f86cf
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.average/inter/main.c
+++ src/raster/r.average/inter/main.c
@@ -15,21 +15,21 @@ main(argc, argv) char *argv[];
     sprintf (temp, " base='%s'", G_fully_qualified_name(name, mapset));
     strcat (command, temp);
 
-/* values map */
-    mapset = G_ask_cell_old ("Enter values map", name);
+/* cover map */
+    mapset = G_ask_cell_old ("Enter cover map", name);
     if (mapset == NULL) exit(0);
-    sprintf (temp, " values='%s'", G_fully_qualified_name(name, mapset));
+    sprintf (temp, " cover='%s'", G_fully_qualified_name(name, mapset));
     strcat (command, temp);
     sprintf (temp, "Are the values to be looked up from the category file for %s? ", name);
     if (G_yes(temp, -1))
 	strcat (command, " -c");
 
-/* result map */
-    if (!G_ask_cell_new("Enter name for the resulting map", name))
+/* output map */
+    if (!G_ask_cell_new("Enter name for the output map", name))
 	exit(0);
-    sprintf (temp, " result='%s'", name);
+    sprintf (temp, " output='%s'", name);
     strcat (command, temp);
 
 /* run the command-line version */
-    system (command);
+    exit(system (command));
 }
