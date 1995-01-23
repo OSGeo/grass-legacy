83a745b125b556c51445d96a51ab8b389e611a3c
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/libes/display/raster2.c
+++ src/libes/display/raster2.c
@@ -1,8 +1,8 @@
 /********************************************************************
  * code in this file is designed to send raster data to the graphics
- * driver. It handles cell->color lookup translation, as well as
+ * driver. It handles raster->color lookup translation, as well as
  * loading appropriate colormaps into the driver and the sending of
- * raster data to the plotter. The loading of colors is desinged to
+ * raster data to the plotter. The loading of colors is designed to
  * never send more colors than the hardware can support - even though
  * the GRASS drivers will allocate virtual colormaps to pretend there are more
  * This code effectively disables that driver feature/mistake.
@@ -17,10 +17,10 @@
  *   D_set_overlay_mode(flag)
  *      int flag;              /1=yes,0=no/
 
- * to plot a row of cell data
- *   D_raster (cell, ncols, nrows, colors)
- *      CELL *cell;            /cell data to be plotted/
- *      int ncols;             /number of columns in cell array/
+ * to plot a row of raster data
+ *   D_raster (raster, ncols, nrows, colors)
+ *      CELL *raster;            /raster data to be plotted/
+ *      int ncols;             /number of columns in raster array/
  *      int nrows;             /number of rows this array represents/
  *      struct Colors *colors; /color info/
  *
@@ -29,10 +29,10 @@
  *      CELL cat
  *      struct Colors *colors; /color info/
  *
- * to translate cell values into colors for calls to R_color()
- *   D_lookup_colors (cell, n, colors)
- *      CELL *cell;            /cell data to be plotted/
- *      int n;                 /number of items in cell array/
+ * to translate raster values into colors for calls to R_color()
+ *   D_lookup_colors (raster, n, colors)
+ *      CELL *raster;            /raster data to be plotted/
+ *      int n;                 /number of items in raster array/
  *      struct Colors *colors; /color info/
  *      
  *
@@ -75,48 +75,67 @@ D_set_colors (colors)
 /* check if the colors fit into the hardware colormap */
     G_get_color_range (&cmin, &cmax, colors);
     fixed = !D_check_colormap_size (cmin, cmax, &ncolors);
+if(getenv("DEBUG"))fprintf (stderr, "# monitor colors = %d (mode: %s)\n", ncolors, fixed?"fixed":"float");
 
     nalloc = 0; /* force a reallocation */
     if (fixed)
     {
-	for(nl=0; nl*nl*nl <= ncolors-1; nl++)
-		;
-	nl--;
-	ncolors = nl*nl*nl+1; /* reset ncolors to what we need for fixed */
-
-	/* create color translation table */
-	for(i=0; i<256; i++)
+	if (is_grey_scale (colors))
 	{
-	    RED[i] = (int)((i / 256.0) * nl) * nl * nl ;
-	    GRN[i] = (int)((i / 256.0) * nl) * nl ;
-	    BLU[i] = (int)((i / 256.0) * nl) ;
-	}
-
-	/* create the colortable for the driver */
-
-	if (ncolors > nalloc)
-	    allocate_colors (ncolors);
- 
-	if (nl > 1)
-	    span = 255.0 / (nl-1) ;
-	else
-	    span = 0.0;
-
-	*red = *grn = *blu = 255;
-	i = 1 ;
-        for(r=0; r<nl; r++)
-	{
-	    R = (int)(r * span) ;
-	    for(g=0; g<nl; g++)
+	    for (i = 0; i < 256; i++)
 	    {
-		G = (int)(g * span) ;
-		for(b=0; b<nl; b++)
+		RED[i] = (int) (i * (ncolors-1))/256;
+		GRN[i] = 0;
+		BLU[i] = 0;
+	    }
+	    if (ncolors > nalloc)
+		allocate_colors (ncolors);
+	    for (i = 0; i < ncolors; i++)
+		red[i] = grn[i] = blu[i] = (i*255)/ncolors;
+	}
+	else  /* use a color cube */
+	{
+	    /* compute size of color cube */
+	    for(nl=0; nl*nl*nl <= ncolors-1; nl++) {}
+	    nl--;
+
+	    /* reset ncolors to what we need for color cube */
+	    ncolors = nl*nl*nl+1;
+
+	    /* create color translation table */
+	    for(i=0; i<256; i++)
+	    {
+		RED[i] = (int)((i / 256.0) * nl) * nl * nl ;
+		GRN[i] = (int)((i / 256.0) * nl) * nl ;
+		BLU[i] = (int)((i / 256.0) * nl) ;
+	    }
+
+	    /* create the colortable for the driver */
+
+	    if (ncolors > nalloc)
+		allocate_colors (ncolors);
+     
+	    if (nl > 1)
+		span = 255.0 / (nl-1) ;
+	    else
+		span = 0.0;
+
+	    *red = *grn = *blu = 255;
+	    i = 1 ;
+	    for(r=0; r<nl; r++)
+	    {
+		R = (int)(r * span) ;
+		for(g=0; g<nl; g++)
 		{
-		    B = (int)(b * span) ;
-		    red[i] = R;
-		    grn[i] = G;
-		    blu[i] = B;
-		    i++;
+		    G = (int)(g * span) ;
+		    for(b=0; b<nl; b++)
+		    {
+			B = (int)(b * span) ;
+			red[i] = R;
+			grn[i] = G;
+			blu[i] = B;
+			i++;
+		    }
 		}
 	    }
 	}
@@ -175,8 +194,8 @@ D_color (cat, colors)
     R_color ((int)x);
 }
 
-D_lookup_colors (cell, ncols, colors)
-    CELL *cell;
+D_lookup_colors (raster, ncols, colors)
+    CELL *raster;
     struct Colors *colors;
 {
     int i;
@@ -186,32 +205,32 @@ D_lookup_colors (cell, ncols, colors)
 	if (ncols > nalloc)
 	    allocate_colors (ncols);
 
-	G_lookup_colors (cell, red, grn, blu, set, ncols, colors);
+	G_lookup_colors (raster, red, grn, blu, set, ncols, colors);
 	for (i = 0; i < ncols; i++)
-	    if (!D__overlay_mode || cell[i])
-		cell[i] = RED[red[i]] + GRN[grn[i]] + BLU[blu[i]] + 1;
+	    if (!D__overlay_mode || raster[i])
+		raster[i] = RED[red[i]] + GRN[grn[i]] + BLU[blu[i]] + 1;
     }
     else
     {
 	for (i = 0; i < ncols; i++)
 	{
-	    if(cat = cell[i])
+	    if(cat = raster[i])
 	    {
 		if (cat < cmin || cat > cmax)
-		    cell[i] = 0;
+		    raster[i] = 0;
 		else
-		    cell[i] -= cmin-1;
+		    raster[i] -= cmin-1;
 	    }
 	}
     }
 }
 
-D_raster (cell, ncols, nrows, colors)
-    CELL *cell;
+D_raster (raster, ncols, nrows, colors)
+    CELL *raster;
     struct Colors *colors;
 {
-    D_lookup_colors (cell, ncols, colors);
-    R_raster (ncols, nrows, !D__overlay_mode, cell);
+    D_lookup_colors (raster, ncols, colors);
+    R_raster (ncols, nrows, !D__overlay_mode, raster);
 }
 
 /* This routine determines if the number of required colors
@@ -239,7 +258,8 @@ D_check_colormap_size (min, max, ncolors)
     R_get_num_colors (&hardware_ncolors);
     *ncolors = max - min + 2; /* extra color for 0 */
 
-/* if we need more colors than there are in the driver, then return hardware_ncolors
+/* if we need more colors than there are in the driver,
+ * then return hardware_ncolors
  * otherwise return ncolors;
  */
     if (*ncolors <= 1 || *ncolors > hardware_ncolors)
@@ -253,8 +273,7 @@ D_check_colormap_size (min, max, ncolors)
 static
 synch()
 {
-    int junk;
-    R_get_num_colors (&junk); /* this will cause a round trip to driver */
+    R_stabilize();
 }
 
 static
@@ -265,4 +284,24 @@ allocate_colors(ncolors)
     blu = (unsigned char *) G_realloc (blu, ncolors);
     set = (unsigned char *) G_realloc (set, ncolors);
     nalloc = ncolors;
+}
+
+static
+is_grey_scale(colors)
+    struct Colors *colors;
+{
+    int r, g, b;
+    CELL min, max;
+
+    G_get_color_range (&min, &max, colors);
+
+    G_get_color ((CELL)0, &r, &g, &b, colors);
+    if (r != g || r != b || g != b) return 0;
+
+    while (min <= max)
+    {
+	G_get_color (min++, &r, &g, &b, colors);
+	if (r != g || r != b || g != b) return 0;
+    }
+    return 1;
 }
