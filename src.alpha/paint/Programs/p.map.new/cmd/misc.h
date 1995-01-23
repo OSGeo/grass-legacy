3edd1a7f3338b13b8a2973ebdcdf044160e29375
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.alpha/paint/Programs/p.map.new/cmd/misc.h
@@ -0,0 +1,13 @@
+#ifdef MAIN
+    int ncolors ;
+    int verbose ;
+    int BLACK;
+    int WHITE;
+    int GREY;
+#else
+    extern int ncolors ;
+    extern int verbose ;
+    extern int BLACK;
+    extern int WHITE;
+    extern int GREY;
+#endif
