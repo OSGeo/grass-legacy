78232904c4c9f38eead9332bd142adec31a5ae3f
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.alpha/display/d.rast.zoom/cur_to_db.c
+++ src.alpha/display/d.rast.zoom/cur_to_db.c
@@ -1,14 +1,14 @@
 #include "gis.h"
 #include "glob.h"
 cur_to_db(window)
-struct Cell_head window;
+struct Cell_head *window;
 {
     char name[30], window_dir[30];
     char *mapset;
 
     strcpy(name,"lastwindow");
     strcpy(window_dir,"windows");
-    G_copy(&cur_window,&window,sizeof cur_window);
+    G_copy(&cur_window,window,sizeof cur_window);
     mapset = G_mapset();
     /*
     mapset = G_ask_new_ext ("please enter a window name to save current window",
