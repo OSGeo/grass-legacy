ab84f84f3219a4984ccfbacf0946b4310bb316ce
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/xgrass/display/y.tab.h
@@ -0,0 +1,36 @@
+
+typedef union  {
+ char *cval;
+ int ival;
+} YYSTYPE;
+extern YYSTYPE yylval;
+# define DOCUMENTSIZE 257
+# define PAGEWIDTH 258
+# define PAGEHEIGHT 259
+# define UNITS 260
+# define NIL 261
+# define OBJECT 262
+# define GEOFRAME 263
+# define REGION 264
+# define RASTER_MAP 265
+# define VECTOR_MAP 266
+# define SITE_MAP 267
+# define STANDARDSITE 268
+# define PIXMAPSITE 269
+# define FREEHANDSITE 270
+# define GRID 271
+# define BARSCALE 272
+# define LEGEND 273
+# define SQUARE 274
+# define RECTANGLE 275
+# define CIRCLE 276
+# define ELLIPSE 277
+# define POLYLINE 278
+# define POLYGON 279
+# define SPLINE 280
+# define OPEN_INTERP_SPLINE 281
+# define CLOSED_INTERP_SPLINE 282
+# define OPEN_APPROX_SPLINE 283
+# define CLOSED_APPROX_SPLINE 284
+# define LABEL 285
+# define String 286
