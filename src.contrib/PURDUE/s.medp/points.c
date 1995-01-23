4bc30a75e663afb52ca0052bdecbc2adc624a6bd
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/PURDUE/s.medp/points.c
@@ -0,0 +1,43 @@
+/*- void write_points (Map,n, thresh)   // no return value
+ * struct Map_info *Map;                // map struct, including file pointer
+ * int n;                               // number of sites
+ * int thresh;                          // threshold
+ *
+ * writes sites as points in the vector file
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include "polish.h"
+#include "gis.h"
+#include "Vect.h"
+#include "dig_structs.h"
+
+void write_points (Map, n,  verbose)
+  struct Map_info *Map;
+  int n,  verbose;
+{
+  static struct line_pnts *Points;
+  double x[2], y[2];
+  int i;
+  extern float *datax, *datay;
+
+  Points = Vect_new_line_struct ();	/* init line_pnts struct */
+
+  if (verbose)
+    fprintf (stderr, "Writing points ...                  ");
+  for (i = 0; i < n; ++i)
+  {
+    x[0] = x[1] = datax[i];
+    y[0] = y[1] = datay[i];
+
+    if (Vect_copy_xy_to_pnts (Points, x, y, 2) > 0)
+      G_fatal_error ("Out of memory");
+    Vect_write_line (Map, DOT, Points);
+    if (verbose)
+      G_percent (i, n, 1);
+  }
+  if (verbose)
+    G_percent (1, 1, 1);
+}
