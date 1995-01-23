7ecd6802b95d3253f59c7ec769d4442eb9d595bc
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.contrib/EPA/grass.ipw/grass.to.ipw/main.c
+++ src.contrib/EPA/grass.ipw/grass.to.ipw/main.c
@@ -1,6 +1,6 @@
 
 #ifndef lint
-static char *SCCSid=	"SCCS version: @(#)   main.c   1.10   5/9/91";
+static char *SCCSid=	"SCCS version: @(#)   main.c   1.12   9/5/91";
 #endif
 
 /*
@@ -8,48 +8,74 @@ static char *SCCSid=	"SCCS version: @(#)   main.c   1.10   5/9/91";
 **	grass.to.ipw -- Converts GRASS raster map layer to IPW image file
 **
 ** SYNOPSIS
-**	grass.to.ipw [-r] map=name [header=name] [units=name] \
+**	grass.to.ipw
+**	grass.to.ipw help
+**	grass.to.ipw [-r] [-o] map=name [header=name] [units=name] \
 **	                 [divisor=value] [offset=value]	
 **
 ** DESCRIPTION
-**	grass.to.ipw converts a GRASS cell file to an IPW image file.
-**	It runs under GRASS and assumes that the GRASS active program window is
-**	set with number of rows and columns equal to that of the target IPW
-**	image file.  The output image is written to standard output.
+**	grass.to.ipw converts a GRASS raster file to an IPW image file.
+**
 **	A linear quantization header may be provided with -h option or stdin;
-**	otherwise the linear quantization values are derived from the data
-**	(the floating point maximum may be increased to preserve the precision
-**	of the data).  If both the -r flag and a header file are specified,
+**	otherwise the number of bits per pixel and floating point map
+**	are derived from the min and max cell values (the floating point
+**	map maximum may be increased to preserve the precision of the data).
+**
+**	If both the -r (raw mode) flag and a header file are specified,
 **	the given linear quantization header will be written to the output
 **	image, but the data values will be copied as raw pixel values
-**	bypassing the floating point mapping.
+**	bypassing the floating point mapping (see examples below).
+**
+**	In GRASS, the geographic region (window) specifies the north,
+**	south, east and west edges of the raster map.  The data values are
+**	assumed to be located at the cell centers so that the data value
+**	for the north-westernmost pixel (1,1) is actually located at
+**	northing NORTH-NSRES/2 and easting WEST+EWRES/2	(where NORTH
+**	is the northern edge of the file, WEST is the western edge of
+**	the file and NSRES and EWRES are the resolutions in the
+**	N/S and E/W directions, respectively).  In IPW, the image origin
+**	(beginning line and sample coordinates) are assumed to be at the
+**	center of pixel (0,0).  Unless the -o (override) option is set,
+**	the output image GEO header will be set so that:
+**		bline = NORTH+NSRES/2
+**		bsamp = WEST+EWRES/2.
+**		dline = -NSRES
+**		dsamp = EWRES
+**	If the -o option is specified, bline will be set to NORTH and
+**	bsamp will be set to west.
+**
+**	The output image is written to standard output.
 **
 ** OPTIONS
 **
 **	Flags:
 **	-r                  raw values (don't convert to linear quantization)
+**	-o		    use the GRASS NORTH and WEST values for the
+**			    GEO origin
+**			    default: origin is offset by half a pixel in
+**			    each direction (see discussion above)
 **
 **	Parameters:
-**      map=name            input GRASS cell file
-**      header=name         input linear quantization header file
-**      units=name          data units for annotation
-**      divisor=value       divide each map data by this value
-**                          default: 1.0
-**      offset=value        add to each map data (prior to division)
-**                          default: 0.0
+**	map=name            input GRASS raster file
+**	header=name         input linear quantization header file
+**	units=name          data units for annotation
+**	divisor=value       divide each map data by this value
+**			    default: 1.0
+**	offset=value	    add to each map data (prior to division)
+**			    default: 0.0
 ** 	
 **
 ** EXAMPLES
 **
-**      The following command,
+**	The following command,
 ** 
-**               grass.to.ipw map=grasslayer divisor=10.0 offset=-100.0 \
-**                         > ipwfile
+**		grass.to.ipw map=grasslayer divisor=10.0 offset=-100.0 \
+**		       > ipwfile
 ** 
-**      will convert a GRASS map layer called  grasslayer to an IPW
-**      image file called ipwfile, subtracting 100.0 from all input
-**      values then dividing the result by 10.0 before storing in the
-**      IPW output file.
+**	will convert a GRASS map layer called  grasslayer to an IPW
+**	image file called ipwfile, subtracting 100.0 from all input
+**	values then dividing the result by 10.0 before storing in the
+**	IPW output file.
 **
 **	The following pipeline of commands,
 **
@@ -58,10 +84,16 @@ static char *SCCSid=	"SCCS version: @(#)   main.c   1.10   5/9/91";
 **
 **	creates an IPW image with 200 lines and 500 samples with linear
 **	floating point mapping defined by the pairs (0,0) and (255,1.275).
-**	The data values in GRASS cell file "rawlayer" are copied as raw
+**	The data values in GRASS raster file "rawlayer" are copied as raw
 **	pixel values, so that a value of 200 in input map layer grasslayer
 **	is copied as raw value 200 in the IPW image, and will be mapped to
 **	floating point value of 1.0.
+**
+**	To convert several GRASS raster files into a multi-band IPW image:
+**		grass.to.ipw map=raster.1 > ipw.1
+**		    ...
+**		grass.to.ipw map=raster.N > ipw.N
+**		mux ipw.1 ... ipw.N > image
 ** 
 **
 ** FILES
@@ -83,6 +115,11 @@ static char *SCCSid=	"SCCS version: @(#)   main.c   1.10   5/9/91";
 **	4/29/91 A header file and raw (-r) can now both be specified - the
 **	        header will be writted, and the pixels will be copied as raw
 **		pixels, by K. Longley, OSU, EPA ERL-C
+**	9/4/91  Changed so that raster file need not be in current mapset and
+**		current window need not be the same as the raster file region;
+**		fixed bug (BIH write) when header specified;
+**		added half-pixel offset for GEO origin and -o option to
+**		override, by K. Longley, OSU, EPA ERL-C
 */
 
 #include "gis.h"
@@ -97,30 +134,37 @@ main (argc, argv)
 	char *argv[];
 {
 	int		ipw_fd;		/* IPW image file descriptor  */
-	int		cf_fd;		/* GRASS cell file descriptor */
+	int		cf_fd;		/* GRASS raster file desc     */
 	int		lqh_fd;		/* LQH file descriptor        */
 	int		got_lqh;	/* LQH specified flag	      */
 	int		verbose;	/* verbose flag               */
 	double		divisor;	/* data divisor		      */
 	double		offset;		/* data offset		      */
-	char	       *mapset;		/* -> name of current mapset  */
-	char		r_mapset[MAXN];	/* name of reclass mapset     */
-	char		r_name[MAXN];	/* name of reclass cell file  */
-	struct Cell_head window;	/* GRASS active window        */
+	char	       *mapset;		/* -> name of mapset          */
+	struct Cell_head cellhd;	/* GRASS cell header          */
 	struct Range 	range;		/* GRASS range of data values */
 
 	struct Option	*grass_file, *lqh, *units, *divisor_opt, *offset_opt;
 	struct Flag	*raw;
+	struct Flag	*override;
+
+   /* Initialize GRASS library */
+
+	G_gisinit (argv[0]);
 
 	raw = G_define_flag();
    	raw->key = 'r';
    	raw->description = "raw values (don't convert to linear quantization)";
 
+	override = G_define_flag();
+   	override->key = 'o';
+   	override->description = "don't offset GEO origin by half-pixel - use GRASS raster file values";
+
 	grass_file = G_define_option();
 	grass_file->key = "map";
 	grass_file->type = TYPE_STRING;
 	grass_file->required = YES;
-	grass_file->description = "input GRASS cell file";
+	grass_file->description = "input GRASS raster file";
 	grass_file->gisprompt = "old,cell,raster";
 
         lqh = G_define_option();
@@ -192,32 +236,38 @@ main (argc, argv)
 	ipw_fd = ustdout();
 	no_tty(ipw_fd);
 
-   /* Initialize GRASS library */
 
-	G_gisinit (argv[0]);
+   /* open GRASS raster file */
 
-   /* open GRASS cell file */
-
-	mapset = G_mapset();
-	cf_fd = G_open_cell_old (grass_file->answer, mapset);
-	if (cf_fd < 0) {
-		error ("Error opening GRASS cell file\n");
+	mapset = G_find_file ("cell", grass_file->answer, "");
+	if (!mapset) {
+		error ("<%s> not found", grass_file->answer);
 	}
 
-   /* get window and range of values in cell file */
+	cf_fd = G_open_cell_old (grass_file->answer, mapset);
+	if (cf_fd < 0) {
+		error ("Error opening GRASS raster file\n");
+	}
 
-	G_get_set_window (&window);
+   /* get cell header and set active program window */
+
+	if (G_get_cellhd (grass_file->answer, mapset, &cellhd) == -1) {
+		error ("Error reading cell header");
+	}
+	G_set_window (&cellhd);
+
+   /* get cell header and range of values in raster file */
 	if (G_read_range (grass_file->answer, mapset, &range) < 0)
 		error ("error reading GRASS range file");
 
-   /* read cell file header/write IPW headers */
+   /* read raster file header/write IPW headers */
 
-	headers (ipw_fd, (int) raw->answer, got_lqh, lqh_fd, window, range,
-		units->answer, divisor, offset);
+	headers (ipw_fd, (int) raw->answer, (int) override->answer, got_lqh,
+		 lqh_fd, cellhd, range, units->answer, divisor, offset);
 
-   /* convert GRASS cell file to IPW file */
+   /* convert GRASS raster file to IPW file */
 
-	grass2ipw (ipw_fd, cf_fd, window.rows, window.cols, divisor, offset,
+	grass2ipw (ipw_fd, cf_fd, cellhd.rows, cellhd.cols, divisor, offset,
 		   (int) raw->answer);
 
    /* finished */
