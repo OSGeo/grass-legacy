ca1770ad5e21602575b8b1d2264a563e240d45d9
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/display/devices/XDRIVER/XDRIVER/Graph_Set.c
+++ src/display/devices/XDRIVER/XDRIVER/Graph_Set.c
@@ -3,17 +3,15 @@
  * Decstation X11r3 by David B. Satnik on 8/90 */
 #include <stdio.h>
 #include <signal.h>
-#include <X11/Xos.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
+#include "includes.h"
 #include <X11/Xatom.h>
 #include <X11/cursorfont.h>
 #include "icon.bit"
 #include "win.h"
+#include "version.h"
 
-#define WIN_NAME   "GRASS 4.0"
-#define BORDER  1
-#define FONT "9x15"
+#define BORDER  3
+#define FONT "fixed"
 
 #define HORIZ_PIXELS 1000        /* 512 x 512 for small driver */
 #define VERT_PIXELS  800        /* 1000 x 800 for big driver */
@@ -26,25 +24,18 @@
 
 /* declare global variables */
 int SCREEN_LEFT, SCREEN_RIGHT, SCREEN_BOTTOM, SCREEN_TOP;
-unsigned SC_WID, SC_HITE;
-int NCOLORS = 256 - 20, backing_store;
+unsigned SC_WID = 0, SC_HITE = 0;
+int NCOLORS, backing_store;
 
-/*
-Display *dpy = NULL;
-Window grwin;
-*/
 int scrn = 0;
 GC gc;
 Colormap floatcmap, fixedcmap, InitColorTableFixed();
-/*
-Colormap fixedcmap, InitColorTableFixed();
-*/
 Cursor grcurse, grxh;
 u_long gemask;
-char *grasstr = "GRASS 4.0 - X Display Window";
 XFontStruct *fontstruct;        /* Font descpritor */
 Atom wmProtocolsAtom;
 Pixmap bkupmap = (Pixmap)0;
+char *malloc(), *calloc(), *realloc();
 
 
 int Graph_Set(argc, argv)
@@ -56,12 +47,16 @@ char **argv;
     XSetWindowAttributes xswa;  /* Set Window Attribute struct */
     XWindowAttributes xwa;      /* Get Window Attribute struct */
     Atom closedownAtom;
+#ifndef X11R3
     XTextProperty windowName, iconName;
+#endif /* X11R3 */
     XSizeHints *szhints;
     XClassHint *clshints;
     XWMHints *wmhints;
     int sigint();
     XColor fg_color, bg_color, xcolors[256];
+    char title[1024];
+
 
     /* Open the display using the $DISPLAY environment variable to
      * locate the X server. Return 0 if cannot open. */
@@ -70,7 +65,8 @@ char **argv;
         return 0;
     }
     scrn = DefaultScreen(dpy);
-    backing_store = DoesBackingStore(DefaultScreenOfDisplay(dpy));
+
+    backing_store = (DoesBackingStore(DefaultScreenOfDisplay(dpy)) == Always);
 
     /* Load the font to use. GRASS doesn't at this point use fonts, but
      * may someday in the future. */
@@ -81,7 +77,19 @@ char **argv;
     }
     /* Allocate and set the fixed colormap, in the default X colormap
      * if possible, and set background and foreground of gc */
-    NCOLORS = DisplayCells(dpy, scrn);
+    /****************************************************************
+     * this code change was suggested by Ann Stout, SCS contractor
+     * (303)-229-0414 to solve the problem if two drivers are started,
+     * the second driver has less colors to work with than the first.
+     * It works wonderfully sometimes, other times the second driver
+     * will die. The color logic in this code (and in GRASS generally)
+     * is convoluted, and needs close scrutiny and revision. This
+     * "hack" just solves one problem in certain cases while creating
+     * more problems in other scenarios
+     ***************************************************************/
+/*  NCOLORS = DisplayCells(dpy, scrn) ; */
+    NCOLORS = GetRealNumberOfColors(dpy, scrn) ;
+
     bg_color.pixel = BlackPixel(dpy, scrn);
     fg_color.pixel = WhitePixel(dpy, scrn);
     XQueryColor(dpy, DefaultColormap(dpy, scrn), &bg_color);
@@ -92,47 +100,37 @@ char **argv;
     /* Allocate floating colormap and set with b/w stripes */
     floatcmap = XCreateColormap(dpy, DefaultRootWindow(dpy),
             DefaultVisual(dpy, scrn), AllocAll);
-    for (i = 0; i < 256; i++) {
+    for (i = 0; i < NCOLORS; i++) {
         xcolors[i].pixel = i;
-    /*
-        xcolors[i].flags = DoRed | DoGreen | DoBlue;
-        if (i & 1)
-            xcolors[i].red = xcolors[i].green = xcolors[i].blue =
-                    (u_short) 65535;
-        else
-            xcolors[i].red = xcolors[i].green = xcolors[i].blue = 0;
-    */
     }
     XQueryColors(dpy, fixedcmap, xcolors, NCOLORS);
     XStoreColors(dpy, floatcmap, xcolors, NCOLORS);
 
     /* Deal with providing the window with an initial position & size.
      * Window is is not resizable */
+#ifdef X11R3
+    szhints = (XSizeHints *)malloc(sizeof(XSizeHints));
+#else
     szhints = XAllocSizeHints();
-    /*
-    szhints->flags = (PPosition | PSize | PMinSize | PMaxSize);
+#endif
+
     szhints->flags = (USPosition | USSize | PMinSize | PMaxSize);
-    szhints->width = szhints->min_width = szhints->max_width =
-            HORIZ_PIXELS;
-    szhints->height = szhints->min_height = szhints->max_height =
-            VERT_PIXELS;
-    */
-    szhints->flags = (USPosition | USSize | PMinSize | PMaxSize);
-    szhints->width    = 450;
-    szhints->height = 400;
-    szhints->max_width    = DisplayWidth(dpy, scrn) - 20;
-    szhints->min_width    = 0;
-    szhints->min_height = 0;
-    szhints->max_height = DisplayHeight(dpy, scrn) - 30 ;
-    szhints->x = szhints->y = 10 ;
+	szhints->max_width	= DisplayWidth(dpy, scrn) - 20;
+	szhints->min_width	= 0;
+	szhints->min_height = 0;
+	szhints->max_height = DisplayHeight(dpy, scrn) - 30 ;
+	get_user ("XDRIVER_HEIGHT", &szhints->height, 400);
+	get_user ("XDRIVER_WIDTH",  &szhints->width,  450);
+	get_user ("XDRIVER_LEFT",   &szhints->x,      10);
+	get_user ("XDRIVER_TOP",    &szhints->y,      10);
 
     /* Start with the window's colormap field pointing to the default
      * colormap. Later pointer enter events will cause a switch to the
      * grass color map. Also, set Bit Gravity to reduce Expose events. */
     xswa.event_mask = gemask = 
-        ExposureMask | ButtonPressMask | StructureNotifyMask ;
+	    ExposureMask | ButtonPressMask | StructureNotifyMask;
 
-    xswa.backing_store = Always;
+    xswa.backing_store = (backing_store ? Always : NotUseful);
     xswa.colormap = fixedcmap;
     xswa.border_pixel = fg_color.pixel;
     xswa.background_pixel = bg_color.pixel;
@@ -141,37 +139,55 @@ char **argv;
 
     grwin = XCreateWindow(dpy, DefaultRootWindow(dpy), szhints->x,
             szhints->y, (unsigned)szhints->width, (unsigned)szhints->height, 
-            BORDER, DefaultDepth(dpy, scrn),
-            InputOutput, DefaultVisual(dpy, scrn),
-            (CWEventMask | CWBackingStore | CWColormap | CWBorderPixel |
-            CWBackPixel), &xswa);
+			BORDER, DefaultDepth(dpy, scrn),
+			InputOutput, DefaultVisual(dpy, scrn),
+			(CWEventMask | CWBackingStore | CWColormap | CWBorderPixel |
+			CWBackPixel), &xswa);
 
     /* properties for window manager */
+#ifdef X11R3
+    wmhints = (XWMHints *)malloc(sizeof(XWMHints));
+#else
     wmhints = XAllocWMHints();
+#endif
     wmhints->icon_pixmap = XCreateBitmapFromData(dpy, grwin, icon_bits,
             icon_width, icon_height);
     wmhints->flags |= IconPixmapHint;
 
+#ifdef X11R3
+    clshints = (XClassHint *)malloc(sizeof(XClassHint));
+#else
+    clshints = XAllocClassHint();
+#endif
+    clshints->res_name = NULL;
+    clshints->res_class = WIN_NAME;
+#ifndef X11R3
+    sprintf (title, "%s - Monitor: %s", WIN_NAME, argv[0]);
     iconName.encoding = XA_STRING;
     iconName.format = 8;
-    iconName.value = (u_char *) WIN_NAME;
+    iconName.value = (u_char *) title;
     iconName.nitems = strlen((char *) iconName.value);
     windowName.encoding = iconName.encoding = XA_STRING;
     windowName.format = iconName.format = 8;
-    windowName.value = (u_char *) WIN_NAME;
+    windowName.value = (u_char *) title;
     windowName.nitems = strlen((char *) windowName.value);
-
-    clshints = XAllocClassHint();
-    clshints->res_name = NULL;
-    clshints->res_class = WIN_NAME;
     XSetWMProperties(dpy, grwin, &windowName, &iconName, argv, argc,
             szhints, wmhints, clshints);
+#endif
     closedownAtom = XInternAtom(dpy, "WM_DELETE_WINDOW", False);
     wmProtocolsAtom = XInternAtom(dpy, "WM_PROTOCOLS", False);
+#ifdef X11R3
+    {
+        char data[25];
+        Long2Bytes((long)closedownAtom, data);
+        AppendProperty(dpy,grwin,wmProtocolsAtom,XA_ATOM,32,data,1);
+    }
+#else
     (void) XSetWMProtocols(dpy, grwin, &closedownAtom, 1);
+#endif
 
     /* Create the cursors to be used later */
-    grcurse = grxh = XCreateFontCursor(dpy, XC_arrow);
+    grcurse = grxh = XCreateFontCursor(dpy, XC_crosshair);
 
     /* Create the GC for writing the text. */
     gcv.font = fontstruct->fid;
@@ -181,6 +197,7 @@ char **argv;
 
     /* Map the window to make it visible. This causes an expose event */
     XMapWindow(dpy, grwin);
+    XFlush(dpy);
 
     /* Find out how big the window really is (in case window manager
      * overrides our request) and set the SCREEN values. */
@@ -194,7 +211,7 @@ char **argv;
     SC_WID = xwa.width;
     SC_HITE = xwa.height;
 
-    if (backing_store != Always) {
+    if (!backing_store) {
         /* Now create a pixmap that will contain same contents as the
          * window. It will be used to redraw from after expose events */
         bkupmap = XCreatePixmap(dpy, grwin, SC_WID, SC_HITE, xwa.depth);
@@ -217,4 +234,70 @@ static sigint()
     exit(-1);
 }
 
+#ifdef X11R3
+
+Long2Bytes(l,data)
+    long l;
+    char data[];
+{
+    int i;
+    union {
+        long l;
+        char bytes[4];
+    } converter;
+    
+    converter.l = 1;
+    for ( i = 0; i < 4; i++ ) {
+        data[i] = converter.bytes[i];
+    }
+}
+
+AppendProperty(display, window, property, target, format, data, number_items)
+    Display *display;
+    Window window;
+    Atom property, target;
+    int format;
+    char *data;
+    int number_items;
+{
+    if ( number_items > 0 ) {
+        XChangeProperty(display, window, property, target, format,
+              PropModeAppend, data, number_items);
+    }
+}
+#endif
+
+GetRealNumberOfColors(dpy,screen)
+Display *dpy;
+int screen;
+{
+    int i = DisplayCells(dpy,screen);
+    int cant_alloc = 1;
+    unsigned long *mypixels = (unsigned long*)calloc(i,sizeof(unsigned long));
+
+    while ( i && cant_alloc ) {
+	int result;
+
+	result = XAllocColorCells(dpy, DefaultColormap(dpy,screen), 0,
+		    NULL, 0, mypixels, i);
+	if ( result != 0 ) cant_alloc = 0;
+	else i--;
+    }
+    if ( !cant_alloc ) 
+        XFreeColors(dpy, DefaultColormap(dpy,screen), mypixels, i, 0);
+    free(mypixels);
+    return i;
+}
+
+static
+get_user (name, n, value)
+    char *name;
+    int *n;
+    int value;
+{
+    char *getenv(), *p;
+
+    if ((p=getenv(name)) && sscanf (p, "%d", n) == 1) return;
+    *n = value;
+}
 /*** end Graph_Set.c ***/
