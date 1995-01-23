c39de0a41a89a8a8240058247affc1e561b4ea73
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.garden/answers/src.answers/raster/r.answers/inter/hit_return.c
@@ -0,0 +1,31 @@
+/* %G% %W% */
+/* 
+    +---------------------------------------------------------+
+    |            ANSWERS on GRASS Integration Project         |
+    |  Developed in the Agriculture Engineering Department    |
+    |                at Purdue University                     |
+    |                        by                               |
+    |           Chris Rewerts and Bernard Engel               |
+    |                                                         |
+    |   (c)Copyright, 1992 Purdue Research Foundation, West   |
+    |   Lafayette, Indiana 47907. Permission to use, copy,    |
+    |   modify, and distribute this software and its          |
+    |   documentation for any purpose and without fee is      |
+    |   hereby granted, provided that the above copyright     |
+    |   notice appear in all copies.  This software is        |
+    |   provided "as is" without express or implied warranty. |
+    +---------------------------------------------------------+
+
+   */
+
+hit_return()
+{
+    char buf[100];
+    extern char *gets();
+    
+    printf ("hit RETURN to continue -->");
+    if (!gets(buf)) exit(0); 
+    G_strip (buf);
+    if (strcmp (buf, "exit") == 0) exit(0);
+        return(0);
+}
