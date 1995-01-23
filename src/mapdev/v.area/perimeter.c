b6338a199a28192b4029d5af58703c5871a74506
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/mapdev/v.area/perimeter.c
+++ src/mapdev/v.area/perimeter.c
@@ -1,4 +1,4 @@
-#include <math.h>
+#include "gis.h"
 
 double
 perimeter(np,x,y)
@@ -6,26 +6,23 @@ perimeter(np,x,y)
   register double *x, *y;
  {
   register double tot_perim;
-  register double hyp;
   register int i;
   register int ii, jj;
-  extern double hypot();
-  
+
   tot_perim = 0.0;
   for (i=0; i < (np-1); i++) 
-   {
-    if ( i==((np-1)-1) )
+  {
+     if ( i==((np-1)-1) )
      {
       ii = i;
       jj = 0;
      }
-    else
+     else
      {
       ii = i;
       jj = i + 1;
      }
-    hyp = hypot(  ((*(x+jj))-(*(x+ii))), ((*(y+jj))-(*(y+ii)))  );
-    tot_perim = tot_perim + hyp;
-   }
+     tot_perim = tot_perim + G_distance(*(x+jj), *(y+jj), *(x+ii), *(y+ii));
+  }
   return(tot_perim);
  }
