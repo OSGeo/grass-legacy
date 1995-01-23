717157f343f9d7e3ac86d17d854f54999965ebea
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/libes/linkm/linkm.h
@@ -0,0 +1,27 @@
+#ifndef FILE
+#  include <stdio.h>
+#endif
+
+#define VOID_T char
+
+
+VOID_T *	link_init ();
+void 		link_set_chunk_size ();
+VOID_T *	link_new ();
+void 		link_destroy ();
+void 		link_cleanup ();
+void 		link__set_next();		/* link next routine */
+VOID_T * 	link__get_next();	/* link next routine */
+
+
+#define PTR_CNT 10
+
+struct link_head {
+    VOID_T **ptr_array;		/* array of pointers to chunks */
+    int max_ptr;		/* num of chunks alloced */
+    int alloced;		/* size of ptr_array */
+    int chunk_size;		/* size of alloc chucks in units */
+    int unit_size;		/* size of each user defined unit */
+    VOID_T *Unused;		/* Unused list pointer */
+    int exit_flag;		/* exit on error ? */
+};
