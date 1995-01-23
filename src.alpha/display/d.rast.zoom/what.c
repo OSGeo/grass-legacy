d483e930021182ede3c04f9c9b9cedfb891331a2
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.alpha/display/d.rast.zoom/what.c
+++ src.alpha/display/d.rast.zoom/what.c
@@ -2,11 +2,10 @@
 
 what (name,window)
 char name[128];
-struct Cell_head window;
+struct Cell_head *window;
 {
     int width;
     int i;
-    struct Cell_head make_window();
     int	count, map_id, t,b,l,r, j;
     int row, col;
     int nrows, ncols;
@@ -23,11 +22,11 @@ struct Cell_head window;
 	exit(1);
 	}
 
-    G_get_set_window (&window);
-    nrows = window.rows;
-    ncols = window.cols;
+    G_get_set_window (window);
+    nrows = window->rows;
+    ncols = window->cols;
 
-    window = make_window(&window, name, mapset);
+    make_window(window, name, mapset);
 
     R_standard_color(D_translate_color("black"));
 
@@ -35,5 +34,5 @@ struct Cell_head window;
     D_clear_window();
     R_close_driver();
 
-    confirm_window(&window, name, mapset);
+    confirm_window(window, name, mapset);
 }
