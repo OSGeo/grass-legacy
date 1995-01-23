b4c44bed91ebc15095db01551221946c22fa46db
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.garden/grass.rim.old/v.db.rim/cmd/opcode.h
@@ -0,0 +1,39 @@
+
+/* constants for command opcodes */
+#define NOP 0
+#define ADD_OP 12
+#define ADD_FIELD_OP 13
+#define ADD_DONE_OP 14
+#define BACKUP_OP 6
+#define CHANGE_OP 19
+#define CHANGE_FIELD_OP 20
+#define CHANGE_DONE_OP 21
+#define DELETE_OP 9
+#define DELETE_SITE_OP 10
+#define DELETE_DONE_OP 11
+#define DELETE_FOUND_OP 23
+#define END_OP 25
+#define FIND_OP 15
+#define LIST_OP 5
+#define MAKE_OP 1
+#define MAKE_ADD_OP 2
+#define MAKE_DONE_OP 3
+#define PACK_OP 7
+#define QUERY_OP 16
+#define QUERY_LINE_OP 17
+#define QUERY_DONE_OP 18
+#define REMOVE_OP 8
+#define SHOW_OP 4
+#define TABLES_OP 22
+#define SYNTAX_ERR 24
+#define INPUT_OP 26
+#define OUTPUT_OP 27
+#define PRINT_OP 28
+#define VECT_MAP_OP 29
+#define FIND_INIT_OP 30
+#define HELP_OP 31
+#define READ_VECT_OP 32
+#define MAP_OP 33
+#define SITE_LIST_OP 34
+#define EXIT_OP 999
+
