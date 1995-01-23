c06708f523d5ca4fa32ad7fcf6df97f850b5017c
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/NPS/r.in.utm/utm.h
@@ -0,0 +1,15 @@
+/* %W% %G% */
+
+struct UTM
+{
+    double north;
+    double south;
+    double east;
+    double west;
+    double ns_res;
+    double ew_res;
+    int ncols;
+    int nrows;
+    int bpc;
+    int sflag;
+};
