2c1b094a22ba6230a188810ebdd1f22a99e3f75f
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/misc/m.dem.examine/usgs.h
+++ src/misc/m.dem.examine/usgs.h
@@ -1,6 +1,7 @@
 #include "gis.h"
 
 #ifndef MAIN
+#define RECORD_SIZE 1024
 #define GLOBAL extern
 #else
 #define GLOBAL
@@ -18,7 +19,9 @@ GLOBAL float	file_north,file_south,file_east,file_west;
 GLOBAL char	*inf,*of,*buffer,*buf_start,*buf_end;
 GLOBAL double  col,row,n,s,e,w;
 GLOBAL int     c,r,cur_row;
+GLOBAL int     record_pos;
 GLOBAL char    *tapename,name[51],command[300];
 GLOBAL int     fd,filestat,tapefile;
+GLOBAL int     count;
 GLOBAL int     skip_row,skip_col,profile_buf_size;
 GLOBAL CELL *profile_buf;
