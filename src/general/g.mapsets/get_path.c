8a6e5b6f7b3cf68d9b9bfb9bf5c70dbfedb7c622
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/general/g.mapsets/get_path.c
+++ src/general/g.mapsets/get_path.c
@@ -1,3 +1,4 @@
+#include <string.h>
 #include "externs.h"
 #include "gis.h"
 get_mapset_path ()
@@ -7,6 +8,7 @@ get_mapset_path ()
     char *b;
     char name[50];
     int n;
+    int action;	/* defines action to be taken in set_path function */
 
     printf("Hit RETURN to keep current list, or ");
     printf("enter a new list of mapsets\n");
@@ -14,6 +16,9 @@ get_mapset_path ()
     {
 	printf("(enter the numbers before the mapset name, ");
 	printf("or the names themselves)\n");
+	printf ("    (to ADD to end of list, use + . . .)\n");
+	printf ("    (to DELETE from list, use - . . .)\n");
+	printf ("    ('+' or '-' must be followed by space)\n");
 	first = 0;
     }
 
@@ -24,10 +29,28 @@ get_mapset_path ()
     if (sscanf (buf,"%s",name) != 1)
 	    goto same;
 
+    if (strlen (name) == 1) 
+	switch (name[0]) {
+	case '+':	/* preload existing path into choice array */
+	    action = ADD;
+            for (n = 0; n < ncurr_mapsets;
+	     choice[nchoices++] = curr_mapset[n++]);
+	    goto next;
+	    break;
+	case '-':
+	    action = DELETE;
+	    goto next;
+	}
+    action = REPLACE;
+	
     while (1)
     {
 	if (sscanf (b, "%s", name) != 1)
+	{
+	    if (action == DELETE)
+		return delete_choices ();
 	    return 1;
+	}
 
 	for (n = 0; n < nmapsets; n++)
 	    if (strcmp (name, mapset_name[n]) == 0)
@@ -65,5 +88,38 @@ same:
 		break;
 	    }
     }
+    return 1;
+}
+
+
+delete_choices ()
+{
+    int i, n;
+    int deletion;	/* map number to be deleted */
+
+/* action is delete:  modify previous mapset list and write to choice array */
+
+    for (i = 0; i < nchoices; i++)
+    {
+	deletion = choice[i];
+        for (n = 0; n < ncurr_mapsets; n++)
+            if (curr_mapset[n] == deletion)  /* delete mapset from path */
+            {
+                curr_mapset[n] = -1;
+                break;
+            }
+	if (n == ncurr_mapsets)
+	{
+	    printf("\n<%s> not found in search list\n\n",
+	     mapset_name[deletion]);
+            return -1;
+        }
+    }
+
+/* prepare choice array list for final processing */
+    nchoices = 0;
+    for (n = 0; n < ncurr_mapsets; n++)
+	if (curr_mapset[n] >= 0)	/* i.e., not deleted */
+             choice[nchoices++] = curr_mapset[n];
     return 1;
 }
