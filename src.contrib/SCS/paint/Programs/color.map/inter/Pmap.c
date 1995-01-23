b6dded1a526ca82e80e21540ad531cf838cf1365
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Programs/color.map/inter/Pmap.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+Pmap (scriptfile, background)
+    char *scriptfile;
+{
+    char *getenv();
+    char arg[1024];
+
+    sprintf (arg, "input=%s", scriptfile);
+    if (background && (G_fork() > 0))
+	exit(0);
+    execlp ("color.map", "color.map", arg,0);
+    G_fatal_error ("unable to exec grey.map");
+}
