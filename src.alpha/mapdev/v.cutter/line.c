3cc2f5fe1ac3ec8475d197c41d95bf71b125fe60
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.alpha/mapdev/v.cutter/line.c
@@ -0,0 +1,33 @@
+/**** line.c ****/
+/*
+**  Written by David Gerdes  Fall 1992
+**  US Army Construction Engineering Research Lab
+**  Copyright  David Gerdes  USA-CERL  1992
+*/
+
+
+
+#include "cutter.h"
+#include "math.h"
+
+main (argc, argv)
+    char *argv[];
+{
+  struct line_t L1;
+  struct point_t P;
+  double x, y;
+  int ret;
+
+      L1.p1.x = atof (argv[1]);
+      L1.p1.y = atof (argv[2]); 
+      L1.p2.x = atof (argv[3]);
+      L1.p2.y = atof (argv[4]);
+      P.x = atof (argv[5]); 
+      P.y = atof (argv[6]);
+
+      ret = point_to_right_of_segment (&L1, &P);
+
+      printf (" return =  %2d: %s\n", ret, ret ? "RIGHT" : "LEFT");
+
+      return !ret;
+}
