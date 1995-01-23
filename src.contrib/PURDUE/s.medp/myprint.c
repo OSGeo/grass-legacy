1dd8c36c1c803c79f721f6235a21353daea11c25
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/PURDUE/s.medp/myprint.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "polish.h"
+myprint (y, yold)
+  double ***y, ***yold;
+{
+  int i, j;
+/*
+  for (i = 23; i >= 0; --i)
+  {
+    for (j = 0; j < 17; ++j)
+      if (y[i][j][0] != EMPTY_CELL)
+	fprintf (stderr, "%02.2f\t", y[i][j][0]);
+      else
+	fprintf (stderr, "      \t");
+    fprintf (stderr, "\n");
+  }
+*/
+}
