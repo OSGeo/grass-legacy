b9835e073415fe20a013a8f60b10f63522c2b6cc
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/imagery/i.composite/gen_colors.c
+++ src/imagery/i.composite/gen_colors.c
@@ -11,7 +11,7 @@ gen_colors ()
     for (r = 0; r < r_level; r++)
 	for (g = 0; g < g_level; g++)
 	    for (b = 0; b < b_level; b++)
-		G_set_color (x++,
+		G_set_color (++x,
 		    r*256/(r_level-1), g*256/(g_level-1), b*256/(b_level-1),
 		    &colr);
     G_write_colors (result, G_mapset(), &colr);
