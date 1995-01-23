f77d3d80f85ebe83f95a0459d0fbe71ba3833b4d
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Programs/color.map/inter/ask_scale.c
@@ -0,0 +1,37 @@
+#include "gis.h"
+
+static char *explanation[] =
+{
+"",
+"you may specify the map scale in one of the following ways:",
+"",
+"  1 : n",
+"  1 inch equals n <units>    <units> can be miles, meters, or kilometers",
+"  n inches                   absolute horizontal width of output",
+"  n panels                   output will fill n panels",
+"",
+0};
+
+ask_scale (fd)
+    FILE *fd;
+{
+    char **c;
+    char buf[200];
+
+/*    printf ("\nSCALE\n");
+    while(1)
+    {
+	printf ("enter map scale: ");
+	input (buf);
+	if (scan_scale (buf))
+	    break;
+	for (c = explanation; *c; c++)
+	    printf ("%s\n", *c);
+    }
+*/
+	sprintf(buf,"1 panel");
+    fprintf (fd, "scale %s\n", buf);
+    begin_record ("SCALE:");
+    add_record (buf);
+    end_record();
+}
