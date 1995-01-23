ea1537995b8f03e20c4492d81d6c6a454e01aeec
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/PURDUE/v.autocorr/att2cat.c
@@ -0,0 +1,23 @@
+#include "gis.h"
+
+double att2cat (Map, i)
+  struct Map_info *Map;
+  int i;
+{
+  double atof (), tmp;
+  extern struct Categories cats;
+
+  tmp = atof (G_get_cat (i, cats));
+  if (!tmp)
+    G_warning ("non-numeric or zero category label found");
+  return tmp;
+}
+
+/*
+ * char * G_get_cat (num, pcats) CELL num                     category number
+ * struct Categories *pcats     structure to hold category info
+ * 
+ * Returns pointer to a string describing category.
+ * 
+ * <3:11am> /usr/grass4/src/libes/gis# more cats.c
+ */
