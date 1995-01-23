fb2cc686659fc635639ceaa9a45fe2491940cbbd
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/display/devices/XDRIVER/XDRIVER/includes.h
@@ -0,0 +1,11 @@
+
+#ifdef INTERGRAPH
+#include <sys/types.h>
+#include <string.h>
+#include <sys/time.h>
+#include <sys/socket.h>
+#else
+#include <X11/Xos.h>
+#endif
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
