91f6419b5879b97c9f9da634bfd73025f2c3c179
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/mapdev/digitizers/numonics/dig_curses.c
+++ src/mapdev/digitizers/numonics/dig_curses.c
@@ -1,11 +1,10 @@
-
-#include  <curses.h>
+#include <curses.h>
 
 #include "dig_menu.h"
-#include "../../v.digit/digit.h"
-#include "../../v.digit/menu.h"
+#include "../../v.digit2/digit.h"
+#include "../../v.digit2/menu.h"
 
 
-#include "../../v.digit/curses.C"
+#include "../../v.digit2/curses.C"
 
 
