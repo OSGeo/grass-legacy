c532f202ca1bd133e4bbd316b27c3277838dbfd8
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.alpha/imagery/i.tape.spot/unmnt_tape.c
@@ -0,0 +1,18 @@
+/********************************************************/
+/* NAME: 		unmount_tape			*/
+/*							*/
+/* FUNCTION:		close tape device		*/
+/*							*/
+/* USAGE:		unmount_tape()			*/
+/*							*/
+/* INPUT:		none				*/
+/*							*/
+/* OUTPUT:		none				*/
+/********************************************************/
+#include "tape.h"
+
+unmount_tape()
+{
+    close (tape.fd);
+    tape.fd = -1;
+}
