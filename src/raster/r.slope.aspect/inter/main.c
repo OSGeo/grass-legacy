1c286cdd75017c22a0a46734cb9e6fbb652d5375
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.slope.aspect/inter/main.c
+++ src/raster/r.slope.aspect/inter/main.c
@@ -6,8 +6,8 @@ char *intro[] =
 "",
 "This program produces aspect and slope maps from elevation data.",
 "The elevation layer must be true elevation values, not rescaled.",
-"The aspect map will indicate the direction of the slopes in 24",
-"directions.  The slope map will have slopes in degrees.",
+"The aspect map will indicate the direction of the slopes in 360",
+"directions.  The slope map will have slopes in degrees or in percent.",
 "Please enter the elevation file, and the names for the resulting",
 "aspect and slope maps.",
 0
@@ -15,12 +15,16 @@ char *intro[] =
 
 main (argc,argv) char *argv[];
 {
-    char elevation[30], *mapset;
-    char aspect[30];
-    char slope[30];
+    char elevation[256], *mapset;
+    char aspect[256];
+    char slope[256];
     char command[1024];
     int i;
     int zero_is_data;
+    int units;
+    char buf[25];
+    char temp_buf[25];
+    double zfactor, min_slp;
 
 
     G_gisinit (argv[0]);
@@ -45,9 +49,8 @@ main (argc,argv) char *argv[];
     else
 	zero_is_data = 0;
 
-
 /* run the command-line version */
-    sprintf (command, "r.slope.aspect 'elevation=%s'", 
+    sprintf (command, "r.slope.aspect elevation='%s'", 
 	G_fully_qualified_name(elevation, mapset));
     if (*aspect)
     {
@@ -59,7 +62,60 @@ main (argc,argv) char *argv[];
 	strcat (command, " slope=");
 	strcat (command, slope);
     }
+    if(*slope!=0) 
+    {
+        if(G_yes("Should the slope be reported in percent instead of degrees?", -1))
+  	    strcat(command, " format=percent");
+    }
+
     if (zero_is_data)
 	strcat (command, " -z");
+    
+    while(1)
+    {
+        printf("\nenter  1 if elevation units are meters\n       2 if elevation units are feet\n       3 for other elevation units.\n>");
+        while(!G_gets(temp_buf));
+        if(sscanf(temp_buf, "%d", &units)!=1) continue;
+        if ((units==1)||(units==2)||(units==3)) break;
+        printf("\n You can only enter 1, 2 or 3\n");
+    }
+    if(units==2)
+       zfactor = .3048000;
+    else 
+    {
+           if(units==1)
+                  zfactor = 1.0;
+           else
+           {
+	      while(1)
+	      {
+                 printf("\nEnter the factor for converting elevation units to meters >");
+		 while(!G_gets(temp_buf));
+		 if(sscanf(temp_buf, "%lf", &zfactor)!=1) continue;
+	         if (zfactor>0) break;
+	         printf("\n zfactor must be positive real number\n");
+              }
+           }
+    }	    
+    sprintf(buf, " zfactor=%lf", zfactor);
+    strcat (command, buf); 
+
+    min_slp = 0.;
+    printf("\nWARNING: The computations of aspect for the points with very\n");
+    printf("small slope are very far from being exact \n");
+    if(G_yes("Do you want to specify a minimum value of slope for which aspect is computed?",-1))
+    {
+       while(1)
+       {
+           printf("\nEnter the minimum value for slope (in percent):");
+           while(!G_gets(temp_buf));
+           if(sscanf(temp_buf, "%lf", &min_slp)!=1) continue;
+           if (min_slp>=0)break;
+           printf("\n min_slp must be non-negative real number\n");
+       }
+    }
+    sprintf(buf, " min_slp=%lf", min_slp);
+    strcat (command, buf); 
+
     exit (system(command));
 }
