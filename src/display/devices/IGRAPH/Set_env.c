a0d44a763ea33598568cbc8f8a43d48fc4742ce5
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/display/devices/IGRAPH/Set_env.c
+++ src/display/devices/IGRAPH/Set_env.c
@@ -35,8 +35,8 @@
 *
 *  Get window parameters from GRASS environment variables (.grassrc).
 *
-*  env variable IGRAPH_TOP =  SCREEN_BOTTOM
-*  env variable IGRAPH_LEFT =  SCREEN_RIGHT
+*  env variable IGRAPH_TOP =  SCREEN_TOP
+*  env variable IGRAPH_LEFT =  SCREEN_LEFT
 *  env variable IGRAPH_BOT =  SCREEN_BOTTOM
 *  env variable IGRAPH_RIGHT =  SCREEN_RIGHT
 *  env variable IGRAPH_VS =  SCREEN_VS
@@ -51,6 +51,8 @@
 #include	"igraphics.h"
 #include	"gis.h"
 
+int     IGRAPH_CLRS_SAV; /*save color range before offset subtraction */
+                         /*for .grassrc file (IGRAPH_COLORS)*/
 extern  int SCREEN_LEFT ;
 extern  int SCREEN_RIGHT ;
 extern  int SCREEN_BOTTOM ;
@@ -98,14 +100,25 @@ Set_screen_parameters()
 
 	find_primary_screen(&SCREEN_VS) ;
 
-	SCREEN_LEFT =  0 ;
-	SCREEN_TOP =  0 ;
+	SCREEN_LEFT =  1 ; /*adust_x_border*/
+	SCREEN_TOP =  29 ; /*adjust y border*/
 
-	SCREEN_BOTTOM = info[SCREEN_VS].vsi_y - 1  ;
-	SCREEN_RIGHT = info[SCREEN_VS].vsi_x - 1 ;
+	SCREEN_BOTTOM = info[SCREEN_VS].vsi_y - 29  ; /*adjust y border*/
+	SCREEN_RIGHT = info[SCREEN_VS].vsi_x - 1 ; /*adjust x border*/
 
 	NCOLORS = info[SCREEN_VS].vsi_vlt_size ;
-
+/*DEBUG*/
+/*
+*      fprintf (stderr,"virtual screen = %d\n", SCREEN_VS);
+*      fprintf (stderr, "vsi_flags = %x vsi_size = %d\n", info[SCREEN_VS].vsi_flags, NCOLORS);
+*      fprintf (stderr, "vsi_fixed_vlt_size = %d\n", info[SCREEN_VS].vsi_fixed_vlt_size);
+*      fprintf (stderr, "vsi_fixed_vlt_start = %d\n", info[SCREEN_VS].vsi_fixed_vlt_start);
+*
+*      fprintf (stderr,"virtual screen = %d\n", SCREEN_VS+1);
+*      fprintf (stderr, "vsi_flags = %x vsi_size = %d\n", info[SCREEN_VS+1].vsi_flags, NCOLORS);
+*      fprintf (stderr, "vsi_fixed_vlt_size = %d\n", info[SCREEN_VS+1].vsi_fixed_vlt_size);
+*      fprintf (stderr, "vsi_fixed_vlt_start = %d\n", info[SCREEN_VS+1].vsi_fixed_vlt_start);
+*/
     if (Raster_Buffer != NULL)
         free (Raster_Buffer);
     Raster_Buffer = (short *) malloc (info[SCREEN_VS].vsi_x * sizeof (short));
@@ -123,7 +136,7 @@ Set_screen_parameters()
 		value = -1 ;
 		value = atoi(env_value) ;
 
-		if ( value >= SCREEN_TOP && value <= SCREEN_BOTTOM )
+		if ( value >= SCREEN_TOP && value <= (SCREEN_BOTTOM - 50) )
 			SCREEN_TOP = value ;
 	}
 
@@ -132,22 +145,31 @@ Set_screen_parameters()
 		value = -1 ;
 		value = atoi(env_value) ;
 
-		if ( value > SCREEN_TOP && value <= SCREEN_BOTTOM
+		if ( value > (SCREEN_TOP + 50) && value <= SCREEN_BOTTOM
 		&&   value > 5)
 			SCREEN_BOTTOM = value ;
-		else
-			SCREEN_BOTTOM = info[SCREEN_VS].vsi_y - ADJUST_Y_BORDER ;
 	}
 	else
-		SCREEN_BOTTOM = info[SCREEN_VS].vsi_y - ADJUST_Y_BORDER ;
+	{
+		/*if IGRAPH_BOT not set, set small screen size:
+		   with bottom at slightly above mid-screen*/
+
+		SCREEN_BOTTOM = (SCREEN_BOTTOM - SCREEN_TOP)/2 - 10;
+    }
 
 	if (( env_value = G__getenv ("IGRAPH_LEFT")) != NULL )
 	{
 		value = -1 ;
 		value = atoi(env_value) ;
 
-		if ( value >= SCREEN_LEFT && value <= SCREEN_RIGHT )
+		if ( value >= SCREEN_LEFT && value <= (SCREEN_RIGHT - 50) )
 			SCREEN_LEFT = value ;
+	}
+	else
+	{
+		/*if IGRAPH_LEFT not set, set small screen size:
+		   with left edge at slightly right of mid-screen*/
+		   SCREEN_LEFT = (SCREEN_RIGHT - SCREEN_LEFT)/2 + 10;
 	}
 
 	if (( env_value = G__getenv ("IGRAPH_RIGHT")) != NULL )
@@ -155,14 +177,10 @@ Set_screen_parameters()
 		value = -1 ;
 		value = atoi(env_value) ;
 
-		if ( value > SCREEN_LEFT && value <= SCREEN_RIGHT
+		if ( value > (SCREEN_LEFT + 50) && value <= SCREEN_RIGHT
 		&&   value > 5)
 			SCREEN_RIGHT = value ;
-		else
-			SCREEN_RIGHT = info[SCREEN_VS].vsi_x - ADJUST_X_BORDER ;
 	}
-	else
-		SCREEN_RIGHT = info[SCREEN_VS].vsi_x - ADJUST_X_BORDER ;
 
 	if (( env_value = G__getenv ("IGRAPH_COLORS")) != NULL )
 	{
@@ -202,6 +220,7 @@ Set_screen_parameters()
 
 	if (NCOLORS > max_colors)
 		NCOLORS =   max_colors ;
+		
 
 /*   unless hardware with limited color capacity, set offset to skip over
 **   igraph fixed colors -- dks    note that offset = 15.
@@ -212,9 +231,11 @@ Set_screen_parameters()
      if (NCOLORS > FEW_COLORS)
          I_COLOR_OFFSET = 15;
      else
-		 I_COLOR_OFFSET = 0;
+	 I_COLOR_OFFSET = 0;
 
-	 NCOLORS -= I_COLOR_OFFSET;
+     IGRAPH_CLRS_SAV = NCOLORS; /*save colors before offset subtraction */
+				/*for writing to .grassrc */
+     NCOLORS -= I_COLOR_OFFSET;
 
 /*  This is used by Panel.c  */
 	VSI_PLANE_MASK =  info[SCREEN_VS].vsi_plane_mask ;
@@ -253,6 +274,7 @@ find_primary_screen(primary_vs_no)
 	int  current_vs_no ;
 
 	Inq_displayed_vs(&current_vs_no) ;
+/*DEBUG fprintf (stderr, "current vs_no = %d\n", current_vs_no);*/
 
 	/*                             xlo,  ylo, xhi, yhi */
 	Create_win( current_vs_no, "VS win ", 10, 10, 30, 30, &w_no) ;
@@ -261,7 +283,7 @@ find_primary_screen(primary_vs_no)
 
 	for ( vs_no = MIN_VS_NO; vs_no <= MAX_VS_NO; ++vs_no)
 	{
-	
+
 		if ( ! Set_win_vs( w_no, vs_no) )
 		{
 			*primary_vs_no = vs_no ;
