81f27e7a0a64815c54e755e6a43c649a9ef4f760
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/mapdev/diglib/clock.c
+++ src/mapdev/diglib/clock.c
@@ -31,3 +31,26 @@ dig_stop_clock (save)
     printf (" %3d min%c %02d sec%c\n", 
 	mins, mins == 1 ? ' ' : 's', secs, secs == 1 ? ' ' : 's');
 }
+
+char *
+dig_stop_clock_str (save)
+    long *save;
+{
+    register int mins, secs;
+    long stop, diff;
+    static char buf[200];
+
+    stop = time (NULL);
+
+    if (save != NULL)
+	diff = stop - *save;
+    else
+	diff = stop - watch;
+
+    mins = diff / 60;
+    secs = diff  % 60;
+    sprintf (buf, " %3d min%c %02d sec%c", 
+	mins, mins == 1 ? ' ' : 's', secs, secs == 1 ? ' ' : 's');
+
+    return buf;
+}
