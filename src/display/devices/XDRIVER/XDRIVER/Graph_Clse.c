dd176863b441eecfb73b1122c836d798c1840960
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/display/devices/XDRIVER/XDRIVER/Graph_Clse.c
+++ src/display/devices/XDRIVER/XDRIVER/Graph_Clse.c
@@ -1,8 +1,7 @@
 /* Close down the graphics processing.  This gets called only at driver
  * termination time. */
-#include <X11/Xos.h>
-#include <X11/Xlib.h>
-#include <X11/Xutil.h>
+
+#include "includes.h"
 
 Graph_Close()
 {
