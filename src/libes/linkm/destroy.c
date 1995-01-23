4a74e92b23a9d1edd01dcf69ede7ae5d614e2598
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/libes/linkm/destroy.c
@@ -0,0 +1,20 @@
+/*
+**  Written by David Gerdes  US Army Construction Engineering Research Lab
+**  	April 1992
+**  Copyright 1992 USA-CERL   All rights reserved.
+**
+*/
+#include "linkm.h"
+
+
+void
+link_destroy (Head, ptr)
+    struct link_head *Head;
+    VOID_T *ptr;
+{
+    if (NULL == ptr)
+	return;
+
+    link__set_next (ptr, Head->Unused);		/* ptr->next = Unused */
+    Head->Unused = ptr;				/* Unused = ptr */
+}
