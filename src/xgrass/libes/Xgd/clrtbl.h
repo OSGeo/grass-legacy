a570811b6c13e36eda13d165220f6caea4f3cde9
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/xgrass/libes/Xgd/clrtbl.h
@@ -0,0 +1,19 @@
+#define FIXEDCOLR	12
+
+Pixel *lookup_tbl;
+Pixel *vect_tbl;
+
+int ncolors;
+
+Pixel s_cidx;
+Pixel e_cidx;
+Pixel c_cidx;
+
+
+
+Pixel plane_masks[2];
+
+#define NO_DITHER               1
+#define DITHER                  2
+
+
