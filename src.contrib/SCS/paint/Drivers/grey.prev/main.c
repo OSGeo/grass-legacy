3b9b673106964d4a755811416af46959d6ff9803
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Drivers/grey.prev/main.c
@@ -0,0 +1,5 @@
+static char rcsid[]="$Header: /gis3/4.0/src/paint/Drivers/preview/RCS/main.c,v 1.1 1991/09/28 22:04:41 paul Exp $";
+main(argc,argv) char *argv[];
+{
+    paint_interface(argc, argv);
+}
