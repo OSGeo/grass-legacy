3c6ce3adeaf9eaee7343867cf29367f3b09b83ce
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.reclass/inter/set_cats.c
+++ src/raster/r.reclass/inter/set_cats.c
@@ -1,22 +1,26 @@
 #include "gis.h"
 
-set_new_cats(cats, new_cats, table) 
+set_new_cats(cats, new_cats, table, min, max) 
     struct Categories *cats ;
     struct Categories *new_cats ;
     long *table ;
+    CELL min, max;
 {
-    long i ;
-    long j ;
+    long i,j ;
+    CELL cMin = table[0];
+    CELL cMax = cMin;
 
-    for (i=0; i <= new_cats->num; i++)
-    {
-	for (j=0; j <= cats->num; j++)
-	{
-	    if (table[j] == i)
-	    {
-		G_set_cat ((CELL)i, G_get_cat ((CELL)j, cats), new_cats);
-		break ;
-	    }
+    for (i=(long)min+1; i <= (long)max; i++) {
+	if (cMin > table[i-min])
+	    cMin = table[i-min];
+	if (cMax < table[i-min])
+	    cMax = table[i-min];
 	}
-    }
+
+    for (j=(long)cMin; j <= (long)cMax;j++)
+         for (i=(long)min; i <= (long)max; i++)
+        {
+	if (table[i-min] == j)
+            G_set_cat (j, G_get_cat ((CELL)i, cats), new_cats);
+        }
 }
