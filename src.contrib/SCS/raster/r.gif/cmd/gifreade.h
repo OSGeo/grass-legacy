0586c70601ac4c680ad130d2edf988aad490561a
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/raster/r.gif/cmd/gifreade.h
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------*/
+/* Copyright (c) 1987							*/
+/* by CompuServe Inc., Columbus, Ohio.  All Rights Reserved		*/
+/* GIFREADE.H can be copied and distributed freely for any		*/
+/* non-commercial purposes. GIFREADE.H can only be incorporated		*/
+/* into commercial software with the permission of CompuServe Inc.	*/
+/*----------------------------------------------------------------------*/
+
+/* Header file GIFREADE.H */
+
+short ReadScreenDesc();
+/*
+ * Read the signature, the screen description, and the optional default
+ * color map.
+ */
+
+short ReadImageDesc();
+/*
+ * Read the image description and the optional color map.
+ */
