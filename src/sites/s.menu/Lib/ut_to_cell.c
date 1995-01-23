71f10c5e074e1842c9fc7b2ab12725c5b77ca434
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/sites/s.menu/Lib/ut_to_cell.c
+++ src/sites/s.menu/Lib/ut_to_cell.c
@@ -8,14 +8,17 @@ float
 northing_to_row (north, window)
 
 	struct Cell_head *window;
+        double north;
 {
-	return (float)((window->north - north)/window->ns_res);
+	return (float)G_northing_to_row(north, window);
 }
 
 float
 easting_to_col (east, window)
 
 	struct Cell_head *window;
+        double east;
 {
-	return (float)((east - window->west)/window->ew_res) ;
+	return (float)G_easting_to_col(east, window);
 }
+
