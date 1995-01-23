99d3766997c3a7c39779e06b7be3b6db2a18ed88
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.alpha/mapdev/v.out.arc/gtoa.h
+++ src.alpha/mapdev/v.out.arc/gtoa.h
@@ -6,6 +6,8 @@ int pol_flg;
 int lin_flg;
 int lab_flg;
 int txt_flg;
+char separator;
+int space = 0;
 
 #else
 
@@ -13,5 +15,7 @@ extern int pol_flg;
 extern int lin_flg;
 extern int lab_flg;
 extern int txt_flg;
+extern char separator;
+extern int space;
 
 #endif
