01dfc0da2ea470999b18102e3b24667382d8a52c
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/imagery/i.tape.other/ask_format.c
+++ src/imagery/i.tape.other/ask_format.c
@@ -10,8 +10,10 @@ ask_format ()
     filesize = 0;
     skipfiles = 0;
     skiprecords = 0;
+    skipbytes = 0;
     nbands = 0;
     tapebufsize = 0;
+    blocking_factor = 1;
 
     do
     {
@@ -24,27 +26,31 @@ ask_format ()
 
 	V_line (3, "tape layout");
 	V_line (5, "          number of tape files to be skipped");
-	V_ques (&skipfiles, 'i', 5, 1, 2);
+	V_ques (&skipfiles, 'i', 5, 1, 4);
 
 	V_line (6, "          number of records in the remaining files to be skipped");
-	V_ques (&skiprecords, 'i', 6, 1, 2);
+	V_ques (&skiprecords, 'i', 6, 1, 4);
+	V_line (7, "          number of bytes in each record to be skipped");
+	V_ques (&skipbytes, 'i', 7, 1, 4);
 
-	V_line (8, "band files");
-	V_line (10,"          number of bands on the tape");
-	V_ques (&nbands, 'i', 10, 1, 1);
+	V_line (9, "band files");
+	V_line (11,"          number of bands on the tape");
+	V_ques (&nbands, 'i', 11, 1, 4);
 
-	V_line (12,"data format");
-	V_line (13,"          band sequential  (BSQ)  |   mark one with an x");
-	V_line (14,"          band interleaved (BIL)  |");
-	V_ques (bsq, 's', 13, 1, 1);
-	V_ques (bil, 's', 14, 1, 1);
+	V_line (13,"data format");
+	V_line (14,"          band sequential  (BSQ)  |   mark one with an x");
+	V_line (15,"          band interleaved (BIL)  |");
+	V_ques (bsq, 's', 14, 1, 1);
+	V_ques (bil, 's', 15, 1, 1);
 
-	V_line (16,"          if you select BSQ format and all the bands are in a single file");
-	V_line (17,"          enter the total number of records in the file. Otherwise enter 0");
-	V_ques (&filesize, 'i', 16, 1, 6);
+	V_line (17,"          if you select BSQ format and all the bands are in a single file,");
+	V_line (18,"          enter the total number of records in the file. Otherwise enter 0");
+	V_ques (&filesize, 'i', 17, 1, 6);
 
-	V_line (19,"          length (in bytes) of the longest record on the tape");
-	V_ques (&tapebufsize, 'i', 19, 1, 6);
+	V_line (20,"          length (in bytes) of the longest record on the tape");
+	V_ques (&tapebufsize, 'i', 20, 1, 6);
+	V_line (21,"          blocking factor of data in the file");
+	V_ques (&blocking_factor, 'i', 21, 1, 4);
 
 	I_v_exec();
 
@@ -57,6 +63,12 @@ ask_format ()
 	if (skiprecords < 0)
 	{
 	    skiprecords = 0;
+	    repeat = 1;
+	}
+
+	if (skipbytes < 0)
+	{
+	    skipbytes = 0;
 	    repeat = 1;
 	}
 
@@ -74,6 +86,11 @@ ask_format ()
 	if (tapebufsize <= 0)
 	{
 	    tapebufsize = 0;
+	    repeat = 1;
+	}
+	if (blocking_factor <=0)
+	{
+	    blocking_factor = 1;
 	    repeat = 1;
 	}
 
