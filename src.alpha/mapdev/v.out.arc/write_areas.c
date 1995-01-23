d2908edb42229697741997c46c4ea3481c44290a
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.alpha/mapdev/v.out.arc/write_areas.c
+++ src.alpha/mapdev/v.out.arc/write_areas.c
@@ -20,11 +20,11 @@ FILE   *lines_file,
 	int offset;
 	int count;
 	int attflag, NumPoints ;
-	char att[1000];
+	char *att;
 	int alloc_points;
 	int n_points;
 	int n_atts;
-	int i,n,catflag=-1;
+	int i,n,catflag= -1;
 	plus_t zero = 0;
 	struct line_pnts *Gpoints;
 	struct Categories cats;
@@ -35,14 +35,11 @@ FILE   *lines_file,
 	printf("write_areas %s %s\n",name,mapset);
 #endif
 
-	if ((catflag=G_read_vector_cats(name,mapset,&cats)) != -1)
+	catflag = G_read_vector_cats(name,mapset,&cats);
+		
+        if(catflag == -1)
 	{
-		n = cats.count;
-		G_sort_cats(&cats);
-	}
-	else 
-	{
-#  ifdef DEBUG
+#ifdef DEBUG
 		printf("   error %d reading vector cats\n",catflag);
 #  endif
 	}
@@ -71,25 +68,28 @@ FILE   *lines_file,
 			attflag=1;
 			ax = map->Att[Area->att].x;
 			ay = map->Att[Area->att].y;
-			fprintf(points_file,"%d %lf %lf\n",count,ax,ay);
+			fprintf(points_file,"%d%c%lf%c%lf\n",count, separator, 
+					    ax, separator, ay);
 			lab_flg=1;
 		}
 
 		/* write category number and attribute text to text_file */
 		if (attflag==1 && catflag!=-1)
 		{
-			/* get attribute text */
-			i=0; 
-			att[0]=0;
-			do {
-				if (cats.list[i].num == map->Att[Area->att].cat)
-				{
-					sscanf(cats.list[i].label,"%s",att);
-					i=n;
-				}
-				i++;
-			}      while (i<n);
-			fprintf(text_file,"%d %d %d %s\n",count,map->Att[Area->att].cat,count,att);
+ 		   /* get attribute text */
+		   i=0; 
+		   att = G_get_cat(map->Att[Area->att].cat, &cats);
+		   if(space)
+		   {
+		      while(att[i])
+		      {
+			 if(att[i] == ' ') att[i] = '_';
+			 i++;
+                      }
+                   }
+		   fprintf(text_file,"%d%c%d%c%d%c%s\n",count, separator, 
+				    map->Att[Area->att].cat, separator,
+				    count, separator, att);
 			txt_flg=1;
 		}
 	}
