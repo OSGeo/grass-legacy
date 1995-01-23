375dc41882cf767f6ccdbc07d56cc44bc5730dd2
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.cross/cats.c
+++ src/raster/r.cross/cats.c
@@ -5,21 +5,43 @@ set_cat (result, cat, pcats)
     CELL *cat;
     struct Categories *pcats;
 {
-    int i;
-    char buf[1024];
-    char item[128];
+    int i,n;
+    static char *buf = NULL;
+    static int len = 0;
+    char *get_label();
+    char *lbl;
 
 
     if (result == 0) return;
-/*
- * the primary file may not have been the first on the command line
- * enforce the results in the order that the files were specified
- */
+
+    n = 0;	
+    for (i = 0; i < nfiles; i++)
+    {
+	lbl = get_label (cat[i], &labels[i]);
+	n += strlen(lbl) + 2;
+    }
+    if (len < n) buf = G_realloc (buf, len = n);
+
     *buf = 0;
     for (i = 0; i < nfiles; i++)
     {
-	sprintf (item, "%s(%ld) ", names[i], (long) cat[i]);
-	strcat (buf, item);
+	if (i) strcat (buf, "; ");
+	lbl = get_label (cat[i], &labels[i]);
+	strcat(buf, lbl);
     }
     G_set_cat (result, buf, pcats);
+}
+
+static char *
+get_label(cat, labels)
+    CELL cat;
+    struct Categories *labels;
+{
+    char *lbl;
+    static char temp[256];
+
+    lbl = G_get_cat (cat, labels);
+    if (*lbl == 0)
+	sprintf (lbl = temp, "category %ld", (long) cat);
+    return lbl;
 }
