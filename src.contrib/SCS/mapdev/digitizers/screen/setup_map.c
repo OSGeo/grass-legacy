976c6e8ab594beed82b06338ba9043ffa52f7b94
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/mapdev/digitizers/screen/setup_map.c
@@ -0,0 +1,16 @@
+/*  @(#)setup_map.c	1.1  5/4/87  */
+#include <stdio.h>
+
+
+setup_map()
+{
+		G_clear_screen() ;
+}
+
+/*
+check_map ()
+{
+	Write_info (3," This is the NULL digitizer.") ;
+
+}
+*/
