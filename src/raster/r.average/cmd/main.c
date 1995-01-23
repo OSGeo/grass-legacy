dee6e7cf087c1366d4ab919add1794bd9a45258f
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.average/cmd/main.c
+++ src/raster/r.average/cmd/main.c
@@ -8,10 +8,10 @@ main(argc, argv) char *argv[];
     char *me;
     char command[1024];
     char *mapset;
-    struct Option *basemap, *valuemap, *resultmap;
+    struct Option *basemap, *covermap, *outputmap;
     struct Flag *flag_c;
     struct Categories cats;
-    long cat0, cat1, cat2;
+    long catb, basecat, covercat;
     double x, area, sum1, sum2;
     int stat, usecats;
     char *tempfile1, *tempfile2;
@@ -26,23 +26,23 @@ main(argc, argv) char *argv[];
     basemap->gisprompt  = "old,cell,raster" ;
     basemap->description = "base raster map";
 
-    valuemap = G_define_option();
-    valuemap->key       = "values";
-    valuemap->type      = TYPE_STRING;
-    valuemap->required  = YES ;
-    valuemap->gisprompt  = "old,cell,raster" ;
-    valuemap->description = "raster map containing values to be averaged";
+    covermap = G_define_option();
+    covermap->key       = "cover";
+    covermap->type      = TYPE_STRING;
+    covermap->required  = YES ;
+    covermap->gisprompt  = "old,cell,raster" ;
+    covermap->description = "cover raster map";
 
-    resultmap = G_define_option();
-    resultmap->key       = "result";
-    resultmap->type      = TYPE_STRING;
-    resultmap->required  = YES ;
-    resultmap->gisprompt  = "new,cell,raster" ;
-    resultmap->description = "resultant raster map";
+    outputmap = G_define_option();
+    outputmap->key       = "output";
+    outputmap->type      = TYPE_STRING;
+    outputmap->required  = YES ;
+    outputmap->gisprompt  = "new,cell,raster" ;
+    outputmap->description = "resultant raster map";
 
     flag_c = G_define_flag();
     flag_c->key = 'c';
-    flag_c->description = "values produced from category labels for values map";
+    flag_c->description = "cover values extracted from the category labels of the cover map";
 
     if (G_parser(argc,argv))
 	exit(1);
@@ -52,23 +52,23 @@ main(argc, argv) char *argv[];
 	fprintf (stderr, "%s: %s - raster map not found\n", me, basemap->answer);
 	exit(1);
     }
-    if (!(mapset = G_find_cell(valuemap->answer,"")))
+    if (!(mapset = G_find_cell(covermap->answer,"")))
     {
-	fprintf (stderr, "%s: %s - raster map not found\n", me, valuemap->answer);
+	fprintf (stderr, "%s: %s - raster map not found\n", me, covermap->answer);
 	exit(1);
     }
-    if (G_legal_filename(resultmap->answer) < 0)
+    if (G_legal_filename(outputmap->answer) < 0)
     {
-	fprintf (stderr, "%s: %s - illegal name\n", me, resultmap->answer);
+	fprintf (stderr, "%s: %s - illegal name\n", me, outputmap->answer);
 	exit(1);
     }
 
     if(usecats = flag_c->answer)
     {
-	if (G_read_cats (valuemap->answer, mapset, &cats) < 0)
+	if (G_read_cats (covermap->answer, mapset, &cats) < 0)
 	{
 	    fprintf (stderr, "%s: ERROR reading category file for %s\n",
-		me, valuemap->answer);
+		me, covermap->answer);
 	    exit(1);
 	}
     }
@@ -76,9 +76,10 @@ main(argc, argv) char *argv[];
     tempfile1 = G_tempfile();
     tempfile2 = G_tempfile();
     sprintf (command, "%s -az input='%s,%s' fs=space > %s",
-	STATS, basemap->answer, valuemap->answer, tempfile1);
+	STATS, basemap->answer, covermap->answer, tempfile1);
     if (stat = system(command))
     {
+	unlink(tempfile1);
 	fprintf (stderr, "%s: ERROR running %s command\n", me, STATS);
 	exit(stat);
     }
@@ -87,35 +88,40 @@ main(argc, argv) char *argv[];
     fd2 = fopen (tempfile2, "w");
     if (fd1 == NULL || fd2 == NULL)
     {
+	unlink(tempfile1);
+	unlink(tempfile2);
 	fprintf (stderr, "%s: ERROR can't open tempfile\n", me);
 	exit(1);
     }
+    out(fd2, 0L, 0.0, 1.0);	/* force at least one reclass rule */
 
-    cat0 = 0;
+    catb = 0;
     sum1 = 0.0;
     sum2 = 0.0;
-    while (fscanf (fd1, "%ld %ld %lf", &cat1, &cat2, &area) == 3)
+    while (fscanf (fd1, "%ld %ld %lf", &basecat, &covercat, &area) == 3)
     {
-	if (cat0 != cat1)
+	if (catb != basecat)
 	{
-	    out(fd2, cat0, sum1, sum2);
+	    out(fd2, catb, sum1, sum2);
 	    sum1 = 0.0;
 	    sum2 = 0.0;
-	    cat0 = cat1;
+	    catb = basecat;
 	}
 	if (usecats)
-	    sscanf (G_get_cat((CELL)cat2, &cats), "%lf", &x);
+	    sscanf (G_get_cat((CELL)covercat, &cats), "%lf", &x);
 	else
-	    x = cat2;
+	    x = covercat;
 	sum1 += x * area;
 	sum2 += area;
     }
-    out(fd2, cat1, sum1, sum2);
+    out(fd2, basecat, sum1, sum2);
     fclose (fd1);
     fclose (fd2);
     sprintf (command, "%s input='%s' output='%s' < %s",
-	RECLASS, basemap->answer, resultmap->answer, tempfile2);
+	RECLASS, basemap->answer, outputmap->answer, tempfile2);
     stat = system(command);
+    unlink (tempfile1);
+    unlink (tempfile2);
     exit(stat);
 }
 out(fd, cat, sum1, sum2)
@@ -123,6 +129,15 @@ out(fd, cat, sum1, sum2)
     long cat;
     double sum1, sum2;
 {
-    if (sum2 != 0.0)
-	fprintf (fd, "%ld = %ld %lf\n", cat, cat, sum1/sum2);
+    char buf[80];
+
+    if (sum2 == 0) return;
+    if (cat == 0)
+	*buf = 0;
+    else
+    {
+	sprintf (buf, "%.10lf", sum1/sum2);
+	G_trim_decimal (buf);
+    }
+    fprintf (fd, "%ld = %ld %s\n", cat, cat, buf);
 }
