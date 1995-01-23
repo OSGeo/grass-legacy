ebea8b435bc42b3cacb6d797f816f03232d7bbac
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.alpha/display/d.rast.zoom/cur_frm_db.c
+++ src.alpha/display/d.rast.zoom/cur_frm_db.c
@@ -1,20 +1,24 @@
 #include "gis.h"
 
-struct Cell_head cur_from_db()
+cur_from_db(window, rast_name, rast_mapset)
+    struct Cell_head *window;
+    char *rast_name, *rast_mapset;
 {
     char name[30], window_dir[30];
-    struct Cell_head window;
     char *mapset;
+    char command[200];
+    char *G__get_window();
 
     strcpy(name,"lastwindow");
     strcpy(window_dir,"windows");
     mapset = G_mapset();
 
-    if (G__get_window (&window, window_dir, name, mapset) < 0){
-       G_get_default_window(&window);
+    if (G__get_window (window, window_dir, name, mapset) !=NULL)
+	{
+	  sprintf(command, "g.region rast=%s@%s", rast_name, rast_mapset);
+	  G_system(command);
+	  G_get_set_window(window);
 	}
     else
 	G_system("g.remove region=lastwindow");
-
-    return (window);
 }
