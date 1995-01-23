007afdc0d99d89ae57cae60c8f88a7968a44ea9d
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/mapdev/v.digit/curses.C
+++ src/mapdev/v.digit/curses.C
@@ -560,6 +560,50 @@ curses_yes_no_default (n, str, def)
     }
 }
 
+/*
+** Interpro computers barfed when going to text mode w/out an endwin()
+**  and also barfed when calling V_call() w/out first calling Old_tty()
+**  Thus the old method of setting up for both those cases [suspend() and respend()]
+**  was not sufficient, so we had to modify suspend(), and make a new vask_suspend()
+**  for resetting everything before calling  V_call().
+*/
+#ifdef INTERPRO
+suspend ()
+{
+    move (LINES-1, 0);
+    refresh ();
+	endwin ();
+}
+
+respend ()
+{
+    move (0, 0);
+    clear ();
+    touchwin (curscr);
+    refresh ();
+    touchwin (BASE_WIN);
+    touchwin (INFO_WIN);
+    wrefresh (BASE_WIN);
+    wrefresh (INFO_WIN);
+}
+
+vask_suspend ()
+{
+    move (LINES-1, 0);
+    refresh ();
+    Old_tty ();
+}
+
+vask_respend ()
+{
+    New_tty();
+	respend ();
+}
+#else
+
+vask_suspend () { return suspend (); }
+vask_respend () { return respend (); }
+
 suspend ()
 {
     move (LINES-1, 0);
@@ -579,6 +623,7 @@ respend ()
     wrefresh (BASE_WIN);
     wrefresh (INFO_WIN);
 }
+#endif
 
 get_type_cnt (type)
     char type;
