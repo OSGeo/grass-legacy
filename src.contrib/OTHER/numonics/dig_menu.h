159fc9c66be980fb395df5d5c1df2a9d8ce047cb
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/OTHER/numonics/dig_menu.h
@@ -0,0 +1,23 @@
+#define dig_menu_lines	16
+
+char *dig_menu[] = {
+" GRASS-DIGIT Version 3.0                                       Digitize menu",
+"-----------------------------------------------------------------------------",
+"  numonics digitizer                            | AMOUNT DIGITIZED",
+"       Key:          Action:                    |   # Lines:",
+"         <1>            digitize point          |   # Area edges:",
+"         <2>            quit digitizing         |   # Sites:",
+"         <3>            update monitor          | - - - - - - - - - - - - - -",
+"         <4>            toggle mode             |   Total points:",
+"-----------------------------------------------------------------------------",
+"                                                | CURRENT DIGITIZER PARAMS.",
+"                                                |",
+"                                                |",
+"                                                | MODE       TYPE",
+"                                                |    point      line",
+"                                                |    stream     area edge",
+"                                                |",
+"                                                |"
+} ;
+
+
