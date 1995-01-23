3dc5b06641f26489ad9759d7dcb0639405872391
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/display/devices/XDRIVER/lib/Num_colors.c
+++ src/display/devices/XDRIVER/lib/Num_colors.c
@@ -7,6 +7,7 @@ Number_of_colors(n)
 /* reduce the number of colors by the number of
  * vector colors, if current table type is float
  */
+    *n -= get_color_offset();
     if (get_table_type() == FLOAT)
 	 *n -= get_max_std_colors();
 }
