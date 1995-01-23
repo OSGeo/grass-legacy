cb75775dd92355dea2d03fdcccef247865ac6ce4
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/display/devices/XDRIVER/OKI/XDRIVER4/XDRIVER/Cont_abs.c
@@ -0,0 +1,40 @@
+#ifdef WWTRACE
+#include <stdio.h>
+#endif /* WWTRACE */
+#include <X11/Xos.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include "../lib/driver.h"
+
+extern Display *dpy;
+extern Window grwin;
+extern GC gc;
+extern Pixmap bkupmap;
+extern int backing_store;
+
+
+Cont_abs(x, y)
+int x, y;
+{
+#ifdef WWTRACE
+	fprintf(stderr,"XDrawLine(the_disp,gr_win,%d,%d,%d,%d);\n",cur_x,cur_y,x,y);
+#endif /* WWTRACE */
+    XDrawLine(dpy, grwin, gc, cur_x, cur_y, x, y);
+    if (backing_store != Always)
+        XDrawLine(dpy, bkupmap, gc, cur_x, cur_y, x, y);
+    cur_x = x;
+    cur_y = y;
+}
+
+
+Cont_rel(x, y)
+int x, y;
+{
+    XDrawLine(dpy, grwin, gc, cur_x, cur_y, cur_x + x, cur_y + y);
+    if (backing_store != Always)
+        XDrawLine(dpy, bkupmap, gc, cur_x, cur_y, cur_x + x, cur_y + y);
+    cur_x += x;
+    cur_y += y;
+}
+
+/*** end cont_abs.c ***/
