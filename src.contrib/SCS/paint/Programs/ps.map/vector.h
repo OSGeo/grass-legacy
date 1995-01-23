9586ea1f3f36033ced6bafa80306298a6ef13484
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Programs/ps.map/vector.h
@@ -0,0 +1,25 @@
+#define MAXVECTORS 20
+
+struct vector
+{
+    int cur;
+    int count;
+    double x, y;
+    int size;
+    char *font;
+    char *linestyle[MAXVECTORS];
+    char *setdash[MAXVECTORS];
+    int colors[MAXVECTORS][9];
+    int width[MAXVECTORS];
+    int hcolor[MAXVECTORS];
+    int hwidth[MAXVECTORS];
+    char masked[MAXVECTORS];
+    char *name[MAXVECTORS];
+    char *mapset[MAXVECTORS];
+} ;
+
+#ifdef MAIN
+    struct vector vector;
+#else
+    extern struct vector vector;
+#endif
