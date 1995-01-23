95331f18044768b3baf9792406ac99bc066f5b53
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src.contrib/SCS/vector/v.report/cmd/do_v_stats.c
+++ src.contrib/SCS/vector/v.report/cmd/do_v_stats.c
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include "global.h"
 #include "Vect.h"
+#include "gis.h"
 
 #define		DIG_DIR		"dig"
 
@@ -34,11 +35,12 @@ char *fd;
 
 {
 	int nArea, nLine, map_type, v_r;
-	int cnt=0, cat_no, line, linea, island;
-	int first, tot_cats, no_cats=0, n_points, cmp();
+	int cnt=0, line, linea, island;
+	CELL cat_no;
+	int first, tot_cats, n_points, cmp();
 	register int area_num, line_num, i, ii, jj, kk;
 	char *calloc(), *label_name, null[1];
-	char aline[80], blank_name[5], unknown_name[20], err_name[20];
+	char aline[80];
 	char map_name[100], *mapset;
 	long len, nalloc=1024;
 	double xcent, ycent, *X, *Y;
@@ -50,8 +52,11 @@ char *fd;
 	P_NODE *Nodes;
 	P_ATT *atts;
 	FILE *dumpfile;
+	extern double G_area_of_polygon();
 	extern double perimeter();
 
+        G_begin_distance_calculations();
+        G_begin_polygon_area_calculations();
 	null[0] = '\0';
         sprintf(map_name,"%s\0",layers[lay_no].name);
 	mapset = layers[lay_no].mapset;
@@ -62,40 +67,11 @@ char *fd;
 
 	nArea = nLine = 0;
 	tot_area = 0.0;
-	strcpy(blank_name,"  ");
-	strcpy(unknown_name,"label unknown ");
-	strcpy(err_name, "cat file error");
 verbose=1;
 	if(verbose)
           fprintf(stderr,"\nLoading vector information.\n\n");
 
 /*fprintf(stderr,"cat.count = %d,  cat.num= %d\n",cats.count,cats.num);*/
-        if (cats.num == 0) no_cats = 1;
-
-	/* Check cats for errors */
-	if (!no_cats)
-	{  /* count should always be one more than the num of categories */
-		if(cats.count != cats.num+1)
-		{
-			fprintf(stderr,"\007");
-			fprintf(stderr,"\nERROR: Error in category support file\n");
-			fprintf(stderr,"\t Expected %d category codes read in only %d\n",cats.num,cats.count-1);
-			sleep(2);
-			return(-1);
-		}
-/*              for (cat_no=0;cat_no<=cats.num;cat_no++)
-		{ ** a null value means less categories than count **
-			if(strcmp(cats.list[cat_no].label,null) == 0)
-			{
-				fprintf(stderr,"\007");
-				fprintf(stderr,"\nERROR: Error in category support file\n");
-				fprintf(stderr,"\t Missing category code(s)\n");
-				sleep(2);
-				return(-1);
-			}
-		}
-*/
-	}
 
 	        /* Do initial read of map_name DIGIT file */
 	if ((v_r = Vect_open_old(&Map,map_name,mapset)) < 0)
@@ -144,24 +120,15 @@ verbose=1;
 			/* get the category number for area "area_num" */
 			cat_no = Map.Att[Map.Area[area_num].att].cat;
 /*fprintf(stderr,"\ncat#= %d\n",cat_no);*/
-			         /* check if category is in valid range */
-			if (cat_no > cats.num || cat_no < 0)
-				label_name = unknown_name;
-			         /* if cat file exists */
-			else if (no_cats) label_name = blank_name;
-			         /* if cat number NOT in cats file */
-			else if(cats.list[cat_no].label == NULL)
-				label_name = unknown_name;
-				/* if cat number in cats file, but is empty */
-			else if(strlen(cats.list[cat_no].label) == 0)
-				label_name = blank_name;
-				/* get the category label for "cat_no" */
-			else label_name = cats.list[cat_no].label;
+			label_name = G_get_cat(cat_no, &cats);
 
 			/* Calculate polygon area */
 			V2_get_area(&Map,area_num,&Areas);
-			dig_find_area2(&Map,Areas,&f_area);
-			tot_area = f_area;
+			Vect_get_area_points(&Map, area_num, Points);
+		        f_area = G_area_of_polygon(Points->x, 
+					       Points->y, Points->n_points);
+						      
+			/* dig_find_area2(&Map,Areas,&f_area);*/
 
 			  /* if this area contains islands remove the
 			    island areas */
@@ -170,11 +137,12 @@ verbose=1;
 			   for (ii=0; ii < Map.Area[area_num].n_isles; ii++)
 			     {
 			     island = Map.Area[area_num].isles[ii];
-			     isle_area(&Map,island,&f_area);
-			     if (f_area > 0) tot_area = tot_area - f_area;
-			     else tot_area = tot_area + f_area;
+			     Vect_get_isle_points(&Map, Map.Area[area_num].isles[ii], Points);
+			     f_area = f_area - G_area_of_polygon(Points->x,
+					      Points->y, Points->n_points);
 			     }
 			   }
+			   tot_area = f_area;
 
 			if (first)
 			{
@@ -236,7 +204,7 @@ verbose=1;
 			return(-1);
 		}
 
-                fprintf(dumpfile,"area\n");
+                fprintf(dumpfile,"area %d\n",nalloc);
 
 		for (i=0; i<cnt; i++)
 		fprintf (dumpfile, "%5d:%4d:%14.2lf%\n",
@@ -268,19 +236,7 @@ verbose=1;
 			/* get the category number for line_num */
 			cat_no = Map.Att[Lines->att].cat;
 /*fprintf(stderr,"\ncat#= %d\n",cat_no);*/
-			/* check if category is in valid range */
-			if (cat_no > cats.num || cat_no < 0)
-				label_name = unknown_name;
-			/* if cat file exists */
-			else if (no_cats) label_name = blank_name;
-			/* if cat number NOT in cats file */
-			else if(strcmp(cats.list[cat_no].label,null) == 0)
-				label_name = unknown_name;
-				/* if cat number in cats file, but is empty */
-			else if(strlen(cats.list[cat_no].label) == 0)
-				label_name = blank_name;
-				/* get the category label for "cat_no" */
-			else label_name = cats.list[cat_no].label;
+			label_name = G_get_cat(cat_no, cats);
 /*fprintf(stderr,"\ncat#= %d, label= <%s>\n",cat_no,label_name);*/
 			if (Lines->type == 1)
 			{
@@ -380,11 +336,13 @@ verbose=1;
 	/************ END REPORT FOR LINES OR SITES *********************/
 
 	Vect_close (&Map);
+/*
 #ifdef SYSV
 	system("tput clear");
 #else
 	system("clear");
 #endif
+*/
 	return(0);
 }
 
@@ -426,56 +384,9 @@ struct Cat_List *a, *b;
 	return 0;
 }
 
-isle_area (map, isle, totalarea)
-    struct Map_info *map;
-    int  isle;
-    double *totalarea;
+static
+ABS (x)
 {
-    int cur_line;
-    int ab_line;
-    int i;
-    double *x, *y;
-    static struct line_pnts points;
-    static int first_time;	/* 0 on startup */
-    double tot_area, sum_area;
-
-
-    *totalarea = 0.;
-
-    if (! map->Isle[isle].n_lines)
-	return(-1);
-
-    tot_area = 0.0;
-
-    if (first_time == 0)
-    {
-	points.alloc_points = 0;
-	first_time = -1;
-    }
-
-    tot_area = 0.0;
-    for (cur_line = 0; cur_line < map->Isle[isle].n_lines ; cur_line++)
-    {
-	ab_line = ABS(map->Isle[isle].lines[cur_line]);
-
-	V2_read_line (map, &points, map->Line[ab_line].offset);
-	if (points.n_points < 2)
-	    continue;
-	
-	x= points.x;
-	y= points.y;
-
-	sum_area = 0.0;
-	for (i=1; i < points.n_points; i++) 
-	{
-	    sum_area += (x[i]-x[i-1]) * (y[i] + y[i-1]);
-	}
-	if (map->Isle[isle].lines[cur_line] > 0)
-	    tot_area += sum_area;
-	else
-	    tot_area -= sum_area;
-    }
-    *totalarea =  0.5 * tot_area;
-    
-    return(0);
+	if (x < 0) x = -x;
+	return x;
 }
