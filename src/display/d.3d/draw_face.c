ef6ffc9f3169b380b85aedba0595c1b4468f1270
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/display/d.3d/draw_face.c
+++ src/display/d.3d/draw_face.c
@@ -7,27 +7,27 @@ draw_north_face(min, max)
 	int scr_x, scr_y ;
 	point_calc(window.west+window.ew_res, window.north-window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_move_abs(scr_x, scr_y) ;
+	D_move_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.north-window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.north-window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.north-window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.north-window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	if (min < 0 && max > 0)
 	{
 		point_calc(window.west+window.ew_res, window.north-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_move_abs(scr_x, scr_y) ;
+		D_move_abs(scr_x, scr_y) ;
 		point_calc(window.east-window.ew_res, window.north-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_cont_abs(scr_x, scr_y) ;
+		D_cont_abs(scr_x, scr_y) ;
 	}
 }
 
@@ -37,27 +37,27 @@ draw_south_face(min, max)
 	int scr_x, scr_y ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_move_abs(scr_x, scr_y) ;
+	D_move_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.south+window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	if (min < 0 && max > 0)
 	{
 		point_calc(window.west+window.ew_res, window.south-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_move_abs(scr_x, scr_y) ;
+		D_move_abs(scr_x, scr_y) ;
 		point_calc(window.east-window.ew_res, window.south-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_cont_abs(scr_x, scr_y) ;
+		D_cont_abs(scr_x, scr_y) ;
 	}
 }
 
@@ -67,27 +67,27 @@ draw_east_face(min, max)
 	int scr_x, scr_y ;
 	point_calc(window.east-window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_move_abs(scr_x, scr_y) ;
+	D_move_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.north-window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.north-window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.south+window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.east-window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	if (min < 0 && max > 0)
 	{
 		point_calc(window.east-window.ew_res, window.south+window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_move_abs(scr_x, scr_y) ;
+		D_move_abs(scr_x, scr_y) ;
 		point_calc(window.east-window.ew_res, window.north-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_cont_abs(scr_x, scr_y) ;
+		D_cont_abs(scr_x, scr_y) ;
 	}
 }
 
@@ -97,26 +97,26 @@ draw_west_face(min, max)
 	int scr_x, scr_y ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_move_abs(scr_x, scr_y) ;
+	D_move_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.north-window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.north-window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		max * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	point_calc(window.west+window.ew_res, window.south+window.ns_res,
 		min * exag, &scr_x, &scr_y) ;
-	R_cont_abs(scr_x, scr_y) ;
+	D_cont_abs(scr_x, scr_y) ;
 	if (min < 0 && max > 0)
 	{
 		point_calc(window.west-window.ew_res, window.south+window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_move_abs(scr_x, scr_y) ;
+		D_move_abs(scr_x, scr_y) ;
 		point_calc(window.west-window.ew_res, window.north-window.ns_res,
 			0., &scr_x, &scr_y) ;
-		R_cont_abs(scr_x, scr_y) ;
+		D_cont_abs(scr_x, scr_y) ;
 	}
 }
