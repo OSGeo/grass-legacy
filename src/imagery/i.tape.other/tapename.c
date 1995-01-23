eda73cdba96bc68668505a875de11c65b5b80a76
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/imagery/i.tape.other/tapename.c
+++ src/imagery/i.tape.other/tapename.c
@@ -1,16 +1,17 @@
+#include "tape.h"
 get_tapename (name)
     char *name;
 {
     for(;;)
     {
-	if (!I_ask ("enter tape device name: ", name, 1))
+	if (!I_ask ("Enter tape device name: ", name, 1))
 	    exit(0);
 	if (access (name, 4) == 0)
 		return;
-	printf ("%s - ",name);
+	fprintf (stderr, "%s - ",name);
 	if (access (name, 0) != 0)
-	    printf ("no such device\n");
+	    fprintf (stderr, "no such device\n");
 	else
-	    printf ("read permission denied\n");
+	    fprintf (stderr, "read permission denied\n");
     }
 }
