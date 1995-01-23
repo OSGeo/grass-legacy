02bcb94bf03789575b0cbcda8a0c019a661abf57
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.report/cmd/main.c
+++ src/raster/r.report/cmd/main.c
@@ -6,7 +6,7 @@ main(argc, argv) char *argv[];
     G_gisinit(argv[0]);
     G_get_window (&window);
 
-    parse_command_line (argc, argv);
+    parse_command_line (argc, argv, &e_format);
 
     get_stats();
 
