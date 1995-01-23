295c74c9de426f9508adacf55ceb6e68906139f2
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/imagery/i.tape.other/mount_tape.c
+++ src/imagery/i.tape.other/mount_tape.c
@@ -4,12 +4,12 @@ NAME:		mount_tape ()
 
 FUNCTION:	open tape drive for reading
 ****************************************************/
+#include "tape.h"
 
 mount_tape (tapename)
-    char *tapename;
+  char *tapename;
 {
-    int tapefd;
-    while((tapefd = open(tapename, 0)) < 0)
-	I_ask("\nMount tape and put on-line. Then hit RETURN-->",0,1) ;
-    return tapefd;
+  while((tapefd = open(tapename, 0)) < 0)
+    I_ask("\nMount tape and put on-line. Then hit RETURN-->",0,1) ;
+  return;
 }
