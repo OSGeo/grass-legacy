f38cd549e04c50402eb56185161adffa11bc493e
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/CERL/raster/r.rvi/globals.h
@@ -0,0 +1,11 @@
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+GLOBAL int *fd_input;    /* the input files */
+GLOBAL int fd_output;    /* the ouput file */
+GLOBAL int fd_modelfile; /* the model input file */
+GLOBAL int nbands;
+GLOBAL char *modelfile;
+GLOBAL char *outputfile;
+GLOBAL char **inputfile;
