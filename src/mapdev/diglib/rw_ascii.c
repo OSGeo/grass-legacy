36af9bc031a8ff2ca4984768babe80144ca77b4b
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/mapdev/diglib/rw_ascii.c
+++ src/mapdev/diglib/rw_ascii.c
@@ -1,6 +1,7 @@
 /*  @(#)rw_ascii.c	2.1  6/26/87  */
 #include <stdio.h>
 #include "dig_head.h"
+#include "gis.h"
 
 /*
 *   2.0 digit files have "UTM ZONE",  3.0 digit files have "ZONE"
@@ -41,7 +42,6 @@ dig_read_head_ascii(dascii, head)
 {
 	char buff[1024] ;
 	char *ptr ;
-	char *rindex() ;
 
 	for(;;)
 	{
@@ -54,7 +54,7 @@ dig_read_head_ascii(dascii, head)
 		if (strncmp(buff, "VERTI:", 6) == 0 )		/* Last line of header */
 			return(0) ;
 
-		if ( ! (ptr = rindex(buff,':')) )
+		if ( ! (ptr = G_rindex(buff,':')) )
 			return(-1);
 		ptr++ ;                 /* Search for the start of text */
 		while (*ptr == ' ')
@@ -67,50 +67,50 @@ dig_read_head_ascii(dascii, head)
 				ptr[sizeof(head->organization)-1] = 0;
 			strcpy( head->organization, ptr) ;
 		}
-		else if (strncmp(buff, "DIGIT DATE:  ", 12) == 0)
+		else if (strncmp(buff, "DIGIT DATE:", 11) == 0)
 		{
 			if (strlen(ptr) >= sizeof(head->date))
 				ptr[sizeof(head->date)-1] = 0;
 			strcpy( head->date, ptr) ;
 		}
-		else if (strncmp(buff, "DIGIT NAME:  ", 12) == 0)
+		else if (strncmp(buff, "DIGIT NAME:", 11) == 0)
 		{
 			if (strlen(ptr) >= sizeof(head->your_name))
 				ptr[sizeof(head->your_name)-1] = 0;
 			strcpy( head->your_name, ptr) ;
 		}
-		else if (strncmp(buff, "MAP NAME:    ", 12) == 0)
+		else if (strncmp(buff, "MAP NAME:", 9) == 0)
 		{
 			if (strlen(ptr) >= sizeof(head->map_name))
 				ptr[sizeof(head->map_name)-1] = 0;
 			strcpy( head->map_name, ptr) ;
 		}
-		else if (strncmp(buff, "MAP DATE:    ", 12) == 0)
+		else if (strncmp(buff, "MAP DATE:", 9) == 0)
 		{
 			if (strlen(ptr) >= sizeof(head->source_date))
 				ptr[sizeof(head->source_date)-1] = 0;
 			strcpy( head->source_date, ptr) ;
 		}
-		else if (strncmp(buff, "MAP SCALE:   ", 12) == 0)
+		else if (strncmp(buff, "MAP SCALE:", 10) == 0)
 			sscanf(ptr, "%d", &(head->orig_scale)) ;
-		else if (strncmp(buff, "OTHER INFO:  ", 12) == 0)
+		else if (strncmp(buff, "OTHER INFO:", 11) == 0)
 		{
 			if (strlen(ptr) >= sizeof(head->line_3))
 				ptr[sizeof(head->line_3)-1] = 0;
 			strcpy( head->line_3, ptr) ;
 		}
-		else if (strncmp(buff, "ZONE:        ", 12) == 0
-		      || strncmp(buff, "UTM ZONE:    ", 12) == 0)
+		else if (strncmp(buff, "ZONE:", 5) == 0
+		      || strncmp(buff, "UTM ZONE:", 9) == 0)
 			sscanf(ptr, "%d", &(head->plani_zone)) ;
-		else if (strncmp(buff, "WEST EDGE:   ", 12) == 0)
+		else if (strncmp(buff, "WEST EDGE:", 10) == 0)
 			sscanf(ptr, "%lf", &(head->W)) ;
-		else if (strncmp(buff, "EAST EDGE:   ", 12) == 0)
+		else if (strncmp(buff, "EAST EDGE:", 10) == 0)
 			sscanf(ptr, "%lf", &(head->E)) ;
-		else if (strncmp(buff, "SOUTH EDGE:  ", 12) == 0)
+		else if (strncmp(buff, "SOUTH EDGE:", 11) == 0)
 			sscanf(ptr, "%lf", &(head->S)) ;
-		else if (strncmp(buff, "NORTH EDGE:  ", 12) == 0)
+		else if (strncmp(buff, "NORTH EDGE:", 11) == 0)
 			sscanf(ptr, "%lf", &(head->N)) ;
-		else if (strncmp(buff, "MAP THRESH:  ", 12) == 0)
+		else if (strncmp(buff, "MAP THRESH:", 11) == 0)
 			sscanf(ptr, "%lf", &(head->map_thresh)) ;
 		else
 			return(-1) ;
