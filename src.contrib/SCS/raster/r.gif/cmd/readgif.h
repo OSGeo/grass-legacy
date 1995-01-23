c871ae8f715015705696c646d87a89f09a612c15
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/raster/r.gif/cmd/readgif.h
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------------------*/
+/* Copyright (c) 1988-1989						*/
+/* by CompuServe Inc., Tucson, AZ.  All Rights Reserved			*/
+/* READGIF.H can be copied and distributed freely for any		*/
+/* non-commercial purposes. READGIF.H can only be incorporated		*/
+/* into commercial software with the permission of CompuServe Inc.	*/
+/*----------------------------------------------------------------------*/
+
+/* Header file READGIF.H */
+
+/* Procedures to read page data out of GIF file */
+
+extern short int	next_GIF_byte();
+
+extern void		close_stream();
+
+extern short int	init_GIF_input();
