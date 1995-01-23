2169d0ff1a3a49384acaf5701a9398b7f5a6582c
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/PURDUE/v.mkgrid/grid_structs.h
@@ -0,0 +1,27 @@
+
+
+
+struct grid_description
+{
+    int    num_rows ;
+    int    num_cols ;
+    int    num_vect_rows ;
+    int    num_vect_cols ;
+    double length ;           /*  distance to shift to the east  */
+    double width ;           /*  distance to shift to the north  */
+    double origin_x ;        /*  lower left point of grid  */
+    double origin_y ;
+    double angle ;
+};
+
+
+
+struct box
+{
+    int    top ;
+    int    bottom ;
+    int    left ;
+    int    right ;
+};
+
+
