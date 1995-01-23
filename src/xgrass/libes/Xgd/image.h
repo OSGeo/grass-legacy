188e04ee255a8ddf7e7cb72bd7e54a2483147df3
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/xgrass/libes/Xgd/image.h
@@ -0,0 +1,15 @@
+unsigned char *entirepic;
+unsigned char *resizepic;
+unsigned char *zoompic;
+/*
+XImage *entireImage;
+XImage *resizeImage;
+XImage *zoomImage;
+*/
+
+
+/*
+int entiredithered;
+*/
+int entirewidth, entireheight;
+int newwidth, newheight;
