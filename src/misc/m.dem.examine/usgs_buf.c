b09adad9501b9929f6894c3388d5939eae9740c7
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/misc/m.dem.examine/usgs_buf.c
+++ src/misc/m.dem.examine/usgs_buf.c
@@ -4,6 +4,7 @@ get_buf()
 {
 	int n_read;
 
+{int i; for(i = 0; i < blocksize; i++)buffer[i] = 0;} /* shapiro - this is a bit dumb */
 	if((n_read = read(tapefile,buffer,blocksize)) < 0){
 		G_fatal_error("error reading tape");
 		exit(1);
@@ -34,3 +35,22 @@ opentape(dev)
 		 exit(1);
 	  }
    }
+
+next_record()
+{
+
+  int the_rest;
+
+  the_rest = RECORD_SIZE - (record_pos%RECORD_SIZE);
+  if (buffer + the_rest >= buf_end) {
+    the_rest -= buf_end - buffer;
+    buffer = buf_start;
+    if (get_buf() <= 0) {
+      fprintf(stderr, "next_record: can't get more buffer\n");
+      return NULL;
+    }
+  }
+
+  buffer += the_rest;
+  record_pos = 0;
+}
