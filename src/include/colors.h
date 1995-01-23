e92997ad73b9c3f2eead6ad0a46ed11a0105111d
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src/include/colors.h
@@ -0,0 +1,15 @@
+#define RED		1
+#define ORANGE		2
+#define YELLOW		3
+#define GREEN		4
+#define BLUE		5
+#define INDIGO		6
+#define VIOLET		7
+#define WHITE		8
+#define BLACK		9
+#define GRAY		10
+#define BROWN		11
+#define MAGENTA		12
+#define AQUA		13
+
+#define D_COLOR_LIST "red,orange,yellow,green,blue,indigo,white,black,brown,magenta,aqua,gray,grey"
