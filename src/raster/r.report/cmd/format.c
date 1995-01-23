df9902dfb4ed50407ccae1b4f3687afb74f6a8fe
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

--- src/raster/r.report/cmd/format.c
+++ src/raster/r.report/cmd/format.c
@@ -8,38 +8,66 @@
  * format_double() does the formating with the
  *     parms determined by format_parms()
  ***************************************************/
-format_parms (v,n,eformat,dp)
+format_parms (v,n,dp, eformat, e_option)
     double v;
-    int *eformat, *dp;
+    int *n, *dp, *eformat, e_option;
 {
     char buf[50];
+    int orig_length, scient_dp;
 
+    orig_length = *n;
+    scient_dp = *dp;
     for(;;)
     {
-	format_double (v, buf, n, *eformat, *dp);
-	if (strlen(buf) <= n) break;
+	if(!*eformat)
+	    format_double (v, buf, *n, *dp);
+        else 
+	    scient_format(v, buf, *n, *dp);
+
+	if (strlen(buf) <= *n) break;
+
 	if(*dp)
+	{
 	    *dp -= 1;
-	else if (*eformat)
-	    break;	/* OOPS */
+	}
 	else
 	{
-	    *eformat = 1;
-	    *dp = n;
+	    if((e_option)&&(!*eformat))
+	    {
+	       *eformat=1;
+	       *dp = scient_dp;
+            }
+            else
+	       *n = strlen(buf);
 	}
     }
 }
 
-format_double (v, buf, n, eformat, dp)
+scient_format(v, buf, n, dp)
+    double v;
+    char *buf;
+{
+    char temp[50];
+    int i;
+
+    sprintf(temp, "%#.*lg", dp, v);
+    for (i=0; i<=n && temp[i] == ' ';i++) {}
+    strcpy (buf, temp+i);
+}
+
+format_double (v, buf, n, dp)
     double v;
     char *buf;
 {
     char fmt[15];
+    char temp[100];
+    int i,ncommas;
 
-    if (eformat)
-	sprintf (fmt, "%%%d.%de", n, dp);
-    else
-	sprintf (fmt, "%%%d.%dlf", n, dp);
-    sprintf (buf, fmt, v);
-    G_insert_commas (buf);
+    sprintf (fmt, "%%%d.%dlf", n, dp);
+    sprintf (temp, fmt, v);
+    strcpy (buf, temp);
+    G_insert_commas (temp);
+    ncommas = strlen (temp) - strlen(buf);
+    for (i=0; i<ncommas && temp[i] == ' ';i++) {}
+    strcpy (buf, temp+i);
 }
