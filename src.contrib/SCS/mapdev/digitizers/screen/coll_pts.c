50b73790d0e44245d5fb797aafac83d6727b0dc2
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/mapdev/digitizers/screen/coll_pts.c
@@ -0,0 +1,16 @@
+/*  @(#)coll_pts.c    1.1  5/4/87  */
+
+#include <stdio.h>
+/*  
+*   collect_points() - collect a set of points.
+*    coll_a_pnt() -  collect a point from digitizer.
+*/
+
+
+collect_points()
+{
+
+	fprintf(stderr," There is no digitizer for this\n");
+
+}    /*  coll_pts()  */
+
