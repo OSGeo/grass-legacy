31d516b56294376d7d0c0464c01c9872a22beef0
bot <grass-svn2git@osgeo.org>
1995-01-24 00:00:00 +0100

    added

---
+++ src.contrib/SCS/paint/Drivers/color.ps/color.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#define LINE  10
+color(n)
+{
+	static int cnt=0;
+    float red,grn,blu;
+    int r,g,b;
+	char *hexits = "0123456789abcdef";
+
+	red = grn = blu = -1.0;
+    Pcolorvalue(n,&red,&grn,&blu);
+    r = red * 255;
+    g = grn * 255;
+    b = blu * 255;
+		{
+    	Poutc(hexits[r >> 4]);
+    	Poutc(hexits[r & 15]);
+    	Poutc(hexits[g >> 4]);
+    	Poutc(hexits[g & 15]);
+    	Poutc(hexits[b >> 4]);
+    	Poutc(hexits[b & 15]);
+		if (++cnt == LINE){
+			cnt = 0;
+		Poutc('\n');
+			}
+		}
+
+}
+
