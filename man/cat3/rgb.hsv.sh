5a901ece4e058b4ebb3ca086e80e386a7b5b72b3
bot <grass-svn2git@osgeo.org>
1997-10-13 21:38:07 +0200

    added

--- man/cat3/rgb.hsv.sh
+++ man/cat3/rgb.hsv.sh
@@ -6,28 +6,28 @@ rgb.hsv.sh <scripts> GRASS Reference Manual  <scripts> rgb.hsv.sh
 
 
 NAME
-     rgb.hsv.sh - Converts RGB (red, green, and blue) cell values
-     to RGB (red, green, and blue) values.
+     rgb.hsv.sh	- Converts RGB (red, green, and	blue) cell values
+     to	RGB (red, green, and blue) values.
      (GRASS Shell Script)
 
 SYNOPSIS
-     rgb.hsv.sh file1 file2 file3
+     rgb.hsv.sh	file1 file2 file3
 
 DESCRIPTION
-     rgb.hsv.sh is a Bourne shell (sh(1)) program which converts
-     RGB values to HSV using r.mapcalc.  The Foley and Van Dam
-     algorithm is the basis for the program.  Input must be three
-     raster files - each file supplying the RGB values.  Three
-     new raster files are created representing HSV.
+     rgb.hsv.sh	is a Bourne shell (sh(1)) program which	converts
+     RGB values	to HSV using r.mapcalc.	 The Foley and Van Dam
+     algorithm is the basis for	the program.  Input must be three
+     raster files - each file supplying	the RGB	values.	 Three
+     new raster	files are created representing HSV.
 
 NOTES
-     Do not use the same names for input and output.
+     Do	not use	the same names for input and output.
 
 FILES
-     This program is simply a shell script stored under the
+     This program is simply a shell script stored under	the
      $GISBASE/scripts directory.  The user is encouraged to
      examine the shell script programs stored here and to produce
-     other such programs.
+     other such	programs.
 
 SEE ALSO
      hsv.rgb.sh
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.1                U.S. Army CERL                         1
+GRASS 4.2		Baylor University			1
 
 
 
