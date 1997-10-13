b8b0a08ba32eb68b4a9a8940783e4a0d44a75714
bot <grass-svn2git@osgeo.org>
1997-10-13 21:38:06 +0200

    added

--- man/cat3/grass.logo.sh
+++ man/cat3/grass.logo.sh
@@ -6,31 +6,31 @@ grass.logo.sh <scriptsGRASS Reference Manu<scripts> grass.logo.sh
 
 
 NAME
-     grass.logo.sh - Displays a GRASS/Army Corps of Engineers
-     logo in the active display frame on the graphics monitor.
+     grass.logo.sh - Displays a	GRASS/Army Corps of Engineers
+     logo in the active	display	frame on the graphics monitor.
      (GRASS Shell Script)
 
 SYNOPSIS
      grass.logo.sh
 
 DESCRIPTION
-     grass.logo.sh is primarily a demonstration of the GRASS
-     d.graph program in a UNIX Bourne shell macro that generates
+     grass.logo.sh is primarily	a demonstration	of the GRASS
+     d.graph program in	a UNIX Bourne shell macro that generates
      the U.S. Army Corps of Engineers logo.  Users are encouraged
-     to generate their own unique logos by writing similar macro
+     to	generate their own unique logos	by writing similar macro
      shell scripts.  Examine the contents of the input file
      called grass.logo.sh located in the GRASS shell script
      command directory ($GISBASE/scripts) to see how the GRASS
-     logo was generated using d.graph graphics commands.  The
+     logo was generated	using d.graph graphics commands.  The
      coordinates for this logo were taken from a drawing done on
      graph paper.
 
-     To view the graphics described by this file use d.graph or
-     d.mapgraph, making sure that a copy of this file is either
-     in your current directory or is given by its full path name.
+     To	view the graphics described by this file use d.graph or
+     d.mapgraph, making	sure that a copy of this file is either
+     in	your current directory or is given by its full path name.
 
 NOTES
-     grass.logo.sh, like d.rast, will overwrite (not overlay)
+     grass.logo.sh, like d.rast, will overwrite	(not overlay)
      whatever display appears in the active graphics frame.
 
      This program requires no command line arguments.
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.1                U.S. Army CERL                         1
+GRASS 4.2		Baylor University			1
 
 
 
