16c0f070e8342f7a5dbfbd5cfd061c8573c558d2
bot <grass-svn2git@osgeo.org>
1997-10-13 21:36:58 +0200

    added

--- man/cat1/i.tape.mss.h
+++ man/cat1/i.tape.mss.h
@@ -6,7 +6,7 @@ i.tape.mss.h <main>  GRASS Reference Manual   <main> i.tape.mss.h
 
 
 NAME
-     i.tape.mss.h - An imagery function that extracts header
+     i.tape.mss.h - An imagery function	that extracts header
      information from LANDSAT Multispectral Scanner (MSS) imagery
      data stored on half-inch tape.
      (GRASS Image Processing Program)
@@ -17,34 +17,34 @@ SYNOPSIS
      i.tape.mss.h tape_drive_name
 
 DESCRIPTION
-     i.tape.mss.h reads the header information on a Multispectral
+     i.tape.mss.h reads	the header information on a Multispectral
      Scanner (MSS) tape.  This program reads the specified input
-     file (the computer-compatible tape tape_drive_name), and by
-     default displays the output to the user's terminal.  The
-     user may redirect output to a file by using the UNIX
+     file (the computer-compatible tape	tape_drive_name), and by
+     default displays the output to the	user's terminal.  The
+     user may redirect output to a file	by using the UNIX
      redirection mechanism.  For example:
 
-          i.tape.mss.h /dev/rmt0 > h.out
+	  i.tape.mss.h /dev/rmt0 > h.out
 
-     The name of the tape drive depends on the computer being
+     The name of the tape drive	depends	on the computer	being
      used.
 
      This program can be run either non-interactively or
      interactively.  The user can run the program by specifying
      program arguments on the command line.  Alternately, the
      user can simply type i.tape.mss.h on the command line,
-     without program arguments.  In this event, the program will
-     prompt the user to enter a tape device name using the
+     without program arguments.	 In this event,	the program will
+     prompt the	user to	enter a	tape device name using the
      standard user interface described in the manual entry for
      parser.
 
 SEE ALSO
      GRASS Tutorial: Image Processing
 
-     i.tape.mss and parser
+     i.tape.mss	and parser
 
 AUTHOR
-     Michael Shapiro, U.S. Army Construction Engineering Research
+     Michael Shapiro, U.S. Army	Construction Engineering Research
      Laboratory
 
 
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.1                U.S. Army CERL                         1
+GRASS 4.2		Baylor University			1
 
 
 
