16a6150fe35d1e684870dee2d3f5b964ac0e543f
bot <grass-svn2git@osgeo.org>
1997-10-13 21:38:07 +0200

    added

--- man/cat3/show.color.sh
+++ man/cat3/show.color.sh
@@ -6,34 +6,34 @@ show.color.sh <scriptsGRASS Reference Manu<scripts> show.color.sh
 
 
 NAME
-     show.color.sh - Displays and names available primary colors
-     used by GRASS programs, in frames on the graphics monitor.
+     show.color.sh - Displays and names	available primary colors
+     used by GRASS programs, in	frames on the graphics monitor.
      (GRASS Shell Script)
 
 SYNOPSIS
      show.color.sh
 
 DESCRIPTION
-     show.color.sh is a UNIX Bourne shell macro that displays and
+     show.color.sh is a	UNIX Bourne shell macro	that displays and
      names available primary colors used by GRASS programs in
-     frames on the graphics monitor.  Available colors are:  red,
+     frames on the graphics monitor.  Available	colors are:  red,
      orange, yellow, green, blue, indigo, violet, white, black,
      gray, brown, magenta, and aqua.
 
-     No program arguments are required to run this program.
+     No	program	arguments are required to run this program.
 
 NOTES
-     The full monitor screen is made the active frame after this
+     The full monitor screen is	made the active	frame after this
      program ends.
 
-     This macro is located in $GISBASE/scripts/show.color.sh.
+     This macro	is located in $GISBASE/scripts/show.color.sh.
 
 SEE ALSO
      d.colormode, d.colors, d.colortable, d.display, d.frame,
      grass.logo.sh, show.fonts.sh
 
 AUTHOR
-     David Gerdes, U.S. Army Construction Engineering Research
+     David Gerdes, U.S.	Army Construction Engineering Research
      Laboratory
 
 
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.1                U.S. Army CERL                         1
+GRASS 4.2		Baylor University			1
 
 
 
