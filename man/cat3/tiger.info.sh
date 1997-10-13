ee9cd8101f26cf7f8ad9b042ca7ecd674c349eda
bot <grass-svn2git@osgeo.org>
1997-10-13 21:38:09 +0200

    added

--- man/cat3/tiger.info.sh
+++ man/cat3/tiger.info.sh
@@ -7,7 +7,7 @@ tiger.info.sh <scriptsGRASS Reference Manu<scripts> tiger.info.sh
 
 NAME
      tiger.info.sh - Provides tract number(s) and classification
-     codes found within a given U.S. Census Bureau TIGER type1
+     codes found within	a given	U.S. Census Bureau TIGER type1
      data file.
      (GRASS Shell Script)
 
@@ -16,22 +16,22 @@ SYNOPSIS
      tiger.info.sh infile
 
 DESCRIPTION
-     tiger.info.sh is a shell script which outputs tract
-     number(s) and classification codes found within the TIGER
-     type1 data file infile.  Output is written to standard out,
-     and can be captured in a file by redirecting output.  This
-     information is useful when querying (using v.db.rim) from
-     the master binary vector file created by v.in.tig.rim.  It
-     also provides tract number(s) which can be used as input to
+     tiger.info.sh is a	shell script which outputs tract
+     number(s) and classification codes	found within the TIGER
+     type1 data	file infile.  Output is	written	to standard out,
+     and can be	captured in a file by redirecting output.  This
+     information is useful when	querying (using	v.db.rim) from
+     the master	binary vector file created by v.in.tig.rim.  It
+     also provides tract number(s) which can be	used as	input to
      the command Gen.Maps.
 
      Parameters:
 
-     infile            Name of a TIGER type1 data file.
+     infile	       Name of a TIGER type1 data file.
 
 NOTES
      This command must be installed separately as part of the
-     package of routines dealing with the import of Census
+     package of	routines dealing with the import of Census
      (TIGER) data.
 
 SEE ALSO
@@ -39,7 +39,7 @@ SEE ALSO
      Gen.tractmap
 
 AUTHOR
-     Marjorie Larson, U.S. Army Construction Engineering Research
+     Marjorie Larson, U.S. Army	Construction Engineering Research
      Laboratory
 
 
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.1                U.S. Army CERL                         1
+GRASS 4.2		Baylor University			1
 
 
 
