74d4e93800d67254f0a7a0e010f029d39955f707
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:17 +0100

    added

---
+++ src/imagery/i.ortho.photo/photo.2image/zoom.c
@@ -0,0 +1,28 @@
+#include "globals.h"
+
+
+zoom()
+{
+    static int use = 1;
+    int cancel();
+    int zoom_box();
+    int zoom_point();
+
+
+    static Objects objects[]=
+    {
+	MENU("CANCEL",cancel,&use),
+	MENU("BOX", zoom_box, &use),
+	MENU("POINT", zoom_point, &use),
+	INFO("Select type of zoom",&use),
+	{0}
+    };
+
+    Input_pointer (objects);
+    return 0;	/* return, but don't QUIT */
+}
+
+cancel()
+{
+    return -1;
+}
