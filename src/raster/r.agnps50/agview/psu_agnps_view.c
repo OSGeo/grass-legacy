48a366c2321b4d376f6e119f888b870c5b72d264
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:25 +0100

    added

---
+++ src/raster/r.agnps50/agview/psu_agnps_view.c
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+/*       ===========================================================
+         psu_agnps_view.c has the sole purpose of running the GIS
+         functions for displaying AGNPS output. 
+         ===========================================================
+*/
+
+
+main()
+{ char input_nps_filename[81];
+
+  G_gisinit("psu_agnps_view");
+  Critical_area_analys(input_nps_filename);
+  show_maps(input_nps_filename);
+}
