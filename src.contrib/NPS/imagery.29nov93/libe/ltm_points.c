2b1ec2949317c4990af0cc655e0a409ae9690a75
bot <grass-svn2git@osgeo.org>
1999-11-01 17:27:15 +0100

    added

---
+++ src.contrib/NPS/imagery.29nov93/libe/ltm_points.c
@@ -0,0 +1,44 @@
+/*======================================================================
+                             ltm_points.c
+
+======================================================================*/
+
+#include "ortho_image.h"
+
+
+I_get_ltm_points_data (group) 
+Rectify_Group *group;
+{
+   /* allocate the control points for landsat TM */
+   group->points = (Control_Points *) G_malloc (sizeof (Control_Points));
+
+   /* now get the control points */
+   get_ltm_points_data(group);
+}
+
+
+get_ltm_points_data(group)
+Rectify_Group    *group;
+{
+Control_Points   *points;
+char             msg[100];
+
+
+    /* make points visiable */
+    points = (Control_Points *) group->points;
+
+    /* get the Lat/Lon control points */
+    if (!I_get_con_points_ll(group, &points->points_ll))
+      	points->points_ll.count = 0;
+
+    /* convert the lat/lon to current locatation coordinates */
+    convert_from_ll (&points->points_ll, &points->points_temp);
+
+}
+
+
+
+
+
+
+
