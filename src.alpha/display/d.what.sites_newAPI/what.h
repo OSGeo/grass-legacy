3a71bd6773bf3ed7593d712fa962c200f409a3a3
bot <grass-svn2git@osgeo.org>
1999-11-01 17:29:10 +0100

    added

---
+++ src.alpha/display/d.what.sites_newAPI/what.h
@@ -0,0 +1,14 @@
+#include "gis.h"
+
+
+#ifndef GLOBAL
+#define GLOBAL extern
+#endif
+
+#define MAXSITES 25000
+
+GLOBAL double SL_e[MAXSITES];
+GLOBAL double SL_n[MAXSITES];
+GLOBAL char *SL_desc[MAXSITES];
+GLOBAL int Snum;
+
