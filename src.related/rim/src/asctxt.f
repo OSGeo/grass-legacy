4bbab5178fda2d3f28b2a2639cd3cdb64b5028eb
bot <grass-svn2git@osgeo.org>
1999-11-01 17:29:57 +0100

    added

---
+++ src.related/rim/src/asctxt.f
@@ -0,0 +1,24 @@
+      SUBROUTINE ASCTXT(ATXT,LTXT,CSTR)
+      INCLUDE 'syspar.d'
+C
+C     RETURN THE ASCII-TEXT EQUIVALENT OF CSTR
+C
+C        ATXT -- PLACE TO PUT ASCII-TEXT
+C        LXTX -- LENGTH OF ATXT
+C        CSTR -- CHARACTER STRING
+C
+      INTEGER ATXT(1)
+      CHARACTER*(*) CSTR
+C
+      INCLUDE 'ascpar.d'
+C
+      L = LEN(CSTR)
+      DO 100 I = 1, LTXT
+      IF (I.LE.L) THEN
+         CALL PUTT(ATXT,I,ASCCHR(CSTR(I:I)))
+      ELSE
+         CALL PUTT(ATXT,I,ABLANK)
+      ENDIF
+100   CONTINUE
+      RETURN
+      END
