cbdcb9b30511b35d599bacb5a356b776d882d76c
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:17 +0100

    added

---
+++ src/imagery/i.ortho.photo/photo.elev/elev.h
@@ -0,0 +1,11 @@
+/* elev.h */
+
+     char *elev_layer;
+     char *mapset_elev;
+     char *tl;
+     char *math_exp;
+     char *units;
+     char *nd;
+     static int  which_env;
+     static int  stat;
+
