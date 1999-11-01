8212c7f42e6603a13a79508c0da2ab5a70924dd3
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:53 +0100

    added

---
+++ src/sites/s.geom/libes/lia/copyright.h
@@ -0,0 +1,19 @@
+/* copyright.h  ---  Generic copyright notice. */
+
+/*--------------------------------------------------------------------------*/
+
+static char copyright[] = "\
+Copyright (C) 1991-93 The Board of Trustees  of the University of  Illinois\n\
+                      and the original author(s)        All rights reserved\n\
+                      The code is provided AS IS and WITHOUT ANY WARRANTIES\n\
+";
+
+/*--------------------------------------------------------------------------*/
+
+#ifndef lint
+# ifdef __DEBUG__
+#  ifndef _no_status_
+    static char status_of_complialtion[] = "@(#) \t w/ -D__DEBUG__";
+#  endif
+# endif
+#endif
