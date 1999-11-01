9bd286f6577eb7bdd268a741b41209cebfba97ed
bot <grass-svn2git@osgeo.org>
1999-11-01 17:26:22 +0100

    added

---
+++ src/tcltkgrass/module/d.zoom.sh
@@ -0,0 +1,8 @@
+interface_build {
+    {d.zoom.sh} 0
+    {Allows the user to change the current geographic region settings interactively, with a mouse.}
+    {checkbox action {Zoom   (for lat/lon databases only).} "" zoom}
+    {checkbox action {Rotate (for lat/lon databases only).} "" rotate}
+    {separator grey 1}
+    {checkbox -q {Run quietly.} "" -q}
+}
