e21e7cf9e94b9d9b60053a86a97650cba1a02166
bot <grass-svn2git@osgeo.org>
1999-11-01 17:28:15 +0100

    added

---
+++ src.contrib/GMSL/ogl3d_sgi/cmd/separate.c
@@ -0,0 +1,33 @@
+
+/* separate:
+** inserts a string of equal signs to seperate data generated
+** by the "whats here" panel.
+*/
+
+#include "interface.h"
+
+void
+separate(w,dc,client_data)
+Widget w;
+data_cell *dc;
+caddr_t client_data;
+{
+
+    char str[80], *str2;
+
+
+    strcpy(str , "==========================================\n");
+
+    str2 = str;
+
+    XmStringCreate(str2, XmSTRING_DEFAULT_CHARSET);
+
+    XmTextInsert(dc->Wtxt, dc->current_position, str2);
+    dc->current_position += strlen(str2);
+    XmTextShowPosition(dc->Wtxt, dc->current_position);
+	
+}
+
+
+
+
