578dce3320a6ce49ad73a6de29e68526e762d483
bot <grass-svn2git@osgeo.org>
1999-11-01 17:28:15 +0100

    added

---
+++ src.contrib/GMSL/ogl3d_sgi/cmd/GLcolormap.c
@@ -0,0 +1,22 @@
+
+/* installcolormap:
+** this is the function that sets the GL colormap apart from the 
+** colormap used by the X server
+*/
+
+#include "interface.h"
+
+
+
+void
+installcolormap(dc)
+data_cell *dc;
+{
+	Window windows[2];
+
+	windows[0] = XtWindow(dc->monitor);
+	windows[1] = XtWindow(dc->toplevel);
+	XSetWMColormapWindows(XtDisplay(dc->toplevel), 
+		                    XtWindow(dc->toplevel), windows,2);
+}
+
