64b40df422a6630a5ba8495c1db7bb3cf319513e
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:32 +0100

    added

---
+++ src/raster/r.agnps50/scripts/close_grass_window.sh
@@ -0,0 +1,21 @@
+total 619
+drwxrwxrwx  2 grass         512 Oct 18 01:16 agnps
+-rw-r--r--  1 grass           2 Oct 17 21:16 cell.id
+-rw-r--r--  1 grass        1382 Oct 18 16:35 cell.out
+-rwxr-xr-x  1 grass         366 Oct  4 13:57 close_grass_window.sh
+-rwxr-xr-x  1 grass        1598 Oct 18 14:15 create_asp_map
+-rwxr-xr-x  1 grass         321 Oct 18 06:45 display_cell_map.sh
+-rwxrwxrwx  1 grass         217 Oct 10 10:54 display_map.sh
+-rwxr-xr-x  1 grass         393 Oct  4 13:57 get_cell_id.sh
+-rw-r--r--  1 grass         175 Oct 17 21:15 new.win
+-rwxrwxr-x  1 grass      139264 Oct 17 19:09 psu_agnps_input_1
+-rwxrwxr-x  1 grass      204800 Oct 17 20:33 psu_agnps_input_2
+-rw-rw-rw-  1 grass       10240 Oct 10 11:23 psu_update_10-10-95.tar
+-rwxr-xr-x  1 grass         418 Oct 18 13:26 remove_old_tmp_files.sh
+-rwxr-xr-x  1 grass         457 Oct 18 13:26 remove_tmp_file.sh
+-rwxr--r--  1 grass      237568 Oct  7 06:12 run_agnps5.0
+-rwxr-xr-x  1 grass         686 Oct 18 16:33 run_psu_agnps_input
+-rw-rw-rw-  1 grass         725 Oct 13 16:09 runinfo
+-rw-rw-rw-  1 grass           0 Oct 18 16:35 temp.out
+-rw-rw-rw-  1 grass          39 Oct 18 16:35 temp_file
+-rw-rw-rw-  1 grass         744 Oct 18 16:34 tmpoutfile
