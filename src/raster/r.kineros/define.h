9d5584f7f36282e5117c76dead5908121c38ad30
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:40 +0100

    added

---
+++ src/raster/r.kineros/define.h
@@ -0,0 +1,19 @@
+#define STREAM (1)
+#define PLANE (2)
+
+#define BASE (2)
+
+#define MAX_SOIL (100)
+
+#define ENGLISH (1)
+#define METRIC (2)
+
+#define SECONDS (1)
+#define MINUTES (2)
+
+#define NO_EROSION (0)
+#define EROSION (1)
+
+#define FT_TO_M (0.3048)
+
+#define MIN_SLOPE (0.00001)
