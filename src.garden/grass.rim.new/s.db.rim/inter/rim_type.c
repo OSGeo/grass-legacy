cbdd4d2aa6ba3d56a5ccfffc4629f7d58bcde803
bot <grass-svn2git@osgeo.org>
1999-11-01 17:28:30 +0100

    added

---
+++ src.garden/grass.rim.new/s.db.rim/inter/rim_type.c
@@ -0,0 +1,35 @@
+#define RIM_TYPE
+
+#include <stdio.h>
+#include "make.h"
+
+#define RIM_F_STR "double"
+#define RIM_I_STR "integer"
+#define RIM_T_STR "text"
+/* This routine is passed a character representing a s.db.rim type and
+   a string pointer to a string to be filled with the RIM type and it
+   returns the string pointer which represents the RIM data type
+   that corresponds to the input character in the form definition code. */
+	char *rim_type(type_c, type_str)
+	char type_c, *type_str;
+{
+   switch (type_c) {
+      case F_FIELD_CHAR: strcpy(type_str, RIM_F_STR);
+                break;
+      case I_FIELD_CHAR: strcpy(type_str, RIM_I_STR);
+                break;
+      case S_FIELD_CHAR: strcpy(type_str, RIM_I_STR);
+                break;
+      case T_FIELD_CHAR: strcpy(type_str, RIM_T_STR);
+                break;
+      case X_FIELD_CHAR: strcpy(type_str, RIM_F_STR);
+                break;
+      case Y_FIELD_CHAR: strcpy(type_str, RIM_F_STR);
+                break;
+      default:  strcpy(type_str, "");
+   }
+
+   return(type_str);
+}
+
+
