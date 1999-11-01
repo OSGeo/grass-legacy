c08ec44d6ec21fdeb706d44abdcfd211cf1008de
bot <grass-svn2git@osgeo.org>
1999-11-01 17:28:45 +0100

    added

---
+++ src.garden/dbms/grass.postgres/d.what.r.pg/buildInfxQry.c
@@ -0,0 +1,69 @@
+#include "gis.h"
+
+
+buildInfxQry(ktab,keycat, joinargs, curcat)
+	struct Option *ktab, *keycat; 
+	char *joinargs[];
+	int curcat;
+{
+	FILE *fp;
+	char buf[1024];
+	char sqlFile[100] ;
+
+	sprintf(sqlFile,"/tmp/%d.sql", getpid() );
+
+        /* Open file to write SQL commands */
+        if((fp = fopen(sqlFile,"w")) == NULL) {
+            fprintf(stderr, "File write error on temproary file (sql).\n");
+            exit(-1);
+           }
+
+
+	if (joinargs)  {	/* two table qry with joinkey */
+/*
+ORIGINAL
+		fprintf(fp,"SELECT DISTINCT * FROM %s\n",joinargs[0]);
+		fprintf(fp,"WHERE %s in\n",joinargs[1]);
+		fprintf(fp,"(select %s from %s \n",
+			joinargs[2],ktab->answer);
+		fprintf(fp,"WHERE ( %s = %d ) )\n",
+			keycat->answer, curcat);
+*/
+/* POSTQUEL-query by J.Soimasuo */
+		fprintf(fp, "SELECT unique (%s.all) \n",joinargs[0]);
+		fprintf(fp, "where %s.%s = %d \n",
+			ktab->answer, keycat->answer, curcat);
+                fprintf(fp, "and %s = %s \n", 
+                        joinargs[2],joinargs[1]);
+	}
+	else 
+	{
+/*
+ORIGINAL
+		fprintf(fp,"SELECT DISTINCT * FROM %s\n",ktab->answer);
+		fprintf(fp,"WHERE ( %s = %d )\n",
+			keycat->answer, curcat);
+*/
+/* POSTQUEL-query by J.Soimasuo */
+		fprintf(fp, "SELECT unique (%s.all) \n",ktab->answer);
+		fprintf(fp, "where %s.%s = %d \n",
+			ktab->answer, keycat->answer, curcat);
+	}
+
+        
+	fclose (fp);
+/* original
+        sprintf(buf,"isql %s  %s 2>/dev/null", G_getenv("DATABASE"), sqlFile);
+*/
+	sprintf(buf,"psql -q -c \"` cat %s `\" %s ",
+		 sqlFile,G__getenv("PG_DBASE"));
+        /* Use the following to see DB output number of rows returned
+        sprintf(buf,"isql %s  %s 2>&1", G_getenv("DATABASE"), sqlFile);
+        */
+
+	if(isatty(1)) strcat (buf," | more");
+	system(buf);
+	unlink(sqlFile);
+
+	return 0;
+}
