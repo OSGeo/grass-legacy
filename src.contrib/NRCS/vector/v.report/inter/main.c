c22dcf64510ecd352ea52e24079172a99e8adcab
bot <grass-svn2git@osgeo.org>
1999-11-01 17:27:45 +0100

    added

---
+++ src.contrib/NRCS/vector/v.report/inter/main.c
@@ -0,0 +1,18 @@
+#define GLOBAL
+#include "global.h"
+main(argc, argv) char *argv[];
+{
+    G_gisinit(argv[0]);
+
+    stats_file = G_tempfile();
+    report_file = G_tempfile();
+
+    ask_layers();
+
+    while(ask_units())
+	run_report(1);
+    
+    unlink (stats_file);
+    unlink (report_file);
+    exit(0);
+}
