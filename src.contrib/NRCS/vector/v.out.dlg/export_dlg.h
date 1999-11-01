16ed38cfad03566e6eb54f2ad15f46eaf13d2f29
bot <grass-svn2git@osgeo.org>
1999-11-01 17:27:49 +0100

    added

---
+++ src.contrib/NRCS/vector/v.out.dlg/export_dlg.h
@@ -0,0 +1,11 @@
+#include "projects.h"
+extern int dlgtype;
+extern int dlgext;
+extern int dlgnad;
+extern float dlgsize_e, dlgsize_n;
+extern char *dlgprefix;
+extern struct pj_info info_in, info_out;
+extern struct Key_Value *in_proj_keys, *in_unit_keys;
+extern struct Key_Value *out_proj_keys, *out_unit_keys;
+
+#define DEF_MAJOR 999
