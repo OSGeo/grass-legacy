0c3449d2fc4895552852adbde86d626797602347
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:14 +0100

    added

---
+++ src/imagery/i.tape.tm.fast/cmd/bil.c
@@ -0,0 +1,17 @@
+/****************************************************************/
+/* NAME:	bil						*/
+/*								*/
+/* FUNCTION:	process band interleaved files			*/
+/*								*/
+/* USAGE:	bil()						*/
+/*								*/
+/* INPUT: 	none						*/
+/* 							 	*/
+/* OUTPUT: 	none						*/
+/****************************************************************/
+#include "tape.h"
+bil()
+{
+    fprintf(stderr, "BIL interleaving: not implemented\n");
+    fprintf(stderr, "	          because TM data is in BSQ only now\n");
+}
