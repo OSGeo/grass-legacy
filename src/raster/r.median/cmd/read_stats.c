00df81ff66790c1a00f4d1d91a0939f262729738
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:41 +0100

    added

---
+++ src/raster/r.median/cmd/read_stats.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+read_stats (fd, cat1, cat2, value)
+    FILE *fd;
+    long *cat1, *cat2;
+    double *value;
+{
+    char buf[1024];
+
+    if (fgets(buf, sizeof buf, fd) == NULL) return 0;
+
+    if (sscanf (buf, "%ld %ld %lf", cat1, cat2, value) == 3)
+	    return 1;
+    
+    fprintf (stderr, "ERROR reading r.stats output\n");
+    exit(1);
+}
