9262a68883f80868930f572b5c3f382f08a1cf95
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:30 +0100

    added

---
+++ src/raster/r.agnps50/hydro_tools/r.cn/data.c
@@ -0,0 +1,144 @@
+/*	January, 1991	Agricultural Engineering, Purdue University
+	Raghavan Srinivasan (srin@ecn.purdue.edu)
+	
+	void data()
+
+	To assign the curve number as per the CN tabel for the
+	corresponding row and column.
+*/
+
+#include "CN.h"
+
+
+void data()
+{
+
+	hy_soil_cover[0][0] = 77;
+	hy_soil_cover[0][1] = 86;
+	hy_soil_cover[0][2] = 91;
+	hy_soil_cover[0][3] = 94;
+	hy_soil_cover[1][0] = 72;
+	hy_soil_cover[1][1] = 81;
+	hy_soil_cover[1][2] = 88;
+	hy_soil_cover[1][3] = 91;
+	hy_soil_cover[2][0] = 67;
+	hy_soil_cover[2][1] = 78;
+	hy_soil_cover[2][2] = 85;
+	hy_soil_cover[2][3] = 89;
+	hy_soil_cover[3][0] = 70;
+	hy_soil_cover[3][1] = 79;
+	hy_soil_cover[3][2] = 84;
+	hy_soil_cover[3][3] = 88;
+	hy_soil_cover[4][0] = 65;
+	hy_soil_cover[4][1] = 75;
+	hy_soil_cover[4][2] = 82;
+	hy_soil_cover[4][3] = 86;
+	hy_soil_cover[5][0] = 66;
+	hy_soil_cover[5][1] = 74;
+	hy_soil_cover[5][2] = 80;
+	hy_soil_cover[5][3] = 82;
+	hy_soil_cover[6][0] = 62;
+	hy_soil_cover[6][1] = 71;
+	hy_soil_cover[6][2] = 78;
+	hy_soil_cover[6][3] = 81;
+	hy_soil_cover[7][0] = 65;
+	hy_soil_cover[7][1] = 76;
+	hy_soil_cover[7][2] = 84;
+	hy_soil_cover[7][3] = 88;
+	hy_soil_cover[8][0] = 63;
+	hy_soil_cover[8][1] = 75;
+	hy_soil_cover[8][2] = 83;
+	hy_soil_cover[8][3] = 87;
+	hy_soil_cover[9][0] = 63;
+	hy_soil_cover[9][1] = 74;
+	hy_soil_cover[9][2] = 82;
+	hy_soil_cover[9][3] = 85;
+	hy_soil_cover[10][0] = 61;
+	hy_soil_cover[10][1] = 73;
+	hy_soil_cover[10][2] = 81;
+	hy_soil_cover[10][3] = 84;
+	hy_soil_cover[11][0] = 61;
+	hy_soil_cover[11][1] = 72;
+	hy_soil_cover[11][2] = 79;
+	hy_soil_cover[11][3] = 82;
+	hy_soil_cover[12][0] = 59;
+	hy_soil_cover[12][1] = 70;
+	hy_soil_cover[12][2] = 78;
+	hy_soil_cover[12][3] = 81;
+	hy_soil_cover[13][0] = 66;
+	hy_soil_cover[13][1] = 77;
+	hy_soil_cover[13][2] = 85;
+	hy_soil_cover[13][3] = 89;
+	hy_soil_cover[14][0] = 58;
+	hy_soil_cover[14][1] = 72;
+	hy_soil_cover[14][2] = 81;
+	hy_soil_cover[14][3] = 85;
+	hy_soil_cover[15][0] = 64;
+	hy_soil_cover[15][1] = 75;
+	hy_soil_cover[15][2] = 83;
+	hy_soil_cover[15][3] = 85;
+	hy_soil_cover[16][0] = 55;
+	hy_soil_cover[16][1] = 69;
+	hy_soil_cover[16][2] = 78;
+	hy_soil_cover[16][3] = 83;
+	hy_soil_cover[17][0] = 63;
+	hy_soil_cover[17][1] = 73;
+	hy_soil_cover[17][2] = 80;
+	hy_soil_cover[17][3] = 83;
+	hy_soil_cover[18][0] = 51;
+	hy_soil_cover[18][1] = 67;
+	hy_soil_cover[18][2] = 76;
+	hy_soil_cover[18][3] = 80;
+	hy_soil_cover[19][0] = 68;
+	hy_soil_cover[19][1] = 79;
+	hy_soil_cover[19][2] = 86;
+	hy_soil_cover[19][3] = 89;
+	hy_soil_cover[20][0] = 49;
+	hy_soil_cover[20][1] = 69;
+	hy_soil_cover[20][2] = 79;
+	hy_soil_cover[20][3] = 84;
+	hy_soil_cover[21][0] = 39;
+	hy_soil_cover[21][1] = 61;
+	hy_soil_cover[21][2] = 74;
+	hy_soil_cover[21][3] = 80;
+	hy_soil_cover[22][0] = 47;
+	hy_soil_cover[22][1] = 67;
+	hy_soil_cover[22][2] = 81;
+	hy_soil_cover[22][3] = 88;
+	hy_soil_cover[23][0] = 25;
+	hy_soil_cover[23][1] = 59;
+	hy_soil_cover[23][2] = 75;
+	hy_soil_cover[23][3] = 83;
+	hy_soil_cover[24][0] = 6;
+	hy_soil_cover[24][1] = 35;
+	hy_soil_cover[24][2] = 70;
+	hy_soil_cover[24][3] = 79;
+	hy_soil_cover[25][0] = 30;
+	hy_soil_cover[25][1] = 58;
+	hy_soil_cover[25][2] = 71;
+	hy_soil_cover[25][3] = 78;
+	hy_soil_cover[26][0] = 45;
+	hy_soil_cover[26][1] = 66;
+	hy_soil_cover[26][2] = 77;
+	hy_soil_cover[26][3] = 83;
+	hy_soil_cover[27][0] = 36;
+	hy_soil_cover[27][1] = 60;
+	hy_soil_cover[27][2] = 73;
+	hy_soil_cover[27][3] = 79;
+	hy_soil_cover[28][0] = 25;
+	hy_soil_cover[28][1] = 55;
+	hy_soil_cover[28][2] = 70;
+	hy_soil_cover[28][3] = 77;
+	hy_soil_cover[29][0] = 59;
+	hy_soil_cover[29][1] = 74;
+	hy_soil_cover[29][2] = 82;
+	hy_soil_cover[29][3] = 86;
+	hy_soil_cover[30][0] = 72;
+	hy_soil_cover[30][1] = 82;
+	hy_soil_cover[30][2] = 87;
+	hy_soil_cover[30][3] = 89;
+	hy_soil_cover[31][0] = 74;
+	hy_soil_cover[31][1] = 84;
+	hy_soil_cover[31][2] = 90;
+	hy_soil_cover[31][3] = 92;
+}
