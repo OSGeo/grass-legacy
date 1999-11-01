6d0e70484134bcceb01bd20a41b395f337061b17
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:27 +0100

    added

---
+++ src/mapdev/v.out.moss/pnt_to_moss.c
@@ -0,0 +1,75 @@
+/*
+*
+*  THIS FUNCTION CONVERTS GRASS POINT DATA (SITE DATA IN DIGIT FILES) INTO 
+*  MOSS IMPORT FORMAT
+*
+*  WRITTEN BY:
+*  CHRIS EMMERICH, AUTOMETRIC INC., 10/3/89
+*/
+
+#include  "digit.h"
+#include  "gis.h"
+
+point_to_moss (map,pcats,moss,proj)
+
+struct Map_info *map;      /* SPATIAL INFO FOR THE DIGIT FILE */
+struct Categories *pcats;  /* CATEGORY INFO FOR THE DIGIT FILE */
+FILE *moss;                /* POINTER TO MOSS IMPORT FILE */
+int proj;                  /* PROJECTION TYPE */
+
+{
+    char *text;
+
+    double *x,*y;
+
+    int category,line,nlines,type;
+    int feature = 0;
+    int ncoord = 1;
+    int isle_flag = 0;
+
+    struct line_pnts *Points;
+
+	Points = Vect_new_line_struct();
+
+    printf 
+    ("\nConverting GRASS points (digit sites) to MOSS import point features\n");
+ 
+    /* GET NUMBER OF LINES, AND PROCESS THEM SEQUENTIALLY */
+    nlines = V2_num_lines (map);
+
+    for (line = 1; line <= nlines; line++)
+    {
+        if ((type = V2_read_line (map, Points, line)) < 0)
+        {
+            printf ("\nNot able to read line %d\n",line);
+            exit (-1);
+        }
+
+        /* PROCESS ITEM IF IT IS A POINT */
+        if (type == DOT)
+        {
+            /* GET POINT ATTRIBUTE */
+            category = V2_line_att (map,line);
+
+            x = Points->x;
+            y = Points->y;
+
+            /* GET CATEGORY TEXT */
+            text = G_get_cat (category,pcats);
+
+            /* WRITE MOSS IMPORT RECORDS FOR THIS LINE */
+            feature++;
+            if (write_moss_header (moss,feature,ncoord,text,proj) < 0 ||
+                write_moss_coordinates (moss,ncoord,x,y,isle_flag,proj) < 0)
+            {
+                printf ("\nNot able to write MOSS import file\n");
+                exit (-1);
+            }
+         }
+    }  /* END LOOP */
+
+	Vect_destroy_line_struct (Points);
+
+    printf ("\n%d Points Converted\n",feature);
+    return (0);
+}
