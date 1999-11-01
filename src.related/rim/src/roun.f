c32899a11a7c0950da15b7b9a78d1078a45cfe09
bot <grass-svn2git@osgeo.org>
1999-11-01 17:30:00 +0100

    added

---
+++ src.related/rim/src/roun.f
@@ -0,0 +1,14 @@
+      SUBROUTINE ROUN(REAL,ND,RO)
+C
+C     RETURN A ROUNDED VERSION OF THE REAL NUMBER
+C     ACCURATE TO ND PLACES.
+C
+      INCLUDE 'syspar.d'
+      DOUBLE PRECISION REAL, V, RO
+1     RO = REAL
+      IF(REAL.EQ.0.) RETURN
+      V = .5
+      IF(REAL.LT.0.) V = -.5
+      RO = REAL + V*(10.0D0**(0-ND))
+9     RETURN
+      END
