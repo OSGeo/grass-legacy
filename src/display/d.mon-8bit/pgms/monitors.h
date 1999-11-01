9127c3e6381b7990b4bd8d57603b474ba63893f1
bot <grass-svn2git@osgeo.org>
1999-11-01 17:21:01 +0100

    added

---
+++ src/display/d.mon-8bit/pgms/monitors.h
@@ -0,0 +1,16 @@
+struct MON_CAP
+{
+  char *name;				/* name of monitor */
+  char *path;				/* full path name to execute monitor */
+  char *comment;			/* like this */
+  char *link;				/* were monitor interfaces to gis */
+  char *tty;				/* where monitor must be run from */
+  char *where;				/* version of above for user */
+};
+
+#define MON_NEXT 1
+#define MON_NAME 2
+#define MON_PATH 3
+#define MON_LINK 4
+#define MON_CLOSE 5
+
