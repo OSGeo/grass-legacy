e33a924faf898ec6bfa0b67e769d1900a7dee0c9
bot <grass-svn2git@osgeo.org>
1999-11-01 17:26:07 +0100

    added

---
+++ src/paint/Programs/p.map.new/cmd/text.h
@@ -0,0 +1,18 @@
+#define XSIZE 25
+#define YSIZE 25
+
+#define LEFT 0
+#define RIGHT 1
+#define LOWER 0
+#define UPPER 1
+#define CENTER 2
+
+
+#define BOX struct _box_
+BOX
+{
+    int top;
+    int bottom;
+    int left;
+    int right;
+};
