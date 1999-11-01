4f60f0007664999be31ec640474ae7276ed22753
bot <grass-svn2git@osgeo.org>
1999-11-01 17:24:16 +0100

    added

---
+++ src/imagery/i.ortho.photo/photo.2image/tty.c
@@ -0,0 +1,34 @@
+#ifdef USE_TERMIO
+#   include <termio.h>
+#   define TTY    termio
+#   define get(x) ioctl (1, TCGETA, x)
+#   define set(x) ioctl (1, TCSETA, x)
+#else
+#   include <sgtty.h>
+#   define TTY    sgttyb
+#   define get(x) gtty (1, x)
+#   define set(x) stty (1, x)
+#endif /* USE_TERMIO */
+
+static struct TTY new ;
+static struct TTY old ;
+
+Get_old_tty()
+{
+    get (&old);
+}
+
+Get_new_tty()
+{
+    get (&new);
+}
+
+Old_tty()
+{
+    set (&old);
+}
+
+New_tty()
+{
+    set (&new);
+}
