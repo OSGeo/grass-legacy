a2a97988c1e09e1e679bdc1634d9a749288d75c5
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:57 +0100

    added

---
+++ src/sites/s.sample/readsite.c
@@ -0,0 +1,50 @@
+#include "gis.h"
+#include "methods.h"
+
+int readsites (fdsite, verbose, xyd, window)
+  FILE *fdsite;
+  int verbose;
+  Z **xyd;
+  struct Cell_head window;
+{
+  char *dum;
+  int i, alloced = 1000;
+  double east, north;
+
+  G_sleep_on_error (0);
+
+  if (verbose)
+    fprintf (stderr, "Reading sites list ...              ");
+
+  /* allocate chunk of memory */
+#ifndef lint
+  (*xyd) = (Z *) G_malloc (alloced * sizeof (Z));
+#endif
+  if ((*xyd) == NULL)
+    G_fatal_error ("cannot allocate memory");
+  i = 0;
+  while (G_get_site (fdsite, &east, &north, &dum) > 0 )
+  {
+    if (east >= window.west && east <= window.east &&
+	north <= window.north && north >= window.south)
+    {
+      if (i == alloced-1)
+      {
+	alloced += 1000;
+#ifndef lint
+	(*xyd) = (Z *) G_realloc (*xyd, alloced * sizeof (Z));
+#endif
+	if ((*xyd) == NULL)
+	  G_fatal_error ("cannot allocate memory");
+      }
+      G_strcpy ((*xyd)[i].desc, dum);
+      (*xyd)[i].x = east;
+      (*xyd)[i++].y = north;
+    }
+  }
+  fclose (fdsite);
+  G_sleep_on_error (1);
+  if (verbose)
+    G_percent (1, 1, 1);
+  return i;
+}
