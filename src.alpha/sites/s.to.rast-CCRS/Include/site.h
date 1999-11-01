bca107c8a4af7b629b5a40d40b45e39c6da7b842
bot <grass-svn2git@osgeo.org>
1999-11-01 17:29:30 +0100

    added

---
+++ src.alpha/sites/s.to.rast-CCRS/Include/site.h
@@ -0,0 +1,20 @@
+
+#define SITE_LIST struct _sitelist
+#define SITE struct _site
+
+SITE
+{
+    int north;
+    int east;
+    char *desc;
+    SITE *next;
+};
+
+SITE_LIST
+{
+    char name[100];
+    char desc[100];
+    SITE *cur;
+    SITE *first;
+    SITE *last;
+};
