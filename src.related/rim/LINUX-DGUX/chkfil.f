bf92bf0ee59af6cdfe393c247224ad342f55e03b
bot <grass-svn2git@osgeo.org>
1999-11-01 17:30:05 +0100

    added

---
+++ src.related/rim/LINUX-DGUX/chkfil.f
@@ -0,0 +1,15 @@
+      LOGICAL FUNCTION CHKFIL(FNAME,RW)
+      INCLUDE 'syspar.d'
+C
+C     **UNIX SYSTEM DEPENDENT INTERNAL ROUTINE **
+C
+C     CHECK FOR A FILE'S EXISTANCE AND READ/WRITE PERMISSION
+C
+      CHARACTER*(*) FNAME
+      LOGICAL RW
+C
+      inquire(FILE=fname,EXIST=rw,iostat=ios)
+      chkfil = rw
+      RW = .TRUE.
+      RETURN
+      END
