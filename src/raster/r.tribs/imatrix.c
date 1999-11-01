c3643a600cb9a4d053879ebaec627505d37c8bdc
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:44 +0100

    added

---
+++ src/raster/r.tribs/imatrix.c
@@ -0,0 +1,25 @@
+#include <malloc.h>
+#include <stdio.h>
+
+int **imatrix(nrl,nrh,ncl,nch)
+int nrl,nrh,ncl,nch;
+{
+    int i,**m;
+
+    m=(int **)malloc((unsigned) (nrh-nrl)*sizeof(int*));
+    if (!m) {
+	printf("\n ERROR: allocation failure 1 in imatrix()");
+	exit(0);
+    }
+    m -= nrl;
+
+    for(i=nrl;i<=nrh;i++) {
+	m[i]=(int *)malloc((unsigned) (nch-ncl)*sizeof(int));
+	if (!m[i]) {
+	    printf("\n ERROR: allocation failure 1 in imatrix()");
+	    exit(0);
+        }
+	m[i] -= ncl;
+    }
+    return m;
+}
