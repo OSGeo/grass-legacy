c09bba14f02a201e0fef7297982bc64a7a2c20f6
bot <grass-svn2git@osgeo.org>
1999-11-01 17:30:02 +0100

    added

---
+++ src.related/rim/src/toled.f
@@ -0,0 +1,24 @@
+      SUBROUTINE TOLED(K,V,N)
+      INCLUDE 'syspar.d'
+C
+C     THIS ROUTINE APPLIES A TOLERANCE TO A DOUBLE ROUTINE
+C
+C     K IS LOCBOO VALUE
+C     V(N) IS DOUBLE ARRAY
+C
+      INCLUDE 'flags.d'
+      DOUBLE PRECISION V(N)
+      DOUBLE PRECISION X
+      X = TOL
+      IF(K.GT.5) X = -X
+      IF(PCENT) GO TO 50
+      DO 20 I=1,N
+      V(I) = V(I) - X
+   20 CONTINUE
+      RETURN
+   50 CONTINUE
+      DO 70 I=1,N
+      V(I) = V(I)*(1.-X)
+   70 CONTINUE
+      RETURN
+      END
