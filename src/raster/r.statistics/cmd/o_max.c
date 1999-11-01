7cddd785b4ee1771930847808b0077b89a00fd8c
bot <grass-svn2git@osgeo.org>
1999-11-01 17:25:42 +0100

    added

---
+++ src/raster/r.statistics/cmd/o_max.c
@@ -0,0 +1,55 @@
+#include "gis.h"
+
+o_max(basemap,covermap,outputmap,usecats, cats)
+char *basemap,*covermap,*outputmap;
+int usecats;
+struct Categories *cats;
+{
+    char command[1024];
+    FILE *popen(), *stats, *reclass;
+    int first;
+    long basecat, covercat, catb, catc;
+
+    sprintf(command, "r.stats input='%s,%s' fs=space", basemap, covermap);
+    stats = popen (command, "r");
+
+    sprintf (command, "r.reclass i='%s' o='%s'", basemap, outputmap);
+    reclass = popen (command, "w");
+
+    first = 1;
+    
+    while (fscanf (stats, "%ld %ld", &basecat, &covercat) == 2)
+    {
+
+	if (first)
+	{
+	    first = 0;
+	    catb  = basecat;
+	    catc  = covercat;
+	}
+	
+	if (basecat != catb)
+	{
+            write_reclass (reclass, catb, catc, G_get_cat (catc, cats), usecats);
+	    catb = basecat;
+	    catc = covercat;
+	}
+
+	if (covercat > catc)
+	    catc = covercat;
+
+
+    }
+    
+    if (first)
+    {
+	catb = catc = 0;
+    }
+    
+    write_reclass (reclass, catb, catc, G_get_cat (catc, cats), usecats);
+    
+    pclose (stats);
+    pclose (reclass);
+
+    return(0);
+}
