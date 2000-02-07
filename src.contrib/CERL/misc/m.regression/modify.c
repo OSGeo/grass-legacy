a8dd76fc51604848530e4f955242f118cffdb3db
Markus Neteler <neteler@gmail.com>
2000-02-07 15:00:01 +0000

    gets updated to fgets
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@170 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.contrib/CERL/misc/m.regression/modify.c
+++ src.contrib/CERL/misc/m.regression/modify.c
@@ -9,7 +9,7 @@ modify (char *name, double *x, int i)
     while(1)
     {
 	fprintf (stdout,"%s[%d]=%g  new value? ",name,i,x[i]);
-	if (!gets(buf)) exit(0);
+	if (!fgets(buf,70,stdin)) exit(0);
 	if (*buf == 0) return;
 	if (sscanf (buf, "%lf", &v) == 1) break;
 	fprintf (stdout,"??\n");
