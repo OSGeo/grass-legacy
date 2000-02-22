42d61a6a54050d9f5e4ba8b73ac18b22c4d5fa91
Markus Neteler <neteler@gmail.com>
2000-02-22 17:49:32 +0000

    bugfix on memory release (was in wrong position)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@227 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.contrib/CERL/imagery/i.shape/zc_curve.c
+++ src.contrib/CERL/imagery/i.shape/zc_curve.c
@@ -104,8 +104,6 @@ int zc_curve (int inputfd, int zcfd, double Width,
 
         G_free(cell_row);
 
-        /* Release memory resources */
-        for (i=0 ; i<2 ; i++) G_free(data[i]);
         fprintf (stdout,"Transform successful\n");
 				/*
 -------------------------------------------------------------------
@@ -311,6 +309,9 @@ fprintf (stdout,"x[%d]=%f, y[%d]=%f\n", s, x[s], s, y[s]);
 fprintf (stdout,"dalpha[%d]=%f\n", s, dalpha[s]);
 *sout=s+1;
 *totals=ltotal;
+
+        /* Release memory resources */
+/*        for (i=0 ; i<2 ; i++) G_free(data[i]);*/
 
 return 0;
 }
