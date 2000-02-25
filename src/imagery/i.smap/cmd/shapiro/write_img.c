e24b6641f29d69564e85a006c8440e89682dd6be
Markus Neteler <neteler@gmail.com>
2000-02-25 18:48:36 +0000

    interim fix to overcome segfault problem on writing image
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@258 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/imagery/i.smap/cmd/shapiro/write_img.c
+++ src/imagery/i.smap/cmd/shapiro/write_img.c
@@ -16,6 +16,7 @@ int write_img (
 	if (!parms->quiet) G_percent (row, nrows, 2);
 	for (col = 0; col < ncols; col++)
 	{
+/* not working 2/2000:
 	   if(G_set_c_null_value(&img[row][col],1))
 		   G_set_c_null_value(&files->cellbuf[col], 1);
            else
@@ -23,6 +24,11 @@ int write_img (
 		class = (int)img[row][col];
 		files->cellbuf[col] = (CELL)S->ClassSig[class].classnum;
            }
+ */
+
+		class = (int)img[row][col];
+		files->cellbuf[col] = (CELL)S->ClassSig[class].classnum;
+ 
 	}
 	G_put_c_raster_row (files->output_fd, files->cellbuf);
     }
