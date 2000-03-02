494015eec96b3d46157758ce033a7465e029e54f
Markus Neteler <neteler@gmail.com>
2000-03-02 18:36:56 +0000

    Radim.Blazek@dhv.cz: Replaced again with G_sqeeze
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@276 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/ps.map/ps.map/inter/input.c
+++ src/ps.map/ps.map/inter/input.c
@@ -6,11 +6,10 @@
 int input (char *buf)
 {
     if (!fgets(buf,100,stdin)) exit(0);
-  /******* remove CR because of fgets   Werner.Droege@mailbox.tu-dresden.de 
-   * required for Solaris */
-    buf[strlen(buf)-1]='\0';
-    
-    G_strip (buf);
+    /* G_strip (buf); */ 
+    /*   replaced by G_squeeze to remove \n, 
+     *   Radim.Blazek@dhv.cz (3/2000) */ 
+    G_squeeze (buf);
     if (strcmp (buf,"exit")==0) exit(0);
 
     return 0;
