2185ec364b76862bc7cdd95ac8a653ef1ddfaed0
Radim Blazek <radim.blazek@gmail.com>
2000-03-14 13:36:54 +0000

    New option ref, LINE_REF_CENTER, LINE_REF_LEFT, ...
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@386 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/ps.map.new/cmd/vector.h
+++ src/ps.map.new/cmd/vector.h
@@ -1,8 +1,23 @@
 /** Modified by: Janne Soimasuo August 1994 line_cat added **/
 /** Modified by: Radim Blazek Jan 2000 acolor, label added **/
 #include "color.h"
+#define PI 3.14159265   
 #define MAXVECTORS 20
 
+/* line justification */
+#define LINE_REF_CENTER 0
+#define LINE_REF_LEFT 1
+#define LINE_REF_RIGHT 2
+
+/* draw line */
+#define LINE_DRAW_LINE 1
+#define LINE_DRAW_HIGHLITE 2
+
+/* construct_path() */
+#define START_PATH  0 
+#define ADD_TO_PATH 1
+#define CLOSE_PATH  2
+#define WHOLE_PATH  3
 
 struct vector
 {
@@ -17,7 +32,8 @@ struct vector
     double width[MAXVECTORS];
     double cwidth[MAXVECTORS];
     double offset[MAXVECTORS];
-    double coffset[MAXVECTORS];            
+    double coffset[MAXVECTORS];
+    int ref[MAXVECTORS];
     int  hcolor[MAXVECTORS];
     double hwidth[MAXVECTORS];
     char masked[MAXVECTORS];
