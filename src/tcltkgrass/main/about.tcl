29fd886e7709a9397fe203913af7becd6b367328
bot <grass-svn2git@osgeo.org>
2000-03-17 17:43:34 +0100

    added

---
+++ src/tcltkgrass/main/about.tcl
@@ -0,0 +1,34 @@
+#
+# Program : gis_intro.tcl
+# 
+
+help {About} {-width 50} {-justify center} \
+{TCLTKGRASS (4.x) 3.0 (March 2000)
+An open graphical user interface for GRASS GIS
+
+written by Jacques Bouchard for
+
+GRASS Research Group
+Baylor University, Texas
+University of Hannover
+www.baylor.edu/~grass/
+http://www.geog.uni-hannover.de/grass/
+
+Please send your comments to:
+
+Markus Neteler
+<neteler@geog.uni-hannover.de>
+
+or
+
+Jacques Bouchard
+<bouchard@onera.fr>
+
+-----
+
+The software is inspired by TCL/TK GRASS
+developed in 1994 by
+
+L.A.S.
+Logiciels et Applications Scientifiques Inc.
+Montreal, Canada}
