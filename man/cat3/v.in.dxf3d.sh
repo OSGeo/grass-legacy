b79274add2ae2939e1a260e568f4f5e6ffbce505
bot <grass-svn2git@osgeo.org>
2000-03-17 11:19:26 +0100

    added

--- man/cat3/v.in.dxf3d.sh
+++ man/cat3/v.in.dxf3d.sh
@@ -6,8 +6,8 @@ v.in.dxf3d.sh <scriptsGRASS Reference Manu<scripts> v.in.dxf3d.sh
 
 
 NAME
-     Iv.in.dxf3d.sh R -	Imports	contour	levels and master contour
-     levels in DXF file	format to GRASS	vector file format.
+     Iv.in.dxf3d.sh R - Imports contour levels and master contour
+     levels in DXF file format to GRASS vector file format.
      (GRASS Vector Program)
 
 SYNOPSIS
@@ -16,24 +16,24 @@ SYNOPSIS
 DESCRIPTION
      The v.in.dxf3d.sh data conversion program generates GRASS
      vector files from a DXF file with contour levels and master
-     contour levels layers with	Z values.
+     contour levels layers with Z values.
 
-     This shell	run successively the programs v.in.dxf,
-     v.in.dxf3d	and v.support for both specified layers.
+     This shell run successively the programs v.in.dxf,
+     v.in.dxf3d and v.support for both specified layers.
 
 
 
-COMMAND	LINE OPTIONS
+COMMAND LINE OPTIONS
      Parameters
 
-     dxf  Name of the DXF input	design file to be converted to
-	  GRASS	vector format.
+     dxf  Name of the DXF input design file to be converted to
+	  GRASS vector format.
 
      lines
-	  Name(s) of layer(s) in DXF input file	containing  the
-	  contour levels and master contour levels with	Z values,
-	  and the mane(s) to be	assigned to the	GRASS vector
-	  files	output.
+	  Name(s) of layer(s) in DXF input file containing  the
+	  contour levels and master contour levels with Z values,
+	  and the mane(s) to be assigned to the GRASS vector
+	  files output.
 
 
 SEE ALSO
@@ -41,7 +41,7 @@ SEE ALSO
 
 
 AUTHOR
-     The shell was writted by Evaristo Quiroga,	Environmental and
+     The shell was writted by Evaristo Quiroga, Environmental and
      Territorial Analisis Center, UAB (12/95).
 
 
@@ -60,7 +60,7 @@ AUTHOR
 
 
 
-GRASS 4.2		Baylor University			1
+GRASS 4.3		Baylor University			1
 
 
 
