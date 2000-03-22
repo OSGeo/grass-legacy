d6a6c16bfabf080b53581423b745b03e25512f57
Alex Shevlakov <sixote@yahoo.com>
2000-03-22 02:05:17 +0000

    Bugfix in paint/interface - flush.c
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@420 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/paint/Interface/applib/flush.c
+++ src/paint/Interface/applib/flush.c
@@ -2,7 +2,9 @@
 
 int Pflush (void)
 {
-    P__opcode (FLUSH);
+ /*   P__opcode (FLUSH);*/
+ P__flushdev (); 
+ /* bugfix 23/03/00 ash - otherwise xbuf[] is not empty before close (wfd) */
 
     return 0;
 }
