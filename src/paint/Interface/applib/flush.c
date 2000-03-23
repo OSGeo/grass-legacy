48f054e4f4caf08a22a0f4dac2f97db71e16af65
Alex Shevlakov <sixote@yahoo.com>
2000-03-23 02:17:34 +0000

    Problem description added
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@431 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/paint/Interface/applib/flush.c
+++ src/paint/Interface/applib/flush.c
@@ -5,6 +5,8 @@ int Pflush (void)
  /*   P__opcode (FLUSH);*/
  P__flushdev (); 
  /* bugfix 23/03/00 ash - otherwise xbuf[] is not empty before close (wfd) */
-
+ /*problem description - as P_send evokes P__writedev, which does not 
+  guarantee that the buffer is flushed, we must check that all data has been
+  sent before we close pipes*/
     return 0;
 }
