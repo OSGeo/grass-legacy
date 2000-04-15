0859dd143c54d738656bf3d1fe9d68e71c6a641f
Markus Neteler <neteler@gmail.com>
2000-04-15 08:10:58 +0000

    Pierre: final bugfix?
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@540 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/imagery/i.rectify2/rectify.c
+++ src/imagery/i.rectify2/rectify.c
@@ -14,6 +14,7 @@ int rectify (char *name, char *mapset, char *result, int order)
     int row, col;
     int infd;
     void *rast;
+	char buf[64]="";
 
     select_current_env();
     if (G_get_cellhd (name, mapset, &cellhd) < 0)
@@ -36,6 +37,7 @@ int rectify (char *name, char *mapset, char *result, int order)
 /* open the file to be rectified
  * set window to cellhd first to be able to read file exactly
  */
+	G_suppress_warnings(1);
     G_set_window (&cellhd);
     infd = G_open_cell_old (name, mapset);
     if (infd < 0)
@@ -81,10 +83,30 @@ int rectify (char *name, char *mapset, char *result, int order)
 	win.west += (win.ew_res * win.cols);
     }
     select_target_env();
+	G_suppress_warnings(0);
+	if (cellhd.proj == 0) { /* x,y imagery */
+			cellhd.proj = target_window.proj;
+			cellhd.zone = target_window.zone;
+	}
+
+	if (target_window.proj != cellhd.proj) {
+			cellhd.proj = target_window.proj;
+			sprintf(buf,"WARNING %s@%s: projection don't match current settings.\n",name,mapset);
+			G_warning(buf);
+	}  
+
+	if (target_window.zone != cellhd.zone) {
+			cellhd.zone = target_window.zone;
+			sprintf(buf,"WARNING %s@%s: zone don't match current settings .\n",name,mapset);
+			G_warning(buf);
+	}  
+
+	G_suppress_warnings(1);
     target_window.compressed=cellhd.compressed;
     write_map(result);
     select_current_env();
 
+	G_suppress_warnings(0);
     G_close_cell (infd);
     G_free (rast);
 
