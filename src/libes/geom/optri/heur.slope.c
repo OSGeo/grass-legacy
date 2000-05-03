1c61da39d5780ea8b70d595400232a9a16b728af
Markus Neteler <neteler@gmail.com>
2000-05-03 16:39:31 +0000

    renamed MAX to MYMAX in order to avoid conflicts
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@642 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/libes/geom/optri/heur.slope.c
+++ src/libes/geom/optri/heur.slope.c
@@ -3,13 +3,7 @@
 #include "geom/lia.h"
 #include "geom/sos.h"
 
-#ifdef __CYGWIN__
-#define MAX(a,b) (a > b ? a : b)
-#endif
-
-#ifdef sparc
-#define MAX(a,b) (a > b ? a : b)
-#endif
+#define MYMAX(a,b) (a > b ? a : b)
 
 /*--------------------------------------------------------------------------*/
 /*--------------------------------------------------------------------------*/
@@ -194,7 +188,7 @@ static int hsTestIfAnchor (
   lia_mul (tmp3, tmp1, A);
   lia_sub (tmp1, tmp3, tmp2);
 
-  if (ISZERO (tmp1)) return (candidate >= MAX (v1, v2));
+  if (ISZERO (tmp1)) return (candidate >= MYMAX (v1, v2));
 
   if (v2 == candidate) return (candidate >= v1);
 
@@ -204,7 +198,7 @@ static int hsTestIfAnchor (
   lia_mul (tmp3, tmp4, A);
   lia_sub (tmp4, tmp3, tmp2);
 
-  if (ISZERO (tmp4)) return (candidate >= MAX (v1, v2));
+  if (ISZERO (tmp4)) return (candidate >= MYMAX (v1, v2));
 
   return (ISPOSITIVE (tmp4) != ISPOSITIVE (tmp1));
 }
