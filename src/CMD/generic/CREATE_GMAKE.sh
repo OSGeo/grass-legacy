b5d8ea4af05ddd290e9454849e14ce1faa6cab40
Markus Neteler <neteler@gmail.com>
2000-05-08 12:13:33 +0000

    added -p to mkdir
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@663 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/CMD/generic/CREATE_GMAKE.sh
+++ src/CMD/generic/CREATE_GMAKE.sh
@@ -3,7 +3,7 @@
 HEAD_FILE=`dirname $0`"/../head/head"
 # set (and create) UNIX_BIN
 eval `cat ${HEAD_FILE} | grep UNIX_BIN | sed "s/ //g"`
-if [ ! -d $UNIX_BIN ]; then mkdir $UNIX_BIN ; fi
+if [ ! -d $UNIX_BIN ]; then mkdir -p $UNIX_BIN ; fi
 
 # create gmake5 script to be used for local compiling
 echo ":"                                   > $UNIX_BIN/gmake5
@@ -11,7 +11,7 @@ echo "SRC=$SRC/src"                       >> $UNIX_BIN/gmake5
 echo "CMD=$SRC/src/CMD"                   >> $UNIX_BIN/gmake5
 echo "HEADER=head"                        >> $UNIX_BIN/gmake5
 echo "HASX=yes"                           >> $UNIX_BIN/gmake5
-echo "HASMotif=no"                        >> $UNIX_BIN/gmake5
+echo "HASMotif=no"                       >> $UNIX_BIN/gmake5
 echo ". $SRC/src/CMD/generic/gmake.sh"    >> $UNIX_BIN/gmake5
 chmod ugo+x $UNIX_BIN/gmake5
 
