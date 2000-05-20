c8d3f8b35046ee147a75caf76b67080939e9112a
Radim Blazek <radim.blazek@gmail.com>
2000-05-20 17:58:38 +0000

    new driver
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@737 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ src/libes/dbmi/drivers/odbc/execute.c
@@ -0,0 +1,35 @@
+#include <dbmi.h>
+#include <odbc/sql.h>
+#include <odbc/sqlext.h>
+#include <odbc/sqltypes.h> 
+#include "globals.h"
+#include "proto.h"
+
+db_driver_execute_immediate (sql)
+    dbString *sql;
+{
+    char *s, msg[OD_MSG], emsg[DB_MSG];
+    cursor *c;
+    SQLRETURN   ret;
+    SQLINTEGER   err;     
+
+    s = db_get_string (sql);
+
+    /* allocate cursor */
+    c = alloc_cursor();
+        if (c == NULL)
+            return DB_FAILED;   
+		    
+    ret = SQLExecDirect(c->stmt,s,SQL_NTS);
+    if ((ret != SQL_SUCCESS) && (ret != SQL_SUCCESS_WITH_INFO))
+    {
+	SQLGetDiagRec(SQL_HANDLE_STMT, c->stmt, 1, NULL, &err, msg, sizeof(msg), NULL);
+	snprintf(emsg, sizeof(emsg), "SQLExecDirect():\n%s\n%s (%d)\n",s,msg,err);
+	report_error(emsg);
+	return DB_FAILED;
+    }   
+
+    free_cursor (c);
+
+    return DB_OK;
+}
---
+++ src/libes/dbmi/drivers/odbc/globals.h
@@ -0,0 +1,18 @@
+#define MAX_CHAR_SIZE	1000	// replace by value from ODBC 
+#define DB_MSG 800  //max length message for report_error()
+#define OD_MSG 500  //max length of message returned by SQLGetDiagRec()
+
+/* cursors */
+typedef struct _cursor {
+    SQLHSTMT    stmt;
+    dbToken token;
+    int type;              /* type of cursor: SELECT, UPDATE, INSERT */
+} cursor;  
+
+#ifndef MAIN
+    SQLHENV	ODenvi;  // Handle ODBC environment
+    SQLHDBC    	ODconn;      // Handle connection   
+#else
+    extern  SQLHENV  ODenvi;  // Handle ODBC environment
+    extern  SQLHDBC  ODconn;      // Handle connection   
+#endif 
