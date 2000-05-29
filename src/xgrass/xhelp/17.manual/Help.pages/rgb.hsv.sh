0d89584c36e737766d25cb0b447418adbcf93ae8
Markus Neteler <neteler@gmail.com>
2000-05-29 16:30:49 +0000

    removed unused xgrass (MOTIF dependent)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@834 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/xgrass/xhelp/17.manual/Help.pages/rgb.hsv.sh
+++
@@ -1,5 +0,0 @@
-
-@man("rgb.hsv.sh") - A GRASS macro, transforming pixel red, green, and blue
-values to hue, saturation, and v (hsv) values.
-
-(GRASS Shell Script)
--- src/xgrass/xhelp/17.manual/Help.pages/start.man.sh
+++
@@ -1,5 +0,0 @@
-
-@man("start.man.sh") - GRASS shell script macro that builds a template for
-GRASS manual entries.
-
-(GRASS Shell Script)
