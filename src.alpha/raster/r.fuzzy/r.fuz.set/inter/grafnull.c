191ed94ca931b640ec2b3487c3d3596658394589
bot <grass-svn2git@osgeo.org>
2000-06-14 12:16:58 +0200

    added

---
+++ src.alpha/raster/r.fuzzy/r.fuz.set/inter/grafnull.c
@@ -0,0 +1,67 @@
+#include <dessin.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+void axex_()
+{
+  int inumx,iaxex,iposx;
+  float dx,xmin,xmax;
+  char legx[50];
+  
+  return;
+}
+void axey_()
+{
+  int inumy,iaxey,iposy; 
+  float dy,ymin,ymax;
+  char legy[50];
+ 
+
+  return;
+}
+
+void courbe_()
+{
+  int inumc,isymb,itrai,icoulc,n;
+  float tabcx,tabcy;
+  char legc[50];
+  
+  return;
+}
+
+
+/*      subroutine hyeto(inumh,pourc,tabhx,tabhz,n,ibar,icolhyeto,legh)
+c     *----------------------------------------------------------------*
+c     I   ssp  ecriture des donnees HYETO dans GRAF.DAT
+c     *----------------------------------------------------------------* 
+c     I    Ce fichier est cree lors du premier appel si un des autres
+c     I    ssp  AXEX, AXEY ou HYETO ne l'a pas deja fait.
+c     *----------------------------------------------------------------* 
+c     I   en entree :
+c     I   inumh      int*4    numero de hyeto
+c     I   pourc               pourcentage d'occupation espace-ecran
+c     I                        attribue a  hyeto
+c     I   tabhx,tabhy         tableau des coordonnees en x ,y
+c     I   n          int*4    nombre de valeurs sgnificatives des 
+c     I                        tableaux tabhx et tabhz
+c     I   ibar       int*4    type de trace :
+c     I                            - 1 = ligne continue
+c     I                            - 2 = barre
+c     I   legh       char*50  legende du hyetogramme  
+c     *----------------------------------------------------------------*
+
+      integer*4         inumh,ibar,n,icolhyeto
+      real*4            tabhx(1),tabhz(1),pourc
+      character*50      legh     
+
+      return
+      end */
+
+void go_()
+{
+  char tit[50];
+
+  
+  return;
+}
