63e9a5030fb49589bbf86d6188d3b7ae85e7c397
Markus Neteler <neteler@gmail.com>
2000-06-30 14:22:19 +0000

    new try to export areas instead of lines
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1115 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/mapdev/v.out.shape/write_areas.c
+++ src/mapdev/v.out.shape/write_areas.c
@@ -1,96 +1,125 @@
 #include "gis.h"
-#include "Vect.h"
 #include "digit.h"
 #include "gtoa.h"
+#include "Vect.h"
+#include <stdio.h>
+
+/*
+ * modified for v.out.shape
+ *   - Markus Neteler 6/2000
+ */
+
+/****************************************************************************
+*  	    	   NECTEC -- High Performance Computing Center --
+*      	    	    	    	Bangkok, Thailand
+*
+* PROJECT: 	Grass tool for v.out.poly
+* FILENAME:	main.c
+* AUTHOR(S):	Justin Hickey - jhickey@hpcc.nectec.or.th
+* PURPOSE: 	To output the polygons of a vector file to a text file. The
+*   	    	format of the output file is as follows:
+*   	    	    A	numPoints   catNumber	catLabel
+*   	    	      x y
+*   	    	      x y
+*   	    	    END
+*   	    	The "A" character indicates the following data is an area just
+*   	    	like the v.out.ascii output. It is redundant since only areas
+*   	    	are output but it provides a marker for scripts to indicate the
+*   	    	start of the polygon. The "numPoints" value is the number of
+*   	    	points in the polygon. The "catNumber" value is the category
+*   	    	number assigned to the polygon and the "catLabel" is the
+*   	    	category label assigned to the polygon. These fields are
+*   	    	delimited by tabs. The "x" and "y" values are the coordinates
+*   	    	of each point in the polygon, which are reversed from the
+*   	    	output of the v.out.ascii command. Note that "x" corresponds to
+*   	    	the easting, while "y" corresponds to the northing. The "END"
+*   	    	value signifies the end of the polygon. This program does not
+*   	    	have an interactive mode and takes both an input file and an
+*   	    	output file for arguments.
+* CMD LINE ARG: Input vector file and output text file
+* OUTPUT:    	Text file containing information about polygons
+* DATE CREATED: Mar 05 1999
+* HISTORY:
+*
+*   Mar 05 1999 Finished the program
+*
+*   Mar 08 1999 Refined the output format of the x and y points. Modified by
+*   	    	Justin Hickey
+*
+*   Mar 12 1999 Added the code to output the END marker and switched the order
+*   	    	of the x and y output. Modified by Justin Hickey
+*
+****************************************************************************/
 
 int 
-write_areas (char *name, char *mapset, struct Map_info *map, FILE *lines_file, FILE *points_file, FILE *text_file)
+write_areas (char *name, char *mapset, struct Map_info map, FILE *lines_file, FILE *points_file, FILE *text_file)
 {
-	P_AREA *Area;
-	P_ISLE *Isle;
-	double ax, ay;
-	double *x, *y;
-	int area;
-	int type;
-	int offset;
 	int count;
-	int attflag, NumPoints ;
-	char *att;
-	int alloc_points;
-	int n_points;
-	int n_atts;
-	int i,n,catflag= -1;
-	plus_t zero = 0;
-	struct line_pnts *Gpoints;
+	struct line_pnts points;
 	struct Categories cats;
-	int n_lines;
 	int NumAreas;
+    int     	    	catNum;     	/* category number */
+    int     	    	haveCat;    	/* flag to indicate category file */
+    int     	    	i, j;  	    	/* counters */
+    char    	    	*catLabel;  	/* category label */
+
 
 #ifdef DEBUG
 	fprintf (stdout,"write_areas %s %s\n",name,mapset);
 #endif
 
-	catflag = G_read_vector_cats(name,mapset,&cats);
-		
-        if(catflag == -1)
-	{
+    if (G_read_vector_cats(name, mapset, &cats) < 0)
+    {
+    	haveCat = 0;
 #ifdef DEBUG
-		fprintf (stdout,"   error %d reading vector cats\n",catflag);
+	fprintf (stdout,"   error %d reading vector cats\n",catflag);
 #  endif
-	}
 
-	/* LOOP through all areas in the dig file */
-	count=0;
-#ifdef OLD_LIB
-	NumAreas = dig_P_num_areas(map);
-#else
-	NumAreas = V2_num_areas(map);
-#endif /*OLD_LIB*/
+    }
+    else
+    {
+    	haveCat = 1;
+fprintf(stderr, "Reading cats...\n");
+    }
 
-	for (area = 1 ; area <= NumAreas; area++)
-	{
-		Area = &(map->Area[area]);
-		attflag=0;
-		if (Area->att || area == 1)
-			n_atts=1;
-		else
-			n_atts=0;
+    /* LOOP through all areas in the dig file */
 
-		/* write label-point coordinates to points_file */
-		if (AREA_LABELED (Area))
-		{
-			count++;
-			attflag=1;
-			ax = map->Att[Area->att].x;
-			ay = map->Att[Area->att].y;
-			fprintf(points_file,"%d%c%f%c%f\n",count, separator, 
-					    ax, separator, ay);
-			lab_flg=1;
-		}
+    NumAreas = V2_num_areas(&map);
 
-		/* write category number and attribute text to text_file */
-		if (attflag==1 && catflag!=-1)
-		{
- 		   /* get attribute text */
-		   i=0; 
-		   att = G_get_cat(map->Att[Area->att].cat, &cats);
-		   if(space)
-		   {
-		      while(att[i])
-		      {
-			 if(att[i] == ' ') att[i] = '_';
-			 i++;
-                      }
-                   }
-		   fprintf(text_file,"%d%c%d%c%d%c%s\n",count, separator, 
-				    map->Att[Area->att].cat, separator,
-				    count, separator, att);
-			txt_flg=1;
-		}
-	}
+    if (NumAreas > 0)
+    {
+    	/* Go through the areas */
+    	for (i = 1; i <=NumAreas; i++)
+    	{
+fprintf(stderr,"%d\n", i);
+     	    /* Get the points of the polygon */
+    	    Vect_get_area_points(&map, i, &points);
+    	    
+   	    /* Print out the number of points */
+   	    fprintf(lines_file, "A\t%d\t", points.n_points);
+   	    
+   	    /* Get and print the category information */
+    	    if(haveCat)
+    	    {
+    	    	catNum = map.Att[map.Area[i].att].cat;
+    	    	catLabel = G_get_cat(catNum, &cats);
+    	    	
+    	    	fprintf(lines_file, "%d\t%s\n", catNum, catLabel);
+    	    }
+    	    
+    	    /* Print the actual points */
+    	    for (j = 0; j < points.n_points; j++)
+    	    {
+    	    	fprintf(lines_file, " %10.6f %10.6f\n", points.x[j], points.y[j]);
+    	    }
+    	    
+    	    /* Print the end marker */
+    	    fprintf(lines_file, "END\n");
+    	}
+    }
+    
+    /* Print end message */
+    fprintf(stderr, "\nCOMPLETE\n");
 
-	fprintf(points_file,"END\n");
-	fprintf(text_file,"END\n");
-
-  return 0;
+    return 0;
 }
