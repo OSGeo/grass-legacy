2091e3a8eb22f1ec64480196a97c5a843a682b51
Bill Hughes <bhughes@grasshoppernet.com>
2000-07-14 10:11:51 +0000

    *** empty log message ***
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1264 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/include/vect/digit.h
+++ src/include/vect/digit.h
@@ -1,14 +1,6 @@
-#ifndef DIGIT__FOO__
-#define DIGIT__FOO__
-
-#ifndef _STDIO_H
-#include <stdio.h>		/* for structs.h */
-#endif
 #include "dig_head.h"
 #include "dig_defines.h"		/* #define's */
 #include "dig_macros.h"		/* #define Macros */
 #include "dig_structs.h"		/* system structs */
 #include "dig_externs.h"		/* function return value definitions */
 #include "dig_globs.h"
-
-#endif
