fd5cb41566dba0d2579f257cb191446badba05f8
David D. Gray <ddgray@armadce.demon.co.uk>
2000-07-24 20:49:10 +0000

    Removing redundant files
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1307 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/mapdev/v.in.shape/pg_local.h
+++
@@ -1,8 +0,0 @@
-/******************************************************************************
- * pg_local.h
- * protoptypes for local functions required for PostgreSQL processing.
-
- * For Copyright notice, see file pgdump.c  
- */
-
-int PgDumpFromFieldD( const fieldDescript *fd1, const int nfields, const char *table_name );
