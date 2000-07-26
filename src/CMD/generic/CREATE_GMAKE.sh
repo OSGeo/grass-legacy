35553ab774f51b904411559c6976b515583b2da1
Markus Neteler <neteler@gmail.com>
2000-07-26 08:50:57 +0000

    added error check
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1350 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/CMD/generic/CREATE_GMAKE.sh
+++ src/CMD/generic/CREATE_GMAKE.sh
@@ -5,6 +5,10 @@ HEAD_FILE=`dirname $0`"/../head/head"
 # set (and create) UNIX_BIN
 eval `cat ${HEAD_FILE} | grep UNIX_BIN | sed "s/ //g"`
 if [ ! -d $UNIX_BIN ]; then mkdir -p $UNIX_BIN ; fi
+if [ $? != 0 ]; then
+ echo "An error occured. Stop."
+ exit 1
+fi
 
 # create gmake5 script to be used for local compiling
 echo ":"                                   > $UNIX_BIN/gmake5
@@ -15,6 +19,11 @@ echo "HASX=yes"                           >> $UNIX_BIN/gmake5
 echo "HASMotif=no"                        >> $UNIX_BIN/gmake5
 echo "MAKE=$MAKE"                         >> $UNIX_BIN/gmake5
 echo ". $SRC/src/CMD/generic/gmake.sh"    >> $UNIX_BIN/gmake5
+if [ $? != 0 ]; then
+ echo "An error occured. Stop."
+ exit 1
+fi
+
 chmod ugo+x $UNIX_BIN/gmake5
 
 # create gmakelinks script to be used for linking after
