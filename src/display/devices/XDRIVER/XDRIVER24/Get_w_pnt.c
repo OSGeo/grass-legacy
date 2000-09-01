c14a81647ff6494285fb0fdedd8e2bf2feee10e9
Huidae Cho <grass4u@gmail.com>
2000-09-01 19:25:30 +0000

    Button2Mask -> Button3Mask
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1510 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/devices/XDRIVER/XDRIVER24/Get_w_pnt.c
+++ src/display/devices/XDRIVER/XDRIVER24/Get_w_pnt.c
@@ -34,7 +34,7 @@ int Get_location_with_pointer (int *wx, int *wy, int *button)
 	XQueryPointer(dpy, grwin, &root, &child, &rx, &ry, wx, wy, &mask);
 	*button = (mask&Button1Mask ? 1
 			: (mask&Button2Mask ? 2
-				: (mask&Button2Mask ? 3
+				: (mask&Button3Mask ? 3
 					: *button)));
     }else{
 	XWindowEvent(dpy, grwin, ButtonPressMask, &bpevent);
