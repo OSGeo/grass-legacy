ca23556d487ac218f97705ef78bfd3c02f4d111a
bot <grass-svn2git@osgeo.org>
2000-10-05 14:22:26 +0200

    added

---
+++ src.alpha/raster/r.fuzzy/fuzzy/include/definitions.h
@@ -0,0 +1,33 @@
+/**************************************************************************/
+/*                  include pour les defines : *.c                        */
+/*                                                                        */
+/*                cree par C. DELON et J.F. SARRAUT                       */
+/*                                                                        */
+/*                   derniere modif : 08/96                               */
+/**************************************************************************/
+#ifndef INT_definitions
+#define INT_definitions
+
+
+#define LONGMOT 50               /* longueur maximale d'un mot lors d'une  */
+                                 /* saisie                                 */
+
+#define longfic 100              /* longueur maximale d'un nom de fichier  */
+
+#define MODE 0644                /* mode d'ouverture d'un fichier lors d'  */
+                                 /* une lecture ou ecriture                */
+
+#define NOMBREELT 1000           /* nombre d'elements maximum que peut     */
+                                 /* contenir la pile                       */
+
+#define nbpoints 21              /* nombre de points pour le dessin d'un   */
+                                 /* nombre flou de type LR                 */
+                                 /* ce nombre doit etre impair !!!!        */
+
+#define PREC 99                  /* nombre de points qui caracterise un nb */
+                                 /* flou apres discretisation              */
+
+#define NBVARMAX 10              /* nombre maximum de variables floues que */
+                                 /* peut contenir une classe floue         */
+
+#endif
