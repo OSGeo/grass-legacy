6af613966b7d8e6ea5f9be71dfc0deb74722b03f
Markus Neteler <neteler@gmail.com>
2000-10-13 15:21:06 +0000

    This module is deprecated in favor of d.site.labels
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1919 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.sites.label/local_proto.h
+++
@@ -1,8 +0,0 @@
-#define SITE_ATTR_CAT 1
-#define SITE_ATTR_STR 2
-#define SITE_ATTR_DBL 3
-#define SITE_ATTR_COORD 4
-#define SITE_ATTR_DIM 5
-
-/* draw_site_label.c */
-int draw_site_label(int, int, int, int, int, int, char *, char *, char *);
