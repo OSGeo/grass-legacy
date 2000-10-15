7e18365369038b1eb5401e707108feb86d898aba
Andreas Lange <andreas.c.lange@gmx.de>
2000-10-15 12:07:20 +0000

    new version, for new cc library
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@1952 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/misc/m.datum.shift/dat.c
+++ src/misc/m.datum.shift/dat.c
@@ -1,17 +1,18 @@
 #include "gis.h"
+#include "CC.h"
 
 char *
-spheroid_list(void)
+datum_list(void)
 {
   int len, n;
   char *list;
   char *name;
   
   len=0;
-  for (n=0; (name = G_ellipsoid_name(n)) != NULL; n++)
+  for (n=0; (name = CC_datum_name(n)) != NULL; n++)
     len += strlen(name)+1;
   list = G_malloc(len);
-  for (n=0; (name = G_ellipsoid_name(n)) != NULL; n++)
+  for (n=0; (name = CC_datum_name(n)) != NULL; n++)
     {
       if (n) 
 	G_strcat (list, ",");
