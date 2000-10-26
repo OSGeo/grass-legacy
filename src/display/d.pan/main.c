deb15ced7ff0f9c7aef944dda12dec1df139240c
Huidae Cho <grass4u@gmail.com>
2000-10-26 22:44:08 +0000

    auto-redraw after selecting center with Markus Neteler.
    D_get_dig_name() in display lib gets current displayed vector mapname.
    For now, only d.vect uses D_set_dig_name() to set dig mapname used by
    D_get_dig_name().
    
    
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@2233 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.pan/main.c
+++ src/display/d.pan/main.c
@@ -12,13 +12,47 @@
 int main (int argc, char **argv)
 {
     int stat;
+    char rast[128], vect[128];
+    char command[128];
     struct Flag *quiet;
-    struct Option *zoom;
+    struct Option *map, *vmap, *zoom;
     double magnify;
+    char *mapset;
 
 /* Initialize the GIS calls */
     G_gisinit(argv[0]) ;
+    R_open_driver();
 
+    if(D_get_cell_name (rast) < 0)
+               *rast = 0;
+    if(D_get_dig_name (vect) < 0)
+               *vect = 0;
+    R_close_driver();
+                    
+    map = G_define_option();
+    map->key = "rast";
+    map->type = TYPE_STRING;
+    if (*rast)
+          map->answer = rast;
+    if (*rast)
+          map->required = NO;
+       else
+          map->required = YES;
+    map->gisprompt = "old,cell,raster" ;
+    map->description = "Name of raster map";
+                                                        
+    vmap = G_define_option();
+    vmap->key = "vector";
+    vmap->type = TYPE_STRING;
+    if (*vect)
+          vmap->answer = vect;
+    if (*vect)
+          vmap->required = NO;
+       else
+          vmap->required = YES;
+    vmap->gisprompt = "old,dig,vector" ;
+    vmap->description = "Name of vector map";
+                                                        
     quiet = G_define_flag();
     quiet->key = 'q';
     quiet->description = "Quiet";
@@ -36,6 +70,20 @@ int main (int argc, char **argv)
 
     sscanf(zoom->answer,"%lf", &magnify); 
 
+/* Make sure map is available */
+    if (map->answer == NULL) exit(0);
+       mapset = G_find_cell2 (map->answer, "");
+    if (mapset == NULL)
+    {
+	char msg[256];
+	sprintf(msg,"Raster file [%s] not available", map->answer);
+	G_fatal_error(msg) ;
+    }
+
+    /* if map was found in monitor: */
+    if (*rast || *vect) 
+       quiet->answer=1;
+        
     R_open_driver();
 
     D_setup(0);
@@ -49,5 +97,20 @@ int main (int argc, char **argv)
 
     R_close_driver();
 
+/* Redraw raster map */
+    if (*rast)
+    {
+      sprintf(command, "d.erase; d.rast map=%s", map->answer);
+      system(command);
+    }
+
+/* Redraw vector map */
+    if (*vect)
+    {
+      sprintf(command, "d.vect map=%s", vmap->answer);
+      system(command);
+    }
+    
     exit(stat);
 }
+
