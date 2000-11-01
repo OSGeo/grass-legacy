e8717677d10fc4d05261ac6869e0db9d7bee0f24
Huidae Cho <grass4u@gmail.com>
2000-11-01 10:00:48 +0000

    added sites list map region
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@2374 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/general/g.region/inter/main.c
+++ src/general/g.region/inter/main.c
@@ -20,6 +20,7 @@ main (int argc, char *argv[])
     int modify_db();
     int from_cellhd();
     int from_vect();
+    int from_sites();
     int from_view();
     char *G_database_projection_name();
     char *G__get_window();
@@ -74,12 +75,13 @@ main (int argc, char *argv[])
 
 	fprintf (stderr, "\n\nPlease select one of the following options\n\n");
 	fprintf (stderr, "   Current Region                           Region Database\n\n");
-	fprintf (stderr, "1  Modify current region directly        7  Save current region in the database\n");
-	fprintf (stderr, "2  Set from default region               8  Create a new region\n");
-	fprintf (stderr, "3  Set from a database region            9  Modify an existing region\n");
+	fprintf (stderr, "1  Modify current region directly        8  Save current region in the database\n");
+	fprintf (stderr, "2  Set from default region               9  Create a new region\n");
+	fprintf (stderr, "3  Set from a database region            0  Modify an existing region\n");
 	fprintf (stderr, "4  Set from a raster map\n");
 	fprintf (stderr, "5  Set from a vector map\n");
-	fprintf (stderr, "6  Set from 3d.view file\n");
+	fprintf (stderr, "6  Set from a sites list map\n");
+	fprintf (stderr, "7  Set from 3d.view file\n");
 	fprintf (stderr, "\n");
 	fprintf (stderr, "RETURN to quit\n\n");
 	fprintf (stderr, "> ");
@@ -93,10 +95,11 @@ main (int argc, char *argv[])
 	else if (selection("3"))   option = cur_from_db;
 	else if (selection("4"))   option = from_cellhd ;
 	else if (selection("5"))   option = from_vect ;
-	else if (selection("7"))   option = cur_to_db;
-	else if (selection("8"))   option = new_db;
-	else if (selection("9"))   option = modify_db;
-	else if (selection("6"))   option = from_view;
+	else if (selection("6"))   option = from_sites ;
+	else if (selection("7"))   option = from_view;
+	else if (selection("8"))   option = cur_to_db;
+	else if (selection("9"))   option = new_db;
+	else if (selection("0"))   option = modify_db;
 
 	else
 	{
