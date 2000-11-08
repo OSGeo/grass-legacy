8257880ef8791a0931d7a353c8e66434f589df99
Justin Hickey <jhickey@hpcc.nectec.or.th>
2000-11-08 09:17:24 +0000

    Made it more ANSI C complient but still needs work
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@9819 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/ogsf/GV2.c
+++ lib/ogsf/GV2.c
@@ -1,3 +1,7 @@
+/*
+* $Id$
+*/
+
 /*  GV.c 
     Bill Brown, USACERL  
     October 1993
@@ -11,383 +15,425 @@
 #define TRACE_GV_FUNCS
 #endif
 
-geovect *gv_get_new_vect();
-geovect *gv_get_vect();
-
 static int Vect_ID[MAX_VECTS];
 static int Next_vect = 0;
 
 /***********************************************************************/
-int
-GV_vect_exists(id)
-int id;
+int GV_vect_exists(int id)
 {
-int i, found=0;
+    int i, found=0;
 
-#ifdef TRACE_GV_FUNCS
-Gs_status("GV_vect_exists");
-#endif
-
-    if(NULL == gv_get_vect(id)) 
-	return(0);
-    for(i=0; i<Next_vect && !found; i++){
-	if(Vect_ID[i] == id)
-	    found = 1;
+    #ifdef TRACE_GV_FUNCS
+    {
+    	Gs_status("GV_vect_exists");
     }
-    return(found);
+    #endif
 
+    if (NULL == gv_get_vect(id))
+    {
+	return(0);
+    }
+    
+    for (i=0; i<Next_vect && !found; i++)
+    {
+	if (Vect_ID[i] == id)
+	{
+	    found = 1;
+	}
+    }
+    
+    return(found);
 }
 
 /***********************************************************************/
-int
-GV_new_vector()
+int GV_new_vector(void)
 {
-geovect *gv, *nv;
-int i;
+    geovect *gv, *nv;
+    int i;
 
-#ifdef TRACE_GV_FUNCS
-Gs_status("GV_new_vector");
-#endif
+    #ifdef TRACE_GV_FUNCS
+    {
+    	Gs_status("GV_new_vector");
+    }
+    #endif
 
-
-    if(Next_vect < MAX_VECTS){
+    if (Next_vect < MAX_VECTS)
+    {
 	nv = gv_get_new_vect();
 	gv_set_defaults(nv);
 	Vect_ID[Next_vect] = nv->gvect_id;
 	++Next_vect;
+	
 	return(nv->gvect_id);
     }
+    
     return(-1);
-
 }
 
 /***********************************************************************/
-int
-GV_num_vects()
+int GV_num_vects(void)
 {
     return(gv_num_vects());
 }
 
 /***********************************************************************/
 /* USER must free!! */
-int *
-GV_get_vect_list(numvects)
-int *numvects;
+int *GV_get_vect_list(int *numvects)
 {
-int i, *ret;
+    int i, *ret;
 
     *numvects = Next_vect;
 
-    if(Next_vect){
-	if(NULL == (ret = (int *)malloc(Next_vect * sizeof(int)))){
+    if (Next_vect)
+    {
+	if (NULL == (ret = (int *)malloc(Next_vect * sizeof(int))))
+	{
 	    fprintf(stderr, "can't malloc\n");
+	
 	    return(NULL);
 	}
+	
 	for(i=0; i<Next_vect; i++)
+	{
 	    ret[i] = Vect_ID[i];
+	}
 	
 	return(ret);
     }
+    
     return(NULL);
-
 }
 
 /***********************************************************************/
-int
-GV_delete_vector(id)
-int id;
+int GV_delete_vector(int id)
 {
-int i, j, found=0;
+    int i, j, found=0;
 
-#ifdef TRACE_GV_FUNCS
-Gs_status("GV_delete_vect");
-#endif
+    #ifdef TRACE_GV_FUNCS
+    {
+    	Gs_status("GV_delete_vect");
+    }
+    #endif
 
-    if(GV_vect_exists(id)){
+    if (GV_vect_exists(id))
+    {
 	gv_delete_vect(id);
-	for(i=0; i<Next_vect && !found; i++){
-	    if(Vect_ID[i] == id){
+	
+	for (i=0; i<Next_vect && !found; i++)
+	{
+	    if (Vect_ID[i] == id)
+	    {
 		found = 1;
-		for(j=i; j<Next_vect; j++){
+		
+		for (j=i; j<Next_vect; j++)
+		{
 		    Vect_ID[j] = Vect_ID[j+1];
 		}
 	    }
 	}
-	if(found){
+	
+	if (found)
+	{
 	    --Next_vect;
 	    return(1);
 	}
     }
+    
     return(-1);
-
 }
 
 /***********************************************************************/
-GV_load_vector(id, filename)
-int id;
-char *filename;
+int GV_load_vector(int id, char *filename)
 {
-geovect *gv;
+    geovect *gv;
 
-/* check to see if handle already loaded, if so - free before loading new */ 
-/* for now, always load to memory */
-/* TODO SOON: load file handle & ready for reading instead of using memory */
-
-    if(NULL == (gv = gv_get_vect(id)))
+    /* check to see if handle already loaded, if so - free before loading */ 
+    /* new for now, always load to memory */
+    /* TODO SOON: load file handle & ready for reading instead of using */
+    /* memory */
+    if (NULL == (gv = gv_get_vect(id)))
+    {
 	return(-1);
+    }
    
-    if(gv->lines) gv_free_vectmem(gv);
-    if(NAME_SIZ > strlen(filename))
+    if (gv->lines)
+    {
+    	gv_free_vectmem(gv);
+    }
+    
+    if (NAME_SIZ > strlen(filename))
+    {
 	strcpy(gv->filename, filename);
+    }
 
-    if(gv->lines = Gv_load_vect(filename, &(gv->n_lines)))
+    if (gv->lines = Gv_load_vect(filename, &(gv->n_lines)))
+    {
 	return(1);
+    }
 
     return(0);
-
 }
 
 /***********************************************************************/
-GV_get_vectname(id, filename)
-int id;
-char *filename;
+int GV_get_vectname(int id, char *filename)
 {
-geovect *gv;
+    geovect *gv;
 
-    if(NULL == (gv = gv_get_vect(id)))
+    if (NULL == (gv = gv_get_vect(id)))
+    {
 	return(-1);
+    }
 
     strcpy(filename, gv->filename);
 
     return(1);
-
 }
    
 /***********************************************************************/
-GV_set_vectmode(id, mem, color, width)
-int id, mem, color, width;
+int GV_set_vectmode(int id, int mem, int color, int width)
 {
-geovect *gv;
+    geovect *gv;
 
-    if(NULL == (gv = gv_get_vect(id))) 
+    if (NULL == (gv = gv_get_vect(id))) 
+    {
 	return(-1);
+    }
+    
     gv->use_mem = mem;
     gv->color = color;
     gv->width = width;
 
     return(1);
-
 }
-/***********************************************************************/
-GV_get_vectmode(id, mem, color, width)
-int id, *mem, *color, *width;
-{
-geovect *gv;
 
-    if(NULL == (gv = gv_get_vect(id))) 
+/***********************************************************************/
+int GV_get_vectmode(int id, int *mem, int *color, int *width)
+{
+    geovect *gv;
+
+    if (NULL == (gv = gv_get_vect(id))) 
+    {
 	return(-1);
+    }
+    
     *mem = gv->use_mem;
     *color = gv->color;
     *width = gv->width;
 
     return(1);
-
 }
-/***********************************************************************/
-/***********************************************************************/
 
-GV_set_trans(id, xtrans, ytrans, ztrans)
-int id;
-float xtrans, ytrans, ztrans;
+/***********************************************************************/
+void GV_set_trans(int id, float xtrans, float ytrans, float ztrans)
 {
-geovect *gv;
+    geovect *gv;
 
-#ifdef TRACE_GV_FUNCS
-Gs_status("GV_set_trans");
-#endif
+    #ifdef TRACE_GV_FUNCS
+    {
+    	Gs_status("GV_set_trans");
+    }
+    #endif
 
     gv = gv_get_vect(id);
-    if(gv){
+    
+    if (gv)
+    {
 	gv->x_trans = xtrans;
 	gv->y_trans = ytrans;
 	gv->z_trans = ztrans;
     }
 
+    return;
 }
 
 /***********************************************************************/
-
-GV_get_trans(id, xtrans, ytrans, ztrans)
-int id;
-float *xtrans, *ytrans, *ztrans;
+int GV_get_trans(int id, float *xtrans, float *ytrans, float *ztrans)
 {
-geovect *gv;
+    geovect *gv;
 
     gv = gv_get_vect(id);
-    if(gv){
+    
+    if (gv)
+    {
 	*xtrans = gv->x_trans;
 	*ytrans = gv->y_trans;
 	*ztrans = gv->z_trans;
+	
 	return(1);
     }
+    
     return(-1);
-
 }
 
 /***********************************************************************/
-GV_select_surf(hv, hs)
-int hv, hs;
+int GV_select_surf(int hv, int hs)
 {
-geovect *gv;
+    geovect *gv;
    
-    if(GV_surf_is_selected(hv, hs)) return(1);
+    if (GV_surf_is_selected(hv, hs))
+    {
+    	return(1);
+    }
 
     gv = gv_get_vect(hv);
-    if(gv && GS_surf_exists(hs)){
+    
+    if (gv && GS_surf_exists(hs))
+    {
 	gv->drape_surf_id[gv->n_surfs] = hs;	
 	gv->n_surfs += 1;
+	
 	return(1);
     }
 
     return(-1);
-
 }
 
 /***********************************************************************/
-GV_unselect_surf(hv, hs)
-int hv, hs;
+int GV_unselect_surf(int hv, int hs)
 {
-geovect *gv;
-int i, j;
+    geovect *gv;
+    int i, j;
 
-    if(!GV_surf_is_selected(hv, hs)) return(1);
+    if (!GV_surf_is_selected(hv, hs))
+    {
+    	return(1);
+    }
 
     gv = gv_get_vect(hv);
-    if(gv){
-	for (i=0; i<gv->n_surfs; i++){
-	    if(gv->drape_surf_id[i] == hs){
-		for (j=i; j<gv->n_surfs-1; j++){
+    
+    if (gv)
+    {
+	for (i=0; i<gv->n_surfs; i++)
+	{
+	    if (gv->drape_surf_id[i] == hs)
+	    {
+		for (j=i; j<gv->n_surfs-1; j++)
+		{
 		    gv->drape_surf_id[j] = gv->drape_surf_id[j+1];
 		}
+		
 		gv->n_surfs -= 1;
+		
 		return(1);
 	    }
 	}
     }
 
     return(-1);
-
 }
 
 /***********************************************************************/
-
-GV_surf_is_selected(hv, hs)
-int hv, hs;
+int GV_surf_is_selected(int hv, int hs)
 {
-int i;
-geovect *gv;
+    int i;
+    geovect *gv;
 
     gv = gv_get_vect(hv);
-    if(gv){
-	for (i=0; i< gv->n_surfs; i++){
+    
+    if (gv)
+    {
+	for (i=0; i< gv->n_surfs; i++)
+	{
 	    if (hs == gv->drape_surf_id[i])
+	    {
 		return(1);
+	    }
 	}
     }
 
     return(0);
-
 }
 
 /***********************************************************************/
-
-GV_draw_vect(vid)
-int vid;
+void GV_draw_vect(int vid)
 {
-geosurf *gs;
-geovect *gv;
-int i;
+    geosurf *gs;
+    geovect *gv;
+    int i;
 
     gv = gv_get_vect(vid);
 
-    if(gv){
-	for (i=0; i<gv->n_surfs; i++){
+    if (gv)
+    {
+	for (i=0; i<gv->n_surfs; i++)
+	{
 	    gs = gs_get_surf(gv->drape_surf_id[i]);
-	    if(gs){
+	
+	    if (gs)
+	    {
 		gvd_vect(gv, gs, 0);
 	    }
 	}
     }
 
+    return;
 }
 
 /***********************************************************************/
-GV_alldraw_vect()
+void GV_alldraw_vect(void)
 {
-int id;
+    int id;
 
-    for(id = 0; id < Next_vect; id++)
+    for (id = 0; id < Next_vect; id++)
+    {
 	GV_draw_vect(Vect_ID[id]);
+    }
 
+    return;
 }
 
-
 /***********************************************************************/
-
-GV_draw_fastvect(vid)
-int vid;
+void GV_draw_fastvect(int vid)
 {
-geosurf *gs;
-geovect *gv;
-int i;
+    geosurf *gs;
+    geovect *gv;
+    int i;
 
     gv = gv_get_vect(vid);
 
-    if(gv){
-	for (i=0; i<gv->n_surfs; i++){
+    if (gv)
+    {
+	for (i=0; i<gv->n_surfs; i++)
+	{
 	    gs = gs_get_surf(gv->drape_surf_id[i]);
-	    if(gs){
+	    
+	    if(gs)
+	    {
 		gvd_vect(gv, gs, 1);
 	    }
 	}
     }
 
-}
-/***********************************************************************/
-GV_Set_ClientData(id, clientd)
-int id;
-void *clientd;
-{
-geovect *gv;
-
-  gv = gv_get_vect(id);
-  if (gv) {
-    gv->clientdata = clientd;
-    return (1);
-  }
-
-  return (-1);
+    return;
 }
 
 /***********************************************************************/
-void *GV_Get_ClientData(id)
-int id;
+int GV_Set_ClientData(int id, void *clientd)
 {
-geovect *gv;
+    geovect *gv;
 
     gv = gv_get_vect(id);
-    if(gv)
+    if (gv)
+    {
+    	gv->clientdata = clientd;
+    	
+	return (1);
+    }
+
+    return (-1);
+}
+
+/***********************************************************************/
+void *GV_Get_ClientData(int id)
+{
+    geovect *gv;
+
+    gv = gv_get_vect(id);
+    
+    if (gv)
+    {
 	return(gv->clientdata);
+    }
 
     return(NULL);
-
 }
-/***********************************************************************/
-
-
-
-
-
-
-
-
--- lib/ogsf/gs_bm.c
+++ lib/ogsf/gs_bm.c
@@ -1,3 +1,7 @@
+/*
+* $Id$
+*/
+
 /*  gs.c
     Bill Brown, USACERL  
     September 1993
@@ -7,207 +11,259 @@
 #include "gsget.h"
 #include <stdio.h>
 
-/*
-*/
-
 /* do combining of bitmaps, make bitmaps from other data w/maskval */
 
-struct BM *
-gsbm_make_mask(frombuff, maskval, rows, cols)
-typbuff *frombuff;
-float maskval;
-int rows, cols;
+struct BM *gsbm_make_mask(typbuff *frombuff, float maskval, int rows, int cols)
 {
-int i, j, ioff;
-struct BM *bm;
-float curval;
+    int i, j, ioff;
+    struct BM *bm;
+    float curval;
     
     bm = BM_create(cols, rows);
 
-    if(frombuff){
-	if(frombuff->bm){
-	    for(i = 0; i < rows; i++){
+    if (frombuff)
+    {
+	if (frombuff->bm)
+	{
+	    for (i = 0; i < rows; i++)
+	    {
 		ioff = i * cols;
-		for(j = 0; j < cols; j++){
+		
+		for (j = 0; j < cols; j++)
+		{
 		    BM_set (bm, j, i, BM_get(frombuff->bm, j, i));
 		}
 	    }
 	}
-	else{
-	    for(i = 0; i < rows; i++){
+	else
+	{
+	    for (i = 0; i < rows; i++)
+	    {
 		ioff = i * cols;
-		for(j = 0; j < cols; j++){
+		
+		for(j = 0; j < cols; j++)
+		{
 		    if (GET_MAPATT(frombuff, (ioff + j), curval))
+		    {
 			BM_set (bm, j, i, (curval == maskval));
+		    }
 		    else
+		    {
 			BM_set (bm, j, i, 0);  /* doesn't mask nulls */
+		    }
 		}
 	    }
 	}
     }
+    
     return(bm);
-
 }
 
-
-gsbm_zero_mask(map)
-struct BM *map;
+void gsbm_zero_mask(struct BM *map)
 {
-int numbytes;
-unsigned char *buf;
+    int numbytes;
+    unsigned char *buf;
 
     numbytes = map->bytes * map->rows;
     buf = map->data;
-    while(numbytes--)
+    
+    while (numbytes--)
+    {
 	*buf++ = 0;
+    }
 
+    return;
 }
-
 
 /* must be same size, ORs bitmaps & stores in bmvar */
-gsbm_or_masks(bmvar, bmcon)
-struct BM *bmvar, *bmcon;
+int gsbm_or_masks(struct BM *bmvar, struct BM *bmcon)
 {
-int i;
-int varsize, consize, numbytes;
+    int i;
+    int varsize, consize, numbytes;
 
     varsize = bmvar->rows * bmvar->cols;
     consize = bmcon->rows * bmcon->cols;
     numbytes = bmvar->bytes * bmvar->rows;
     
-    if(bmcon && bmvar){
-	if(varsize != consize){
+    if (bmcon && bmvar)
+    {
+	if (varsize != consize)
+	{
 	    fprintf(stderr,"bitmap mismatch\n");
+	    
 	    return(-1);
 	}
-	if(bmvar->sparse || bmcon->sparse)
-	    return(-1);
 	
-	for (i=0; i<numbytes; i++){
+	if (bmvar->sparse || bmcon->sparse)
+	{
+	    return(-1);
+	}
+	
+	for (i=0; i<numbytes; i++)
+	{
 	    bmvar->data[i] |= bmcon->data[i];
 	}
+	
 	return (0);
     }
 
+    return(-1);
 }
-
 
 /* must be same size, ORs bitmap with ~bmcon & stores in bmvar */
-gsbm_ornot_masks(bmvar, bmcon)
-struct BM *bmvar, *bmcon;
+int gsbm_ornot_masks(struct BM *bmvar, struct BM *bmcon)
 {
-int i;
-int varsize, consize, numbytes;
+    int i;
+    int varsize, consize, numbytes;
 
     varsize = bmvar->rows * bmvar->cols;
     consize = bmcon->rows * bmcon->cols;
     numbytes = bmvar->bytes * bmvar->rows;
     
-    if(bmcon && bmvar){
-	if(varsize != consize){
+    if (bmcon && bmvar)
+    {
+	if (varsize != consize)
+	{
 	    fprintf(stderr,"bitmap mismatch\n");
+	
 	    return(-1);
 	}
-	if(bmvar->sparse || bmcon->sparse)
-	    return(-1);
 	
-	for (i=0; i<numbytes; i++){
+	if (bmvar->sparse || bmcon->sparse)
+	{
+	    return(-1);
+	}
+	
+	for (i=0; i<numbytes; i++)
+	{
 	    bmvar->data[i] |= ~bmcon->data[i];
 	}
+	
 	return (0);
     }
 
+    return(-1);
 }
+
 /* must be same size, ANDs bitmaps & stores in bmvar */
-gsbm_and_masks(bmvar, bmcon)
-struct BM *bmvar, *bmcon;
+int gsbm_and_masks(struct BM *bmvar, struct BM *bmcon)
 {
-int i;
-int varsize, consize, numbytes;
+    int i;
+    int varsize, consize, numbytes;
 
     varsize = bmvar->rows * bmvar->cols;
     consize = bmcon->rows * bmcon->cols;
     numbytes = bmvar->bytes * bmvar->rows;
     
-    if(bmcon && bmvar){
-	if(varsize != consize)
+    if (bmcon && bmvar)
+    {
+	if (varsize != consize)
+	{
 	    return(-1);
-	if(bmvar->sparse || bmcon->sparse)
-	    return(-1);
+	}
 	
-	for (i=0; i<numbytes; i++){
+	if (bmvar->sparse || bmcon->sparse)
+	{
+	    return(-1);
+	}
+	
+	for (i=0; i<numbytes; i++)
+	{
 	    bmvar->data[i] &= bmcon->data[i];
 	}
+	
 	return (0);
     }
+
+    return(-1);
 }
 
 /* must be same size, XORs bitmaps & stores in bmvar */
-gsbm_xor_masks(bmvar, bmcon)
-struct BM *bmvar, *bmcon;
+int gsbm_xor_masks(struct BM *bmvar, struct BM *bmcon)
 {
-int i;
-int varsize, consize, numbytes;
+    int i;
+    int varsize, consize, numbytes;
 
     varsize = bmvar->rows * bmvar->cols;
     consize = bmcon->rows * bmcon->cols;
     numbytes = bmvar->bytes * bmvar->rows;
     
-    if(bmcon && bmvar){
-	if(varsize != consize)
+    if (bmcon && bmvar)
+    {
+	if (varsize != consize)
+	{
 	    return(-1);
-	if(bmvar->sparse || bmcon->sparse)
-	    return(-1);
+	}
 	
-	for (i=0; i<numbytes; i++){
+	if (bmvar->sparse || bmcon->sparse)
+	{
+	    return(-1);
+	}
+	
+	for (i=0; i<numbytes; i++)
+	{
 	    bmvar->data[i] ^= bmcon->data[i];
 	}
+	
 	return (0);
     }
+    
+    return(-1);
 }
 
-
-
 /***********************************************************************/
-
-gs_update_curmask(surf)
-geosurf *surf;
+int gs_update_curmask(geosurf *surf)
 {
-struct BM *b_mask, *b_topo, *b_color;
-typbuff *t_topo, *t_mask, *t_color;
-int row, col, offset, destroy_ok=1;
-gsurf_att *coloratt;
+    struct BM *b_mask, *b_topo, *b_color;
+    typbuff *t_topo, *t_mask, *t_color;
+    int row, col, offset, destroy_ok=1;
+    gsurf_att *coloratt;
     
-    if(surf->mask_needupdate){
+    if (surf->mask_needupdate)
+    {
 	surf->mask_needupdate = 0; 
 	surf->norm_needupdate = 1; /* edges will need to be recalculated */
 
 	t_topo = gs_get_att_typbuff(surf, ATT_TOPO, 0);
-	if(!t_topo){
+	
+	if (!t_topo)
+	{
 	    surf->mask_needupdate = 1; 
+	
 	    return(0);
 	}
 
-	if(surf->nz_topo || surf->nz_color || 
-			 gs_mask_defined(surf) || t_topo->nm){
+	if (surf->nz_topo || surf->nz_color || 
+			 gs_mask_defined(surf) || t_topo->nm)
+    	{
 	    b_mask = b_topo = b_color = NULL;
 
-	    if(!surf->curmask)
+	    if (!surf->curmask)
+	    {
 		surf->curmask = BM_create(surf->cols, surf->rows);
+	    }
 	    else
+	    {
 		gsbm_zero_mask(surf->curmask);
+	    }
 
-	    if(surf->nz_topo){ /* no_zero elevation */
+	    if (surf->nz_topo)
+	    {
+	    	/* no_zero elevation */
 		b_topo = gsbm_make_mask(t_topo, 0.0, surf->rows, surf->cols);
 	    }
 
 	    /* make_mask_from_color */
-	    if(surf->nz_color && surf->att[ATT_COLOR].att_src == MAP_ATT){ 
+	    if (surf->nz_color && surf->att[ATT_COLOR].att_src == MAP_ATT)
+	    {
 		t_color = gs_get_att_typbuff(surf, ATT_COLOR, 0);
 		coloratt = &(surf->att[ATT_COLOR]);
 		b_color = BM_create(surf->cols, surf->rows);
-		for(row=0; row < surf->rows; row++){
-		    for(col=0; col < surf->cols; col++){
+		
+		for (row=0; row < surf->rows; row++)
+		{
+		    for (col=0; col < surf->cols; col++)
+		    {
 			offset = row*surf->cols + col;
 			BM_set(b_color, col, row, 
 			(NULL_COLOR == gs_mapcolor(t_color, coloratt, offset)));
@@ -215,17 +271,24 @@ gsurf_att *coloratt;
 		}
 	    }
 
-	    if(gs_mask_defined(surf)){
+	    if (gs_mask_defined(surf))
+	    {
 		t_mask = gs_get_att_typbuff(surf, ATT_MASK, 0);
-		if(t_mask->bm){
+		
+		if (t_mask->bm)
+		{
 		    b_mask = t_mask->bm;
 		    destroy_ok = 0;
 		}
-		else{
+		else
+		{
 		    b_mask = BM_create(surf->cols, surf->rows);
 		    gs_set_maskmode((int)surf->att[ATT_MASK].constant);
-		    for(row=0; row < surf->rows; row++){
-			for(col=0; col < surf->cols; col++){
+		    
+		    for (row=0; row < surf->rows; row++)
+		    {
+			for (col=0; col < surf->cols; col++)
+			{
 			    offset = row*surf->cols + col;
 			    BM_set(b_mask, col, row, 
 				    gs_masked(t_mask, col, row, offset));
@@ -234,61 +297,81 @@ gsurf_att *coloratt;
 		}
 	    }
 
-	    if(b_topo){
-fprintf(stderr,"update topo mask\n");
+	    if (b_topo)
+	    {
+    	    	fprintf(stderr,"update topo mask\n");
 		gsbm_or_masks(surf->curmask, b_topo);
 		BM_destroy(b_topo);
 	    }
-	    if(b_color){
-fprintf(stderr,"update color mask\n");
+	    
+	    if (b_color)
+	    {
+    	    	fprintf(stderr,"update color mask\n");
 		gsbm_or_masks(surf->curmask, b_color);
 		BM_destroy(b_color);
 	    }
-	    if(t_topo->nm){
-fprintf(stderr,"update elev null mask\n");
+	    
+	    if (t_topo->nm)
+	    {
+    	    	fprintf(stderr,"update elev null mask\n");
 		gsbm_or_masks(surf->curmask, t_topo->nm);
 	    }
-	    if(b_mask){
-fprintf(stderr,"update mask mask\n");
-		if(t_mask->bm) {
-		    if(surf->att[ATT_MASK].constant) /* invert */
+	    
+	    if (b_mask)
+	    {
+    	    	fprintf(stderr,"update mask mask\n");
+		
+		if (t_mask->bm)
+		{
+		    if (surf->att[ATT_MASK].constant)
+		    {
+		    	/* invert */
 			gsbm_or_masks(surf->curmask, t_mask->bm);
+		    }
 		    else
+		    {
 			gsbm_ornot_masks(surf->curmask, t_mask->bm);
+		    }
 		}
 		else
+		{
 		    gsbm_or_masks(surf->curmask, b_mask);
-		if(destroy_ok)
+		}
+		
+		if (destroy_ok)
+		{
 		    BM_destroy(b_mask);
+		}
 	    }
 
-
-/*
-fprintf(stderr,"masks updated\n");
-*/
-/* TODO: update surf->zminmasked */
+    	    /* TODO: update surf->zminmasked */
 	    return(1);
 	}
-	else if(surf->curmask){
+	else if (surf->curmask)
+	{
 	    BM_destroy(surf->curmask);
 	    surf->curmask = NULL;
 	    surf->zminmasked = surf->zmin;
 	}
     }
+    
     return(0);
 }
 
 /***********************************************************************/
-
-print_bm (bm)
-struct BM *bm;
+void print_bm(struct BM *bm)
 {
-int i,j;
+    int i,j;
 
-            for(i = 0; i < bm->rows; i++){
-                for(j = 0; j < bm->cols; j++){
-                    fprintf(stderr,"%d ", BM_get(bm, j, i));
-                }
-		fprintf(stderr,"\n");
-            }
+    for (i = 0; i < bm->rows; i++)
+    {
+    	for (j = 0; j < bm->cols; j++)
+	{
+            fprintf(stderr,"%d ", BM_get(bm, j, i));
+        }
+	
+	fprintf(stderr,"\n");
+    }
+    
+    return;
 }
