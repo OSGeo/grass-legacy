61bd63049462a93319d6fc2c01b70fe950242df7
Huidae Cho <grass4u@gmail.com>
2000-11-26 19:52:09 +0000

    updated to recreate current window
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@2965 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.pan/main.c
+++ src/display/d.pan/main.c
@@ -5,21 +5,24 @@
  */
 
 #include "gis.h"
-#include "raster.h"
 #include "display.h"
+#include "raster.h"
+#include "Vect.h"
+#define MAIN
 #include "local_proto.h"
 
 int main (int argc, char **argv)
 {
     int stat;
-    char **rast, **vect, **site;
-    int nrasts, nvects, nsites;
     char command[128];
+#ifdef QUIET
     struct Flag *quiet;
+#endif
+    struct Flag *just;
     struct Option *rmap, *vmap, *smap, *zoom;
     double magnify;
     char *mapset;
-    int i;
+    int i, first=1;
 
 /* Initialize the GIS calls */
     G_gisinit(argv[0]) ;
@@ -81,15 +84,6 @@ int main (int argc, char **argv)
     smap->gisprompt = "old,site_lists,sites" ;
     smap->description = "Name of site file";
                                                         
-    if(!rast && !vect && !site)
-    {
-	    rmap->required = YES;
-    }
-
-    quiet = G_define_flag();
-    quiet->key = 'q';
-    quiet->description = "Quiet";
-
     zoom = G_define_option() ;
     zoom->key        = "zoom" ;
     zoom->type       = TYPE_DOUBLE ;
@@ -98,29 +92,131 @@ int main (int argc, char **argv)
     zoom->options    = "0.001-1000.0" ;
     zoom->description= "magnification: >1.0 zooms in, <1.0 zooms out" ;
 
+#ifdef QUIET
+    quiet = G_define_flag();
+    quiet->key = 'q';
+    quiet->description = "Quiet";
+#endif
+
+    just = G_define_flag();
+    just->key = 'j';
+    just->description = "Just redraw given maps using default colors";
+
+    if(!rast && !vect && !site)
+    {
+	    rmap->required = YES;
+	    just->answer = 1;
+    }
+
     if (argc > 1 && G_parser(argc,argv))
 	exit(1);
 
     sscanf(zoom->answer,"%lf", &magnify); 
 
+#ifdef QUIET
+    /* if map was found in monitor: */
+    if (rast || vect || site) 
+       quiet->answer=1;
+#endif
+
+    cmd = NULL;
+    if (!just->answer)
+    {
+        R_open_driver();
+	stat = R_pad_get_item("list", &list, &nlists);
+	R_close_driver();
+	if (stat || !nlists)
+	{
+	    fprintf(stderr, "ERROR: can not get \"list\" items\n");
+	    fprintf(stderr, "-j flag forced\n");
+	    just->answer = 1;
+	}
+	else
+	{
+	    cmd = (char *)G_malloc(strlen(list[0])+1);
+	    strcpy(cmd, list[0]);
+	    for(i=1; i<nlists; i++)
+	    {
+		cmd = (char *)G_realloc(cmd, strlen(cmd)+strlen(list[i])+2);
+		strcat(cmd, "\n");
+		strcat(cmd, list[i]);
+	    }
+	}
+    }
+
+    if (just->answer)
+    {
+    	if (rmap->answers && rmap->answers[0])
+	    rast = rmap->answers;
+	else
+	{
+	    rast = NULL;
+	    nrasts = 0;
+	}
+    	if (vmap->answers && vmap->answers[0])
+	    vect = vmap->answers;
+	else
+	{
+	    vect = NULL;
+	    nvects = 0;
+	}
+    	if (smap->answers && smap->answers[0])
+	    site = smap->answers;
+	else
+	{
+	    site = NULL;
+	    nsites = 0;
+	}
+    }
+
 /* Make sure map is available */
     if (rmap->required == YES && rmap->answers == NULL)
-	exit(0);
-
-    if (rmap->answers)
     {
+	fprintf(stderr, "ERROR: No map is given\n");
+	exit(1);
+    }
+
+    if (rast)
+    {
+        struct Cell_head window;
+
 	if (!nrasts)
 	{
-		for(i=0; rmap->answers[i]; i++);
+		for(i=0; rast[i]; i++);
 		nrasts = i;
 	}
 	for(i=0; i<nrasts; i++){
-    		mapset = G_find_cell2 (rmap->answers[i], "");
+    		mapset = G_find_cell2 (rast[i], "");
     		if (mapset == NULL)
     		{
 			char msg[256];
-			sprintf(msg,"Raster file [%s] not available", rmap->answers[i]);
+			sprintf(msg,"Raster file [%s] not available", rast[i]);
 			G_fatal_error(msg) ;
+		}
+		else
+		{
+	 		if(G_get_cellhd(rast[i], mapset, &window) >= 0)
+			{
+	 			if(first)
+	 			{
+					first = 0;
+					U_east = window.east;
+					U_west = window.west;
+					U_south = window.south;
+					U_north = window.north;
+	 			}
+	 			else
+				{
+					if(window.east > U_east)
+						U_east = window.east;
+					if(window.west < U_west)
+						U_west = window.west;
+					if(window.south < U_south)
+						U_south = window.south;
+					if(window.north > U_north)
+						U_north = window.north;
+				}
+			}
 		}
 	}
     }
@@ -128,20 +224,48 @@ int main (int argc, char **argv)
     if (vmap->required == YES && vmap->answers == NULL)
 	exit(0);
 
-    if (vmap->answers)
+    if (vect)
     {
+        struct Map_info Map;
+
 	if (!nvects)
 	{
-		for(i=0; vmap->answers[i]; i++);
+		for(i=0; vect[i]; i++);
 		nvects = i;
 	}
+        Vect_set_open_level(1);
 	for(i=0; i<nvects; i++){
-    		mapset = G_find_vector2 (vmap->answers[i], "");
+    		mapset = G_find_vector2 (vect[i], "");
     		if (mapset == NULL)
     		{
 			char msg[256];
-			sprintf(msg,"Vector file [%s] not available", vmap->answers[i]);
+			sprintf(msg,"Vector file [%s] not available", vect[i]);
 			G_fatal_error(msg) ;
+		}
+		else
+		{
+			if(Vect_open_old(&Map, vect[i], mapset) == 1)
+			{
+	 			if(first)
+	 			{
+					first = 0;
+					U_east = Map.head.E;
+					U_west = Map.head.W;
+					U_south = Map.head.S;
+					U_north = Map.head.N;
+	 			}
+	 			else
+				{
+					if(Map.head.E > U_east)
+						U_east = Map.head.E;
+					if(Map.head.W < U_west)
+						U_west = Map.head.W;
+					if(Map.head.S < U_south)
+						U_south = Map.head.S;
+					if(Map.head.N > U_north)
+						U_north = Map.head.N;
+				}
+			}
 		}
 	}
     }
@@ -149,28 +273,99 @@ int main (int argc, char **argv)
     if (smap->required == YES && smap->answers == NULL)
 	exit(0);
 
-    if (smap->answers)
+    if (site)
     {
+	FILE *fp;
+	Site *s;
+	int rtype, ndim, nstr, ndec;
+
 	if (!nsites)
 	{
-		for(i=0; smap->answers[i]; i++);
+		for(i=0; site[i]; i++);
 		nsites = i;
 	}
 	for(i=0; i<nsites; i++){
-    		mapset = G_find_file ("site_lists", smap->answers[i], "");
+    		mapset = G_find_sites2 (site[i], "");
     		if (mapset == NULL)
     		{
 			char msg[256];
-			sprintf(msg,"Site file [%s] not available", smap->answers[i]);
+			sprintf(msg,"Site file [%s] not available", site[i]);
 			G_fatal_error(msg) ;
+		}
+		else
+		{
+			if(NULL != (fp = G_fopen_sites_old(site[i], mapset)))
+			{
+				rtype = -1;
+				G_site_describe(fp, &ndim, &rtype, &nstr, &ndec);
+				s = G_site_new_struct(rtype, ndim, nstr, ndec);
+				/*
+				while(G_site_get(fp, s) == 0)
+				{
+				*/
+				while(!feof(fp))
+				{
+					if(G_site_get(fp, s))
+						continue;
+					if(first)
+					{
+						first = 0;
+						U_east = s->east;
+						U_west = s->east;
+						U_south = s->north;
+						U_north = s->north;
+					}
+					else
+					{
+						if(s->east > U_east)
+							U_east = s->east;
+						if(s->east < U_west)
+							U_west = s->east;
+						if(s->north < U_south)
+							U_south = s->north;
+						if(s->north > U_north)
+							U_north = s->north;
+					}
+				}
+
+				/* is 100 enough to contain one point from
+				 * boundary?
+				 */
+				U_east += 100;
+				U_west -= 100;
+				U_south -= 100;
+				U_north += 100;
+
+				G_free(s);
+				fclose(fp);
+			}
 		}
 	}
     }
 
-    /* if map was found in monitor: */
-    if (rast || vect || site) 
-       quiet->answer=1;
-        
+#ifdef BOUNDARY
+    if(!first)
+    {
+    /*
+	    if(U_east == U_west)
+	    {
+		    U_east += 100;
+		    U_west -= 100;
+	    }
+	    if(U_south == U_north)
+	    {
+		    U_south -= 100;
+		    U_north += 100;
+	    }
+    */
+
+	    U_east += 0.05 * (U_east - U_west);
+	    U_west -= 0.05 * (U_east - U_west);
+	    U_south -= 0.05 * (U_north - U_south);
+	    U_north += 0.05 * (U_north - U_south);
+    }
+#endif
+
     R_open_driver();
 
     D_setup(0);
@@ -180,45 +375,22 @@ int main (int argc, char **argv)
     }
 
 /* Do the pan */
+#ifndef QUIET
+    stat = pan(1,magnify) ;
+#else
     stat = pan(quiet->answer,magnify) ;
+#endif
 
     R_close_driver();
 
-    sprintf(command, "d.erase");
-    system(command);
-
-/* Redraw raster map */
     if (rast)
-    {
-      for(i=0; i<nrasts; i++)
-      {
-      	sprintf(command, "d.rast -o map=%s", rmap->answers[i]);
-      	system(command);
-      }
       R_pad_freelist(rast, nrasts);
-    }
 
-/* Redraw vector map */
     if (vect)
-    {
-      for(i=0; i<nvects; i++)
-      {
-      	sprintf(command, "d.vect map=%s", vmap->answers[i]);
-      	system(command);
-      }
       R_pad_freelist(vect, nvects);
-    }
     
-/* Redraw site map */
     if (site)
-    {
-      for(i=0; i<nsites; i++)
-      {
-      	sprintf(command, "d.sites sitefile=%s", smap->answers[i]);
-      	system(command);
-      }
       R_pad_freelist(site, nsites);
-    }
     
     exit(stat);
 }
