c827ccab59620baf31b4b63e522a30a8acb6de6d
Markus Neteler <neteler@gmail.com>
2000-12-06 13:34:27 +0000

    moved to src.nonGPL
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@3171 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/libes/geom/optri/local_proto.h
+++
@@ -1,4 +0,0 @@
-/* heur.height.c */
-int hhHeightLT0(graphType *, indexType, indexType, indexType);
-int hhHeightLT(graphType *, indexType, indexType, indexType, indexType, indexType, indexType);
-void hhPrintHeight(FILE *, indexType, coordType, coordType, coordType, indexType, coordType, coordType, coordType, indexType, coordType, coordType, coordType);
