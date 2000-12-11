e098c2d8569802b4bcdaaee4f6762c40b1cf22db
Markus Neteler <neteler@gmail.com>
2000-12-11 11:48:49 +0000

    parser supported version (not finished yet)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@3233 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ src/raster/r.support/cmd/check_new.c
@@ -0,0 +1,6 @@
+#include "gis.h"
+int 
+check_new_compressed (struct Cell_head *cellhd, int fd)
+{
+    return 1;
+}
---
+++ src/raster/r.support/cmd/check_old.c
@@ -0,0 +1,6 @@
+#include "gis.h"
+int 
+check_old_compressed (struct Cell_head *cellhd, int fd)
+{
+    return 1;
+}
