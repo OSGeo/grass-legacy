ba61710114d7e185d20e0d170f0db9f0354fc978
Bob Covill <bcovill@tekmap.ns.ca>
2001-02-08 20:57:32 +0000

    Added routine for high res output
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10013 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/ogsf/gsd_img_ppm.c
+++ lib/ogsf/gsd_img_ppm.c
@@ -30,6 +30,59 @@ int GS_write_ppm(char *name)
 
     gsd_getimage(&pixbuf, &xsize, &ysize);
 
+
+    if (NULL == (fp = fopen (name, "w")))
+    {
+	fprintf (stderr, "Cannot open file for output\n"),exit(1);
+    }
+
+    fprintf(fp, "P6 %d %d 255\n", xsize, ysize);
+
+    for (y=ysize-1; y>=0; y--)
+    {
+	for(x=0; x<xsize; x++)
+	{
+	    if (!swapFlag)
+	    {
+		/* big endian: SUN et al. */
+		rbuf[x] = (pixbuf[y*xsize + x] & 0xFF000000)>>24;
+		gbuf[x] = (pixbuf[y*xsize + x] & 0x00FF0000)>>16;
+		bbuf[x] = (pixbuf[y*xsize + x] & 0x0000FF00)>>8;
+	    }
+	    else
+	    {
+		/* little endian: Linux et al. */
+	        rbuf[x] = (pixbuf[y*xsize + x] & 0x000000FF);
+        	gbuf[x] = (pixbuf[y*xsize + x] & 0x0000FF00)>>8;
+                bbuf[x] = (pixbuf[y*xsize + x] & 0x00FF0000)>>16;
+	    }
+
+	    fputc((int)rbuf[x], fp);
+	    fputc((int)gbuf[x], fp);
+	    fputc((int)bbuf[x], fp);	
+	}
+
+    }
+    free(pixbuf);
+    fclose (fp);
+
+    return(0);
+}
+
+int GS_write_zoom(char *name, unsigned int xsize, unsigned int ysize)
+{
+    int y, x;
+    FILE *fp;
+    unsigned long *pixbuf;
+    char all_buf[3];
+    int swapFlag;
+
+
+    /* endian test */
+    swapFlag = G_is_little_endian();    
+
+    gsd_writeView(&pixbuf, xsize, ysize);
+
     if (NULL == (fp = fopen (name, "w")))
     {
 	fprintf (stderr, "Cannot open file for output\n"),exit(1);
