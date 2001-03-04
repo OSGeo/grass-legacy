f2870ecd4ebba3205c5d553f876d332ff59f2d9a
Huidae Cho <grass4u@gmail.com>
2001-03-04 14:30:44 +0000

    minor change
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@4079 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.what.sites/local_proto.h
+++ src/display/d.what.sites/local_proto.h
@@ -11,11 +11,6 @@
 #define MAXSITES 25000
 
 
-/*
-GLOBAL double SL_e[MAXSITES];
-GLOBAL double SL_n[MAXSITES];
-GLOBAL char *SL_desc[MAXSITES];
-*/
 GLOBAL char **site;
 GLOBAL int nsites;
 GLOBAL struct Cell_head *Wind;
@@ -23,7 +18,7 @@ GLOBAL int *Snum;
 GLOBAL Site ***CurSites;
 
 
-/* opensite.c */
+/* loadsites.c */
 int load_sites(int, struct Cell_head *, int);
 int site_mem(Site *);
 int compress_cached_site(Site *);
