ed4cc0fa4a326aa8b51281091d99e043afe6a803
Markus Neteler <neteler@gmail.com>
2001-03-11 14:09:58 +0000

    Andrea Aime <aaime@libero.it>:  * o category support bug has been fixed: r.contour now produces also dig_cats  *   file, so d.what.vect no more complain about missing category support.  *   Since a contour level could be floatig point I've inserted in the label  *   the true value, whilst category number remains its integer equivalent;  * o a little trick (derived from GMT contour program) avoids any kind of  *   backtrack, so running v.spag -i is no more needed (I hope): data matrix  *   is parsed, and if a cell whose data matches exactly a contour value its  *   value is added a small quantity (the smaller one that makes cell[i] +  *   small != cell[i], I've taken into consideration double encoding)  * o one can specify a minimum number of point for a contour line to be put  *   into outout, so that small spurs, single points and so on won't be  *   present and the output will be more clear (that's optional anyway);  * o in my opinion there were minor memory handling problems in r.contour,  *   I've corrected them (Head.map_name was not guaranteed to be properly  *   terminated, Points structures in contour function were not  *   deallocated).
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@4174 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/raster/r.contour/local_proto.h
+++ src/raster/r.contour/local_proto.h
@@ -1,8 +1,10 @@
 /* cont.c */
-void contour (double *,int,struct Map_info,DCELL **,struct Cell_head,FILE *,int,int);
+void contour (double *,int,struct Map_info,DCELL **,struct Cell_head,FILE *,int,int,int);
 int checkedge (DCELL,DCELL,double);
 /* line_cent.c */
 int get_line_center(double *, double *, struct line_pnts *);
 /* main.c */
 DCELL **get_z_array (int, int,int, int);
 double *getlevels(struct Option *, struct Option *, struct Option *, struct Option *, struct FPRange *, int *, int);
+void displaceMatrix(DCELL**, int, int, double*, int, int);
+void writeCategorySupport(double* lev, int nlevels, char* vName, char* mName);
