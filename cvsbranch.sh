335abdff8f7cda291712bb19edcd4aed921d1771
Markus Neteler <neteler@gmail.com>
2001-03-12 17:43:51 +0000

    fixed ugly bug
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@4200 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- cvsbranch.sh
+++ cvsbranch.sh
@@ -13,16 +13,15 @@ TAG="testbranch_5.0.0"
 FILE_LIST=`cat src/CMD/lists/GRASS | grep -v '#'`
 
 #list of extra files not in compile list:
-FURTHER_FILES=config.guess \
+FURTHER_FILES="config.guess \
 	config.sub GPL.TXT testgrass.sh COPYING README SUBMITTING INSTALL \
 	WARNING.html install-sh ONGOING REQUIREMENTS README.xdriver \
 	binaryInstall.src README.cygwin Makefile.in AUTHORS BUGS TODO.txt \
-	configure.in configure NEWS.html
+	configure.in configure NEWS.html"
 
 #list of extra directories not in compile list:
-FURTHER_DIRECTORIES=documents ALPHA64 testsuite tools unused
+FURTHER_DIRECTORIES="documents ALPHA64 testsuite tools unused"
 
 
 #tag it...
 cvs tag $TAG  $FURTHER_FILES $FURTHER_DIRECTORIES $FILE_LIST
-
