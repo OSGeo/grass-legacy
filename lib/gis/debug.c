bd693ec773e2071cd80796f8134c36edf7ad476d
Radim Blazek <radim.blazek@gmail.com>
2002-01-06 18:09:57 +0000

    make
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10146 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/gis/debug.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include "gis.h"
+
+/*
+ *
+ * G_debug (int level, char *msg)
+ * 
+ * Print debugging message if environmet variable GRASS_DEBUG_LEVEL
+ * is set to level equal or greater  
+ *
+ * Levels: (recommended levels) 
+ * 1 - message is printed once or few times per module
+ * 3 - each row (raster) or line (vector)
+ * 5 - each cell (raster) or point (vector) 
+*/
+static grass_debug_level = -1;
+
+int G_debug (int level, char *msg,...)
+{
+    char buffer[1000], *lstr;
+    va_list ap;
+   
+    if (grass_debug_level < 0) {
+        lstr =  getenv("GRASS_DEBUG_LEVEL");
+
+        if ( lstr != NULL )
+            grass_debug_level = atoi ( lstr );
+        else
+            grass_debug_level = 0;
+    }
+	
+    if ( grass_debug_level >= level ) {
+        va_start(ap,msg);
+        vsprintf(buffer,msg,ap);
+        va_end(ap);
+        fprintf (stderr, "D%d: %s\n", grass_debug_level, buffer);
+    }
+	
+    return 0;
+}
+
