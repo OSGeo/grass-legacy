7ab6b63f0571fab552de36df20d26fadd1b69950
Markus Neteler <neteler@gmail.com>
2002-01-11 15:12:00 +0000

    Bob Covill and Markus Neteler: added CMD version, updated to GRASS 5 raster management
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@6638 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/imagery/i.rectify3/cmd/exec.c
+++ src/imagery/i.rectify3/cmd/exec.c
@@ -22,7 +22,6 @@ exec_rectify (struct Ref ref)
     char *name;
     char *mapset;
     char *result;
-    char *type;
     int i,n;
     struct Colors colr;
     struct Categories cats;
@@ -34,43 +33,44 @@ exec_rectify (struct Ref ref)
 
 
 /* allocate the output cell matrix */
-    cell_buf = (CELL **) G_calloc (NROWS, sizeof (CELL *));
-    n = NCOLS * sizeof (CELL);
+    cell_buf = (void **) G_calloc (NROWS, sizeof(void *));
+    n = NCOLS * G_raster_size(map_type);
     for (i=0; i < NROWS; i++)
     {
-	cell_buf[i] = (CELL *) G_malloc (n);
+	cell_buf[i] = (void *) G_malloc (n);
+	G_set_null_value(cell_buf[i], NCOLS, map_type);
     }
 
 
 /* go into background */
 /**************  DO NOT FORK OR MAIL ANYTHING 
-/**    fprintf (stderr, "\nYou will receive mail when %s is complete\n",
-/**	G_program_name());
-/**    if (G_fork()) exit(0);
+**    fprintf (stderr, "\nYou will receive mail when %s is complete\n",
+**	G_program_name());
+**    if (G_fork()) exit(0);
 *********************************************/
 
 /* note: all calls to G_tempfile() should happen after the fork */
 
 /* create a mailfile */
 /*******************************************
-/**    mailfile = G_tempfile();
-/**    unlink (mailfile);
-/**    close(creat(mailfile,0666));
+**    mailfile = G_tempfile();
+**    unlink (mailfile);
+**    close(creat(mailfile,0666));
 *******************************************/
 
 /* open stderr to /dev/null so all GRASS error messages will be
  * mailed to the user
  */
 /******************************************
-/**    freopen ("/dev/null","w",stderr);
-/**    freopen ("/dev/null","w",stdout);
+**    freopen ("/dev/null","w",stderr);
+**    freopen ("/dev/null","w",stdout);
 ******************************************/
 
 /* rectify each file */
     for (n = 0; n < ref.nfiles; n++)
     {
 	if ((i = ref_list[n]) < 0)
-	    continue;
+	  continue;
 	name   = ref.file[i].name;
 	mapset = ref.file[i].mapset;
 	result = new_name[n];
@@ -80,13 +80,7 @@ exec_rectify (struct Ref ref)
 	G_suppress_warnings(1);
 	cats_ok = G_read_cats (name, mapset, &cats) >= 0;
 	colr_ok = G_read_colors (name, mapset, &colr) > 0;
-/*
 	hist_ok = G_read_history (name, mapset, &hist) >= 0;
-*/
-	/* Initialze History */
-        type = "raster";
-        G_short_history(name, type, &hist);
-
 	G_suppress_warnings(0);
 
 	time (&start_time);
@@ -110,14 +104,8 @@ exec_rectify (struct Ref ref)
 		G_write_colors (result, G_mapset(), &colr) ;
 		G_free_colors (&colr);
 	    }
-
-                /* Write out History Structure History */
-                sprintf(hist.title, "%s", result);
-                sprintf(hist.datsrc_1, "%s", name);
-                sprintf(hist.edhist[0], "Created from: i.rectify3");
-                hist.edlinecnt = 1;
-                G_write_history (result, &hist) ;
-
+	    if(hist_ok)
+		G_write_history (result, &hist) ;
 	    select_current_env();
 	    time (&rectify_time);
 	    if (compress(result))
@@ -130,9 +118,9 @@ exec_rectify (struct Ref ref)
 	    report (mailfile, name, mapset, result, (long)0, (long)0, 0);
     }
 /******************
-/**    mail (mailfile);
-/**    unlink (mailfile);
-/**    G_done_msg ("Check your mail");
+**    mail (mailfile);
+**    unlink (mailfile);
+**    G_done_msg ("Check your mail");
 ******************/
 
     G_done_msg ("All files have been rectified!");
