b731fbf60be1ddf40610c4be1fd2e24b91b33519
Radim Blazek <radim.blazek@gmail.com>
2002-01-24 18:53:36 +0000

    new
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10188 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ display/d.what.vect/main.c
@@ -0,0 +1,145 @@
+/*
+ * $Id$
+ *
+ * attempt to auto-select vector maps displayed in monitor (like d.zoom)
+ *
+ * d.what.vect
+*/
+
+#define MAIN
+#include <string.h>
+#include "gis.h"
+#include "display.h"
+#include "Vect.h"
+#include "raster.h"
+#include "what.h"
+#include "dbmi.h"
+
+/* Vector map grabbing taken from d.zoom */
+
+int main(int argc, char **argv)
+{
+  struct Flag *once, *terse, *attr;
+  struct Option *opt1;
+  struct GModule *module;
+  char *mapset, *openvect();
+  char temp[128], *str;
+  int i, j, level, width, mwidth;
+  
+  /* Initialize the GIS calls */
+  G_gisinit (argv[0]) ;
+
+  /* have a look if vector maps are already drawn in monitor */
+  if (R_open_driver() != 0)
+    G_fatal_error ("No graphics device selected");
+  
+  if(D_get_dig_list (&vect, &nvects) < 0)
+	vect = NULL;
+  else
+    {
+	vect = (char **)G_realloc(vect, (nvects+1)*sizeof(char *));
+	vect[nvects] = NULL;
+    }
+  R_close_driver();
+
+  once = G_define_flag();
+  once->key = '1';
+  once->description = "Identify just one location";
+  
+  opt1 = G_define_option() ;
+  opt1->key        = "map" ;
+  opt1->type       = TYPE_STRING ;
+  opt1->multiple   = YES;
+  if (vect)
+          opt1->answers = vect;
+  opt1->required   = NO ;
+  opt1->gisprompt  = "old,dig,vector" ;
+  opt1->description= "Name of existing vector map" ;
+  
+  terse = G_define_flag();
+  terse->key = 't';
+  terse->description = "Terse output. For parsing by programs";
+ 
+  attr = G_define_flag();
+  attr->key = 'a';
+  attr->description = "Display attributes";
+  
+  module = G_define_module();
+  module->description = 
+    "Allows the user to interactively query a vector map layer "
+    "at user-selected locations within the current geographic region.";
+
+  if(!vect)
+    {
+    	  opt1->required = YES;
+    }
+    	  	      
+  if((argc > 1 || !vect) && G_parser(argc,argv))
+    exit(-1);
+
+  if (opt1->answers && opt1->answers[0])
+      vect = opt1->answers;
+
+  /* Look at maps given on command line */
+
+  if(vect)
+    {
+      for(i=0; vect[i]; i++);
+      nvects = i;
+
+      for(i=0; i<nvects; i++)
+	{
+	  mapset = openvect(vect[i]);
+	  if(mapset == NULL)
+	    {
+              fprintf (stderr, "Unable to open %s\n", vect[i]) ;
+              exit(1) ;
+	    }
+	}
+
+      Map = (struct Map_info *) G_malloc(nvects * sizeof(struct Map_info));
+    
+      width = mwidth = 0;
+      for(i=0; i<nvects; i++)
+        {
+          str = strchr(vect[i], '@');
+          if(str) j = str - vect[i];
+          else    j = strlen(vect[i]);
+          if(j > width)
+            width = j;
+    
+          mapset = openvect(vect[i]);
+          j = strlen(mapset);
+          if(j > mwidth)
+            mwidth = j;
+    
+          level = Vect_open_old (&Map[i], vect[i], mapset);
+          if (level < 0) {
+    	      sprintf(temp, "Vector file [%s] not available", vect[i]);
+              G_fatal_error (temp);
+    	  }
+    
+          if (level < 2) {
+    	      sprintf(temp, "%s: You must build topology on vector file", vect[i]);
+              G_fatal_error (temp);
+    	  }
+        }
+    }
+
+  if (R_open_driver() != 0)
+    G_fatal_error ("No graphics device selected");
+  D_setup(0);
+
+  what(once->answer, terse->answer, width, mwidth, attr->answer); 
+
+  for(i=0; i<nvects; i++)
+      Vect_close (&Map[i]);
+
+  R_close_driver();
+  R_pad_freelist(vect, nvects);
+  
+  exit(0);
+}
+
+
+
---
+++ display/d.what.vect/what.c
@@ -0,0 +1,233 @@
+#include <string.h>
+#include "gis.h"
+#include "raster.h"
+#include "display.h"
+#include "Vect.h"
+#include "what.h"
+
+static int nlines = 50;
+
+#define WDTH 2
+
+int what(int once, int terse, int width, int mwidth,
+	 int dodbmi)
+{
+  int lcat, acat ;
+  int row, col;
+  int nrows, ncols;
+  struct Cell_head window;
+  int screen_x, screen_y ;
+  double east, north ;
+  int button ;
+  char east_buf[40], north_buf[40];
+  double sq_meters;
+  double x1, y1, x2, y2, z;
+  int notty;
+  double maxdist;
+  int getz;
+  struct Plus_head *Plus ;
+  struct field_info *Fi;
+  
+  P_LINE_2D *Line ;
+  P_AREA_2D *Area ;
+  plus_t line, area, centroid ;
+  int i, j;
+  struct line_pnts * Points;
+  struct line_cats *Cats;
+  char temp[512], *str;
+  
+  
+  G_get_set_window (&window);
+  G_begin_polygon_area_calculations();
+  nrows = window.rows;
+  ncols = window.cols;
+  
+  screen_x = ((int)D_get_d_west() + (int)D_get_d_east()) / 2 ;
+  screen_y = ((int)D_get_d_north() + (int)D_get_d_south()) / 2 ;
+  
+  Points = Vect_new_line_struct();
+  Cats = Vect_new_cats_struct ();
+
+  if (!isatty(fileno(stdout)))
+    notty = 1;
+  else 
+    notty = 0;
+  
+  do
+    {
+      if (!terse)
+	show_buttons (once);
+      R_get_location_with_pointer(&screen_x, &screen_y, &button) ;
+      if (!once)
+	{
+	  if (button == 3) break;
+	  if (button == 2) continue;
+	}
+      
+      east  = D_d_to_u_col((double)screen_x) ;
+      north = D_d_to_u_row((double)screen_y) ;
+
+      row = (window.north - north) / window.ns_res ;
+      col = (east - window.west) / window.ew_res ;
+      if (row < 0 || row >= nrows) continue;
+      if (col < 0 || col >= ncols) continue;
+      
+      x1 = D_d_to_u_col ((double)(screen_x-WDTH));
+      y1 = D_d_to_u_row ((double)(screen_y-WDTH));
+      x2 = D_d_to_u_col ((double)(screen_x+WDTH));
+      y2 = D_d_to_u_row ((double)(screen_y+WDTH));
+
+      maxdist = abs ( D_d_to_u_col(11) - D_d_to_u_col(1) );
+      printf ("maxdist = %f\n", maxdist);
+      
+      for(i=0; i<nvects; i++)
+        {
+          Plus = &(Map[i].plus);
+          line = Vect_find_line (&Map[i], east, north, 
+		                    GV_POINT|GV_LINE|GV_BOUNDARY|GV_CENTROID,
+				    maxdist);
+          area = Vect_find_area (&Map[i], east, north) ;
+	  getz = Vect_tin_get_z (&Map[i], east, north, &z, NULL, NULL);
+          
+	  if(!i)
+	    {
+              G_format_easting(east, east_buf, G_projection());
+              G_format_northing(north, north_buf, G_projection());
+              fprintf(stdout,"\n%s(E) %s(N)\n", east_buf, north_buf);
+              if (notty)
+                fprintf(stderr,"\n%s(E) %s(N)\n", east_buf, north_buf);
+              nlines++ ;
+	    }
+    
+	  strcpy(temp, vect[i]);
+	  if((str = strchr(temp, '@'))) *str = 0;
+
+	  fprintf(stdout, "%*s in %-*s  ", width, temp, mwidth,
+			  G_find_vector2(vect[i], ""));
+	  if (notty)
+	    fprintf(stderr, "%*s in %-*s  ", width, temp, mwidth,
+			  G_find_vector2(vect[i], ""));
+	  nlines++ ;
+
+          if (line + area == 0)
+            {
+              fprintf (stdout,"Nothing Found.\n") ;
+              if (notty)
+                fprintf (stderr,"Nothing Found.\n") ;
+              nlines++ ;
+            }
+           
+          if (line == 0)
+            // fprintf (stdout,"Line not found.\n") 
+	    ;
+          else 
+            {
+              fprintf (stdout,"Line %d\n", line);
+	      
+	      V2_read_line (Map, Points, Cats, line );
+              for(j = 0; j < Cats->n_cats; j++ ) {
+                  fprintf (stdout,"field = %d category = %d\n", Cats->field[j],
+			             Cats->cat[j]);
+	          if(dodbmi) {
+		      Fi = Vect_get_field_info(Map[i].name, Map[i].mapset, Cats->field[j]);    
+		      if (Fi == NULL) {
+			  G_warning ("Cannot read field info");
+		      } else {
+                          fprintf (stdout,"driver=%s, database=%s, table=%s, key=%s\n", 
+    			           Fi->driver, Fi->database, Fi->table, Fi->key);
+	                  disp_attr( Fi->driver, Fi->database, Fi->table, 
+				       Fi->key, Cats->cat[j] );
+			  G_free ( Fi );
+		      }
+		  }
+	      }
+              nlines++ ;
+            }
+          
+          if (area == 0)
+            {
+              //fprintf (stdout,"Area not found.\n") 
+	      ;
+            }
+          else 
+            {
+	      if (Map->head.with_z && getz )	
+                  fprintf (stdout,"Area %d z = %f\n", area, z);
+	      else
+                  fprintf (stdout,"Area %d\n", area);
+
+	      centroid = Vect_get_area_centroid ( Map, area );
+	      if ( centroid > 0 ) {
+		  V2_read_line (Map, Points, Cats, centroid );
+		  for(j = 0; j < Cats->n_cats; j++ ) {
+		      fprintf (stdout,"field = %d category = %d\n", Cats->field[j],
+					 Cats->cat[j]);
+		      if(dodbmi) {
+			  Fi = Vect_get_field_info(Map[i].name, Map[i].mapset, Cats->field[j]);    
+			  if (Fi == NULL) {
+			      G_warning ("Cannot read field info");
+			  } else {
+			      fprintf (stdout,"driver=%s, database=%s, table=%s, key=%s\n", 
+				       Fi->driver, Fi->database, Fi->table, Fi->key);
+			      disp_attr( Fi->driver, Fi->database, Fi->table, 
+					   Fi->key, Cats->cat[j] );
+			      G_free ( Fi );
+			  }
+		      }
+		  }
+	      }
+	      
+              Vect_get_area_points(&Map[i], area, Points);
+              sq_meters = G_area_of_polygon(Points->x, Points->y, Points->n_points);
+              
+	      /* substructing island areas */
+	      Area = Plus->Area_2d[area];
+              for(j = 0;j<Area->n_isles;j++)
+                {
+                  Vect_get_isle_points(&Map[i], Area->isles[j], Points);
+                  sq_meters -= G_area_of_polygon(Points->x, Points->y, Points->n_points);
+                }
+              
+              fprintf (stdout,"Size - Sq Meters: %.3f\t\tHectares: %.3f\n",
+            	   sq_meters, (sq_meters/10000.) );
+              
+              fprintf (stdout,"           Acres: %.3f\t\tSq Miles: %.4f\n",
+            	   ((sq_meters*10.763649)/43560.),((sq_meters*10.763649)/43560.)/640. );
+              if (notty) {
+                fprintf (stderr,"Size - Sq Meters: %.3f\t\tHectares: %.3f\n",
+            	     sq_meters, (sq_meters/10000.) );
+                
+                fprintf (stderr,"           Acres: %.3f\t\tSq Miles: %.4f\n",
+            	     ((sq_meters*10.763649)/43560.),((sq_meters*10.763649)/43560.)/640. );
+              }
+              nlines += 3 ;
+            }
+          fflush(stdout);
+        }
+    }while (!once);
+  Vect_destroy_line_struct(Points);
+  
+  return 0;
+}
+
+/* TODO */
+int 
+show_buttons (int once)
+{
+  if (once)
+    {
+      fprintf (stderr, "\nClick mouse button on desired location\n\n");
+      nlines = 3;
+    }
+  else if (nlines >= 18)      /* display prompt every screen full */
+    {
+      fprintf (stderr, "\n");
+      fprintf (stderr, "Buttons\n");
+      fprintf (stderr, " Left:  what's here\n");
+      fprintf (stderr, " Right: quit\n");
+      nlines = 4;
+    }
+  
+  return 0;
+}
+
