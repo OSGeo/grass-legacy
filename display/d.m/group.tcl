61050277d13f1e8ef4555dc10670cd24517c23bc
Radim Blazek <radim.blazek@gmail.com>
2002-02-27 13:45:26 +0000

    new d.m
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10266 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ display/d.m/group.tcl
@@ -0,0 +1,85 @@
+
+namespace eval DmGroup {
+    variable array opt 
+    variable count 1 
+}
+
+
+proc DmGroup::create { tree parent } {
+    variable opt
+    variable count 
+
+    set fon [font create -size 10]
+    set frm [ frame .groupicon$count ]
+    set check [checkbutton $frm.check -text "" -font $fon \
+                           -variable DmGroup::opt($count,_check) \
+                           -height 1]
+    set image [label $frm.image -image [Bitmap::get folder] ]
+    pack $check $image -side left
+
+    set node "group:$count"
+    $tree insert end $parent $node \
+	-text      "group $count" \
+	-window    $frm \
+	-drawcross auto \
+        -open 1
+	
+    set opt($count,_check) 1
+    set opt($count,treeimagepath) $image
+
+    incr count
+
+    return $node
+}
+
+proc DmGroup::save { tree depth node } {
+    variable opt
+
+    if { $node != "root" } {
+	set id [Dm::node_id $node] 
+       Dm::rc_write $depth _check $opt($id,_check)
+    }
+
+    foreach n [$tree nodes $node] {
+        Dm::save_node $depth $n
+    }
+
+}
+
+proc DmGroup::display { node } {
+    variable opt
+
+    set tree $Dm::tree
+    if { $node != "root" } {
+	set id [Dm::node_id $node] 
+        if { ! ( $opt($id,_check) ) } { return }
+    }
+
+    foreach n [$tree nodes $node] {
+        Dm::display_node $n
+    }
+
+}
+
+proc DmGroup::set_option { node key value } {
+    variable opt
+ 
+    set id [Dm::node_id $node]
+ 
+    set opt($id,$key) $value
+}
+
+proc DmGroup::open { id } {
+    variable opt
+
+
+    $DmGroup::opt($id,treeimagepath) configure -image [Bitmap::get openfold]
+
+}
+
+proc DmGroup::close { id } {
+    variable opt
+
+    $DmGroup::opt($id,treeimagepath) configure -image [Bitmap::get folder]
+}
+
