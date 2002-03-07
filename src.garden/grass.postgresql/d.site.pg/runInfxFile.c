2a077d26251c6f9127c6990a7f5ddae2214ae633
Alex Shevlakov <sixote@yahoo.com>
2002-03-07 15:41:12 +0000

    Added site_list category definition through pg_type selection
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7033 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.garden/grass.postgresql/d.site.pg/runInfxFile.c
+++ src.garden/grass.postgresql/d.site.pg/runInfxFile.c
@@ -1,3 +1,5 @@
+/*added pg_type selection for site_list --alex 03/2002*/
+
 #include <stdlib.h>
 #include <string.h>
 #include "gis.h"
@@ -27,6 +29,7 @@ int runInfxFile(SQL_stmt, map,  plotargs )
     PGresult *res;
     char    *pghost;
     Site *site;
+    int ftype=0;
     
     i = 1;
     retval  = 0 ;
@@ -76,6 +79,44 @@ int runInfxFile(SQL_stmt, map,  plotargs )
     }
     
     nflds = PQnfields (res);
+    
+      if ( nflds == 3 ) {
+      ftype = PQftype(res,2);
+      switch (ftype) {
+			
+			case 1042:
+			break;
+			
+			case 1043:
+			break;
+			
+			case 18:
+			break;
+						
+			case 25:
+			break;
+
+
+			case 20:
+			break;
+
+			case 21:
+			break;
+
+			case 23:
+			break;
+			
+			case 700:
+			break;
+			
+			case 701:
+			break;
+			
+			default:
+			printf("Can not use the field type id %d for category\n",ftype);
+		}
+	}
+
     for ( i=0; i < PQntuples(res); i++)  {
       strcpy (buf1, PQgetvalue (res, i, 0));
       strcpy (buf2, PQgetvalue (res, i, 1));
@@ -83,24 +124,72 @@ int runInfxFile(SQL_stmt, map,  plotargs )
       y = atof (buf2);
   /* use ascii buffer to Postgres until MSB / LSB issues can be determined */
   /* cfa 11/98    */
-  
-      if ( nflds == 3 ) {
-      
-      strcpy (buf3, PQgetvalue (res, i, 2));
-      if (strlen(buf3) == 0) strcpy(buf3,"no data");
-      }
+
       retval = plotsite (x,y,icon,size);
       if (retval != 0) {
       	printf(_("Display error(exiting..)\n"));
       	exit (-1);
       }
-      if (map) {
+      if (map ) {
+	if (ftype) {
+		
+		strcpy (buf3, PQgetvalue (res, i, 2));
 
-      	site = G_site_new_struct (-1, 2, 1, 0);
+		switch (ftype) {
+
+/*char, bpchar,varchar,text*/
+			case 18:
+			site = G_site_new_struct (-1, 2, 1, 0);
+			if (strlen(buf3) == 0) strcpy(buf3,"no data");
+			G_strncpy (site->str_att[0], buf3, strlen(buf3));
+			break;
+			case 1042:
+			site = G_site_new_struct (-1, 2, 1, 0);
+			if (strlen(buf3) == 0) strcpy(buf3,"no data");
+			G_strncpy (site->str_att[0], buf3, strlen(buf3));
+			break;
+			case 1043:
+			site = G_site_new_struct (-1, 2, 1, 0);
+			if (strlen(buf3) == 0) strcpy(buf3,"no data");
+			G_strncpy (site->str_att[0], buf3, strlen(buf3));
+			break;
+			case 25:
+			site = G_site_new_struct (-1, 2, 1, 0);
+			if (strlen(buf3) == 0) strcpy(buf3,"no data");
+			G_strncpy (site->str_att[0], buf3, strlen(buf3));
+			break;
+/*int4, int2, int8*/
+			case 20:
+			site = G_site_new_struct (CELL_TYPE, 2, 0, 0);
+			sscanf(buf3,"%d",&site->ccat);
+			break;
+			case 21:
+			site = G_site_new_struct (CELL_TYPE, 2, 0, 0);
+			sscanf(buf3,"%d",&site->ccat);
+			break;
+			case 23:
+			site = G_site_new_struct (CELL_TYPE, 2, 0, 0);
+			sscanf(buf3,"%d",&site->ccat);
+			break;
+/*float4, float8*/
+			case 700:
+			site = G_site_new_struct (FCELL_TYPE, 2, 0, 0);
+			sscanf(buf3,"%f", &site->fcat);
+			break;
+			case 701:
+			site = G_site_new_struct (FCELL_TYPE, 2, 0, 0);
+			sscanf(buf3,"%f", &site->fcat);
+			break;
+
+			default:
+			site = G_site_new_struct (-1, 2, 0, 0);
+		}
+	   } else 
+	   	site = G_site_new_struct (-1, 2, 0, 0);
+	
 	site->east = x;
 	site->north = y;
-	G_strncpy (site->str_att[0], buf3, strlen(buf3));
-      	G_site_put(fpout,site);
+     	G_site_put(fpout,site);
 	G_site_free_struct (site);
 	}
     }
