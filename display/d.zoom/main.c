45bd27c810e6ddb33d1ce50e0c2ae15f8abd7aa4
Radim Blazek <radim.blazek@gmail.com>
2002-03-09 13:58:45 +0000

    new
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10299 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ display/d.zoom/main.c
@@ -0,0 +1,430 @@
+/*
+ *   d.zoom
+ *
+ *   Get region through graphics
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include "gis.h"
+#include "site.h"
+#include "display.h"
+#include "raster.h"
+#include "Vect.h"
+#define	MAIN
+#include "local_proto.h"
+
+int 
+main (int argc, char **argv)
+{
+    int stat;
+/* rotate?
+    int rotate;
+*/
+#ifdef QUIET
+    struct Flag *quiet;
+#endif
+    struct Flag *just, *pan;
+    struct Option *rmap, *vmap, *smap, *zoom;
+    struct GModule *module;
+    double magnify;
+    int i, first=1;
+    char *mapset;
+    
+    /* Initialize globals */
+    rast = vect = site = NULL;
+    nrasts = nvects = nsites = 0;
+    
+/* Initialize the GIS calls */
+    G_gisinit(argv[0]) ;
+
+    module = G_define_module();
+    module->description = 
+	    "Allows the user to change the current geographic "\
+             "region settings interactively, with a mouse.";
+
+    /* Conditionalize R_open_driver() so "help" works, open quiet as well */
+    R__open_quiet();
+    if (R_open_driver() == 0)
+    {
+        if(D_get_cell_list (&rast, &nrasts) < 0)
+            rast = NULL;
+        else
+        {
+            rast = (char **)G_realloc(rast, (nrasts+1)*sizeof(char *));
+            rast[nrasts] = NULL;
+        }
+
+        if(D_get_dig_list (&vect, &nvects) < 0)
+            vect = NULL;
+        else
+        {
+            vect = (char **)G_realloc(vect, (nvects+1)*sizeof(char *));
+            vect[nvects] = NULL;
+        }
+
+        if(D_get_site_list (&site, &nsites) < 0)
+            site = NULL;
+        else
+        {
+            site = (char **)G_realloc(site, (nsites+1)*sizeof(char *));
+            site[nsites] = NULL;
+        }
+
+        R_close_driver();
+    }
+                    
+    rmap = G_define_option();
+    rmap->key = "rast";
+    rmap->type = TYPE_STRING;
+    rmap->multiple = YES;
+    if (rast)
+          rmap->answers = rast;
+    rmap->required = NO;
+    rmap->gisprompt = "old,cell,raster" ;
+    rmap->description = "Name of raster map";
+                                                        
+    vmap = G_define_option();
+    vmap->key = "vector";
+    vmap->type = TYPE_STRING;
+    vmap->multiple = YES;
+    if (vect)
+          vmap->answers = vect;
+    vmap->required = NO;
+    vmap->gisprompt = "old,dig,vector" ;
+    vmap->description = "Name of vector map";
+                                                        
+    smap = G_define_option();
+    smap->key = "site";
+    smap->type = TYPE_STRING;
+    smap->multiple = YES;
+    if (site)
+          smap->answers = site;
+    smap->required = NO;
+    smap->gisprompt = "old,site_lists,sites" ;
+    smap->description = "Name of site file";
+
+    zoom = G_define_option() ;
+    zoom->key        = "zoom" ;
+    zoom->type       = TYPE_DOUBLE ;
+    zoom->required   = NO ;
+    zoom->answer     = "0.75" ;
+    zoom->options    = "0.001-1000.0" ;
+    zoom->description= "magnification: >1.0 zooms in, <1.0 zooms out" ;
+
+#ifdef QUIET
+    quiet = G_define_flag();
+    quiet->key = 'q';
+    quiet->description = "Quiet";
+#endif
+
+    just = G_define_flag();
+    just->key = 'j';
+    just->description = "Just redraw given maps using default colors";
+
+    pan = G_define_flag();
+    pan->key = 'p';
+    pan->description = "Unzoom with panning";
+
+    if(!rast && !vect && !site)
+    {
+	  rmap->required = YES;
+	  just->answer = 1;
+    }
+
+    if ((argc > 1 || (!rast && !vect && !site)) && G_parser(argc,argv))
+	exit(1);
+
+    sscanf(zoom->answer,"%lf", &magnify);
+
+#ifdef QUIET
+    /* if map was found in monitor: */
+    if (rast || vect || site) 
+       quiet->answer=1;
+#endif
+
+    cmd = NULL;
+    if (!just->answer)
+    {
+        if (R_open_driver() != 0)
+	    G_fatal_error ("No graphics device selected");
+	stat = R_pad_get_item("list", &list, &nlists);
+	R_close_driver();
+	if (stat || !nlists)
+	{
+	    fprintf(stderr, "ERROR: can not get \"list\" items\n");
+	    fprintf(stderr, "-j flag forced\n");
+	    just->answer = 1;
+	}
+	else
+	{
+	    cmd = (char *)G_malloc(strlen(list[0])+1);
+	    strcpy(cmd, list[0]);
+	    for(i=1; i<nlists; i++)
+	    {
+		cmd = (char *)G_realloc(cmd, strlen(cmd)+strlen(list[i])+2);
+		strcat(cmd, "\n");
+		strcat(cmd, list[i]);
+	    }
+	}
+    }
+
+    if (just->answer)
+    {
+    	if (rmap->answers && rmap->answers[0])
+	    rast = rmap->answers;
+	else
+	{
+	    rast = NULL;
+	    nrasts = 0;
+	}
+    	if (vmap->answers && vmap->answers[0])
+	    vect = vmap->answers;
+	else
+	{
+	    vect = NULL;
+	    nvects = 0;
+	}
+    	if (smap->answers && smap->answers[0])
+	    site = smap->answers;
+	else
+	{
+	    site = NULL;
+	    nsites = 0;
+	}
+    }
+
+/* Make sure map is available */
+    if (rmap->required == YES && rmap->answers == NULL)
+    {
+	fprintf(stderr, "ERROR: No map is displayed in GRASS monitor\n");
+	exit(1);
+    }
+
+    if (rast)
+    {
+        struct Cell_head window;
+
+	for(i=0; rast[i]; i++);
+	nrasts = i;
+
+	for(i=0; i<nrasts; i++){
+    		mapset = G_find_cell2 (rast[i], "");
+    		if (mapset == NULL)
+    		{
+			char msg[256];
+			sprintf(msg,"Raster file [%s] not available", rast[i]);
+			G_fatal_error(msg) ;
+		}
+		else
+		{
+	 		if(G_get_cellhd(rast[i], mapset, &window) >= 0)
+			{
+	 			if(first)
+	 			{
+					first = 0;
+					U_east = window.east;
+					U_west = window.west;
+					U_south = window.south;
+					U_north = window.north;
+	 			}
+	 			else
+				{
+					if(window.east > U_east)
+						U_east = window.east;
+					if(window.west < U_west)
+						U_west = window.west;
+					if(window.south < U_south)
+						U_south = window.south;
+					if(window.north > U_north)
+						U_north = window.north;
+				}
+			}
+		}
+	}
+    }
+
+    if (vmap->required == YES && vmap->answers == NULL)
+	exit(0);
+
+    if (vect)
+    {
+        struct Map_info Map;
+	BOUND_BOX box;
+
+	for(i=0; vect[i]; i++);
+	nvects = i;
+
+        Vect_set_open_level(2);
+	for(i=0; i<nvects; i++){
+    		mapset = G_find_vector2 (vect[i], "");
+    		if (mapset == NULL)
+    		{
+			char msg[256];
+			sprintf(msg,"Vector file [%s] not available", vect[i]);
+			G_fatal_error(msg) ;
+		}
+		else
+		{
+			if(Vect_open_old(&Map, vect[i], mapset) == 1)
+			{
+			        Vect_get_map_box (&Map, &box );
+	 			if(first)
+	 			{
+					first = 0;
+					U_east = box.E;
+					U_west = box.W;
+					U_south = box.S;
+					U_north = box.N;
+	 			}
+	 			else
+				{
+					if(box.E > U_east)
+						U_east = box.E;
+					if(box.W < U_west)
+						U_west = box.W;
+					if(box.S < U_south)
+						U_south = box.S;
+					if(box.N > U_north)
+						U_north = box.N;
+				}
+			}
+		}
+	}
+    }
+
+    if (smap->required == YES && smap->answers == NULL)
+	exit(0);
+
+    if (site)
+    {
+	FILE *fp;
+	Site *s;
+	int rtype, ndim, nstr, ndec;
+
+	for(i=0; site[i]; i++);
+	nsites = i;
+
+	for(i=0; i<nsites; i++){
+    		mapset = G_find_sites2 (site[i], "");
+    		if (mapset == NULL)
+    		{
+			char msg[256];
+			sprintf(msg,"Site file [%s] not available", site[i]);
+			G_fatal_error(msg) ;
+		}
+		else
+		{
+			if(NULL != (fp = G_fopen_sites_old(site[i], mapset)))
+			{
+				rtype = -1;
+				G_site_describe(fp, &ndim, &rtype, &nstr, &ndec);
+				s = G_site_new_struct(rtype, ndim, nstr, ndec);
+				/*
+				while(G_site_get(fp, s) == 0)
+				{
+				*/
+				while(!feof(fp))
+				{
+					if(G_site_get(fp, s))
+						continue;
+					if(first)
+					{
+						first = 0;
+						U_east = s->east;
+						U_west = s->east;
+						U_south = s->north;
+						U_north = s->north;
+					}
+					else
+					{
+						if(s->east > U_east)
+							U_east = s->east;
+						if(s->east < U_west)
+							U_west = s->east;
+						if(s->north < U_south)
+							U_south = s->north;
+						if(s->north > U_north)
+							U_north = s->north;
+					}
+				}
+
+				/* is 100 enough to contain one point from
+				 * boundary?
+				 */
+				U_east += 100;
+				U_west -= 100;
+				U_south -= 100;
+				U_north += 100;
+
+				G_free(s);
+				fclose(fp);
+			}
+		}
+	}
+    }
+
+#ifdef BOUNDARY
+    if(!first)
+    {
+    /*
+	    if(U_east == U_west)
+	    {
+		    U_east += 100;
+		    U_west -= 100;
+	    }
+	    if(U_south == U_north)
+	    {
+		    U_south -= 100;
+		    U_north += 100;
+	    }
+    */
+
+	    U_east += 0.05 * (U_east - U_west);
+	    U_west -= 0.05 * (U_east - U_west);
+	    U_south -= 0.05 * (U_north - U_south);
+	    U_north += 0.05 * (U_north - U_south);
+    }
+#endif
+
+    if (R_open_driver() != 0)
+        G_fatal_error ("No graphics device selected");
+    
+    D_setup(0);
+
+    do
+    {
+
+        fprintf(stderr, "%d raster%s, %d vector%s, %d site file%s\n",
+		    nrasts, (nrasts > 1 ? "s":""),
+		    nvects, (nvects > 1 ? "s":""),
+		    nsites, (nsites > 1 ? "s":""));
+    /* Do the zoom */
+#ifndef QUIET
+/*
+        stat = zoomwindow(1, rotate, magnify, pan->answer);
+*/
+        stat = zoomwindow(1, magnify, pan->answer);
+#else
+/*
+        stat = zoomwindow(quiet->answer, rotate, magnify, pan->answer);
+*/
+        stat = zoomwindow(quiet->answer, magnify, pan->answer);
+#endif
+    
+    } while(stat == 2);
+
+    R_close_driver();
+
+    if (rast)
+      R_pad_freelist(rast, nrasts);
+
+    if (vect)
+      R_pad_freelist(vect, nvects);
+
+    if (site)
+      R_pad_freelist(site, nsites);
+
+    exit(stat);
+}
+
---
+++ general/g.region/cmd/main.c
@@ -0,0 +1,692 @@
+/***************************************************************************
+ * $Id$
+ *
+ * MODULE: 	g.region (commandline)
+ * AUTHOR(S):	Michael Shapiro, CERL
+ *              datum added by Andreas Lange <andreas.lange@rhein-main.de>
+ * PURPOSE: 	Program to manage and print the boundary definitions for the
+ *              geographic region.
+ * 
+ * COPYRIGHT:  	(C) 2000 by the GRASS Development Team
+ *
+ *   	    	This program is free software under the GPL (>=v2)
+ *   	    	Read the file COPYING that comes with GRASS for details.
+ ****************************************************************************/
+
+#include <string.h>
+#include <stdlib.h>
+#include "gis.h"
+#include "site.h"
+#include "Vect.h"
+#include "local_proto.h"
+#include "projects.h"
+
+static int nsew(char *,char *,char *,char *);
+static void die(struct Option *);
+static char *llinfo(char *,char *,int);
+
+int main (int argc, char *argv[])
+{
+	int i;
+	int print_flag, dist_flag;
+	int set_flag;
+	double x;
+	struct Cell_head window, temp_window;
+	char msg[200];
+	char *value;
+	char *name;
+	char *mapset;
+	char *err;
+	char *G_align_window();
+	int projection;
+
+	struct GModule *module;
+	struct
+	    {
+		struct Flag
+		*update,
+		*print,
+		*gprint,
+		*lprint,
+		*center,
+		*res_set,
+		*dist_res,
+		*dflt;
+	} flag;
+	struct
+	    {
+		struct Option
+		*north,*south,*east,*west,
+		*res, *nsres, *ewres,
+		*save, *region, *view,
+		*raster, *align, *zoom, *vect, *sites;
+	} parm;
+
+	G_gisinit (argv[0]);
+
+	module = G_define_module();
+	module->description =
+		"Program to manage the boundary definitions for the "
+		"geographic region.";
+
+	/* get current region.
+	 * if current region not valid, set it from default
+	 * note: G_get_default_window() dies upon error
+	 */
+	if (G__get_window (&window, "", "WIND", G_mapset()) != NULL)
+	{
+		G_get_default_window (&window);
+		G_put_window (&window);
+	}
+	projection = window.proj;
+
+	/* flags */
+
+	flag.dflt = G_define_flag();
+	flag.dflt->key         = 'd';
+	flag.dflt->description = "Set from default region";
+
+	flag.gprint = G_define_flag();
+	flag.gprint->key         = 'g';
+	flag.gprint->description = "Print the current region (shell script style)";
+
+	flag.print = G_define_flag();
+	flag.print->key         = 'p';
+	flag.print->description = "Print the current region";
+
+	flag.lprint = G_define_flag();
+	flag.lprint->key         = 'l';
+	flag.lprint->description = "Print the current region in lat/long";
+
+	flag.center = G_define_flag();
+	flag.center->key         = 'c';
+	flag.center->description = "Print the current region map center coordinates";
+
+        flag.dist_res= G_define_flag();
+        flag.dist_res->key         = 'm';
+        flag.dist_res->description = "Print region resolution in meters (geodesic)";
+
+        flag.res_set= G_define_flag();
+        flag.res_set->key         = 'a';
+        flag.res_set->description = "Align region to resolution [default = align to bounds]";
+
+	flag.update = G_define_flag();
+	flag.update->key         = 'u';
+	flag.update->description = "Do not update the current region";
+
+	/* parameters */
+
+	parm.region = G_define_option();
+	parm.region->key         = "region";
+	parm.region->key_desc    = "name";
+	parm.region->required    = NO;
+	parm.region->multiple    = NO;
+	parm.region->type        = TYPE_STRING;
+	parm.region->description = "Set current region from named region";
+
+	parm.raster = G_define_option();
+	parm.raster->key         = "raster";
+	parm.raster->key_desc    = "name";
+	parm.raster->required    = NO;
+	parm.raster->multiple    = NO;
+	parm.raster->type        = TYPE_STRING;
+	parm.raster->description = "Set region to match this raster map";
+
+	parm.vect = G_define_option();
+	parm.vect->key         = "vector";
+	parm.vect->key_desc    = "name";
+	parm.vect->required    = NO;
+	parm.vect->multiple    = NO;
+	parm.vect->type        = TYPE_STRING;
+	parm.vect->description = "Set region to match this vector map";
+
+	parm.sites = G_define_option();
+	parm.sites->key         = "sites";
+	parm.sites->key_desc    = "name";
+	parm.sites->required    = NO;
+	parm.sites->multiple    = NO;
+	parm.sites->type        = TYPE_STRING;
+	parm.sites->description = "Set region to match this sites map";
+
+	parm.view = G_define_option();
+	parm.view->key         = "3dview";
+	parm.view->key_desc    = "name";
+	parm.view->required    = NO;
+	parm.view->multiple    = NO;
+	parm.view->type        = TYPE_STRING;
+	parm.view->description = "Set region to match this 3dview file";
+
+	parm.north = G_define_option();
+	parm.north->key         = "n";
+	parm.north->key_desc    = "value";
+	parm.north->required    = NO;
+	parm.north->multiple    = NO;
+	parm.north->type        = TYPE_STRING;
+	parm.north->description = llinfo("Value for the northern edge", G_lat_format_string(), window.proj);
+
+	parm.south = G_define_option();
+	parm.south->key         = "s";
+	parm.south->key_desc    = "value";
+	parm.south->required    = NO;
+	parm.south->multiple    = NO;
+	parm.south->type        = TYPE_STRING;
+	parm.south->description = llinfo("Value for the southern edge", G_lat_format_string(), window.proj);
+
+	parm.east = G_define_option();
+	parm.east->key         = "e";
+	parm.east->key_desc    = "value";
+	parm.east->required    = NO;
+	parm.east->multiple    = NO;
+	parm.east->type        = TYPE_STRING;
+	parm.east->description = llinfo("Value for the eastern edge ", G_lon_format_string(), window.proj);
+
+	parm.west = G_define_option();
+	parm.west->key         = "w";
+	parm.west->key_desc    = "value";
+	parm.west->required    = NO;
+	parm.west->multiple    = NO;
+	parm.west->type        = TYPE_STRING;
+	parm.west->description = llinfo("Value for the western edge ", G_lon_format_string(), window.proj);
+
+	parm.res = G_define_option();
+	parm.res->key         = "res";
+	parm.res->key_desc    = "value";
+	parm.res->required    = NO;
+	parm.res->multiple    = NO;
+	parm.res->type        = TYPE_STRING;
+	parm.res->description = "Grid resolution (both north-south and east-west)";
+
+	parm.nsres = G_define_option();
+	parm.nsres->key         = "nsres";
+	parm.nsres->key_desc    = "value";
+	parm.nsres->required    = NO;
+	parm.nsres->multiple    = NO;
+	parm.nsres->type        = TYPE_STRING;
+	parm.nsres->description = llinfo("North-south grid resolution", G_llres_format_string(), window.proj);
+
+	parm.ewres = G_define_option();
+	parm.ewres->key         = "ewres";
+	parm.ewres->key_desc    = "value";
+	parm.ewres->required    = NO;
+	parm.ewres->multiple    = NO;
+	parm.ewres->type        = TYPE_STRING;
+	parm.ewres->description = llinfo("East-west grid resolution  ", G_llres_format_string(), window.proj);
+
+	parm.zoom = G_define_option();
+	parm.zoom->key         = "zoom";
+	parm.zoom->key_desc    = "name";
+	parm.zoom->required    = NO;
+	parm.zoom->multiple    = NO;
+	parm.zoom->type        = TYPE_STRING;
+	parm.zoom->description = "Raster map to zoom into";
+
+	parm.align = G_define_option();
+	parm.align->key         = "align";
+	parm.align->key_desc    = "name";
+	parm.align->required    = NO;
+	parm.align->multiple    = NO;
+	parm.align->type        = TYPE_STRING;
+	parm.align->description = "Raster map to align to";
+
+	parm.save = G_define_option();
+	parm.save->key         = "save";
+	parm.save->key_desc    = "name";
+	parm.save->required    = NO;
+	parm.save->multiple    = NO;
+	parm.save->type        = TYPE_STRING;
+	parm.save->description = "Name the current region";
+
+	if (G_parser(argc,argv))
+		exit(1);
+
+	set_flag = ! flag.update->answer;
+	if (flag.center->answer)
+		print_flag = 4;
+	else if (flag.lprint->answer)
+		print_flag = 3;
+	else if (flag.gprint->answer)
+		print_flag = 2;
+	else if (flag.print->answer)
+		print_flag = 1;
+	else
+		print_flag = 0;
+
+	/* Flag for reporting distance in meters */
+	if (flag.dist_res->answer) {
+		dist_flag = 1;
+		/* Set -g default output */
+		if ( print_flag == 0)
+		print_flag = 2;
+	} else
+		dist_flag = 0;
+
+	if (flag.dflt->answer)
+		G_get_default_window (&window);
+
+	/* region= */
+	if (name = parm.region->answer)
+	{
+		mapset = G_find_file ("windows", name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "region <%s> not found", name);
+			G_fatal_error (msg);
+		}
+		if (G__get_window (&window, "windows", name, mapset) != NULL)
+		{
+			sprintf (msg, "can't read region <%s> in <%s>", name, mapset);
+			G_fatal_error (msg);
+		}
+	}
+
+	/* 3dview= */
+	if (name = parm.view->answer)
+	{
+		struct G_3dview v;
+		FILE *fp;
+		int ret;
+		
+		mapset = G_find_file2 ("3d.view", name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "3dview file <%s> not found", name);
+			G_fatal_error (msg);
+		}
+
+		G_3dview_warning(0); /* suppress boundary mismatch warning */
+
+		if(NULL == (fp = G_fopen_old("3d.view",name,mapset))){
+		    sprintf (msg, "can't open 3dview file <%s> in <%s>", name, mapset);
+		    G_fatal_error (msg);
+		}
+
+		G_copy (&temp_window, &window, sizeof(window));
+
+		if(0 > (ret = G_get_3dview(name, mapset, &v))){
+		    sprintf (msg, "can't read 3dview file <%s> in <%s>", name, mapset);
+		    G_fatal_error (msg);
+		}
+		if (ret == 0){
+		    sprintf (msg, "Old 3dview file. Region not found in <%s> in <%s>", name, mapset);
+		    G_fatal_error (msg);
+		}
+
+                 
+		window.north = v.vwin.north;
+		window.south = v.vwin.south;
+		window.west  = v.vwin.west;
+		window.east  = v.vwin.east;
+
+		window.rows = v.vwin.rows;
+		window.cols = v.vwin.cols;
+		window.ns_res = v.vwin.ns_res;
+		window.ew_res = v.vwin.ew_res;
+
+		fclose (fp);
+
+	}
+
+	/* raster= */
+	if (name = parm.raster->answer)
+	{
+		mapset = G_find_cell2 (name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "raster map <%s> not found", name);
+			G_fatal_error (msg);
+		}
+		if (G_get_cellhd (name, mapset, &window) < 0)
+		{
+			sprintf (msg, "can't read header for <%s> in <%s>",
+			    name, mapset);
+			G_fatal_error (msg);
+		}
+	}
+
+	/* vect= */
+	if (name = parm.vect->answer)
+	{
+		struct Map_info Map;
+		BOUND_BOX box;
+
+		mapset = G_find_vector2 (name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "vector map <%s> not found", name);
+			G_fatal_error (msg);
+		}
+
+		G_copy (&temp_window, &window, sizeof(window));
+
+		Vect_set_open_level (2);
+		if (2 != Vect_open_old (&Map, name, mapset))
+		{
+			sprintf (msg, "can't open vector file <%s> in <%s>", name, mapset);
+			G_fatal_error (msg);
+		}
+
+		Vect_get_map_box (&Map, &box );
+		window.north = box.N;
+		window.south = box.S;
+		window.west  = box.W;
+		window.east  = box.E;
+
+       	        if(window.north == window.south)
+       	        {
+       	              window.north = window.north + 0.5 * temp_window.ns_res;
+                      window.south = window.south - 0.5 * temp_window.ns_res;
+                }
+                if(window.east==window.west)
+                {
+                      window.west = window.west - 0.5 * temp_window.ew_res;
+                      window.east = window.east + 0.5 * temp_window.ew_res;
+                }
+
+		G_align_window (&window, &temp_window);
+
+		Vect_close (&Map);
+	}
+
+	/* sites= */
+	if (name = parm.sites->answer)
+	{
+		FILE *fp;
+		int i, rtype, ndim, nstr, ndec;
+                Site *mysite;
+
+		mapset = G_find_sites2 (name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "sites map <%s> not found", name);
+			G_fatal_error (msg);
+		}
+		if (NULL == (fp = G_fopen_sites_old (name, mapset)))
+		{
+			sprintf (msg, "Could not open sites map <%s>", name);
+			G_fatal_error (msg);
+		}
+
+		rtype = -1;
+		G_site_describe(fp, &ndim, &rtype, &nstr, &ndec);
+		mysite = G_site_new_struct(rtype, ndim, nstr, ndec);
+
+		for (i = 0; G_site_get (fp, mysite) == 0; i++)
+		{
+			if (i==0)
+			{
+				G_copy (&temp_window, &window, sizeof(window));
+				window.east = window.west = mysite->east;
+				window.north = window.south = mysite->north;
+			}
+			else
+			{
+				if (mysite->east > window.east) 
+					window.east = mysite->east;
+				if (mysite->east < window.west) 
+					window.west = mysite->east;
+				if (mysite->north > window.north) 
+					window.north = mysite->north;
+				if (mysite->north < window.south) 
+					window.south = mysite->north;
+			}
+		}
+		G_free(mysite);
+		fclose (fp);
+		if (i)
+		{
+		     window.east += 100;
+		     window.west -= 100;
+		     window.south -= 100;
+		     window.north += 100;
+
+       	             if(window.north == window.south)
+       	             {
+       	                   window.north = window.north + 0.5 * temp_window.ns_res;
+                           window.south = window.south - 0.5 * temp_window.ns_res;
+                     }
+                     if(window.east==window.west)
+                     {
+                           window.west = window.west - 0.5 * temp_window.ew_res;
+                           window.east = window.east + 0.5 * temp_window.ew_res;
+                     }
+
+		     G_align_window (&window, &temp_window);
+                }
+	}
+
+
+
+	/* n= */
+	if (value = parm.north->answer)
+	{
+		if(i = nsew(value, "n+", "n-", "s+"))
+		{
+			if (!G_scan_resolution (value+2, &x, window.proj))
+				die(parm.north);
+			switch(i)
+			{
+			case 1:
+				window.north += x;
+				break;
+			case 2:
+				window.north -= x;
+				break;
+			case 3:
+				window.north = window.south + x;
+				break;
+			}
+		}
+		else if (G_scan_northing (value, &x, window.proj))
+			window.north = x;
+		else
+			die(parm.north);
+	}
+
+	/* s= */
+	if (value = parm.south->answer)
+	{
+		if(i = nsew(value, "s+", "s-", "n-"))
+		{
+			if (!G_scan_resolution (value+2, &x, window.proj))
+				die(parm.south);
+			switch(i)
+			{
+			case 1:
+				window.south += x;
+				break;
+			case 2:
+				window.south -= x;
+				break;
+			case 3:
+				window.south = window.north - x;
+				break;
+			}
+		}
+		else if (G_scan_northing (value, &x, window.proj))
+			window.south = x;
+		else
+			die(parm.south);
+	}
+
+	/* e= */
+	if (value = parm.east->answer)
+	{
+		if(i = nsew(value, "e+", "e-", "w+"))
+		{
+			if (!G_scan_resolution (value+2, &x, window.proj))
+				die(parm.east);
+			switch(i)
+			{
+			case 1:
+				window.east += x;
+				break;
+			case 2:
+				window.east -= x;
+				break;
+			case 3:
+				window.east = window.west + x;
+				break;
+			}
+		}
+		else if (G_scan_easting (value, &x, window.proj))
+			window.east = x;
+		else
+			die(parm.east);
+	}
+
+	/* w= */
+	if (value = parm.west->answer)
+	{
+		if(i = nsew(value, "w+", "w-", "e-"))
+		{
+			if (!G_scan_resolution (value+2, &x, window.proj))
+				die(parm.west);
+			switch(i)
+			{
+			case 1:
+				window.west += x;
+				break;
+			case 2:
+				window.west -= x;
+				break;
+			case 3:
+				window.west = window.east - x;
+				break;
+			}
+		}
+		else if (G_scan_easting (value, &x, window.proj))
+			window.west = x;
+		else
+			die(parm.west);
+	}
+
+	/* res= */
+	if (value = parm.res->answer)
+	{
+		if (!G_scan_resolution (value, &x, window.proj))
+			die(parm.res);
+		window.ns_res = x;
+		window.ew_res = x;
+	if (flag.res_set->answer) {
+		window.north =  ceil(window.north/x) * x ;
+		window.south = floor(window.south/x) * x ;
+		window.east = ceil(window.east/x) * x ;
+		window.west = floor(window.west/x) * x ;
+                }
+	}
+
+	/* nsres= */
+	if (value = parm.nsres->answer)
+	{
+		if (!G_scan_resolution (value, &x, window.proj))
+			die(parm.nsres);
+		window.ns_res = x;
+	if (flag.res_set->answer) {
+		window.north = 2 * x * ( (int)(window.north/2/x));
+                window.south = 2 * x * ( (int)(window.south/2/x));
+		}
+	}
+
+	/* ewres= */
+	if (value = parm.ewres->answer)
+	{
+		if (!G_scan_resolution (value, &x, window.proj))
+			die(parm.ewres);
+		window.ew_res = x;
+	if (flag.res_set->answer) {
+		window.east =  2 * x * ( (int)(window.east/2/x));
+                window.west =  2 * x * ( (int)(window.west/2/x));
+		}
+	}
+
+	/* zoom= */
+	if (name = parm.zoom->answer)
+	{
+		mapset = G_find_cell2 (name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "raster map <%s> not found", name);
+			G_fatal_error (msg);
+		}
+		zoom (&window, name, mapset);
+	}
+
+	/* align= */
+	if (name = parm.align->answer)
+	{
+		mapset = G_find_cell2 (name, "");
+		if (!mapset)
+		{
+			sprintf (msg, "raster map <%s> not found", name);
+			G_fatal_error (msg);
+		}
+		if (G_get_cellhd (name, mapset, &temp_window) < 0)
+		{
+			sprintf (msg, "can't read header for <%s> in <%s>",
+			    name, mapset);
+			G_fatal_error (msg);
+		}
+		if (err = G_align_window (&window, &temp_window))
+		{
+			sprintf (msg, "%s in %s: %s", name, mapset, err);
+			G_fatal_error (msg);
+		}
+	}
+
+	/* save= */
+	if (name = parm.save->answer)
+	{
+		if (G_legal_filename (name) < 0)
+		{
+			sprintf (msg, "<%s> - illegal region name", name);
+			G_fatal_error (msg);
+		}
+		G_copy (&temp_window, &window, sizeof(window));
+		adjust_window (&temp_window);
+		if (G__put_window (&temp_window, "windows", name) < 0)
+		{
+			sprintf (msg, "can't write region <%s>", name);
+			G_fatal_error (msg);
+		}
+	}
+
+	adjust_window (&window);
+	if (set_flag)
+	{
+		if (G_put_window (&window) < 0)
+			G_fatal_error ("unable to update current region");
+	}
+	if (print_flag)
+	{
+		print_window (&window, print_flag, dist_flag);
+	}
+
+	exit(0);
+}
+
+static void die(struct Option *parm)
+{
+	fprintf(stderr,"<%s=%s> ** illegal value **\n\n", parm->key, parm->answer);
+	/*
+    G_usage();
+    */
+	exit(1);
+}
+
+static int nsew(char *value,char *a,char *b,char *c)
+{
+	if (strncmp (value, a, strlen(a)) == 0 ) return 1;
+	if (strncmp (value, b, strlen(b)) == 0 ) return 2;
+	if (strncmp (value, c, strlen(c)) == 0 ) return 3;
+	return 0;
+}
+
+static char *llinfo(char *msg,char *llformat,int proj)
+{
+	char buf[256];
+	if (proj != PROJECTION_LL)
+		return msg;
+
+	sprintf (buf, "%s (format %s)", msg, llformat);
+	return G_store(buf);
+}
