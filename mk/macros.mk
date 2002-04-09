f67d47e7d5ba54028d6fccde01edc4149f53f0a0
Glynn Clements <glynn@gclements.plus.com>
2002-04-09 00:29:20 +0000

    Change MAKEALL definition to fail on error, so that errors are reported
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7175 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mk/macros.mk
+++ mk/macros.mk
@@ -3,7 +3,7 @@ OBJARCH = $(subst $(SRCDIR),$(DSTDIR),$(CURDIR))
 
 MAKEALL = for dir in $(wildcard *) ; do \
 	if test -f $$dir/makefile ; then \
-		$(MAKE) -f makefile -C $$dir dir ; \
+		$(MAKE) -f makefile -C $$dir dir || exit 1; \
 	else true ; fi ; done
 
 GMAKE = $(MAKE) -f makefile dir -C
