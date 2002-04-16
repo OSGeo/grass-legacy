7c00ce13e70089b9e771dc9527d2a0faeb0068be
Roger S. Miller <rgrmill@rt66.com>
2002-04-16 16:46:00 +0000

    Fixed segmentation fault on gcc, did some cleanup
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7254 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.contrib/SCS/vector/v.make.subj/cmd/main.c
+++ src.contrib/SCS/vector/v.make.subj/cmd/main.c
@@ -16,16 +16,18 @@
 #include <ctype.h>
 #include "gis.h"
 
+#define BUFSZ 100
+
 int sort_by_label(struct Categories *);
 int cmp(const void *, const void *);
 
 int main (int argc, char *argv[])
 {
-	register int i,k, ret;
-	int old_cat_cnt=0, cat_cnt=0, recd, record, reccnt;
-	int area_id, search, icode, cat_num;
-	char path[1024],buffer[100], Obuffer[100];
-	char area_name[100], cat_name[100], sav_name[100];
+	int i,cnt;
+	int old_cat_cnt=0, cat_cnt=0;
+	int area_id, icode, cat_num=0;
+	char path[1024],buffer[BUFSZ], Obuffer[BUFSZ];
+	char area_name[BUFSZ], cat_name[BUFSZ];
 	char *in_name, *out_name, *mapset, *tempname;
 	char *nptr, *cptr;
 	char errmsg[200];
@@ -64,175 +66,165 @@ int main (int argc, char *argv[])
              /* create SUBJ directory, if not existing */
         G__make_mapset_element("SUBJ") ;
 	out_name = subj->answer;
+
              /* open category file for reading, if it exists*/
 	sprintf(path,"SUBJ/%s",out_name);
         tempname = G_tempfile() ;
-        if ((ret = access(path,0)) != -1)
-           {            /* the category file exists, copy categ. to tmp */
+        if ( access(path,0) != -1)
+        {
+            /* the category file exists, copy data to tmp */
            Out = fopen (path,"r");
-	   tmp_file = fopen (tempname,"a");           
-                        /*  get current category count */
+	   tmp_file = fopen (tempname,"w+");           
+
+            /*  get current category count */
            fgets (buffer, sizeof(buffer), Out);         
            sscanf(buffer,"%*s%d%*s",&old_cat_cnt);
-                        /*  read past next four records */
-           for (recd=0;recd<=3;++recd)                     
-             {                       
-             fgets (buffer, sizeof(buffer), Out);
-             }        /*  copy remaining records to tmp */
-           for (recd=0;;++recd)                              
-             {                
-             if (!fgets (buffer, sizeof(buffer), Out)) break;
-             fputs (buffer,tmp_file);                              
-             }                          
+
+            /*  read past next four records */
+           for (cnt=0;cnt<=3;++cnt)                     
+               fgets (buffer, sizeof(buffer), Out);
+
+            /*  copy remaining records to temp */
+           while(fgets (buffer, sizeof(buffer), Out))
+               fputs (buffer,tmp_file);                              
+
            fclose (Out);
-           fclose (tmp_file);      
-           tmp_file = fopen (tempname,"r");
-           }
+        }
         else   
-	   tmp_file = fopen (tempname,"a");
+            /* open the temp file */
+	   tmp_file = fopen (tempname,"w+");
 
         cat_cnt = old_cat_cnt;
 
 	fprintf (stdout, "\n");
 	i = 0;
 	while (map->answers[i])
-	    {
+	{
 	    in_name = map->answers[i++];
 	    if ((mapset = G_find_vector2 (in_name, "")) == NULL)
-	      {
+	    {
 	      sprintf (errmsg, "Could not find category file <%s>\n", in_name);
 	      G_fatal_error (errmsg);
-	      }
+	    }
 
-	              /* Open dig_cats file */
+	     /* Open dig_cats file */
             sprintf(path,"%s/%s/%s/dig_cats/%s",
 		G_gisdbase(),G_location(),mapset,in_name);
-	    fprintf (stdout, "\n    Reading category file %s\n", in_name);
+	    fprintf (stdout, "\nReading category file %s\n", in_name);
             if ( (In = fopen(path, "r") ) == NULL )
 		G_fatal_error("Reading input file.") ;
 
-                     /* Cycle through file1 */
-                        /*  read past first five records */
-            for (recd=0;recd<=4;++recd)                     
-               {                       
+             /*  read past first five records */
+            for (cnt=0;cnt<=4;++cnt)                     
+            {                       
                fgets (buffer, sizeof(buffer), In);
-               }       
-            for (record=0;;++record)
-               {                    /* select category records */
-               if (!fgets (buffer, sizeof(buffer), In)) break;
-               reccnt++;                                         
-/*fprintf(stderr,"\tlength cat input = %d\n\t<%s>\n",strlen(buffer),buffer);*/
-               sscanf (buffer,"%d:%s",&area_id,area_name); 
-	       sprintf(sav_name,"%s",area_name);
-	       nptr = area_name;
-		  /* look for field separating colons (SCS version) */
-	       while (*nptr != '\0')
-		 {
-		 if (*nptr == '\072')
-		    {
-		    *nptr = '\0';
-		    break;
-		    }
-		 nptr++;
-		 }
+            }       
+
+             /* compare each category record to those already found */
+            while(fgets (buffer, sizeof(buffer), In))
+            {
+               for(cnt=0,nptr=buffer;cnt<BUFSZ;cnt++,nptr++)
+               {
+                  if(*nptr!='\n')continue;
+                  *nptr='\0';
+                  break;
+               }
+               if(sscanf (buffer,"%d:%s",&area_id,area_name)!=2)
+               {
+                  sprintf(errmsg,"Error scanning data in %s",map->answers[i-1]);
+                  G_warning(errmsg);
+                  continue;
+               }
+
+               nptr=buffer;
+               while (*nptr != ':')nptr++;
+               strncpy(area_name,++nptr,BUFSZ);
  
-	       nptr = area_name;
-               k = strlen(area_name);
-/*fprintf(stderr,"record %d  %d Bytes in !%s!\n",record,k,nptr); */
 
                           /* find input string in category file, 
 			     assign category value to the
                              area_name based on category file record number*/
-               search = 1;                                          
                icode = 0;  
                rewind (tmp_file);
-               while ( search )   
-                 {         
-                 for (recd=0;;++recd)
-                   {            /* category file search */
-                   if (!fgets (buffer, sizeof(buffer), tmp_file)) break;
-/*fprintf(stderr,"\tsubj recd %d length input = %d\n\t<%s>\n",recd,strlen(buffer),buffer); */
-                   sscanf (buffer,"%d:%s",&cat_num,cat_name);
-                   cptr = cat_name;                                 
-		       /* look for field separating colons (SCS version) */
-	           while (*cptr != '\0')
-		     {
-		     if (*cptr == '\072')
-		        {
-		        *cptr = '\0';
-		        break;
-		        }
-		     cptr++;
-		     }
- 
-                   cptr = cat_name;                                 
-/*fprintf(stderr," comp leng= %d  cat |%s|  subjcat |%s|  subjrec %d\n",k,nptr,cptr,recd);
-sleep (1);*/
-	                        /* compare for match */
-                   if (strcmp(nptr,cptr) == 0)
-                      {       /* match, assigned already */
-                      icode = cat_num;                            
-                      break;     
-                      }      
-                   }         /* end of category search */
-at_end:                                                            
-                 if (icode == 0)
-                   {      /* no match, add this name */
-                   fclose (tmp_file);                   
-                   tmp_file = fopen (tempname,"a");
-                   icode = cat_num+1;                     
-                   sprintf (Obuffer,"%d:%s\n",icode,sav_name);
-                   fputs (Obuffer, tmp_file);                
-                   fclose (tmp_file);         
-                   if ((old_cat_cnt - cat_cnt) == 0)
-                   fprintf(stderr,"\t   Adding new categ. :     ");    
-                   cat_cnt++;
-                   fprintf(stderr,"\b\b\b\b%4d",cat_cnt);
-/*fprintf(stderr,"\n\t new code - cat (%d) !%s!, cnt %d\n",icode,nptr,cat_cnt);     
-sleep(1);*/
-                   tmp_file = fopen (tempname,"r");
-                   break;
-                   }
-                 search = 0;
-                 }             /* end while loop */
-               }               /* end select category records */
-             fclose(In);
+               while(fgets (buffer, sizeof(buffer), tmp_file))
+               {
+                 for(cnt=0,cptr=buffer;cnt<BUFSZ;cnt++,cptr++)
+                 {
+                    if(*cptr!='\n')continue;
+                    *cptr='\0';
+                    break;
+                 }
+                 if(sscanf (buffer,"%d:%s",&cat_num,cat_name)!=2)
+                 {
+                   fclose(tmp_file);
+                   unlink(tempname);
+                   G_fatal_error("Error scanning tmp file");
+                 }
 
-	}
+                 cptr=buffer;
+                 while (*cptr != ':')cptr++;
+                 strncpy(cat_name,++cptr,BUFSZ);
+
+                 if (strcmp(area_name,cat_name) == 0)
+                 {       /* match, assigned already */
+                    icode = cat_num;                            
+                    break;    
+                 }      
+               }         /* end for loop through tmp file */
+
+               if (icode == 0)
+               {      /* no match, add this name */
+                 if(fseek(tmp_file,0,SEEK_END))
+                 {
+                    fclose(tmp_file);
+                    unlink(tempname);
+                    G_fatal_error("Error seeking end of tmp file");
+                 }
+                 icode = cat_num+1;                     
+                 sprintf (Obuffer,"%d:%s\n",icode,area_name);
+                 fputs (Obuffer, tmp_file);                
+                 cat_cnt+=1;
+               }
+            }           /* end loop through the cat file */
+            fclose(In);
+            cnt=cat_cnt-old_cat_cnt;
+            if(cnt>0)fprintf(stderr,"\tAdded %d new categories\n",cnt);
+            old_cat_cnt=cat_cnt;
+
+	}  /* repeat if necessary with a new category file */
+
 
                  /* all done, good-bye */
         fprintf(stderr,"\n");
-  fprintf(stderr,"\tSUBJect category processing finished\n\t  %d categorie(s) added\n",cat_cnt - old_cat_cnt);
-                 /* make a final mapset master category file
-                 put in first three records  */
+        fprintf(stderr,"\tAdded a total of %d categories\n",cat_cnt);
+
+                 /* make a final mapset master category file */
         sprintf(path,"%s/%s/%s/SUBJ/%s",
 		G_gisdbase(),G_location(),G_mapset(),out_name);
         Out = fopen (path,"w");
-/*      rewind (Out); */
+
+                 /* put in first three records  */
         sprintf (Obuffer,"#%5d categories\n",cat_cnt);
         fputs(Obuffer,Out);
         sprintf(Obuffer,"Title\n");
         fputs(Obuffer,Out);
         sprintf(Obuffer,"\n0.00 0.00 0.00 0.00\n0:  \n");
         fputs(Obuffer,Out);
-        rewind (tmp_file);
+
                   /*   now append the tmp_file data  */
-        for (recd=0;;++recd)         
-          {                  
-          if (!fgets (buffer, sizeof(buffer), tmp_file)) break;
-          fputs (buffer,Out);                    
-          }                             
-        fclose (tmp_file);
+        rewind (tmp_file);
+        while(fgets (buffer, sizeof(buffer), tmp_file))
+            fputs (buffer,Out);                    
         fclose (Out);
 
 		  /* now read the subj file for sorting */
         G_suppress_warnings (1);
 	G__read_cats ("SUBJ", out_name, G_mapset(), &cats, 1);
-  	G_suppress_warnings (0); 
-	sort_by_label (&cats);
+	sort_by_label (&cats); 
 		  /* have to put in "no data" category */
 	cats.labels[0] = G_store("no data");
-	G__write_cats ("SUBJ", out_name, &cats);
+	G__write_cats ("SUBJ", out_name, &cats); 
+  	G_suppress_warnings (0); 
                   /* remove the tmp_file*/
         unlink(tempname); 
 	exit (0);
