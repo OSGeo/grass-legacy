1ee4de275088f6bdb04305ac718e9b30da998549
Glynn Clements <glynn@gclements.plus.com>
2002-04-19 18:53:24 +0000

    Use resizable array for category list Miscellaneous tidying
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7284 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/mapdev/v.extract/cmd/main.c
+++ src/mapdev/v.extract/cmd/main.c
@@ -37,38 +37,39 @@
 #include "Vect.h"
 #include "local_proto.h"
 
-#define		B_DIR		"dig"
-#define		A_DIR		"dig"
-#define		ATT_DIR		"dig_att"
-#define		CAT_DIR		"dig_cats"
+static int *cat_array, cat_count, cat_size;
 
-struct Map_info Map;
-int cat_array[5000];
-char  buf[1024] ;
-struct dig_head Head;
+static void add_cat(int x)
+{
+    if (cat_count >= cat_size)
+    {
+	cat_size = (cat_size < 1000) ? 1000 : cat_size * 2;
+	cat_array = G_realloc(cat_array, cat_size * sizeof(int));
+    }
 
+    cat_array[cat_count++] = x;
+}
 
 int main (int argc, char **argv)
 {
-	int i, cat_index, new_cat, max_att, line_type;
-	int cat_count;
-	int result;
-	int dissolve=0, x, y;
-        char buffr[80], file_name[80], text[80];
-        char *input, *output, *mapset;
-        struct Categories cats, temp_cats;
-		struct GModule *module;
-        struct Option *inopt, *outopt, *fileopt, *newopt, *typopt, *listopt;
-        struct Flag *d_flag, *n_flag;
-        FILE *in;
+    int i, new_cat, max_att, line_type;
+    int result;
+    int dissolve=0, x, y;
+    char buffr[1024], text[80];
+    char *input, *output, *mapset;
+    struct Categories cats, temp_cats;
+    struct GModule *module;
+    struct Option *inopt, *outopt, *fileopt, *newopt, *typopt, *listopt;
+    struct Flag *d_flag, *n_flag;
+    FILE *in;
 
 
-            /* set up the options and flags for the command line parser */
+    /* set up the options and flags for the command line parser */
 
-	module = G_define_module();
-	module->description =
-		"Selects vector objects from an existing vector map and "
-		"creates a new map containing only the selected objects.";
+    module = G_define_module();
+    module->description =
+	"Selects vector objects from an existing vector map and "
+	"creates a new map containing only the selected objects.";
 
     d_flag = G_define_flag();
     d_flag->key              = 'd';
@@ -123,240 +124,174 @@ int main (int argc, char **argv)
     G_gisinit (argv[0]);
 
 
-       /* heeeerrrrrre's the   PARSER */
+    /* heeeerrrrrre's the   PARSER */
     if (G_parser (argc, argv))
         exit (-1);
 
-       /* start checking options and flags */
-    if (listopt->answers == NULL && fileopt->answer == NULL)
-	{
-        	fprintf(stderr,"\nEither [list] or [file] should be given.\n");
-		exit(1);
-	}
+    /* start checking options and flags */
+    if (!listopt->answers && !fileopt->answer)
+	G_fatal_error("Either [list] or [file] should be given.");
 
-       /* set input vector file name and mapset */
+    /* set input vector file name and mapset */
     input = inopt->answer;
     mapset = G_find_vector (input, "") ;
-    cat_index = 0;
-    if (mapset == NULL)
-	{
-		sprintf(buffr,"Vector file [%s] not available in search list",
-		    input);
-		G_fatal_error(buffr) ;
-	}
+
+    if (!mapset)
+	G_fatal_error("Vector file [%s] not available in search list",
+		      input);
       
-       /* set output vector file name */
+    /* set output vector file name */
     output = outopt->answer;
 
     if ( d_flag->answer ) dissolve = 1;
-    if ( !*newopt->answer) new_cat = 0;
+
+    if (!newopt->answer) new_cat = 0;
     else new_cat = atoi(newopt->answer);
 
     if ( n_flag->answer )    /* check name flag */
-      {    
-              /* the n_flag is set, read in the categories for this file */
-      G_read_vector_cats (input, mapset, &cats);
-              /* check for a file of category names */
-      if ( fileopt->answer == NULL )
-         {
+    {    
+	/* the n_flag is set, read in the categories for this file */
+	G_read_vector_cats (input, mapset, &cats);
+	/* check for a file of category names */
+	if ( fileopt->answer == NULL )
+	{
             /* no file of names to read, process name list */
-                  /* first check for validity */
-         for (i = 0; listopt->answers[i]; i++)
-            if (!scan_names (&cats, listopt->answers[i], &x))
+	    /* first check for validity */
+	    for (i = 0; listopt->answers[i]; i++)
+		if (!scan_names (&cats, listopt->answers[i], &x))
+		    G_fatal_error("Category label <%s> not found",
+				  listopt->answers[i]);
+	    /* valid names, put this into a values array */
+	    for (i = 0; listopt->answers[i]; i++)
 	    {
-            fprintf(stderr,"\nCategory label <%s> not found\n",
-                              listopt->answers[i]);
-		exit(1);
+		scan_names (&cats, listopt->answers[i], &x);
+		add_cat(x);
 	    }
-                  /* valid names, put this into a values array */
-         for (i = 0; listopt->answers[i]; i++)
-            {
-            scan_names (&cats, listopt->answers[i], &x);
-            cat_array[cat_index] = x; cat_index++; 
-            }
-         }
-      else /* got file of category names */
-         {
-	 sprintf(file_name,"%s",fileopt->answer);
-
+	}
+	else /* got file of category names */
+	{
             /* open input file */
-         if( (in = fopen(file_name,"r")) == NULL )
-	   {
-	   fprintf(stderr,"Can't open name file <%s> \n", file_name) ;
-	   exit (-1);
-	   }
+	    if( (in = fopen(fileopt->answer,"r")) == NULL )
+		G_fatal_error("Can't open name file <%s>", fileopt->answer) ;
 
-         while (1)
-           {
-           if (!fgets (buffr, 39, in)) break;
-           sscanf (buffr, "%[a-zA-Z., -_/$%@!#0-9]", text); 
+	    while (1)
+	    {
+		if (!fgets (buffr, 39, in)) break;
+		sscanf (buffr, "%[a-zA-Z., -_/$%@!#0-9]", text); 
 	   	/*sscanf (buffr, "%s", text); */
 		/*scan %s stops at whitespace?*/
-           scan_names (&cats, text, &x);
-           cat_array[cat_index] = x; cat_index++; 
-           }
-         fclose(in);
-         }
-      }
+		scan_names (&cats, text, &x);
+		add_cat(x);
+	    }
+	    fclose(in);
+	}
+    }
     else   /* NO name flag */
-      {    /* check for a file of categories */
-      if ( fileopt->answer == NULL )
-         {
-             /* no file of categories to read, process cat list */
+    {    /* check for a file of categories */
+	if ( fileopt->answer == NULL )
+	{
+	    /* no file of categories to read, process cat list */
 
             /* first check for valid list */
-         for (i = 0; listopt->answers[i]; i++)
-            if (!scan_cats (listopt->answers[i], &x, &y))
-	    {
-            fprintf(stderr,"\nCategory value in <%s> not valid\n",
-                              listopt->answers[i]);
-		exit(1);
-	    }
+	    for (i = 0; listopt->answers[i]; i++)
+		if (!scan_cats (listopt->answers[i], &x, &y))
+		    G_fatal_error("Category value in <%s> not valid",
+				  listopt->answers[i]);
 
             /* valid list, put into cat value array */
-	 cat_index = 0;
-         for (i = 0; listopt->answers[i]; i++)
-            {
-            scan_cats (listopt->answers[i], &x, &y);
-            while (x <= y)
-               {
-               cat_array[cat_index] = x++; cat_index++; 
-               } 
-            }
-         }
-      else  /* got a file of category numbers */
-         {
-	 sprintf(file_name,"%s",fileopt->answer);
-         fprintf(stderr,"process file <%s> for cats\n",file_name);
+
+	    for (i = 0; listopt->answers[i]; i++)
+	    {
+		scan_cats (listopt->answers[i], &x, &y);
+		while (x <= y)
+		    add_cat(x++);
+	    }
+	}
+	else  /* got a file of category numbers */
+	{
+	    fprintf(stderr,"process file <%s> for cats\n",fileopt->answer);
 
             /* open input file */
-         if( (in = fopen(file_name,"r")) == NULL )
-	   {
-	   fprintf(stderr,"Can't open category file <%s> \n", file_name) ;
-	   exit (-1);
-	   }
-         while (1)
-           {
-           if (!fgets (buffr, 39, in)) break;
-           sscanf (buffr, "%[a-zA-Z., -_/$%@!#0-9]", text); 
+	    if( (in = fopen(fileopt->answer,"r")) == NULL )
+		G_fatal_error("Can't open category file <%s>", fileopt->answer) ;
+	    while (1)
+	    {
+		if (!fgets (buffr, 39, in)) break;
+		sscanf (buffr, "%[a-zA-Z., -_/$%@!#0-9]", text); 
 	   	/*sscanf (buffr, "%s", text); */
 		/*scan %s stops at whitespace?*/
-           scan_cats (text, &x, &y);
-	   cat_index = 0;
-           while (x <= y)
-              {
-              cat_array[cat_index] = x++; cat_index++; 
-              } 
-           }
-         fclose(in);
-         }
-      }
-/* for (i=0; i<cat_index;i++)
- fprintf(stderr,"cat-array[%d]= %d\n",i,cat_array[i]);*/
+		scan_cats (text, &x, &y);
+
+		while (x <= y)
+		    add_cat(x++);
+	    }
+	    fclose(in);
+	}
+    }
 
     if (*typopt->answer == 'a')
-       {
-       max_att = xtract_area(cat_index,cat_array,input,output,dissolve,new_cat);
-       if ( 0 > max_att)
-          {
-          fprintf(stderr," Error in area extraction processing\n");
-          exit(1);
-          }
-       }
+    {
+	max_att = xtract_area(cat_count,cat_array,input,output,dissolve,new_cat);
+	if ( 0 > max_att)
+	    G_fatal_error("Error in area extraction processing");
+    }
     else
-       {
-       line_type=AREA;
-       if(*typopt->answer == 'l')line_type=LINE;
-       else if(*typopt->answer == 's')line_type=DOT;
+    {
+	line_type=AREA;
+	if(*typopt->answer == 'l')line_type=LINE;
+	else if(*typopt->answer == 's')line_type=DOT;
 
-       max_att = xtract_line(cat_index,cat_array,input,output,new_cat,line_type);
-       if ( 0 > max_att)
-          {
-          fprintf(stderr," Error in line/site extraction processing\n");
-          exit(1);
-          }
-       }
-                     /* Open output "dig_cats" file */
-    /* G__file_name(file_name, CAT_DIR, output, G_mapset()) ; */
+	max_att = xtract_line(cat_count,cat_array,input,output,new_cat,line_type);
+	if ( 0 > max_att)
+	    G_fatal_error("Error in line/site extraction processing");
+    }
+
     result = G_init_cats( (CELL)0, "", &temp_cats);
     fprintf( stderr, "Result of cats initialisation is %d\n", result );
-    /* if ( (catf = fopen (file_name, "w")) == NULL)
-       {
-       fprintf(stderr,"Can't create output dig_cats file <%s> \n", file_name) ;
-       return (-1);
-       } */
+
     G_set_cats_title( output, &temp_cats );
     G_set_cat( 0, "no data", &temp_cats);
 
     fprintf (stdout,"\n");
     fprintf (stdout,"    Making category file\n");
 
-                      /* make a cats file */
-    /* if (dissolve)
-       sprintf(buffr,"# %d categories\nTitle %s\n",new_cat,output);
-    else
-       sprintf(buffr,"# %d categories\nTitle %s\n",max_att,output);
-    fputs(buffr,catf);
-    sprintf(buffr,"\n0.00 0.00 0.00 0.00\n0:no data\n");
-    fputs(buffr,catf);
-    */
-
     if (dissolve) {
-      /* for (i = 1; i <= new_cat; i++)
-           {
-           sprintf(buffr,"%d:\n",i);
-           fputs(buffr,catf);
-           }
-      */
-      if( new_cat == 0 ) new_cat = cat_array[0];
-      if (G_read_vector_cats(input, mapset, &cats) == 0)
-	G_set_cat( new_cat, G_get_cat(new_cat, &cats), &temp_cats);
-      else
-	G_set_cat(new_cat, "", &temp_cats);
+	if( new_cat == 0 ) new_cat = cat_array[0];
+	if (G_read_vector_cats(input, mapset, &cats) == 0)
+	    G_set_cat( new_cat, G_get_cat(new_cat, &cats), &temp_cats);
+	else
+	    G_set_cat(new_cat, "", &temp_cats);
     }
-     else
-	 {
-		      /* first try reading parent cat file data */
-	   if (G_read_vector_cats(input, mapset, &cats) == 0) 
-	     {
-	       if(new_cat == 0)
-		 {
-		   cat_count = 0;
-		   while(cat_array[cat_count])
-		     {
-		       G_set_cat( cat_array[cat_count], G_get_cat(cat_array[cat_count],
-								  &cats), &temp_cats );
-		       cat_count++;
-		     }
-		 }
-	       else
-		 G_set_cat(new_cat, G_get_cat(new_cat, &cats), &temp_cats);
-	     }
-         else   /* build an empty cat file */
-	    {
-	       if(new_cat == 0)
-		 {
-		   cat_count = 0;
-		   while(cat_array[cat_count])
-		     {
-		       G_set_cat( cat_array[cat_count], "", &temp_cats );
-		       cat_count++;
-		     }
-		 }
-	       else
-		 G_set_cat(new_cat, "", &temp_cats);
-	    }
-	 }
-     if( G_write_vector_cats( output, &temp_cats ) < 0 )
-       fprintf( stderr, "Could not write category file, see error above." );
+    else
+    {
+	/* first try reading parent cat file data */
+	if (G_read_vector_cats(input, mapset, &cats) == 0) 
+	{
+	    if(new_cat == 0)
+		for (i = 0; i < cat_count; i++)
+		    G_set_cat(cat_array[i], G_get_cat(cat_array[i],&cats), &temp_cats);
+	    else
+		G_set_cat(new_cat, G_get_cat(new_cat, &cats), &temp_cats);
+	}
+	else   /* build an empty cat file */
+	{
+	    if(new_cat == 0)
+		for (i = 0; i < cat_count; i++)
+		    G_set_cat( cat_array[i], "", &temp_cats );
+	    else
+		G_set_cat(new_cat, "", &temp_cats);
+	}
+    }
+    if( G_write_vector_cats( output, &temp_cats ) < 0 )
+	fprintf( stderr, "Could not write category file, see error above." );
 
-     sprintf( buffr, "%s/etc/v.build  map=%s  thresh=no",G_gisbase(),output);
-     system(buffr);
+    sprintf(buffr, "%s/etc/v.build map=%s thresh=no",G_gisbase(),output);
+    system(buffr);
 
-		       /* give the user this message  */
-     fprintf(stderr, "\n\nExtracted vector file <%s> has been created in the 'dig' directory\n\n",output);
+    /* give the user this message  */
+    fprintf(stderr, "\n\nExtracted vector file <%s> has been created in the 'dig' directory\n\n",output);
 
-     exit(0);
+    exit(0);
 }
 
 int 
@@ -370,7 +305,7 @@ scan_cats (char *s, int *x, int *y)
 	return (*dummy == 0 && *x <= *y);
     *dummy = 0;
     if (sscanf (s, "%d%1s", x, dummy) == 1 && *dummy == 0)
-   {
+    {
 	*y = *x;
 	return 1;
     }
@@ -389,15 +324,15 @@ int scan_names (struct Categories *pcats, char *s, int *x)
     /*sscanf (s, "%s%1s", area_name, dummy);
       nptr = area_name;*/
       
-      nptr=s;
+    nptr=s;
 
-      icode = 0;
-	/* find input string in category struct, assign category value to the
-		    area_name based on category file record number*/
+    icode = 0;
+    /* find input string in category struct, assign category value to the
+       area_name based on category file record number*/
 
-      recd = pcats->num;             /* set the number of categories */
-      for (i=0;i<recd;i++)                /* category search */
-	{     /* cycle cat names, look for a match */
+    recd = pcats->num;             /* set the number of categories */
+    for (i=0;i<recd;i++)                /* category search */
+    {     /* cycle cat names, look for a match */
         cat_name[0] = '\0';
         strcat(cat_name,pcats->labels[i]); /* get a category label */
         pntr1 = cat_name;
@@ -405,12 +340,12 @@ int scan_names (struct Categories *pcats, char *s, int *x)
         *cptr = '\0';  
         
 	while (1)
-           {   /* look for cats field separator SCS version */
-           /*if (ispunct(*pntr1) || *pntr1 == '\0') break;*/
-	   if (*pntr1 == '\0') break;
-           *(cptr) = *(pntr1);
-           pntr1++; cptr++;
-           }
+	{   /* look for cats field separator SCS version */
+	    /*if (ispunct(*pntr1) || *pntr1 == '\0') break;*/
+	    if (*pntr1 == '\0') break;
+	    *(cptr) = *(pntr1);
+	    pntr1++; cptr++;
+	}
         
 	*cptr = '\0'; cptr = buff;
 	
@@ -418,13 +353,13 @@ int scan_names (struct Categories *pcats, char *s, int *x)
   fprintf(stderr,"       compare value= %d\n",strcmp(cptr,nptr));
   sleep(2);*/
 	if (strcmp(nptr,cptr) == 0)     /* compare for match */
-	   {                           /* match, assigned already */
-           *x = i ; /* return category code */
-           return(1);
-	   }
+	{                           /* match, assigned already */
+	    *x = i ; /* return category code */
+	    return(1);
+	}
 	   
-	} 
-	/* end of category search for loop */
+    } 
+    /* end of category search for loop */
 
-      return(0);
+    return(0);
 }
