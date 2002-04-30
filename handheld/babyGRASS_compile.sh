0ddf42cf895a0b89708bf77f73b1831628b49931
Markus Neteler <neteler@gmail.com>
2002-04-30 14:29:40 +0000

    added scripts to package/build GRASs on iPAQ or other handhelds
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7394 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ handheld/babyGRASS_compile.sh
@@ -0,0 +1,142 @@
+#!/bin/sh
+
+# Markus Neteler 4/2002
+# neteler@itc.it
+# (c) GPL >= 2
+#
+#build babyGRASS with shared libraries
+#
+#TODO: - make compilation a loop to write to ERROR_LOG
+#      - add prefix, bindir parameter
+
+### configure:
+PREFIX=.  # where to put GRASS (/usr/local/grass5/)
+BINDIR=.  # where to put the start script "grass5" (/usr/local/bin/)
+
+##################### nothing to change below #####################
+ERROR_LOG=error.log
+
+trap "exit" 2 3 9 15
+
+echo "building new makefiles. Takes a while..."
+mk/mkmakefiles   
+
+echo "configuring"
+
+CFLAGS="-Os" LDFLAGS="-s"  ./configure --prefix=$PREFIX --bindir=$BINDIR --without-motif --without-lapack --without-blas --without-fftw --without-freetype --without-gdal --without-dbm --without-tcltk --without-postgres --without-obdc --without-tiff --without-png --without-gd --without-opengl --without-dbm --without-odbc
+if [ $? -eq 1 ]
+	then
+	 echo "an error occured"
+	 exit
+fi
+
+ARCH=`grep ARCH config.status | cut -d'%' -f3`
+
+cp -f ./mk/Makefile Makefile
+cp  ./mk/mid.mk  ./mk/mid.mk.org
+cp ./mk/mid.mk.shlib ./mk/mid.mk
+
+##################### prepare local target directories:
+mkdir -p $BINDIR/bin.$ARCH 
+GISBASE=$PREFIX/dist.$ARCH
+# Make sure GISBASE sub-directories exist
+for i in \
+    bin \
+    man man/help man/man1 man/man2 man/man3 man/man4 man/man5 \
+    etc etc/paint etc/paint/driver etc/paint/driver.sh etc/paint/driver.uninst \
+    etc/sites etc/dig_drivers etc/imagery \
+    etc/bin etc/bin/inter etc/bin/cmd \
+    txt txt/COMBINE txt/DIGIT txt/DIGIT2 txt/WEIGHT txt/MONITOR \
+    lib include \
+    driver locks dev
+do
+    if test ! -d $GISBASE/$i
+    then
+	mkdir -p $GISBASE/$i || exit 1
+	echo $GISBASE/$i created
+    fi
+done
+
+
+##################### ready to build now:
+
+echo "Start of compilation: "`date` > ${ERROR_LOG}
+
+#generate ./gmake5 script:
+make gmake || echo "Compilation error: " >> ${ERROR_LOG} 
+
+#build the minimum libs:
+./gmake5 src/libes/tools || echo "Compilation error: lib" >> ${ERROR_LOG}
+./gmake5 src/libes/datetime || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/gis || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/raster || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/dlg || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/proj || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/display || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/lock || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/D || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/edit
+./gmake5 src/libes/vask
+./gmake5 src/libes/linkm
+./gmake5 src/libes/imagery
+./gmake5 src/libes/dig_atts
+./gmake5 src/libes/vect32/libes || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/vect32/diglib || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/vect32/Vlib || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/libes/vect32/georef || echo "Compilation error: " >> ${ERROR_LOG}
+#next also contains the db.* modules:
+./gmake5 src/libes/dbmi
+
+#build the internal modules and drivers:
+./gmake5 src/general/init || echo "Compilation error: driver" >> ${ERROR_LOG}
+./gmake5 src/display/devices/lib || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/display/devices/monitorcap || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/display/devices/XDRIVER || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/fonts/for_grass || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/front.end || echo "Compilation error: " >> ${ERROR_LOG}
+./gmake5 src/general/g.gisenv
+
+#build the user modules:
+./gmake5 src/display/d.mon || echo "Compilation error: d.mon" >> ${ERROR_LOG}
+./gmake5 src/display/d.rast || echo "Compilation error: d.rast " >> ${ERROR_LOG}
+./gmake5 src/display/d.vect
+./gmake5 src/display/d.sites
+./gmake5 src/display/d.erase
+./gmake5 src/display/d.what.rast
+./gmake5 src/display/d.what.vect
+./gmake5 src/display/d.what.sites
+./gmake5 src/display/d.measure
+./gmake5 src/display/d.pan
+./gmake5 src/display/d.zoom
+./gmake5 src/general/g.region/cmd
+./gmake5 src/general/manage
+./gmake5 src/raster/r.in.ascii
+./gmake5 src/sites/s.in.ascii
+
+echo "End of compilation: "`date` > ${ERROR_LOG}
+
+#needed to link against front.end:
+CURRWD=`pwd`
+cp Makefile $PREFIX/dist.$ARCH
+cd $PREFIX/dist.$ARCH
+make links
+cd $CURRWD
+
+########### locally install libs
+mkdir -p $PREFIX/dist.$ARCH/lib
+cp -af ./src/libes/*.so*  $PREFIX/dist.$ARCH/lib
+
+########### clean up:
+echo "Cleaning up stuff not needed now..."
+rm -rf $PREFIX/dist.$ARCH/etc/nad   # only NAD27/NAD83
+rm -rf $PREFIX/dist.$ARCH/etc/paint
+rm -rf $PREFIX/dist.$ARCH/txt
+rm -rf $PREFIX/dist.$ARCH/etc/bin/inter/
+
+########### install to destination:
+
+echo "babyGRASS start script is in: $BINDIR/bin.$ARCH"
+echo "babyGRASS binaries are in:    $PREFIX/dist.$ARCH"
+
+#or use (after changing PREFIX):
+#make install
---
+++ handheld/babyGRASS_packagingsrc.sh
@@ -0,0 +1,87 @@
+#!/bin/sh
+
+# Markus Neteler 4/2002
+# neteler@itc.it
+# (c) GPL >= 2
+#
+#extract babyGRASS from standard source code
+#
+
+TARBALL=babyGRASS_src.tar.gz
+
+if ! test -f ./configure
+then
+  echo "Be sure to run this script inside the GRASS source code main directory"
+  exit
+fi
+
+LIST='mk
+src/CMD
+src/include
+src/libes/tools
+src/libes/datetime
+src/libes/gis
+src/libes/raster
+src/libes/dlg
+src/libes/proj
+src/libes/display
+src/libes/lock
+src/libes/D
+src/libes/edit
+src/libes/vask
+src/libes/linkm
+src/libes/imagery
+src/libes/dig_atts
+src/libes/vect32/libes
+src/libes/vect32/diglib
+src/libes/vect32/Vlib
+src/libes/vect32/georef
+src/libes/dbmi
+src/general/init
+src/display/devices/lib
+src/display/devices/monitorcap
+src/display/devices/XDRIVER
+src/fonts
+src/front.end
+src/general/g.gisenv
+src/display/d.mon
+src/display/d.rast
+src/display/d.vect
+src/display/d.sites
+src/display/d.erase
+src/display/d.what.rast
+src/display/d.what.vect
+src/display/d.what.sites
+src/display/d.measure
+src/display/d.pan
+src/display/d.zoom
+src/general/g.region/cmd
+src/general/manage
+src/raster/r.in.ascii
+src/sites/s.in.ascii'
+
+#generate list string:
+FullList=""
+for i in $LIST
+do
+ FullList="$FullList $i"
+done
+
+#mkdir babysrc dir
+mkdir babyGRASS_src
+echo "Copying related files to tmp dir..."
+tar cf - $FullList | (cd ./babyGRASS_src; tar xf -)
+
+#copy further files:
+cp AUTHORS BUGS COPYING NEWS.html README REQUIREMENTS.html TODO.txt Makefile.in config* install-sh INSTALL babyGRASS_compile.sh babyGRASS_src/
+
+#keep rubbish outside:
+EXCLUDE="--exclude=CVS* --exclude=*.o --exclude=makefile --exclude=OBJ.* --exclude=LIB.* --exclude=lib*.a"
+
+#packaging:
+echo Packaging...
+tar cvfz $TARBALL $EXCLUDE babyGRASS_src/*
+
+rm -rf babyGRASS_src
+
+echo "The source code for babyGRASS is stored into: $TARBALL"
