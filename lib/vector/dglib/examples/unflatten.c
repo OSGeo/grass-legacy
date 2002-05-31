e9f131aefff85f38080d27821034b8d26318bcb5
Roberto Micarelli <miro@iol.it>
2002-05-31 13:33:03 +0000

    Just unflatten the input graph - written in order to 'time' the unflatting of a large graph.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10512 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/vector/dglib/examples/unflatten.c
@@ -0,0 +1,81 @@
+/* LIBDGL -- a Directed Graph Library implementation
+ * Copyright (C) 2002 Roberto Micarelli
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+/*
+ * Source best viewed with tabstop=4
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <fcntl.h>
+#include <time.h>
+#include <errno.h>
+#include <math.h>
+
+#include "../type.h"
+#include "../graph.h"
+
+#include "opt.h"
+
+int main( int argc , char ** argv )
+{
+	gnGrpGraph_s  	graph;
+	int			 	nret , fd;
+
+	/* program options
+	 */
+ 	char	*	pszGraph;
+ 
+	GNO_BEGIN/* short   long        default     variable        help */
+ 	GNO_OPTION( "g", 	"graph", 	NULL ,  	& pszGraph ,	"Output Graph file" )
+ 	GNO_END
+ 
+
+	if ( GNO_PARSE( argc , argv ) < 0 )
+	{
+		return 1;
+	}
+	/*
+	 * options parsed
+	 */
+
+	if ( (fd = open( pszGraph , O_RDONLY )) < 0 )
+	{
+		perror( "open" ); return 1;
+	}
+	nret = gnGrpRead( & graph , fd );
+	if ( nret < 0 ) {
+		fprintf( stderr , "gnGrpRead error: %s\n", gnGrpStrerror( & graph ) );
+		return 1;
+	}
+	close( fd );
+
+	printf( "Graph unflatten:\n" );
+	nret = gnGrpUnflatten( & graph );
+	if ( nret < 0 ) {
+		fprintf( stderr , "gnGrpUnflatten error: %s\n", gnGrpStrerror( & graph ) );
+		return 1;
+	}
+	printf( "Done.\n" );
+	gnGrpRelease( & graph );
+	return 0;
+}
