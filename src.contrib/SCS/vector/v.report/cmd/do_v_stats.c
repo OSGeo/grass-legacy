2388537299b9cbc264c6df482f12b2bff98c768b
Radim Blazek <radim.blazek@gmail.com>
2002-10-09 15:57:08 +0000

    length calc fix
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7858 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.contrib/SCS/vector/v.report/cmd/do_v_stats.c
+++ src.contrib/SCS/vector/v.report/cmd/do_v_stats.c
@@ -40,13 +40,13 @@ int do_v_stats(int verbose, int lay_no, char *fd)
 	int cnt=0, line, linea, island;
 	CELL cat_no;
 	int first, tot_cats, n_points;
-	register int area_num, line_num, i, ii, jj, kk;
+	register int area_num, line_num, i, ii, jj, kk, p;
 	char *label_name, null[1];
 	char aline[80];
 	char map_name[100], *mapset;
 	long len, nalloc=1024;
 	double xcent, ycent, *X, *Y;
-	double perim;
+	double length;
 	double f_area;
 	double ot_area;
 	double tot_area;
@@ -245,23 +245,26 @@ verbose=1;
 					sleep(2);
 					return (-1);
 					}
-				/* Calculate line length */
+			   /* Calculate line length */
                            n_points = Points->n_points;
 			   X = Points->x;
 			   Y = Points->y;
 
-			   perim = perimeter(n_points,X,Y);
+			   length = 0.0;
+			   for ( p = 0; p < Points->n_points - 1; p++ ) {
+                               length += G_distance( Points->x[p], Points->y[p], Points->x[p+1], Points->y[p+1]); 
+			   }
 			}
 			else 
 			{
-				perim = 0.0;
+				length = 0.0;
 			}
 
 			if (first)
 			{
 				vect[0].num = cat_no;
 				vect[0].line_cnt = 1;
-				vect[0].length = perim;
+				vect[0].length = length;
 				i = 0;
 				cnt = 1;
 				first = 0;
@@ -273,7 +276,7 @@ verbose=1;
 					if (vect[i].num == cat_no)
 					{
 					vect[i].line_cnt = vect[i].line_cnt + 1;
-					vect[i].length = vect[i].length + perim;
+					vect[i].length = vect[i].length + length;
 					break;
 					}
 				}
@@ -282,7 +285,7 @@ verbose=1;
 					cnt++;
 					vect[i].num = cat_no;
 					vect[i].line_cnt = 1;
-					vect[i].length = perim;
+					vect[i].length = length;
 					qsort (vect, cnt, sizeof (struct Line_List), cmp);
 	                                if (cnt == nalloc)
 	                                 {
