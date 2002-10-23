a368ce1cb1d4ad33c033c5bf0a401d3e7c6bd9a7
Markus Neteler <neteler@gmail.com>
2002-10-23 08:58:40 +0000

    polished
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10734 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/generate_dox.sh
+++ lib/vector/Vlib/generate_dox.sh
@@ -3,19 +3,12 @@
 # dirty trick to generate module-pages for doxygen:
 # Markus Neteler 10/2002
 
-# TODO: improve function name extraction, see below
-
 echo "Extracting all Vect_*() functions into local context pages..."
 
 #get list of all C files:
 LIST=`ls -1 *.c | sed 's/.c$//g'`
 
-#unused:
-#get list of all C files, ignore *_something.c
-#LIST=`ls -1 *.c | sed 's/.c$//g' | cut -d'_' -f1 | sort -u`
-
 #build the doxygen module pages:
-
 for i in $LIST
 do
 
