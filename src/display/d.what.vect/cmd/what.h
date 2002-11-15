7eef4177969277cadb829a82ead94abdb776b6ad
Alex Shevlakov <sixote@yahoo.com>
2002-11-15 10:33:27 +0000

    New feature added: flashing selected vector objects on mouse click
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7940 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.what.vect/cmd/what.h
+++ src/display/d.what.vect/cmd/what.h
@@ -3,6 +3,10 @@ char *openvect(char *);
 /* what.c */
 int what(int, int, int, int, int, char *, char *);
 int show_buttons(int);
+char * colr_str(int);
+/*flash.c*/
+void flash_area(struct Map_info *, plus_t, struct line_pnts *, int);
+void flash_line(struct Map_info *, plus_t, struct line_pnts *, int);
 /* attr.c */
 int disp_attr(char *, char *, int);
 
