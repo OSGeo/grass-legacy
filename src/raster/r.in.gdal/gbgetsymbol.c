696fb9390721a5bbcf9f6dae3e7b47ef760fb2e1
Glynn Clements <glynn@gclements.plus.com>
2002-11-24 21:08:29 +0000

    Conditionalise entire bridge code upon "#ifndef USE_GDAL_H"
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@7953 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/raster/r.in.gdal/gbgetsymbol.c
+++ src/raster/r.in.gdal/gbgetsymbol.c
@@ -30,7 +30,10 @@
  ******************************************************************************
  *
  * $Log$
- * Revision 1.4  2002-04-26 15:05:32  glynn
+ * Revision 1.5  2002-11-24 21:08:29  glynn
+ * Conditionalise entire bridge code upon "#ifndef USE_GDAL_H"
+ *
+ * Revision 1.4  2002/04/26 15:05:32  glynn
  * Improve previous fix; check for either __unix or __unix__
  *
  * Revision 1.3  2002/04/26 15:02:23  glynn
@@ -49,6 +52,8 @@
  * New
  *
  */
+
+#ifndef USE_GDAL_H
 
 #include <stdio.h>
 #include "gdalbridge.h"
@@ -144,4 +149,6 @@ void *GBGetSymbol( const char * pszLibrary, const char * pszSymbolName )
 }
 
 #endif /* def _WIN32 */
+
+#endif /* USE_GDAL_H */
 
