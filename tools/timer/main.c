7801ef65f2ea1a0f9332ab0e3b2b370bbd650f78
Markus Neteler <neteler@gmail.com>
2003-01-22 13:04:02 +0000

    fix
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10970 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/timer/main.c
+++ tools/timer/main.c
@@ -1,19 +1,19 @@
-/* prints the current time in milliseconds */
+/* prints the current time in seconds.milliseconds */
 
 #include <sys/time.h>
 #include <string.h>
 #include <stdio.h>
-#include <unistd.h> 
 
 int main()
 {
-   struct timeval *t; 
+   struct timeval t; 
           
-   if (gettimeofday(t,NULL) == -1) { 
-      puts("gettimeofday error"); 
+   if (gettimeofday(&t, (struct timezone *)NULL) == -1) { 
+      fprintf(stderr,"gettimeofday error"); 
       return 1; 
    } 
-   printf("%li.%li\n",t->tv_sec,t->tv_usec); 
+   fprintf(stdout,"%li.%li\n", t.tv_sec, t.tv_usec); 
+   fflush(stdout);
+
    return 0; 
-                                                                            
 }
