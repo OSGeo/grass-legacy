d594a59a2037185b683e9b84d8353fef2e6c8623
Radim Blazek <radim.blazek@gmail.com>
2003-01-23 15:37:05 +0000

    +Vect_delete(), Vect_copy()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@10990 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/map.c
+++ lib/vector/Vlib/map.c
@@ -14,7 +14,12 @@
 *   	    	for details.
 *
 *****************************************************************************/
+#include <stdio.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
 #include "Vect.h"
+#include "dbmi.h"
 
 /*!
  \fn int Vect_copy_map_lines ( struct Map_info *In, struct Map_info *Out )
@@ -50,5 +55,172 @@ Vect_copy_map_lines ( struct Map_info *In, struct Map_info *Out )
 
     
     /* Not reached */
+}
+
+/*!
+ \fn int Vect_copy ( char *in, char *mapset, char *out )
+ \brief copy a map including attribute tables
+ \return -1 error, 0 success
+ \param  input name, input mapset, output name
+*/
+int 
+Vect_copy ( char *in, char *mapset, char *out )
+{
+    int i, n, ret, type;
+    struct Map_info In, Out;
+    struct field_info *Fi, *Fin;
+
+    G_debug (3, "Copy vector '%s' in '%s' to '%s'", in, mapset, out );
+
+    /* Open input */
+    Vect_set_open_level (1); /* Topo not needed */
+    ret = Vect_open_old (&In, in, mapset);
+    if ( ret < 1 ) {
+	G_warning ( "Cannot open input vector %s in mapset %s", in, mapset );
+	return -1;
+    }
+    
+    /* Open output */
+    ret = Vect_open_new (&Out, out, Vect_is_3d(&In) );
+    if ( ret < 1 ) {
+	G_warning ( "Cannot open output vector %s", out );
+	return -1;
+    }
+    
+    /* Copy lines */
+    ret = Vect_copy_map_lines ( &In, &Out );
+    if ( ret == 1 ) {
+	G_warning ( "Cannot copy vector lines" );
+	return -1;
+    }
+
+    /* Copy tables */
+    n = Vect_get_num_dblinks ( &In );
+    type = GV_1TABLE;
+    if ( n > 1 ) type = GV_MTABLE;
+    for ( i = 0; i < n; i++ ) {
+	Fi = Vect_get_dblink ( &In, i );
+	if ( Fi == NULL ) {
+	    G_warning ( "Cannot get db link info" );
+	    Vect_close ( &In );
+	    Vect_close ( &Out );
+	    return -1;
+	}
+	Fin = Vect_default_field_info ( Out.name, Fi->number, Fi->name, type );
+        G_debug (3, "Copy drv:db:table '%s:%s:%s' to '%s:%s:%s'", 
+	              Fi->driver, Fi->database, Fi->table, Fin->driver, Fin->database, Fin->table );
+	Vect_map_add_dblink ( &Out, Fi->number, Fi->name, Fin->table, Fi->key, Fin->database, Fin->driver);
+        
+	ret = db_copy_table ( Fi->driver, Fi->database, Fi->table, 
+		    Fin->driver, Vect_subst_var(Fin->database,Out.name,G_mapset()), Fin->table );
+	if ( ret == DB_FAILED ) {
+	    G_warning ( "Cannot copy table" );
+	    Vect_close ( &In );
+	    Vect_close ( &Out );
+	    return -1;
+	}
+    }
+    
+    Vect_build ( &Out, NULL );
+    Vect_close ( &In );
+    Vect_close ( &Out );
+
+    return 0;
+}
+
+
+/*!
+ \fn int Vect_delete ( char *map )
+ \brief delete a map including attribute tables
+ \return -1 error, 0 success
+ \param  map name
+*/
+int 
+Vect_delete ( char *map )
+{
+    int i, n, ret;
+    struct Map_info Map;
+    struct field_info *Fi;
+    char   buf[5000];
+    DIR    *dir;
+    struct dirent *ent; 
+
+    G_debug (3, "Delete vector '%s'", map );
+
+    G_chop ( map );
+
+    if ( map == NULL || strlen ( map ) == 0 ) {
+	G_warning ( "Wrong map name '%s'", map );
+	return -1;
+    }
+
+    /* Open input */
+    Vect_set_open_level (1); /* Topo not needed */
+    ret = Vect_open_old (&Map, map, G_mapset());
+    if ( ret < 1 ) {
+	G_warning ( "Cannot open vector %s", map );
+	return -1;
+    }
+
+    /* Delete PostGis tables (shoud be in some _post.c) */
+    if ( Map.format == GV_FORMAT_POSTGIS ) {
+	ret = Vect_delete_post_tables (  &Map );
+	if ( ret == -1 ) {
+	    G_warning ("Cannot delete PostGIS tables" );
+	    return -1;
+	}
+    }
+    
+    /* Delete all tables */
+    n = Vect_get_num_dblinks ( &Map );
+    for ( i = 0; i < n; i++ ) {
+	Fi = Vect_get_dblink ( &Map, i );
+	if ( Fi == NULL ) {
+	    G_warning ( "Cannot get db link info" );
+	    Vect_close ( &Map );
+	    return -1;
+	}
+        G_debug (3, "Delete drv:db:table '%s:%s:%s'", Fi->driver, Fi->database, Fi->table);
+        
+	ret = db_delete_table ( Fi->driver, Fi->database, Fi->table );
+	if ( ret == DB_FAILED ) {
+	    G_warning ( "Cannot delete table" );
+	    Vect_close ( &Map );
+	    return -1;
+	}
+    }
+    Vect_close ( &Map );
+
+    /* Delete all files from vector/name directory */
+    sprintf ( buf, "%s/%s/vector/%s", G_location_path(), G_mapset(), map );
+    G_debug (3, "opendir '%s'", buf ); 
+    dir = opendir( buf );
+    if (dir == NULL) {
+	G_warning ( "Cannot open directory '%s'", buf );
+	return -1;
+    }
+
+    while ( (ent = readdir (dir)) ) {
+	if ( (strcmp (ent->d_name, ".") == 0) || (strcmp (ent->d_name, "..") == 0) ) continue;
+	sprintf ( buf, "%s/%s/vector/%s/%s", G_location_path(), G_mapset(), map, ent->d_name );
+	G_debug (3, "delete file '%s'", buf );
+	ret = unlink ( buf );
+	if ( ret == -1 ) { 
+	    G_warning ( "Cannot delete file '%s'", buf );
+	    closedir (dir);
+	    return -1;
+	}
+    }
+    closedir (dir);
+
+    sprintf ( buf, "%s/%s/vector/%s", G_location_path(), G_mapset(), map );
+    G_debug (3, "delete directory '%s'", buf );
+    ret = rmdir ( buf );
+    if ( ret == -1 ) { 
+	G_warning ( "Cannot delete directory '%s'", buf );
+	return -1;
+    }
+
+    return 0;
 }
 
