740c4e961ef540d372d3e91478f5faf42f039c0a
Radim Blazek <radim.blazek@gmail.com>
2003-01-28 14:14:56 +0000

    Write dig_att
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@11021 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.ascii/local_proto.h
+++ vector/v.ascii/local_proto.h
@@ -1,7 +1,7 @@
 /* asc_to_bin.c */
 int asc_to_bin(FILE *, struct Map_info *, int pnt);
 /* bin_to_asc.c */
-int bin_to_asc(FILE *, struct Map_info *, int ver, int pnt);
+int bin_to_asc(FILE *, FILE *, struct Map_info *, int ver, int pnt);
 
 int write_head ( FILE * dascii, struct Map_info *Map);
 int read_head ( FILE * dascii, struct Map_info *Map );
