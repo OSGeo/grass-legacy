f6522fa6474f384c345d5928d0331497de58a4fd
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2003-02-04 09:04:02 +0000

    Fix G_find_key_value usage
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8086 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/libes/image3/convert_ll.c
+++ src/libes/image3/convert_ll.c
@@ -39,10 +39,7 @@ int convert_to_ll (Control_Points_LL *cpll, Control_Points_2D *cptemp)
   char   *ellps;    /* target ellps paramet */
   char *tmp_bob;
 
-  /** TODO -- fix this and free at end  **/
-  ellps = (char *) G_malloc (256 * sizeof (char));
-
-
+   
   /* initialize */
   cpll->count  = 0;
   cpll->e1     = NULL;
@@ -82,15 +79,16 @@ if (!strcmp(tmp_bob, "ll")) return 0;
 
   /* save the target ellps parameter */
   ellps = G_find_key_value ("ellps", targ_proj_keys);
-  if( ellps == NULL )
-      sprintf(ellps, "wgs84");
 
   /* set up ll_proj_keys */
   ll_proj_keys = G_create_key_value();
   ll_unit_keys = G_create_key_value();
   G_set_key_value ("name", "Lat/Lon", ll_proj_keys);
   G_set_key_value ("proj", "ll", ll_proj_keys);
-  G_set_key_value ("ellps", ellps, ll_proj_keys);
+  if( ellps != NULL )
+      G_set_key_value ("ellps", ellps, ll_proj_keys);
+  else
+      G_set_key_value ("ellps", "wgs84", ll_proj_keys);
 
   /*   There is no longer a pj_get_kv() in libproj.a   Sep-15-1999 */
   if (pj_get_kv(&ll_proj_info, ll_proj_keys, ll_unit_keys) < 0) {
@@ -103,7 +101,7 @@ if (!strcmp(tmp_bob, "ll")) return 0;
   G_free_key_value (targ_unit_keys);
   G_free_key_value (ll_proj_keys);
   G_free_key_value (ll_unit_keys);
-  G_free (ellps);
+   
 
   /* loop through all the temp points */
   for (i = 0; i < cptemp->count; i++) {
@@ -167,9 +165,6 @@ convert_from_ll (Control_Points_LL *cpll, Control_Points_2D *cptemp)
   struct Key_Value  *ll_proj_keys,   *ll_unit_keys;
   char   *ellps;    /* target ellps paramet */
 
-  /** TODO -- fix this and free at end  **/
-  ellps = (char *) G_malloc (256 * sizeof (char));
-
 
   /* initialize */
   cptemp->count  = 0;
@@ -203,15 +198,16 @@ convert_from_ll (Control_Points_LL *cpll, Control_Points_2D *cptemp)
 
   /* save the target ellps parameter */
   ellps = G_find_key_value ("ellps", targ_proj_keys);
-  if( ellps == NULL )
-      sprintf(ellps, "wgs84");
 
   /* set up ll_proj_keys */
   ll_proj_keys = G_create_key_value();
   ll_unit_keys = G_create_key_value();
   G_set_key_value ("name", "Lat/Lon", ll_proj_keys);
   G_set_key_value ("proj", "ll", ll_proj_keys);
-  G_set_key_value ("ellps", ellps, ll_proj_keys);
+  if( ellps != NULL)
+      G_set_key_value ("ellps", ellps, ll_proj_keys);
+  else
+      G_set_key_value ("ellps", "wgs84", ll_proj_keys);
 
   /*   There is no longer a pj_get_kv() in libproj.a   Sep-15-1999 */
   if (pj_get_kv(&ll_proj_info, ll_proj_keys, ll_unit_keys) < 0) {
@@ -223,7 +219,7 @@ convert_from_ll (Control_Points_LL *cpll, Control_Points_2D *cptemp)
   G_free_key_value (targ_unit_keys);
   G_free_key_value (ll_proj_keys);
   G_free_key_value (ll_unit_keys);   
-  G_free (ellps);
+
    
   for (i = 0; i < cpll->count; i++) {
 
