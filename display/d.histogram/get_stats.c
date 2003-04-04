6d88d9822d9812dc8336790ec7f26afd022dd207
Eric G. Miller <egm2@jps.net>
2003-04-04 05:00:58 +0000

    Revert revert and remove va_copy.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@11357 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.histogram/get_stats.c
+++ display/d.histogram/get_stats.c
@@ -1,9 +1,38 @@
 #include <stdio.h> 
 #include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 #include <unistd.h>
 #include "gis.h"
 #include "options.h"
 #include "dhist.h"
+
+static char *
+mk_command (const char *fmt, int nargs, ...)
+{
+    /* asprintf() would solve this problem better */
+    size_t len = strlen(fmt) + 1;
+    char *cmd;
+    va_list    ap;
+    
+    va_start (ap, nargs);
+    
+    while (nargs--) {
+        cmd = va_arg (ap, char *);
+        len += strlen (cmd);
+    }
+
+    va_end (ap);
+    
+    cmd = G_malloc (len);
+    
+    va_start (ap, nargs);
+    vsprintf (cmd, fmt, ap);
+
+    va_end (ap);
+
+    return cmd;
+}
 
 int get_stats(
 char *mapname,
@@ -11,10 +40,11 @@ char *mapset,
 struct stat_list *dist_stats,      /* linked list of stats */
 int quiet)
 {
-    char              buf[4096];    /* input buffer for reading stats */
+    char              buf[1024];    /* input buffer for reading stats */
     int               done = 0;
     char              *tempfile;     /* temp file name */
     char              *fullname;
+    char              *cmd;
     FILE              *fd;      /* temp file pointer */
 
     long int          cat;             /* a category value */
@@ -41,11 +71,18 @@ int quiet)
         if(G_read_fp_range (map_name, mapset, &fp_range) <= 0)
 	      G_fatal_error("Can't read frange file");
     }
-    if(cat_ranges)
-        sprintf(buf,"r.stats -Cr%s%s '%s' > %s\n", type==COUNT?"c":"a", quiet?"q":"", fullname, tempfile);
-    else
-	sprintf(buf,"r.stats -r%s%s '%s' nsteps=%d> %s\n", type==COUNT?"c":"a", quiet?"q":"", fullname, nsteps, tempfile);
-    if(system(buf))
+    if(cat_ranges) {
+        cmd = mk_command ("r.stats -Cr%s%s '%s' > '%s'\n", 4, 
+                type==COUNT?"c":"a", quiet?"q":"", fullname, tempfile);
+    }
+    else {
+        sprintf (buf, "%d", nsteps);
+        cmd = mk_command ("r.stats -r%s%s '%s' nsteps=%s > '%s'\n", 5, 
+                type==COUNT?"c":"a", quiet?"q":"", fullname, 
+                buf, tempfile);
+    }
+   
+    if(system(cmd))
         G_fatal_error ("%s: ERROR running r.stats", G_program_name());
 
     /* open temp file and read the stats into a linked list */
