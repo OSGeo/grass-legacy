a812d31b9ec86421de8d6cc4fdd54258468576b1
Mike Thomas <miketh@brisbane.paradigmgeo.com>
2003-04-05 22:59:53 +0000

    Calls to libgis from TCL is working now.  Will continue adding tests.
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8282 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.todo/swig/test.tcl
+++ src.todo/swig/test.tcl
@@ -1,7 +1,95 @@
-catch { load ./libgis.so libgis}
-catch { load ./libgis.dll libgis}
+package require cmdline
 
-set mod [_G_define_module]
-set get [_G_define_option]
-puts $mod
-puts $get
+# An OK button message box
+proc okbox { msg } {
+    tk_messageBox -icon info -message $msg  -type ok
+}
+
+# Do this better as errors in finding 
+# DLL's are otherwise not reported and/or
+# dealt with leading to unexplained problems
+# when DLL functions are referenced.
+if {![string compare $tcl_platform(platform) windows]} {
+    load libgis.dll libgis
+} else {
+    load ./libgis.so libgis
+}
+
+# okbox $env(HOME)
+
+if { [catch {set a $env(HOME)} gisrcdir] } {
+    okbox "You don't have a HOME environment variable, please specify where you want to keep GRASS initialisation information"
+    set gisrcdir [tk_chooseDirectory -title "Select your Grass initialisation directory"]
+    set env(HOME) $gisrcdir
+}
+
+# okbox "gisrcdir = $gisrcdir"
+# okbox "HOME     = $env(HOME)"
+
+if { [catch {set a $env(GISBASE)}] } {
+    set env(GISBASE) [tk_chooseDirectory -title "Select your Grass installation directory"]
+}
+
+# set env(GISBASE) e:/cvs/grassmingw/dist.i686-pc-mingw32
+
+# GIS_LOCK pid of startup shell script
+# GISRC GRASS environment file
+set env(GIS_LOCK) [pid]
+set h $env(HOME)
+set env(GISRC) $h/.grassrc5
+# okbox "GIS_LOCK = $env(GIS_LOCK),  GISRC = $env(GISRC)"
+
+set env(LOCATION_NAME) spearfish
+set env(GISDBASE) e:/data/grass
+set cmdname [::cmdline::getArgv0]
+
+okbox "GISBASE = $env(GISBASE),  Command name to be used for init: $cmdname"
+if { [catch {G_gisinit $cmdname} res ] } {
+    okbox "G_gisinit exception: $res"
+}
+# okbox Initted
+
+set our_region [new_Cell_head]
+if { [catch {G_get_window $our_region} res] } {
+    okbox "G_get_window exception: $res"
+}
+set ewr [Cell_head_ew_res_get $our_region]
+set zone [Cell_head_zone_get $our_region]
+set cols [Cell_head_cols_get $our_region]
+set rows [Cell_head_rows_get $our_region]
+
+set cols1 [G_window_cols]
+set rows1 [G_window_rows]
+
+
+okbox "our_region ew res $ewr, zone $zone, rows $rows $rows1,  cols $cols $cols1 "
+
+# GISBASE top level grass module dir
+
+# $(HOME)/.grassrc5 holds next three vars
+# GISDBASE - directory in which locations appear
+set dbmapset [G_gisdbase]/[G_location]/[G_mapset]
+okbox "Our mapset: $dbmapset"
+
+# LOCATION_NAME - current location
+# G_location
+
+# MAPSET - current mapset
+# G_mapset
+
+# Also MONITOR, PAINTER, DIGITIZER
+# G_getenv
+
+# Path to current location 
+# G_location_path
+
+# Current and default region from WIND file
+# G_get_window
+# G_get_default_window
+
+# descriptive name for location
+# G_myname
+
+# program name
+# G_program_name
+
