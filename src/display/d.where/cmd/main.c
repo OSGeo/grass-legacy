bead7a76b7625934ee8406b5fd2cdc44cb6cd795
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2003-04-14 16:06:26 +0000

    Fix d.where to use proj instead of coorcnv lib (good for GRASS 5.1) and add some extra functionality afforded by use of proj over coorcnv
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8307 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.where/cmd/main.c
+++ src/display/d.where/cmd/main.c
@@ -1,26 +1,26 @@
 #include <string.h>
 #include "gis.h"
-#include "CC.h"
+#include "gprojects.h"
 #include "display.h"
 #include "raster.h"
 #include "local_proto.h"
 
+struct pj_info iproj, oproj;
+
 int main (int argc, char **argv)
 {
-	struct GModule *module;
-    struct Option *spheroid;
-    struct Flag *once, *decimal;
+    struct GModule *module;
+    struct Flag *once, *decimal, *latlong, *wgs84;
     char s_names[2048];
     char *name;
-    int have_spheroid;
-    double a,e;
+    int have_spheroid = 0;
     int i;
 
 /* Initialize the GIS calls */
     G_gisinit(argv[0]) ;
 
-	module = G_define_module();
-	module->description =
+    module = G_define_module();
+    module->description =
 		"Identifies the geographic coordinates associated with "
 		"point locations in the active frame on the graphics monitor.";
 
@@ -31,45 +31,69 @@ int main (int argc, char **argv)
     decimal = G_define_flag() ;
     decimal->key        = 'd' ;
     decimal->description= "Output lat/long in decimal degree";
+   
+    latlong = G_define_flag() ;
+    latlong->key        = 'l' ;
+    latlong->description= "Output lat/long referenced to current ellipsoid";
 
-    spheroid = G_define_option() ;
-    spheroid->key        = "spheroid" ;
-    spheroid->type       = TYPE_STRING ;
-    spheroid->required   = NO ;
-    spheroid->description= "Name of a spheroid (for lat/lon coordinate conversion)";
-    spheroid->options    = s_names;
-
-    *s_names = 0;
-    for (i = 0; name = CC_spheroid_name(i); i++)
-    {
-	if(i) strcat (s_names, ",");
-	strcat (s_names,name);
-    }
-
-
+    wgs84 = G_define_flag() ;
+    wgs84->key        = 'w' ;
+    wgs84->description= "Output lat/long referenced to WGS84 ellipsoid using datum\n"
+                        "       transformation parameters defined in current location if available";
+     
+     
     if (argc > 1 && G_parser(argc,argv))
 	exit(1);
 
-    if ( (G_projection() != PROJECTION_LL) )
-       decimal->answer=0;  /* -d only makes sense in LL projection */
-
-    have_spheroid = 0;
-    if(name = spheroid->answer)
+    if ( ((G_projection() == PROJECTION_LL) && wgs84->answer) || 
+	 ((G_projection() != PROJECTION_LL) && (latlong->answer || wgs84->answer)) )
+       have_spheroid = 1;
+   
+    if(have_spheroid == 1)
     {
-	if ( (G_projection() != PROJECTION_UTM) )
-	{
-	    fprintf (stderr,"WARNING: %s=%s: only valid for UTM databses. Ignored\n",
-		spheroid->key, spheroid->answer);    
-	}
-	else if (CC_get_spheroid (name, &a, &e) == 0)
-	    G_fatal_error("ERROR: %s=%s: unknown spheroid",
-		spheroid->key, spheroid->answer);    
-	else
-	{
-	    CC_u2ll_spheroid_parameters (a,e);
-	    CC_u2ll_zone (G_zone());
-	    have_spheroid = 1;
-	}
+       struct Key_Value *in_proj_info, *in_unit_info;
+       struct Key_Value *out_proj_info, *out_unit_info;
+       double a, es;
+       char buff[100];
+       
+       /* read current projection info */
+       if ((in_proj_info = G_get_projinfo()) == NULL)
+          G_fatal_error("Can't get projection info of current location");
+
+       if ((in_unit_info = G_get_projunits()) == NULL)
+          G_fatal_error("Can't get projection units of current location");
+
+       if (pj_get_kv(&iproj, in_proj_info, in_unit_info) < 0)
+          G_fatal_error("Can't get projection key values of current location");
+	
+       /* set output projection to lat/long w/ same ellipsoid as input */
+       out_proj_info = G_create_key_value();
+       out_unit_info = G_create_key_value();
+       
+       G_set_key_value("proj", "ll", out_proj_info);
+
+       if(!wgs84->answer)
+       {
+          G_get_ellipsoid_parameters(&a, &es);
+          sprintf(buff, "%f", a);
+          G_set_key_value("a", buff, out_proj_info);
+          sprintf(buff, "%f", es);
+          G_set_key_value("es", buff, out_proj_info);
+       }
+       else
+	  G_set_key_value("datum", "wgs84", out_proj_info);
+       
+       G_set_key_value("unit", "degree", out_unit_info);
+       G_set_key_value("units", "degrees", out_unit_info);
+       G_set_key_value("meters", "1.0", out_unit_info);
+       
+       if (pj_get_kv(&oproj, out_proj_info, out_unit_info) < 0)
+          G_fatal_error("Unable to set up lat/long projection parameters");       
+		
+       G_free_key_value( in_proj_info );
+       G_free_key_value( in_unit_info );
+       G_free_key_value( out_proj_info );
+       G_free_key_value( out_unit_info );
     }
 
     if (R_open_driver() != 0)
