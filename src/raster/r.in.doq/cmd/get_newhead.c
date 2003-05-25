3469ceaefaf58fc9b8008514b69816eb8cfad9a0
Glynn Clements <glynn@gclements.plus.com>
2003-05-25 23:22:04 +0000

    Fix exit() usage
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8432 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/raster/r.in.doq/cmd/get_newhead.c
+++ src/raster/r.in.doq/cmd/get_newhead.c
@@ -44,7 +44,7 @@ else if (strstr(buf,"RASTER_ORDER") != NULL) {
 /*** Check data ordering ******/
 	if (strstr(buf,"LEFT_RIGHT/TOP_BOTTOM") == NULL) {
 		fprintf(stderr,"Unable to handle this data ordering: %s\n",buf);
-		exit();
+		exit(1);
 		}
 	}
 else if (strstr(buf,"SAMPLES_AND_LINES") != NULL) {
@@ -63,7 +63,7 @@ else if (strstr(buf,"BAND_CONTENT") != NULL) {
 /*********** Get # bands now handlws one ***************/
 	if (strstr(buf,"BLACK&WHITE") == NULL) {
 		fprintf(stderr,"There are multiple bands in this image.\n This routine presently handles only one\n");
-		exit();
+		exit(1);
 		}
 	}
 else if (strstr(buf,"HORIZONTAL_DATUM") != NULL) {
