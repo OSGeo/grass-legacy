8f5ae234508b3683134b072204efd98e140d11d5
Glynn Clements <glynn@gclements.plus.com>
2003-05-27 03:44:05 +0000

    Fix embedded newlines in string literals
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8437 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src.garden/grass.postgresql/g.stats.pg/pgStats.c
+++ src.garden/grass.postgresql/g.stats.pg/pgStats.c
@@ -36,8 +36,8 @@ int pgStats(tab, col, freq, where, verbose)
     }
 
     if (freq) {
-	sprintf(SQL_stmt, "SELECT count(*) as count, %s from %s %s 
-         group by %s order by count(*)", col, tab, wherecl, col);
+	sprintf(SQL_stmt, "SELECT count(*) as count, %s from %s %s\n"
+         "group by %s order by count(*)", col, tab, wherecl, col);
     }
     else {
 	sprintf(SQL_stmt, "select avg(%s) from %s where oid is null", col,
@@ -48,13 +48,13 @@ int pgStats(tab, col, freq, where, verbose)
 
 	PQclear(res);
 	if (can_avg_col) {
-	    sprintf(SQL_stmt, "SELECT min(%s) as Min, max(%s) as Max, avg(%s) as Mean
-       FROM %s %s", col, col, col, tab,
+	    sprintf(SQL_stmt, "SELECT min(%s) as Min, max(%s) as Max, avg(%s) as Mean\n"
+       "FROM %s %s", col, col, col, tab,
 		    wherecl);
 	}
 	else {
-	    sprintf(SQL_stmt, "SELECT min(%s) as Min, max(%s) as Max
-       FROM %s %s", col, col, tab, wherecl);
+	    sprintf(SQL_stmt, "SELECT min(%s) as Min, max(%s) as Max\n"
+       "FROM %s %s", col, col, tab, wherecl);
 	}
     }
 
