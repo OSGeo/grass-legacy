efcea496dc1cf90afec303e48da5395e8609d86d
Bob Covill <bcovill@tekmap.ns.ca>
2003-06-30 16:46:57 +0000

    initial d.nviz
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@11753 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ display/d.nviz/local.h
@@ -0,0 +1,7 @@
+static int move(int,int);
+static int cont(int,int);
+static int min_range[5], max_range[5];
+static int which_range;
+static int change_range;
+static double dist, e=0, n=0;
+static double DIST, HT;
