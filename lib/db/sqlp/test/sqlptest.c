6e700e43040f544d6c978dbf66596e91335def0e
Radim Blazek <radim.blazek@gmail.com>
2003-07-22 08:42:50 +0000

    allow new line in string value
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@11797 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/db/sqlp/test/sqlptest.c
+++ lib/db/sqlp/test/sqlptest.c
@@ -1,32 +1,54 @@
 #include <stdlib.h>
+#include <string.h>
 #include "gis.h"
+#include "dbmi.h"
 #include "sqlp.h"
 
 int
 main (int argc, char **argv) 
 {
     SQLPSTMT *st;
-    char sql[5000];
+    char buf[5000], buf2[5000];
+    dbString stmt;
+    int len;
+    
 
     st = sqpInitStmt();
     
-    
-    while ( fgets ( sql, 5000, stdin) )
-      {
-        fprintf (stdout, "\nInput statement: %s", sql);
-    
-        st->stmt = sql;
-        sqpInitParser(st);
-    
-        if ( yyparse() != 0 )
-        {
-            fprintf (stdout, "Error: statement was not parsed\n");
-            sqpFreeStmt(st); 
-            return (1);
-        } 
+    db_init_string (&stmt);
 
-        sqpPrintStmt(st);
-      }
+    while ( fgets (buf, 5000, stdin) ) {
+	fprintf ( stdout, "Input row: -->>%s<<--\n", buf );
+	strcpy ( buf2, buf );
+	G_chop (buf2);
+	len = strlen (buf2);
+	
+	if ( buf2[ len - 1 ] == ';' ) {  /* end of statement */
+	    buf2 [len - 1] = 0;          /* truncate ';' */
+	    db_append_string (&stmt, buf2);
+            
+	    st->stmt = db_get_string(&stmt);
+            sqpInitParser(st);
+
+	    fprintf ( stdout, "Input statement: -->>%s<<--\n", st->stmt );
+
+	    if ( yyparse() != 0 )
+	    {
+		fprintf (stdout, "Error: statement was not parsed\n");
+		sqpFreeStmt(st); 
+		return (1);
+	    } 
+
+	    sqpPrintStmt(st);
+
+	    db_zero_string (&stmt);
+
+        } else { 
+	    db_append_string (&stmt, buf);
+	}
+    }
+
+    
     sqpFreeStmt(st); 
 
     exit(0);
