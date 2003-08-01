6910b51aa20ba4d3e3315e279c9572c813f1e36e
Radim Blazek <radim.blazek@gmail.com>
2003-08-01 05:59:44 +0000

    new tools
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@11823 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.clean/proto.h
+++ vector/v.clean/proto.h
@@ -1,7 +1,9 @@
 #define TOOL_BREAK    1 /* break at intersection */
 #define TOOL_RMDUPL   2 /* remove duplicates */
 #define TOOL_RMDANGLE 3 /* remove dangles */
-#define TOOL_RTDANGLE 4 /* retype 'boundary' dangles to 'line' */
+#define TOOL_CHDANGLE 4 /* retype 'boundary' dangles to 'line' */
 #define TOOL_SVTLX    5 /* snap vertex to a line and create new vertex at that line */
 #define TOOL_RMDAC    6 /* remove duplicate area centroids */
 #define TOOL_BPOL     7 /* break polygons on shared points with different angles */
+#define TOOL_RMBRIDGE 8 /* remove bridges connecting area and island or 2 islands */
+#define TOOL_CHBRIDGE 9 /* change the type of bridges connecting area and island or 2 islands from boundary to line */
