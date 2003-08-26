62cd41ade365aad0dee457b044084f8a4714c28a
Markus Neteler <neteler@gmail.com>
2003-08-26 13:40:45 +0000

    reverted clone removal as being more complicated than expected
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8577 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/imagery/i.ortho.photo/photo.2image/equ.c
+++ src/imagery/i.ortho.photo/photo.2image/equ.c
@@ -3,7 +3,7 @@
 int 
 Compute_equation (void)
 {
-    group.ref_equation_stat = I_compute_georef_equations(&group.photo_points,
+    group.ref_equation_stat = I_compute_ref_equations(&group.photo_points,
 	group.E12, group.N12, group.E21, group.N21);
 
     return 0;
