50abc0c17534bd81e6b607f19bfc7a2edb424776
Markus Neteler <neteler@gmail.com>
2003-10-21 10:10:12 +0000

    Ivan Michelazzi: fixed wrong menu references, message cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8743 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/imagery/i.ortho.photo/photo.rectify/cp.c
+++ src/imagery/i.ortho.photo/photo.rectify/cp.c
@@ -12,24 +12,24 @@ int get_conz_points (void)
     /* I_convert_con_points (group.name, &group.control_points, &group.photo_points,  group.E12, group.N12); */
 
 
-    sprintf (msg, "Control Z Point file for group [%s] in [%s] - ",
+    sprintf (msg, "Control Z Point file for group [%s] in [%s] \n \n",
 	group.name,G_mapset());
 
-    fprintf (stderr,"Computing equations...\n");
+    fprintf (stderr,"Computing equations...\n\n");
 
     Compute_ortho_equation();
 
     switch (group.con_equation_stat)
     {
     case -1:
-	strcat (msg, "Poorly placed control points.\n");
+	strcat (msg, "Poorly placed Control Points!\n");
 	strcat (msg, "Can not generate the transformation equation.\n");
-	strcat (msg, "Run OPTION 4 again!\n");
+	strcat (msg, "Run OPTION 7 again!\n");
 	break;
     case 0:
-	strcat (msg, "No active control points");
+	strcat (msg, "No active Control Points!\n");
 	strcat (msg, "Can not generate the transformation equation.\n");
-	strcat (msg, "Run OPTION 4 !\n");
+	strcat (msg, "Run OPTION 7 !\n");
 	break;
     default:
 	return 1;
@@ -45,22 +45,23 @@ int get_ref_points (void)
     if(!I_get_ref_points (group.name, &group.photo_points))
 	exit(0);
 
-    sprintf (msg, "Reference Point file for group [%s] in [%s] - ",
-	group.name, G_mapset());
+    sprintf (msg, "Reference Point file for group [%s] in [%s] \n \n",
+      group.name, G_mapset());
 
     Compute_ref_equation();
     switch (group.ref_equation_stat)
     {
-    case -1:
-	strcat (msg, "Poorly placed reference points.");
-	strcat (msg, "Can not generate the transformation equation.");
-	strcat (msg, "Run OPTION 2 again!\n");     
-	break;
-    case 0:
-	strcat (msg, "No active control points\n");
+    case -1: 
+	strcat (msg, "Poorly placed Reference Points!\n");
 	strcat (msg, "Can not generate the transformation equation.\n");
-	strcat (msg, "Run OPTION 2!\n");
+	strcat (msg, "Run OPTION 5 again!\n");     
 	break;
+        
+    case 0:
+        strcat (msg, "No active Reference Points!\n");
+        strcat (msg, "Can not generate the transformation equation.\n");
+        strcat (msg, "Run OPTION 5!\n");
+        break;
     default:
 	E12a = E12[0]; E12b = E12[1]; E12c = E12[2];
 	N12a = N12[0]; N12b = N12[1]; N12c = N12[2];
