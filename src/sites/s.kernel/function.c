6d63170cf9c457dee86921419dd4eb6d4b533394
Markus Neteler <neteler@gmail.com>
2003-10-31 14:36:56 +0000

    Stefano Menegon <menegon@itc.it>: add MASK support, bug fix on gaussian kernel function
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8791 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/sites/s.kernel/function.c
+++ src/sites/s.kernel/function.c
@@ -64,6 +64,60 @@ int read_list_of_sites(list,Lsite)
   return nfiles;
 }
 
+double gaussian2dBySigma(d,sigma)
+     /*
+       probability for gaussian distribution 
+     */
+     double sigma, d;
+{
+  double res;
+  res=1/(2.*M_PI*sigma*sigma)*exp(-d*d/(2.*sigma*sigma));
+  return(res);
+}
+
+double gaussian2dByTerms(d,term1,term2)
+     /*
+       term1=1./(2.*M_PI*sigma*sigma)
+       term2=2.*sigma*sigma;
+     */
+     double d, term1, term2;
+{
+  double res;
+  res=term1*exp(-d*d/term2);
+  return(res);
+}
+
+
+double segno(double x){
+  double y; 
+  y = (x > 0 ? 1. : 0.) + (x < 0 ? -1. : 0.);
+  return y;
+}
+
+
+double kernel1(d,rs,lambda)
+     /*
+     */
+     double d, rs, lambda;
+{
+  double res,a;
+  a=lambda-1.;
+  if(lambda==1.){
+    res=1./(M_PI*(d*d+rs*rs));
+  }else{
+    res=segno(a)*(a/M_PI)*(pow(rs,2.*a))*(1/pow(d*d+rs*rs,lambda));
+  }
+    /*  res=1./(M_PI*(d*d+rs*rs));*/
+  return(res);
+}
+
+double invGaussian2d(sigma,prob)
+     double sigma, prob;
+{
+  double d;
+  d = sqrt(-2*sigma*sigma*log(prob*M_PI*2*sigma*sigma));
+  return(d);
+}     
 
 double euclidean_distance(x,y,n)
      /*
@@ -87,3 +141,4 @@ double euclidean_distance(x,y,n)
 }
 
   
+
--- src/sites/s.kernel/global.h
+++ src/sites/s.kernel/global.h
@@ -1,15 +1,20 @@
 #include "gis.h"
 #include "site.h"
-#define PIG           3.14159265358979323846 
 
 typedef struct
-/*manegement of lists of site files*/
+/*management of lists of site files*/
 {
-  char *name; /*name of site file*/
+  char *name;   /*name of site file*/
   Site **sites; /*sites contained within the site file*/
-  int nsites; /*number of sites*/
+  int nsites;   /*number of sites*/
 } ListSite;
 
 int read_list_of_sites();
 void compute_distance();
 double euclidean_distance();
+double gaussian2dBySigma();
+double invGaussian2d();
+double gaussian2dByTerms();
+int readSitesFiles();
+double kernel1();
+double segno();
