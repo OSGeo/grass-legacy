38ed7085da13d889b400eb449c1aa63f1fc8abe9
Glynn Clements <glynn@gclements.plus.com>
2003-10-31 16:21:35 +0000

    Re-write PNG driver to remove dependency upon GD
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12144 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/drivers/PNG/Graph_Clse.c
+++ display/drivers/PNG/Graph_Clse.c
@@ -3,20 +3,102 @@
  * termination time.
  */
 
-#include "png.h"
+#include <stdio.h>
+#include <png.h>
+
 #include "gis.h"
-#include "driverlib.h"
+#include "pngdriver.h"
 
-
-int Graph_Close(void)
+int
+Graph_Close(void)
 {
-#if defined(HAVE_GDIMAGEPNG)
-	gdImagePng(im, output);
-#elif defined(HAVE_GDIMAGEGIF)
-	gdImageGif(im, output);
-#else
-# error Neither PNG nor GIF supported
-#endif
+	static jmp_buf jbuf;
+	static png_struct *png_ptr;
+	static png_info *info_ptr;
+	FILE *output;
+	int x, y;
+	unsigned int *p;
+	png_bytep line;
+
+	png_ptr = png_create_write_struct(
+		PNG_LIBPNG_VER_STRING, &jbuf, NULL, NULL);
+	if (!png_ptr)
+		G_fatal_error("PNG: couldn't allocate PNG structure");
+
+	info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+		G_fatal_error("PNG: couldn't allocate PNG structure");
+
+	if (setjmp(png_jmpbuf(png_ptr)))
+		G_fatal_error("error writing PNG file");
+
+	output = fopen(file_name, "wb");
+	if (!output)
+		G_fatal_error("PNG: couldn't open output file %s", file_name);
+
+	png_init_io(png_ptr, output);
+
+	png_set_IHDR(
+		png_ptr, info_ptr,
+		width, height, 8,
+		true_color ? PNG_COLOR_TYPE_RGB_ALPHA : PNG_COLOR_TYPE_PALETTE,
+		PNG_INTERLACE_NONE,
+		PNG_COMPRESSION_TYPE_DEFAULT,
+		PNG_FILTER_TYPE_DEFAULT);
+
+	if (!true_color)
+	{
+		png_color png_pal[256];
+		png_byte trans = (png_byte) transparent;
+		int i;
+
+		for (i = 0; i < 256; i++)
+		{
+			png_pal[i].red   = palette[i][0];
+			png_pal[i].green = palette[i][1];
+			png_pal[i].blue  = palette[i][2];
+		}
+
+		png_set_PLTE(png_ptr, info_ptr, png_pal, 256);
+
+		png_set_tRNS(png_ptr, info_ptr, &trans, 1, NULL);
+	}
+
+	png_set_invert_alpha(png_ptr);
+
+	png_write_info(png_ptr, info_ptr);
+
+	line = G_malloc(width * 4);
+
+	for (y = 0, p = grid; y < height; y++)
+	{
+		png_bytep q = line;
+
+		if (true_color)
+			for (x = 0; x < width; x++, p++)
+			{
+				unsigned int c = *p;
+				*q++ = (png_byte) (c >> 16);
+				*q++ = (png_byte) (c >>  8);
+				*q++ = (png_byte) (c >>  0);
+				*q++ = (png_byte) (c >> 24);
+			}
+		else
+			for (x = 0; x < width; x++, p++, q++)
+				*q = (png_byte) *p;
+
+		png_write_row(png_ptr, line);
+	}
+
+	G_free(line);
+
+	png_write_end(png_ptr, info_ptr);
+
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+
 	fclose(output);
-	gdImageDestroy(im);
+	G_free(grid);
+
+	return 0;
 }
+
--- display/drivers/PNG/Panel.c
+++ display/drivers/PNG/Panel.c
@@ -6,108 +6,23 @@
  * a pointer name to the saved image.
  */
 
-#include <unistd.h>
-#include <fcntl.h>
+#include <stdio.h>
 
-#include "png.h"
-
-static void *get_ptr(int i)
-{
-#ifdef HAVE_GDIMAGECREATETRUECOLOR
-    return true_color
-	? (void *) im->tpixels[i]
-	: (void *) im->pixels[i];
-#else
-    return (void *) im->pixels[i];
-#endif
-}
+#include "pngdriver.h"
 
 int Panel_save(char *name, int top, int bottom, int left, int right)
 {
-    int width, height;
-    int bytes, xoffset, depth;
-    int fd;
-    int i;
-
-    /* Adjust panel edges if outside window necessary */
-    if (top < screen_top)
-        top = screen_top;
-    if (bottom > screen_bottom)
-        bottom = screen_bottom;
-    if (left < screen_left)
-        left = screen_left;
-    if (right > screen_right)
-        right = screen_right;
-
-    height = bottom - top;
-    width = right - left;
-
-    bytes = true_color ? width * 4 : width;
-    xoffset = 0;
-    depth = true_color ? 32 : 8;
-
-    /* open the file */
-    fd = creat(name, 0644);
-    if (fd < 0)
-    {
-	perror("unable to create panel file");
-	return -1;
-    }
-
-    /* write the header */
-    write(fd, &left,    sizeof(left));
-    write(fd, &top,     sizeof(top));
-    write(fd, &width,   sizeof(width));
-    write(fd, &height,  sizeof(height));
-    write(fd, &bytes,   sizeof(bytes));
-    write(fd, &xoffset, sizeof(xoffset));
-    write(fd, &depth,   sizeof(depth));
-
-    /* write the data */
-    for (i = 0; i < height; i++)
-	write(fd, get_ptr(i), bytes);
-
-    close(fd);
-
-    return 0;
+    return -1;
 }
 
 int Panel_restore(char *name)
 {
-    int left, top, width, height;
-    int bytes, xoffset, depth;
-    int fd;
-    int i;
-
-    /* open the file */
-    fd = open(name, O_RDONLY);
-    if (fd < 0)
-    {
-	perror("unable to open panel file");
-	return -1;
-    }
-
-    /* read the header */
-    read(fd, &left,    sizeof(left));
-    read(fd, &top,     sizeof(top));
-    read(fd, &width,   sizeof(width));
-    read(fd, &height,  sizeof(height));
-    read(fd, &bytes,   sizeof(bytes));
-    read(fd, &xoffset, sizeof(xoffset));
-    read(fd, &depth,   sizeof(depth));
-
-    /* read the data */
-    for (i = 0; i < height; i++)
-	read(fd, get_ptr(i), bytes);
-
-    close(fd);
-
-    return 0;
+    return -1;
 }
 
 int Panel_delete(char *name)
 {
-    unlink(name);
+    remove(name);
     return 0;
 }
 
