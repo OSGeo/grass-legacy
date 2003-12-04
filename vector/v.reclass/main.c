89eaaab00703be784cba4e45339da784494243d6
Radim Blazek <radim.blazek@gmail.com>
2003-12-04 16:22:59 +0000

    new/upgrade
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12245 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ vector/v.reclass/main.c
@@ -0,0 +1,195 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "gis.h"
+#include "Vect.h"
+#include "dbmi.h"
+
+#define KEY(x) (G_strcasecmp(key,x)==0)
+
+int inpt (FILE *rulefd, char *buf);
+int key_data (char *buf, char **k, char **d);
+int reclass ( struct Map_info *In, struct Map_info *Out, int type, int field, dbCatValArray *cvarr, int optiond);
+
+int 
+main (int argc, char *argv[])
+{
+    struct GModule *module;
+    struct Flag *d_flag;
+    struct Option *in_opt, *out_opt, *type_opt, *field_opt, *rules_opt, *col_opt;
+    char   *mapset, *key, *data, buf[1024];
+    int    rclelem, type, field;
+    struct Map_info In, Out;
+
+    struct field_info *Fi;
+    dbDriver *Driver;
+    dbCatValArray cvarr;
+	    
+    G_gisinit (argv[0]);
+
+    module = G_define_module();
+    module->description = "Changes vector category values for an existing vector map "
+	    "according to results of SQL queries.";
+
+    /* TODO: Dissolve common boundaries */
+    /*
+    d_flag = G_define_flag();
+    d_flag->key              = 'd';
+    d_flag->description      = "Dissolve common boundaries (default is no) ";
+    */
+
+    in_opt = G_define_standard_option(G_OPT_V_INPUT);
+
+    out_opt =  G_define_standard_option(G_OPT_V_OUTPUT);
+
+    rules_opt = G_define_option();
+    rules_opt->key = "rules";
+    rules_opt->required = NO;
+    rules_opt->type = TYPE_STRING;
+    rules_opt->description =  "Full path to the reclass rule file";
+
+    col_opt = G_define_option();
+    col_opt->key            = "col";
+    col_opt->type           = TYPE_STRING;
+    col_opt->required       = NO;
+    col_opt->multiple       = NO;
+    col_opt->description    = "Column name";
+    
+    type_opt = G_define_standard_option(G_OPT_V_TYPE);
+
+    field_opt = G_define_standard_option(G_OPT_V_FIELD);
+
+    if (G_parser(argc, argv)) exit(1);
+
+    type = Vect_option_to_types ( type_opt );
+    field = atoi (field_opt->answer);
+    
+    if ( (!(rules_opt->answer) && !(col_opt->answer)) || 
+	 (rules_opt->answer && col_opt->answer) ) { 
+	G_fatal_error ( "Either 'rules' or 'col' must be specified.");
+    }
+    
+    mapset = G_find_vector2 (in_opt->answer, NULL);
+    if(mapset == NULL) G_fatal_error ("Could not find input %s\n", in_opt->answer);
+    Vect_set_open_level ( 2 );
+    Vect_open_old (&In, in_opt->answer, mapset);
+    
+    Vect_open_new ( &Out, out_opt->answer, Vect_is_3d (&In) );
+
+    /* Read column values from database */
+    db_CatValArray_init ( &cvarr );
+
+    Fi = Vect_get_field( &In, field);
+    if ( Fi == NULL )
+	G_fatal_error ("Cannot read field info");
+
+    Driver = db_start_driver_open_database ( Fi->driver, Fi->database );
+    if (Driver == NULL)
+	G_fatal_error("Cannot open database %s by driver %s", Fi->database, Fi->driver);
+
+    if ( col_opt->answer ) {
+        int nrec;
+
+	nrec = db_select_CatValArray ( Driver, Fi->table, Fi->key, col_opt->answer, NULL, &cvarr );
+	G_debug (3, "nrec = %d", nrec );
+
+	if ( cvarr.ctype != DB_C_TYPE_INT )
+	    G_fatal_error ( "Column type must be integer." );
+
+    } else {
+	int cat;
+        char *label, *where;
+        FILE *rulefd;
+
+	G_debug (2, "Reading rules");
+	
+	if ( (rulefd = fopen(rules_opt->answer, "r")) == NULL )
+	    G_fatal_error ("Unable to open rule file %s", rules_opt->answer);
+
+	db_CatValArray_alloc ( &cvarr, Vect_get_num_lines(&In) );
+	
+	cat = 0;
+	where = label = NULL;
+	while ( inpt(rulefd, buf) ) {
+	    if (!key_data(buf, &key, &data)) continue ;
+
+	    G_strip(data);
+	    G_debug (3, "key = %s data = %s", key, data);
+	    
+	    if (KEY("cat")) {	
+		if ( cat > 0 ) G_fatal_error ( "Category %d overwritten by %s", cat, data);
+		cat = atoi ( data );
+		if( cat <= 0 ) G_fatal_error ( "Category '%s' invalid", data);
+	    } else if (KEY("label")) {
+		if ( label ) G_fatal_error ( "Label '%s' overwritten by '%s'", label, data);
+		label = G_store ( data );
+	    } else if (KEY("where")) {	
+		if ( where ) G_fatal_error ( "Condition '%s' overwritten by '%s'", where, data);
+		where = G_store(data);
+	    } else {
+		G_fatal_error ( "Unknown rule option: '%s'", key );
+	    }
+
+	    if ( cat > 0 && where ) {
+		int i, over, *cats, ncats;
+		dbCatVal *catval;
+		
+		G_debug (2, "cat = %d, where = '%s'", cat, where);
+		if ( !label ) label = where;
+
+		ncats = db_select_int ( Driver, Fi->table, Fi->key, where, &cats);
+		if ( ncats == -1 ) G_fatal_error("Cannot select values from database.");
+		G_debug (3, "  ncats = %d", ncats);
+		
+
+		/* If the category already exists, overwrite it cvarr, set to 0 in cats
+		 * and don't add second time */
+		over = 0;
+		for ( i = 0; i < ncats; i++ ) {
+		    if ( db_CatValArray_get_value ( &cvarr, cats[i], &catval ) == DB_OK ) {
+			catval->val.i = cat;
+			cats[i] = 0;
+			over++;
+		    }
+		}
+		if (over > 0) 
+		    G_warning ("%d previously set categories overwritten by new category %d.", over, cat);
+			
+		for ( i = 0; i < ncats; i++ ) {
+		    if ( cats[i] <= 0 ) continue;
+
+		    if ( cvarr.n_values == cvarr.alloc ) {
+			db_CatValArray_realloc ( &cvarr, (int)10+cvarr.alloc/3 );
+		    }
+		    G_debug (3, "Add old cat %d", cats[i]);
+		    cvarr.value[cvarr.n_values].cat = cats[i];
+		    cvarr.value[cvarr.n_values].val.i = cat;
+		    cvarr.n_values++;
+		}
+		
+		db_CatValArray_sort ( &cvarr );
+
+		free ( cats );
+		cat = 0;
+		where = label = NULL;
+	    }
+	}
+
+	if ( cat > 0 || where )
+	    G_fatal_error ( "Incomplete rule");
+    }
+
+    db_close_database_shutdown_driver(Driver);
+    
+    /* reclass vector map */    
+    rclelem = reclass ( &In, &Out, type, field, &cvarr, 0);
+    
+    Vect_close (&In);
+
+    Vect_build (&Out, stderr);
+    Vect_close (&Out);
+
+    fprintf(stderr,"%d elments written\n", rclelem);
+
+    exit(0);
+}
+
---
+++ vector/v.reclass/reclass.c
@@ -0,0 +1,142 @@
+/*  From preliminary work (pull.vect) by Dave Gerdes - CERL
+ *  created by:         R.L.Glenn, SCS
+ *  modified to function by RB 5/2000
+ *
+ * Function reclass() reads input vector map and writes reclassed elements to output map.
+ *
+ * Arguments:
+ * in_name - name of input vector map
+ * out_name - name of output vector map
+ * new - reclass table
+ * type - elements type 
+ * optiond - do not output boundaries between areas with the same cat 
+ *
+ * Returns:
+ * number of elements created or -1 on error
+ */
+
+#include <stdlib.h>
+#include <string.h>
+#include  "gis.h"
+#include  "dbmi.h"
+#include "Vect.h"
+
+int reclass ( struct Map_info *In, struct Map_info *Out, int type, int field, dbCatValArray *cvarr, int dissolve)
+{
+    int nlines, nareas, line, area, ltype, old_cat, new_cat;	
+    int nocat = 0, rclelem=0;
+    struct line_pnts *Points;
+    struct line_cats *Cats;
+
+    Points = Vect_new_line_struct();
+    Cats = Vect_new_cats_struct ();
+
+    /* --------------------- Lines Section ------------------------------- */
+    /* Cycle through all lines */
+    nlines = Vect_get_num_lines (In);
+    for ( line = 1; line <= nlines; line++) { 
+	int written = 0;
+
+	G_percent(line, nlines, 1);
+	
+	ltype = Vect_read_line ( In, Points, Cats, line );
+
+	if ( (ltype & type) ) { /* GV_POINTS or GV_LINES */
+	    Vect_cat_get ( Cats, field, &old_cat );
+
+	    if ( old_cat <= 0 ) continue;
+	    
+	    G_debug (3, "  old_cat = %d", old_cat ); 
+
+	    if ( db_CatValArray_get_value_int ( cvarr, old_cat, &new_cat ) != DB_OK ) {
+		nocat++;
+	    } else { 
+	        G_debug (3, "  new_cat = %d", new_cat ); 
+		Vect_reset_cats ( Cats );
+		Vect_cat_set ( Cats, 1, new_cat );
+
+		Vect_write_line (Out, ltype, Points, Cats);
+		rclelem++;
+		written = 1;
+	    }
+	}
+
+	if ( !written && (type & GV_AREA) && (ltype == GV_BOUNDARY) ) { /* Area boundary */
+	    int left, right, lcat, rcat, new_lcat, new_rcat;
+	    
+	    /* Get the category for areas left & right */
+	    lcat = rcat = 0;
+	    Vect_get_line_areas ( In, line, &left, &right );
+
+	    G_debug (3, "  left = %d right = %d", left, right ); 
+
+	    if ( left < 0 ) left = Vect_get_isle_area ( In, abs(left) );
+	    if ( left > 0 ) lcat = Vect_get_area_cat ( In, left, field );
+
+	    if ( right < 0 ) right = Vect_get_isle_area ( In, abs(right) );
+	    if ( right > 0 ) rcat = Vect_get_area_cat ( In, right, field );
+	    
+	    G_debug (3, "  lcat = %d rcat = %d", lcat, rcat ); 
+
+	    if ( lcat <= 0 || db_CatValArray_get_value_int ( cvarr, lcat, &new_lcat ) != DB_OK )
+		new_lcat = 0;
+
+	    if ( rcat <= 0 || db_CatValArray_get_value_int ( cvarr, rcat, &new_rcat ) != DB_OK )
+		new_rcat = 0;
+	
+	    /* if area not labeled */
+	    if ( new_lcat == 0 && new_rcat == 0 ) continue;
+	
+	    /* if user requested -d option */
+	    /* TODO: decide which centroid keep and store this info  */
+	    /*
+	    if (optiond && new_lcat == new_rcat ) {
+		continue;
+	    }
+	    */
+
+	    Vect_reset_cats ( Cats );
+	    Vect_write_line (Out, ltype, Points, Cats);
+	}
+    }  /* end lines section */
+
+    /* ------------------ Area attributes section ------------------- */
+    if (type & GV_AREA) {
+	nareas = Vect_get_num_areas (In);
+	for (area = 1 ; area <= nareas ; area++) {
+	    int centroid, old_cat, new_cat;
+	    
+	    /* TODO: Dissolve common boundaries */
+
+	    G_debug (3, "area = %d", area ); 
+
+	    centroid =  Vect_get_area_centroid ( In, area );
+	    G_debug (3, "  centroid = %d", centroid ); 
+	    if ( centroid <= 0 ) continue;
+	
+	    Vect_read_line ( In, Points, Cats, centroid );
+
+	    Vect_cat_get ( Cats, field, &old_cat );
+	    G_debug (3, "  old_cat = %d", old_cat ); 
+	    if ( old_cat <= 0 ) continue;
+
+	    if ( db_CatValArray_get_value_int ( cvarr, old_cat, &new_cat ) != DB_OK ) {
+		nocat++;
+		continue;
+	    }
+	    G_debug (3, "  new_cat = %d", new_cat ); 
+
+	    Vect_reset_cats ( Cats );
+	    Vect_cat_set ( Cats, 1, new_cat );
+
+	    Vect_write_line (Out, GV_CENTROID, Points, Cats);
+	    rclelem++;
+	} 
+    }  /* end area attributes section */
+
+    if ( nocat > 0 )
+	G_warning ("For %d elements no new category was defined", nocat );
+
+    return(rclelem) ;
+}
+
