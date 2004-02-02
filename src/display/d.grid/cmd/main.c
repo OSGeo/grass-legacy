e10db88f4bcf04e571400751b5d2b539509517ee
Hamish Bowman <hamish_b@yahoo.com>
2004-02-02 02:31:43 +0000

    add R:G:B triplet support for color= and bordercolor=
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@8936 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/display/d.grid/cmd/main.c
+++ src/display/d.grid/cmd/main.c
@@ -1,14 +1,17 @@
-/*
+/*   $Id$
  *   d.grid
  *
  *   Draw the coordinate grid
  *   the user wants displayed on top of the current image.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "gis.h"
 #include "display.h"
 #include "raster.h"
+#include "colors.h"
 
 int plot_grid(double, double, double);
 int plot_border(double, double, double);
@@ -16,7 +19,11 @@ int plot_border(double, double, double);
 int 
 main (int argc, char **argv)
 {
-	int colorg, colorb ;
+	int colorg = 0;
+	int colorb = 0;
+	const int customGcolor = MAXCOLORS + 1;
+	const int customBcolor = MAXCOLORS + 2;
+	int R, G, B;
 	double size ;
 	double east, north ;
 	struct GModule *module;
@@ -43,8 +50,9 @@ main (int argc, char **argv)
 	opt1->type       = TYPE_STRING ;
 	opt1->required   = NO;
 	opt1->answer     = "gray" ;
-	opt1->options    = D_color_list();
-	opt1->description= "Sets the current grid color";
+/*	opt1->options    = D_color_list(); */
+	opt1->description=
+	    "Sets the current grid color, either a standard GRASS color or R:G:B triplet (separated by colons)";
 
 	opt3 = G_define_option() ;
 	opt3->key        = "origin" ;
@@ -59,8 +67,9 @@ main (int argc, char **argv)
 	opt4->type       = TYPE_STRING ;
 	opt4->required   = NO;
 	opt4->answer     = "brown" ;
-	opt4->options    = D_color_list();
-	opt4->description= "Sets the border color";
+/*	opt4->options    = D_color_list(); */
+	opt4->description=
+	    "Sets the border color, either a standard GRASS color or R:G:B triplet";
 
 	nogrid = G_define_flag();
 	nogrid->key = 'g';
@@ -73,15 +82,35 @@ main (int argc, char **argv)
 
 	/* Check command line */
 	if (G_parser(argc, argv))
-		exit(-1);
+		exit(1);
 
-	colorg = D_translate_color(opt1->answer);
-	if (colorg == 0)
-		G_fatal_error ("Don't know the color %s", opt1->answer);
+	/* Parse and select grid color */
+	if(sscanf(opt1->answer, "%d:%d:%d", &R, &G, &B) == 3) {
+	    if (R>=0 && R<256 && G>=0 && G<256 && B>=0 && B<256) {
+		R_reset_color(R, G, B, customGcolor);
+		colorg = customGcolor;
+	    }
+	}
+	else
+	    colorg = D_translate_color(opt1->answer);
 
-	colorb = D_translate_color(opt4->answer);
-	if (colorb == 0)
-		G_fatal_error ("Don't know the color %s", opt4->answer);
+	if(!colorg)
+	    G_fatal_error("[%s]: No such color", opt1->answer);
+
+
+	/* Parse and select border color */
+	if(sscanf(opt4->answer, "%d:%d:%d", &R, &G, &B) == 3) {
+	    if (R>=0 && R<256 && G>=0 && G<256 && B>=0 && B<256) {
+		R_reset_color(R, G, B, customBcolor);
+		colorb = customBcolor;
+	    }
+	}
+	else
+	    colorb = D_translate_color(opt4->answer);
+
+	if(!colorb)
+	    G_fatal_error("[%s]: No such color", opt4->answer);
+
 
 	if(!G_scan_resolution (opt2->answer, &size, G_projection()) || size <= 0.0)
 		G_fatal_error ("Invalid grid size <%s>", opt2->answer);
@@ -107,25 +136,31 @@ main (int argc, char **argv)
 
 	if(!nogrid->answer)
 	{
-	  /* Set grid color */
-	  R_standard_color(colorg) ;
+	    /* Set grid color */
+	    if (colorg > MAXCOLORS)  /* ie custom RGB color */
+		R_color(colorg);
+	    else
+		R_standard_color(colorg) ;
 
-	  /* Do the grid plotting */
-	  plot_grid(size, east, north) ;
+	    /* Do the grid plotting */
+	    plot_grid(size, east, north) ;
 	}
 
 	if(!noborder->answer)
 	{
-	  if (G_projection() == PROJECTION_LL)
+	    if (G_projection() == PROJECTION_LL)
 	  	G_warning("border not yet implemented for LatLong locations: border not drawn.");
-	  else
-	  {
-	    /* Set border color */
-	    R_standard_color(colorb) ;
+	    else
+	    {
+		/* Set border color */
+		if (colorb > MAXCOLORS)  /* ie custom RGB color */
+		    R_color(colorb);
+		else
+		    R_standard_color(colorb) ;
 	  
-	    /* Do the border plotting */
-	    plot_border(size, east, north) ;
-	  }
+		/* Do the border plotting */
+		plot_border(size, east, north) ;
+	    }
 	}
 
 	D_add_to_list(G_recreate_command()) ;
