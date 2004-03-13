622eb86e503f5679b979fbb6065d4bd46f5931c7
Helena Mitasova <hmitaso@ncsu.edu>
2004-03-13 05:49:11 +0000

    null fix jh,hm
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12521 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/simwe/simlib/erod.c
+++ raster/simwe/simlib/erod.c
@@ -18,31 +18,36 @@ void erod(double **hw)
 
   double dyp, dyn, dya, dxp, dxn, dxa;
   int k,l;
-  int l1, lp, k1, kp, ln, kn;
+  int l1, lp, k1, kp, ln, kn, k2, l2;
 
             
         for (k = 0; k < my; k++) {
             for (l = 0; l < mx; l++) {
-		
 		lp = max(0,l-2);
 		l1 = lp + 1;
-
-		dxp = (v1[k][lp] * hw[k][lp] - v1[k][l1] * hw[k][l1]) / stepx;
-		ln = min(mx-1,l+1);
-		l1 = ln - 1;
-                dxn = (v1[k][l1] * hw[k][l1] - v1[k][ln] * hw[k][ln]) / stepx;
-		dxa = 0.5 * (dxp + dxn);
-
                 kp = max(0,k-2);
                 k1 = kp + 1;
+                ln = min(mx-1,l+1);
+                l2 = ln - 1;
+                kn = min(my-1,k+1);
+                k2 = kn - 1;
+
+        if (zz[k][l] != UNDEF || zz[k][ln] != UNDEF || zz[kp][l] != UNDEF || \
+			zz[k][lp] != UNDEF || zz[k][l1] != UNDEF || \
+			zz[k1][l] != UNDEF || zz[kn][l] != UNDEF) { /* jh fix */
+
+		dxp = (v1[k][lp] * hw[k][lp] - v1[k][l1] * hw[k][l1]) / stepx;
+                dxn = (v1[k][l2] * hw[k][l2] - v1[k][ln] * hw[k][ln]) / stepx;
+		dxa = 0.5 * (dxp + dxn);
+
 
                 dyp = (v2[kp][l] * hw[kp][l] - v2[k1][l] * hw[k1][l]) / stepy;
-                kn = min(my-1,k+1);
-                k1 = kn - 1;
-                dyn = (v2[k1][l] * hw[k1][l] - v2[kn][l] * hw[kn][l]) / stepy;
+                dyn = (v2[k2][l] * hw[k2][l] - v2[kn][l] * hw[kn][l]) / stepy;
                 dya = 0.5 * (dyp + dyn);
 
 		er[k][l] = (dxa + dya) / deltap;
+
+	} else er[k][l] = UNDEF;
 
 		}
 	}
