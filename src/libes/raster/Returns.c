f2f5834ce345c4c29767830218edbbec5c622492
Glynn Clements <glynn@gclements.plus.com>
2004-03-28 00:09:30 +0000

    Fix behaviour, documentation of R_screen_{left,rite,top,bot}
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_5_5@9067 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- src/libes/raster/Returns.c
+++ src/libes/raster/Returns.c
@@ -1,14 +1,10 @@
 #include "raster.h"
 #include "graph.h"
-static int first = 1 ;
-static int t, b, l, r ;
-static int getall(void);
-
 
 /*!
  * \brief screen left edge
  *
- * Returns the pixel column number of the left edge of the screen.
+ * Returns the coordinate of the left edge of the screen.
  *
  *  \param void
  *  \return int
@@ -16,16 +12,16 @@ static int getall(void);
 
 int R_screen_left(void)
 {
-	if(first) getall() ;
-	return(l) ;
+	int l;
+	_send_ident(SCREEN_LEFT);
+	_get_int(&l);
+	return l;
 }
-
 
 /*!
  * \brief screen right edge
  *
- * Returns the pixel column
- * number of the right edge of the screen.
+ * Returns the coordinate of the right edge of the screen.
  *
  *  \param void
  *  \return int
@@ -33,16 +29,17 @@ int R_screen_left(void)
 
 int R_screen_rite(void)
 {
-	if(first) getall() ;
-	return(r) ;
+	int r;
+	_send_ident(SCREEN_RITE);
+	_get_int(&r);
+	return r;
 }
 
 
 /*!
  * \brief bottom of screen
  *
- * Returns the pixel row number of
- * the bottom of the screen.
+ * Returns the coordinate of the bottom of the screen.
  *
  *  \param void
  *  \return int
@@ -50,16 +47,17 @@ int R_screen_rite(void)
 
 int R_screen_bot(void)
 {
-	if(first) getall() ;
-	return(b) ;
+	int b;
+	_send_ident(SCREEN_BOT);
+	_get_int(&b);
+	return b;
 }
 
 
 /*!
  * \brief top of screen
  *
- * Returns the pixel row number of
- * the top of the screen.
+ * Returns the coordinate of the top of the screen.
  *
  *  \param void
  *  \return int
@@ -67,21 +65,9 @@ int R_screen_bot(void)
 
 int R_screen_top(void)
 {
-	if(first) getall() ;
-	return(t) ;
+	int t;
+	_send_ident(SCREEN_TOP);
+	_get_int(&t);
+	return t;
 }
 
-static int getall(void)
-{
-	_send_ident(SCREEN_TOP) ;
-	_get_int(&t) ;
-	_send_ident(SCREEN_BOT) ;
-	_get_int(&b) ;
-	_send_ident(SCREEN_LEFT) ;
-	_get_int(&l) ;
-	_send_ident(SCREEN_RITE) ;
-	_get_int(&r) ;
-	first = 0 ;
-
-	return 0;
-}
