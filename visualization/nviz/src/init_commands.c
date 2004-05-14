93244f19c35e58f71f07cc638507cc2e7c5f45be
Radim Blazek <radim.blazek@gmail.com>
2004-05-14 14:28:57 +0000

    Postgres -> DBMI
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12752 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ visualization/nviz/src/init_commands.c
@@ -0,0 +1,331 @@
+/*	Alex Shevlakov sixote@yahoo.com 02/2000
+*	function added to handle postgres queries
+*/
+#include "interface.h"
+
+extern int
+Nresize_cmd(),
+Nset_background_cmd(),
+Nchange_position_cmd(),
+Nget_position_cmd(),
+Nchange_twist_cmd(),
+Nchange_persp_cmd(),
+Nchange_exag_cmd(),
+Nchange_height_cmd(),
+Nget_first_exag_cmd(),
+Nget_height_cmd(),
+Nfinish_cmd(),
+Nlights_cmd(),
+Nlight_obj_cmd(),
+Nnew_light_cmd(),
+Ninit_view_cmd(),
+Nlibinit_cmd(),
+Nhas_transparency_cmd(),
+Ntransp_is_set_cmd(),
+Nis_masked_cmd(),
+Nget_def_color_cmd(),
+Nclear_cmd(),
+Nset_cancel_cmd(),
+Nunset_cancel_func_cmd(),
+Nget_cancel_cmd(),
+Nset_draw_cmd(),
+Nready_draw_cmd(),
+Ndone_draw_cmd(),
+Nnew_map_obj_cmd(),
+Ninit_view_cmd(),
+Nget_to_cmd(),
+Nget_from_cmd(),
+Nlook_here_cmd(),
+Nset_focus_cmd(),
+Nset_focus_real_cmd(),
+Nset_focus_top_cmd(),
+Nset_focus_gui_cmd(),
+Nget_focus_gui_cmd(),
+Nget_real_position_cmd(),
+Nget_focus_cmd(),
+Nhas_focus_cmd(),
+Nset_focus_map_cmd(),
+Nset_no_focus_cmd(),
+Nmove_to_cmd(),
+Nmove_to_real_cmd(),
+Nset_fov_cmd(),
+Nget_fov_cmd(),
+Nset_twist_cmd(),
+Nget_twist_cmd(),
+Nget_region_cmd(),
+Nget_point_on_surf_cmd(),
+Nget_point_on_surf_vect(),
+Nget_longdim_cmd(),
+Nget_zrange_cmd(),
+Nget_zextents_cmd(),
+Nget_exag_cmd(),
+Nset_exag_cmd(),
+Nquick_draw_cmd(),
+Nauto_draw_cmd(),
+Ndraw_all_cmd(),
+Nsurf_draw_all_cmd(),
+Nvect_draw_all_cmd(),
+Nsite_draw_all_cmd(),
+Ndraw_line_on_surf_cmd(),
+Ndraw_model_cmd(),
+Ndraw_wire_cmd(),
+Ndraw_X_cmd(),
+Ndraw_Narrow_cmd(),
+Ndraw_legend_cmd(),
+Ndelete_list_cmd(),
+Ndone_draw_cmd(),
+Nready_draw_cmd(),
+Nget_dist_along_surf_cmd(),
+Nget_cat_at_xy_cmd(),
+Nget_val_at_xy_cmd(),
+Nset_light_to_view_cmd(),
+Nset_interp_mode_cmd(),
+Nset_tension_cmd(),
+Nshowtension_start_cmd(),
+Nupdate_tension_cmd(),
+Nshowtension_stop_cmd(),
+Nupdate_frames_cmd(),
+Nset_numsteps_cmd(),
+Nclear_keys_cmd(),
+Nadd_key_cmd(),
+Ndo_framestep_cmd(),
+Nshow_path_cmd(),
+Nshow_site_cmd(),
+Nshow_vect_cmd(),
+Ndelete_key_cmd(),
+Nmove_key_cmd(),
+Nprint_keys_cmd(),
+Nwrite_rgb_cmd(),
+Nwrite_ppm_cmd(),
+Nwrite_tif_cmd(),
+Nstart_zoom_cmd(),
+Noff_screen_cmd(),
+Ncutplane_obj_cmd(),
+Nnew_cutplane_obj_cmd(),
+Nnum_cutplane_obj_cmd(),
+Nset_current_cutplane_cmd(),
+Nget_current_cutplane_cmd(),
+Nget_cutplane_list_cmd(),
+Nset_fence_color_cmd(),
+Nget_fence_color_cmd(),
+Nget_xyrange_cmd(),
+Nset_SDsurf_cmd(),
+Nunset_SDsurf_cmd(),
+Nset_SDscale_cmd(),
+Nget_surf_list_cmd(),
+Nget_vect_list_cmd(),
+Nget_site_list_cmd(),
+Nsave_3dview_cmd(), Nload_3dview_cmd(), Nset_cancel_func_cmd(),
+/*  Tk_Tkspecial_waitCmd(), */
+    SetScriptFile_Cmd(),
+SetState_Cmd(),
+CloseScripting_Cmd(),
+ScriptAddString_Cmd(),
+Tk_ScaleCmd(),
+Tk_SendCmd(),
+Nsurf_draw_one_cmd(),
+Nvect_draw_one_cmd(),
+Nsite_draw_one_cmd(),
+Nliteral_from_logical_cmd(), Nlogical_from_literal_cmd(), Nplace_label_cmd();
+
+extern Tk_Window mainWindow;
+
+int init_commands(Tcl_Interp * interp, Nv_data * data)
+{
+    /* Disabled security version of send */
+/*  Tcl_CreateCommand(interp, "send", Tk_SendCmd,
+		    (ClientData) mainWindow, (void (*)()) NULL); */
+
+    /* Scripting commands */
+    Tcl_CreateCommand(interp, "Nv_set_script_file", SetScriptFile_Cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+    Tcl_CreateCommand(interp, "Nv_set_script_state", SetState_Cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+    Tcl_CreateCommand(interp, "Nv_close_scripting", CloseScripting_Cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+    Tcl_CreateCommand(interp, "Nv_script_add_string", ScriptAddString_Cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+
+    /* Add the cancel function command */
+    Tcl_CreateCommand(interp, "Nset_cancel_func", Nset_cancel_func_cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+    Tcl_CreateCommand(interp, "Nunset_cancel_func", Nunset_cancel_func_cmd,
+		      (ClientData) mainWindow, (void (*)()) NULL);
+
+    /* Add the special tkwait command */
+    /* REMOVED 26-Feb-2000 by Philip Warner. Replaced with an Idle handler */
+/*
+ *  Tcl_CreateCommand(interp, "tkspecial_wait", Tk_Tkspecial_waitCmd,
+ *		    (ClientData) mainWindow, (void (*)()) NULL);
+ */
+
+    /* Commands for handling logical names */
+    Tcl_CreateCommand(interp, "Nliteral_from_logical",
+		      Nliteral_from_logical_cmd, (ClientData) mainWindow,
+		      (void (*)()) NULL);
+    Tcl_CreateCommand(interp, "Nlogical_from_literal",
+		      Nlogical_from_literal_cmd, (ClientData) mainWindow,
+		      (void (*)()) NULL);
+
+    /* Commands for generating lists of map objects */
+    Tcl_CreateCommand(interp, "Nget_surf_list", Nget_surf_list_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nget_vect_list", Nget_vect_list_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nget_site_list", Nget_site_list_cmd,
+		      data, NULL);
+
+    Tcl_CreateCommand(interp, "Nbackground", Nset_background_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nresize", Nresize_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nchange_position", Nchange_position_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nget_position", Nget_position_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nchange_persp", Nchange_persp_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nchange_twist", Nchange_twist_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nchange_height", Nchange_height_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_first_exag", Nget_first_exag_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_height", Nget_height_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nchange_exag", Nchange_exag_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ngl_finish", Nfinish_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nlights", Nlights_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nnew_light", Nnew_light_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ninit_view", Ninit_view_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nlibinit", Nlibinit_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nhas_transparency", Nhas_transparency_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Ntransp_is_set", Ntransp_is_set_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nis_masked", Nis_masked_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_def_color", Nget_def_color_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nclear", Nclear_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_cancel", Nset_cancel_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_cancel", Nget_cancel_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_draw", Nset_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nready_draw", Nready_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndone_draw", Ndone_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nnew_map_obj", Nnew_map_obj_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_to", Nget_to_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_from", Nget_from_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nlook_here", Nlook_here_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nhas_focus", Nhas_focus_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_focus", Nget_focus_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_focus", Nset_focus_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_focus_real", Nset_focus_real_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_focus_top", Nset_focus_top_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nset_focus_gui", Nset_focus_gui_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_focus_gui", Nget_focus_gui_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_real_position", Nget_real_position_cmd, data,
+		    NULL);
+    Tcl_CreateCommand(interp, "Nset_focus_map", Nset_focus_map_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nset_no_focus", Nset_no_focus_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nmove_to", Nmove_to_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nmove_to_real", Nmove_to_real_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_fov", Nset_fov_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_fov", Nget_fov_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_twist", Nset_twist_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_twist", Nget_twist_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_region", Nget_region_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_point_on_surf", Nget_point_on_surf_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nget_point_on_surf_vect",
+		      Nget_point_on_surf_vect, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_longdim", Nget_longdim_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_zrange", Nget_zrange_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_zextents", Nget_zextents_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_exag", Nget_exag_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_exag", Nset_exag_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nquick_draw", Nquick_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nauto_draw", Nauto_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_all", Ndraw_all_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nsurf_draw_all", Nsurf_draw_all_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nsurf_draw_one", Nsurf_draw_one_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nvect_draw_all", Nvect_draw_all_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nvect_draw_one", Nvect_draw_one_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nsite_draw_all", Nsite_draw_all_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nsite_draw_one", Nsite_draw_one_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Ndraw_line_on_surf", Ndraw_line_on_surf_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_model", Ndraw_model_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_wire", Ndraw_wire_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_X", Ndraw_X_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_Narrow", Ndraw_Narrow_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndraw_legend", Ndraw_legend_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndelete_list", Ndelete_list_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndone_draw", Ndone_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nready_draw", Nready_draw_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_dist_along_surf",
+		      Nget_dist_along_surf_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_cat_at_xy", Nget_cat_at_xy_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_val_at_xy", Nget_val_at_xy_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nset_light_to_view", Nset_light_to_view_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nset_SDsurf", Nset_SDsurf_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nunset_SDsurf", Nunset_SDsurf_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nset_SDscale", Nset_SDscale_cmd, data, NULL);
+
+    /* Keyframe Animation */
+    Tcl_CreateCommand(interp, "Nset_interp_mode", Nset_interp_mode_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nset_tension", Nset_tension_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nshowtension_start", Nshowtension_start_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nupdate_tension", Nupdate_tension_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nshowtension_stop", Nshowtension_stop_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nupdate_frames", Nupdate_frames_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nset_numsteps", Nset_numsteps_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nclear_keys", Nclear_keys_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nadd_key", Nadd_key_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndo_framestep", Ndo_framestep_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nshow_path", Nshow_path_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nshow_site", Nshow_site_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nshow_vect", Nshow_vect_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Ndelete_key", Ndelete_key_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nmove_key", Nmove_key_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nprint_keys", Nprint_keys_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nwrite_rgb", Nwrite_rgb_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nwrite_ppm", Nwrite_ppm_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nwrite_tif", Nwrite_tif_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nstart_zoom", Nstart_zoom_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Noff_screen", Noff_screen_cmd, data, NULL);
+
+    /* Cutplane Junk */
+    Tcl_CreateCommand(interp, "Ncutplane_obj", Ncutplane_obj_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nnew_cutplane_obj", Nnew_cutplane_obj_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nnum_cutplane_obj", Nnum_cutplane_obj_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nset_current_cutplane",
+		      Nset_current_cutplane_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_current_cutplane",
+		      Nget_current_cutplane_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nget_cutplane_list", Nget_cutplane_list_cmd,
+		      data, NULL);
+    Tcl_CreateCommand(interp, "Nset_fence_color", Nset_fence_color_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_fence_color", Nget_fence_color_cmd, data,
+		      NULL);
+    Tcl_CreateCommand(interp, "Nget_xyrange", Nget_xyrange_cmd, data, NULL);
+
+    /* Miscellanious */
+    Tcl_CreateCommand(interp, "Nsave_3dview", Nsave_3dview_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nload_3dview", Nload_3dview_cmd, data, NULL);
+    Tcl_CreateCommand(interp, "Nplace_label", Nplace_label_cmd, data, NULL);
+}
---
+++ visualization/nviz/src/interface.h
@@ -0,0 +1,301 @@
+#define TRACE_GS_FUNCS
+/*----------------- this is the include file section -----------------*/
+#define SGI
+/*
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <tk.h>
+#include "gis.h"
+#ifdef SGI
+/*#include <gl.h>*/
+#include <GL/gl.h>
+/*#include <gl/glws.h>*/
+#endif
+/*
+#include <X11/Xirisw/GlxMDraw.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+*/
+#include "gsurf.h"
+#include "gstypes.h"
+
+/* extern int *GV_get_vect_list(int *);
+extern int *GS_get_surf_list(int *);
+extern int *GP_get_site_list(int *); */
+
+
+/*-------------- this is the data define section ---------------*/
+
+#define X 0
+#define Y 1
+#define Z 2
+#define W 3
+
+#define SURF 0
+#define VECT 1
+#define SITE 2
+#define VOL  3
+
+/* I don't like this - to be consistant, should really do all scaling 
+in library, so can send real world coords. (seems like it would make
+type-ins & sliders easier, too) */
+#define RANGE (5 * GS_UNIT_SIZE)
+#define RANGE_OFFSET (2 * GS_UNIT_SIZE)
+#define ZRANGE (3 * GS_UNIT_SIZE)
+#define ZRANGE_OFFSET (1 * GS_UNIT_SIZE)
+
+#define DEFAULT_SURF_COLOR 0x33BBFF
+#define DEFAULT_WIRE_COLOR 0x999999
+#define DEFAULT_WIRE_CNT 10 
+#define DEFAULT_POLY_CNT 2
+
+/* Attributes for vector and site files */
+#define SV_ATT_WIDTH    -1 
+#define SV_ATT_MARKER   -2
+#define SV_ATT_SIZE     -3
+#define SV_ATT_USEATT   -4
+#define SV_ATT_DISPLAY  -5
+#define SV_ATT_MAP      -6
+#define SV_ATT_FLAT     -7
+
+#ifdef FontBase_MAIN
+GLuint FontBase;
+#else
+extern GLuint FontBase;
+#endif
+
+/*------------------------------------------------------------------------ 
+-            this is the data type declaration section                   -
+------------------------------------------------------------------------*/
+
+typedef struct{
+        int id;
+	float brt;
+	float r, g, b;
+	float ar, ag, ab;  /* ambient rgb */
+	float x, y, z, w; /* position */
+} light_data;
+
+typedef struct {
+        float Zrange, XYrange;
+	
+	int NumCplanes;
+	int CurCplane, Cp_on[MAX_CPLANES];
+	float Cp_trans[MAX_CPLANES][3];
+	float Cp_rot[MAX_CPLANES][3];
+
+	light_data light[MAX_LIGHTS];
+
+	int BGcolor;
+} Nv_data; 
+
+/* - The following structure is used to associate client data with surfaces.
+ * We do this so that we don't have to rely on the surface ID (which is libal to change
+ * between subsequent executions of nviz) when saving set-up info to files.
+ */
+
+typedef struct {
+  /* We use logical names to assign textual names to map objects.
+     When Nviz needs to refer to a map object it uses the logical name
+     rather than the map ID.  By setting appropriate logical names, we
+     can reuse names inbetween executions of Nviz.  The Nviz library
+     also provides a mechanism for aliasing between logical names.
+     Thus several logical names may refer to the same map object.
+     Aliases are meant to support the case in which two logical names
+     happen to be the same.  The Nviz library automatically assigns
+     logical names uniquely if they are not specified in the creation
+     of a map object.  When loading a saved file containing several map
+     objects, it is expected that the map 0bjects will be aliased to
+     their previous names.  This ensures that old scripts will work.
+     */
+
+    char *logical_name;
+  
+} Nv_clientData;
+
+/* Here are the functions that use these structures */
+
+/* anim_support.c */
+int Nset_interp_mode_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_tension_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nshowtension_start_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nupdate_tension_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nshowtension_stop_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nupdate_frames_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_numsteps_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nclear_keys_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nadd_key_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndelete_key_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nmove_key_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndo_framestep_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nshow_site_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nshow_vect_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nshow_path_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nwrite_rgb_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* change_view.c */
+int Nchange_persp_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nchange_position_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nchange_height_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_light_to_view_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nchange_exag_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* cutplane_obj.c */
+int Nset_fence_color_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_fence_color_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ncutplane_obj_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nnew_cutplane_obj_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nnum_cutplane_obj_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_current_cutplane_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_current_cutplane_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_cutplane_list_cmd(Nv_data *, Tcl_Interp *, int, char **);
+void cp_draw(int, Nv_data *, int, int);
+int draw_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int on_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int off_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int state_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_set_rot(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_set_trans(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_get_rot(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_get_trans(Nv_data *, Tcl_Interp *, int, char **, int);
+int get_cp_idnum(char *);
+/* draw.c */
+void CancelFunc_Hook(void);
+int Nunset_cancel_func_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_cancel_func_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_draw_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ntransp_is_set_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nis_masked_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nhas_transparency_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_def_color_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nclear_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndraw_wire_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndraw_X_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndraw_line_on_surf_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Ndraw_model_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nsurf_draw_one_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nvect_draw_one_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nsite_draw_one_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nsurf_draw_all_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_cancel_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nvect_draw_all_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nsite_draw_all_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nready_draw_cmd(void);
+int Ndone_draw_cmd(void);
+int check_blank(Tcl_Interp *, int);
+/* exag.c */
+int Nget_first_exag_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_height_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* glwrappers.c */
+int tcl_color_to_int(char *);
+int Nresize_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nfinish_cmd(ClientData, Tcl_Interp *, int, char **);
+int Nset_background_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* init_commands.c */
+int init_commands(Tcl_Interp *, Nv_data *);
+/* label.c */
+int Nplace_label_cmd(Nv_data *, Tcl_Interp *, int, char **);
+void G_site_destroy_struct(void *);
+/* lights.c */
+int Nlight_obj_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nnew_light_cmd(Nv_data *, Tcl_Interp *);
+int Nlights_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* map_obj.c */
+int Nliteral_from_logical_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nlogical_from_literal_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nget_surf_list_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nget_vect_list_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nget_site_list_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nnew_map_obj_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int Nmap_obj_cmd(Nv_data *, Tcl_Interp *, int, char *[]);
+int get_idnum(char *);
+int get_type(char *);
+int att_atoi(char *);
+/* misc.c */
+int Nlibinit_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_cancel_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_SDsurf_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nunset_SDsurf_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_SDscale_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* nviz_init.c */
+int Ninit(Tcl_Interp *, Tk_Window);
+int Ninitdata(Tcl_Interp *, Nv_data *);
+int Ngetargs(Tcl_Interp *, char ***);
+int parse_command(Nv_data *, Tcl_Interp *, int, char **);
+/* position.c */
+int Ninit_view_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_to_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_from_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nlook_here_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nhas_focus_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_focus_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_no_focus_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_focus_map_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nmove_to_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_fov_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_region_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_point_on_surf_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_point_on_surf_vect(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_dist_along_surf_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_cat_at_xy_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_val_at_xy_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_focus_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_longdim_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_zrange_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_xyrange_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_zextents_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nget_exag_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nset_exag_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nsave_3dview_cmd(Nv_data *, Tcl_Interp *, int, char **);
+int Nload_3dview_cmd(Nv_data *, Tcl_Interp *, int, char **);
+/* quick_draw.c */
+int Nquick_draw_cmd(Nv_data *, Tcl_Interp *);
+/* script_support.c */
+int ScriptAddString_Cmd(ClientData, Tcl_Interp *, int, char **);
+int CloseScripting_Cmd(ClientData, Tcl_Interp *, int, char **);
+int SetState_Cmd(ClientData, Tcl_Interp *, int, char **);
+int SetScriptFile_Cmd(ClientData, Tcl_Interp *, int, char **);
+/* tkAppInit.c */
+int Tcl_AppInit(Tcl_Interp *);
+/* tkBind.c */
+int TkCopyAndGlobalEval(Tcl_Interp *, char *);
+/* tkSend.c */
+#if TK_MAJOR_VERSION>8 || (TK_MAJOR_VERSION==8 && TK_MINOR_VERSION>=4)
+    CONST char *Tk_SetAppName(Tk_Window, CONST char *);
+#else
+    char *Tk_SetAppName(Tk_Window, char *);
+#endif
+int Tk_SendCmd(ClientData, Tcl_Interp *, int, char **);
+int TkGetInterpNames(Tcl_Interp *, Tk_Window);
+/* tkSend_old.c */
+int Tk_RegisterInterp(Tcl_Interp *, char *, Tk_Window);
+int Tk_SendCmd(ClientData, Tcl_Interp *, int, char **);
+int TkGetInterpNames(Tcl_Interp *, Tk_Window);
+/* tkSpecial_Wait.c */
+int Tk_Tkspecial_waitCmd(ClientData, Tcl_Interp *, int, char **);
+int update_ranges(Nv_data *);
+void cp_draw(int, Nv_data *, int, int);
+int draw_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int on_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int off_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int state_cp_obj(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_set_rot(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_set_trans(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_get_rot(Nv_data *, Tcl_Interp *, int, char **, int);
+int cp_get_trans(Nv_data *, Tcl_Interp *, int, char **, int);
+int surf_draw_all(Nv_data *, Tcl_Interp *);
+int init_default_slider_vals1(Nv_data *, float *, float *, float *);
+int set_drawmode(Tcl_Interp *, int, int, Nv_data *, int, char *[]);
+int get_drawmode(int, int, Nv_data *, Tcl_Interp *);
+int delete_obj(int, int, Nv_data *, Tcl_Interp *);
+int set_wirecolor(Tcl_Interp *, int, int, Nv_data *, int, char *[]);
+int get_wirecolor(int, int, Nv_data *, Tcl_Interp *);
+int set_nozero(Tcl_Interp *, int, int, Nv_data *, int, char *[]);
+int get_nozero(int, int, Nv_data *, Tcl_Interp *, int, char *[]);
+int load_obj(int, int, Nv_data *, int, char *[], Tcl_Interp *);
+int get_att(int, int, Nv_data *, Tcl_Interp *, int, char *[]);
+int set_att(int, int, Nv_data *, Tcl_Interp *, int, char *[]);
+int get_mask_mode(int, int, Nv_data *, Tcl_Interp *);
+int set_mask_mode(int, int, Nv_data *, Tcl_Interp *, int, char *[]);
+int set_default_wirecolors(Nv_data *, int);
