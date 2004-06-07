0bc54eda127786d16b8cd594d2e63a0396f1a4af
Michael Barton <michael.barton@asu.edu>
2004-06-07 20:32:35 +0000

    7 June 2004 - Michael Barton
    Updated help file to match behavior of GRASS 5.7 g.parser compliant autogenerated tcltk dialogs
      Removed reference to right mouse button for contextual help.
      Removed reference to right mouse button for popup menu.
      Removed reference to saving window arrangement and size configuration.
    
    Updated URL's to point to grass.itc.it
    
    
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12834 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/tcltkgrass/main/help.tcl
+++ gui/tcltkgrass/main/help.tcl
@@ -20,11 +20,7 @@ INTRODUCTION
  
 HOW TO USE TclTkGRASS?
 
-  The usage is quite simple: Use the mouse buttons... 
-
-               left button: select menu/function
-               right button: contextual menu for a specific menu 
-                             (keep it pressed)
+  The usage is quite simple: Use the left mouse button to select menu/function.
 
   This interface allows to access the standard GRASS modules with mouse.
   Please note: Before you use map display commands, you have to open 
@@ -45,13 +41,10 @@ HOW TO USE TclTkGRASS?
   find at:
 
   "GRASS GIS Documentation Project":
-        http://www.geog.uni-hannover.de/grass/gdp/
-
-  "GRASS GIS Europe pages":
-        http://www.geog.uni-hannover.de/grass/
+        http://grass.itc.it/gdp
 
   "GRASS GIS official site" from GRASS Development Team:
-        http://www.baylor.edu/~grass/
+        http://grass.itc.it
 
   Beside the module windows you can also enter commands directly into 
   the shell window (as usual).
@@ -59,55 +52,23 @@ HOW TO USE TclTkGRASS?
 
 SPECIAL FEATURES
 
-  Some specialities and new features are there:
-   1) A popup menu is posted every time the mouse right button is pressed 
-      inside a module window (except on the "Run" button). It can perform 
-      4 actions:
-
-         1.a resize the window to its natural size                    
-         1.b reinitialize (remove user entries from input lines etc.)
-         1.c raise the window (put it on top of the stacking order)   
-         1.d lower the window (put it on bottom of the stacking order)
-         1.e quit the window                                          
-
-    The stacking order changed through 1.c or 1.d is memorized with 
-           "Config" 
-            -> "Save config"
-    or 
-           "Quit" (when leaving TclTkGRASS)
-            -> "Save config before quitting?" 
-    in main menu.
-    The "quit" function (1.e) is selected on popup, so the window is closed
-    if you simply press and release the right button of the mouse (except 
-    if you do this near the top edge of the screen).
-
-   2) You get the command help if you press the mouse right button on 
-      the "Run" button. More help is available from the main menu through
-           "?"
-            -> "Manual pages"
-
-      Using the select button, you can choose the specific module's manpage.
-
-   3) If you click on a separator line (-----) at the top of each main menu,
+      If you click on a separator line (-----) at the top of each main menu,
       the menu will become a separated window. That is useful, if you frequently
       need the same menu.
       Generally you can leave all module windows on the screen (useful for 
       frequently used windows like "d.erase" etc.).
-
-   4) The window's positions and the windows, which shall be open also in the 
-      next session, can be stored when leaving TclTkGRASS:
-           "Quit"
-              -> "Save config before quitting?" (Yes/No/Cancel)
-           
-      The GRASS monitors are closed when leaving TclTkGRASS.
-      When (re-)starting TclTkGRASS again, the last saved windows
-      configuration is used.
+	 
+	 Each module generates commands in the text box at the bottom. These commands
+	 can be copied and pasted to the shell window (using the middle mouse button) 
+	 or pasted into a command box in the Display Manager.
 
 
 HOW CAN I CREATE/ADD MY OWN WINDOWS?
 
    Please read the file 
+   
        tcltkgrass/docs/Programming_intro.txt 
+   
    for details. You will see, that window programming is quite easy. 
    But you have to be careful with syntax (spaces, braces etc.).
 }
