b0f38ac0ec1b3e8bf6c994fe295feddcac75acff
Radim Blazek <radim.blazek@gmail.com>
2004-06-18 13:25:27 +0000

    input +id, optional coordinates
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12886 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.net.path/path.c
+++ vector/v.net.path/path.c
@@ -11,6 +11,9 @@
 #define SP_UNREACHABLE 1  /* Node is not reachable */
 #define SP_NOPOINT     2  /* Missing point of given category */
 
+#define INPUT_MODE_NODE  1
+#define INPUT_MODE_COOR  2
+
 typedef struct { /* category index */
     int cat;     /* line category */
     int line;     /* line number */
@@ -18,16 +21,17 @@ typedef struct { /* category index */
 
 int cmp ( const void *, const void *);
 
-int path ( struct Map_info *In, struct Map_info *Out, int nfield )
+int path ( struct Map_info *In, struct Map_info *Out, int nfield, double maxdist)
 {
-    int    i, nlines, line, npoints, type, cat, fcat, tcat, fline, tline, fnode, tnode, count;
-    int    ret, sp;
+    int    i, nlines, line, npoints, type, cat, id, fcat, tcat, fline, tline, fnode, tnode, count;
+    int    ret, sp, input_mode, unreachable, nopoint, formaterr;
     struct ilist *AList;
     double cost;
     struct line_pnts *Points, *OPoints;
     struct line_cats *Cats;
     CIDX   *Cidx, *Citem;
-    char   buf[2000];
+    char   buf[2000], dummy[2000];
+    double fx, fy, tx, ty;
 
     /* Attribute table */
     dbString sql;
@@ -74,8 +78,9 @@ int path ( struct Map_info *In, struct Map_info *Out, int nfield )
     if ( driver == NULL )
 	G_fatal_error ( "Cannot open database %s by driver %s", Fi->database, Fi->driver );
     
-    sprintf ( buf, "create table %s ( cat integer, fcat int, tcat int, " 
-	      "sp int, cost double precision )", Fi->table );
+    sprintf ( buf, "create table %s ( cat integer, id integer, fcat integer, tcat integer, " 
+	      "sp integer, cost double precision, fdist double precision, tdist double precision )", 
+	      Fi->table );
 
     db_set_string ( &sql, buf );
     G_debug ( 2, db_get_string ( &sql ) );
@@ -85,75 +90,106 @@ int path ( struct Map_info *In, struct Map_info *Out, int nfield )
 	G_fatal_error ( "Cannot create table: %s", db_get_string ( &sql )  );
     }
     
-    
     /* Read stdin, find shortest path, and write connectin line and new database record */
     cat = 0;
+    formaterr = nopoint = unreachable = 0;
     while ( fgets (buf, sizeof(buf), stdin) != NULL ) {
+	double fdist, tdist;
+
 	G_chop(buf);
-	ret = sscanf ( buf, "%d %d", &fcat, &tcat);
-	if ( ret != 2 ) {
-	    G_warning ( "Wrong input: %s", buf);
-	    continue;
+	
+	ret = sscanf ( buf, "%d %lf %lf %lf %lf %s", &id, &fx, &fy, &tx, &ty, dummy);
+	if ( ret == 5 ) {
+	    input_mode = INPUT_MODE_COOR;
+	} else {
+	    ret = sscanf ( buf, "%d %d %d %s", &id, &fcat, &tcat, dummy);
+	    if ( ret != 3 ) {
+		G_warning ( "Wrong input: %s", buf);
+		formaterr++;
+		continue;
+	    }
+	    input_mode = INPUT_MODE_NODE;
 	}
+
+	G_debug ( 3, "mode = %d", input_mode );
 	
 	cat++; /* Output category */
 	sp = SP_OK;
-	cost = 0;
+	cost = fdist = tdist = 0;
 
-	/* From */
-	Citem = (CIDX *) bsearch( (void *) &fcat, Cidx, npoints, sizeof(CIDX), cmp);
-	if ( Citem == NULL ) {
-	    G_warning ( "No point with category %d", fcat);
-	    sp = SP_NOPOINT; 
-	    fline = 0;
-	    fnode = 0;
-	} else {
-	    fline = Citem->line;
-	    Vect_get_line_nodes ( In, fline, &fnode, NULL);
-	}
-	G_debug ( 3, "from: cat = %5d point(line) = %5d node = %5d", fcat, fline, fnode );
-	
-	/* To */
-	Citem = (CIDX *) bsearch( (void *) &tcat, Cidx, npoints, sizeof(CIDX), cmp);
-	if ( Citem == NULL ) {
-	    G_warning ( "No point with category %d", tcat);
-	    sp = SP_NOPOINT;
-	    tline = 0;
-	    tnode = 0;
-	} else { 
-	    tline = Citem->line;
-	    Vect_get_line_nodes ( In, tline, &tnode, NULL);
-	}
-	G_debug ( 3, "to  : cat = %5d point(line) = %5d node = %5d", tcat, tline, tnode );
-		
-	if ( sp != SP_NOPOINT ) {
-	    ret = Vect_net_shortest_path ( In, fnode, tnode, AList, &cost);
-
-	    if ( ret == -1 ) {
-		sp = SP_UNREACHABLE;
-		G_warning ( "Point with category %d is not reachable from point with category %d\n", 
-			    tcat, fcat );
+	if ( input_mode == INPUT_MODE_NODE ) {
+	    /* From */
+	    Citem = (CIDX *) bsearch( (void *) &fcat, Cidx, npoints, sizeof(CIDX), cmp);
+	    if ( Citem == NULL ) {
+		G_warning ( "No point with category %d", fcat);
+		sp = SP_NOPOINT; 
+		fline = 0;
+		fnode = 0;
+		nopoint++;
 	    } else {
-		/* Write new line connecting 'from' and 'to' */
-		G_debug ( 3, "Number of arcs = %d, total costs = %f",  AList->n_values, cost);
+		fline = Citem->line;
+		Vect_get_line_nodes ( In, fline, &fnode, NULL);
+	    }
+	    G_debug ( 3, "from: cat = %5d point(line) = %5d node = %5d", fcat, fline, fnode );
+	    
+	    /* To */
+	    Citem = (CIDX *) bsearch( (void *) &tcat, Cidx, npoints, sizeof(CIDX), cmp);
+	    if ( Citem == NULL ) {
+		G_warning ( "No point with category %d", tcat);
+		sp = SP_NOPOINT;
+		tline = 0;
+		tnode = 0;
+		nopoint++;
+	    } else { 
+		tline = Citem->line;
+		Vect_get_line_nodes ( In, tline, &tnode, NULL);
+	    }
+	    G_debug ( 3, "to  : cat = %5d point(line) = %5d node = %5d", tcat, tline, tnode );
+		    
+	    if ( sp != SP_NOPOINT ) {
+		ret = Vect_net_shortest_path ( In, fnode, tnode, AList, &cost);
 
-		Vect_reset_line ( OPoints );
+		if ( ret == -1 ) {
+		    sp = SP_UNREACHABLE;
+		    unreachable++;
+		    G_warning ( "Point with category %d is not reachable from point with category %d\n", 
+				tcat, fcat );
+		} else {
+		    /* Write new line connecting 'from' and 'to' */
+		    G_debug ( 3, "Number of arcs = %d, total costs = %f",  AList->n_values, cost);
+
+		    Vect_reset_line ( OPoints );
+		    Vect_reset_cats ( Cats );
+		    Vect_cat_set ( Cats, 1, cat );
+		    for ( i = 0; i < AList->n_values; i++ ) {
+			line = AList->value[i];
+			Vect_read_line ( In, Points, NULL, abs(line) );
+			if ( line > 0 ) 
+			    Vect_append_points ( OPoints,  Points, GV_FORWARD );
+			else 
+			    Vect_append_points ( OPoints,  Points, GV_BACKWARD );
+		    }	
+		    Vect_write_line ( Out, GV_LINE, OPoints, Cats);
+		}
+	    }
+	} else { /* INPUT_MODE_COOR */
+	    fcat = tcat = 0;
+	    ret = Vect_net_shortest_path_coor ( In, fx, fy, 0.0, tx, ty, 0.0, maxdist, maxdist,
+                                                &cost, OPoints, &fdist, &tdist );
+
+	    if ( ret == 0 ) {
+		sp = SP_UNREACHABLE;
+		unreachable++;
+		G_warning ( "Point %f,%f is not reachable from point %f,%f\n", tx, ty, fx, fy );
+	    } else {
 		Vect_reset_cats ( Cats );
-	        Vect_cat_set ( Cats, 1, cat );
-		for ( i = 0; i < AList->n_values; i++ ) {
-		    line = AList->value[i];
-		    Vect_read_line ( In, Points, NULL, abs(line) );
-		    if ( line > 0 ) 
-			Vect_append_points ( OPoints,  Points, GV_FORWARD );
-		    else 
-			Vect_append_points ( OPoints,  Points, GV_BACKWARD );
-		}	
+		Vect_cat_set ( Cats, 1, cat );
 		Vect_write_line ( Out, GV_LINE, OPoints, Cats);
 	    }
 	}
 
-	sprintf (buf, "insert into %s values ( %d, %d, %d, %d, %f)", 
-		       Fi->table, cat, fcat, tcat, sp, cost );
+	sprintf (buf, "insert into %s values ( %d, %d, %d, %d, %d, %f, %f, %f)", 
+		       Fi->table, cat, id, fcat, tcat, sp, cost, fdist, tdist );
 	db_set_string ( &sql, buf );
 	G_debug ( 3, db_get_string ( &sql ) );
 	
@@ -172,14 +208,24 @@ int path ( struct Map_info *In, struct Map_info *Out, int nfield )
 
     /* Print table info */
     fprintf ( stderr, "Attributes:\n");
-    fprintf ( stderr, "  cat - path id\n");
+    fprintf ( stderr, "  cat  - path unique category assigned by module\n");
+    fprintf ( stderr, "  id   - path id (read from input)\n");
     fprintf ( stderr, "  fcat - from point category\n");
     fprintf ( stderr, "  tcat - to point category\n");
     fprintf ( stderr, "  sp - result status:\n");
     fprintf ( stderr, "       0 - OK, path found\n");
     fprintf ( stderr, "       1 - node is not reachable\n");
     fprintf ( stderr, "       2 - point of given category does not exist\n");
-    fprintf ( stderr, "  cost - travelling costs\n");
+    fprintf ( stderr, "  cost - travelling costs (on the network, not to/from network)\n");
+    fprintf ( stderr, "  fdist - the distance from first point to the network\n");
+    fprintf ( stderr, "  tdist - the distance from the network to second point\n");
+
+    if ( formaterr )
+       G_warning ( "%d input format errors", formaterr );
+    if ( nopoint )
+       G_warning ( "%d points of given category missing", nopoint );
+    if ( unreachable )
+       G_warning ( "%d destinations unreachable (including points out of threshold)", unreachable );
 
     return 1;
 }
