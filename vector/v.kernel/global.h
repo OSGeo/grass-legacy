d6d422ec9df4e213d91b24d5ed83d84d546ba533
Markus Neteler <neteler@gmail.com>
2004-07-20 13:32:59 +0000

    Stefano Menegon <menegon itc it> + Radim Blazek: update of command with new brent function and new parameter 'net'
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@12999 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.kernel/global.h
+++ vector/v.kernel/global.h
@@ -2,9 +2,12 @@
 
 double euclidean_distance(double *x, double *y, int n);
 double gaussian2dBySigma( double d, double sigma );
+double gaussianFunction(double x, double sigma, double dimension);
+double gaussianKernel(double x, double term);
+
 double invGaussian2d(double sigma, double prob);
 double gaussian2dByTerms(double d, double term1, double term2);
-double brent(double ax, double bx, double cx, double (*f)(), double tol, double *xmin);
+double brent_iterate (double (* f)(),double x_lower, double x_upper,int maxiter);
 double kernel1(double d, double rs, double lambda);
 double segno(double x);
 
@@ -12,8 +15,8 @@ int read_points(struct Map_info *In, double ***coordinate, double dsize);
 double compute_all_distances(double **coordinate, double **dists, int n, double dmax);
 double compute_all_net_distances(struct Map_info *In, struct Map_info *Net, double netmax, 
 	                         double **dists, double dmax);
-void compute_distance( double N, double E, struct Map_info *In,
-	                       double term1, double term2, double *gaussian, double dmax);
-void compute_net_distance( double x, double y, struct Map_info *In, struct Map_info *Net, double netmax,
-	                       double term1, double term2, double *gaussian, double dmax);
+void compute_distance( double N, double E, struct Map_info *In, 
+	               double sigma, double term, double *gaussian, double dmax);
+void compute_net_distance( double x, double y, struct Map_info *In, struct Map_info *Net, double netmax, 
+			   double sigma, double term, double *gaussian, double dmax);
 
