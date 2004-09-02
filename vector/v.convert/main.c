ea5a61a5b49549962d5b325225d2ce54101f3cdf
Hamish Bowman <hamish_b@yahoo.com>
2004-09-02 03:08:54 +0000

    less ambiguous description cleanup whitespace
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@13178 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.convert/main.c
+++ vector/v.convert/main.c
@@ -6,8 +6,6 @@
  * *               
  * * PURPOSE:      Convert GRASS vector files versions:
  * *               from 3 or 4 to 5.0
- * *               or
- * *               from 5.0 to 4.10
  * *               
  * * COPYRIGHT:    (C) 2001 by the GRASS Development Team
  * *
@@ -28,13 +26,13 @@
 int 
 main (int argc, char *argv[])
 {
-    struct Option *opt_in, *opt_out, *opt_end; 
+    struct Option *opt_in, *opt_out, *opt_end;
     int    endian;
     struct GModule *module;
-    
+
     module = G_define_module();
-    module->description = "Converts between GRASS vector versions.";
-    
+    module->description = "Imports older versions of GRASS vectors.";
+
     /* input vector map */
     opt_in = G_define_option();
     opt_in->key         = "input";
@@ -42,8 +40,8 @@ main (int argc, char *argv[])
     opt_in->required    = YES ;
     opt_in->multiple    = NO ;
     opt_in->gisprompt   = "old,dig,vector" ;
-    opt_in->description = "input vector map";  
-    
+    opt_in->description = "input vector map";
+
     /* output vector map */
     opt_out = G_define_standard_option(G_OPT_V_OUTPUT);
 
@@ -53,26 +51,22 @@ main (int argc, char *argv[])
     opt_end->type        = TYPE_STRING ;
     opt_end->required    = NO;
     opt_end->multiple    = NO;
-    opt_end->options     = "big,little";    
-    opt_end->description = "endian of input vector map";      
-    opt_end->answer = "big"; 
-    
+    opt_end->options     = "big,little";
+    opt_end->description = "endian of input vector map";
+    opt_end->answer = "big";
+
     G_gisinit(argv[0]);
-    
+
     if (G_parser(argc,argv))
-        exit(-1);   
+        exit(-1);
 
     /* Numbers in portable format files are saved as big endians */
-    if ( opt_end->answer[0] == 'l' )  
+    if ( opt_end->answer[0] == 'l' )
 	endian = ENDIAN_LITTLE;
-    else	
+    else
 	endian = ENDIAN_BIG;
 
     old2new (opt_in->answer, opt_out->answer, endian);
-	    
+
     exit(0);
 }
-
-
-
-
