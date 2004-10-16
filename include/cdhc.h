47e199384a7ae39b7e9221c9427c209295493c55
Radim Blazek <radim.blazek@gmail.com>
2004-10-16 10:25:14 +0000

    +cdhc
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@13305 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ include/cdhc.h
@@ -0,0 +1,36 @@
+#pragma ident "cdh C Version 1.0 <12 Sep 1994>"
+
+int dcmp ();
+int dblcomp ();
+double enormp ();
+double normp ();
+double xinormal ();
+double *dmax ();
+double *dmax_exp ();
+double *omnibus_moments ();
+double *geary_test ();
+double *student_range ();
+double *dagostino_d ();
+double *extreme ();
+double *kuipers_v ();
+double *watson_u2();
+double *durbins_exact ();
+double *anderson_darling();
+double *cramer_von_mises();
+double *kolmogorov_smirnov ();
+double *chi_square();
+double *shapiro_wilk();
+double *shapiro_francia();
+double *weisberg_bingham ();
+double *royston();
+double *shapiro_wilk_exp();
+double *kolmogorov_smirnov_exp ();
+double *cramer_von_mises_exp();
+double *kuipers_v_exp ();
+double *watson_u2_exp ();
+double *anderson_darling_exp();
+double *chi_square_exp();
+double *mod_maxlik_ratio ();
+double *coeff_variation();
+double *kotz_families();
+
