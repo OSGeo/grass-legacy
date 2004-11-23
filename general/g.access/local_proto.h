8777be1ffd921a28d451c2497f3eac4f07db616f
Radim Blazek <radim.blazek@gmail.com>
2004-11-23 13:22:40 +0000

    noninteractive
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16103 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.access/local_proto.h
+++ general/g.access/local_proto.h
@@ -1,10 +1,6 @@
-/* ask_perms.c */
-int ask_perms(int *, int *);
 /* exp_perms.c */
 char *explain_perms(int, int, int);
 /* get_perms.c */
 int get_perms(char *, int *, int *, int *);
 /* set_perms.c */
 int set_perms(char *, int, int, int);
-/* verify_perms.c */
-int verify_perms(int, int);
