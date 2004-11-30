faa141e5f950bc5d836c0bec0307345dcbb472a1
Markus Neteler <neteler@gmail.com>
2004-11-30 16:32:11 +0000

    return NULL if input string is NULL
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16180 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/store.c
+++ lib/gis/store.c
@@ -1,22 +1,15 @@
 #include "gis.h"
-/*
- *******************************************************************
- * G_store (string)
- *
- * allocates permanent memory to hold 'string'
- * copies 'string' to this memory, and returns the address
- * of the allocated memory
- *******************************************************************/
 #include <string.h>
 
 
 /*!
  * \brief copy string to allocated memory
  *
- * This routine
- * allocates enough memory to hold the string <b>s</b>, copies <b>s</b> to
- * the allocated memory, and returns a pointer to the allocated memory.
- *
+ * This routine allocates enough memory to hold the string <b>s</b>,
+ * copies <b>s</b> to the allocated memory, and returns a pointer
+ * to the allocated memory. In case the string <b>s</b> is NULL,
+ * the function returns NULL.
+ * 
  *  \param s
  *  \return char * 
  */
@@ -25,6 +18,8 @@ char *G_store (s) char *s;
 {
     char *buf;
 
+    if ( s == NULL ) return NULL ;
+    
     buf = G_malloc (strlen(s) + 1);
     strcpy (buf, s);
     return buf;
