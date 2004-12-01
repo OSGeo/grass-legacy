88a0ae166eabcc30f9331d0a8826e16f6b63b239
Markus Neteler <neteler@gmail.com>
2004-12-01 12:45:55 +0000

    maintain grass-run.sh here
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16199 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/init/grass-run.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#script to launch GRASS commands (workaround for systems
+#with xterm is setuid/setgid
+
+#http://grass.itc.it/pipermail/grass5/2004-September/015409.html
+
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"$GISBASE/lib"
+export LD_LIBRARY_PATH
+exec "$@"
+
