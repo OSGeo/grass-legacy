94fa3f9b4c3f6e27a0e4b2cea1ee37d4a766853d
Radim Blazek <radim.blazek@gmail.com>
2004-12-09 13:19:05 +0000

    _
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16275 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/mysql/db.c
+++ db/drivers/mysql/db.c
@@ -15,6 +15,7 @@
 *****************************************************************************/
 #include <stdlib.h>
 #include <string.h>
+#include <glocale.h>
 #include <gis.h>
 #include <dbmi.h>
 #include "globals.h"
