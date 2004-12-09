d09eb28ec566f75227ff4d7564fffaaa7822c2b2
Markus Neteler <neteler@gmail.com>
2004-12-09 17:04:18 +0000

    inverted G_store/NULL change
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16289 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/store.c
+++ lib/gis/store.c
@@ -7,8 +7,7 @@
  *
  * This routine allocates enough memory to hold the string <b>s</b>,
  * copies <b>s</b> to the allocated memory, and returns a pointer
- * to the allocated memory. In case the string <b>s</b> is NULL,
- * the function returns NULL.
+ * to the allocated memory.
  * 
  *  \param s
  *  \return char * 
@@ -18,8 +17,6 @@ char *G_store (s) char *s;
 {
     char *buf;
 
-    if ( s == NULL ) return NULL ;
-    
     buf = G_malloc (strlen(s) + 1);
     strcpy (buf, s);
     return buf;
