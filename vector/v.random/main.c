17edaad315fb62e770aaabd4a7bf0c4b625235a6
Radim Blazek <radim.blazek@gmail.com>
2004-12-14 16:55:11 +0000

    n limit removed
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16340 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.random/main.c
+++ vector/v.random/main.c
@@ -91,7 +91,7 @@ int main (int argc, char *argv[])
   parm.nsites->key = "n";
   parm.nsites->type = TYPE_INTEGER;
   parm.nsites->required = YES;
-  parm.nsites->description = "number of points to be created (1-32767)";
+  parm.nsites->description = "number of points to be created";
 
   flag.drand48 = G_define_flag ();
   flag.drand48->key = 'd';
