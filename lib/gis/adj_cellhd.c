84b8ec76974267e892a99912bab777907f99f730
Radim Blazek <radim.blazek@gmail.com>
2004-12-15 10:55:02 +0000

    G_adjust_Cell_head only 2D
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16355 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/adj_cellhd.c
+++ lib/gis/adj_cellhd.c
@@ -50,8 +50,7 @@ G_adjust_Cell_head (cellhd, row_flag, col_flag)
  * <i>rows</i> is computed from the north-south resolution in the structure, similarly for
  * <b>col_flag</b> and the number of columns and the east-west resolution. 
  *
- * If row_flag is true, top-bottom resolution is calculated from depths.
- * If row_flag are false, number of depths is calculated from top-bottom resolution.
+ * 3D values are not adjusted.
  *
  * This routine returns NULL if execution occurs without error, otherwise it returns
  * an error message.
@@ -64,7 +63,75 @@ G_adjust_Cell_head (cellhd, row_flag, col_flag)
 
 char *G_adjust_Cell_head(struct Cell_head *cellhd,int row_flag,int col_flag)
 {
-    return G_adjust_Cell_head3 ( cellhd, row_flag, col_flag, row_flag );
+    if (!row_flag)
+    {
+	if (cellhd->ns_res <= 0)
+	    return (_("Illegal n-s resolution value"));
+    }
+    else
+    {
+	if (cellhd->rows <= 0)
+	    return (_("Illegal row value"));
+    }
+    if (!col_flag)
+    {
+	if (cellhd->ew_res <= 0)
+	    return (_("Illegal e-w resolution value"));
+    }
+    else
+    {
+	if (cellhd->cols <= 0)
+	    return (_("Illegal col value"));
+    }
+
+/* for lat/lon, check north,south. force east larger than west */
+    if (cellhd->proj == PROJECTION_LL)
+    {
+	if (cellhd->north > 90.0)
+	    return (_("Illegal latitude for North"));
+	if (cellhd->south < -90.0)
+	    return (_("Illegal latitude for South"));
+	while (cellhd->east <= cellhd->west)
+	    cellhd->east += 360.0;
+    }
+
+/* check the edge values */
+    if (cellhd->north <= cellhd->south)
+    {
+	if (cellhd->proj == PROJECTION_LL)
+	    return (_("North must be north of South"));
+	else
+	    return (_("North must be larger than South"));
+    }
+    if (cellhd->east <= cellhd->west)
+	return (_("East must be larger than West"));
+
+
+/* compute rows and columns, if not set */
+    if (!row_flag)
+    {
+	cellhd->rows = (cellhd->north - cellhd->south + cellhd->ns_res/2.0) / cellhd->ns_res;
+	if (cellhd->rows == 0)
+	    cellhd->rows = 1;
+    }
+    if (!col_flag)
+    {
+	cellhd->cols = (cellhd->east - cellhd->west + cellhd->ew_res/2.0) / cellhd->ew_res;
+	if (cellhd->cols == 0)
+	    cellhd->cols = 1;
+    }
+
+    if (cellhd->cols < 0 || cellhd->rows < 0 )
+    {
+	return (_("Invalid coordinates"));
+    }
+
+
+/* (re)compute the resolutions */
+    cellhd->ns_res = (cellhd->north - cellhd->south) / cellhd->rows;
+    cellhd->ew_res = (cellhd->east  - cellhd->west)  / cellhd->cols;
+	
+    return NULL;
 }
 
 /*!
