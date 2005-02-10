e992e98ce8c35fa3dd3d5fb918f1cc0ec8b6372e
Huidae Cho <grass4u@gmail.com>
2005-02-10 21:41:02 +0000

    Made it work with a non-GNU sed
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16693 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/mk_dbdriver_h.sh
+++ db/drivers/mk_dbdriver_h.sh
@@ -11,14 +11,14 @@ cat <<'EOT'> dbdriver.h
 
 EOT
 
-grep -h '^\( *int *\)\?db__driver' *.c | sed -e 's/^\( *int *\)\?/int /' -e 's/ *(.*).*$/();/' > $tmp
+grep -h '^\( *int *\)\?db__driver' *.c | sed -e 's/^\( *int *\)*/int /' -e 's/ *(.*).*$/();/' > $tmp
 cat $tmp >> dbdriver.h
 
 cat <<'EOT' >> dbdriver.h
 #define	init_dbdriver() do{\
 EOT
 
-sed 's/^int *db__\([a-zA-Z_]\+\).*$/db_\1 = db__\1;\\/' $tmp >> dbdriver.h
+sed 's/^int *db__\([a-zA-Z_]*\).*$/db_\1 = db__\1;\\/' $tmp >> dbdriver.h
 cat <<'EOT'>> dbdriver.h
 }while(0)
 
