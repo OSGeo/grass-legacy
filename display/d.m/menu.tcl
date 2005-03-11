9f5b2bdf24cf6c0fd797520279bbcb496951f9a8
Huidae Cho <grass4u@gmail.com>
2005-03-11 19:47:06 +0000

    Added Cygwin hack for GRASS_HTML_BROWSER. The idea was provided by Marco Lechner.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16849 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.m/menu.tcl
+++ display/d.m/menu.tcl
@@ -495,8 +495,8 @@
 			}}
  } 
  "&Help" all options 1 {
- {command "GRASS help" {} "g.manual" {} -command { exec $env(GRASS_HTML_BROWSER) $env(GISBASE)/docs/html/index.html & } }
- {command "d.m &help" {} "d.m help" {} -command { exec $env(GRASS_HTML_BROWSER) $env(GISBASE)/docs/html/d.m.html & } }
+ {command "GRASS help" {} "g.manual" {} -command { exec g.manual -i > /dev/null & } }
+ {command "d.m &help" {} "d.m help" {} -command { exec g.manual d.m > /dev/null & } }
  {command "About &GRASS" {} "About GRASS" {} -command { source $env(GISBASE)/etc/dm/grassabout.tcl} }
  {command "About &System" {} "About System" {} -command { exec $env(GRASS_WISH) $env(GISBASE)/etc/dm/tksys.tcl --tcltk } }
  }
