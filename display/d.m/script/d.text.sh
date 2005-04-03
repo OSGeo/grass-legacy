bccb2b4f6aebb40e300a7c7485b7f3f100d6f67b
Hamish Bowman <hamish_b@yahoo.com>
2005-04-03 04:45:40 +0000

    add two scripts from Michael Barton
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@16958 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ display/d.m/script/d.text.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+#
+############################################################################
+#
+# MODULE:		d.text.sh for GRASS 6
+# AUTHOR(S):	Michael Barton 
+# PURPOSE:	    Make module d.text useable from the GUI
+# COPYRIGHT:	(C) 2005 by the GRASS Development Team
+#
+#		This program is free software under the GNU General Public
+#		License (>=v2). Read the file COPYING that comes with GRASS
+#		for details.
+#
+#############################################################################
+
+
+#%Module
+#%  description: d.text.sh - Interactively create text in active display
+#%End
+#%option
+#% key: size
+#% type: string
+#% description: type point size (default 5 points)
+#% required : no
+#%end
+#%option
+#% key: color
+#% type: string
+#% description: Text color (standard GRASS color or r:g:b triplet - default grey)
+#% required : no
+#%end
+#%option
+#% key: line
+#% type: integer
+#% description: Screen line where text will be drawn (1-1000)
+#% options: 1-1000
+#% required : no
+#%end
+#%option
+#% key: at
+#% type: double
+#% description: Screen location for text (in percentage from left,bottom [0,0])
+#% options: 0-100
+#% required : no
+#%end
+#% flag
+#% key: b
+#% description: bold text
+#%end
+
+if  [ -z $GISBASE ] ; then
+ echo "You must be in GRASS GIS to run this program."
+ exit 1
+fi   
+
+if [ "$1" != "@ARGS_PARSED@" ] ; then
+  exec g.parser "$0" "$@"
+fi
+
+cmd="d.text"
+
+if [ $GIS_OPT_size ] ; then
+    cmd=$cmd" size="$GIS_OPT_size
+fi
+
+if [ $GIS_OPT_color ] ; then
+    cmd=$cmd" color="$GIS_OPT_color
+fi
+
+if [ $GIS_OPT_line ] ; then
+    cmd=$cmd" line="$GIS_OPT_line
+fi
+
+if [ $GIS_OPT_at ] ; then
+    cmd=$cmd" at="$GIS_OPT_at
+fi
+
+if [ $GIS_FLAG_b = 1 ] ; then
+    cmd=$cmd" -b"
+fi
+
+echo $comd
+
+eval `exec xterm -e $cmd`
