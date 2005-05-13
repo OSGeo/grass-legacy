b5a5c4b4e80a02c3b85abb3b5e89b8a6877baf3c
Markus Neteler <neteler@gmail.com>
2005-05-13 12:32:00 +0000

    fixed declaration
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17128 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.cca/globals.h
+++ imagery/i.cca/globals.h
@@ -2,6 +2,10 @@
 #define MX 9
 #define MC 50
 
-
+#ifdef MAIN
 char outputfile[50];
 char groupname[50], subgroup[50], signame[50];
+#else
+extern char outputfile[50];
+extern char groupname[50], subgroup[50], signame[50];
+#endif
