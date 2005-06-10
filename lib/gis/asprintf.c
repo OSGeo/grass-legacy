4d206857ff254f04ca8d6e7bd83be42e0a066303
Brad Douglas <rez@touchofmadness.com>
2005-06-10 02:40:32 +0000

    Updated to use G_*() commands. ie. G_tempfile(), G_calloc()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17258 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/asprintf.c
+++ lib/gis/asprintf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 #include <assert.h>
 #include "gis.h"
 
@@ -10,51 +11,55 @@
  * 
  * 
  * I've got a sort of cheat for asprintf. We can't use vsnprintf for the
- * same reason we can't use snprintf ;-)  Comments welcome (I'm not too
- * sure tmpfile() is safe.  It's apparently an anonymous file on my
- * machine, as nothing shows up in /tmp or the current directory...)
+ * same reason we can't use snprintf ;-)  Comments welcome.
  */
 
 /* Make sure the macro doesn't impact our function, if it is defined */
 #undef G_asprintf
 
 /* We cheat by printing to a tempfile via vfprintf and then reading it
- *  * back in.  Not the most efficient way, probably and tmpfile() is
- *  * not safe?
+ *  * back in.  Not the most efficient way, probably.
  *  */
 int
   G_asprintf (char **out, const char *fmt, ...)
 {
        va_list ap;
        int ret_status = EOF;
-       int count = 0;
-       FILE *fp = NULL;
+       char *fn = NULL;
        char *work = NULL;
        
        assert (out != NULL && fmt != NULL);
        
        va_start (ap, fmt);
-       if ((fp = tmpfile()))
+
+       if ((fn = G_tempfile()))
      {
+        FILE *fp;
+        int count;
+
+                fp = fopen (fn, "w+b");
 	        count = vfprintf (fp, fmt, ap);
 	        if (count >= 0)
 	  {
-	                 work = calloc (count + 1, 1);
+	                 work = G_calloc (count + 1, sizeof(char));
 	                 if (work != NULL)
 	       {
 		                  rewind (fp);
-		                  ret_status = fread (work, 1, count, fp);
+		                  ret_status = fread (work, sizeof(char), count, fp);
 		                  if (ret_status != count)
 		    {
 		                           ret_status = EOF;
-		                           free (work);
+		                           G_free (work);
 		                           work = NULL;
 		    }
 	       }
 	  }
-	        fclose (fp);
+          fclose (fp);
+          unlink (fn);
+          G_free (fn);
      }
        va_end (ap);
        *out = work;
+
        return ret_status;
 }
