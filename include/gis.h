dc0a536b8c0f845b99e254ae427337d90404c4b3
Markus Neteler <neteler@gmail.com>
2005-07-12 16:47:12 +0000

    M_PI_2 as well
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17450 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/gis.h
+++ include/gis.h
@@ -79,7 +79,11 @@ static const char *GRASS_copyright __attribute__((unused))
 
 /* define PI if not present */
 #ifndef M_PI
-#define M_PI 3.14159265358979323846
+#define M_PI    3.14159265358979323846 /* pi */
+#endif
+
+#ifndef M_PI_2
+#define M_PI_2  1.57079632679489661923 /* pi/2 */
 #endif
 
 /* Location of envariment variables */
