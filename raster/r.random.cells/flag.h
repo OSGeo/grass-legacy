cdf42dd44bb26715ca8ec0a11a46d8b57564f4fd
Brad Douglas <rez@touchofmadness.com>
2005-08-14 19:22:44 +0000

    - Upgraded to modern programming standards. - Added local_proto.h for global functions. - Made local functions static. - Removed unused variables.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17767 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.random.cells/flag.h
+++ raster/r.random.cells/flag.h
@@ -1,41 +1,8 @@
-/* flag.[ch] is a set of routines which will set up an array of bits
-** that allow the programmer to "flag" cells in a cell file.
-**
-** FLAG *
-** FlagCreate(nrows,ncols)
-** int nrows, ncols;
-**	opens the structure flag.  
-**	The flag structure will be a two dimensional array of bits the
-**	size of nrows by ncols.  Will initalize flags to zero (unset).
-**
-** FlagDestroy(flags)
-** FLAG *flags;
-**	closes flags and gives the memory back to the system.
-**
-** FlagClearAll(flags)
-** FLAG *flags;
-**	sets all values in flags to zero.
-**
-** FlagUnset(flags, row, col)
-** FLAG *flags;
-** int row, col;
-**	sets the value of (row, col) in flags to zero.
-**
-** FlagSet(flags, row, col)
-** FLAG *flags;
-** int row, col;
-**	will set the value of (row, col) in flags to one.
-**
-** int
-** FlagGet(flags, row, col)
-** FLAG *flags;
-** int row, col;
-**	returns the value in flags that is at (row, col).
-**
-** idea by Michael Shapiro
+/** idea by Michael Shapiro
 ** code by Chuck Ehlschlaeger
 ** April 03, 1989
 */
+
 #include <stdio.h>
 #define FLAG	struct _f_l_a_g_
 
@@ -53,3 +20,28 @@ FLAG
 
 #define FLAG_GET(flags,row,col) \
 	(flags)->array[(row)][(col)>>3] & (1<<((col) & 7))
+
+
+/* flag.[ch] is a set of routines which will set up an array of bits
+** that allow the programmer to "flag" cells in a cell file.
+**/
+FLAG *FlagCreate(int nrows, int ncols);
+/**	opens the structure flag.  
+**	The flag structure will be a two dimensional array of bits the
+**	size of nrows by ncols.  Will initalize flags to zero (unset).
+**/
+void FlagDestroy(FLAG *flags);
+/**	closes flags and gives the memory back to the system.
+**/
+void FlagClearAll(FLAG *flags);
+/**	sets all values in flags to zero.
+**/
+void FlagUnset(FLAG *flags, int row, int col);
+/**	sets the value of (row, col) in flags to zero.
+**/
+void FlagSet(FLAG *flags, int row, int col);
+/**	will set the value of (row, col) in flags to one.
+**/
+int FlagGet(FLAG *flags, int row, int col);
+/**	returns the value in flags that is at (row, col).
+**/
