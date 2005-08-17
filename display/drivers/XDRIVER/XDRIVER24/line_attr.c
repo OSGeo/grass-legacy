f3a6bb17f3150a535b62e8ef6be66c00af5335ee
Huidae Cho <grass4u@gmail.com>
2005-08-17 21:36:21 +0000

    removed unnecessary line
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17796 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/drivers/XDRIVER/XDRIVER24/line_attr.c
+++ display/drivers/XDRIVER/XDRIVER24/line_attr.c
@@ -6,7 +6,6 @@ line_width(int width)
 {
 	XGCValues gc_values;
 
-	XGetGCValues(dpy, gc, GCLineWidth|GCCapStyle, &gc_values);
 	gc_values.line_width = (width < 0 ? 0 : width);
 	gc_values.cap_style = CapRound;
 	XChangeGC(dpy, gc, GCLineWidth|GCCapStyle, &gc_values);
