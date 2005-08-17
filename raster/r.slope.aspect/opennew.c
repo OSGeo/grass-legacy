539a7e6393285fc5f76ffb33e024e6eedeab56df
Brad Douglas <rez@touchofmadness.com>
2005-08-17 08:00:50 +0000

    - GRASS6 usage of G_fatal_error(). - Remove now unused buffer variable.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17780 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.slope.aspect/opennew.c
+++ raster/r.slope.aspect/opennew.c
@@ -1,17 +1,13 @@
 #include <stdlib.h>
 #include "gis.h"
 
+
 int opennew (char *name, RASTER_MAP_TYPE wr_type)
 {
     int fd;
-    char err[400];
 
     if (G_legal_filename (name) < 0)
-    {
-	sprintf (err, "%s - ** illegal name **", name);
-	G_fatal_error (err);
-	exit(1);
-    }
+	G_fatal_error ("%s - ** illegal name **", name);
 
     if(wr_type < 0) /* default fp type */
        fd = G_open_fp_cell_new (name);
@@ -19,11 +15,7 @@ int opennew (char *name, RASTER_MAP_TYPE wr_type)
        fd = G_open_raster_new (name, wr_type);
 
     if (fd < 0)
-    {
-	sprintf (err, "failed in attempt to open %s\n", name);
-	G_fatal_error (err);
-	exit(1);
-    }
+	G_fatal_error ("failed in attempt to open %s\n", name);
 
     return fd;
 }
