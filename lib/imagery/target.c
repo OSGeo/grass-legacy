17240608f0d765f4534f9ffdba19d87d64e14704
Brad Douglas <rez@touchofmadness.com>
2005-08-18 04:03:22 +0000

    - GRASS6 usage of G_warning(). - Remove now unused variable 'msg'. - Add stdio.h.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17799 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/imagery/target.c
+++ lib/imagery/target.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include "gis.h"
 #include "imagery.h"
 
 
@@ -31,15 +33,15 @@ int I_get_target(
     G_suppress_warnings (0);
     if (fd == NULL)
 	return 0;
+
     ok = (fscanf (fd, "%s %s", location, mapset) == 2);
     fclose (fd);
     if (!ok)
     {
-	char msg[100];
 	*location = *mapset = 0;
-	sprintf (msg, "unable to read target file for group [%s]", group);
-	G_warning (msg);
+	G_warning ("unable to read target file for group [%s]", group);
     }
+
     return ok;
 }
 
@@ -71,7 +73,9 @@ int I_put_target (
     fd = I_fopen_group_file_new (group, "TARGET");
     if (fd == NULL)
 	return 0;
+
     fprintf (fd, "%s\n%s\n", location, mapset);
     fclose (fd);
+
     return 1;
 }
