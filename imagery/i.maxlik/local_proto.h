cbd869de22ff6afadf0932d7a457a2c0774c7c6e
Brad Douglas <rez@touchofmadness.com>
2005-08-19 10:45:25 +0000

    - Removed unused functions from local_proto.h. - GRASS6 usage of message functions. - Fix compiler warnings. - Use GRASS6 message functions instead of fprintf(stderr,...).
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@17814 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.maxlik/local_proto.h
+++ imagery/i.maxlik/local_proto.h
@@ -1,16 +1,9 @@
 /* classify.c */
 int classify(CELL *, CELL *, int);
-/* dump_sigs.c */
-int main(int, char *[]);
-int test_signatures(struct Signature *);
-int print_sig(struct One_Sig *, int);
-int usage(char *);
 /* hist.c */
 int make_history(char *, char *, char *, char *);
 /* invert.c */
 int invert_signatures(void);
 int invert(struct One_Sig *, int, int *, int *, double *);
-/* main.c */
-int main(int, char *[]);
 /* open.c */
 int open_files(void);
