4e66e9b16985f44366227b2993d17cdc28d0c3fc
Markus Neteler <neteler@gmail.com>
2005-11-22 20:14:27 +0000

    Jaro Hofierka: variables declaration fixes
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@18392 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/simwe/simlib/waterglobs.h
+++ raster/simwe/simlib/waterglobs.h
@@ -2,6 +2,7 @@
 #define MAXW    7000000
 #define UNDEF	-9999
 
+#ifdef MAIN
 FILE *fdelevin, *fddxin, *fddyin, *fdrain, *fdinfil, *fdtraps, *fdmanin, *fddepth, *fddisch, *fderr, *fdoutwalk,*fdwalkers;
 FILE *fdwdepth,*fddetin,*fdtranin,*fdtauin, *fdtc, *fdet, *fdconc, *fdflux,*fderdep;
 FILE *fdsfile,*fw;
@@ -67,7 +68,7 @@ int seedg(long int,long int);
 int grad_check(void);
 void erod(double **);
 void main_loop(void);
-int output_data(int,double);    
+int output_data(int,double);
 int output_et(void);
 double ulec(void);
 double gasdev(void);
@@ -111,3 +112,116 @@ int iterout, mx2o, my2o;
 int miter,nwalka,lwwfin;
 double timec;
 int ts, timesec;
+#else
+extern FILE *fdelevin, *fddxin, *fddyin, *fdrain, *fdinfil, *fdtraps, *fdmanin, *fddepth, *fddisch, *fderr, *fdoutwalk,*fdwalkers;
+extern FILE *fdwdepth,*fddetin,*fdtranin,*fdtauin, *fdtc, *fdet, *fdconc, *fdflux,*fderdep;
+extern FILE *fdsfile,*fw;
+
+extern char *elevin;
+extern char *dxin;
+extern char *dyin;
+extern char *rain;
+extern char *infil;
+extern char *traps;
+extern char *manin;
+extern char *sfile;
+extern char *depth;
+extern char *disch;
+extern char *err;
+extern char *outwalk;
+extern char *mapset;
+extern char *mscale;
+extern char *tserie;
+
+extern char *wdepth;
+extern char *detin;
+extern char *tranin;
+extern char *tauin;
+extern char *tc;
+extern char *et;
+extern char *conc;
+extern char *flux;
+extern char *erdep;
+
+
+extern struct
+  {
+    struct Option *elevin,*dxin,*dyin,*rain,*infil,*traps,*manin,*sfile,*depth,*disch,*err,
+*outwalk,*nwalk,*niter,*outiter,*density,*diffc,*hmax,*halpha,*hbeta,*wdepth,
+*detin,*tranin,*tauin,*tc,*et,*conc,*flux,*erdep;
+  } parm;
+
+extern struct
+  {
+    struct Flag *mscale, *tserie;
+  } flag;
+
+
+extern struct {
+    long int is1, is2;
+} seed;
+
+
+extern struct Cell_head cellhd;
+
+extern struct Point
+{
+    double north, east;
+    double z1;
+};
+extern struct Point *points;
+extern int npoints;
+extern int npoints_alloc;
+
+
+extern int input_data(void);
+extern int seeds(long int,long int);
+extern int seedg(long int,long int);
+extern int grad_check(void);
+extern void erod(double **);
+extern void main_loop(void);
+extern int output_data(int,double);
+extern int output_et(void);
+extern double ulec(void);
+extern double gasdev(void);
+extern double amax1(double,double);
+extern double amin1(double,double);
+extern int min(int,int);
+extern int max(int,int);
+
+extern double xmin, ymin, xmax, ymax;
+extern double mayy, miyy, maxx, mixx;
+extern int mx, my;
+extern int mx2, my2;
+
+extern double bxmi,bymi,bxma,byma,bresx,bresy;
+extern int maxwab;
+extern double step,conv;
+
+extern double frac;
+extern double bxmi, bymi;
+
+extern float **zz, **cchez;
+extern double **v1, **v2, **slope;
+extern double **gama, **gammas,**si,**inf,**sigma;
+extern float **dc,**tau,**er, **ct, **trap;
+extern float  **dif;
+
+extern double vavg[MAXW][2], stack[MAXW][3],w[MAXW][3];
+extern int iflag[MAXW];
+
+extern double hbeta;
+extern int ldemo;
+extern double hhmax, sisum,vmean;
+extern double infsum,infmean;
+extern int maxw, maxwa, nwalk;
+extern double rwalk, bresx, bresy, xrand, yrand;
+extern double stepx, stepy, xp0, yp0;
+extern double chmean, si0, deltap, deldif, cch, hhc,halpha;
+extern double eps;
+extern int maxwab, nstack;
+extern int iterout, mx2o, my2o;
+extern int miter,nwalka,lwwfin;
+extern double timec;
+extern int ts, timesec;
+#endif
