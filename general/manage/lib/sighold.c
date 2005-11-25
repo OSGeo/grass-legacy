9d9f289e4c9b77f4f807bad8d61ea687491a12a3
Radim Blazek <radim.blazek@gmail.com>
2005-11-25 12:50:16 +0000

    dont use SIGQUIT on MINGW (from Javier Yebrin)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@18405 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/manage/lib/sighold.c
+++ general/manage/lib/sighold.c
@@ -9,7 +9,11 @@ hold_signals (int hold)
     sig = hold ? SIG_IGN : SIG_DFL;
 
     signal (SIGINT, sig);
+
+#ifndef __MINGW32__
     signal (SIGQUIT, sig);
+#endif
+
 #ifdef SIGTSTP
     signal (SIGTSTP, sig);
 #endif
