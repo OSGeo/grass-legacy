9dac395954da75d38c3ff01ddd06b79181a85440
Markus Neteler <neteler@gmail.com>
2006-01-16 14:00:57 +0000

    Martin Landa <landa martin gmail com>: extended to display also colored lines and points based on GRASSRGB column (not only areas)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@18736 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.vect/local_proto.h
+++ display/d.vect/local_proto.h
@@ -1,9 +1,12 @@
+#include "symbol.h"
+
 FILE *open_vect(char *, char *);
 int close_vect(FILE *);
-int plot1(struct Map_info *, int, int, struct cat_list *, int, int, int, SYMBOL *, int, int);
+int plot1(struct Map_info *, int, int, struct cat_list *, int, int, int, SYMBOL *, int, int, int, int);
 int label(struct Map_info *, int, int, struct cat_list *, LATTR *, int);
 int topo(struct Map_info *, int, int, LATTR *);
 int dir(struct Map_info *, int, struct cat_list *, int);
 int darea(struct Map_info *, struct cat_list *, int, int, int, int, int, int, struct Cell_head *);
 int attr(struct Map_info *, int, char *, struct cat_list *, LATTR *, int);
 int zcoor(struct Map_info *, int, LATTR *);
+int test_bg_color (const char*);
