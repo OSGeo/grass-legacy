2f511317df8aee60619acf5f47e19cc72147b14c
Michael Barton <michael.barton@asu.edu>
2006-02-06 19:23:30 +0000

    update for thematic mapping, creation of new canvas for thematic map legend
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@18906 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/gis.m/thematic.tcl
+++ display/gis.m/thematic.tcl
@@ -1,10 +1,13 @@
 ###############################################################
 # thematic.tcl - thematic vector mapping layer options file for GRASS GIS Manager
-# January 2006 Michael Barton, Arizona State University
+# February 2006 Michael Barton, Arizona State University
 ###############################################################
 
 namespace eval GmThematic {
     variable array opt # thematic options
+	variable array tlegend # mon
+	variable array tlegcan # mon
+
     variable count 1
 }
 
@@ -13,6 +16,8 @@ proc GmThematic::create { tree parent } {
     variable opt
     variable count
     global gmpath
+    global mon
+    global frm
 
     set node "thematic:$count"
 
@@ -43,7 +48,7 @@ proc GmThematic::create { tree parent } {
     set opt($count,where) "" 
     set opt($count,layer) 1 
     set opt($count,icon) "basic/circle" 
-    set opt($count,iconsize) 5 
+    set opt($count,ptsize) 5 
     set opt($count,maxsize) 20 
     set opt($count,nint) 4 
     set opt($count,colorscheme) "blue-red" 
@@ -51,12 +56,15 @@ proc GmThematic::create { tree parent } {
     set opt($count,linecolor) \#000000 
     set opt($count,startcolor) \#FF0000 
     set opt($count,endcolor) \#0000FF 
-    set opt($count,legmon) "x1" 
-    set opt($count,thmlegend) 1 
     set opt($count,update_rgb) 0 
     set opt($count,math) 0 
     set opt($count,psmap) "" 
     set opt($count,border) 1 
+    set opt($count,titlefont) "{times} 14 bold" 
+    set opt($count,subtitlefont) "{times} 12 bold" 
+    set opt($count,labelfont) "{times} 12" 
+    set opt($count,tfontcolor) \#000000  
+    set opt($count,lfontcolor) \#000000  
     
     incr count
     return $node
@@ -77,6 +85,29 @@ proc GmThematic::select_map { id } {
         set GmThematic::opt($id,map) $m
         GmTree::autonamel "thematic map for $m"
     }
+}
+
+proc GmThematic::select_tfont { id } {
+	variable opt
+    global frm 
+    
+    set fon [SelectFont $frm.font -type dialog -sampletext 1 -title "Select font"]
+	if { $fon != "" } {set opt($id,titlefont) $fon}
+}
+
+proc GmThematic::select_stfont { id } {
+	variable opt
+    global frm 
+    
+    set fon [SelectFont $frm.font -type dialog -sampletext 1 -title "Select font"]
+	if { $fon != "" } {set opt($id,subtitlefont) $fon}
+}
+proc GmThematic::select_lfont { id } {
+	variable opt
+    global frm 
+    
+    set fon [SelectFont $frm.font -type dialog -sampletext 1 -title "Select font"]
+	if { $fon != "" } {set opt($id,labelfont) $fon}
 }
 
 proc GmThematic::show_columns { id } {
@@ -217,7 +248,7 @@ proc GmThematic::options { id frm } {
     # point options2
     set row [ frame $frm.pts2 ]  
     Label $row.a -text "    size/min size (graduated pts/lines)" 
-    SpinBox $row.b -range {1 50 1} -textvariable GmThematic::opt($id,iconsize) \
+    SpinBox $row.b -range {1 50 1} -textvariable GmThematic::opt($id,ptsize) \
         -width 2 -helptext "icon size/min size (graduated pts/lines)" -entrybg white 
     Label $row.c -text "max size (graduated pts)" 
     SpinBox $row.d -range {1 50 1} -textvariable GmThematic::opt($id,maxsize) \
@@ -227,10 +258,11 @@ proc GmThematic::options { id frm } {
 
     # color options1
     set row [ frame $frm.color1 ]
-    Label $row.a -text [G_msg "Graduate colors: preset color schemes"] 
+    Label $row.a -text [G_msg "Graduated colors: preset color schemes"] 
     ComboBox $row.b -padx 2 -width 18 -textvariable GmThematic::opt($id,colorscheme) \
         -values {"blue-red" "red-blue" "green-red" "red-green" \
-        "blue-green" "custom_gradient" "single_color" } -entrybg white
+        "blue-green" "green-blue" "cyan-yellow" "yellow-cyan" "custom_gradient" \
+        "single_color" } -entrybg white
     pack $row.a $row.b -side left
     pack $row -side top -fill both -expand yes
 
@@ -252,21 +284,48 @@ proc GmThematic::options { id frm } {
     pack $row.a $row.b -side left
     pack $row -side top -fill both -expand yes
 
-    # legend1
+    # legend 1
     set row [ frame $frm.legend1 ]
-    Label $row.a -text "Legend: "
-    checkbutton $row.b -text [G_msg "create graphic legend"] -variable \
-        GmThematic::opt($id,thmlegend) 
-    checkbutton $row.c -text [G_msg "use math notation in legend"] -variable \
-        GmThematic::opt($id,math) 
+    Label $row.a -text [G_msg "Legend: title font"] 
+	Button $row.b -text [G_msg "font"] \
+	    -command "GmThematic::select_tfont $id"
+    Entry $row.c -width 15 -text "$opt($id,titlefont)" \
+	    -textvariable GmThematic::opt($id,titlefont) \
+	    -background white 
+    Label $row.d -text " font color"
+    SelectColor $row.e -type menubutton -variable GmThematic::opt($id,tfontcolor)
+    pack $row.a $row.b $row.c $row.d $row.e -side left
+    pack $row -side top -fill both -expand yes
+    
+    # legend 2
+    set row [ frame $frm.legend2 ]
+    Label $row.a -text [G_msg "    subtitle font   "] 
+	Button $row.b -text [G_msg "font"] \
+	    -command "GmThematic::select_stfont $id"
+    Entry $row.c -width 15 -text "$opt($id,subtitlefont)" \
+	    -textvariable GmThematic::opt($id,subtitlefont) \
+	    -background white 
     pack $row.a $row.b $row.c -side left
     pack $row -side top -fill both -expand yes
-
-    # legend2
-    set row [ frame $frm.legend2 ]
-    Label $row.a -text [G_msg "    select monitor for legend"] 
-    ComboBox $row.b -padx 2 -width 3 -textvariable GmThematic::opt($id,legmon) \
-        -values {"x0" "x1" "x2" "x3" "x4" "x5" "x6"} -entrybg white
+    
+    # legend 3
+    set row [ frame $frm.legend3 ]
+    Label $row.a -text [G_msg "    label font       "] 
+	Button $row.b -text [G_msg "font"] \
+	    -command "GmThematic::select_lfont $id"
+    Entry $row.c -width 15 -text "$opt($id,labelfont)" \
+	    -textvariable GmThematic::opt($id,labelfont) \
+	    -background white 
+    Label $row.d -text " font color"
+    SelectColor $row.e -type menubutton -variable GmThematic::opt($id,lfontcolor)
+    pack $row.a $row.b $row.c $row.d $row.e -side left
+    pack $row -side top -fill both -expand yes
+    
+	# legend 4
+    set row [ frame $frm.legend4 ]
+    Label $row.a -text "   "
+    checkbutton $row.b -text [G_msg "use math notation in legend"] -variable \
+        GmThematic::opt($id,math) 
     pack $row.a $row.b -side left
     pack $row -side top -fill both -expand yes
 
@@ -277,7 +336,6 @@ proc GmThematic::options { id frm } {
             -entrybg white
     pack $row.a $row.b -side left
     pack $row -side top -fill both -expand yes
-
 }
 
 proc GmThematic::save { tree depth node } {
@@ -286,8 +344,9 @@ proc GmThematic::save { tree depth node } {
     set id [GmTree::node_id $node]
 
     foreach key { _check map type column themetype themecalc breakpoints where \
-             layer icon iconsize maxsize nint colorscheme pointcolor linecolor\
-             startcolor endcolor border legmon thmlegend update_rgb math psmap} {
+             layer icon ptsize maxsize nint colorscheme pointcolor linecolor\
+             startcolor endcolor border legmon thmlegend update_rgb math psmap\
+             titlefont tfontcolor subtitlefont labelfont lfontcolor} {
         GmTree::rc_write $depth "$key $opt($id,$key)"
     } 
 }
@@ -317,13 +376,16 @@ proc GmThematic::display { node } {
     set linecolor [Gm::color $opt($id,linecolor)]
     set startcolor [Gm::color $opt($id,startcolor)]
     set endcolor [Gm::color $opt($id,endcolor)]
+    
+    # turn off x11 display
+    set monitor "none"
 
     #create d.vect.thematic command
-    set cmd "d.vect.thematic map=$opt($id,map) type=$opt($id,type) column=$opt($id,column) \
-			layer=$opt($id,layer) icon=$opt($id,icon) size=$opt($id,iconsize) \
+    set cmd "d.vect.thematic -s map=$opt($id,map) type=$opt($id,type) column=$opt($id,column) \
+			layer=$opt($id,layer) icon=$opt($id,icon) size=$opt($id,ptsize) \
             maxsize=$opt($id,maxsize) nint=$opt($id,nint) pointcolor=$pointcolor \
 			linecolor=$linecolor startcolor=$startcolor endcolor=$endcolor \
-			monitor=$opt($id,legmon) themetype=$opt($id,themetype) \
+			themetype=$opt($id,themetype) monitor=$monitor \
 			themecalc=$opt($id,themecalc) colorscheme=$opt($id,colorscheme)"
              
     # breakpoints
@@ -351,18 +413,15 @@ proc GmThematic::display { node } {
         append cmd " -u"
     }
 
-    # legend
-    if { $opt($id,thmlegend) == 1 } { 
-        append cmd " -l"
-    }
-
     # math notation
     if { $opt($id,math) == 1 } { 
         append cmd " -m"
     }
     
-        run_panel $cmd
 
+	run_panel $cmd
+	GmThematic::tlegend $mon
+	GmThematic::tleg_item $mon $id
 }
 
 
@@ -407,7 +466,7 @@ proc GmThematic::duplicate { tree parent node id } {
     set opt($count,where) "$opt($id,where)" 
     set opt($count,layer) "$opt($id,layer)"
     set opt($count,icon) "$opt($id,icon)" 
-    set opt($count,iconsize) "$opt($id,iconsize)"
+    set opt($count,ptsize) "$opt($id,ptsize)"
     set opt($count,maxsize) "$opt($id,maxsize)"
     set opt($count,nint) "$opt($id,nint)"
     set opt($count,colorscheme) "$opt($id,colorscheme)"
@@ -421,7 +480,215 @@ proc GmThematic::duplicate { tree parent node id } {
     set opt($count,update_rgb) "$opt($id,update_rgb)" 
     set opt($count,math) "$opt($id,math)" 
     set opt($count,psmap) "$opt($id,psmap)" 
+    set opt($count,titlefont) "$opt($id,titlefont)"
+    set opt($count,subtitlefont) "$opt($id,subtitlefont)"
+    set opt($count,labelfont) "$opt($id,labelfont)"
+    set opt($count,tfontcolor) "$opt($id,tfontcolor)" 
+    set opt($count,lfontcolor) "$opt($id,lfontcolor)" 
 
     incr count
     return $node
+}
+
+###############################################################################
+# create graphic legend in separate display canvas
+proc GmThematic::tlegend { mon } {
+	global legendtitle
+	global bgcolor
+	global dtxt
+	global keycontrol
+	global gmpath
+    global env
+
+	variable tlegend
+	variable tlegcan
+
+	if { [winfo exists .tlegend($mon)] } {return}
+
+	set legendtitle "Legend for Map $mon"
+	toplevel .tlegend($mon)
+    wm title .tlegend($mon) [G_msg $legendtitle]
+
+
+    wm withdraw .tlegend($mon)
+    #wm overrideredirect $txt 1
+
+	# create canvas for legend
+	set tlegmf [MainFrame .tlegend($mon).mf -bg $bgcolor ]
+	set tlegcan($mon) [canvas $tlegmf.can -background #ffffff \
+		-borderwidth 0 -closeenough 1.0 \
+        -relief ridge -selectbackground #c4c4c4 \
+        -width 300 -height 300 ]
+	   
+    # setting geometry
+    place $tlegcan($mon) \
+        -in $tlegmf -x 0 -y 0 -anchor nw \
+        -bordermode ignore 
+
+	# control buttons
+	set tleg_tb [$tlegmf addtoolbar]
+	set tlbb [ButtonBox $tleg_tb.bb -orient horizontal -background $bgcolor]
+	$tlbb add -text "clear" -command "GmThematic::tleg_erase $mon" -bg #dddddd \
+		-highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
+        -helptext [G_msg "Clear legend"] -highlightbackground $bgcolor
+	$tlbb add -text "save" -command "GmThematic::tleg_save $mon"  -bg #dddddd \
+		-highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
+        -helptext [G_msg "Save legend to EPS file"] -highlightbackground $bgcolor
+
+	pack $tlegmf -expand yes -fill both -padx 0 -pady 0
+	pack $tlegcan($mon) -fill both -expand yes
+	pack $tlbb -side left -anchor w
+			
+	BWidget::place .tlegend($mon) 0 0 at 500 100
+    wm deiconify .tlegend($mon)
+
+}
+
+# read legend file and create legend items
+proc GmThematic::tleg_item { mon id } {
+	variable tlegend
+	variable tlegcan
+	variable opt
+	
+	GmThematic::tleg_erase $mon 
+	set ltxt [open "gismlegend.txt" r]
+	set x1 30
+	set y1 40
+	set txtx 60
+	set font $opt($id,labelfont)
+	regexp {.*\s(\d*)} $font string lineht
+	set yinc [expr $lineht * 2]	
+	
+	set titlefont $opt($id,titlefont)
+	set tfontcolor $opt($id,tfontcolor)
+	set subtitlefont $opt($id,subtitlefont)
+
+	set labelfont $opt($id,labelfont)
+	set lfontcolor $opt($id,lfontcolor)
+	while {![eof $ltxt]} {
+		gets $ltxt line
+		set type [lindex $line 0]
+		set fcolor [lindex $line 1]
+		set lcolor [lindex $line 2]
+		set size [lindex $line 3]
+		set label [lindex $line 4]
+		if { $fcolor != "-" } { set xfcolor [GmThematic::rgb2hex $fcolor] }
+		if { $lcolor != "-" } { set xlcolor [GmThematic::rgb2hex $lcolor] }
+		switch $type {
+			title {
+				regexp {.*\s(\d*)\s.*} $titlefont string lineht
+				set yinc [expr $lineht * 2]	
+				set x2 [expr $x1 + 15]
+				set y2 [expr $y1 + 15]
+				$tlegcan($mon) create text $x1 $y2 -anchor sw -width 250 \
+					-fill $tfontcolor -font $titlefont -text "$label"
+			}
+			subtitle {
+				regexp {.*\s(\d*)\s.*} $subtitlefont string lineht
+				set yinc [expr $lineht * 2]	
+				set x2 [expr $x1 + 15]
+				set y2 [expr $y1 + 15]
+				$tlegcan($mon) create text $x1 $y2 -anchor sw -width 250 \
+					-fill $tfontcolor -font $subtitlefont -text "$label"
+				incr y2 10
+				$tlegcan($mon) create line $x1 $y2 [expr $x1 + 250] $y2 \
+					-width 1 -fill #000000				
+				incr y1 10
+			}
+			text {
+				regexp {.*\s(\d*)\s.*} $labelfont string lineht
+				set yinc [expr $lineht * 2]	
+				set x2 [expr $x1 + 15]
+				set y2 [expr $y1 + 15]
+				$tlegcan($mon) create text $x1 $y2 -anchor sw -width 250 \
+					-fill $lfontcolor -font $labelfont -text "$label"
+			}
+			area {
+				regexp {.*\s(\d*)\s.*} $labelfont string lineht
+				set yinc [expr $lineht * 2]	
+				set x2 [expr $x1 + 15]
+				set y2 [expr $y1 + 15]
+				$tlegcan($mon) create rectangle $x1 $y1 $x2 $y2 -fill $xfcolor \
+					-outline $xlcolor
+				$tlegcan($mon) create text [expr $x2 + 15] [expr (($y2-$y1)/2) + $y1] \
+				-fill $lfontcolor -anchor w -font $labelfont -text "$label"
+			}
+			point {
+				regexp {.*\s(\d*)\s.*} $labelfont string lineht
+				set yinc [expr $lineht * 2]	
+				if { $size > [expr $yinc + 2] } {
+					incr y1 [expr int(($size/5) + 2)]
+				}
+				if { $txtx <= [expr $x1 + $size + 15] } {
+					set txtx [expr $x1 + $size + 15]
+				}
+				set x2 [expr $x1 + $size]
+				set y2 [expr $y1 + $size]
+				$tlegcan($mon) create oval $x1 $y1 $x2 $y2 -fill $xfcolor \
+					-outline $xlcolor
+				$tlegcan($mon) create text $txtx [expr (($y2-$y1)/2) + $y1] \
+				-fill $lfontcolor -anchor w -font $labelfont -text "$label"
+			}
+			line {
+				regexp {.*\s(\d*)\s.*} $labelfont string lineht
+				set yinc [expr $lineht * 2]	
+				set x2 [expr $x1 + 15]
+				set y2 [expr $y1 + 15]
+				$tlegcan($mon) create line $x1 $y1 $x2 $y2 -width $size  \
+					-fill $xlcolor
+				$tlegcan($mon) create text [expr $x2 + 15] [expr (($y2-$y1)/2) + $y1] \
+				-fill $lfontcolor -anchor w -font $labelfont -text "$label"
+			}
+			default { break }
+		}
+		if { $size > $yinc } {
+			incr y1 [expr int($size + 2)]
+		} else {
+			incr y1 $yinc
+		}
+	}
+	
+
+	return
+}
+
+# rgb to hex color convertor
+proc GmThematic::rgb2hex { clr } {
+	set rgb [split $clr :]
+	set r [lindex $rgb 0]
+	set g [lindex $rgb 1]
+	set b [lindex $rgb 2]
+	if {$r == "" || $g == "" || $b == ""} {return}
+	set xclr [format "#%02x%02x%02x" $r $g $b]
+	return $xclr
+}
+
+# erase legend canvas
+proc GmThematic::tleg_erase { mon } {
+	variable tlegcan
+	
+	$tlegcan($mon) delete all
+	return
+}
+
+#save legend canvas (might use maptool procedures)
+proc GmThematic::tleg_save { mon } {
+	global env
+	variable tlegcan
+		
+	set types {
+    {{EPS} {.eps}}
+	}
+
+	if { [info exists HOME] } {
+		set dir $env(HOME)
+		set path [tk_getSaveFile -filetypes $types -initialdir $dir \
+			-defaultextension ".eps"]
+	} else {
+		set path [tk_getSaveFile -filetypes $types -defaultextension ".eps"]
+	}
+	
+	$tlegcan($mon) postscript -file "$path"
+
+	return
 }
