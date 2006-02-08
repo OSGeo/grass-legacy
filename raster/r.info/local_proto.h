35ead48df86aedc68fba655c35f635581695b056
Markus Neteler <neteler@gmail.com>
2006-02-08 22:40:21 +0000

    Soeren Gebbert: timestamp print flag added
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@18928 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.info/local_proto.h
+++ raster/r.info/local_proto.h
@@ -1,5 +1,3 @@
-/* main.c */
-int main(int, char *[]);
 /* reclas_txt.c */
 int reclass_text(char *, CELL, struct Reclass *, int);
 int do_text(char *, long, long);
--- raster/r.info/main.c
+++ raster/r.info/main.c
@@ -1,3 +1,18 @@
+/***************************************************************************
+*
+* MODULE:       r.info
+*
+* AUTHOR(S):    Michael O'Shea
+*
+* PURPOSE:      Outputs basic information about a user-specified raster map layer.
+*
+* COPYRIGHT:    (C) 2005 by the GRASS Development Team
+*
+*               This program is free software under the GNU General Public
+*               License (>=v2). Read the file COPYING that comes with GRASS
+*               for details.
+*
+*****************************************************************************/
 #include <stdlib.h>
 #include <string.h>
 #include "gis.h"
@@ -11,23 +26,27 @@
         fprintf(out,"-");\
     fprintf (out,"%c\n",x)
 
-
+/*local prototype */
+int format_double(double value, char *buf);
 static char *name;
 
-int 
-main (int argc, char *argv[])
+/**************************************************************************/
+int
+main(int argc, char *argv[])
 {
     char *mapset;
-    char line[200];
+    char *line = NULL;
     char tmp1[100], tmp2[100], tmp3[100];
     int i;
     CELL mincat, maxcat, cat;
-    double zmin, zmax; /* min and max data values */
+    double zmin, zmax;		/* min and max data values */
     FILE *out, *fopen();
     struct FPRange range;
     struct Cell_head cellhd;
     struct Categories cats;
     struct History hist;
+    struct TimeStamp ts;
+    int time_ok = 0;
     int head_ok;
     int cats_ok;
     int hist_ok;
@@ -35,275 +54,366 @@ main (int argc, char *argv[])
     RASTER_MAP_TYPE data_type;
     struct Reclass reclass;
     char *G_program_name();
-	struct GModule *module;
+    struct GModule *module;
     struct Option *opt1;
     struct Flag *rflag;
     struct Flag *sflag;
     struct Flag *tflag;
+    struct Flag *timestampflag;
     struct Flag *gflag;
     struct Flag *hflag;
 
     G_gisinit(argv[0]);
 
-	module = G_define_module();
-	module->description =
-		_("Outputs basic information about a " 
-		"user-specified raster map layer.");
+    module = G_define_module();
+    module->description =
+	_("Outputs basic information about a user-specified raster map layer.");
 
-    opt1 = G_define_option() ;
-    opt1->key        = "map" ;
-    opt1->type       = TYPE_STRING ;
-    opt1->required   = YES ;
-    opt1->gisprompt  = "old,cell,raster" ;
-    opt1->description= _("Name of existing raster map") ;
+    opt1 = G_define_standard_option(G_OPT_R_MAP);
 
     rflag = G_define_flag();
-    rflag->key            = 'r';
-    rflag->description    = _("Print range only");
+    rflag->key = 'r';
+    rflag->description = _("Print range only");
 
     sflag = G_define_flag();
-    sflag->key            = 's';
-    sflag->description    = _("Print resolution (NS-res, EW-res) only");
+    sflag->key = 's';
+    sflag->description = _("Print resolution (NS-res, EW-res) only");
 
     tflag = G_define_flag();
-    tflag->key            = 't';
-    tflag->description    = _("Print raster map type only");
+    tflag->key = 't';
+    tflag->description = _("Print raster map type only");
 
     gflag = G_define_flag();
-    gflag->key            = 'g';
-    gflag->description    = _("Print raster map region only");
+    gflag->key = 'g';
+    gflag->description = _("Print raster map region only");
 
-    hflag = G_define_flag ();
-    hflag->key             = 'h';
-    hflag->description     = _("Print raster history instead of info");
+    hflag = G_define_flag();
+    hflag->key = 'h';
+    hflag->description = _("Print raster history instead of info");
+
+    timestampflag = G_define_flag();
+    timestampflag->key = 'p';
+    timestampflag->description =
+	_("Print additionally map timestamp, (day.month.year hour:minute:seconds)");
 
     if (G_parser(argc, argv))
-        exit(EXIT_FAILURE);
-    
-    name = G_store(opt1->answer);
-    if ((mapset = G_find_cell2 (name, "")) == NULL)
-        G_fatal_error ( _("Cannot find %s"), name);	
+	exit(EXIT_FAILURE);
 
-    head_ok = G_get_cellhd (name, mapset, &cellhd) >= 0;
-    cats_ok = G_read_cats (name, mapset, &cats) >= 0;
-    hist_ok = G_read_history (name, mapset, &hist) >= 0;
-    is_reclass = G_get_reclass (name, mapset, &reclass);
+    name = G_store(opt1->answer);
+    if ((mapset = G_find_cell2(name, "")) == NULL)
+	G_fatal_error(_("Cannot find %s"), name);
+
+    head_ok = G_get_cellhd(name, mapset, &cellhd) >= 0;
+    cats_ok = G_read_cats(name, mapset, &cats) >= 0;
+    hist_ok = G_read_history(name, mapset, &hist) >= 0;
+    is_reclass = G_get_reclass(name, mapset, &reclass);
     data_type = G_raster_map_type(name, mapset);
 
-    if (G_read_fp_range (name, mapset, &range) < 0)
-        G_fatal_error (_("could not read range file"));
-    G_get_fp_range_min_max(&range,&zmin,&zmax);
+    /*Check the Timestamp */
+    if (timestampflag->answer)
+	time_ok = G_read_raster_timestamp(name, mapset, &ts) >= 0;
+
+    if (G_read_fp_range(name, mapset, &range) < 0)
+	G_fatal_error(_("could not read range file"));
+    G_get_fp_range_min_max(&range, &zmin, &zmax);
 
     out = stdout;
 
-  if (!rflag->answer && !sflag->answer && !tflag->answer && !gflag->answer && !hflag->answer)
-  {
-    divider ('+');
+    if (!rflag->answer && !sflag->answer && !tflag->answer && !gflag->answer &&
+	!hflag->answer) {
+	divider('+');
 
-    sprintf (line, "Layer:    %-29.29s  Date: %s", name, hist_ok ? hist.mapid : "??");
-    printline (line);
+	if (G_asprintf
+	    (&line, "Layer:    %-29.29s  Date: %s", name,
+	     hist_ok ? hist.mapid : "??") > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
 
-    sprintf (line, "Mapset:   %-29.29s  Login of Creator: %s", mapset, hist_ok ? hist.creator : "??");
-    printline (line);
+	if (G_asprintf
+	    (&line, "Mapset:   %-29.29s  Login of Creator: %s", mapset,
+	     hist_ok ? hist.creator : "??") > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
 
-    sprintf (line, "Location: %s", G_location());
-    printline (line);
+	if (G_asprintf(&line, "Location: %s", G_location()) > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
 
-    sprintf (line, "DataBase: %s", G_gisdbase());
-    printline (line);
+	if (G_asprintf(&line, "DataBase: %s", G_gisdbase()) > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
 
-    sprintf (line, "Title:    %s ( %s )", cats_ok ? cats.title : "??", hist_ok ? hist.title : "??");
-    printline (line);
+	if (G_asprintf
+	    (&line, "Title:    %s ( %s )", cats_ok ? cats.title : "??",
+	     hist_ok ? hist.title : "??") > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
 
-    divider ('|');
-    printline ("");
+	/*This shows the TimeStamp */
+	if (time_ok && timestampflag->answer) {
+	    if (G_asprintf
+		(&line,
+		 "Timestamp: %i.%i.%i %02i:%02i:%02g to %i.%i.%i %02i:%02i:%02g",
+		 ts.dt[0].day, ts.dt[0].month, ts.dt[0].year, ts.dt[0].hour,
+		 ts.dt[0].minute, ts.dt[0].second, ts.dt[1].day, ts.dt[1].month,
+		 ts.dt[1].year, ts.dt[1].hour, ts.dt[1].minute,
+		 ts.dt[1].second) > 0) {
 
-    sprintf (line, "  Type of Map:  %-20.20s", hist_ok ? hist.maptype : "??");
-    strcat (line, "Number of Categories: ");
-    if (cats_ok)
-    {
-        char temp[20];
-        sprintf (temp, "%-9ld", (long)cats.num);
-        strcat (line, temp);
-    }
-    else
-        strcat (line, "??");
-    printline (line);
-
-    sprintf (line, "  Data Type:    %s",
-			(data_type ==  CELL_TYPE ?  "CELL" :
-			(data_type == DCELL_TYPE ? "DCELL" :
-			(data_type == FCELL_TYPE ? "FCELL" : "??"))));
-    printline (line);
-
-    if (head_ok)
-    {
-        sprintf (line, "  Rows:         %d", cellhd.rows);
-	printline (line);
-
-        sprintf (line, "  Columns:      %d", cellhd.cols);
-	printline (line);
-
-        sprintf (line, "  Total Cells:  %ld", (long)cellhd.rows * cellhd.cols);
-	printline (line);
-
-	sprintf (line, "       Projection: %s (zone %d)",
-	    G_database_projection_name(), G_zone());
-        printline (line);
-
-	G_format_northing (cellhd.north, tmp1, cellhd.proj);
-	G_format_northing (cellhd.south, tmp2, cellhd.proj);
-	G_format_resolution (cellhd.ns_res, tmp3, cellhd.proj);
-        sprintf (line, "           N: %10s    S: %10s   Res: %5s",
-	    tmp1, tmp2, tmp3);
-        printline (line);
-
-	G_format_easting (cellhd.east, tmp1, cellhd.proj);
-	G_format_easting (cellhd.west, tmp2, cellhd.proj);
-	G_format_resolution (cellhd.ew_res, tmp3, cellhd.proj);
-        sprintf (line, "           E: %10s    W: %10s   Res: %5s",
-	    tmp1, tmp2, tmp3);
-        printline (line);
-
-        if (data_type ==  CELL_TYPE)
-	  sprintf(line, "  Range of data:    min =  %i max = %i", (CELL)zmin, (CELL)zmax);
-        else
-	  sprintf(line, "  Range of data:    min =  %f max = %f", zmin, zmax);
-	printline (line);
-    }
-
-    printline ("");
-
-    if (hist_ok)
-    {
-        printline ("  Data Source:");
-        sprintf (line, "   %s", hist.datsrc_1);
-	printline(line);
-        sprintf (line, "   %s", hist.datsrc_2);
-	printline(line);
-	printline("");
-
-        printline ("  Data Description:");
-        sprintf (line, "   %s", hist.keywrd);
-	printline(line);
-	printline("");
-        if(hist.edlinecnt)
-	{
-	    printline ("  Comments:  ");
-
-	    for (i = 0; i < hist.edlinecnt; i++)
-	    /**************************************/
-	    {
-		sprintf (line, "   %s", hist.edhist[i]);
 		printline(line);
 	    }
-        }
-
-        printline ("");
-    }
-
-    if (is_reclass > 0)
-    {
-	int first = 1;
-
-        divider ('|');
-        sprintf (line, "  Reclassification of [%s] in mapset [%s]",
-            reclass.name, reclass.mapset);
-        printline (line);
-        printline ("");
-        printline ("        Category        Original categories");
-        printline ("");
-
-	for (i = 0; i < reclass.num; i++)
-	{
-	    CELL x = reclass.table[i];
-	    if (G_is_c_null_value(&x))
-		continue;
-	    if (first || x < mincat)
-		mincat = x;
-	    if (first || x > maxcat)
-		maxcat = x;
-	    first = 0;
+	    else {
+		G_fatal_error(_("Cannot allocate memory for string"));
+	    }
 	}
 
-	if (!first)
-        for (cat = mincat; cat <= maxcat; cat++)
-        {
-            char text[80];
-            char num [10];
-            int next;
+	divider('|');
+	printline("");
 
-	    if (cat == 0) continue;
-            sprintf (num, "%5ld", (long)cat);
-            next = 0;
-            do
-            {
-                next = reclass_text (text, cat, &reclass, next);
-                sprintf (line, "     %5s              %s", num, text);
-                printline (line);
-                *num = 0;
-            }
-            while (next >= 0);
-        }
-    }
-    divider ('+');
-
-    fprintf(out,"\n");
-   }
-   else /* rflag or sflag or tflag or gflag or hflag */
-   {
-
-     if (rflag->answer){
-        if (data_type ==  CELL_TYPE)
-        {
-	  fprintf(out, "min=%i\n", (CELL)zmin);
-	  fprintf(out, "max=%i\n", (CELL)zmax);
+	if (cats_ok) {
+	    format_double((double)cats.num, tmp1);
 	}
-        else
-        {
-	  fprintf(out, "min=%f\n", zmin);
-	  fprintf(out, "max=%f\n", zmax);
-	}
-     }
-     else if (gflag->answer){
-	G_format_northing (cellhd.north, tmp1, cellhd.proj);
-	G_format_northing (cellhd.south, tmp2, cellhd.proj);
-        fprintf(out, "north=%s\n", tmp1);
-        fprintf(out, "south=%s\n", tmp2);
-            
-	G_format_easting (cellhd.east, tmp1, cellhd.proj);
-	G_format_easting (cellhd.west, tmp2, cellhd.proj);
-        fprintf(out, "east=%s\n", tmp1);
-        fprintf(out, "west=%s\n", tmp2);
-     }
-     else if (sflag->answer){
-	G_format_resolution (cellhd.ns_res, tmp3, cellhd.proj);
-        fprintf (out, "nsres=%s\n", tmp3);
 
-	G_format_resolution (cellhd.ew_res, tmp3, cellhd.proj);
-        fprintf (out, "ewres=%s\n", tmp3);
-     }
-     else if (tflag->answer){
-         fprintf (out, "datatype=%s\n",
-			(data_type ==  CELL_TYPE ?  "CELL" :
+	if (G_asprintf
+	    (&line, "  Type of Map:  %-20.20s Number of Categories: %-9s",
+	     hist_ok ? hist.maptype : "??", cats_ok ? tmp1 : "??") > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
+
+	if (G_asprintf(&line, "  Data Type:    %s",
+		       (data_type == CELL_TYPE ? "CELL" :
 			(data_type == DCELL_TYPE ? "DCELL" :
-			(data_type == FCELL_TYPE ? "FCELL" : "??"))));
-     }
-     else if (hflag->answer){
-	     if (hist_ok) {
-		fprintf (out, "Data Source:\n");
-		fprintf (out, "   %s\n", hist.datsrc_1);
-		fprintf (out, "   %s\n", hist.datsrc_2);
-		fprintf (out, "Data Description:\n");
-		fprintf (out, "   %s\n", hist.keywrd);
-		if(hist.edlinecnt) {
-			fprintf (out, "Comments:\n");
-			for (i = 0; i < hist.edlinecnt; i++)
-				fprintf (out, "   %s\n", hist.edhist[i]);
+			 (data_type == FCELL_TYPE ? "FCELL" : "??")))) > 0)
+	    printline(line);
+	else
+	    G_fatal_error(_("Cannot allocate memory for string"));
+
+	if (head_ok) {
+	    if (G_asprintf(&line, "  Rows:         %d", cellhd.rows) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    if (G_asprintf(&line, "  Columns:      %d", cellhd.cols) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    if (G_asprintf
+		(&line, "  Total Cells:  %ld",
+		 (long)cellhd.rows * cellhd.cols) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    if (G_asprintf
+		(&line, "       Projection: %s (zone %d)",
+		 G_database_projection_name(), G_zone()) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    G_format_northing(cellhd.north, tmp1, cellhd.proj);
+	    G_format_northing(cellhd.south, tmp2, cellhd.proj);
+	    G_format_resolution(cellhd.ns_res, tmp3, cellhd.proj);
+	    if (G_asprintf
+		(&line, "           N: %10s    S: %10s   Res: %5s", tmp1, tmp2,
+		 tmp3) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    G_format_easting(cellhd.east, tmp1, cellhd.proj);
+	    G_format_easting(cellhd.west, tmp2, cellhd.proj);
+	    G_format_resolution(cellhd.ew_res, tmp3, cellhd.proj);
+	    if (G_asprintf(&line, "           E: %10s    W: %10s   Res: %5s",
+			   tmp1, tmp2, tmp3) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    if (data_type == CELL_TYPE) {
+		if (G_asprintf
+		    (&line, "  Range of data:    min =  %i max = %i",
+		     (CELL) zmin, (CELL) zmax) > 0)
+		    printline(line);
+		else
+		    G_fatal_error(_("Cannot allocate memory for string"));
+
+	    }
+	    else {
+		if (G_asprintf
+		    (&line, "  Range of data:    min =  %f max = %f", zmin,
+		     zmax) > 0)
+		    printline(line);
+		else
+		    G_fatal_error(_("Cannot allocate memory for string"));
+
+	    }
+	}
+
+	printline("");
+
+	if (hist_ok) {
+	    printline("  Data Source:");
+	    if (G_asprintf(&line, "   %s", hist.datsrc_1) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    if (G_asprintf(&line, "   %s", hist.datsrc_2) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    printline("");
+
+	    printline("  Data Description:");
+	    if (G_asprintf(&line, "   %s", hist.keywrd) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    printline("");
+	    if (hist.edlinecnt) {
+		printline("  Comments:  ");
+
+		for (i = 0; i < hist.edlinecnt; i++)
+
+	    /**************************************/
+		{
+		    if (G_asprintf(&line, "   %s", hist.edhist[i]) > 0)
+			printline(line);
+		    else
+			G_fatal_error(_("Cannot allocate memory for string"));
+
 		}
-	     }
-     }
-   } /* else rflag or sflag or tflag or gflag or hflag */
-   
-   return EXIT_SUCCESS;
+	    }
+
+	    printline("");
+	}
+
+	if (is_reclass > 0) {
+	    int first = 1;
+
+	    divider('|');
+	    if (G_asprintf(&line, "  Reclassification of [%s] in mapset [%s]",
+			   reclass.name, reclass.mapset) > 0)
+		printline(line);
+	    else
+		G_fatal_error(_("Cannot allocate memory for string"));
+
+	    printline("");
+	    printline("        Category        Original categories");
+	    printline("");
+
+	    for (i = 0; i < reclass.num; i++) {
+		CELL x = reclass.table[i];
+		if (G_is_c_null_value(&x))
+		    continue;
+		if (first || x < mincat)
+		    mincat = x;
+		if (first || x > maxcat)
+		    maxcat = x;
+		first = 0;
+	    }
+
+	    if (!first)
+		for (cat = mincat; cat <= maxcat; cat++) {
+		    char text[80];
+		    char *num;
+		    int next;
+
+		    if (cat == 0)
+			continue;
+		    if (G_asprintf(&num, "%5ld", (long)cat) < 1) ;
+		    G_fatal_error(_("Cannot allocate memory for string"));
+
+		    next = 0;
+		    do {
+			next = reclass_text(text, cat, &reclass, next);
+			if (G_asprintf
+			    (&line, "     %5s              %s", num, text) > 0)
+			    printline(line);
+			else
+			    G_fatal_error(_
+					  ("Cannot allocate memory for string"));
+
+			*num = 0;
+		    }
+		    while (next >= 0);
+		}
+	}
+	divider('+');
+
+	fprintf(out, "\n");
+    }
+    else {			/* rflag or sflag or tflag or gflag or hflag */
+
+
+	if (rflag->answer) {
+	    if (data_type == CELL_TYPE) {
+		fprintf(out, "min=%i\n", (CELL) zmin);
+		fprintf(out, "max=%i\n", (CELL) zmax);
+	    }
+	    else {
+		fprintf(out, "min=%f\n", zmin);
+		fprintf(out, "max=%f\n", zmax);
+	    }
+	}
+	else if (gflag->answer) {
+	    G_format_northing(cellhd.north, tmp1, cellhd.proj);
+	    G_format_northing(cellhd.south, tmp2, cellhd.proj);
+	    fprintf(out, "north=%s\n", tmp1);
+	    fprintf(out, "south=%s\n", tmp2);
+
+	    G_format_easting(cellhd.east, tmp1, cellhd.proj);
+	    G_format_easting(cellhd.west, tmp2, cellhd.proj);
+	    fprintf(out, "east=%s\n", tmp1);
+	    fprintf(out, "west=%s\n", tmp2);
+	}
+	else if (sflag->answer) {
+	    G_format_resolution(cellhd.ns_res, tmp3, cellhd.proj);
+	    fprintf(out, "nsres=%s\n", tmp3);
+
+	    G_format_resolution(cellhd.ew_res, tmp3, cellhd.proj);
+	    fprintf(out, "ewres=%s\n", tmp3);
+	}
+	else if (tflag->answer) {
+	    fprintf(out, "datatype=%s\n",
+		    (data_type == CELL_TYPE ? "CELL" :
+		     (data_type == DCELL_TYPE ? "DCELL" :
+		      (data_type == FCELL_TYPE ? "FCELL" : "??"))));
+	}
+	else if (hflag->answer) {
+	    if (hist_ok) {
+		fprintf(out, "Data Source:\n");
+		fprintf(out, "   %s\n", hist.datsrc_1);
+		fprintf(out, "   %s\n", hist.datsrc_2);
+		fprintf(out, "Data Description:\n");
+		fprintf(out, "   %s\n", hist.keywrd);
+		if (hist.edlinecnt) {
+		    fprintf(out, "Comments:\n");
+		    for (i = 0; i < hist.edlinecnt; i++)
+			fprintf(out, "   %s\n", hist.edhist[i]);
+		}
+	    }
+	}
+    }				/* else rflag or sflag or tflag or gflag or hflag */
+
+    return EXIT_SUCCESS;
+}
+
+/**************************************************************************/
+int format_double(double value, char *buf)
+{
+
+    sprintf(buf, "%.8lf", value);
+    G_trim_decimal(buf);
+    return 0;
 }
