012575e1a6dc5cbf7cadee9c76b1efb39857cead
Huidae Cho <grass4u@gmail.com>
2006-02-15 23:52:57 +0000

    rm dependency removed
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19017 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/remove.c
+++ lib/gis/remove.c
@@ -16,8 +16,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
 #include <grass/gis.h>
 
+static int _remove(const char *path);
 
 /*!
  * \brief remove a database file
@@ -36,10 +40,9 @@
 
 int G_remove ( char *element, char *name)
 {
-    char path[1040];
+    char path[1024];
     char *mapset;
     char xname[512], xmapset[512];
-    char cmd[1100];
 
     if (G_legal_filename(name) < 0)
 	    return -1;
@@ -54,16 +57,41 @@ int G_remove ( char *element, char *name)
     if (access (G__file_name (path, element, name, mapset),0) != 0)
 	    return 0;
 
-    if ( remove ( path ) == 0)
-	    return 1;
-    if (strchr(path, '\''))
-	    return -1;
-
-    sprintf(cmd, "rm -rf '%s'", path);
-
-    if (G_system(cmd) == 0)
+    if (_remove(path) == 0)
 	    return 1;
 
     return -1;
 }
 
+/* equivalent to rm -rf path */
+static int
+_remove(const char *path)
+{
+	DIR *dirp;
+	struct dirent *dp;
+	struct stat sb;
+	char path2[1024];
+
+	if(lstat(path, &sb))
+		return 1;
+	if(!S_ISDIR(sb.st_mode))
+		return remove(path) == 0 ? 0 : 1;
+
+	if((dirp = opendir(path)) == NULL)
+		return 1;
+	while((dp = readdir(dirp)) != NULL)
+	{
+		if(dp->d_name[0] == '.')
+			continue;
+		sprintf(path2, "%s/%s", path, dp->d_name);
+		if(lstat(path2, &sb))
+			continue;
+		if(S_ISDIR(sb.st_mode))
+			_remove(path2);
+		else
+			remove(path2);
+	}
+	closedir(dirp);
+
+	return rmdir(path) == 0 ? 0 : 1;
+}
