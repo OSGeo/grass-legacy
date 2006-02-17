dc96effcf91e39c7699316f33b3a6407b7f4bda9
Markus Neteler <neteler@gmail.com>
2006-02-17 14:16:16 +0000

    Soeren Gebbert: added option dp to truncate the float output of point coordinates; added localisation; code prettified with indent
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19049 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.out.ascii/b2a.c
+++ vector/v.out.ascii/b2a.c
@@ -2,138 +2,165 @@
 #include <grass/gis.h>
 #include "local_proto.h"
 
-int bin_to_asc(
-    FILE *ascii,
-    FILE *att,
-    struct Map_info *Map,
-    int ver,
-    int format)
+int bin_to_asc(FILE * ascii,
+	       FILE * att, struct Map_info *Map, int ver, int format, int dp)
 {
-	int type, ctype, i, cat;
-	double *xptr, *yptr, *zptr, x, y;
-	static struct line_pnts *Points;
-	char buf1[100], buf2[100];
-        struct line_cats *Cats;
+    int type, ctype, i, cat, proj;
+    double *xptr, *yptr, *zptr, x, y;
+    static struct line_pnts *Points;
+    struct line_cats *Cats;
+    char *xstring = NULL, *ystring = NULL, *zstring = NULL;
 
-	Points = Vect_new_line_struct ();	/* init line_pnts struct */
-        Cats = Vect_new_cats_struct (); 
+    Points = Vect_new_line_struct();	/* init line_pnts struct */
+    Cats = Vect_new_cats_struct();
 
+    proj = Vect_get_proj(Map);
 
-	/* by default, read_next_line will NOT read Dead lines */
-	/*  but we can override that (in Level I only) by specifying */
-	/*  the type  -1, which means match all line types */
+    /* by default, read_next_line will NOT read Dead lines */
+    /* but we can override that (in Level I only) by specifying */
+    /* the type  -1, which means match all line types */
 
-	Vect_rewind ( Map );
+    Vect_rewind(Map);
 
-	while(1)
-	{
-	        if (-1 == (type = Vect_read_next_line (Map, Points, Cats)))
-		    return (-1);
+    while (1) {
+	if (-1 == (type = Vect_read_next_line(Map, Points, Cats)))
+	    return (-1);
 
-		if (type == -2)	/* EOF */
-	            return (0);
+	if (type == -2)		/* EOF */
+	    return (0);
 
-		if ( format == FORMAT_POINT && !(type & GV_POINTS) ) continue;
+	if (format == FORMAT_POINT && !(type & GV_POINTS))
+	    continue;
 
-		if ( ver < 5 ) { Vect_cat_get ( Cats, 1, &cat ); }
-
-		switch(type)
-		{
-		case GV_BOUNDARY:
-		    	if ( ver == 5 )
-			    ctype = 'B';
-			else 
-			    ctype = 'A';
-			break;
-		case GV_CENTROID:
-			if ( ver < 5 ) { 
-			    if ( att != NULL ) {
-				if ( cat > 0 ) {
-				    fprintf(att, "A %f %f %d\n", Points->x[0], Points->y[0], cat);
-				}
-			    }
-			    continue; 
-			}
-			ctype = 'C';			
-			break;			
-		case GV_LINE:
-			ctype = 'L';
-			break;
-		case GV_POINT:
-			ctype = 'P';
-			break;
-		case GV_FACE:
-			ctype = 'F';
-			break;
-		case GV_KERNEL:
-			ctype = 'K';
-			break;
-		default:
-			ctype = 'X';
-			fprintf (stderr, "got type %d\n", (int) type);
-			break;
-		}
-
-		if ( format == FORMAT_POINT ) {
-		    /* fprintf(ascii, "%c", ctype); */
-		    G_format_easting (Points->x[0], buf1, -1);
-		    G_format_northing (Points->y[0], buf2, -1);
-		    if ( Map->head.with_z  && ver == 5 ) {
-			fprintf(ascii, "%s|%s|%f", buf1, buf2, Points->z[0]);
-		    }
-		    else {
-			fprintf(ascii, "%s|%s", buf1, buf2);
-		    }
-		    if ( Cats->n_cats > 0 )		
-	                fprintf(ascii, "|%d", Cats->cat[0]);
-	            fprintf(ascii, "\n");
-		} else { 
-		    /* FORMAT_STANDARD */
-		    if ( ver == 5 && Cats->n_cats > 0 )
-			fprintf(ascii, "%c  %d %d\n", ctype, Points->n_points, Cats->n_cats);		
-		    else 
-			fprintf(ascii, "%c  %d\n", ctype, Points->n_points);
-
-		    xptr = Points->x;
-		    yptr = Points->y;
-		    zptr = Points->z;
-		    
-		    while (Points->n_points--)
-		    {
-			    if ( ver == 4 ) {
-			        G_format_northing (*yptr++, buf1, -1);
-			        G_format_easting (*xptr++, buf2, -1);
-			    } else {
-			        G_format_easting (*xptr++, buf1, -1);
-			        G_format_northing (*yptr++, buf2, -1);
-			    }
-			    
-			    if ( Map->head.with_z  && ver == 5 ) {
-				fprintf(ascii, " %-12s %-12s %-12f\n", buf1, buf2, *zptr++);
-			    }
-			    else {
-				fprintf(ascii, " %-12s %-12s\n", buf1, buf2);
-			    }
-		    }
-
-		    if ( ver == 5 ) {
-			for ( i=0; i< Cats->n_cats; i++ ) {		
-			    fprintf(ascii, " %-5d %-10d\n", Cats->field[i], Cats->cat[i]);
-			}
-		    } else {
-			if ( cat > 0 ) {
-			    if ( type == GV_POINT ) {
-				fprintf(att, "P %f %f %d\n", Points->x[0], Points->y[0], cat);
-			    } else {
-				x = (Points->x[1] + Points->x[0] ) / 2;
-				y = (Points->y[1] + Points->y[0] ) / 2;
-				fprintf(att, "L %f %f %d\n", x, y, cat);
-			    }
-			}
-		    }
-		}
+	if (ver < 5) {
+	    Vect_cat_get(Cats, 1, &cat);
 	}
 
-	/* not reached */
-}
+	switch (type) {
+	case GV_BOUNDARY:
+	    if (ver == 5)
+		ctype = 'B';
+	    else
+		ctype = 'A';
+	    break;
+	case GV_CENTROID:
+	    if (ver < 5) {
+		if (att != NULL) {
+		    if (cat > 0) {
+			G_asprintf(&xstring, "%.*f", dp, Points->x[0]);
+			G_trim_decimal(xstring);
+			G_asprintf(&ystring, "%.*f", dp, Points->y[0]);
+			G_trim_decimal(ystring);
+			fprintf(att, "A %s %s %d\n", xstring, ystring, cat);
+		    }
+		}
+		continue;
+	    }
+	    ctype = 'C';
+	    break;
+	case GV_LINE:
+	    ctype = 'L';
+	    break;
+	case GV_POINT:
+	    ctype = 'P';
+	    break;
+	case GV_FACE:
+	    ctype = 'F';
+	    break;
+	case GV_KERNEL:
+	    ctype = 'K';
+	    break;
+	default:
+	    ctype = 'X';
+	    fprintf(stderr, "got type %d\n", (int)type);
+	    break;
+	}
 
+	if (format == FORMAT_POINT) {
+	    /*fprintf(ascii, "%c", ctype); */
+
+	    G_asprintf(&xstring, "%.*f", dp, Points->x[0]);
+	    G_trim_decimal(xstring);
+	    G_asprintf(&ystring, "%.*f", dp, Points->y[0]);
+	    G_trim_decimal(ystring);
+
+	    if (Map->head.with_z && ver == 5) {
+		G_asprintf(&zstring, "%.*f", dp, Points->z[0]);
+		G_trim_decimal(zstring);
+		fprintf(ascii, "%s|%s|%s", xstring, ystring, zstring);
+	    }
+	    else {
+		fprintf(ascii, "%s|%s", xstring, ystring);
+	    }
+	    if (Cats->n_cats > 0)
+		fprintf(ascii, "|%d", Cats->cat[0]);
+
+	    fprintf(ascii, "\n");
+	}
+	else {
+	    /* FORMAT_STANDARD */
+	    if (ver == 5 && Cats->n_cats > 0)
+		fprintf(ascii, "%c  %d %d\n", ctype, Points->n_points,
+			Cats->n_cats);
+	    else
+		fprintf(ascii, "%c  %d\n", ctype, Points->n_points);
+
+	    xptr = Points->x;
+	    yptr = Points->y;
+	    zptr = Points->z;
+
+	    while (Points->n_points--) {
+
+		G_asprintf(&xstring, "%.*f", dp, *xptr++);
+		G_trim_decimal(xstring);
+		G_asprintf(&ystring, "%.*f", dp, *yptr++);
+		G_trim_decimal(ystring);
+
+		if (ver == 5) {
+		    if (Map->head.with_z) {
+			G_asprintf(&zstring, "%.*f", dp, *zptr++);
+			G_trim_decimal(zstring);
+			fprintf(ascii, " %-12s %-12s %-12s\n", xstring, ystring,
+				zstring);
+		    }
+		    else {
+			fprintf(ascii, " %-12s %-12s\n", xstring, ystring);
+		    }
+		}		/*Version 4 */
+		else {
+		    fprintf(ascii, " %-12s %-12s\n", ystring, xstring);
+		}
+	    }
+
+	    if (ver == 5) {
+		for (i = 0; i < Cats->n_cats; i++) {
+		    fprintf(ascii, " %-5d %-10d\n", Cats->field[i],
+			    Cats->cat[i]);
+		}
+	    }
+	    else {
+		if (cat > 0) {
+		    if (type == GV_POINT) {
+			G_asprintf(&xstring, "%.*f", dp, Points->x[0]);
+			G_trim_decimal(xstring);
+			G_asprintf(&ystring, "%.*f", dp, Points->y[0]);
+			G_trim_decimal(ystring);
+			fprintf(att, "P %s %s %d\n", xstring, ystring, cat);
+		    }
+		    else {
+			x = (Points->x[1] + Points->x[0]) / 2;
+			y = (Points->y[1] + Points->y[0]) / 2;
+
+			G_asprintf(&xstring, "%.*f", dp, x);
+			G_trim_decimal(xstring);
+			G_asprintf(&ystring, "%.*f", dp, y);
+			G_trim_decimal(ystring);
+			fprintf(att, "L %s %s %d\n", xstring, ystring, cat);
+		    }
+		}
+	    }
+	}
+    }
+
+    /* not reached */
+}
--- vector/v.out.ascii/out.c
+++ vector/v.out.ascii/out.c
@@ -1,107 +1,152 @@
-/*  @(#)b_a_dig.c	2.1  6/26/87  */
+/*
+ *
+ ****************************************************************************
+ *
+ * MODULE:     r.out.ascii
+ * AUTHOR(S):  Michael Higgins, U.S. Army Construction Engineering Research Laboratory
+ *             James Westervelt, U.S. Army Construction Engineering Research Laboratory
+ *             Radim Blazek, ITC-Irst, Trento, Italy
+ *
+ * PURPOSE:    r.out.ascii: writes ASCII GRID file
+ * COPYRIGHT:  (C) 2000 by the GRASS Development Team
+ *
+ *             This program is free software under the GNU General Public
+ *              License (>=v2). Read the file COPYING that comes with GRASS
+ *              for details.
+ *
+ ****************************************************************************
+ */
+/*  @(#)b_a_dig.c       2.1  6/26/87  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <grass/gis.h>
 #include <grass/Vect.h>
+#include <grass/glocale.h>
 #include "local_proto.h"
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	FILE *ascii, *att;
-	struct Option *old, *new, *format_opt;
-	struct Flag *verf;
-	int  format;
-	struct Map_info Map;
-	int    ver=5, pnt=0; 
-	struct GModule *module;
+    FILE *ascii, *att;
+    struct Option *input, *output, *format_opt, *dp_opt;
+    struct Flag *verf;
+    int format, dp;
+    struct Map_info Map;
+    int ver = 5, pnt = 0;
+    struct GModule *module;
 
-	G_gisinit(argv[0]);
+    G_gisinit(argv[0]);
 
-	module = G_define_module();
-	module->description = "Convert a GRASS binary vector map to a GRASS ASCII vector map";
+    module = G_define_module();
+    module->description =
+	_("Convert a GRASS binary vector map to a GRASS ASCII vector map");
 
-	old = G_define_standard_option(G_OPT_V_INPUT);
+    input = G_define_standard_option(G_OPT_V_INPUT);
 
-	new = G_define_option();
-	new->key		= "output";
-	new->type		=  TYPE_STRING;
-	new->required		=  NO;
-	new->multiple		=  NO;
-	new->gisprompt  	= "file,file,file" ;
-	new->description	= "path to resulting ascii file or ASCII vector name if '-o' is defined";
+    output = G_define_option();
+    output->key = "output";
+    output->type = TYPE_STRING;
+    output->required = NO;
+    output->multiple = NO;
+    output->gisprompt = "file,file,file";
+    output->description =
+	_("Path to resulting ASCII file or ASCII vector name if '-o' is defined");
 
-	format_opt = G_define_option();
-	format_opt->key		= "format";
-	format_opt->type	=  TYPE_STRING;
-	format_opt->required	=  NO;
-	format_opt->multiple	=  NO;
-	format_opt->options	= "point,standard";
-	format_opt->answer	= "point";
-	format_opt->description	= "output format";
+    format_opt = G_define_option();
+    format_opt->key = "format";
+    format_opt->type = TYPE_STRING;
+    format_opt->required = NO;
+    format_opt->multiple = NO;
+    format_opt->options = "point,standard";
+    format_opt->answer = "point";
+    format_opt->description = _("Output format");
 
-        verf = G_define_flag ();
-        verf->key               = 'o';
-        verf->description       = "create old (version 4) ascii file"; 
+    dp_opt = G_define_option();
+    dp_opt->key = "dp";
+    dp_opt->type = TYPE_INTEGER;
+    dp_opt->required = NO;
+    dp_opt->description =
+	_("Number of significant digits (floating point only)");
 
-	if (G_parser (argc, argv)) exit(-1);
+    verf = G_define_flag();
+    verf->key = 'o';
+    verf->description = _("Create old (version 4) ASCII file");
 
-	if ( format_opt->answer[0] == 'p' )
-	    format = FORMAT_POINT;
-	else 
-	    format = FORMAT_ALL;
-	
-	if ( verf->answer ) ver = 4;	
+    if (G_parser(argc, argv))
+	exit(EXIT_FAILURE);
 
-	if ( ver == 4 && format == FORMAT_POINT ) {
-	   G_fatal_error ("format 'point' is not supported for old version" );
+    if (format_opt->answer[0] == 'p')
+	format = FORMAT_POINT;
+    else
+	format = FORMAT_ALL;
+
+    if (verf->answer)
+	ver = 4;
+
+    if (ver == 4 && format == FORMAT_POINT) {
+	G_fatal_error(_("format 'point' is not supported for old version"));
+    }
+
+    if (ver == 4 && output->answer == NULL) {
+	G_fatal_error(_("'output' must be given for old version"));
+    }
+
+    Vect_set_open_level(1);	/* only need level I */
+    Vect_open_old(&Map, input->answer, "");
+
+
+    if (output->answer) {
+	if (ver == 4) {
+	    ascii = G_fopen_new("dig_ascii", output->answer);
+	}
+	else {
+	    ascii = fopen(output->answer, "w");
 	}
 
-	if ( ver == 4 && new->answer == NULL ) {
-	   G_fatal_error ("'output' must be given for old version" );
+	if (ascii == NULL) {
+	    G_fatal_error(_("Cannot open file [%s]"), output->answer);
 	}
+    }
+    else {
+	ascii = stdout;
+    }
 
-	Vect_set_open_level (1);	/* only need level I */
-	Vect_open_old (&Map, old->answer, ""); 
+    if (format == FORMAT_ALL) {
+	write_head(ascii, &Map);
+	fprintf(ascii, "VERTI:\n");
+    }
 
-		
-	if ( new->answer ) {
-	    if ( ver == 4 ) {
-	        ascii = G_fopen_new("dig_ascii", new->answer);
-	    } else {
-	        ascii = fopen ( new->answer, "w" );
-	    }
-		
-	    if ( ascii == NULL ) {
-		G_fatal_error("Cannot open file [%s]", new->answer );
-	    }
-	} else {
-	    ascii = stdout;
-	}
-	
-	if ( format == FORMAT_ALL ) {
-	    write_head(ascii, &Map) ;
-	    fprintf (ascii, "VERTI:\n");
-        }
+    /*The precision of the output */
+    if (dp_opt->answer) {
+	if (sscanf(dp_opt->answer, "%d", &dp) != 1)
+	    G_fatal_error(_("failed to interpret dp as an integer"));
+	if (dp > 8 || dp < 0)
+	    G_fatal_error(_("dp has to be from 0 to 8"));
+    }
+    else {
+	dp = 8;			/*This value is taken from the lib settings in G_format_easting */
+    }
 
-	/* Open dig_att */
-	att = NULL;
-	if ( ver == 4 && !pnt ) {
-	    if ( G_find_file ("dig_att", new->answer, G_mapset() ) != NULL )
-		G_fatal_error ( "dig_att file already exist" );
-		
-	    if ( (att = G_fopen_new("dig_att", new->answer) ) == NULL )
-	        G_fatal_error ( "Not able to open dig_att file <%s>\n", new->answer) ;
-	}
+    /* Open dig_att */
+    att = NULL;
+    if (ver == 4 && !pnt) {
+	if (G_find_file("dig_att", output->answer, G_mapset()) != NULL)
+	    G_fatal_error(_("dig_att file already exist"));
 
-	bin_to_asc (ascii, att, &Map, ver, format ) ;
+	if ((att = G_fopen_new("dig_att", output->answer)) == NULL)
+	    G_fatal_error(_("Not able to open dig_att file <%s>\n"),
+			  output->answer);
+    }
 
-	if ( ascii != NULL )  fclose(ascii) ;
-	if ( att != NULL )  fclose(att) ;
+    bin_to_asc(ascii, att, &Map, ver, format, dp);
 
-	Vect_close (&Map);
+    if (ascii != NULL)
+	fclose(ascii);
+    if (att != NULL)
+	fclose(att);
 
-	exit(0) ;
+    Vect_close(&Map);
+
+    exit(EXIT_SUCCESS);
 }
-
