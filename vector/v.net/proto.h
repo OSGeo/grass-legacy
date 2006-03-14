7f538a20438d62cc98b6d0939d2d9f5b1eae4b29
Radim Blazek <radim.blazek@gmail.com>
2006-03-14 16:22:54 +0000

    nodes report
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19232 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.net/proto.h
+++ vector/v.net/proto.h
@@ -1,5 +1,8 @@
+#define REPORT 0
+#define NREPORT 1
+
 /* nodes.c */
 int nodes(char *, char *, int add_cats, int nfield );
 
 /* report.c */
-int report(char *, int, int);
+int report(char *in, int afield, int nfield, int action);
