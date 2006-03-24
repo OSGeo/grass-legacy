be987752c47e9b725ba6ced7afd6f36d72fabafa
Cedric Shock <cedricgrass@shockfamily.net>
2006-03-24 02:57:14 +0000

    Options.tcl now controls visual display options for gui.tcl, gis.m, d.m.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19348 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/gtcltk/options.tcl
@@ -0,0 +1,86 @@
+############################################################################
+#
+# LIBRARY:      options.tcl gui options
+# AUTHOR(S):    Cedric Shock (cedricgrass AT shockfamily.net)
+# PURPOSE:      Default options and load user options
+# COPYRIGHT:    (C) 2006 GRASS Development Team
+#
+#               This program is free software under the GNU General Public
+#               License (>=v2). Read the file COPYING that comes with GRASS
+#               for details.
+#
+############################################################################
+
+# set background color and help font
+# These globals are still used in a few places by things in gis.m
+set bgcolor HoneyDew2
+set helpfont helpfont
+
+##############################################################################
+# Create fonts
+
+proc fontcreate {font args} {
+	if {[lsearch [font names] $font] == -1} {
+		eval font create $font $args
+	} else {
+		eval font configure $font $args
+	}
+}
+
+fontcreate ballon-help -family Helvetica -size -12 -weight bold
+fontcreate default -family Helvetica -size -12
+fontcreate textfont -family Courier -size -12
+fontcreate helpfont -family Verdana -size 12
+
+
+##############################################################################
+# Configure balloon help:
+
+DynamicHelp::configure -font balloon-help -fg black -bg "#FFFF77"
+
+##############################################################################
+# Configure almost everything using the options database
+
+# Font to use everywhere
+option add *font default
+
+# Various background colors
+option add *background HoneyDew2
+option add *activeBackground HoneyDew2
+
+# Things that are selected:
+option add *selectBackground #ffff9b
+# Menus use active instead of selected
+option add *Menu.activeBackground #ffff9b
+
+# Scrollbar trough color
+option add *troughColor HoneyDew3
+
+# Entry widgets and text widgets should have a white background
+option add *Entry.background white
+option add *Text.background white
+option add *Entry.font textfont
+option add *Text.font textfont
+
+# Options for map canvases
+option add *mapcanvas.background HoneyDew2
+option add *mapcanvas.insertbackground black
+option add *mapcanvas.selectbackground #c4c4c4
+option add *mapcanvas.selectforeground black
+
+
+##############################################################################
+# Platform specific default settings:
+# keycontrol is control key used in copy-paste bindings
+
+set keycontrol "Control"
+
+if {[info exists env(osxaqua)]} {
+    set osxaqua $env(osxaqua)
+} else {
+    set osxaqua "0"
+}
+
+if { $osxaqua == "1"} {
+    set keycontrol "Command"
+}
