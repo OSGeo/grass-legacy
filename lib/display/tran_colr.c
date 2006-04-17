bb6e4e516dffd773c1296147655a8841a8a24e34
Cedric Shock <cedricgrass@shockfamily.net>
2006-04-17 21:58:05 +0000

    Translated colors are looked up from the standard color definitions in colors.h. This changes (corrects) the meanings of some colors, notably violet and magenta, and adds the colors cyan and purple. A new command, D_translate_or_add_color, does most of the work needed for a display module to use custom colors using G_str_to_color instead of disparate implementations of color parsers.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19606 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/display/tran_colr.c
+++ lib/display/tran_colr.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <grass/display.h>
 #include <grass/colors.h>
+#include <grass/raster.h>
 
 
 /*!
@@ -21,21 +22,69 @@
 
 int D_translate_color(const char *str )
 {
-	if (! strcmp(str, "red"))	return(RED) ;
-	if (! strcmp(str, "orange"))	return(ORANGE) ;
-	if (! strcmp(str, "yellow"))	return(YELLOW) ;
-	if (! strcmp(str, "green"))	return(GREEN) ;
-	if (! strcmp(str, "blue"))	return(BLUE) ;
-	if (! strcmp(str, "indigo"))	return(INDIGO) ;
-	if (! strcmp(str, "violet"))	return(VIOLET) ;
-	if (! strcmp(str, "white"))	return(WHITE) ;
-	if (! strcmp(str, "black"))	return(BLACK) ;
-	if (! strcmp(str, "gray"))	return(GRAY) ;
-	if (! strcmp(str, "brown"))	return(BROWN) ;
-	if (! strcmp(str, "magenta"))	return(MAGENTA) ;
-	if (! strcmp(str, "aqua"))	return(AQUA) ;
+  int i;
 
-	if (! strcmp(str, "grey"))	return(GRAY) ;
+  for (i = 0; i < MAX_COLOR_NAMES; i ++) {
+    if (! strcmp (str, standard_color_names[i].name))
+      return standard_color_names[i].number ;
+  }
 
-	return(0) ;
+  return(0) ;
+}
+
+
+/*!
+ * \brief color name and suggested number to actual number
+ *
+ * Takes a color <b>name</b> or <b>red:green:blue</b> code in ascii
+ * and a <b>suggested color index</b>.
+ * If the color is a standard preallocated color it returns the color number for that color.
+ * Otherwise (if the color is not standard) it sets the color of the supplied index to 
+ * the specified color (see R_reset_color).
+ * Returns -1 if color is not known and 0 if the color is none.
+ *
+ *  \param name_or_code
+ *  \param suggested_color_index
+ *  \return int
+ */
+
+int D_translate_or_add_color (const char * str, int index)
+{
+	int redi, greeni, bluei;
+	int i, preallocated, ret;
+
+	char lowerstr[MAX_COLOR_LEN];
+
+	/* Make the color string lowercase for display colors */
+	G_strcpy (lowerstr, str );
+	G_chop (lowerstr);
+	G_tolcase (lowerstr);	
+
+	preallocated = D_translate_color (lowerstr);
+	
+	if (preallocated != 0) {
+		return preallocated;
+	}
+
+	ret = G_str_to_color (str, &redi, &greeni, &bluei);
+
+	if (ret == 2) {
+		/* None color */
+		return 0;
+	} else if (ret == 1) {
+		/* It would be possible to, at this point, search through
+		   the preallocated colors for this color and return the
+		   preallocated index on a match. That is what this does: */
+		for (i = 1; i <= MAX_COLOR_NUM; i++)
+			if (standard_colors_rgb[i].r == redi &&
+			    standard_colors_rgb[i].g == greeni &&
+			    standard_colors_rgb[i].b == bluei)
+				return i ;
+
+		/* Add the specified color to the suggested index */
+		R_reset_color ( (unsigned char) redi, (unsigned char) greeni, (unsigned char) bluei, index) ;
+		return index ;
+	} 
+
+	return -1 ;
 }
