2412a22ce3680729bd2fbc5fd2c44f99d17b4282
Hamish Bowman <hamish_b@yahoo.com>
2006-04-26 06:27:48 +0000

    make module description functional (fixes bug #4338), whitespace cleanup, i18n
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@19720 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.digit/main.c
+++ vector/v.digit/main.c
@@ -1,19 +1,14 @@
 /* ***************************************************************
- * *
- * * MODULE:       v.digit
- * * 
- * * AUTHOR(S):    Radim Blazek
- * *               
- * * PURPOSE:      Edit vector
- * *              
- * * COPYRIGHT:    (C) 2001 by the GRASS Development Team
- * *
- * *               This program is free software under the 
- * *               GNU General Public License (>=v2). 
- * *               Read the file COPYING that comes with GRASS
- * *               for details.
- * *
- * **************************************************************/
+ * MODULE:	 v.digit
+ * AUTHOR(S):	 Radim Blazek
+ * PURPOSE:	 Edit vector
+ * COPYRIGHT:	 (C) 2001 by the GRASS Development Team
+ *		 This program is free software under the 
+ *		 GNU General Public License (>=v2). 
+ *		 Read the file COPYING that comes with GRASS
+ *		 for details.
+ **************************************************************/
+
 #define MAIN
 #include <stdio.h>
 #include <stdlib.h>
@@ -23,6 +18,7 @@
 #include <grass/display.h>
 #include <grass/raster.h>
 #include <grass/Vect.h>
+#include <grass/glocale.h>
 #include "global.h"
 #include "proto.h"
 
@@ -39,31 +35,31 @@ int Tcl_AppInit(Tcl_Interp* interp)
     Toolbox = interp;
 
     Tcl_CreateCommand(interp, "c_tool_centre", (Tcl_CmdProc*) c_tool_centre, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_next_tool", (Tcl_CmdProc*) c_next_tool, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_cancel", (Tcl_CmdProc*) c_cancel, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_set_color", (Tcl_CmdProc*) c_set_color, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_set_on", (Tcl_CmdProc*) c_set_on, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_create_table", (Tcl_CmdProc*) c_create_table, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_table_definition", (Tcl_CmdProc*) c_table_definition, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_var_set", (Tcl_CmdProc*) c_var_set, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_create_bgcmd", (Tcl_CmdProc*) c_create_bgcmd, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_set_bgcmd", (Tcl_CmdProc*) c_set_bgcmd, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_add_blank_bgcmd", (Tcl_CmdProc*) c_add_blank_bgcmd, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_del_cat", (Tcl_CmdProc*) c_del_cat, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
     Tcl_CreateCommand(interp, "c_add_cat", (Tcl_CmdProc*) c_add_cat, (ClientData) NULL, 
-	                      (Tcl_CmdDeleteProc*) NULL);
+			      (Tcl_CmdDeleteProc*) NULL);
 
     /* Init variables */
     var_init ();
@@ -77,18 +73,16 @@ int Tcl_AppInit(Tcl_Interp* interp)
     sprintf(buf,"%s/etc/v.digit/toolbox.tcl", G_gisbase());
     ret = Tcl_EvalFile(interp, buf);
     if ( ret == TCL_ERROR) {
-        if (interp->result != NULL) 
-            sprintf(buf,"Cannot open toolbox: %s\n", interp->result);
+	if (interp->result != NULL) 
+	    G_fatal_error(_("Cannot open toolbox: %s"), interp->result);
 	else 
-            sprintf(buf,"Cannot open toolbox.\n");
-        G_fatal_error(buf) ;
+	    G_fatal_error(_("Cannot open toolbox."));
     }
 
     return TCL_OK;
 }
 
-int 
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
     int    i;
     struct GModule *module;
@@ -99,6 +93,9 @@ main (int argc, char *argv[])
     
     G_gisinit(argv[0]);
 
+    module = G_define_module(); 
+    module->description = _("Interactive editing and digitization of vector maps.");
+
     map_opt = G_define_standard_option(G_OPT_V_MAP);
     
     bgcmd_opt = G_define_option();
@@ -107,47 +104,48 @@ main (int argc, char *argv[])
     bgcmd_opt->required = NO;
     bgcmd_opt->multiple = NO;
     bgcmd_opt->answer = "";
-    bgcmd_opt->description  = "d.* commands to be used for background separated by ';'";
+    bgcmd_opt->description  = _("Display commands to be used for canvas backdrop (separated by ';')");
     
     new_f = G_define_flag ();
-    new_f->key             = 'n';
-    new_f->description     = "Create new file if it does not exist.";
+    new_f->key	     = 'n';
+    new_f->description     = _("Create new file if it does not exist.");
     
-    if (G_parser (argc, argv)) exit(EXIT_FAILURE); 
+    if (G_parser (argc, argv))
+	exit(EXIT_FAILURE); 
+
 
     if(getenv("GRASS_ANOTHER_BUTTON")){
 	    leftb   = 1;
 	    middleb = 3;
 	    rightb  = 2;
-    }else{
+    } else {
 	    leftb   = 1;
 	    middleb = 2;
 	    rightb  = 3;
     }
 
-    module = G_define_module(); 
-    module->description = "Edit GRASS vector.";
-
     G_debug (2, "Variable = %p", Variable );
  
     /* Read background commands */
     if ( bgcmd_opt->answer ) {
-        tokens = G_tokenize (bgcmd_opt->answer, ";");
-        for (i = 0; tokens[i] != NULL ; i++) {
+	tokens = G_tokenize (bgcmd_opt->answer, ";");
+	for (i = 0; tokens[i] != NULL ; i++) {
 	    G_debug (2, "cmd %d : %s", i, tokens[i]);
 	    bg_add ( tokens[i] ); 
 	}
-        G_free_tokens (tokens);
+	G_free_tokens (tokens);
     }
     
     for ( i = 0; i < nbgcmd; i++ ) G_debug (2, "cmd %d : %s", i, Bgcmd[i]);
     
     Tool_next = TOOL_NOTHING;
     G_get_window(&Region);
-    G_debug (1, "Region: N = %f S = %f E = %f W = %f", Region.north, Region.south, Region.east, Region.west);
+    G_debug (1, "Region: N = %f S = %f E = %f W = %f", Region.north,
+	Region.south, Region.east, Region.west);
     
     /* Check driver */
-    if (R_open_driver() != 0) G_fatal_error ("No graphics device selected");
+    if (R_open_driver() != 0)
+	G_fatal_error(_("No graphics device selected"));
     R_close_driver();
 
     G_debug (1, "Driver opened");
@@ -156,18 +154,18 @@ main (int argc, char *argv[])
     mapset = G_find_vector2 (map_opt->answer, G_mapset()); 
     if ( mapset == NULL ) {
        if ( new_f->answer ) {
-           fprintf (stderr, "New empty map created.\n");	
-	   Vect_open_new (&Map, map_opt->answer, 0 ); 
-           Vect_build ( &Map, NULL );
-           Vect_close (&Map);
-           Vect_open_update (&Map, map_opt->answer, G_mapset());
+	   G_message(_("New empty map created."));
+	   Vect_open_new (&Map, map_opt->answer, 0 );
+	   Vect_build ( &Map, NULL );
+	   Vect_close (&Map);
+	   Vect_open_update (&Map, map_opt->answer, G_mapset());
        } else {
-            G_message ( "Map does not exist. Add flag -n to create a new map.");
-	   exit (EXIT_FAILURE);
+	   G_message(_("Map does not exist. Add flag -n to create a new map."));
+	   exit(EXIT_FAILURE);
        }
     } else {
-        Vect_set_open_level(2);
-        Vect_open_update (&Map, map_opt->answer, mapset);
+	Vect_set_open_level(2);
+	Vect_open_update (&Map, map_opt->answer, mapset);
     }
     Vect_set_category_index_update ( &Map );
     Vect_hist_command ( &Map );
