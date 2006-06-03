496dd2a2fbb205c073243702ecb8907a8fe86446
Brad Douglas <rez@touchofmadness.com>
2006-06-03 08:43:49 +0000

    - localized text - updated debug code
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20102 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/closecell.c
+++ lib/gis/closecell.c
@@ -140,12 +140,14 @@ static int close_old (int fd)
     if (FCB.reclass_flag)
 	G_free_reclass (&FCB.reclass);
     FCB.open_mode = -1;
+
     if(FCB.map_type != CELL_TYPE)
     {
         G_quant_free(&FCB.quant);
         xdr_destroy(&FCB.xdrstream);
     } 
     close (fd);
+
     return 1;
 }
 
@@ -154,21 +156,25 @@ static int close_new (int fd,int ok)
     int stat;
     struct Categories cats;
     struct History hist;
-    char buf[4096];
     char path[4096];
     CELL cell_min, cell_max;
     int row, i, open_mode;
     char element[100];
 
     if (ok) {
-#ifdef DEBUG
-switch (FCB.open_mode)
-{
-case OPEN_NEW_COMPRESSED: fprintf (stderr, "close %s compressed\n",FCB.name); break;
-case OPEN_NEW_UNCOMPRESSED: fprintf (stderr, "close %s uncompressed\n",FCB.name); break;
-case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
-}
-#endif
+        switch (FCB.open_mode)
+        {
+        case OPEN_NEW_COMPRESSED:
+            G_debug(1, "close %s compressed", FCB.name); 
+            break;
+        case OPEN_NEW_UNCOMPRESSED:
+            G_debug(1, "close %s uncompressed", FCB.name);
+            break;
+        case OPEN_NEW_RANDOM:
+            G_debug(1, "close %s random", FCB.name);
+            break;
+        }
+
 	if (FCB.open_mode != OPEN_NEW_RANDOM && FCB.cur_row < FCB.cellhd.rows) {
 	    G_zero_raster_buf (FCB.data, FCB.map_type);
 	    for (row = FCB.cur_row; row < FCB.cellhd.rows; row++)
@@ -182,6 +188,7 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
         G__file_name(path, element, NULL_FILE, G_mapset());
         G__make_mapset_element(element);
         remove ( path );
+
         if(FCB.null_cur_row > 0) {
         /* if temporary NULL file exists, write it into cell_misc/name/null */
             int null_fd;
@@ -189,6 +196,7 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
             null_fd = G__open_null_write(fd);
 	    if(null_fd <= 0) return -1;
             if(null_fd < 1) return -1;
+
             /* first finish writing null file */
             /* write out the rows stored in memory */
 	    for (row = FCB.min_null_row; 
@@ -207,16 +215,14 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
             }
             close (null_fd);
             
-
 #ifdef __MINGW32__
 	    if ( CopyFile ( FCB.null_temp_name, path, FALSE ) == 0 ) {
 #else
 	    if(link (FCB.null_temp_name, path) < 0) {
 #endif
 		if(rename(FCB.null_temp_name, path)) {
-	            sprintf(buf,"closecell: can't move %s\nto null file %s",
-		    FCB.null_temp_name, path);
-	            G_warning (buf);
+	            G_warning(_("closecell: can't move %s\nto null file %s"),
+		                FCB.null_temp_name, path);
 	            stat = -1;
 	        }
 	    } else {
@@ -231,14 +237,15 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
             FCB.row_ptr[FCB.cellhd.rows] = lseek (fd, 0L, 1);
             G__write_row_ptrs (fd);
         }
+
         if(FCB.map_type != CELL_TYPE) {  /* floating point map */
            int cell_fd;
 
             if (G__write_fp_format(fd) != 0) {
-               sprintf(buf, "Error writing floating point format file for map %s", FCB.name);
-               G_warning(buf);
+               G_warning(_("Error writing floating point format file for map %s"), FCB.name);
                stat = -1;
             }
+
             /* now write 0-length cell file */
             G__make_mapset_element ("cell");
             cell_fd = creat (G__file_name(path, "cell", FCB.name, FCB.mapset), 0666);
@@ -287,33 +294,33 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
 	if(link (FCB.temp_name, path) < 0) {
 #endif
 	    if(rename(FCB.temp_name, path)) {
-	        sprintf(buf,"closecell: can't move %s\nto cell file %s",
-	        FCB.temp_name, path);
-	        G_warning (buf);
+	        G_warning(_("closecell: can't move %s\nto cell file %s"),
+	                    FCB.temp_name, path);
 	        stat = -1;
 	    }
         } else {
             remove ( FCB.temp_name );
         }
     }
+
     if (FCB.temp_name != NULL) {
 	G_free (FCB.temp_name);
     }
 
     if (ok) {
-/* remove color table */
+	/* remove color table */
 	G_remove_colr (FCB.name);
 
-/* create a history file */
+	/* create a history file */
         G_short_history (FCB.name, "raster", &hist);
 	G_write_history (FCB.name, &hist);
 
-/* write the range */
+	/* write the range */
         if(FCB.map_type == CELL_TYPE) {
        	     G_write_range (FCB.name, &FCB.range);
              G__remove_fp_range(FCB.name);
         }
-/*NOTE: int range for floating point maps is not written out */
+	/*NOTE: int range for floating point maps is not written out */
         else /* if(FCB.map_type != CELL_TYPE) */
         {
        	     G_write_fp_range (FCB.name, &FCB.fp_range);
@@ -326,10 +333,10 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
         else /* CELL map */
 	   FCB.cellhd.format = FCB.nbytes - 1;
 
-/* write header file */
+	/* write header file */
         G_put_cellhd (FCB.name, &FCB.cellhd);
 
-/* if map is floating point write the quant rules, otherwise remove f_quant */
+	/* if map is floating point write the quant rules, otherwise remove f_quant */
         if(FCB.map_type != CELL_TYPE) {
 	/* DEFAULT RANGE QUANT
 	     G_get_fp_range_min_max(&FCB.fp_range, &dcell_min, &dcell_max);
@@ -342,7 +349,7 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
         */
 	     G_quant_round(&FCB.quant);
              if( G_write_quant (FCB.name, FCB.mapset, &FCB.quant) < 0)
-                      G_warning(" can't write quant file!");
+                      G_warning(_("unable to write quant file!"));
         } else {
             /* remove cell_misc/name/f_quant */
             sprintf(element,"cell_misc/%s",FCB.name);
@@ -350,16 +357,15 @@ case OPEN_NEW_RANDOM: fprintf (stderr, "close %s random\n",FCB.name); break;
             remove ( path );
         }
 
-/* create empty cats file */
+	/* create empty cats file */
        G_get_range_min_max(&FCB.range, &cell_min, &cell_max);
        if(G_is_c_null_value(&cell_max)) cell_max = 0;
        G_init_cats (cell_max, (char *)NULL, &cats);
        G_write_cats (FCB.name, &cats);
        G_free_cats (&cats);
 
-/* write the histogram */
-/* only works for integer maps */
-
+	/* write the histogram */
+	/* only works for integer maps */
        if((FCB.map_type == CELL_TYPE)
             &&(FCB.want_histogram)) {
         	    G_write_histogram_cs (FCB.name, &FCB.statf);
@@ -391,7 +397,7 @@ int G__write_fp_format (int fd)
 
    if(FCB.map_type == CELL_TYPE)
    {
-       G_warning("Can't write f_format file for CELL maps");
+       G_warning(_("unable to write f_format file for CELL maps"));
        return 0;
    }
    format_kv = G_create_key_value();
@@ -412,5 +418,6 @@ int G__write_fp_format (int fd)
    G_write_key_value_file (path, format_kv, &stat);
 
    G_free_key_value(format_kv);
+
    return stat;
 }
