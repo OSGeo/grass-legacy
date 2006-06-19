e5513e941a45a36b9e4925c8622d359acaf16327
Hamish Bowman <hamish_b@yahoo.com>
2006-06-19 23:44:13 +0000

    G_standard_option(), EXIT_FAILURE, simplify error handling (bug #4631)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20240 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.param.scale/interface.c
+++ raster/r.param.scale/interface.c
@@ -46,30 +46,21 @@ void interface(int argc, char **argv)
       _("Extracts terrain parameters from a DEM. Uses a multi-scalar approach"
       " by taking fitting quadratic parameters to any size window (via least squares)");
 
-    rast_in   = G_define_option();	/* Request memory for each option.	*/
-    rast_out  = G_define_option();
-    tol1_val  = G_define_option();	
-    tol2_val  = G_define_option();	
-    win_size  = G_define_option();	
-    parameter = G_define_option();	
-    expon     = G_define_option();	
-    vert_sc   = G_define_option();	
+    rast_in   = G_define_standard_option(G_OPT_R_INPUT);
+    rast_out  = G_define_standard_option(G_OPT_R_OUTPUT);
+    tol1_val  = G_define_option();		/* Request memory for each option.	*/
+    tol2_val  = G_define_option();
+    win_size  = G_define_option();
+    parameter = G_define_option();
+    expon     = G_define_option();
+    vert_sc   = G_define_option();
 
     constr    = G_define_flag();
 
     /* Each option has a 'key' (short descriptn), a 'description` (longer one)	*/
     /* a 'type' (eg int, or string), and an indication whether manditory or not	*/
 
-    rast_in->key	  = "in";
-    rast_in->description  = _("Raster surface layer to process");
-    rast_in->type	  = TYPE_STRING;
-    rast_in->required	  = YES;
-
-
-    rast_out->key	  = "out";
     rast_out->description = _("Output raster layer containing morphometric parameter");
-    rast_out->type	  = TYPE_STRING;
-    rast_out->required	  = YES;
 
     tol1_val->key	  = "s_tol";
     tol1_val->description = _("Slope tolerance that defines a 'flat' surface (degrees)");
@@ -114,7 +105,8 @@ void interface(int argc, char **argv)
 
 
     if (G_parser(argc,argv))		/* Actually performs the prompting for	*/
-	exit(-1);			/* keyboard input. 			*/
+	exit(EXIT_FAILURE);		/* keyboard input. 			*/
+
 
     rast_in_name  = rast_in->answer;	/* Now  keyboard input has been parsed, */
     rast_out_name = rast_out->answer;	/* can place the contents into strings	*/
@@ -162,22 +154,15 @@ void interface(int argc, char **argv)
 	    			    	    mparam = FEATURE;
 			    	    else
 			    	    {
-	    				G_warning("Morphometric parameter not recognised. Assuming 'Elevation'");
+	    				G_warning(_("Morphometric parameter not recognised. Assuming 'Elevation'"));
 	    				mparam  = ELEV;
     			    	    }
 
     /*--------------------------------------------------------------------------*/
     /*                      CHECK INPUT RASTER FILE EXISTS			*/
     /*--------------------------------------------------------------------------*/
-
-
-    if ((mapset_in=G_find_cell2(rast_in_name,""))==NULL)
-    {
-	char err[256];
-	sprintf(err,"Raster map [%s] not available.",rast_in_name);
-	G_fatal_error(err);
-    }
-
+    if ( (mapset_in=G_find_cell2(rast_in_name,"")) == NULL)
+	G_fatal_error(_("Raster map [%s] not found"), rast_in_name);
 
     /*--------------------------------------------------------------------------*/
     /*                  CHECK OUTPUT RASTER FILE DOES NOT EXIST			*/
@@ -186,33 +171,13 @@ void interface(int argc, char **argv)
     mapset_out = G_mapset();		/* Set output to current mapset.	*/
 
     if (!G_legal_filename(rast_out_name))
-    {
-        char err[256];
-        sprintf(err,"Illegal file name. Please try another.");
-        G_fatal_error(err);
-    }
-    else
-    {
-
- /* commented for overall consistency */
- /*       if (G_find_cell2(rast_out_name,mapset_out) !=NULL)
-        {
-            char err[256];
-            sprintf(err,"Raster map [%s] exists.\nPlease try another\n",rast_out_name);
-            G_fatal_error(err);
-        }
-  */
-    }
+	G_fatal_error(_("[%s] is an illegal name"), rast_out_name);
 
     /*--------------------------------------------------------------------------*/
     /*                 CHECK WINDOW SIZE IS NOT EVEN OR TOO LARGE		*/
     /*--------------------------------------------------------------------------*/
 
     if ( (wsize/2 != (wsize-1)/2) || (wsize > MAX_WSIZE) )
-    {
-    	char err[256];
-    	sprintf(err,"Inappropriate window size (too big or even)");
-    	G_fatal_error(err);
-    }
+	G_fatal_error(_("Inappropriate window size (too big or even)"));
 
 }
