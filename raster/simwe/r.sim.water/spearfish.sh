8408c79902f7a7c4ec68e9d70c9af3a5f42fde81
Markus Neteler <neteler@gmail.com>
2006-07-05 10:07:48 +0000

    more rain; cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20403 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/simwe/r.sim.water/spearfish.sh
+++ raster/simwe/r.sim.water/spearfish.sh
@@ -9,15 +9,20 @@ g.region rast=${dem} -p
 
 echo "Preparing input maps..."
 r.slope.aspect --o elevation=$dem dx=${dem}_dx dy=${dem}_dy
-r.mapcalc "${dem}_rain=if(${dem},0.0001,null())"
+r.mapcalc "${dem}_rain=if(${dem},10.9,null())"
 r.mapcalc "${dem}_manin=if(${dem},0.04,null())"
 r.mapcalc "${dem}_infil=if(${dem},0.9,null())"
   
-echo "r.sim.water elevin=${dem} dxin=${dem}_dx dyin=${dem}_dy \
+echo "r.sim.water --o elevin=${dem} dxin=${dem}_dx dyin=${dem}_dy \
       rain=${dem}_rain manin=${dem}_manin infil=${dem}_infil \
-      depth=${dem}_depth disch=${dem}_disch"
+      depth=${dem}_depth disch=${dem}_disch err=${dem}_err"
   
-r.sim.water elevin=${dem} dxin=${dem}_dx dyin=${dem}_dy \
+r.sim.water --o elevin=${dem} dxin=${dem}_dx dyin=${dem}_dy \
   rain=${dem}_rain manin=${dem}_manin infil=${dem}_infil \
-  depth=${dem}_depth disch=${dem}_disch
+  depth=${dem}_depth disch=${dem}_disch err=${dem}_err
 
+echo "Written:
+ Output water depth raster file:     ${dem}_depth
+ Output water discharge raster file: ${dem}_disch
+ Output simulation error raster file: ${dem}_err
+"
