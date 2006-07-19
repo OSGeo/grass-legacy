26d34901b113a8a7e7a05993c597de4c55950841
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2006-07-19 08:55:14 +0000

    Convert projection to lat/long correctly (bug 4877)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20529 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.grid/plot.c
+++ display/d.grid/plot.c
@@ -233,41 +233,23 @@ struct Cell_head window ;
 void
 init_proj(struct pj_info *info_in, struct pj_info *info_out)
 {
-double a, es;
-struct Key_Value *in_proj_keys, *in_unit_keys;
 struct Key_Value *out_proj_keys, *out_unit_keys;
-char buff[100];
-
 
 /* Proj stuff for geo grid */
 /* Out Info */
 out_proj_keys = G_get_projinfo();
 out_unit_keys = G_get_projunits();
-if (pj_get_kv(*&info_out,out_proj_keys,out_unit_keys) < 0)
-        G_fatal_error( _("Can't get projection key values of current location"));
-
-/* In Info */
-in_proj_keys = G_create_key_value();
-in_unit_keys = G_create_key_value();
-
-G_set_key_value("proj", "ll", in_proj_keys);
-G_get_ellipsoid_parameters(&a, &es);
-sprintf(buff, "%f", a);
-G_set_key_value("a", buff, in_proj_keys);
-sprintf(buff, "%f", es);
-G_set_key_value("es", buff, in_proj_keys);
-
-G_set_key_value("unit", "degree", in_unit_keys);
-G_set_key_value("units", "degrees", in_unit_keys);
-G_set_key_value("meters", "1.0", in_unit_keys);
-
-if (pj_get_kv(*&info_in, in_proj_keys, in_unit_keys) < 0)
-    G_fatal_error( _("Unable to set up lat/long projection parameters"));
-
-G_free_key_value( in_proj_keys);
-G_free_key_value( in_unit_keys);
+if (pj_get_kv(info_out,out_proj_keys,out_unit_keys) < 0)
+        G_fatal_error( _("Can't get projection key values of current location"));   
 G_free_key_value( out_proj_keys);
 G_free_key_value( out_unit_keys);
+
+/* In Info */
+info_in->zone = 0;
+info_in->meters = 1.;
+sprintf(info_in->proj, "ll");
+if ((info_in->pj = pj_latlong_from_proj(info_out->pj)) == NULL)
+   G_fatal_error(_("Unable to set up lat/long projection parameters"));   
 
 return;
 
