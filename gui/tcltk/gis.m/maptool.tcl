f64654f494cb51f29d612ac815acf26fcf0d5bb4
Michael Barton <michael.barton@asu.edu>
2006-08-24 19:59:53 +0000

    Map display toolbar: added zoom entry for saving current geometry to named region
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20837 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/tcltk/gis.m/maptool.tcl
+++ gui/tcltk/gis.m/maptool.tcl
@@ -1,11 +1,11 @@
 ###############################################################
 # maptool.tcl - toolbar file GRASS GIS Manager map display canvas
 # January 2006 Michael Barton, Arizona State University
-# COPYRIGHT:	(C) 1999 - 2006 by the GRASS Development Team
+# COPYRIGHT:    (C) 1999 - 2006 by the GRASS Development Team
 #
-#		This program is free software under the GNU General Public
-#		License (>=v2). Read the file COPYING that comes with GRASS
-#		for details.
+#       This program is free software under the GNU General Public
+#       License (>=v2). Read the file COPYING that comes with GRASS
+#       for details.
 ###############################################################
 
 
@@ -27,14 +27,14 @@ proc MapToolBar::create { tb } {
     global iconpath
     global bgcolor
     variable toolbar
-    
+
     set selclr #88aa88
     set toolbar $tb
     set maptools "pointer"
-    
+
     # DISPLAY AND MONITOR SELECTION
     set bbox1 [ButtonBox $toolbar.bbox1 -spacing 0 ]
-    
+
     # display
     $bbox1 add -image [image create photo -file "$iconpath/gui-display.gif"] \
         -command "MapCanvas::request_redraw $mon 0" \
@@ -42,7 +42,7 @@ proc MapToolBar::create { tb } {
         -highlightbackground $bgcolor  -activebackground $bgcolor\
         -helptext [G_msg "Display active layers"]
 
-    # zoom to current region  
+    # zoom to current region
     $bbox1 add -image [image create photo -file "$iconpath/gui-redraw.gif"] \
         -command "MapCanvas::request_redraw $mon 1" \
         -highlightthickness 0 -takefocus 0 -relief link -borderwidth 1  \
@@ -67,43 +67,43 @@ proc MapToolBar::create { tb } {
 
     set sep1 [Separator $toolbar.sep1 -orient vertical -background $bgcolor ]
     pack $sep1 -side left -fill y -padx 5 -anchor w
-    
+
     # DISPLAY TOOLS
 
     # pointer
     set pointer [radiobutton $tb.pointer \
-    	-image [image create photo -file "$iconpath/gui-pointer.gif"] \
+        -image [image create photo -file "$iconpath/gui-pointer.gif"] \
         -command "MapCanvas::stoptool $mon; MapCanvas::pointer $mon" \
-		-variable maptools -value pointer  -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor  ]
+        -variable maptools -value pointer  -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor  ]
     DynamicHelp::register $pointer balloon [G_msg "Pointer"]
 
     # zoom in
     set zoomin [radiobutton $tb.zoomin \
-    	-image [image create photo -file "$iconpath/gui-zoom_in.gif"] \
+        -image [image create photo -file "$iconpath/gui-zoom_in.gif"] \
         -command "MapCanvas::stoptool $mon; MapCanvas::zoombind $mon 1" \
-		-variable maptools -value zoomin -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]   
+        -variable maptools -value zoomin -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $zoomin balloon [G_msg "Zoom In"]
-    
+
     #zoom out
     set zoomout [radiobutton $tb.zoomout \
-		-image [image create photo -file "$iconpath/gui-zoom_out.gif"] \
+        -image [image create photo -file "$iconpath/gui-zoom_out.gif"] \
         -command "MapCanvas::stoptool $mon; MapCanvas::zoombind $mon -1" \
-		-variable maptools -value zoomout  -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]    
+        -variable maptools -value zoomout  -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $zoomout balloon [G_msg "Zoom Out"]
 
     # pan
     set pan [radiobutton $tb.pan \
-		-image [image create photo -file "$iconpath/gui-pan.gif"] \
+        -image [image create photo -file "$iconpath/gui-pan.gif"] \
         -command "MapCanvas::stoptool $mon; MapCanvas::panbind $mon" \
-		-variable maptools -value pan  -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]    
+        -variable maptools -value pan  -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $pan balloon [G_msg "Pan"]
 
     pack $pointer $zoomin $zoomout $pan -side left -anchor w
@@ -120,73 +120,76 @@ proc MapToolBar::create { tb } {
         -highlightbackground $bgcolor -activebackground $bgcolor \
         -helptext [G_msg "Return to previous zoom"]
 
-	set mapzoom [menubutton $tb.mapzoom  \
-		-image [image create photo -file "$iconpath/gui-mapzoom.gif"] \
+    set mapzoom [menubutton $tb.mapzoom  \
+        -image [image create photo -file "$iconpath/gui-mapzoom.gif"] \
         -highlightthickness 0 -takefocus 0 -relief flat -borderwidth 1  \
         -highlightbackground $bgcolor -activebackground honeydew \
         -bg $bgcolor -width 32 -indicatoron 0 -direction below]
     DynamicHelp::register $mapzoom balloon [G_msg "Zoom to..."]
 
-	# menu zooming display
-	set zoommenu [menu $mapzoom.zm -type normal]
-	
-	set zmimg [image create photo -file "$iconpath/gui-zoom_map.gif"]
-	set zrimg [image create photo -file "$iconpath/gui-zoom_region.gif"]
-	set zcimg [image create photo -file "$iconpath/gui-zoom_current.gif"]
-	set zdimg [image create photo -file "$iconpath/gui-zoom_default.gif"]
+    # menu zooming display
+    set zoommenu [menu $mapzoom.zm -type normal]
 
-	$zoommenu add command \
-		-compound top \
-		-label {Zoom to selected map} \
-		-command {MapCanvas::zoom_map $mon}
-	$zoommenu add command \
-		-compound right \
-		-label {Zoom to saved region} \
-		-command {MapCanvas::zoom_region $mon}
-	$zoommenu add command \
-		-compound center \
-		-label {Zoom to current region (set with g.region)} \
-		-command {MapCanvas::zoom_current $mon}
-	$zoommenu add command \
-		-compound center \
-		-label {Zoom to default region} \
-		-command {MapCanvas::zoom_default $mon}
-	$zoommenu add command \
-		-compound center \
-		-label {Set current region (WIND file) to match display} \
-		-command {MapCanvas::set_wind $mon}
+    set zmimg [image create photo -file "$iconpath/gui-zoom_map.gif"]
+    set zrimg [image create photo -file "$iconpath/gui-zoom_region.gif"]
+    set zcimg [image create photo -file "$iconpath/gui-zoom_current.gif"]
+    set zdimg [image create photo -file "$iconpath/gui-zoom_default.gif"]
 
-	$mapzoom configure -menu $zoommenu
+    $zoommenu add command \
+        -compound top \
+        -label {Zoom to selected map} \
+        -command {MapCanvas::zoom_map $mon}
+    $zoommenu add command \
+        -compound right \
+        -label {Zoom to saved region} \
+        -command {MapCanvas::zoom_region $mon}
+    $zoommenu add command \
+        -compound center \
+        -label {Save display geometry to named region} \
+        -command {MapCanvas::save_region $mon}
+    $zoommenu add command \
+        -compound center \
+        -label {Zoom to current region (set with g.region)} \
+        -command {MapCanvas::zoom_current $mon}
+    $zoommenu add command \
+        -compound center \
+        -label {Zoom to default region} \
+        -command {MapCanvas::zoom_default $mon}
+    $zoommenu add command \
+        -compound center \
+        -label {Set current region (WIND file) to match display} \
+        -command {MapCanvas::set_wind $mon}
+
+    $mapzoom configure -menu $zoommenu
 
     pack $bbox2 -side left -anchor w
 
-	pack $mapzoom -side left -anchor w -expand no -fill y 
+    pack $mapzoom -side left -anchor w -expand no -fill y
 
     set sep3 [Separator $toolbar.sep3 -orient vertical -background $bgcolor ]
     pack $sep3 -side left -fill y -padx 5 -anchor w
 
-
     # query
     set query [radiobutton $tb.query \
-		-image [image create photo -file "$iconpath/gui-query.gif"] \
+        -image [image create photo -file "$iconpath/gui-query.gif"] \
         -command "MapCanvas::stoptool $mon; MapCanvas::querybind $mon" \
-		-variable maptools -value query  -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]    
+        -variable maptools -value query  -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $query balloon [G_msg "Query"]
 
     # measure
     set measure [radiobutton $tb.measure \
-		-image [image create photo -file "$iconpath/gui-measure.gif"]  \
-    	-command "MapCanvas::stoptool $mon; MapCanvas::measurebind $mon"\
-		-variable maptools -value measure -relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]    
+        -image [image create photo -file "$iconpath/gui-measure.gif"]  \
+        -command "MapCanvas::stoptool $mon; MapCanvas::measurebind $mon"\
+        -variable maptools -value measure -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $measure balloon [G_msg "Measure"]
 
     set bbox3 [ButtonBox $toolbar.bbox3 -spacing 0 ]
     $bbox3 add -image [image create photo -file "$iconpath/gui-profile.gif"] \
-    	-command "MapCanvas::stoptool $mon; MapCanvas::startprofile $mon" \
+        -command "MapCanvas::stoptool $mon; MapCanvas::startprofile $mon" \
         -highlightthickness 0 -takefocus 0 -relief link -borderwidth 1  \
         -highlightbackground $bgcolor -activebackground $bgcolor \
         -helptext [G_msg "Create profile of raster map"]
@@ -201,42 +204,42 @@ proc MapToolBar::create { tb } {
 
     # FILE & PRINT
     set bbox4 [ButtonBox $toolbar.bbox4 -spacing 0 ]
-    
+
     $bbox4 add -image [image create photo -file "$iconpath/file-print.gif"]  \
-    	-command "MapCanvas::printcanvas $mon" \
+        -command "MapCanvas::printcanvas $mon" \
         -highlightthickness 0 -takefocus 0 -relief link -borderwidth 1  \
         -highlightbackground $bgcolor -activebackground $bgcolor \
         -helptext [G_msg "Print raster & vector maps to eps file"]
 
-	set mapsave [menubutton $tb.mapsave  \
-		-image [image create photo -file "$iconpath/gui-filesave.gif"] \
+    set mapsave [menubutton $tb.mapsave  \
+        -image [image create photo -file "$iconpath/gui-filesave.gif"] \
         -highlightthickness 0 -takefocus 0 -relief flat -borderwidth 1  \
         -highlightbackground $bgcolor -activebackground honeydew \
         -bg $bgcolor -width 32 -indicatoron 0 -direction below]
     DynamicHelp::register $mapsave balloon [G_msg "Export display to graphics file"]
 
-	pack $mapsave -side left -anchor w -expand no -fill y 
+    pack $mapsave -side left -anchor w -expand no -fill y
     pack $bbox4 -side left -anchor w
 
-	# menu for saving display
-	set savefile [menu $mapsave.sf -type normal]
-	set pngfile [menu $savefile.png -type normal]
-	set ppmfile [menu $savefile.ppm -type normal]
-	set jpgfile [menu $savefile.jpg -type normal]
+    # menu for saving display
+    set savefile [menu $mapsave.sf -type normal]
+    set pngfile [menu $savefile.png -type normal]
+    set ppmfile [menu $savefile.ppm -type normal]
+    set jpgfile [menu $savefile.jpg -type normal]
 
-	$savefile add command -label "PPM/PNM" -command {MapToolBar::savefile ppm 0}
-	$savefile add command -label "TIF*" -command {MapToolBar::savefile tif 0}
-	$savefile add cascade -label "JPG*" -menu $jpgfile
-		$jpgfile add command -label "low quality (50)"  \
-			-command {MapToolBar::savefile jpg 50}
-		$jpgfile add command -label "mid quality (75)" \
-			-command {MapToolBar::savefile jpg 75}
-		$jpgfile add command -label "high quality (95)" \
-			-command {MapToolBar::savefile jpg 95}
-	$savefile add command -label "BMP*" -command {MapToolBar::savefile bmp 0}
-	$savefile add command -label "(* requires gdal)" -state disabled
+    $savefile add command -label "PPM/PNM" -command {MapToolBar::savefile ppm 0}
+    $savefile add command -label "TIF*" -command {MapToolBar::savefile tif 0}
+    $savefile add cascade -label "JPG*" -menu $jpgfile
+        $jpgfile add command -label "low quality (50)"  \
+            -command {MapToolBar::savefile jpg 50}
+        $jpgfile add command -label "mid quality (75)" \
+            -command {MapToolBar::savefile jpg 75}
+        $jpgfile add command -label "high quality (95)" \
+            -command {MapToolBar::savefile jpg 95}
+    $savefile add command -label "BMP*" -command {MapToolBar::savefile bmp 0}
+    $savefile add command -label "(* requires gdal)" -state disabled
 
-	$mapsave configure -menu $savefile
+    $mapsave configure -menu $savefile
 
     set sep5 [Separator $toolbar.sep5 -orient vertical ]
     pack $sep5 -side left -fill y -padx 5 -anchor w
@@ -246,22 +249,22 @@ proc MapToolBar::create { tb } {
     # Strict render mode
     # Uses previous resolution and exact boundaries
     set strictdraw [radiobutton $tb.strictdraw \
-		-command "MapCanvas::exploremode $mon 0" \
-		-variable MapToolBar::explore($mon) -value strict \
-		-relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]
+        -command "MapCanvas::exploremode $mon 0" \
+        -variable MapToolBar::explore($mon) -value strict \
+        -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $strictdraw balloon [G_msg "Constrain map to region geometry"]
     icon_configure $strictdraw drawmode strict
 
     # Explore render mode
     # Uses resolution to match display and expanded boundaries to fill display
     set exploredraw [radiobutton $tb.strictzoom \
-		-command "MapCanvas::exploremode $mon 1" \
-		-variable MapToolBar::explore($mon) -value explore \
-		-relief flat -offrelief flat -overrelief raised \
-		-borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
-		-activebackground $bgcolor -highlightbackground $bgcolor ]
+        -command "MapCanvas::exploremode $mon 1" \
+        -variable MapToolBar::explore($mon) -value explore \
+        -relief flat -offrelief flat -overrelief raised \
+        -borderwidth 1 -indicatoron false -bg $bgcolor -selectcolor $selclr \
+        -activebackground $bgcolor -highlightbackground $bgcolor ]
     DynamicHelp::register $exploredraw balloon [G_msg "Map fills display window"]
     icon_configure $exploredraw drawmode explore
 
@@ -272,12 +275,13 @@ proc MapToolBar::create { tb } {
     pack $strictdraw $exploredraw -side left -anchor w
 }
 
+
 ###############################################################################
 # changes button on keypress
 proc MapToolBar::changebutton { rbname } {
-	global maptools
-	
-	set maptools $rbname
+    global maptools
+
+    set maptools $rbname
 }
 
 ###############################################################################
@@ -285,46 +289,46 @@ proc MapToolBar::changebutton { rbname } {
 
 # save png file
 proc MapToolBar::savefile { type quality } {
-	global env
-	global mon
-	global outfile
-	global tmpdir
-	
+    global env
+    global mon
+    global outfile
+    global tmpdir
 
-	if { [info exists HOME] } {
-		set dir $env(HOME)
-		set path [tk_getSaveFile -initialdir $dir \
-			-title "Save file: do not add extension to file name"]
-	} else {
-		set path [tk_getSaveFile  \
-			 -title "Save file: do not add extension to file name"]
-	}
-	set currdir [pwd]
-	cd $tmpdir
 
-	file copy -force $outfile($mon) $path.ppm
+    if { [info exists HOME] } {
+        set dir $env(HOME)
+        set path [tk_getSaveFile -initialdir $dir \
+            -title "Save file: do not add extension to file name"]
+    } else {
+        set path [tk_getSaveFile  \
+             -title "Save file: do not add extension to file name"]
+    }
+    set currdir [pwd]
+    cd $tmpdir
 
-	cd $currdir
-	
-	if { $path != "" } {				
-		switch $type {
-			"ppm" {
-				return
-			}
-			"tif" { 
-				exec gdal_translate $path.ppm $path.tif -of GTIFF
-				exec rm $path.ppm
-			}
-			"bmp" { 
-				exec gdal_translate $path.ppm $path.bmp -of BMP
-				exec rm $path.ppm
-			}
-			"jpg" { 
-				exec gdal_translate $path.ppm $path.jpg -of JPEG -co QUALITY=$quality
-				exec rm $path.ppm
-			}
-		}
-	}
-	return
+    file copy -force $outfile($mon) $path.ppm
+
+    cd $currdir
+
+    if { $path != "" } {
+        switch $type {
+            "ppm" {
+                return
+            }
+            "tif" {
+                exec gdal_translate $path.ppm $path.tif -of GTIFF
+                exec rm $path.ppm
+            }
+            "bmp" {
+                exec gdal_translate $path.ppm $path.bmp -of BMP
+                exec rm $path.ppm
+            }
+            "jpg" {
+                exec gdal_translate $path.ppm $path.jpg -of JPEG -co QUALITY=$quality
+                exec rm $path.ppm
+            }
+        }
+    }
+    return
 }
 
