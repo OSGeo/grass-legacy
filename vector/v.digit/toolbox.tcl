1651a909fb042203dcf71d18a3b434f3132ad196
Michael Barton <michael.barton@asu.edu>
2006-08-25 00:11:48 +0000

    cosmetic improvements
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20842 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.digit/toolbox.tcl
+++ vector/v.digit/toolbox.tcl
@@ -4,7 +4,9 @@ package require -exact BWidget 1.2.1
 # I'm not sure how to grab the map name, so I'll leave it obviously broken
 # in the hope someone will fix it. If you comment out the following line,
 # then the toolbox window takes on the map name! argh! We want both.
-wm title . "v.digit - \$mapname"
+#wm title . "v.digit - \$mapname"
+#set winname [winfo atomname -displayof .]
+#puts "win name is $winname"
 
 source $env(GISBASE)/etc/gtcltk/gmsg.tcl
 source $env(GISBASE)/etc/gtcltk/select.tcl
@@ -44,13 +46,14 @@ proc new_line_options { create } {
 
     Label $row1.flab -padx 2 -pady 2 -relief flat -anchor w -text [G_msg "  Layer"]
     set GWidget(field) [Entry $row1.fval -width 6 -textvariable GVariable(field) \
-                -command { c_var_set field $GVariable(field) } ]
+                -bg white -command { c_var_set field $GVariable(field) } ]
         bind $GWidget(field) <KeyRelease> { c_var_set field $GVariable(field) }
     Label $row1.clab -padx 2 -pady 2 -relief flat -anchor w -text [G_msg "  Category"]
     set GWidget(cat) [Entry $row1.cval -width 6 -textvariable GVariable(cat) \
-                -command { c_var_set cat $GVariable(cat) }]
+                -bg white -command { c_var_set cat $GVariable(cat) }]
         bind $GWidget(cat) <KeyRelease> { c_var_set cat $GVariable(cat) }
-    set GWidget(cat_mode) [ComboBox $row1.cmode -label [G_msg "  Mode "] -width 12 -textvariable cmode \
+    set GWidget(cat_mode) [ComboBox $row1.cmode -label [G_msg "  Mode "]\
+            -entrybg white -width 12 -textvariable cmode \
             -modifycmd {
                 set GVariable(cat_mode) [ $GWidget(cat_mode) getvalue]
                             c_var_set cat_mode $GVariable(cat_mode)
@@ -72,86 +75,86 @@ proc new_line_options { create } {
 
 
 # button frame row 2
-set bbox1 [ButtonBox .bbox1 -spacing 1 -padx 1 -pady 1]
+set bbox2 [ButtonBox .bbox2 -spacing 1 -padx 1 -pady 1]
 
 # --- Draw new ---
-$bbox1 add -image [image create photo -file "$vdpath/new.point.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/new.point.gif"] \
         -command "c_next_tool new_point" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Digitize new point"]
 
-$bbox1 add -image [image create photo -file "$vdpath/new.line.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/new.line.gif"] \
         -command "c_next_tool new_line" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Digitize new line"]
 
-$bbox1 add -image [image create photo -file "$vdpath/new.boundary.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/new.boundary.gif"] \
         -command "c_next_tool new_boundary" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Digitize new boundary"]
 
-$bbox1 add -image [image create photo -file "$vdpath/new.centroid.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/new.centroid.gif"] \
         -command "c_next_tool new_centroid" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Digitize new centroid"]
 
 # --- Edit old ---
-$bbox1 add -image [image create photo -file "$vdpath/move.vertex.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/move.vertex.gif"] \
         -command "c_next_tool move_vertex" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Move vertex"]
 
-$bbox1 add -image [image create photo -file "$vdpath/add.vertex.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/add.vertex.gif"] \
         -command "c_next_tool add_vertex" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Add vertex"]
 
-$bbox1 add -image [image create photo -file "$vdpath/rm.vertex.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/rm.vertex.gif"] \
         -command "c_next_tool rm_vertex" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Remove vertex"]
 
-$bbox1 add -image [image create photo -file "$vdpath/split.line.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/split.line.gif"] \
         -command "c_next_tool split_line" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Split line"]
 
-$bbox1 add -image [image create photo -file "$vdpath/edit.line.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/edit.line.gif"] \
         -command "c_next_tool edit_line" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Edit line/boundary"]
 
-$bbox1 add -image [image create photo -file "$vdpath/move.line.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/move.line.gif"] \
         -command "c_next_tool move_line" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Move point, line, boundary, or centroid"]
 
-$bbox1 add -image [image create photo -file "$vdpath/delete.line.gif"] \
+$bbox2 add -image [image create photo -file "$vdpath/delete.line.gif"] \
         -command "c_next_tool delete_line" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Delete point, line, boundary, or centroid"]
 
 
 # button frame row 1
-set bbox2 [ButtonBox .bbox2 -spacing 1 -padx 1 -pady 1]
+set bbox1 [ButtonBox .bbox1 -spacing 1 -padx 1 -pady 1]
 
 # --- Zoom / Display ---
-$bbox2 add -image [image create photo -file "$vdpath/zoom.window.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/zoom.window.gif"] \
         -command "c_next_tool zoom_window" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Zoom in by window"]
 
-$bbox2 add -image [image create photo -file "$vdpath/zoom.out.centre.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/zoom.out.centre.gif"] \
         -command "c_next_tool zoom_out_centre" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Zoom out"]
 
-$bbox2 add -image [image create photo -file "$vdpath/zoom.pan.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/zoom.pan.gif"] \
         -command "c_next_tool zoom_pan" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Pan"]
 
-$bbox2 add -image [image create photo -file "$vdpath/zoom.default.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/zoom.default.gif"] \
         -command "c_next_tool zoom_default" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Zoom to default region"]
@@ -164,51 +167,51 @@ proc zoom_region { } {
     }
 }
 
-$bbox2 add -image [image create photo -file "$vdpath/zoom.region.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/zoom.region.gif"] \
         -command "zoom_region" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Zoom to region"]
 
-$bbox2 add -image [image create photo -file "$vdpath/redraw.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/redraw.gif"] \
         -command "c_next_tool redraw" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Redraw"]
 
 # --- Attributes ---
-$bbox2 add -image [image create photo -file "$vdpath/display.cats.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/display.cats.gif"] \
         -command "c_next_tool display_cats" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Display categories"]
 
-$bbox2 add -image [image create photo -file "$vdpath/copy.cats.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/copy.cats.gif"] \
         -command "c_next_tool copy_cats" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Copy categories"]
 
-$bbox2 add -image [image create photo -file "$vdpath/display.attributes.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/display.attributes.gif"] \
         -command "c_next_tool display_attributes" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Display attributes"]
 
 # --- Stop ---
-#$bbox2 add -image [image create photo -file "$vdpath/stop.gif"] \
+#$bbox1 add -image [image create photo -file "$vdpath/stop.gif"] \
 #        -command "c_cancel" \
 #        -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
 #        -helptext "Quit running tool"
 
 # --- Others ---
-$bbox2 add -image [image create photo -file "$vdpath/settings.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/settings.gif"] \
         -command "c_next_tool settings" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Open settings"]
 
-$bbox2 add -image [image create photo -file "$vdpath/exit.gif"] \
+$bbox1 add -image [image create photo -file "$vdpath/exit.gif"] \
         -command "c_next_tool exit" \
         -highlightthickness 0 -takefocus 0 -relief raised -borderwidth 1 \
         -helptext [G_msg "Save and exit"]
 
-pack $bbox2 -side top -anchor w
 pack $bbox1 -side top -anchor w
+pack $bbox2 -side top -anchor w
 
 
 frame .pf
@@ -216,16 +219,17 @@ pack .pf -fill x -side top
 Label .pf.prompt -padx 3 -pady 2 -relief flat -anchor w -textvariable prompt
 pack .pf.prompt -fill x  -side left
 
-labelframe .bpf -text [G_msg "mouse button actions (left, right, center)"] -labelanchor n
+labelframe .bpf -text [G_msg "mouse button actions (left, right, center)"]\
+    -labelanchor n -bg HoneyDew2
 pack .bpf -fill x -side top -padx 8
-Label .bpf.left -width 10  -pady 5 -relief raised -anchor center -textvariable prompt_left
-Label .bpf.middle -width 10 -padx 2 -pady 5 -relief raised -anchor center -textvariable prompt_middle
-Label .bpf.right -width 10 -pady 5 -relief raised -anchor center -textvariable prompt_right
+Label .bpf.left -width 10  -pady 5 -relief raised -anchor center -textvariable prompt_left -bg WhiteSmoke
+Label .bpf.middle -width 10 -padx 2 -pady 5 -relief raised -anchor center -textvariable prompt_middle -bg WhiteSmoke
+Label .bpf.right -width 10 -pady 5 -relief raised -anchor center -textvariable prompt_right -bg WhiteSmoke
 pack .bpf.left .bpf.middle .bpf.right -fill x -side left -expand yes -padx 4 -pady 8
 
 frame .coorf
 pack .coorf -fill x -side top -padx 8 -pady 5
-Label .coorf.prompt -width 50 -padx 3 -pady 2 -relief flat -anchor w    -textvariable coor
+Label .coorf.prompt -width 50 -padx 3 -pady 2 -relief flat -anchor w -textvariable coor
 pack .coorf.prompt -fill x  -side left
 
 set destroyed 0
