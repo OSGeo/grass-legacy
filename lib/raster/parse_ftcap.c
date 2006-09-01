92dd7a2af284be4a52adf76091deeadc55ac2bcc
Markus Neteler <neteler@gmail.com>
2006-09-01 09:57:39 +0000

    missing header includes added
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@20933 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/raster/parse_ftcap.c
+++ lib/raster/parse_ftcap.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <grass/gis.h>
 #include <grass/glocale.h>
 #include <grass/freetypecap.h>
--- lib/raster/rem_proto.c
+++ lib/raster/rem_proto.c
@@ -3,6 +3,7 @@
 #ifdef HAVE_SOCKET
 
 #include <stdio.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
