30b7ea964f2d8e2f0b3beeabbf3204226f07a694
Soeren Gebbert <soerengebbert@googlemail.com>
2006-09-24 20:41:59 +0000

    Code cleaning Added exit status Replaced fprintf with G_warning
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21150 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.water.outlet/legal.c
+++ raster/r.water.outlet/legal.c
@@ -3,13 +3,12 @@
 
 int do_legal (char *file_name)
 {
-	char buf[120];
+	char *buf;
 
 	if (G_legal_filename (file_name) == -1) {
-		sprintf (buf, "map layer [%s] not legal for GRASS\n",
+		G_asprintf (&buf, "map layer [%s] not legal for GRASS\n",
 			file_name);
 		G_fatal_error (buf);
-		exit (1);
 	}
 
 	return 0;
@@ -17,13 +16,13 @@ int do_legal (char *file_name)
 
 char *do_exist (char *file_name)
 {
-	char buf[120], *file_mapset;
+	char *buf, *file_mapset;
 
 	file_mapset = G_find_cell2 (file_name, "");
 	if (file_mapset == NULL)	{
-		sprintf (buf, "[%s] map not found\n", file_name);
+		G_asprintf (&buf, "[%s] map not found\n", file_name);
 		G_fatal_error (buf);
-		exit (1);
 	}
 	return (file_mapset);
 }
+
