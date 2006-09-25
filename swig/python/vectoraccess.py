592634d2e753bc5d6429ae1f8c9802fd363f8eb4
Markus Neteler <neteler@gmail.com>
2006-09-25 08:20:22 +0000

    cleanup
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21154 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- swig/python/vectoraccess.py
+++ swig/python/vectoraccess.py
@@ -8,17 +8,13 @@ input = 'soils'
 mapset = 'PERMANENT'
 
 g6lib.G_gisinit('')
+map = g6lib.Map_info()
+g6lib.Vect_open_old(map, input, mapset)
 g6lib.Vect_set_open_level (2)
 
-map = g6lib.Map_info()
-
 print 'Vect is 3D: ', g6lib.Vect_is_3d (map)
-
-g6lib.Vect_open_old(map, input, mapset)
 print 'Vect DB links: ', g6lib.Vect_get_num_dblinks(map)
-
 print 'Map Scale:  1:', g6lib.Vect_get_scale(map)
-
 print 'Number of lines:', g6lib.Vect_get_num_lines(map)
 
 g6lib.Vect_close(map)
