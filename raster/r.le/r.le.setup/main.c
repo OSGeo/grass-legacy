c63d1bcfec6a06e10586f61d5045cc4b100a24dd
Hamish Bowman <hamish_b@yahoo.com>
2006-09-27 06:05:23 +0000

    use G_parser(); don't hang on G_system("d.frame -e"); remove GRASS 5 sites prompt
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21182 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.le/r.le.setup/main.c
+++ raster/r.le/r.le.setup/main.c
@@ -26,7 +26,7 @@
 #include <grass/gis.h>
 #include <grass/raster.h>
 #include <grass/display.h>
-#include "local_proto.h"
+#include <grass/glocale.h>
 
 #define MAIN
 #include "setup.h"
@@ -35,89 +35,62 @@
 
 				/* MAIN PROGRAM */
 
-int
-main (int argc, char *argv[])
-
+int main (int argc, char *argv[])
 {
 
-				/* setup a structure to store the window
-				   parameters */
+    struct GModule *module;
+    struct Option *input, *vect;
 
-  struct Cell_head  window;
-  int               bot, right, t0, b0, l0, r0, respond, clear=0;
-  double 	    Rw_l, Rscr_wl;
-  char 		    tmp[20], *v_name=NULL, *s_name=NULL;
+    struct Cell_head  window;
+    int bot, right, t0, b0, l0, r0, clear=0;
+    double Rw_l, Rscr_wl;
+    char *map_name=NULL, *v_name=NULL, *s_name=NULL;
 
-  char              name[20];
-  char              map_name[20];
-  char              info[20];
-  char              *mapset;
+    /* must run in a term window */
+    G_putenv("GRASS_UI_TERM","1");
+
+
+    /* Initialize the GIS calls */
+    G_gisinit(argv[0]);
+
+    module = G_define_module();
+    module->keywords = _("raster");
+    module->description =
+      _("Interactive tool used to setup the sampling and analysis framework "
+	"that will be used by the other r.le programs.");
+
+    input = G_define_standard_option(G_OPT_R_MAP);
+    input->description = _("Raster map to use to setup sampling");
+
+    vect = G_define_standard_option(G_OPT_V_INPUT);
+    vect->key	      = "vect";
+    vect->description = _("Vector map to overlay");
+    vect->required    = NO ;
+
+    if (G_parser(argc, argv))
+        exit(EXIT_FAILURE);
+
 
   setbuf (stdout, NULL);	/* unbuffered */
   setbuf (stderr, NULL);
-  G_gisinit(argv[0]); 
 
   G_sleep_on_error(0);
 
 
-				/* get the user input */
+  map_name = input->answer;
+  v_name = vect->answer;
+  s_name = NULL;  /* sites not used in GRASS 6 */
 
-  user_input(argc,argv) ; 
-
-				/* query for the map to be setup */
-
-  if (R_open_driver() != 0)
-      G_fatal_error ("No graphics device selected");
-      
-  fprintf(stderr, "\n") ;
-  fprintf(stderr, "OPTION   Raster map to use to setup sampling\n") ;
-  fprintf(stderr, "     key:map\n") ;
-  fprintf(stderr, "required:YES\n") ;
-  fprintf(stderr, "\n") ;
-
-  mapset = G_ask_cell_old("enter raster map name: ",map_name) ;
-  if (mapset == NULL) exit(0) ;
-
-
-				/* query for a vector map overlay */
-
-  fprintf(stderr, "\n") ;
-  fprintf(stderr, "OPTION   Vector map to overlay.\n") ;
-  fprintf(stderr, "     key:vect\n") ;
-  fprintf(stderr, "required:NO\n") ;
-  fprintf(stderr, "\n") ;
-
-  if ( G_yes("Overlay a vector file? ",0)) {
-      G_ask_vector_old("enter vector file name to be overlaid: ",name) ;
-      v_name = G_malloc(30);
-      strcpy(v_name, name);
-     }
-
-				/* query for a site map overlay */
-
-  fprintf(stderr, "\n") ;
-  fprintf(stderr, "OPTION   Site map to overlay.\n") ;
-  fprintf(stderr, "     key:site\n") ;
-  fprintf(stderr, "required:NO\n") ;
-  fprintf(stderr, "\n") ;
-
-
-   if ( G_yes("Overlay a site file? ",0)) {
-      mapset=G_ask_sites_old("enter site map to overlay", info) ;
-      if (mapset == NULL) exit(0) ;
-      G_fopen_sites_old(info,mapset) ;
-      s_name = G_malloc(30);
-      strcpy(s_name, info);
-   }
-
-
+  /* setup the r.le.para directory */
   get_pwd();
 
-  				/* setup the current window for 
-				   display */
+  /* query for the map to be setup */
+  if (R_open_driver() != 0)
+      G_fatal_error ("No graphics device selected");
 
-  /* G_system(" d.colormode fixed");*/
-  G_system(" d.frame -e");
+  /* setup the current window for display & clear screen */
+  D_setup(1);
+
   Rw_l = (double)G_window_cols()/G_window_rows();
   /*R_open_driver(); */
   /* R_font("romant");*/
@@ -137,7 +110,6 @@ main (int argc, char *argv[])
     right = r0;
     bot = t0 + (r0 - l0) / Rw_l; 
   }
-
   D_new_window("a", t0, bot, l0, right);
   D_set_cur_wind("a");
   D_show_window(D_translate_color("green"));
@@ -145,7 +117,6 @@ main (int argc, char *argv[])
   R_close_driver();
 
   				/* invoke the setup modules */
-
   set_map(map_name, v_name, s_name, window, t0, bot, l0, right);
 
   return (EXIT_SUCCESS);
@@ -154,10 +125,7 @@ main (int argc, char *argv[])
 
 
 
-
-
-				/* SETUP THE R.LE.PARA DIRECTORY */
-
+/* SETUP THE R.LE.PARA DIRECTORY */
 void   get_pwd()
 {
   DIR    *dp;
