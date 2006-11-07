d937455bf465138954707d0398650b7465eb7967
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2006-11-07 11:42:00 +0000

    Handle non-standard ETRS_1989 datum name
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21605 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/proj/convert.c
+++ lib/proj/convert.c
@@ -708,6 +708,8 @@ static const char *papszDatumEquiv[] =
     "European_Terrestrial_Reference_System_1989",
     "European_Reference_System_1989",
     "European_Terrestrial_Reference_System_1989",
+    "ETRS_1989",
+    "European_Terrestrial_Reference_System_1989",
     "NZGD_2000",
     "New_Zealand_Geodetic_Datum_2000",
     "Monte_Mario_Rome",
