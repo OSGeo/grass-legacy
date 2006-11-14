2bdb45cf8ac1b59a7b4917926c2fd61e9c46e619
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2006-11-14 16:50:00 +0000

    * Detect Windows instead of Msys in Tcl files * Don't use Msys shell to launch modules from gis.m on Windows - call   C modules normally and Shell scripts via batch files * Use new "grocat" program to merge stdout and stderr for modules   run from gronsole.tcl
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21677 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/tcltk/gis.m/runandoutput.tcl
+++ gui/tcltk/gis.m/runandoutput.tcl
@@ -66,16 +66,10 @@ proc make_fun_buttons {dlg path} {
 
 proc run_ui {cmd} {
     global dlg path
-    global mingw
 
     set program [lindex $cmd 0]
 
-    if { $mingw == "1" } {
-	# shell scripts for MSys
-	set code [exec -- sh -c '$program --tcltk']
-    } else {
-	set code [exec -- $program --tcltk]
-    }
+    set code [exec -- $program --tcltk]
 
     set path .dialog$dlg
     toplevel $path
@@ -155,13 +149,9 @@ proc execute {cmd} {
 
 ###############################################################################
 proc spawn {cmd args} {
-	global mingw
 
-	if { $mingw == "1" } {
-		eval [list exec -- sh -c '$cmd] $args' &
-	} else {
-		eval [list exec -- $cmd] $args &
-	}
+    eval [list exec -- $cmd] $args &
+
 }
 
 ###############################################################################
@@ -181,18 +171,14 @@ proc run_panel {cmd} {
 
 ###############################################################################
 proc run {cmd args} {
-	global mingw
 	global devnull
 
 	# This and runcmd are being used to run command in the background
 	# These used to go to stdout and stderr
 	# but we don't want to pollute that console.
 	# eval exec -- $cmd $args >@ stdout 2>@ stderr
-	if { $mingw == "1" } {
-		eval [list exec -- sh -c '$cmd] $args' >& $devnull
-	} else {
-		eval [list exec -- $cmd] $args >& $devnull
-	}
+	eval [list exec -- $cmd] $args >& $devnull
+	
 }
 
 ###############################################################################
