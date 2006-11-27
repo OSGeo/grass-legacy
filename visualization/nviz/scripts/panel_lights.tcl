bb15593aed221fd4c256ae81142c086590c203fb
Michael Barton <michael.barton@asu.edu>
2006-11-27 22:53:49 +0000

    Update GUI. Major code changes.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21904 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- visualization/nviz/scripts/panel_lights.tcl
+++ visualization/nviz/scripts/panel_lights.tcl
@@ -15,8 +15,8 @@ proc mklightsPanel { BASE } {
 
     set Nv_(FollowView) 0
     set Nv_(ShowModel) 1
-    set Nv_(LIGHT_XY) $BASE.bottom.xy
-    set Nv_(LIGHT_HGT) $BASE.bottom.height
+    set Nv_(LIGHT_XY) $BASE.top.left.1.xy
+    set Nv_(LIGHT_HGT) $BASE.top.left.1.height
     set Nv_(LIGHT_BASE) $BASE
     
     catch {destroy $BASE}
@@ -26,48 +26,69 @@ proc mklightsPanel { BASE } {
     } else {
 	set panel $Nv_($BASE)
     }
-    frame $BASE  -relief groove -borderwidth 2
+    frame $BASE  -relief flat -borderwidth 0
     Nv_mkPanelname $BASE "Lighting Panel"
     
-    frame $BASE.top 
+    frame $BASE.top
     frame $BASE.bottom
     frame $BASE.top.right
+    frame $BASE.top.right.1
+    frame $BASE.top.right.2
     frame $BASE.top.left 
+    frame $BASE.top.left.1
     
     # Set up a binding so we do ShowModel correctly
     bind $BASE <Map> {if {$Nv_(FollowView) == 0} { do_light_draw } }
+    
+    Label $BASE.top.left.label -text " Light source position" -fg black
+    pack $BASE.top.left.label -side top -fill none -expand 0 -anchor w
 
-    checkbutton $BASE.top.left.follow -relief flat -text "Follow ViewPoint" \
-	-variable Nv_(FollowView) -command follow -onvalue 1 -offvalue 0
-    checkbutton $BASE.top.left.show -relief flat -text "Show Model" \
-	-variable Nv_(ShowModel)
-    Nv_mkScale $BASE.top.left.bright h Brightness 0 100 80 set_brt 2 
-    Nv_mkScale $BASE.top.left.ambient h Ambient 0 100 20 set_amb 2
+    # create lighting height slider
+	Nv_mkScale $BASE.top.left.1.height v Height 100 0 80 set_lgt_hgt 2
+   # pack $BASE.bottom.height -side right -expand 1 -fill y
+
+	# create interactive panel for lighting point of view
+    Nv_mkXYScale $BASE.top.left.1.xy puck LIGHT_POS 125 125 105 105 \
+		set_lgt_position unset_follow
+	pack $BASE.top.left.1.xy $BASE.top.left.1.height -side left \
+		-expand 1 -padx 3
+	pack $BASE.top.left.1 -side top -pady 2
+
+	# create checkbuttons
+    checkbutton $BASE.top.left.follow -text "Follow surface viewpoint" \
+		-variable Nv_(FollowView) -command follow -onvalue 1 -offvalue 0
+    checkbutton $BASE.top.left.show -text "Show lighting model" \
+		-variable Nv_(ShowModel)
     pack $BASE.top.left.follow $BASE.top.left.show \
-        $BASE.top.left.bright $BASE.top.left.ambient \
-	-side top -fill x -expand 1
+		-side top -fill none -expand 0 -anchor w -pady 2
     
-    Nv_mkScale $BASE.top.right.red h Red 0 100 100 set_red 2
-    Nv_mkScale $BASE.top.right.green h Green 0 100 100 set_green 2
-    Nv_mkScale $BASE.top.right.blue h Blue 0 100 100 set_blue 2
-    pack $BASE.top.right.red $BASE.top.right.green $BASE.top.right.blue \
-	-side top -expand 1
-    
-    pack \
-    [Nv_mkXYScale $BASE.bottom.xy puck LIGHT_POS 125 125 105 105 \
-	set_lgt_position unset_follow]  \
-    -side right -expand 1
+    #create lighting color, ambient, and brightness sliders
+	Label $BASE.top.right.1.lbl -text "Light color" -fg black
+    Nv_mkScale $BASE.top.right.1.red h Red 0 100 100 set_red 2   
+    Nv_mkScale $BASE.top.right.1.green h Green 0 100 100 set_green 2
+    Nv_mkScale $BASE.top.right.1.blue h Blue 0 100 100 set_blue 2
+    Label $BASE.top.right.2.lbl -text "Light intensity" -fg black
+    Nv_mkScale $BASE.top.right.2.bright h Brightness 0 100 80 set_brt 2 
+    Nv_mkScale $BASE.top.right.2.ambient h Ambient 0 100 20 set_amb 2
+    pack $BASE.top.right.1.lbl $BASE.top.right.1.red \
+    	$BASE.top.right.1.green $BASE.top.right.1.blue \
+		-side top -expand 1 -pady 2
+	pack $BASE.top.right.1 -side top -expand 1
+    pack $BASE.top.right.2.lbl $BASE.top.right.2.bright \
+    	$BASE.top.right.2.ambient -side top -expand 1 -pady 2
+	pack $BASE.top.right.2 -side top -expand 1 -pady 4
 
-    pack [Nv_mkScale $BASE.bottom.height v Height 100 0 80 set_lgt_hgt 2] \
-    -side left -expand 1 -fill y
+    button $BASE.bottom.reset -text "Reset" -command "Nviz_lights_reset" -bd 1
+    pack $BASE.bottom.reset -side left
+    button $BASE.bottom.close -text "Close" -command "Nv_closePanel $BASE" -bd 1
+    pack $BASE.bottom.close -side right
 
-    pack $BASE.top.left -side left -expand 1
-    pack $BASE.top.right -side right -expand 1
-    pack $BASE.top -side top -fill x -expand 1
-    pack $BASE.bottom -side top -fill x -expand 1
 
-    button $BASE.close -text Close -command "Nv_closePanel $BASE" -anchor s
-    pack $BASE.close -side right
+    pack $BASE.top.left -side left -expand 1 -pady 4 -anchor n
+    pack $BASE.top.right -side right -expand 1 -pady 4 -anchor n
+    pack $BASE.top -side top -fill both -expand 1
+    pack $BASE.bottom -side bottom -fill x -expand 1
+
     return $panel
 }
 
@@ -151,32 +172,36 @@ proc Nviz_lights_load {file_hook} {
 # on the current settings of light attributes
 proc update_light_panel {} {
     global Nv_ Light
+    global Nauto_draw
 
     set BASE $Nv_(LIGHT_BASE)
 
     # brightness
     set val [$Light(1) get_bright]
-    Nv_scaleCallback $BASE.top.left.bright b 2 null [expr int($val * 100)]
+    Nv_scaleCallback $BASE.top.right.2.bright b 2 null [expr int($val * 100)]
 
     # ambient
     set val [$Light(1) get_ambient]
     set val [lindex $val 0]
-    Nv_scaleCallback $BASE.top.left.ambient b 2 null [expr int($val * 100)]
+    Nv_scaleCallback $BASE.top.right.2.ambient b 2 null [expr int($val * 100)]
 
     # color
     set val [$Light(1) get_color]
-    Nv_scaleCallback $BASE.top.right.red b 2 null [expr int([lindex $val 0] * 100)]
-    Nv_scaleCallback $BASE.top.right.green b 2 null [expr int([lindex $val 1] * 100)]
-    Nv_scaleCallback $BASE.top.right.blue b 2 null [expr int([lindex $val 2] * 100)]
+    Nv_scaleCallback $BASE.top.right.1.red b 2 null [expr int([lindex $val 0] * 100)]
+    Nv_scaleCallback $BASE.top.right.1.green b 2 null [expr int([lindex $val 1] * 100)]
+    Nv_scaleCallback $BASE.top.right.1.blue b 2 null [expr int([lindex $val 2] * 100)]
 
     # height + XY position (puck)
     set val [$Light(1) get_position]
     set x [expr [lindex $val 0] / 2.0 + 0.5]
     set y [expr [lindex $val 1] / -2.0 + 0.5]
     set z [lindex $val 2]
-    Nv_scaleCallback $BASE.bottom.height b 2 null [expr int($z * 100)]
-    Nv_itemDrag $BASE.bottom.xy $Nv_(LIGHT_POS) [expr int(125 * $x)] [expr int(125 * $y)]
+    Nv_scaleCallback $BASE.top.left.1.height b 2 null [expr int($z * 100)]
+    Nv_itemDrag $BASE.top.left.1.xy $Nv_(LIGHT_POS) [expr int(125 * $x)] [expr int(125 * $y)]
 
+	if {$Nauto_draw == 1} {
+		Ndraw_all
+	} 
 }
 
 proc do_light_draw {} {
@@ -191,24 +216,25 @@ proc do_light_draw {} {
     }
 }
 
-proc set_red {val} {set_lgt_color r $val}
+proc set_red {val} {
+	set_lgt_color r $val
+}
 proc set_green {val} {set_lgt_color g $val}
 proc set_blue {val} {set_lgt_color b $val}
 
 proc set_lgt_color {c val} {
     global Light
-
+    
     set c_list [$Light(1) get_color]
-
     if {[llength $c_list] == 3} {
         if {$c == "r"} {set r $val} else {set r [lindex $c_list 0]}
         if {$c == "g"} {set g $val} else {set g [lindex $c_list 1]}
         if {$c == "b"} {set b $val} else {set b [lindex $c_list 2]}
         $Light(1) set_color $r $g $b
-
         do_light_draw
-    } else { return -code error "light colors not set $c_list"}
-
+    } else { 
+    	return -code error "light colors not set $c_list"
+    }
 }
 
 proc set_amb {a} {
