4bd3c3b6944784125dac299f185436328a1c6370
Markus Neteler <neteler@gmail.com>
2006-12-05 21:32:50 +0000

    copyright statement added
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@21983 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/lidar/lidarlib/InterpSpline.c
+++ vector/lidar/lidarlib/InterpSpline.c
@@ -1,3 +1,21 @@
+/***********************************************************************
+ *
+ * MODULE:       lidarlib
+ *
+ * AUTHOR(S):    Roberto Antolin
+ *
+ * PURPOSE:      LIDAR Spline Interpolation
+ *
+ * COPYRIGHT:    (C) 2006 by Politecnico di Milano -
+ *                           Polo Regionale di Como
+ *
+ *               This program is free software under the
+ *               GNU General Public License (>=v2).
+ *               Read the file COPYING that comes with GRASS
+ *               for details.
+ *
+ **************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <float.h>
