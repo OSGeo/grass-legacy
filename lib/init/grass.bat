9d551347429daf087fb834f30dcbb1e2d3ae37ea
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2006-12-06 10:57:35 +0000

    Experimental batch file startup scripts for GRASS on Windows (i.e. no bourne/bash shell required). Doesn't do mapset locking.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22004 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/init/grass.bat
@@ -0,0 +1,9 @@
+@echo off
+rem #########################################################################
+rem #
+rem #		GRASS Initialization
+rem #
+rem #########################################################################
+
+set WINGISBASE=GISBASE_VALUE
+"%WINGISBASE%\etc\init.bat" %*
