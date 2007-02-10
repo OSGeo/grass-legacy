4c08f8f909ea933ff2ab89907842eae38bbd8c98
Markus Neteler <neteler@gmail.com>
2007-02-10 09:50:18 +0000

    typo
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22518 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.li/r.li.shannon/shannon.c
+++ raster/r.li/r.li.shannon/shannon.c
@@ -45,7 +45,7 @@ int main(int argc, char *argv[])
     
     conf = G_define_option();
     conf->key = "conf";
-    conf->description = "configuration file in ~/.r.li/history/ folder (i.e conf=my_configurationi)";
+    conf->description = "configuration file in ~/.r.li/history/ folder (i.e conf=my_configuration)";
     conf->gisprompt = "old_file,file,input";
     conf->type = TYPE_STRING;
     conf->required = YES;
