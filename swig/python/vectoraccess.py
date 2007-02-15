1103164044da8a714406e485db71f9ec51131ed7
Markus Neteler <neteler@gmail.com>
2007-02-15 14:42:38 +0000

    cmd line support; don't crash if not in GRASS
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22626 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- swig/python/vectoraccess.py
+++ swig/python/vectoraccess.py
@@ -2,9 +2,18 @@
 
 # run within GRASS Spearfish session
 
+import os, sys
 import python_grass6 as g6lib
 
-input = 'soils'
+if not os.environ.has_key("GISBASE"):
+    print "You must be in GRASS GIS to run this program."
+    sys.exit(1)
+
+if len(sys.argv)==2:
+  input = sys.argv[1]
+else:
+  input = raw_input("Vector Map Name? ")
+
 mapset = 'PERMANENT'
 
 # initialize
@@ -20,10 +29,17 @@ g6lib.Vect_set_open_level (2)
 g6lib.Vect_open_old(map, input, mapset)
 
 # query
+print 'Vect map: ', input
 print 'Vect is 3D: ', g6lib.Vect_is_3d (map)
 print 'Vect DB links: ', g6lib.Vect_get_num_dblinks(map)
 print 'Map Scale:  1:', g6lib.Vect_get_scale(map)
-print 'Number of lines:', g6lib.Vect_get_num_lines(map)
+# misleading:
+# print 'Number of lines:', g6lib.Vect_get_num_lines(map)
+print 'Number of points: ', g6lib.Vect_get_num_primitives(map,g6lib.GV_POINT)
+# confusing:
+#print 'Number of lines: ', g6lib.Vect_get_num_primitives(map,g6lib.GV_LINE)
+#print 'Number of areas:', g6lib.Vect_get_num_primitives(map,g6lib.GV_AREA)
+print 'Number of areas:', g6lib.Vect_get_num_areas(map)
 
 # close map
 g6lib.Vect_close(map)
