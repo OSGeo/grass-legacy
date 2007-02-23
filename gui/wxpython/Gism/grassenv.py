a9e4ce53811dac4a982f098cb3eda8258d125fc6
Jachym Cepicky <jachym.cepicky@gmail.com>
2007-02-23 10:55:01 +0000

    more grass exceptions
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28526 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/Gism/grassenv.py
+++ gui/wxpython/Gism/grassenv.py
@@ -6,6 +6,27 @@ class NotInGRASSSession(Exception):
     def __str__(self):
         return "You must be in running GRASS session"
 
+class CouldNotStartMonitor(Exception):
+    def __init__(self,monitor):
+        self.monitor = monitor
+
+    def __str__(self):
+        return "Could not start GRASS monitor <%s>" % self.monitor
+
+class CouldNotStopMonitor(Exception):
+    def __init__(self,monitor):
+        self.monitor = monitor
+
+    def __str__(self):
+        return "Could not start GRASS monitor <%s>" % self.monitor
+
+class CouldNotExecute(Exception):
+    def __init__(self,command):
+        self.command = command
+    def __str__(self):
+        return "Could not execute GRASS command: %s" % self.command
+
+
 if not os.getenv("GISBASE"):
     raise NotInGRASSSession()
 else:
