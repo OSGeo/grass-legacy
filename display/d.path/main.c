d20857f5b106ba6300bf2ce5db1102b253231d1d
Hamish Bowman <hamish_b@yahoo.com>
2007-02-26 03:51:52 +0000

    i18n, EXIT_SUCCESS
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22751 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.path/main.c
+++ display/d.path/main.c
@@ -39,83 +39,85 @@ int main(int argc, char **argv)
     struct color_rgb color, hcolor, bgcolor;
     int    r, g, b;
 
+
     /* Initialize the GIS calls */
     G_gisinit (argv[0]) ;
 
     module = G_define_module();
     module->keywords = _("display, networking");
     module->description = 
-    "Find shortest path for selected starting and ending node";
+	_("Find shortest path for selected starting and ending node");
 
     map = G_define_standard_option(G_OPT_V_MAP);
 
     type_opt =  G_define_standard_option(G_OPT_V_TYPE);
     type_opt->options    = "line,boundary";
     type_opt->answer     = "line,boundary";
-    type_opt->description = "Arc type";
+    type_opt->description = _("Arc type");
 
     afield_opt = G_define_standard_option(G_OPT_V_FIELD);
     afield_opt->key = "alayer";
     afield_opt->answer = "1";
-    afield_opt->description = "Arc layer";
+    afield_opt->description = _("Arc layer");
     
     nfield_opt = G_define_standard_option(G_OPT_V_FIELD);
     nfield_opt->key = "nlayer";
     nfield_opt->answer = "2";
-    nfield_opt->description = "Node layer";
+    nfield_opt->description = _("Node layer");
 
     afcol = G_define_option() ;
     afcol->key         = "afcol" ;
     afcol->type        = TYPE_STRING ;
     afcol->required    = NO ; 
-    afcol->description = "Arc forward/both direction(s) cost column" ;
+    afcol->description = _("Arc forward/both direction(s) cost column");
     
     abcol = G_define_option() ;
     abcol->key         = "abcol" ;
     abcol->type        = TYPE_STRING ;
     abcol->required    = NO ; 
-    abcol->description = "Arc backward direction cost column" ;
+    abcol->description = _("Arc backward direction cost column");
     
     ncol = G_define_option() ;
     ncol->key         = "ncol" ;
     ncol->type        = TYPE_STRING ;
     ncol->required    = NO ; 
-    ncol->description = "Node cost column" ;
+    ncol->description = _("Node cost column");
     
     color_opt = G_define_option() ;
     color_opt->key        = "color" ;
     color_opt->type       = TYPE_STRING ;
     color_opt->answer     = DEFAULT_FG_COLOR ;
-    color_opt->description= "Original line color" ;
+    color_opt->description= _("Original line color");
     color_opt->gisprompt  = GISPROMPT_COLOR ;
 	
     hcolor_opt = G_define_option() ;
     hcolor_opt->key        = "hcolor" ;
     hcolor_opt->type       = TYPE_STRING ;
     hcolor_opt->answer     = "red" ;
-    hcolor_opt->description= "Highlight color" ;
+    hcolor_opt->description= _("Highlight color");
     hcolor_opt->gisprompt  = GISPROMPT_COLOR ;
     
     bgcolor_opt = G_define_option() ;
     bgcolor_opt->key        = "bgcolor" ;
     bgcolor_opt->type       = TYPE_STRING ;
     bgcolor_opt->answer     = DEFAULT_BG_COLOR ;
-    bgcolor_opt->description= "Background color" ;
+    bgcolor_opt->description= _("Background color");
     bgcolor_opt->gisprompt  = GISPROMPT_COLOR ;
 
     geo_f = G_define_flag ();
     geo_f->key             = 'g';
-    geo_f->description     = "Use geodesic calculation for longitude-latitude locations";
+    geo_f->description     =
+	_("Use geodesic calculation for longitude-latitude locations");
     
     if(G_parser(argc,argv))
-        exit(-1);
+        exit(EXIT_FAILURE);
 
     type = Vect_option_to_types ( type_opt ); 
     afield = atoi (afield_opt->answer);
     nfield = atoi (nfield_opt->answer);
 
     if (R_open_driver() != 0)
-       G_fatal_error ("No graphics device selected");
+       G_fatal_error (_("No graphics device selected"));
 
     color = G_standard_color_rgb(BLACK);
     if ( G_str_to_color(color_opt->answer, &r, &g, &b) ) {
@@ -142,14 +144,14 @@ int main(int argc, char **argv)
     {
        geo = 1; 
        if (G_projection () != PROJECTION_LL)
-          G_fatal_error("The current projection is not longitude-latitude");
+          G_fatal_error(_("The current projection is not longitude-latitude"));
     }
     else geo = 0;
-    
+
     mapset = G_find_vector2 (map->answer, NULL); 
-      
+
     if ( mapset == NULL) 
-      G_fatal_error ("Could not find input %s\n", map->answer);
+      G_fatal_error (_("Could not find input map <%s>"), map->answer);
 
     Vect_set_open_level(2);
     Vect_open_old (&Map, map->answer, mapset); 
@@ -160,8 +162,9 @@ int main(int argc, char **argv)
 		D_get_d_west(), D_get_d_east(),
 		D_move_abs, D_cont_abs);
 
-    Vect_net_build_graph ( &Map, type , afield, nfield, afcol->answer, abcol->answer, ncol->answer, 
-	                   geo, 0 );
+    Vect_net_build_graph ( &Map, type , afield, nfield, afcol->answer,
+			   abcol->answer, ncol->answer, geo, 0 );
+
     path ( &Map, &color, &hcolor, &bgcolor ); 
 
 
@@ -169,7 +172,7 @@ int main(int argc, char **argv)
 
     Vect_close(&Map);
 
-    exit(0);
+    exit(EXIT_SUCCESS);
 }
 
 
