f82dbad67ed3e914a026f615d7263bdb48133b14
Glynn Clements <glynn@gclements.plus.com>
2007-03-06 03:25:30 +0000

    Fix raster scaling
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22840 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/driver/scale.c
+++ lib/driver/scale.c
@@ -11,18 +11,28 @@ static int nalloc;
 static int src[2][2];
 static int dst[2][2];
 
-static int scale(int k, const int src[2], const int dst[2])
+static double scale(double k, const int src[2], const int dst[2])
 {
-	return (int) floor(0.5 + dst[0] + (double) (k - src[0]) * (dst[1] - dst[0]) / (src[1] - src[0]));
+	return dst[0] + (double) (k - src[0]) * (dst[1] - dst[0]) / (src[1] - src[0]);
 }
 
-static int next_row(int sy, int dy, const int src[2], const int dst[2])
+static int scale_fwd_y(int sy)
+{
+	return (int) floor(scale(sy, src[1], dst[1]) + 0.5);
+}
+
+static int scale_rev_x(int dx)
+{
+	return (int) floor(scale(dx + 0.5, dst[0], src[0]));
+}
+
+static int next_row(int sy, int dy)
 {
 	sy++;
 
 	for (;;)
 	{
-		int y = scale(sy, src, dst);
+		int y = scale_fwd_y(sy);
 		if (y > dy)
 			return sy - 1;
 		sy++;
@@ -54,20 +64,20 @@ void LIB_begin_scaled_raster(int s[2][2], int d[2][2])
 	alloc_buffers();
 
 	for (i = 0; i < ncols; i++)
-		trans[i] = scale(d[0][0] + i, dst[0], src[0]);
+		trans[i] = scale_rev_x(d[0][0] + i);
 }
 
 int LIB_scaled_raster(
 	int n, int row,
 	unsigned char *red, unsigned char *grn, unsigned char *blu, unsigned char *nul)
 {
-	int d_y0 = scale(row + 0, src[1], dst[1]);
-	int d_y1 = scale(row + 1, src[1], dst[1]);
+	int d_y0 = scale_fwd_y(row + 0);
+	int d_y1 = scale_fwd_y(row + 1);
 	int d_rows = d_y1 - d_y0;
 	int i;
 
 	if (d_rows <= 0)
-		return next_row(row, d_y0, src[1], dst[1]);
+		return next_row(row, d_y0);
 
 	/* Make the screen raster */
 	for (i = 0; i < ncols; i++)
@@ -84,6 +94,6 @@ int LIB_scaled_raster(
 	COM_Move_abs(dst[0][0], d_y0);
 	COM_RGB_raster(ncols, d_rows, d_red, d_grn, d_blu, nul ? d_nul : nul);
 
-	return next_row(row, d_y1, src[1], dst[1]);
+	return next_row(row, d_y1);
 }
 
