db0a82972d8e353e8150e5147b6cf15ec28bdeea
Brad Douglas <rez@touchofmadness.com>
2007-03-09 14:52:43 +0000

    - Code readability cleanup - Updated API
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@22906 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.target/main.c
+++ imagery/i.target/main.c
@@ -14,63 +14,58 @@
 int 
 main (int argc, char *argv[])
 {
-    struct Option *grp, *map, *loc;
+    struct Option *group, *mapset, *loc;
     struct GModule *module;
     struct Flag *c;
 
     G_gisinit (argv[0]);
 
-	module = G_define_module();
-	module->keywords = _("imagery");
+    module = G_define_module();
+    module->keywords = _("imagery");
     module->description =
         	_("Targets an imagery group to a GRASS location and mapset.");
         	
-	grp = G_define_option();
-	grp->key             = "group";
-	grp->type            =  TYPE_STRING;
-	grp->required        =  YES;
-	grp->gisprompt        = "old,group,group";
-	grp->description     = _("Name of imagery group");
+    group = G_define_standard_option (G_OPT_I_GROUP);
 
-	loc = G_define_option();
-	loc->key             = "location";
-	loc->type            =  TYPE_STRING;
-	loc->required        =  NO;
-	loc->description     = _("Name of imagery target location");
+    loc = G_define_option();
+    loc->key             = "location";
+    loc->type            =  TYPE_STRING;
+    loc->required        =  NO;
+    loc->description     = _("Name of imagery target location");
 
-	map = G_define_option();
-	map->key             = "mapset";
-	map->type            =  TYPE_STRING;
-	map->required        =  NO;
-	map->description     = _("Name of target mapset");
+    mapset = G_define_option();
+    mapset->key          = "mapset";
+    mapset->type         =  TYPE_STRING;
+    mapset->required     =  NO;
+    mapset->description  = _("Name of target mapset");
 
-	c = G_define_flag();
-	c->key              = 'c';
-	c->description      = _("Set current location and mapset as target for of imagery group");
+    c = G_define_flag();
+    c->key              = 'c';
+    c->description      = _("Set current location and mapset as target for of imagery group");
 
     if (G_parser(argc, argv))
         exit(EXIT_FAILURE);
 
     /* error if -c is specified with other options */
-    if ((!c->answer) && (!map->answer || !loc->answer))
+    if ((!c->answer) && (!mapset->answer || !loc->answer))
 	G_fatal_error(_("You must use either the Current Mapset and "
              "Location Flag (-c)\n OR\n manually enter the variables."));
 
     /* error if -c is not specified and not enough other options */
-    if ((c->answer) && (map->answer || loc->answer))
+    if ((c->answer) && (mapset->answer || loc->answer))
 	G_fatal_error(_("You must use either the Current Mapset and "
              "Location Flag (-c)\n OR\n manually enter the variables."));
 
     if (c->answer) {
         /* point group target to current mapset and location */
-        I_put_target(grp->answer, G_location(), G_mapset());
+        I_put_target(group->answer, G_location(), G_mapset());
         G_message(_("group [%s] targeted for location [%s], mapset [%s]\n"),
-                grp->answer, G_location(), G_mapset());
+                group->answer, G_location(), G_mapset());
     } else {
         /* point group target to specified mapset and location */
-        I_put_target(grp->answer, loc->answer, map->answer);
+        I_put_target(group->answer, loc->answer, mapset->answer);
         G_message(_("group [%s] targeted for location [%s], mapset [%s]\n"),
-                grp->answer, loc->answer, map->answer);
+                group->answer, loc->answer, mapset->answer);
     }
 
     return 0;
