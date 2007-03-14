d67e155448ff7612f16ab4349cc1d98a640c2671
Michael Barton <michael.barton@asu.edu>
2007-03-14 03:05:31 +0000

    Closing map display now will also close associated controls page with map layer tree. Fixed minor bug in mapdisp.py.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28573 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/Gism/track.py
+++ gui/wxpython/Gism/track.py
@@ -11,85 +11,59 @@ class Track:
 	This class has functions and variables for tracking map display,
 	associated notebook pages, and other index values.
 	"""
-#	def __init__(self):
-
 
 	def SetCtrlDict(self, idx, disp, page, tree):
+		"""
+		This method stores the associated display index, display ID,
+		controls book page ID, and map layer tree ID in a dictionary
+		"""
 		global ctrl_dict
 		ctrl_dict[idx]=[disp, page, tree]
 		return ctrl_dict
 
 	def GetCtrls(self, idx, num):
+		"""
+		Returns widget ID for map display (num=0), controls book page
+		(num=1), or map layer tree (num=2) for a given map display
+		index (idx)
+		"""
 		global ctrl_dict
 		ctrls = ctrl_dict[idx][num]
 		return ctrls
 
-	# store and retrieve index of display with focus
-#	def SetDisp_idx(self, idx):
-#		global disp_idx
-#		disp_idx = idx
-#		return disp_idx
+	def popCtrl(self, idx):
+		"""
+		Removes entry from display and control dictionary.
+		Used when display is closed
+		"""
+		global ctrl_dict
+		if ctrl_dict != "":
+			ctrl_dict.pop(idx)
 
 	def GetDisp_idx(self, ctrl_id):
+		"""
+		Returns the display index for the display/controls dictionary entry
+		given the widget ID of the control (ctrl_id)
+		"""
 		global ctrl_dict
-		for di,ctrl in ctrl_dict.items():
+		for idx,ctrl in ctrl_dict.items():
 			if ctrl_id in ctrl:
-				return di
+				return idx
 
-	#store and retrieve current display with focus
 	def SetDisp(self, disp_idx, disp_id):
+		"""
+		Creates a tuple of the currently active display index and its corresponding
+		map display frame ID.
+		"""
 		global curr_disp
 		curr_disp = (disp_idx, disp_id)
 		return curr_disp
 
 	def GetDisp(self):
+		"""
+		Returns the (display index, display ID) tuple of the currently
+		active display
+		"""
 		global curr_disp
 		return curr_disp
-
-	# store and retrieve choicebook widget that hold all GIS manager controls
-#	def SetChbk(self, cb):
-#		gm_cb = cb
-#		return gm_cb
-#
-#	def GetChbk(self):
-#		return gm_cb
-
-	#store and retrieve display index for choicebook page
-#	def SetCB_idx(self, page, disp_idx):
-#		cb_page[page] = disp_idx
-#		return cb_page[page]
-#
-#	def GetCB_idx(self, page):
-#		return cb_page[page]
-
-	#store and retrieve page number of choicebook page
-#	def SetCB_page(self, page , pgnum):
-#		cb_pgnum[page] = pgnum
-#		return cb_pgnum[page]
-#
-#	def GetCB_page(self, page):
-#		return cb_pgnum[page]
-#
-#	def SetDispCtrl(self, disp_idx, disp, chbk):
-#		disp_ctrl[disp_idx] = (disp, chbk)
-#		return disp_ctrl[disp_idx]
-#
-#	def GetDispCtrl(self, disp_idx):
-#		return disp_ctrl[disp_idx]
-
-	#store and retriev notebook with layer tree and console for display with focus
-#	def SetNB(self, disp_idx, notebk):
-#		nb[disp_idx] = notebk
-#		return nb[disp_idx]
-#
-#	def GetNB(self, disp_idx):
-#		return nb[disp_idx]
-
-	#store and retriev notebook with layer tree and console for display with focus
-#	def SetTree(self, disp_idx, tree):
-#		layertree[disp_idx] = tree
-#		return layertree[disp_idx]
-#
-#	def GetTree(self, disp_idx):
-#		return layertree[disp_idx]
 
