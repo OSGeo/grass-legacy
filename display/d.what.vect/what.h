447a3994d30d7aefe8112f3403d9a577bff431ca
Glynn Clements <glynn@gclements.plus.com>
2007-03-15 19:35:28 +0000

    Eliminate use of form library
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23011 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.what.vect/what.h
+++ display/d.what.vect/what.h
@@ -1,13 +1,15 @@
 /* openvect.c */
 char *openvect(char *);
 /* what.c */
-int what(int, int, int, int, int, int, int, int);
+int what(int, int, int, int, int, int);
 int show_buttons(int,int);
 /*flash.c*/
 void flash_area(struct Map_info *, plus_t, struct line_pnts *, int);
 void flash_line(struct Map_info *, plus_t, struct line_pnts *, int);
 /* attr.c */
 int disp_attr(char *, char *, char *, char *, int);
+/* generate.c */
+int generate(char *, char *, char *, char *, int);
 
 #ifdef MAIN
 #define GLOBAL
