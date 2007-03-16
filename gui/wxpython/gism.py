5a8b2662923b6e87474a64e6c47b7363bdaabc71
Martin Landa <landa.martin@gmail.com>
2007-03-16 15:59:09 +0000

    syntax cosmetics, no code improving
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28590 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gism.py
+++ gui/wxpython/gism.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+"""
+Classes:
+* GRasterDialog
+* GMFrame
+* SetVal
+* GMApp
+"""
 import sys
 import os
 import wx
@@ -45,10 +52,7 @@ panels and layer tree done and demo tree items appear"""
 #
 ##########################################################################
 
-
-
 menucmd = {}
-
 
 class GRasterDialog(wx.Frame):
     def __init__(self,parent,id=-1,title="Set raster layer"):
@@ -95,7 +99,7 @@ class GRasterDialog(wx.Frame):
 class GMFrame(wx.Frame):
     '''GIS Manager frame with notebook widget for controlling
     GRASS GIS. Includes command console page for typing GRASS
-    (and other) commands, tree widget page for managing GIS map layers,'''
+    (and other) commands, tree widget page for managing GIS map layers.'''
     def __init__(self, parent, id, title):
         self.parent = parent
         wx.Frame.__init__(self, parent=parent, id=-1, title=title, style=wx.DEFAULT_FRAME_STYLE)
@@ -112,9 +116,7 @@ class GMFrame(wx.Frame):
         # do layout
         self.SetTitle(_("GRASS GIS Manager - wxPython Prototype"))
         self.SetMinSize((450, 450))
-        #self.SetMinSize((100, 100))
         # self.nb_panel = wx.Panel(self)
-
 
         # initialize variables
         self.mapdisplays = {} #dictionary to index open map displays
@@ -124,22 +126,24 @@ class GMFrame(wx.Frame):
 
         self.Bind(wx.EVT_CLOSE, self.onCloseWindow)
 
-        #item, proportion, flag, border, userData)
-        self.sizer.Add(self.notebook,1, wx.EXPAND,  1)
-        self.sizer.Add(self.cmdinput,0, wx.EXPAND, 1)
+        # item, proportion, flag, border, userData
+        self.sizer.Add(self.notebook, proportion=1, flag=wx.EXPAND, border=1)
+        self.sizer.Add(self.cmdinput, proportion=0, flag=wx.EXPAND, border=1)
         self.SetSizer(self.sizer)
         self.sizer.Fit(self)
         self.Layout()
         wx.CallAfter(self.notebook.SetSelection, 0)
 
-        #start default initial display
+        # start default initial display
         self.newDisplay()
 
     def __createCommandInput(self):
+        """Creates command input area"""
         #l = wx.StaticText(self, -1, "GRASS> ")
 
-        self.cmdinput = wx.TextCtrl(self, -1, "", style=wx.HSCROLL|wx.TE_LINEWRAP|
-                                                   wx.TE_PROCESS_ENTER)
+        self.cmdinput = wx.TextCtrl(self, id=wx.ID_ANY, value="", style=wx.HSCROLL|wx.TE_LINEWRAP|
+                                    wx.TE_PROCESS_ENTER)
+        
         #self.cmdinput.SetFont(wx.Font(10, wx.DEFAULT, wx.NORMAL, wx.NORMAL, 0, "Monospace"))
         wx.CallAfter(self.cmdinput.SetInsertionPoint, 0)
 
@@ -151,6 +155,8 @@ class GMFrame(wx.Frame):
         return self.cmdinput
 
     def __createMenuBar(self):
+        """Creates menubar"""
+        
         self.menubar = wx.MenuBar()
         menud = menudata.Data()
         for eachMenuData in menud.GetMenu():
@@ -163,6 +169,8 @@ class GMFrame(wx.Frame):
         return self.menubar
 
     def __createMenu(self, menuData):
+        """Cretes menu"""
+        
         menu = wx.Menu()
         for eachItem in menuData:
             if len(eachItem) == 2:
@@ -174,6 +182,8 @@ class GMFrame(wx.Frame):
         return menu
 
     def __createMenuItem(self, menu, label, help, handler, gcmd, kind=wx.ITEM_NORMAL):
+        """Creates menu items"""
+        
         if not label:
             menu.AppendSeparator()
             return
@@ -184,33 +194,35 @@ class GMFrame(wx.Frame):
         self.Bind(wx.EVT_MENU, rhandler, menuItem)
 
     def __createNoteBook(self):
-        #create main notebook widget
-#        bookStyle=FN.FNB_DEFAULT_STYLE #| FN.FNB_FANCY_TABS
-#        bookStyle=FN.FNB_DEFAULT_STYLE|FN.FNB_BOTTOM|FN.FNB_NO_X_BUTTON|FN.FNB_NO_NAV_BUTTONS
+        """Creates notebook widgets"""
+        
+        # create main notebook widget
+        #bookStyle=FN.FNB_DEFAULT_STYLE #| FN.FNB_FANCY_TABS
+        #bookStyle=FN.FNB_DEFAULT_STYLE|FN.FNB_BOTTOM|FN.FNB_NO_X_BUTTON|FN.FNB_NO_NAV_BUTTONS
         nbStyle=FN.FNB_FANCY_TABS|FN.FNB_BOTTOM|FN.FNB_NO_X_BUTTON|FN.FNB_NO_NAV_BUTTONS
-        self.notebook = FN.FlatNotebook(self, wx.ID_ANY, style=nbStyle)
+        self.notebook = FN.FlatNotebook(self, id=wx.ID_ANY, style=nbStyle)
 
-         #create displays notebook widget and add it to main notebook page
+        # create displays notebook widget and add it to main notebook page
         cbStyle=FN.FNB_VC8|FN.FNB_BACKGROUND_GRADIENT|FN.FNB_X_ON_TAB|FN.FNB_TABS_BORDER_SIMPLE
-#        self.cb_panel = wx.Panel(self,-1, style = wx.EXPAND)
-        self.gm_cb = FN.FlatNotebook(self, wx.ID_ANY, style=cbStyle)
+        #self.cb_panel = wx.Panel(self,-1, style = wx.EXPAND)
+        self.gm_cb = FN.FlatNotebook(self, id=wx.ID_ANY, style=cbStyle)
         self.gm_cb.SetTabAreaColour(wx.Colour(125,200,175))
-        self.notebook.AddPage(self.gm_cb, "Map layers for each display")
+        self.notebook.AddPage(self.gm_cb, text="Map layers for each display")
 
-        #create command output text area and add it to main notebook page
-#        self.outpanel = wx.Panel(self,-1, style = wx.EXPAND)
+        # create command output text area and add it to main notebook page
+        #self.outpanel = wx.Panel(self,-1, style = wx.EXPAND)
         self.goutput = gismutils.GMConsole(self)
-#        self.goutput =  wx.richtext.RichTextCtrl(self,style=wx.VSCROLL|wx.HSCROLL|wx.NO_BORDER)
-        self.outpage = self.notebook.AddPage(self.goutput, "Command output")
+        #self.goutput =  wx.richtext.RichTextCtrl(self,style=wx.VSCROLL|wx.HSCROLL|wx.NO_BORDER)
+        self.outpage = self.notebook.AddPage(self.goutput, text="Command output")
 
         self.Bind(FN.EVT_FLATNOTEBOOK_PAGE_CHANGED, self.onCBPageChanged, self.gm_cb)
-        self.Bind(FN.EVT_FLATNOTEBOOK_PAGE_CLOSING, self.onCBPageClosed, self.gm_cb)
+        self.Bind(FN.EVT_FLATNOTEBOOK_PAGE_CLOSING, self.onCBPageClosed,  self.gm_cb)
 
         self.out_sizer = wx.BoxSizer(wx.VERTICAL)
-        self.out_sizer.Add(self.goutput,1, wx.EXPAND,  1)
+        self.out_sizer.Add(self.goutput, proportion=1, flag=wx.EXPAND, border=1)
         self.SetSizer(self.out_sizer)
-#        self.out_sizer.Fit(self.outpage)
-#        self.outpage.Layout()
+        #self.out_sizer.Fit(self.outpage)
+        #self.outpage.Layout()
 
         self.Centre()
         return self.notebook
@@ -218,12 +230,14 @@ class GMFrame(wx.Frame):
 
     # choicebook methods
     def onCBPageChanged(self, event):
+        """Page in notebook changed"""
+        
         old_pgnum = event.GetOldSelection()
         new_pgnum = event.GetSelection()
-        curr_pg = self.gm_cb.GetCurrentPage()
+        curr_pg   = self.gm_cb.GetCurrentPage()
         sel_pgnum = self.gm_cb.GetSelection()
 
-         #get ID of associated display if more than one
+        # get ID of associated display if more than one
         disp_idx = track.Track().GetDisp_idx(curr_pg)
         if disp_idx != None:
             #get associated display and make it active
@@ -233,6 +247,8 @@ class GMFrame(wx.Frame):
         event.Skip()
 
     def onCBPageClosed(self, event):
+        """Page of notebook closed"""
+        
         curr_pg = self.gm_cb.GetCurrentPage()
         disp_idx = track.Track().GetDisp_idx(curr_pg)
         if disp_idx != None:
@@ -246,16 +262,18 @@ class GMFrame(wx.Frame):
                 pass
 
     def runCmd(self,event):
+        """Run command"""
+        
         #global gmpath
         print 'the command = ',self.cmdinput.GetValue()
         cmd = self.cmdinput.GetLineText(0)
-
 
         self.goutput.runCmd(cmd)
         #menuform.GUI().parseCommand(cmd, gmpath)
 
     def runMenuCmd(self, event):
-        '''Run menu command'''
+        """Run menu command"""
+        
         menuitem = self.menubar.FindItemById(event.GetId())
         itemtext = menuitem.GetText()
         cmd = menucmd[itemtext]
@@ -270,12 +288,16 @@ class GMFrame(wx.Frame):
 #        return ctrltree
 
     def __createToolBar(self):
+        """Creates toolbar"""
+        
         toolbar = self.CreateToolBar()
         for each in self.toolbarData():
             self.addToolbarButton(toolbar, *each)
         toolbar.Realize()
 
     def addToolbarButton(self, toolbar, label, iconfile, help, handler):
+        """Adds button to the given toolbar"""
+        
         if not label:
             toolbar.AddSeparator()
             return
@@ -284,48 +306,49 @@ class GMFrame(wx.Frame):
         self.Bind(wx.EVT_TOOL, handler, tool)
 
     def toolbarData(self):
-
+        
         return (
-            ('newdisplay', os.path.join(gismutils.icons,'gui-startmon.gif'), 'Start new display', self.newDisplay),
+            ('New display', os.path.join(gismutils.icons,'gui-startmon.gif'), 'Start new display', self.newDisplay),
             ('', '', '', ''),
-            ('addRaster', os.path.join(gismutils.icons,'element-cell.gif'), 'Add raster layer', self.addRaster),
-            ('addvect', os.path.join(gismutils.icons,'element-vector.gif'), 'Add vector layer', self.addVector),
-            ('addcmd', os.path.join(gismutils.icons,'gui-cmd.gif'), 'Add command layer', self.addCommand)
+            ('Add raster layer', os.path.join(gismutils.icons,'element-cell.gif'), 'Add raster layer', self.addRaster),
+            ('Add vector layer', os.path.join(gismutils.icons,'element-vector.gif'), 'Add vector layer', self.addVector),
+            ('Add command layer', os.path.join(gismutils.icons,'gui-cmd.gif'), 'Add command layer', self.addCommand)
             )
 
-    #---Start display---#000000#FFFFFF----------------------------------------------
     def newDisplay(self, event=None):
-        '''Create new map display widget'''
+        """Create new map display frame"""
 
         newdisp = self.mapdisplays[self.disp_idx] = mapdisp.MapFrame(self,
-                   -1, pos=wx.DefaultPosition, size=wx.DefaultSize,
-                   style=wx.DEFAULT_FRAME_STYLE,
-                   cb=self.gm_cb, idx=self.disp_idx)
-        newdisp.SetTitle(_("Map Display-"+str(self.disp_idx)))
+                                                                     id=wx.ID_ANY, pos=wx.DefaultPosition, size=wx.DefaultSize,
+                                                                     style=wx.DEFAULT_FRAME_STYLE,
+                                                                     cb=self.gm_cb, idx=self.disp_idx)
+        # title
+        newdisp.SetTitle(_("Map Display " + str(self.disp_idx)))
         #self.maptree[self.disp_idx] = self.mapdisplays[self.disp_idx].getTree()
 
         #add notebook page to GIS Manager
 
-       # make a new page in the bookcontrol for the layer tree (on page 0 of the notebook)
-        self.pg_panel = wx.Panel(self.gm_cb,-1, style= wx.EXPAND)
-        self.gm_cb.AddPage(self.pg_panel, "Display "+str(self.disp_idx), select = True)
+        # make a new page in the bookcontrol for the layer tree (on page 0 of the notebook)
+        self.pg_panel = wx.Panel(self.gm_cb, id=wx.ID_ANY, style= wx.EXPAND)
+        self.gm_cb.AddPage(self.pg_panel, text="Display "+ str(self.disp_idx), select = True)
         self.cb_page = self.gm_cb.GetCurrentPage()
 
-       #create layer tree (tree control for managing GIS layers)  and put on new notebook page
-        self.maptree = gismutils.LayerTree(self.cb_page, -1, wx.DefaultPosition, wx.DefaultSize, wx.TR_HAS_BUTTONS
-            |wx.TR_LINES_AT_ROOT|wx.TR_EDIT_LABELS|wx.TR_HIDE_ROOT
-            |wx.TR_DEFAULT_STYLE|wx.NO_BORDER|wx.FULL_REPAINT_ON_RESIZE)
+        # create layer tree (tree control for managing GIS layers)  and put on new notebook page
+        self.maptree = gismutils.LayerTree(self.cb_page, id=wx.ID_ANY, pos=wx.DefaultPosition,
+                                           size=wx.DefaultSize, ctstyle=wx.TR_HAS_BUTTONS
+                                           |wx.TR_LINES_AT_ROOT|wx.TR_EDIT_LABELS|wx.TR_HIDE_ROOT
+                                           |wx.TR_DEFAULT_STYLE|wx.NO_BORDER|wx.FULL_REPAINT_ON_RESIZE)
 
-        #layout for controls
+        # layout for controls
         cb_boxsizer = wx.BoxSizer(wx.VERTICAL)
-        cb_boxsizer.Add(self.maptree, 1, wx.EXPAND, 1)
+        cb_boxsizer.Add(self.maptree, proportion=1, flag=wx.EXPAND, border=1)
         self.cb_page.SetSizer(cb_boxsizer)
         cb_boxsizer.Fit(self.cb_page)
         self.cb_page.Layout()
-#        self.cb_page.SetAutoLayout(True)
-#        self.Centre()
+        #self.cb_page.SetAutoLayout(True)
+        #self.Centre()
 
-        #store information about display and associated controls in a dictionary in track.py
+        # store information about display and associated controls in a dictionary in track.py
         track.Track().SetDisp(self.disp_idx,self)
         track.Track().SetCtrlDict(self.disp_idx, newdisp, self.cb_page, self.maptree)
 
@@ -336,16 +359,19 @@ class GMFrame(wx.Frame):
 
         self.disp_idx += 1
 
-    #ToolBar button handlers
+    # toolBar button handlers
     def addRaster(self, event):
+        """Add raster layer"""
         self.SetTree('rast')
         event.Skip()
 
     def addVector(self, event):
+        """Add vector layer"""
         self.SetTree('vect')
         event.Skip()
 
     def addCommand(self, event):
+        """Add command line layer"""
         self.SetTree('cmd')
         event.Skip()
 
@@ -383,32 +409,38 @@ class GMFrame(wx.Frame):
         print 'self.disp_idx is now', self.disp_idx
 
 class SetVal:
-	'''Class to store and set values needed by map, gism,
-	and other modules. This should work but doesn't for some reason.'''
+    """
+    Class to store and set values needed by map, gism,
+    and other modules. This should work but doesn't for some reason.
+    """
 
-	def setMdFocus(self, mdnum=-1):
-		#get the id number of map display that has the focus
-		#and use it to set md
-		global mdfocus
-		if mdnum > -1:
-			mdfocus = mdnum
-		else:
-			return mdfocus
-
+    def setMdFocus(self, mdnum=-1):
+        #get the id number of map display that has the focus
+        #and use it to set md
+        global mdfocus
+        if mdnum > -1:
+            mdfocus = mdnum
+        else:
+            return mdfocus
+        
 	def getMdFocus(self):
-		global mdfocus
-		return mdfocus
-
-
+            global mdfocus
+            return mdfocus
+        
 class GMApp(wx.App):
-	def OnInit(self):
+    """
+    GMApp class
+    """
+    def OnInit(self):
 ##	  reexec_with_pythonw()
-		wx.InitAllImageHandlers()
-		mainframe = GMFrame(None, -1, "")
-		self.SetTopWindow(mainframe)
-		mainframe.Show()
-		return 1
-
+        # initialize all available image handlers
+        wx.InitAllImageHandlers()
+        # create and show main frame
+        mainframe = GMFrame(None, -1, "")
+        self.SetTopWindow(mainframe)
+        mainframe.Show()
+        return 1
+    
 def reexec_with_pythonw():
 	if sys.platform == 'darwin' and\
 		not sys.executable.endswith('MacOS/Python'):
@@ -424,11 +456,3 @@ if __name__ == "__main__":
 	gettext.install("GMApp") # replace with the appropriate catalog name
 	app = GMApp(0)
 	app.MainLoop()
-
-
-
-
-
-
-
-
