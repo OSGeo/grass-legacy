d1bb053e5466b78e0dd8060b39a9456ef767c252
Michael Barton <michael.barton@asu.edu>
2007-04-02 16:48:53 +0000

    update to include new images. I don't know if this is needed or not.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28813 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/images/__init__.py
+++ gui/wxpython/images/__init__.py
@@ -2,4 +2,7 @@ all = ["grass.form.gif",
        "grass.map.gif",
        "grass.smlogo.gif",
        "grasslogo_big.gif",
+       "db_open_table.png",
+       "grass_db.png",
+       "grass_sql.png",
        ]
