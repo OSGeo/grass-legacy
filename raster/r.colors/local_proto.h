56a53f6a395698bdb5d7869e823ebc531f1f4847
Glynn Clements <glynn@gclements.plus.com>
2007-04-11 16:15:06 +0000

    Add prototype for G_make_histogram_log_colors() Change G_make_histogram_log_colors() to map minimum value to black Add color_xform.c: G_histogram_eq_colors(), G_log_colors()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23403 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.colors/local_proto.h
+++ raster/r.colors/local_proto.h
@@ -23,7 +23,6 @@
 int eq_grey_colors(char *, char *, struct Colors *);
 /* log.c */
 int log_grey_colors(char *, char *, struct Colors *,  int, int);
-int G_make_histogram_log_colors(struct Colors *, struct Cell_stats *, int, int);
 /* main.c */
 int main(int, char *[]);
 int more_usage(void);
