eb76d72e2cc009fd3bb0d214b2eb51a9e64f35f7
Moritz Lennert <mlennert@club.worldonline.be>
2007-04-13 14:40:55 +0000

    include views in the list of tables
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23417 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/drivers/postgres/listtab.c
+++ db/drivers/postgres/listtab.c
@@ -7,37 +7,66 @@
 int db__driver_list_tables (dbString **tlist, int *tcount, int system)
 
 {
-    int i, nrows, ncols, tablecol, schemacol;
+    int i, nrows, trows, vrows, ncols, tablecol, tschemacol, viewcol, vschemacol;
     dbString *list;
-    PGresult *res;
+    PGresult *rest, *resv;
     char buf[1000];
 
     init_error();
     *tlist = NULL;
     *tcount = 0;
 
-    res = PQexec(pg_conn, "select * from pg_tables where tablename !~ 'pg_*' order by tablename");
+
+    /* Get table names */
+    rest = PQexec(pg_conn, "select * from pg_tables where tablename !~ 'pg_*' order by tablename");
     
-    if (!res || PQresultStatus(res) != PGRES_TUPLES_OK) {
+    if (!rest || PQresultStatus(rest) != PGRES_TUPLES_OK) {
 	append_error ( "Cannot select table names\n" );
 	append_error ( PQerrorMessage(pg_conn) );
 	report_error();
-	PQclear(res);
+	PQclear(rest);
 	return DB_FAILED;
     }
 
     /* Find table and schema col */
-    ncols = PQnfields(res);
-    schemacol = -1;
+    ncols = PQnfields(rest);
+    tschemacol = -1;
     for (i = 0; i < ncols; i++) {
-	if ( strcmp(PQfname(res, i),"tablename") == 0 )
+	if ( strcmp(PQfname(rest, i),"tablename") == 0 )
 	    tablecol = i;
 
-	if ( strcmp(PQfname(res, i),"schemaname") == 0 )
-	    schemacol = i;
+	if ( strcmp(PQfname(rest, i),"schemaname") == 0 )
+	    tschemacol = i;
     }
 
-    nrows = PQntuples(res);
+
+    /* Get view names */
+    resv = PQexec(pg_conn, "SELECT * FROM pg_views WHERE schemaname NOT IN ('pg_catalog','information_schema') AND viewname !~ '^pg_'");
+    
+    if (!resv || PQresultStatus(resv) != PGRES_TUPLES_OK) {
+	append_error ( "Cannot select view names\n" );
+	append_error ( PQerrorMessage(pg_conn) );
+	report_error();
+	PQclear(resv);
+	return DB_FAILED;
+    }
+
+    /* Find viewname and schema col */
+    ncols = PQnfields(resv);
+    vschemacol = -1;
+    for (i = 0; i < ncols; i++) {
+	if ( strcmp(PQfname(resv, i),"viewname") == 0 )
+	    viewcol = i;
+
+	if ( strcmp(PQfname(resv, i),"schemaname") == 0 )
+	    vschemacol = i;
+    }
+
+
+
+    trows = PQntuples(rest);
+    vrows = PQntuples(resv);
+    nrows = trows + vrows;
     
     list = db_alloc_string_array(nrows);
     
@@ -47,17 +76,31 @@ int db__driver_list_tables (dbString **tlist, int *tcount, int system)
 	return DB_FAILED;
     }
 
-    for (i = 0; i < nrows; i++) {
-	if ( schemacol >= 0 ) {
-	   sprintf ( buf, "%s.%s", (char *) PQgetvalue(res, i, schemacol), 
-		                   (char *) PQgetvalue(res, i, tablecol) );
+    for (i = 0; i < trows; i++) {
+	if ( tschemacol >= 0 ) {
+	   sprintf ( buf, "%s.%s", (char *) PQgetvalue(rest, i, tschemacol), 
+		                   (char *) PQgetvalue(rest, i, tablecol) );
 	} else {
-	   sprintf ( buf, "%s", (char *) PQgetvalue(res, i, tablecol) );
+	   sprintf ( buf, "%s", (char *) PQgetvalue(rest, i, tablecol) );
 	}
 	db_set_string(&list[i], buf );
     }
 
-    PQclear(res);
+    PQclear(rest);
+
+
+    for (i = 0; i < vrows; i++) {
+	if ( vschemacol >= 0 ) {
+	   sprintf ( buf, "%s.%s", (char *) PQgetvalue(resv, i, vschemacol), 
+		                   (char *) PQgetvalue(resv, i, viewcol) );
+	} else {
+	   sprintf ( buf, "%s", (char *) PQgetvalue(resv, i, viewcol) );
+	}
+	db_set_string(&list[i], buf );
+    }
+
+    PQclear(resv);
+
 
     *tlist = list;
     *tcount = nrows;
