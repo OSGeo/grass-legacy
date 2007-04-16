0452ce7228d51a7429569a8bf252e8db95d9bed2
Glynn Clements <glynn@gclements.plus.com>
2007-04-16 17:20:48 +0000

    Remove unused code
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23453 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/display/scan_float.c
+++
@@ -1,9 +0,0 @@
-#include <grass/display.h>
-
-int D_scan_float ( char *buf, float *f)
-{
-    char dummy[2];
-
-    *dummy = 0;
-    return sscanf (buf, "%f%1s", f, dummy) == 1 && *dummy == 0 ;
-}
--- lib/display/scan_int.c
+++
@@ -1,9 +0,0 @@
-#include <grass/display.h>
-
-int D_scan_int ( char *buf, int *f)
-{
-    char dummy[2];
-
-    *dummy = 0;
-    return sscanf (buf, "%d%1s", f, dummy) == 1 && *dummy == 0 ;
-}
