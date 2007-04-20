1431bb9c24884b3a981f70310fc932f2542c904a
Martin Landa <landa.martin@gmail.com>
2007-04-20 16:21:16 +0000

    silk icon theme created
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28933 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ gui/wxpython/icons/silk/__init__.py
@@ -0,0 +1,19 @@
+"""
+Silk icon set, v1.3
+http://www.famfamfam.com/lab/icons/silk/
+
+"""
+__author__ = "Mark James"
+
+IconsSilk = {
+    # map display
+    # gis manager
+    "newdisplay" : 'application_add.png',
+    "addrast"    : 'image_add.png',
+    "addvect"    : 'map_add.png',
+    "addcmd"     : 'cog_add.png',
+    "addgrp"     : 'folder_add.png',
+    "addovl"     : 'images.png',
+    "delcmd"     : 'cross.png',
+    "attributetable" : 'table_add.png'
+    }
