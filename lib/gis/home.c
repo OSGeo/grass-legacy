78d7bcb895dec05ec5de6aeccb7787a36586c01c
Glynn Clements <glynn@gclements.plus.com>
2007-04-21 05:29:11 +0000

    Remove bogus Unix implementation - use getenv("HOME")
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23508 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/home.c
+++ lib/gis/home.c
@@ -32,38 +32,27 @@
 char *
 G_home (void)
 {
-    char *home;
-    
-/*!
- * \brief user's home directory
- *
- * Returns a pointer to a string
- * which is the full path name of the user's home directory.
- *
- *  \param ~
- *  \return char * 
- */
+    char *home = G__home();
 
-char *G_home(void);
-
-    if ((home = G__home()))
+    if (home)
 	return home;
     
-    G_fatal_error (_("unable to determine user's home directory"));
-    exit(EXIT_FAILURE);
+    G_fatal_error(_("unable to determine user's home directory"));
+    return NULL;
 }
 
 char *
 G__home (void)
 {
     static char *home = 0;
-    char buf[GPATH_MAX];
 
     if (home)
         return home;
 
 #ifdef __MINGW32__
     { 
+	char buf[GPATH_MAX];
+
 	/* TODO: we should probably check if the dir exists */
 	home = getenv ( "USERPROFILE" ) ;
 
@@ -80,20 +69,7 @@ G__home (void)
 	    home = getenv ( "HOME" ) ;
     }
 #else
-    {
-	FILE *fd;
-
-/* first call must get home
-* execute the command "cd; pwd" and read the
-* output to get the home directory
-*/
-	if((fd = G_popen ("cd; pwd","r")))
-	{
-	    if (fscanf (fd,"%s", buf) == 1)
-		home = G_store (buf);
-	    G_pclose (fd);
-	}
-    }
+    home = getenv("HOME");
 #endif
     G_debug (2, "G__home home = %s", home );
     return home;
