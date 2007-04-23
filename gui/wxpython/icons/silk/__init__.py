f2c5dae8295f6997e739662c8fcba29350bc66b5
Martin Landa <landa.martin@gmail.com>
2007-04-23 11:39:01 +0000

    more silk icons for map display
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28952 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/icons/silk/__init__.py
+++ gui/wxpython/icons/silk/__init__.py
@@ -4,9 +4,21 @@ http://www.famfamfam.com/lab/icons/silk/
 
 """
 __author__ = "Mark James"
+__version__ = "1.3"
 
 IconsSilk = {
     # map display
+    "displaymap" : 'application.png',
+    "rendermap"  : 'application_lightning.png',
+    "erase"      : 'cross.png',
+    "pointer"    : 'cursor.png',
+    "zoom_in"    : 'zoom_in.png',
+    "zoom_out"   : 'zoom_out.png',
+    "zoom_back"  : 'zoom_back.png',
+    "zoommenu"   : 'zoom.png',
+    "query"      : 'information.png',
+    "savefile"   : 'picture_save.png',
+    "printmap"   : 'printer.png',
     # gis manager
     "newdisplay" : 'application_add.png',
     "addrast"    : 'image_add.png',
