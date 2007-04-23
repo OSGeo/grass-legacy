72f3b781c80c19d3206e558f4ef82839441715cf
Martin Landa <landa.martin@gmail.com>
2007-04-23 17:24:22 +0000

    icons update (more silk icons)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@28953 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/icons/icon.py
+++ gui/wxpython/icons/icon.py
@@ -36,17 +36,23 @@ icons_default = {
     "zoom_back"  : 'gui-zoom_back.gif', 
     "zoommenu"   : 'gui-mapzoom.gif', 
     "dec"        : 'module-d.barscale.gif', 
-    "savefile"   : 'gui-file-save.gif', 
-    "printmap"   : None, 
+    "savefile"   : wx.ART_FILE_SAVE, 
+    "printmap"   : wx.ART_PRINT,
+    # digit
+    "addvpoint"  : wx.ART_ERROR,
+    "addvline"   : wx.ART_ERROR,
+    "addvbound"  : wx.ART_ERROR,
+    "addvcentr"  : wx.ART_ERROR,
+    "exit"       : wx.ART_ERROR,
     # gis manager
     "newdisplay" : 'gui-startmon.gif', 
     "addrast"    : 'element-cell.gif', 
     "addvect"    : 'element-vector.gif', 
     "addcmd"     : 'gui-cmd.gif', 
-    "addgrp"     : None, 
+    "addgrp"     : wx.ART_ERROR, 
     "addovl"     : 'module-d.grid.gif', 
-    "delcmd"     : None, 
-    "attributetable" : None,
+    "delcmd"     : wx.ART_ERROR, 
+    "attrtable"  : wx.ART_ERROR,
     "addrgb"     : 'module-d.rgb.gif',
     "addhis"     : 'channel-his.gif',
     "addlegend"  : 'module-d.legend.gif',
@@ -58,34 +64,38 @@ icons_default = {
     }
 
 # merge icons dictionaries, join paths
-if iconpath and iconpath.find('silk') > -1:
+if iconpath and iconpath.find('silk') > -1: # silk icon theme
     from silk import IconsSilk as icons_img
     # use default icons if needed
     for key, img in icons_default.iteritems():
         if not icons_img.has_key(key): # add key 
-            if icons_default[key]:
-                icons_img[key] = os.path.join(iconpath_default, icons_default[key])
-            else:
-                icons_img[key] = None
-        elif icons_img[key]: 
-            icons_img[key] = os.path.join(iconpath, icons_img[key])
-        elif img:
-            icons_img[key] = os.path.join(iconpath_default, icons_default[key])
+            icons_img[key] = img
+            iconpath_tmp = iconpath_default
+        else:
+            iconpath_tmp = iconpath
+
+        if icons_img[key]: # join paths
+            if type (icons_img[key]) == type(''):
+                icons_img[key] = os.path.join(iconpath_tmp, icons_img[key])
 else: # default icons
     icons_img = icons_default
-    for key, path in icons_img.iteritems():
-        if path:
-            icons_img[key] = os.path.join(iconpath_default, path)
+    for key, img in icons_img.iteritems():
+        if img and type (icons_img[key]) == type(''):
+            icons_img[key] = os.path.join(iconpath_default, img)
 
 class MetaIcon:
     """
     Handle icon metadata (image path, tooltip, ...)
     """
     def __init__(self, img, label, desc=None):
-        self.imagepath = None
-        if img:
-            if os.path.isfile(img) and os.path.getsize(img):
-                self.imagepath = img
+        self.imagepath = img
+        if not self.imagepath:
+            self.type = 'unknown'
+        else:
+            if self.imagepath.find ('wxART_') > -1:
+                self.type = 'wx'
+            else:
+                self.type = 'img'
 
         self.label = label
 
@@ -96,17 +106,23 @@ class MetaIcon:
 
     def __str__(self):
         """Debugging"""
-        return "label=%s, img=%s" % (self.label, self.imagepath)
+        return "label=%s, img=%s, type=%s" % (self.label, self.imagepath, self.type)
 
     def GetBitmap (self, size=None):
-        if size and len(size) == 2:
-            image = wx.Image (name=self.imagepath)
-            image.Rescale (size[0], size[1])
-            bmp = image.ConvertToBitmap()
-        elif self.imagepath:
-            bmp = wx.Bitmap (name=self.imagepath)
-        else:
-            bmp=None
+        """Get bitmap"""
+        bmp = None
+        
+        if self.type == 'wx':
+            bmp = wx.ArtProvider.GetBitmap(id=self.imagepath, client=wx.ART_TOOLBAR, size=size)
+        elif self.type == 'img':
+            if os.path.isfile(self.imagepath) and os.path.getsize(self.imagepath):
+                if size and len(size) == 2:
+                    image = wx.Image (name=self.imagepath)
+                    image.Rescale (size[0], size[1])
+                    bmp = image.ConvertToBitmap()
+                elif self.imagepath:
+                    bmp = wx.Bitmap (name=self.imagepath)
+
         return bmp
 
     def GetLabel (self):
@@ -141,17 +157,25 @@ Icons = {
     "addgrp"     : MetaIcon (img=icons_img["addgrp"], label="Add layer group"),
     "addovl"     : MetaIcon (img=icons_img["addovl"], label="Add grid or vector labels overlay"),
     "delcmd"     : MetaIcon (img=icons_img["delcmd"], label="Delete selected layer"),
-    "attributetable" : MetaIcon (img=icons_img["attributetable"], label="Show attribute table"),
-    "addrgb"    : MetaIcon  (img=icons_img["addrgb"], label="Add RGB layer"),
+    "attrtable"  : MetaIcon (img=icons_img["attrtable"], label="Show attribute table"),
+    "addrgb"     : MetaIcon (img=icons_img["addrgb"], label="Add RGB layer"),
     "addhis"     : MetaIcon (img=icons_img["addhis"], label="Add HIS layer"),
     "addlegend"  : MetaIcon (img=icons_img["addlegend"], label="Add legend"),
     "elvect"     : MetaIcon (img=icons_img["elvect"], label=""),
     "addthematic": MetaIcon (img=icons_img["addthematic"], label="Add thematic layer"),
     "addchart"   : MetaIcon (img=icons_img["addchart"], label="Add thematic chart layer"),
     "addgrid"    : MetaIcon (img=icons_img["addgrid"], label="Add grid layer"),
-    "addlabels"  : MetaIcon (img=icons_img["addlabels"], label="Add labels")}
+    "addlabels"  : MetaIcon (img=icons_img["addlabels"], label="Add labels"),
+    # digit
+    "addvpoint"  : MetaIcon (img=icons_img["addvpoint"], label="Digitize new point"),
+    "addvline"   : MetaIcon (img=icons_img["addvline"], label="Digitize new line"),
+    "addvbound"  : MetaIcon (img=icons_img["addvbound"], label="Digitize new boundary"),
+    "addvcentr"  : MetaIcon (img=icons_img["addvcentr"], label="Digitize new centroid"),
+    "exit"       : MetaIcon (img=icons_img["exit"], label="Quit digitization tool")}
 
 # testing ...
 if __name__ == "__main__":
     for k,v in Icons.iteritems():
         print k, "/", v
+
+    print icons_default["savefile"]
