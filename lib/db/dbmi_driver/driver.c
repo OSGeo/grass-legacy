abf22dbc68af40da607eb130fb07bd183fa46e51
Scott Mitchell <smitch@me.com>
2007-04-26 17:18:00 +0000

    provenance review - add standard header
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23583 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/db/dbmi_driver/driver.c
+++ lib/db/dbmi_driver/driver.c
@@ -1,3 +1,22 @@
+/****************************************************************************
+ *
+ * MODULE:       DBMI library - drivers
+ * AUTHOR(S):    Joel Jones (CERL/UIUC) <jjones zorro.cecer.army.mil>
+ *               (overall original contributor of db libraries)
+ *               This file (and driver directory) originates with GRASS 5.0:
+ *               Radim Blazek <radim.blazek gmail.com> (original contributor)
+ *               Glynn Clements <glynn gclements.plus.com>,
+ *               Markus Neteler <neteler itc.it>,
+ *               Huidae Cho <grass4u gmail.com>,
+ * PURPOSE:      database management driver functions 
+ * COPYRIGHT:    (C) 2003-2006 by the GRASS Development Team
+ *
+ *               This program is free software under the GNU General Public
+ *               License (>=v2). Read the file COPYING that comes with GRASS
+ *               for details.
+ *
+ *****************************************************************************/
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdlib.h>
