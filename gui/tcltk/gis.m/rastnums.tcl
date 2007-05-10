561beb676d8248d8e84c59eb20092f7fca4a1bdf
Hamish Bowman <hamish_b@yahoo.com>
2007-05-10 15:08:24 +0000

    add missing icon button press binding (last one for sure)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23789 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/tcltk/gis.m/rastnums.tcl
+++ gui/tcltk/gis.m/rastnums.tcl
@@ -281,7 +281,9 @@ proc GmRnums::duplicate { tree parent node id } {
 
     image create photo nico -file "$iconpath/module-d.rast.num.gif"
     set ico [label $frm.ico -image nico -bd 1 -relief raised]
-    
+
+    bind $ico <ButtonPress-1> "GmTree::selectn $tree $node"
+
     pack $check $ico -side left
 
 	# where to insert new layer
