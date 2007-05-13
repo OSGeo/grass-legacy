c438600f7f166d94eef4e144f94587b5bdb77214
Hamish Bowman <hamish_b@yahoo.com>
2007-05-13 15:39:49 +0000

    use D_polygon() instead of G_plot_polygon() so PS driver output looks nice
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23817 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.vect.chart/bar.c
+++ display/d.vect.chart/bar.c
@@ -7,7 +7,8 @@
 #include "global.h"
 
 int 
-bar ( double cx, double cy, int size, double scale, double *val, int ncols, COLOR *ocolor, COLOR *colors, int y_center, double *max_reference ) 
+bar ( double cx, double cy, int size, double scale, double *val, int ncols,
+	COLOR *ocolor, COLOR *colors, int y_center, double *max_reference )
 {
     int i, j;
     double max;
@@ -27,39 +28,40 @@ bar ( double cx, double cy, int size, double scale, double *val, int ncols, COLO
     max = 0;
     for ( i = 0; i < ncols; i++ ) { 
 	if ( val[i] > max) max = val[i]; 
-    };
+    }
     
     /* debug */
     /* printf("%f \n", max_reference); */
     
-    if(y_center == 0) /* draw the columns with the bottom at the y value of the point  */
-		y0 = cy ; 
-	else
-		y0 = cy - scale * max * pixel / 2;	/* center the columns around the y value of the point */
-    
+    if(y_center == 0)
+	/* draw the columns with the bottom at the y value of the point  */
+	y0 = cy;
+    else
+	/* center the columns around the y value of the point */
+	y0 = cy - scale * max * pixel / 2;
+
     /* Left (x0) */
     x0 = cx - size * pixel / 2;
 
     bw = size * pixel / ncols;
-	
-	if(max_reference) {
-		/* Draw polygon outlining max value in dataset with no fill color*/
-		for ( i = 0; i < ncols; i++ ) {
-		Vect_reset_line ( max_Points );
-		Vect_append_point ( max_Points, x0 + i*bw, y0, 0);
-		Vect_append_point ( max_Points, x0 + (i+1)*bw, y0, 0);
-		Vect_append_point ( max_Points, x0 + (i+1)*bw, y0 + scale * max_reference[i] * pixel, 0);
-		Vect_append_point ( max_Points, x0 + i*bw, y0 + scale * max_reference[i] * pixel, 0);
-		Vect_append_point ( max_Points, x0 + i*bw, y0, 0);
-		
-		
-		/* the outline color : default is black*/
-		R_RGB_color ( ocolor->r, ocolor->g, ocolor->b );
-		for (j = 1; j < max_Points->n_points; j++ ) { 
-			G_plot_line(max_Points->x[j], max_Points->y[j], max_Points->x[j-1], max_Points->y[j-1]);
-		}
-		}
+
+    if(max_reference) {
+	/* Draw polygon outlining max value in dataset with no fill color*/
+	for ( i = 0; i < ncols; i++ ) {
+	    Vect_reset_line ( max_Points );
+	    Vect_append_point ( max_Points, x0 + i*bw, y0, 0);
+	    Vect_append_point ( max_Points, x0 + (i+1)*bw, y0, 0);
+	    Vect_append_point ( max_Points, x0 + (i+1)*bw, y0 + scale * max_reference[i] * pixel, 0);
+	    Vect_append_point ( max_Points, x0 + i*bw, y0 + scale * max_reference[i] * pixel, 0);
+	    Vect_append_point ( max_Points, x0 + i*bw, y0, 0);
+
+	    /* the outline color : default is black*/
+	    R_RGB_color ( ocolor->r, ocolor->g, ocolor->b );
+	    for (j = 1; j < max_Points->n_points; j++ ) { 
+		G_plot_line(max_Points->x[j], max_Points->y[j], max_Points->x[j-1], max_Points->y[j-1]);
+	    }
 	}
+    }
 	
     /* Draw polygon for each value */
     for ( i = 0; i < ncols; i++ ) {
@@ -72,7 +74,7 @@ bar ( double cx, double cy, int size, double scale, double *val, int ncols, COLO
 
 	if(!colors[i].none) {
 	    R_RGB_color ( colors[i].r, colors[i].g, colors[i].b );
-	    G_plot_polygon ( Points->x, Points->y, Points->n_points);
+	    D_polygon( Points->x, Points->y, Points->n_points);
 	}
 
 	R_RGB_color ( ocolor->r, ocolor->g, ocolor->b );
@@ -87,5 +89,3 @@ bar ( double cx, double cy, int size, double scale, double *val, int ncols, COLO
 
     return 0;
 }
-
-
