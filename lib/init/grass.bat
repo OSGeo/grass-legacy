3b53992d92d38435084eb5abb29fa006d2263e04
Moritz Lennert <mlennert@club.worldonline.be>
2007-05-20 23:28:52 +0000

    another fix: forgot the "set" command; also added hint about adding msys lib and bin directory to path
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@23949 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/init/grass.bat
+++ lib/init/grass.bat
@@ -9,19 +9,22 @@ rem *******Environment variables***********
 rem Uncomment and set the following values if they differ from the indicated default
 
 rem Directory where your .grassrc6 file will be stored
-rem HOME=%USERPROFILE%
+rem set HOME=%USERPROFILE%
 
 rem Name of the wish (Tk) executable
-rem GRASS_WISH=wish.exe
+rem set GRASS_WISH=wish.exe
 
-rem Path to the shell command
-rem GRASS_SH=c:\msys\1.0\bin\sh.exe
+rem Path to the shell command 
+rem set GRASS_SH=c:\msys\1.0\bin\sh.exe
+
+rem You also might want to add the shell's bin and lib directory to PATH (not done by default)
+rem set PATH=%PATH%;c:\msys\1.0\lib;\c:\msys\1.0\bin
 
 rem Path to your web browser
-rem GRASS_HTML_BROWSER=%SYSTEMDRIVE%/PROGRA~1/INTERN~1/IEXPLORE.EX
+rem set GRASS_HTML_BROWSER=%SYSTEMDRIVE%/PROGRA~1/INTERN~1/IEXPLORE.EX
 
 rem Path to the proj files (notably the epsg projection list)
-rem GRASS_PROJSHARE=/c/grass/share/proj
+rem set GRASS_PROJSHARE=/c/grass/share/proj
 
 set WINGISBASE=GISBASE_VALUE
 "%WINGISBASE%\etc\init.bat" %*
