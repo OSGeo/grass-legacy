ce5592a6044c20d6a8280ad4d226d87f5731117d
Michael Barton <michael.barton@asu.edu>
2007-06-26 04:55:25 +0000

    Update menu and rules entry to make use of new rules= option for r.reclass and r.recode.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@29166 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxgui.py
+++ gui/wxpython/wxgui.py
@@ -386,22 +386,18 @@ class GMFrame(wx.Frame):
             output = open(gtemp,"w")
             output.write(dlg.rules)
             output.close()
-            if dlg.overwrite == True:
-                overwrite = '--o'
-            else:
-                overwrite -= ''
 
             if command == 'r.colors':
                 cmdlist = [command,'map=%s' % dlg.inmap,'rules=%s' % gtemp,'--verbose']
-                cmd.Command(cmdlist)
-            elif command == 'v.reclass':
-                cmdlist = [command,'input=%s' % dlg.inmap,'output=%s' % dlg.outmap,'rules=%s' % gtemp,overwrite,'--verbose']
-                cmd.Command(cmdlist)
             else:
-                cmdlist = [command,'input=%s' % dlg.inmap,'output=%s' % dlg.outmap,overwrite,'--verbose']
-                input = open(gtemp,"r")
-                p = subprocess.Popen(cmdlist, stdin=input, stdout=subprocess.PIPE)
-                input.close()
+                cmdlist = [command,'input=%s' % dlg.inmap,'output=%s' % dlg.outmap,'rules=%s' % gtemp]
+
+            if dlg.overwrite == True:
+                cmdlist.append('--o')
+
+            cmdlist.append('--verbose')
+
+            cmd.Command(cmdlist)
 
         dlg.Destroy()
 
