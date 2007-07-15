cad33b9a8d99b18c1ff4bbd005ab202968ae62ac
Markus Neteler <neteler@gmail.com>
2007-07-15 08:26:49 +0000

    No longer maintained and scheduled for demolition
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@24417 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.bilinear/main.c
+++ raster/r.bilinear/main.c
@@ -61,7 +61,7 @@ int main( int argc, char *argv[])
     if (G_parser (argc, argv))
 	exit (EXIT_FAILURE);
 
-    G_warning(_("This module is deprecated. Please use 'r.stats.interp' instead."));
+    G_warning(_("This module is deprecated. Please use 'r.resamp.interp' instead."));
 
     o_east = o_north = 0.0;
 
