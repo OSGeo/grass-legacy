88b39a9971fe6eb7d751e4b1016c03bf91a322c6
Martin Landa <landa.martin@gmail.com>
2007-07-24 12:10:02 +0000

    New operation 'connect' added. Manual page need update.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@24492 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.net/proto.h
+++ vector/v.net/proto.h
@@ -1,8 +1,13 @@
-#define REPORT 0
-#define NREPORT 1
+#define TOOL_NODES   0
+#define TOOL_CONNECT 1
+#define TOOL_REPORT  2
+#define TOOL_NREPORT 3
+
+/* connect.c */
+int connect_arcs (struct Map_info *In, struct Map_info *Pnts, struct Map_info *Out, int nfield, double thresh);
 
 /* nodes.c */
-int nodes(char *, char *, int add_cats, int nfield );
+int nodes(struct Map_info *In, struct Map_info *Out, int add_cats, int nfield );
 
 /* report.c */
-int report(char *in, int afield, int nfield, int action);
+int report(struct Map_info *In, int afield, int nfield, int action);
