64b6b4d172486eccbfd1c552d4faf3ec439b5d6d
Brad Douglas <rez@touchofmadness.com>
2007-08-29 09:53:54 +0000

    - Added docbook documentation
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@24673 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/basename.c
+++ lib/gis/basename.c
@@ -1,23 +1,37 @@
+/**
+ * \file basename.c
+ *
+ * \brief Program basename routines.
+ *
+ * This program is free software under the GNU General Public License
+ * (>=v2). Read the file COPYING that comes with GRASS for details.
+ *
+ * \author GRASS GIS Development Team
+ *
+ * \date 1999-2007
+ */
+
 #include <ctype.h>
 #include <string.h>
 
+
 /**
- * \brief Truncates filename to the base part (before the last .)
+ * \fn char * G_basename (char *filename, const char *desired_ext)
+ *
+ * \brief Truncates filename to the base part (before the last '.')
  * if it matches the extension, otherwise leaves it unchanged.
  * 
  * Checks if a filename matches a certain file extension
  * (case insensitive) and if so, truncates the string to the
  * base file name (cf. basename Unix command)
  *
- * \param filename String containing filename
- *
- * \param ext String containing extension to look for (case
+ * \param[in] filename string containing filename
+ * \param[in] desired_ext string containing extension to look for (case
  * insensitive and  as long as needs to be, e.g. tif will
  * match .tif and .tiff, sh will match .shp and .shx, htm will
  * match .htm and .html)
- *
  * \return Pointer to filename
- **/
+ */
 
 char * G_basename(char *filename, const char *desired_ext)
 {
