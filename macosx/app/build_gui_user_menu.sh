a3f270c54d2ee1dd406fe9a3f7cb66ad7aabd101
William Kyngesburye <kyngchaos@kyngchaos.com>
2007-09-11 02:10:10 +0000

    remove outer braces on menu items
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@24755 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- macosx/app/build_gui_user_menu.sh
+++ macosx/app/build_gui_user_menu.sh
@@ -2,12 +2,15 @@
 
 # Build addon menu files, from the global /Library/GRASS/$GRASS_MMVER/Modules
 # and the user's $HOME/Library/GRASS/$GRASS_MMVER/Modules.
-# It builds a similar etc/gm/Xtns folder full of .gem menu files.
+# It builds a etc/gm/addons folder full of tcl .menu menu files.
 
 # test files to make sure they are appropriate for adding to the GUI menu.
 # Using 'file', assume executable binaries OK.  Check scripts to see if they
 # have GRASS options configured - a simple grep for #%Module.
 # Other script languages may need their own test.
+
+# addon commands can't have spaces in them or sh for loop messes up.
+# may be my limited knowledge of sh scripting and there could be a way.
 
 GRASS_MMVER=`cut -d . -f 1-2 "$GISBASE/etc/VERSIONNUMBER"`
 BINDIR="$HOME/Library/GRASS/$GRASS_MMVER/Modules/bin"
@@ -32,7 +35,7 @@ if [ "$CMDLISTG" != "" ] ; then
   do
     ftype="`file $BINDIRG/$i`"
     if [ "`echo $ftype | grep 'Mach-O'`" ] || [ "`grep '#% *Module' $BINDIRG/$i`" ] ; then
-      echo "{command {[G_msg \"\"]} {} \"$i\" {} -command {execute $i }}" > $MENUDIR/$i.menu
+      echo "command {[G_msg \"$i\"]} {} \"$i\" {} -command {execute $i }" > $MENUDIR/$i.menu
     fi
   done
 fi
@@ -46,7 +49,7 @@ if [ "$CMDLIST" != "" ] ; then
   do
     ftype="`file $BINDIR/$i`"
     if [ "`echo $ftype | grep 'Mach-O'`" ] || [ "`grep '#% *Module' $BINDIR/$i`" ] ; then
-      echo "{command {[G_msg \"\"]} {} \"$i\" {} -command {execute $i }}" > $MENUDIR/$i.menu
+      echo "command {[G_msg \"$i\"]} {} \"$i\" {} -command {execute $i }" > $MENUDIR/$i.menu
     fi
   done
 fi
