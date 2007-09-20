dc8e091676ca6ff58c26fa2a0956b705fb8071e7
Brad Douglas <rez@touchofmadness.com>
2007-09-20 00:12:38 +0000

    - Added new function: print_transform_matrix()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@24798 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/libtrans.h
+++ include/libtrans.h
@@ -1,3 +1,6 @@
+#ifndef __LIBTRANS_H__
+#define __LIBTRANS_H__
+
 /* to give the size of the multi-dim array parameter is necessary here,
    because DEC's cc V5.6-079 on Digital UNIX V4.0 (Rev. 878) seems to need it
  */
@@ -14,3 +17,6 @@ int transform_a_into_b(double, double, double *, double *);
 int transform_b_into_a(double, double, double *, double *);
 int residuals_a_predicts_b(double *, double *, double *, double *, int *, int, double *, double *);
 int residuals_b_predicts_a(double *, double *, double *, double *, int *, int, double *, double *);
+int print_transform_matrix(void);
+
+#endif /* __LIBTRANS_H__ */
