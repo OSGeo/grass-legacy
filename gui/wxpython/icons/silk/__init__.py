5217e1a6fe769f9d9df63b1b082600f2c8a3b11a
Martin Landa <landa.martin@gmail.com>
2007-09-30 20:18:26 +0000

    Add new silk icons
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@29279 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/icons/silk/__init__.py
+++ gui/wxpython/icons/silk/__init__.py
@@ -12,30 +12,61 @@ IconsSilk = {
     "rendermap"  : 'arrow_refresh.png',
     "erase"      : 'cross.png',
     "pointer"    : 'cursor.png',
+    "query"      : 'information.png',
+    "savefile"   : 'picture_save.png',
+    "printmap"   : 'printer.png',
+    "pan"        : 'mouse.png', # should be changed, not nice icon
+    # zoom (mapdisplay)
     "zoom_in"    : 'zoom_in.png',
     "zoom_out"   : 'zoom_out.png',
     "zoom_back"  : 'zoom_back.png',
     "zoommenu"   : 'zoom.png',
-    "query"      : 'information.png',
-    "savefile"   : 'picture_save.png',
-    "printmap"   : 'printer.png',
-    "dec"        : 'overlays.png',
-    "pan"        : 'mouse.png', # should be changed, not nice icon
+    # analyze raster (mapdisplay)
+    "analyze"    : 'application_lightning.png',
+    "measure"    : 'sum.png',
+    "profile"    : 'wand.png',
+    "histogram"  : 'chart_bar.png',
+    # overlay (mapdisplay)
+    "overlay"    : 'overlays.png',
+    "addtext"    : 'textfield_add.png',
+    "addbarscale": 'page_white_picture.png',
+    "addlegend"  : 'page_green.png',
     # digit
-    "digaddpoint": 'bullet_add.png',
-    "digaddline" : 'vector_add.png',
-    "digaddbound": 'plugin_add.png',
-    "digaddcentr": 'shape_square_add.png',
-    "digexit"    : 'door_in.png',
+    ## add feature
+    "digAddPoint": 'bullet_add.png',
+    "digAddLine" : 'vector_add.png',
+    "digAddBoundary": 'shape_handles.png',
+    "digAddCentroid": 'shape_square_add.png',
+    ## vertex
+    "digAddVertex" : 'chart_line_add.png',
+    "digMoveVertex" : 'chart_line.png',
+    "digRemoveVertex" : 'chart_line_delete.png',
+    "digSplitLine" : 'chart_line_link.png',
+    ## edit feature
+    "digEditLine" : 'chart_line_edit.png',
+    "digMoveLine" : 'bullet_go.png',
+    "digDeleteLine" : 'vector_delete.png',
+    ## cats
+    "digDispCats" : 'chart_organisation.png',
+    "digCopyCats" : 'chart_organisation_add.png',
+    ## attributes
+    "digDispAttr" : 'table.png',
+    ## general
+    "digSettings" : 'color_swatch.png',
+    "digExit"     : 'door_in.png',
     # gis manager
     "newdisplay" : 'application_add.png',
     "addrast"    : 'image_add.png',
+    "addshaded"  : 'picture_empty.png',
+    "addrarrow"  : 'arrow_inout.png',
     "addvect"    : 'map_add.png',
     "addcmd"     : 'cog_add.png',
     "addgrp"     : 'folder_add.png',
     "addovl"     : 'images.png',
+    "addgrid"    : 'application_view_icons.png',
+    "addlabels"  : 'tag_blue_add.png',
     "delcmd"     : 'cross.png',
-    "attrtable"  :  'application_view_columns.png',
+    "attrtable"  : 'application_view_columns.png',
     "addrgb"     : 'rgb.png',
     "addhis"     : 'his.png',
     "addthematic": 'thematic.png',
