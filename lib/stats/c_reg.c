72dc825c4ed9e716c749e9d3cb4928869906d29f
Glynn Clements <glynn@gclements.plus.com>
2007-10-23 12:11:17 +0000

    Add regression coefficient (exactly *which* coefficient, I don't know ...)
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@25036 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/stats/c_reg.c
+++ lib/stats/c_reg.c
@@ -1,10 +1,14 @@
 #include <grass/gis.h>
 
-static void regression(DCELL *result, DCELL *values, int n, int offset)
+#define REGRESSION_SLOPE	0
+#define REGRESSION_OFFSET	1
+#define REGRESSION_COEFF_DET	2
+
+static void regression(DCELL *result, DCELL *values, int n, int which)
 {
 	DCELL xsum, ysum;
 	DCELL xbar, ybar;
-	DCELL numer, denom;
+	DCELL numer, denom, denom2;
 	int count;
 	int i;
 
@@ -42,26 +46,52 @@ static void regression(DCELL *result, DCELL *values, int n, int offset)
 			denom += (DCELL) i * i;
 	denom -= count * xbar * xbar;
 
-	*result = offset
-		? (ybar - xbar * numer / denom)
-		: (numer / denom);
+	if (which == REGRESSION_COEFF_DET)
+	{
+		denom2 = 0.0;
+		for (i = 0; i < n; i++)
+			if (!G_is_d_null_value(&values[i]))
+				denom2 += values[i] * values[i];
+		denom2 -= count * ybar * ybar;
+	}
+
+	switch (which)
+	{
+	case REGRESSION_SLOPE:
+		*result = numer / denom;
+		break;
+	case REGRESSION_OFFSET:
+		*result = ybar - xbar * numer / denom;
+		break;
+	case REGRESSION_COEFF_DET:
+		*result = (numer * numer) / (denom * denom2);
+		break;
+	default:
+		G_set_d_null_value(result, 1);
+		break;
+	}
 }
 
 void c_reg_m(DCELL *result, DCELL *values, int n)
 {
-	regression(result, values, n, 0);
+	regression(result, values, n, REGRESSION_SLOPE);
 }
 
 void c_reg_c(DCELL *result, DCELL *values, int n)
 {
-	regression(result, values, n, 1);
+	regression(result, values, n, REGRESSION_OFFSET);
 }
 
-static void regression_w(DCELL *result, DCELL (*values)[2], int n, int offset)
+void c_reg_r2(DCELL *result, DCELL *values, int n)
+{
+	regression(result, values, n, REGRESSION_COEFF_DET);
+}
+
+static void regression_w(DCELL *result, DCELL (*values)[2], int n, int which)
 {
 	DCELL xsum, ysum;
 	DCELL xbar, ybar;
-	DCELL numer, denom;
+	DCELL numer, denom, denom2;
 	int count;
 	int i;
 
@@ -99,18 +129,44 @@ static void regression_w(DCELL *result, DCELL (*values)[2], int n, int offset)
 			denom += (DCELL) i * i * values[i][1];
 	denom -= count * xbar * xbar;
 
-	*result = offset
-		? (ybar - xbar * numer / denom)
-		: (numer / denom);
+	if (which == REGRESSION_COEFF_DET)
+	{
+		denom2 = 0.0;
+		for (i = 0; i < n; i++)
+			if (!G_is_d_null_value(&values[i][0]))
+				denom2 += values[i][0] * values[i][0] * values[i][1];
+		denom2 -= count * ybar * ybar;
+	}
+
+	switch (which)
+	{
+	case REGRESSION_SLOPE:
+		*result = numer / denom;
+		break;
+	case REGRESSION_OFFSET:
+		*result = ybar - xbar * numer / denom;
+		break;
+	case REGRESSION_COEFF_DET:
+		*result = (numer * numer) / (denom * denom2);
+		break;
+	default:
+		G_set_d_null_value(result, 1);
+		break;
+	}
 }
 
 void w_reg_m(DCELL *result, DCELL (*values)[2], int n)
 {
-	regression_w(result, values, n, 0);
+	regression_w(result, values, n, REGRESSION_SLOPE);
 }
 
 void w_reg_c(DCELL *result, DCELL (*values)[2], int n)
 {
-	regression_w(result, values, n, 1);
+	regression_w(result, values, n, REGRESSION_OFFSET);
+}
+
+void w_reg_r2(DCELL *result, DCELL (*values)[2], int n)
+{
+	regression_w(result, values, n, REGRESSION_COEFF_DET);
 }
 
