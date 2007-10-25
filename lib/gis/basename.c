21619d77a9345a12d41bbaacd95613c3f390ee5c
Paul Kelly <paul-grass@stjohnspoint.co.uk>
2007-10-25 15:18:12 +0000

    Change behaviour of G_basename() so it only matches when there's an exact match. Update r.out.tiff to work with new behaviour.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@25074 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/basename.c
+++ lib/gis/basename.c
@@ -11,6 +11,8 @@
  * \date 1999-2007
  */
 
+#include <grass/gis.h>
+
 #include <ctype.h>
 #include <string.h>
 
@@ -27,9 +29,7 @@
  *
  * \param[in] filename string containing filename
  * \param[in] desired_ext string containing extension to look for (case
- * insensitive and  as long as needs to be, e.g. tif will
- * match .tif and .tiff, sh will match .shp and .shx, htm will
- * match .htm and .html)
+ * insensitive)
  * \return Pointer to filename
  */
 
@@ -38,28 +38,8 @@ char * G_basename(char *filename, const char *desired_ext)
     /* Find the last . in the filename */
     char *dot = strrchr(filename, '.');
 
-    /* Check there is a . and it's not the last character
-     * in the string, i.e. there is an extension */
-    if(dot && ((dot - filename) < strlen(filename)) )
-    {
-        char *ext = dot + 1;
-        int i, match = 1;
-
-        /* if the extension matches (case insensitive)
-         * then truncate the filename to the basename */
-        for( i = 0; i < strlen(desired_ext); i++ )
-        {
-            if( (ext[i] == '\0') || (tolower(ext[i]) != tolower(desired_ext[i])) )
-            {
-                match = 0;
-                break;
-            }
-        }
-        
-        if( match )
-            *dot = '\0';
-
-    }
+    if(dot && G_strcasecmp(dot + 1, desired_ext) == 0)
+        *dot = '\0';
 
     return filename;
 }
