2429bac6c41fc4e57840d19c2bfbb0d6a40f33c3
Markus Neteler <neteler@gmail.com>
2007-11-23 10:47:12 +0000

    various fixes merged from HEAD
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_3@25320 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/cairodriver/write.c
+++ lib/cairodriver/write.c
@@ -1,5 +1,10 @@
 #include "cairodriver.h"
 
+#if CAIRO_HAS_XLIB_SURFACE
+#include <X11/Xlib.h>
+#include <cairo-xlib.h>
+#endif
+
 void write_image(void)
 {
 	G_debug(1, "write_image");
@@ -7,15 +12,37 @@ void write_image(void)
 	if (!modified)
 		return;
 
-	if (cairo && surface)
+	if (mapped)
+		return;
+
+	if (!cairo || !surface)
+		return;
+
+	if (file_type == FTYPE_PPM)
 	{
-		if (file_type == FTYPE_PNG)
-		{
-			G_debug(1, "Writing image to %s", file_name);
-			cairo_surface_write_to_png(surface, file_name);
-		}
-		/* vector format files are written directly to file */
+		G_debug(1, "Writing image to %s", file_name);
+		write_ppm();
 	}
+	else if (file_type == FTYPE_BMP)
+	{
+		G_debug(1, "Writing image to %s", file_name);
+		write_bmp();
+	}
+#if CAIRO_HAS_PNG_FUNCTIONS
+	else if (file_type == FTYPE_PNG)
+	{
+		G_debug(1, "Writing image to %s", file_name);
+		cairo_surface_write_to_png(surface, file_name);
+	}
+#endif
+#if CAIRO_HAS_XLIB_SURFACE
+	else if (file_type == FTYPE_X11)
+	{
+		XFlush(cairo_xlib_surface_get_display(surface));
+	}
+#endif
+	/* vector format files are written directly to file */
 
 	modified = 0;
 }
+
