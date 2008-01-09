8954dcfb784b5b534d87b8d468adf88f8b72b93c
Martin Landa <landa.martin@gmail.com>
2008-01-09 20:07:37 +0000

    Doxygen documentation updated
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@29642 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/key_value4.c
+++ lib/gis/key_value4.c
@@ -1,11 +1,33 @@
+/*!
+   \file key_value4.c
+
+   \brief Key_Value management.
+
+   (C) 2001-2008 by the GRASS Development Team
+   
+   This program is free software under the 
+   GNU General Public License (>=v2). 
+   Read the file COPYING that comes with GRASS
+   for details.
+   
+   \author CERL
+*/
+
 #include <grass/gis.h>
 #include <string.h>
 
-/* -1 can't open file for reading
- * -2 no memory for key,value info, file not modified
- * -3 can't open file for re-write
- * -4 error writing the file (might be damaged)
- */
+/*!
+  \brief Update file, set up value for given key
+
+  \param[in] file  filename to be updated
+  \param[in] key   key value
+  \param[in] value value to be updated
+
+  \return -1 can't open file for reading
+  \return -2 no memory for key,value info, file not modified
+  \return -3 can't open file for re-write
+  \return -4 error writing the file (might be damaged)
+*/
 int G_update_key_value_file (const char *file, const char *key, const char *value)
 {
     struct Key_Value *kv;
@@ -27,10 +49,18 @@ int G_update_key_value_file (const char *file, const char *key, const char *valu
     return stat;
 }
 
-/* returns: <0 are file/memory errors
- *           0 not found
- *           1 ok
- */
+/*!
+  \brief Look up for key in file
+
+  \param[in]  file  filename
+  \param[in]  key   key to be found in file
+  \param[out] value value for key
+  \param[in]  n     number of characters to be copied
+
+  \return <0 are file/memory errors
+  \return 0 not found
+  \return 1 ok
+*/
 int G_lookup_key_value_from_file(
     const char *file,
     const char *key,
