858355dfef6ea4c5bc733b4a478fca7477726047
Martin Landa <landa.martin@gmail.com>
2008-01-17 20:54:30 +0000

    gislib: some doxygen strings updated
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@29740 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/gisdbase.c
+++ lib/gis/gisdbase.c
@@ -1,24 +1,29 @@
-/**********************************************************************
- *
- *   char *
- *   G_gisdbase()
- *
- *   returns:    pointer to string containing the base directory of
- *               GRASS-GRID data.
- **********************************************************************/
+/*!
+  \file gisdbase.c
+  
+  \brief GIS library - environment routines (gisdbase)
+  
+  (C) 2001-2008 by the GRASS Development Team
+  
+  This program is free software under the 
+  GNU General Public License (>=v2). 
+  Read the file COPYING that comes with GRASS
+  for details.
+  
+  \author Original author CERL
+*/
 
 #include <grass/gis.h>
 
-
 /*!
- * \brief top level database directory
+ * \brief Get name of top level database directory
  *
- * Returns the
- * full UNIX path name of the directory which holds the database locations. See
- * GISDBASE for a full explanation of this directory.
+ * Returns the full UNIX path name of the directory which holds the
+ * database locations. See GISDBASE for a full explanation of this
+ * directory.
  *
- *  \param void
- *  \return char * 
+ *  \param
+ *  \return pointer to string containing the base directory
  */
 
 char *
