6396949f95232ca5f362e66c0e8ecf43a0e3f59b
Hamish Bowman <hamish_b@yahoo.com>
2008-02-12 05:04:13 +0000

    * expand and reword help page and some messages; * reorder GUIs to put tcltk first * add flag to bypass making the given GUI type permanent * add, but not activate, rc file option (what to call it?) * only reset the default if it differs from the given type
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@30090 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.gui/main.c
+++ general/g.gui/main.c
@@ -21,7 +21,8 @@
 
 int main(int argc, char *argv[])
 {
-    struct Option *type;
+    struct Option *type, *rc_file;
+    struct Flag *oneoff;
     struct GModule *module;
     char *gui_type_env;
 
@@ -30,17 +31,17 @@ int main(int argc, char *argv[])
     module = G_define_module();
     module->keywords = _("general, gui");
     module->description =
-	_("Starts GRASS graphical user interface (GUI).");
+	_("Launches a GRASS graphical user interface (GUI) session.");
 
     type = G_define_option();
     type->key = "type";
     type->type = TYPE_STRING;
     type->required = YES;
     type->description = _("GUI type");
-    type->descriptions = _("oldtcltk;Old TCL/TK based GUI - Display Manager (d.m);"
-			   "tcltk;TCL/TK based GUI - GIS Manager (gis.m);"
-			   "wxpython;wxPython based new generation GUI (wxgui)");
-    type->options = "oldtcltk,tcltk,wxpython";
+    type->descriptions = _("tcltk;TCL/TK based GUI - GIS Manager (gis.m);"
+			   "oldtcltk;Old TCL/TK based GUI - Display Manager (d.m);"
+			   "wxpython;wxPython based next generation GUI (wxgui)");
+    type->options = "tcltk,oldtcltk,wxpython";
     gui_type_env = G__getenv("GRASS_GUI");
     if (gui_type_env && strcmp(gui_type_env, "text")) {
 	type->answer = G_store(gui_type_env);
@@ -48,16 +49,29 @@ int main(int argc, char *argv[])
     else {
 	type->answer = "tcltk";
     }
+/*
+    rc_file = G_define_option();
+    rc_file->key = "rc";
+    rc_file->type = TYPE_STRING;
+    rc_file->required = NO;
+    rc_file->description = _("Name of GIS manager settings file (.grc)");
+*/
+    oneoff = G_define_flag();
+    oneoff->key         = 'u';
+    oneoff->description = _("Do not modify default GUI setting");
 
     if (argc > 1 && G_parser(argc, argv))
 	exit(EXIT_FAILURE);
 
-    if (gui_type_env) {
-	if (strcmp(gui_type_env, type->answer)) {
-	    G_message(_("<%s> is now default GUI"), type->answer);
+    if (gui_type_env && !oneoff->answer) {
+	if (strcmp(gui_type_env, type->answer) != 0) {
+	    G_message(_("<%s> is now the default GUI"), type->answer);
 	    G_setenv("GRASS_GUI", type->answer);
 	}
     }
+
+/* TODO: build string to include dmrc= file */
+/*       use G_spawn() ? */
 
     if (strcmp(type->answer, "oldtcltk") == 0) {
 	G_system("d.m");
