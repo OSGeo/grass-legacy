bff09975f524ea974e1a43e912e5d3a54becb232
Hamish Bowman <hamish_b@yahoo.com>
2008-02-13 10:56:30 +0000

    GNAME_MAX, i18n, G_warning()
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@30111 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.what.rast/opencell.c
+++ display/d.what.rast/opencell.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <grass/gis.h>
+#include <grass/glocale.h>
 
 int 
 opencell (char *fullname, char *name, char *mapset)
@@ -11,16 +12,18 @@ opencell (char *fullname, char *name, char *mapset)
 	m = G_find_cell2 (name, "");
 	if (m == NULL)
 	{
-		fprintf (stderr, "warning: %s - raster map not found\n", name);
-		return -1;
+	    G_warning(_("Raster map <%s> not found"), name);
+	    return -1;
 	}
+
 	if (strlen (m) == 0)
-	    strcpy (mapset, G_mapset ());
+	    strcpy (mapset, G_mapset());
 	else
 	    strcpy (mapset, m);
+
 	fd = G_open_cell_old (name, mapset);
 	if (fd < 0)
-		fprintf (stderr, "warning: unable to open [%s] in [%s]\n",
-		    name, mapset);
+	    G_warning(_("Unable to open raster map <%s>"), name);
+
 	return fd;
 }
