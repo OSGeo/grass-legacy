5c257a08f2e824f10347d25211d37cf8eeae0e7a
Glynn Clements <glynn@gclements.plus.com>
2008-02-25 06:20:38 +0000

    Only perform reclass check for rasters.
    git-svn-id: https://svn.osgeo.org/grass/grass/trunk@30341 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/manage/cmd/remove.c
+++ general/manage/cmd/remove.c
@@ -24,6 +24,74 @@
 #include "list.h"
 #include "local_proto.h"
 
+static int
+check_reclass(const char *name, const char *mapset, int force)
+{
+    char rname[GNAME_MAX], rmapset[GMAPSET_MAX];
+    char **rmaps;
+    int nrmaps;
+
+    if (G_is_reclassed_to(name, mapset, &nrmaps, &rmaps) > 0)
+    {
+	for ( ; *rmaps; rmaps++)
+	{
+	    /* force remove */
+	    if (force)
+		G_warning(
+		    _("[%s@%s] is a base map for [%s]. Remove forced."),
+		    name, mapset,  *rmaps);
+	    else
+		G_warning(
+		    _("[%s@%s] is a base map. Remove reclassed map first: %s"),
+		    name, mapset,  *rmaps);
+	}
+
+	if (!force)
+	    return 1;
+    }
+
+    if (G_is_reclass(name, mapset, rname, rmapset) > 0 &&
+	G_is_reclassed_to(rname, rmapset, &nrmaps, &rmaps) > 0)
+    {
+	char path[GPATH_MAX];
+	char *p = strchr(rname, '@');
+	char *qname = G_fully_qualified_name(name, mapset);
+
+	if (p)
+	    *p = '\0';
+
+	G__file_name_misc(path, "cell_misc", "reclassed_to", rname, rmapset);
+
+	if(nrmaps == 1 && !G_strcasecmp(rmaps[0], qname))
+	{
+			
+	    if (remove(path) < 0)
+		G_warning(
+		    _("Removing information about reclassed map from [%s@%s] failed"),
+		    rname, rmapset);
+	}
+	else
+	{
+	    FILE *fp = fopen(path, "w");
+
+	    if (fp)
+	    {
+		for ( ; *rmaps; rmaps++)
+		    if (G_strcasecmp(*rmaps, qname))
+			fprintf(fp, "%s\n", *rmaps);
+		fclose(fp);
+	    }
+	    else
+		G_warning(
+		    _("Removing information about reclassed map from [%s@%s] failed"),
+		    rname, rmapset);
+
+	}
+    }
+
+    return 0;
+}
+
 int 
 main (int argc, char *argv[])
 {
@@ -32,10 +100,7 @@ main (int argc, char *argv[])
     struct Option **parm, *p;
     struct Flag *force_flag;
     char *name, *mapset;
-    char rname[256], rmapset[256];
-    int nrmaps;
-    char **rmaps, *location_path;
-    FILE *fp;
+    char *location_path;
     int result = EXIT_SUCCESS;
     int force = 0;
 
@@ -80,62 +145,10 @@ main (int argc, char *argv[])
 	if (parm[n]->answers)
 	    for (i = 0; (name = parm[n]->answers[i]); i++)
 	    {
-		if(G_is_reclassed_to(name, mapset, &nrmaps, &rmaps) > 0)
-		{
-		    for(; *rmaps; rmaps++) {
-                        /* force remove */
-                        if ( force ) {
-                            G_warning(
-                            _("[%s@%s] is a base map for [%s]. Remove forced."),
-                                                name, mapset,  *rmaps);
-                        }
-                        else {
-                            G_warning(
-                            _("[%s@%s] is a base map. Remove reclassed map first: %s"),
-                                                name, mapset,  *rmaps);
-                        }
-                    }
-                    if ( !force ) 
-                        continue;
-		}
-		if(G_is_reclass(name, mapset, rname, rmapset) > 0 &&
-		   G_is_reclassed_to(rname, rmapset, &nrmaps, &rmaps) > 0)
-		{
-		    char path[GPATH_MAX];
-		    char *p = strchr(rname, '@');
-		    char *qname = G_fully_qualified_name(name, mapset);
-		    if (p)
-			*p = '\0';
-		    G__file_name_misc(path, "cell_misc", "reclassed_to", rname, rmapset);
+		if (G_strcasecmp(list[n].alias, "rast") == 0 &&
+		    check_reclass(name, mapset, force))
+		    continue;
 
-		    if(nrmaps == 1 && !G_strcasecmp(rmaps[0], qname))
-		    {
-			
-		        if (  remove(path) < 0 ) {
-                            G_warning(
-                            _("Removing information about reclassed map from [%s@%s] failed"),
-                                                rname, rmapset);
-                        }
-
-		    }
-		    else
-		    {
-		        if ( (fp = fopen(path, "w")) ) {
-                            for(; *rmaps; rmaps++)
-                            {
-                                if(G_strcasecmp(*rmaps, qname))
-                                    fprintf(fp, "%s\n", *rmaps);
-                            }
-                            fclose(fp);
-                        }
-                        else {
-                            G_warning(
-                            _("Removing information about reclassed map from [%s@%s] failed"),
-                                                rname, rmapset);
-
-                        }
-		    }
-		}
 		if ( do_remove (n, name) == 1 )
                 {
 		    result = EXIT_FAILURE;
