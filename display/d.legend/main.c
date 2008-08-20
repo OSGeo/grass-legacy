639aac0ec7ce46ce38a55eed27b3a01d58d5c0b0
Markus Neteler <neteler@gmail.com>
2008-08-20 12:30:29 +0000

    catch no data map (merge from trunk, https://trac.osgeo.org/grass/changeset/32919)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_3@32921 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.legend/main.c
+++ display/d.legend/main.c
@@ -353,6 +353,8 @@ int main( int argc, char **argv )
 		    G_fatal_error(_("Range information for <%s> not available (run r.support)"), map_name);
 
 		G_get_range_min_max (&range, &min_ind, &max_ind);
+		if (G_is_c_null_value(&min_ind))
+		    G_fatal_error(_("Input map contains no data"));
 
 		G_get_color_range(&min_colr, &max_colr, &colors);
 
