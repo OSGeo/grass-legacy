ec7df4bfdec8453591c8cb601f76224d81de48ad
Martin Landa <landa.martin@gmail.com>
2008-12-02 15:55:29 +0000

    guisection: query renamed to selection
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@34667 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- display/d.rast/main.c
+++ display/d.rast/main.c
@@ -64,7 +64,7 @@ int main(int argc, char **argv)
     catlist->required = NO;
     catlist->multiple = YES;
     catlist->description = _("List of categories to be displayed (INT maps)");
-    catlist->guisection = _("Query");
+    catlist->guisection = _("Selection");
 
     vallist = G_define_option();
     vallist->key = "vallist";
@@ -73,7 +73,7 @@ int main(int argc, char **argv)
     vallist->required = NO;
     vallist->multiple = YES;
     vallist->description = _("List of values to be displayed (FP maps)");
-    vallist->guisection = _("Query");
+    vallist->guisection = _("Selection");
 
     bg = G_define_option();
     bg->key = "bg";
@@ -90,7 +90,7 @@ int main(int argc, char **argv)
     flag_i = G_define_flag();
     flag_i->key = 'i';
     flag_i->description = _("Invert catlist");
-    flag_i->guisection = _("Query");
+    flag_i->guisection = _("Selection");
 
     flag_x = G_define_flag();
     flag_x->key = 'x';
