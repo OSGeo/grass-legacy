2c8a8632a3a7a629f0d05eba420d84f6b96f9ec3
Markus Neteler <neteler@gmail.com>
2009-04-16 19:48:26 +0000

    hcho: Removed unused variables; Dynamic buffer for large number of mapsets and long mapset names (bugfix for many mapsets with long names, merge from trunk, https://trac.osgeo.org/grass/changeset/36709)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@36763 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- general/g.mapsets/dsply_path.c
+++ general/g.mapsets/dsply_path.c
@@ -1,32 +1,32 @@
 #include <string.h>
-#include "externs.h"
 #include <grass/gis.h>
+#include <grass/glocale.h>
+#include "local_proto.h"
 
-int display_mapset_path(int verbose)
+int display_mapset_path(const char *fs)
 {
     int n;
     int map;			/* pointer into list of available mapsets */
     int offset = 6;		/* accounts for " <x>, " */
-    char *name;
+    const char *name;
     int len;
     int nleft;
 
-    if (verbose) {
+    if (!fs) {
 	/* account for largest mapset number in offset value */
 	for (n = nmapsets; n /= 10; offset++) ;
 
-	fprintf(stdout, "Your mapset search list:\n");
-	ncurr_mapsets = 0;
+	fprintf(stdout, _("Your mapset search list:\n"));
     }
 
     nleft = 78;
-    for (n = 0; name = G__mapset_name(n); n++) {
+    for (n = 0; (name = G__mapset_name(n)); n++) {
 	/* match each mapset to its numeric equivalent */
-	if (verbose) {
+	if (!fs) {
 	    for (map = 0; map < nmapsets && strcmp(mapset_name[map], name);
 		 map++) ;
 	    if (map == nmapsets)
-		G_fatal_error("%s not found in mapset list:  call greg",
+		G_fatal_error(_("<%s> not found in mapset list"),
 			      name);
 	}
 
@@ -36,21 +36,21 @@ int display_mapset_path(int verbose)
 	    nleft = 78;
 	}
 
-	if (verbose) {
+	if (!fs) {
 	    if (n)
 		fprintf(stdout, ", ");
 	    fprintf(stdout, "%s <%d>", name, map + 1);
 	    nleft -= (len + offset);
-	    curr_mapset[n] = map;
-	    ++ncurr_mapsets;
 	}
 	else {
-	    fprintf(stdout, "%s ", name);
+	    fprintf(stdout, "%s", name);
+	    if (G__mapset_name(n+1))
+		fprintf(stdout, "%s", fs);
 	    nleft -= (len + 1);
 	}
     }
     fprintf(stdout, "\n");
-    if (verbose)
+    if (!fs)
 	fprintf(stdout, "\n");
 
     return 0;
