6676269a7cd9de3b847e8c9fc8aab5f8d04607f9
Martin Landa <landa.martin@gmail.com>
2009-05-04 14:54:56 +0000

    wxGUI: raise also first map window
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@36978 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxgui.py
+++ gui/wxpython/wxgui.py
@@ -186,6 +186,7 @@ class GMFrame(wx.Frame):
         self.goutput.Redirect()
         
         # start with layer manager on top
+        self.curr_page.maptree.mapdisplay.Raise()
         self.Raise()
 
     def __doLayout(self):
