3ecbd03862c378b66201ca635284ca7215f1a7d5
Martin Landa <landa.martin@gmail.com>
2009-05-20 21:56:34 +0000

    progress in geos support (new Vlib fns)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@37316 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/geos.c
+++ lib/vector/Vlib/geos.c
@@ -5,8 +5,6 @@
   
   Higher level functions for reading/writing/manipulating vectors.
   
-  Note: current GEOS support is *very* slow
-
   (C) 2009 by the GRASS Development Team
   
   This program is free software under the GNU General Public License
@@ -23,22 +21,28 @@
 #ifdef HAVE_GEOS
 #include <geos_c.h>
 
-static struct line_pnts *Points;
-
 static GEOSGeometry *Vect__read_line_geos(struct Map_info *, long);
+static GEOSCoordSequence *V1_read_line_geos(struct Map_info *, long);
+static GEOSCoordSequence *V2_read_line_geos(struct Map_info *, int);
 
 /*!
    \brief Read vector feature and stores it as GEOSGeometry instance
 
-   Note: Free allocated memory by GEOSGeom_destroy().
+   Supported feature types:
+    - GV_POINT     -> POINT
+    - GV_LINE      -> LINESTRING
+    - GV_BOUNDARY  -> LINESTRING / LINEARRING
+   
+   You should free allocated memory by GEOSGeom_destroy().
 
    \param Map pointer to Map_info structure
    \param line feature id
 
    \return pointer to GEOSGeometry instance
+   \return empty GEOSGeometry for unsupported feature type
    \return NULL on error
  */
-GEOSGeometry *Vect_read_line_geos(const struct Map_info *Map, int line)
+GEOSGeometry *Vect_read_line_geos(struct Map_info *Map, int line)
 {
     P_LINE *Line;
     
@@ -64,9 +68,9 @@ GEOSGeometry *Vect_read_line_geos(const struct Map_info *Map, int line)
 }
 
 /*!
-   \brief Read vector area and stores it as GEOSGeometry instance
+   \brief Read vector area and stores it as GEOSGeometry instance (polygon)
 
-   Note: Free allocated memory by GEOSGeom_destroy().
+   You should free allocated memory by GEOSGeom_destroy().
 
    \param Map pointer to Map_info structure
    \param area area id 
@@ -74,21 +78,14 @@ GEOSGeometry *Vect_read_line_geos(const struct Map_info *Map, int line)
    \return pointer to GEOSGeometry instance
    \return NULL on error
  */
-GEOSGeometry *Vect_read_area_geos(const struct Map_info * Map, int area)
+GEOSGeometry *Vect_read_area_geos(struct Map_info * Map, int area)
 {
-    int i, type, nholes, isle;
+    int i, nholes, isle;
     GEOSGeometry *boundary, **holes;
+    
+    G_debug(3, "Vect_read_area_geos(): area = %d", area);
 
-    if (!Points)
-	Points = Vect_new_line_struct();
-
-    G_debug(3, "Vect_read_area_geos(area=%d)", area);
-
-    if (Vect_get_area_points(Map, area, Points) == -1) {
-	G_fatal_error(_("Vect_read_area_geos(): unable to read area id %d"),
-		      area);
-    }
-    boundary = Vect_line_to_geos(Map, Points, GV_BOUNDARY);
+    boundary = GEOSGeom_createLinearRing(Vect_get_area_points_geos(Map, area));
     if (!boundary) {
 	G_fatal_error(_("Vect_read_area_geos(): unable to read area id %d"),
 		      area);
@@ -100,12 +97,12 @@ GEOSGeometry *Vect_read_area_geos(const struct Map_info * Map, int area)
 	isle = Vect_get_area_isle(Map, area, i);
 	if (isle < 1)
 	    continue;
-	if (Vect_get_isle_points(Map, isle, Points) < 0)
+	holes[i] = GEOSGeom_createLinearRing(Vect_get_isle_points_geos(Map, isle));
+	if (!(holes[i]))
 	    G_fatal_error(_("Vect_read_area_geos(): unable to read isle id %d of area id %d"),
 			  isle, area);
-	holes[i] = Vect_line_to_geos(Map, Points, GV_BOUNDARY);
     }
-
+    
     return GEOSGeom_createPolygon(boundary, holes, nholes);
 }
 
@@ -115,9 +112,9 @@ GEOSGeometry *Vect_read_area_geos(const struct Map_info * Map, int area)
    Supported types:
    - GV_POINT    -> POINT
    - GV_LINE     -> LINESTRING
-   - GV_BOUNDARY -> LINERING
+   - GV_BOUNDARY -> LINEARRING
 
-   Note: Free allocated memory by GEOSGeom_destroy().
+   You should free allocated memory by GEOSGeom_destroy().
 
    \param Map pointer to Map_info structure
    \param type feature type (see supported types)
@@ -125,7 +122,7 @@ GEOSGeometry *Vect_read_area_geos(const struct Map_info * Map, int area)
    \return pointer to GEOSGeometry instance
    \return NULL on error
  */
-GEOSGeometry *Vect_line_to_geos(const struct Map_info * Map,
+GEOSGeometry *Vect_line_to_geos(struct Map_info * Map,
 				const struct line_pnts * points, int type)
 {
     int i, with_z;
@@ -180,8 +177,10 @@ GEOSGeometry *Vect_line_to_geos(const struct Map_info * Map,
 /*!  
   \brief Read line from coor file
   
+  You should free allocated memory by GEOSGeom_destroy().
+
   \param Map pointer to Map_info
-  \param offset lineoffset
+  \param offset line offset
  
   \return pointer to GEOSGeometry
   \return NULL on error
@@ -190,80 +189,16 @@ GEOSGeometry *Vect_line_to_geos(const struct Map_info * Map,
 */
 GEOSGeometry *Vect__read_line_geos(struct Map_info *Map, long offset)
 {
-    int i;
-    int n_points;
     int type;
-    char rhead;
     double *x, *y, *z;
     
     GEOSGeometry *geom;
     GEOSCoordSequence *pseq;
     
-    G_debug(3, "Vect__read_line_geos(): offset = %ld", offset);
+    pseq = V1_read_line_geos(Map, offset);
+    if (!pseq)
+	G_fatal_error(_("Unable to read line offset %ld"), offset);
     
-    Map->head.last_offset = offset;
-    
-    /* reads must set in_head, but writes use default */
-    dig_set_cur_port(&(Map->head.port));
-    
-    dig_fseek(&(Map->dig_fp), offset, 0);
-    
-    if (0 >= dig__fread_port_C(&rhead, 1, &(Map->dig_fp)))
-	return NULL;            /* end of file */
-    
-    if (!(rhead & 0x01))	/* dead line */
-	return NULL;
-    
-    rhead >>= 2;
-    type = dig_type_from_store((int)rhead);
-    
-    /* read only points / lines / boundaries */
-    if (!(type & (GV_POINT | GV_LINES)))
-	return NULL;
-    
-    if (type & GV_POINTS) {
-	n_points = 1;
-    }
-    else {
-	if (0 >= dig__fread_port_I(&n_points, 1, &(Map->dig_fp)))
-	    return NULL;
-	if (n_points < 2)
-	    /* line / boundary is not valid */
-	    return NULL;
-    }
-    
-    G_debug(3, "    n_points = %d dim = %d", n_points, (Map->head.with_z) ? 3 : 2);
-    
-    pseq = GEOSCoordSeq_create(n_points, (Map->head.with_z) ? 3 : 2);
-    
-    x = (double *) G_malloc(n_points * sizeof(double));
-    y = (double *) G_malloc(n_points * sizeof(double));
-    if (Map->head.with_z)
-	z = (double *) G_malloc(n_points * sizeof(double));
-    else
-	z = NULL;
-    
-    if (0 >= dig__fread_port_D(x, n_points, &(Map->dig_fp)))
-	return NULL; /* end of file */
-
-    if (0 >= dig__fread_port_D(y, n_points, &(Map->dig_fp)))
-	return NULL; /* end of file */
-
-    if (Map->head.with_z) {
-	if (0 >= dig__fread_port_D(z, n_points, &(Map->dig_fp)))
-	    return NULL; /* end of file */
-
-    }
-
-    for (i = 0; i < n_points; i++) {
-	GEOSCoordSeq_setX(pseq, i, x[i]);    
-	GEOSCoordSeq_setY(pseq, i, y[i]);
-	if (Map->head.with_z)
-	    GEOSCoordSeq_setZ(pseq, i, z[i]);
-    }
-    
-    G_debug(3, "    off = %ld", dig_ftell(&(Map->dig_fp)));
-
     if (type & GV_POINT) {
 	G_debug(3, "    geos_type = point");
 	geom = GEOSGeom_createPoint(pseq);
@@ -292,6 +227,289 @@ GEOSGeometry *Vect__read_line_geos(struct Map_info *Map, long offset)
     /* GEOSCoordSeq_destroy(pseq); */
     
     return geom;
+}
+
+/*!  
+  \brief Read line from coor file into GEOSCoordSequence
+  
+  You should free allocated memory by GEOSCoordSeq_destroy().
+  
+  \param Map pointer to Map_info
+  \param line line id
+  
+  \return pointer to GEOSCoordSequence
+  \return empty GEOSCoordSequence for dead line or unsuppored feature type
+  \return NULL end of file
+*/
+GEOSCoordSequence *V2_read_line_geos(struct Map_info *Map, int line)
+{
+    P_LINE *Line;
+    
+    G_debug(3, "V2_read_line_geos(): line = %d", line);
+    
+    Line = Map->plus.Line[line];
+
+    if (Line == NULL)
+	G_fatal_error("V2_read_line_geos(): %s %d",
+		      _("Attempt to read dead line"), line);
+    
+    return V1_read_line_geos(Map, Line->offset);
+}
+
+
+/*!  
+  \brief Read feature from coor file into GEOSCoordSequence
+
+  Note: Function reads only points, lines and boundaries, other
+  feature types are ignored (empty coord array is returned)!
+  
+  You should free allocated memory by GEOSCoordSeq_destroy().
+  
+  \param Map pointer to Map_info
+  \param offset line offset
+ 
+  \return pointer to GEOSCoordSequence
+  \return empty GEOSCoordSequence for dead line or unsuppored feature type
+  \return NULL end of file
+*/
+GEOSCoordSequence *V1_read_line_geos(struct Map_info *Map, long offset)
+{
+    int i, n_points, type;
+    char rhead;
+    double *x, *y, *z;
+    
+    GEOSCoordSequence *pseq;
+    
+    G_debug(3, "V1_read_line_geos(): offset = %ld", offset);
+    
+    Map->head.last_offset = offset;
+    
+    /* reads must set in_head, but writes use default */
+    dig_set_cur_port(&(Map->head.port));
+    
+    dig_fseek(&(Map->dig_fp), offset, 0);
+    
+    if (0 >= dig__fread_port_C(&rhead, 1, &(Map->dig_fp)))
+	return NULL;            /* end of file */
+    
+    if (!(rhead & 0x01))	/* dead line */
+	return GEOSCoordSeq_create(0, (Map->head.with_z) ? 3 : 2);
+    
+    rhead >>= 2;
+    type = dig_type_from_store((int)rhead);
+    
+    /* read only points / lines / boundaries */
+    if (!(type & (GV_POINT | GV_LINES)))
+	return GEOSCoordSeq_create(0, (Map->head.with_z) ? 3 : 2);
+    
+    if (type & GV_POINTS) {
+	n_points = 1;
+    }
+    else {
+	if (0 >= dig__fread_port_I(&n_points, 1, &(Map->dig_fp)))
+	    return NULL;
+    }
+    
+    G_debug(3, "    n_points = %d dim = %d", n_points, (Map->head.with_z) ? 3 : 2);
+    
+    pseq = GEOSCoordSeq_create(n_points, (Map->head.with_z) ? 3 : 2);
+    
+    x = (double *) G_malloc(n_points * sizeof(double));
+    y = (double *) G_malloc(n_points * sizeof(double));
+    if (Map->head.with_z)
+	z = (double *) G_malloc(n_points * sizeof(double));
+    else
+	z = NULL;
+    
+    if (0 >= dig__fread_port_D(x, n_points, &(Map->dig_fp)))
+	return NULL; /* end of file */
+
+    if (0 >= dig__fread_port_D(y, n_points, &(Map->dig_fp)))
+	return NULL; /* end of file */
+
+    if (Map->head.with_z) {
+	if (0 >= dig__fread_port_D(z, n_points, &(Map->dig_fp)))
+	    return NULL; /* end of file */
+
+    }
+
+    for (i = 0; i < n_points; i++) {
+	GEOSCoordSeq_setX(pseq, i, x[i]);
+	GEOSCoordSeq_setY(pseq, i, y[i]);
+	if (Map->head.with_z)
+	    GEOSCoordSeq_setZ(pseq, i, z[i]);
+    }
+    
+    G_debug(3, "    off = %ld", dig_ftell(&(Map->dig_fp)));
+    
+    G_free((void *) x);
+    G_free((void *) y);
+    if (z)
+	G_free((void *) z);
+    
+    return pseq;
+}
+
+/*!
+   \brief Returns the polygon array of points, i.e. outer ring (shell)
+
+   You should free allocated memory by GEOSCoordSeq_destroy().
+
+   See also Vect_get_area_points().
+
+   \param Map pointer to Map_info
+   \param area area id
+
+   \return pointer to GEOSCoordSequence
+   \return empty GEOSCoordSequence for dead area
+   \return NULL on error
+ */
+GEOSCoordSequence *Vect_get_area_points_geos(struct Map_info *Map, int area)
+{
+    int i, j, k;
+    int line, aline;
+    unsigned int n_points, n_points_shell;
+    double x, y, z;
+
+    struct Plus_head *Plus;
+    P_AREA *Area;
+    
+    GEOSCoordSequence **pseq, *pseq_shell;
+    
+    G_debug(3, "Vect_get_area_points_geos(): area = %d", area);
+    
+    Plus = &(Map->plus);
+    Area = Plus->Area[area];
+
+    if (Area == NULL) {		/* dead area */
+	G_warning(_("Attempt to read points of nonexistent area id %d"), area);
+	return NULL;		/* error , because we should not read dead areas */
+    }
+    
+    G_debug(3, "  n_lines = %d", Area->n_lines);
+    pseq = (GEOSCoordSequence **) G_malloc(Area->n_lines * sizeof(GEOSCoordSequence *));
+
+    n_points_shell = 0;
+    for (i = 0; i < Area->n_lines; i++) {
+	line = Area->lines[i];
+	aline = abs(line);
+	G_debug(3, "  append line(%d) = %d", i, line);
+
+	/*
+	  TODO
+	if (line > 0)
+	    dir = GV_FORWARD;
+	else
+	    dir = GV_BACKWARD;
+	*/
+
+	pseq[i] = V2_read_line_geos(Map, aline);
+	if (!(pseq[i])) {
+	    G_fatal_error(_("Unable to read feature id %d"), aline);
+	}
+	
+	GEOSCoordSeq_getSize(pseq[i], &n_points);
+	G_debug(3, "  line n_points = %d", n_points);
+	n_points_shell += n_points;
+    }
+
+    /* create shell (outer ring) */
+    pseq_shell = GEOSCoordSeq_create(n_points_shell, Map->head.with_z ? 3 : 2);
+    k = 0;
+    for (i = 0; i < Area->n_lines; i++) {
+	GEOSCoordSeq_getSize(pseq[i], &n_points);
+	for (j = 0; j < (int) n_points; j++, k++) {
+	    GEOSCoordSeq_getX(pseq[i], j, &x);
+	    GEOSCoordSeq_setX(pseq_shell, k, x);
+	    GEOSCoordSeq_getY(pseq[i], j, &y);
+	    GEOSCoordSeq_setY(pseq_shell, k, y);
+	    if (Map->head.with_z) {
+		GEOSCoordSeq_getY(pseq[i], j, &z);
+		GEOSCoordSeq_setZ(pseq_shell, k, z);
+	    }
+	}
+    }
+    
+    G_free((void *) pseq);
+
+    return pseq_shell;
+}
+
+/*!
+   \brief Returns the polygon (isle) array of points (inner ring)
+
+   You should free allocated memory by GEOSCoordSeq_destroy().
+   
+   See also Vect_get_isle_points().
+
+   \param Map pointer to Map_info
+   \param isle isel id
+
+   \return pointer to GEOSGeometry
+   \return NULL on error or dead line
+ */
+GEOSCoordSequence *Vect_get_isle_points_geos(struct Map_info *Map, int isle)
+{
+    int i, j, k;
+    int line, aline;
+    unsigned n_points, n_points_shell;
+    double x, y, z;
+
+    struct Plus_head *Plus;
+    P_ISLE *Isle;
+
+    GEOSCoordSequence **pseq, *pseq_shell;
+
+    G_debug(3, "Vect_get_isle_points_geos(): isle = %d", isle);
+
+    Plus = &(Map->plus);
+    Isle = Plus->Isle[isle];
+    
+    G_debug(3, "  n_lines = %d", Isle->n_lines);
+    for (i = 0; i < Isle->n_lines; i++) {
+	line = Isle->lines[i];
+	aline = abs(line);
+	G_debug(3, "  append line(%d) = %d", i, line);
+
+
+	pseq[i] = V2_read_line_geos(Map, aline);
+	if (!(pseq[i])) {
+	    G_fatal_error(_("Unable to read feature id %d"), aline);
+	}
+
+	GEOSCoordSeq_getSize(pseq[i], &n_points);
+	G_debug(3, "  line n_points = %d", n_points);
+	n_points_shell += n_points;
+
+	/*
+	  TODO
+	if (line > 0)
+	    dir = GV_FORWARD;
+	else
+	    dir = GV_BACKWARD;
+	*/
+    }
+
+    /* create shell (outer ring) */
+    pseq_shell = GEOSCoordSeq_create(n_points_shell, Map->head.with_z ? 3 : 2);
+    k = 0;
+    for (i = 0; i < Isle->n_lines; i++) {
+	GEOSCoordSeq_getSize(pseq[i], &n_points);
+	for (j = 0; j < (int) n_points; j++, k++) {
+	    GEOSCoordSeq_getX(pseq[i], j, &x);
+	    GEOSCoordSeq_setX(pseq_shell, k, x);
+	    GEOSCoordSeq_getY(pseq[i], j, &y);
+	    GEOSCoordSeq_setY(pseq_shell, k, y);
+	    if (Map->head.with_z) {
+		GEOSCoordSeq_getY(pseq[i], j, &z);
+		GEOSCoordSeq_setZ(pseq_shell, k, z);
+	    }
+	}
+    }
+    
+    G_free((void *) pseq);
+
+    return pseq_shell;
 }
 
 #endif /* HAVE_GEOS */
