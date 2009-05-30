4a0f6e3f6de68131b52a7831147ed8961c0df6c5
Martin Landa <landa.martin@gmail.com>
2009-05-30 15:30:25 +0000

    message cosmetics
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@37619 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/support/update_menudata.py
+++ gui/wxpython/support/update_menudata.py
@@ -30,6 +30,8 @@ import xml.sax.handler
 HandlerBase=xml.sax.handler.ContentHandler
 from xml.sax import make_parser
 
+from grass.script import core as grass
+
 def ParseInterface(cmd):
     """!Parse interface
     
@@ -47,26 +49,29 @@ def ParseInterface(cmd):
         
     return grass_task
     
-
 def parseModules():
     """!Parse modules' interface"""
     modules = dict()
     
     # list of modules to be ignored
-    ignore =  [ 'mkftcap' ]
+    ignore =  [ 'mkftcap',
+                'g.parser',
+                'r.mapcalc',
+                'r3.mapcalc',
+                'vcolors' ]
     
     count = len(globalvar.grassCmd['all'])
     i = 0
     for module in globalvar.grassCmd['all']:
         i += 1
         if i % 10 == 0:
-            print '   %d/%d' % (i, count)
+            grass.info('* %d/%d' % (i, count))
         if module in ignore:
             continue
         try:
             interface = ParseInterface(cmd = [module])
         except IOError, e:
-            print >> sys.stderr, e
+            grass.error(e)
             continue
         modules[interface.name] = { 'label'   : interface.label,
                                     'desc'    : interface.description }
@@ -88,7 +93,7 @@ def updateData(data, modules):
         
         module = item['command'].split(' ')[0]
         if not modules.has_key(module):
-            print 'WARNING: \'%s\' not found in modules' % item['command']
+            grass.warning("'%s' not found in modules" % item['command'])
             continue
         
         if modules[module]['label']:
@@ -110,14 +115,14 @@ def main(argv = None):
         print >> sys.stderr, __doc__
         return 1
     
-    print "Step 1: parse modules..."
+    grass.info("Step 1: parsing modules...")
     modules = dict()
     modules = parseModules()
-    print "Step 2: read menu data..."
+    grass.info("Step 2: reading menu data...")
     data = menudata.Data()
-    print "Step 3: update menu data..."
+    grass.info("Step 3: updating menu data...")
     updateData(data, modules)
-    print "Step 4: write menu data (menudata.xml)..."
+    grass.info("Step 4: writing menu data (menudata.xml)...")
     writeData(data)
     
     return 0
