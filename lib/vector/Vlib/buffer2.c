3f8b10746e6f87b1aeb245b08f5a171d75eb4c57
Markus Metz <markus.metz.giswork@gmail.com>
2009-07-25 17:09:38 +0000

    Vect_area_buffer2 still broken
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@38526 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/vector/Vlib/buffer2.c
+++ lib/vector/Vlib/buffer2.c
@@ -1091,7 +1091,7 @@ void Vect_area_buffer2(struct Map_info *Map, int area, double da, double db,
     Vect_append_point(outer, outer->x[0], outer->y[0], outer->z[0]);
 
     /* inner contours */
-    for (i = 1; i <= isles_count; i++) {
+    for (i = 0; i < isles_count; i++) {
 	isle = Vect_get_area_isle(Map, area, i);
 	Vect_get_isle_points(Map, isle, tPoints);
 
