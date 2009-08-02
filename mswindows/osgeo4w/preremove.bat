678c377bdb7c0bf6612015d58c49f3fbbdcbc720
Markus Neteler <neteler@gmail.com>
2009-08-02 08:20:52 +0000

    Juergen Fischer: osgeo4w additions/updates
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@38562 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ mswindows/osgeo4w/preremove.bat
@@ -0,0 +1,15 @@
+del "%OSGEO4W_STARTMENU%\GRASS.lnk"
+del "%ALLUSERSPROFILE%\Desktop\grass.lnk"
+
+del "%OSGEO4W_STARTMENU%\GRASS GIS\wxpython.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS\tcltk.lnk"
+del "%OSGEO4W_STARTMENU%\GRASS GIS\text.lnk"
+rmdir "%OSGEO4W_STARTMENU%\GRASS GIS"
+
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (wxPython).lnk"
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (TclTk).lnk"
+del "%ALLUSERSPROFILE%\Desktop\GRASS GIS (Text).lnk"
+
+del "%OSGEO4W_ROOT%"\apps\grass\grass-6.4.0svn\etc\fontcap
+del "%OSGEO4W_ROOT%"\apps\grass\bin\grass64
+del "%OSGEO4W_ROOT%"\bin\grass64.bat
