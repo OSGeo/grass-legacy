0b6798a29921fb3cb5e1daf20f8e2a99315bdf35
Hamish Bowman <hamish_b@yahoo.com>
2009-10-05 06:33:11 +0000

    write metadata
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@39399 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.rgb.his/closefiles.c
+++ imagery/i.rgb.his/closefiles.c
@@ -12,6 +12,7 @@ int closefiles(char *h_name, char *i_name, char *s_name,
     int i;
     struct Colors colors;
     struct Range range;
+    struct History history;
     CELL min, max;
     char *mapset;
 
@@ -22,6 +23,8 @@ int closefiles(char *h_name, char *i_name, char *s_name,
 
     mapset = G_mapset();
 
+    /* write colors */
+    /*   set to 0,max_level instead of min,max ?? */
     G_read_range(h_name, mapset, &range);
     G_get_range_min_max(&range, &min, &max);
     G_make_grey_scale_colors(&colors, min, max);
@@ -37,5 +40,22 @@ int closefiles(char *h_name, char *i_name, char *s_name,
     G_make_grey_scale_colors(&colors, min, max);
     G_write_colors(s_name, mapset, &colors);
 
+    /* write metadata */
+    G_short_history(h_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(h_name, &history);
+    G_put_cell_title(h_name, "Image hue");
+
+    G_short_history(i_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(i_name, &history);
+    G_put_cell_title(i_name, "Image intensity");
+
+    G_short_history(s_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(s_name, &history);
+    G_put_cell_title(s_name, "Image saturation");
+
     return 0;
 }
+
