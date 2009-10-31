c229849f2c6a42e36bba0d375ea07a8e2e995301
Markus Neteler <neteler@gmail.com>
2009-10-31 12:37:46 +0000

    backport correction for numbering of terrain classification
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_3@39651 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/lidar/v.lidar.edgedetection/edgedetection.c
+++ vector/lidar/v.lidar.edgedetection/edgedetection.c
@@ -35,9 +35,9 @@
 int edge_detection (struct Cell_head elaboration_reg, BOUND_BOX Overlap_Box, double *parBilin, double obsX, double obsY, \
 			double *partial, double alpha, double residual, double gradHigh, double gradLow)
 {
-    /* 0 = PRE_TERRAIN */
-    /* 1 = PRE_EDGE */
-    /* 2 = UNKNOWN */
+    /* 1 = PRE_TERRAIN */
+    /* 2 = PRE_EDGE */
+    /* 3 = UNKNOWN */
     
     int c1, c2;
     double g[9][2], *gradient, gradPto, dirPto;
