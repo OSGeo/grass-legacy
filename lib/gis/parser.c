6a2136abc82edfbd2f2ea75feadff9e0c8ee6df8
Markus Neteler <neteler@gmail.com>
2010-01-28 13:58:50 +0000

    workaround Gentoo bug (backport from 6.5)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@40722 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- lib/gis/parser.c
+++ lib/gis/parser.c
@@ -1989,7 +1989,7 @@ static void G_gui_wx(void)
 
     sprintf(script, "%s/etc/wxpython/gui_modules/menuform.py",
 	    getenv("GISBASE"));
-    G_spawn(getenv("GRASS_PYTHON"), "menuform.py", script, pgm_name, NULL);
+    G_spawn(getenv("GRASS_PYTHON"), getenv("GRASS_PYTHON"), script, pgm_name, NULL);
 }
 
 /**
