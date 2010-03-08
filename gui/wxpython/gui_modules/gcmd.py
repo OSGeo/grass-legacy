0753b79c3226396bd43fc2ca1282665d6832d89f
Martin Landa <landa.martin@gmail.com>
2010-03-08 15:44:47 +0000

    wxGUI: encode message only if locales are defined
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@41349 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/gcmd.py
+++ gui/wxpython/gui_modules/gcmd.py
@@ -447,8 +447,12 @@ class Command:
 
         for type, msg in self.__ProcessStdErr():
             if type == 'ERROR':
-                return unicode(msg, locale.getdefaultlocale()[1])
-
+                enc = locale.getdefaultlocale()[1]
+                if enc:
+                    return unicode(msg, enc)
+                else:
+                    return msg
+        
         return ''
     
     def PrintModuleOutput(self, error=True, warning=False, message=False):
