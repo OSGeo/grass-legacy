fa22a24ff45b1ea5f887159dfe0ddb20daec4f84
Martin Landa <landa.martin@gmail.com>
2010-05-10 14:31:31 +0000

    another fix help -> ghelp (merge https://trac.osgeo.org/grass/changeset/42215 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42216 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxgui.py
+++ gui/wxpython/wxgui.py
@@ -91,8 +91,8 @@ import gui_modules.prompt as prompt
 import gui_modules.menu as menu
 import gui_modules.gmodeler as gmodeler
 from   gui_modules.debug import Debug
-from   gui_modules.help import MenuTreeWindow
-from   gui_modules.help import AboutWindow
+from   gui_modules.ghelp import MenuTreeWindow
+from   gui_modules.ghelp import AboutWindow
 from   icons.icon import Icons
 
 UserSettings = preferences.globalSettings
