9cbcb29131dfe9ad985185faef15a2fd789a1082
Martin Landa <landa.martin@gmail.com>
2010-05-25 15:54:11 +0000

    wxNviz: tools clean up layer properties & settings tabs recovered (merge https://trac.osgeo.org/grass/changeset/42346 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42347 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/nviz_mapdisp.py
+++ gui/wxpython/gui_modules/nviz_mapdisp.py
@@ -183,7 +183,7 @@ class GLWindow(MapWindow, glcanvas.GLCanvas):
                 win.SetItems(self.GetLayerNames('raster'))
 
             self.init = True
-                        
+        
         self.UpdateMap()
 
     def OnMouseAction(self, event):
@@ -545,9 +545,9 @@ class GLWindow(MapWindow, glcanvas.GLCanvas):
                     toolWin.win['vector']['lines']['surface'])
                 win.SetItems(self.GetLayerNames(layer.type))
 
-            toolWin.UpdatePage(nvizType)
-            toolWin.SetPage(nvizType)
-            
+            #toolWin.UpdatePage(nvizType)
+            #toolWin.SetPage(nvizType)
+        
         return id
 
     def UnloadRaster(self, item):
