09f8ff395f2d33079ba1232c764427706d83e3cf
Martin Landa <landa.martin@gmail.com>
2010-05-30 12:10:48 +0000

    wxGUI: fix d.vect for more layers (merge https://trac.osgeo.org/grass/changeset/42395 from devbr6)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42396 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/menuform.py
+++ gui/wxpython/gui_modules/menuform.py
@@ -192,7 +192,7 @@ class UpdateThread(Thread):
         if not p or \
                 not p.has_key('wxId-bind'):
             return
-
+        
         # get widget prompt
         pType = p.get('prompt', '')
         if not pType:
@@ -200,6 +200,7 @@ class UpdateThread(Thread):
         
         # check for map/input parameter
         pMap = self.task.get_param('map', raiseError=False)
+        
         if not pMap:
             pMap = self.task.get_param('input', raiseError=False)
 
@@ -1513,10 +1514,10 @@ class cmdPanel(wx.Panel):
             pMap['wxId-bind'] = copy.copy(pColumnIds)
             if pLayer:
                 pMap['wxId-bind'] += pLayerIds
+        if pLayer:
+            for p in pLayer:
+                p['wxId-bind'] = copy.copy(pColumnIds)
         
-        for p in pLayer:
-            p['wxId-bind'] = copy.copy(pColumnIds)
-
         if pDriver and pTable:
             pDriver['wxId-bind'] = pTable['wxId']
 
