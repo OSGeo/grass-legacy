aa3ad6b09e8f8c2214099f8f4044e294692cf215
Martin Landa <landa.martin@gmail.com>
2010-06-10 20:21:11 +0000

    wxGUI/modeler: start implementing loops (merge https://trac.osgeo.org/grass/changeset/42545 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42546 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/preferences.py
+++ gui/wxpython/gui_modules/preferences.py
@@ -561,6 +561,12 @@ class Settings:
                         'height' : 50,
                         },
                     },
+                'loop' : {
+                    'size' : {
+                        'width' : 175,
+                        'height' : 50,
+                        },
+                    },
                 },
             }
         
