21de772c639170a43ad34512272311a75a5fa493
Martin Landa <landa.martin@gmail.com>
2010-07-12 19:53:00 +0000

    wxGUI/modeler: various loop-related fixes (merge https://trac.osgeo.org/grass/changeset/42770 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42771 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/preferences.py
+++ gui/wxpython/gui_modules/preferences.py
@@ -564,12 +564,12 @@ class Settings:
                 'loop' : {
                     'size' : {
                         'width' : 175,
-                        'height' : 50,
+                        'height' : 40,
                         },
                     },
                 'condition' : {
                     'size' : {
-                        'width' : 100,
+                        'width' : 150,
                         'height' : 40,
                         },
                     },
