eb0ed253cd92fc20c9f2fed60985c4b2c00d7e08
Martin Landa <landa.martin@gmail.com>
2010-07-14 12:11:38 +0000

    wxGUI/modeler: fix if-else statements (loading from file) (merge https://trac.osgeo.org/grass/changeset/42784 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@42785 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/preferences.py
+++ gui/wxpython/gui_modules/preferences.py
@@ -567,7 +567,7 @@ class Settings:
                         'height' : 40,
                         },
                     },
-                'condition' : {
+                'if-else' : {
                     'size' : {
                         'width' : 150,
                         'height' : 40,
