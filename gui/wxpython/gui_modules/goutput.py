f35d7e123db6d8638b0e94811f68876c36bb58e5
Martin Landa <landa.martin@gmail.com>
2010-08-25 21:38:35 +0000

    wxGUI/modeler: fix OnCmdRun/Done (merge https://trac.osgeo.org/grass/changeset/43271 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@43272 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/goutput.py
+++ gui/wxpython/gui_modules/goutput.py
@@ -664,10 +664,7 @@ class GMConsole(wx.SplitterWindow):
     def OnCmdRun(self, event):
         """!Run command"""
         if self.parent.GetName() == 'Modeler':
-            try:
-                self.parent.GetModel().GetActions()[event.pid].Update(running = True)
-            except IndexError:
-                pass
+            self.parent.OnCmdRun(event)
         
         self.WriteCmdLog('(%s)\n%s' % (str(time.ctime()), ' '.join(event.cmd)))
         self.btn_abort.Enable()
@@ -675,10 +672,7 @@ class GMConsole(wx.SplitterWindow):
     def OnCmdDone(self, event):
         """!Command done (or aborted)"""
         if self.parent.GetName() == 'Modeler':
-            try:
-                self.parent.GetModel().GetActions()[event.pid].Update(running = False)
-            except IndexError:
-                pass
+            self.parent.OnCmdDone(event)
         
         if event.aborted:
             # Thread aborted (using our convention of None return)
