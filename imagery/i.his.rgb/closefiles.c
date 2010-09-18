d9a2ccc65fb82b656f6b7338d029da99e47279f9
Markus Neteler <neteler@gmail.com>
2010-09-18 16:11:29 +0000

    backport: metadata and cleanup
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@43508 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- imagery/i.his.rgb/closefiles.c
+++ imagery/i.his.rgb/closefiles.c
@@ -12,6 +12,7 @@ int closefiles(char *r_name, char *g_name, char *b_name,
     int i;
     struct Colors colors;
     struct Range range;
+    struct History history;
     CELL min, max;
     char *mapset;
 
@@ -22,6 +23,8 @@ int closefiles(char *r_name, char *g_name, char *b_name,
 
     mapset = G_mapset();
 
+    /* write colors */
+    /*   set to 0,max_level instead of min,max ?? */
     G_read_range(r_name, mapset, &range);
     G_get_range_min_max(&range, &min, &max);
     G_make_grey_scale_colors(&colors, min, max);
@@ -36,6 +39,22 @@ int closefiles(char *r_name, char *g_name, char *b_name,
     G_get_range_min_max(&range, &min, &max);
     G_make_grey_scale_colors(&colors, min, max);
     G_write_colors(b_name, mapset, &colors);
+
+    /* write metadata */
+    G_short_history(r_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(r_name, &history);
+    G_put_cell_title(r_name, "Image red");
+
+    G_short_history(g_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(g_name, &history);
+    G_put_cell_title(g_name, "Image green");
+
+    G_short_history(b_name, "raster", &history);
+    G_command_history(&history);
+    G_write_history(b_name, &history);
+    G_put_cell_title(b_name, "Image blue");
 
     return 0;
 }
