0dd32f59c7ec375b9151fb02423be86b3e38f97c
Markus Neteler <neteler@gmail.com>
2010-09-21 10:37:32 +0000

    backport: parameters keys sychronized
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@43562 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- raster/r.basins.fill/main.c
+++ raster/r.basins.fill/main.c
@@ -8,7 +8,7 @@
  *
  * PURPOSE:      Generates a raster map layer showing watershed subbasins.
  *
- * COPYRIGHT:    (C) 2005 by the GRASS Development Team
+ * COPYRIGHT:    (C) 2005, 2010 by the GRASS Development Team
  *
  *               This program is free software under the GNU General Public
  *               License (>=v2). Read the file COPYING that comes with GRASS
@@ -50,38 +50,27 @@ int main(int argc, char *argv[])
     G_gisinit(argv[0]);
 
     module = G_define_module();
-    module->keywords = _("raster");
+    module->keywords = _("raster, hydrology");
     module->description =
-	_("Generates a raster map layer showing " "watershed subbasins.");
+	_("Generates watershed subbasins raster map.");
+    
+    opt2 = G_define_standard_option(G_OPT_R_INPUT);
+    opt2->key = "c_map";
+    opt2->description = _("Name of input coded stream network raster map");
+
+    opt3 = G_define_standard_option(G_OPT_R_INPUT);
+    opt3->key = "t_map";
+    opt3->description = _("Name of input thinned ridge network raster map");
+
+    opt4 = G_define_standard_option(G_OPT_R_OUTPUT);
+    opt4->key = "result";
 
     opt1 = G_define_option();
     opt1->key = "number";
     opt1->type = TYPE_INTEGER;
     opt1->required = YES;
     opt1->description = _("Number of passes through the dataset");
-    opt1->gisprompt = "old,cell,raster";
-
-    opt2 = G_define_option();
-    opt2->key = "c_map";
-    opt2->type = TYPE_STRING;
-    opt2->required = YES;
-    opt2->description = _("Coded stream network file name");
-    opt2->gisprompt = "old,cell,raster";
-
-    opt3 = G_define_option();
-    opt3->key = "t_map";
-    opt3->type = TYPE_STRING;
-    opt3->required = YES;
-    opt3->description = _("Thinned ridge network file name");
-    opt3->gisprompt = "old,cell,raster";
-
-    opt4 = G_define_option();
-    opt4->key = "result";
-    opt4->type = TYPE_STRING;
-    opt4->required = YES;
-    opt4->description = _("Name for the resultant watershed partition file");
-    opt4->gisprompt = "new,cell,raster";
-
+    
     if (G_parser(argc, argv))
 	exit(EXIT_FAILURE);
 
