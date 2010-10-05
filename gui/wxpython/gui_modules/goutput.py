a2b6c77472b8a516f6fe6cdfb5ee9918559a1214
Martin Landa <landa.martin@gmail.com>
2010-10-05 12:09:17 +0000

    wxGUI:redraw display when r.colors is launched (merge https://trac.osgeo.org/grass/changeset/43792 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@43794 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/goutput.py
+++ gui/wxpython/gui_modules/goutput.py
@@ -127,7 +127,8 @@ class CmdThread(threading.Thread):
                     self.requestCmdColor = callable(*argsColor, **kwds)
                     self.resultQ.put((requestId, self.requestCmdColor.run()))
             
-            event = wxCmdDone(aborted = aborted,
+            event = wxCmdDone(cmd = args[0],
+                              aborted = aborted,
                               returncode = returncode,
                               time = requestTime,
                               pid = requestId,
@@ -696,7 +697,7 @@ class GMConsole(wx.SplitterWindow):
             self.btn_abort.Enable(False)
         
         if event.onDone:
-            event.onDone(returncode = event.returncode)
+            event.onDone(cmd = event.cmd, returncode = event.returncode)
         
         self.console_progressbar.SetValue(0) # reset progress bar on '0%'
 
