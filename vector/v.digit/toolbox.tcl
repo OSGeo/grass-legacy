0c0cbbf17916eff894f82bf7192f61bafd912e8c
Michael Barton <michael.barton@asu.edu>
2011-01-10 00:12:48 +0000

    Hopefully fixing .bind_scroll_list bug. Also removing puts debugging code
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@44921 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.digit/toolbox.tcl
+++ vector/v.digit/toolbox.tcl
@@ -22,6 +22,7 @@ set prompt_left [G_msg "Left button"]
 set prompt_middle [G_msg "Middle button"]
 set prompt_right [G_msg "Right button"]
 set coor ""
+set bind_scroll_list {}
 
 wm title . "v.digit toolbox - $map_name@$map_mapset"
 
@@ -52,7 +53,6 @@ proc create_screen {} {
 
 		# right-click is acting like a middle-click...
 		bind .screen.canvas <ButtonPress> {
-            puts "button: %b"
 			if { %b == 3 } {
 				c_update_tool %x %y 2
 			} elseif { %b == 2 } {
