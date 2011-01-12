6d02ab88641e8683f44abe4e10f8dfbbb7573df1
Hamish Bowman <hamish_b@yahoo.com>
2011-01-12 23:11:48 +0000

    fix bashism (see http://www.linuxjournal.com/content/daca-could-mean-less-bugs-debian    http://qa.debian.org/daca/checkbashisms/source/squeeze/grass_6.4.0~rc6+42329-3.html)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@44999 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- tools/g51.extract_description.sh
+++ tools/g51.extract_description.sh
@@ -16,7 +16,7 @@ PATHGRASS50="$1"
 FILE=$PATHGRASS50/html/html/$2.html
 
 CUTLINE="`grep -ni '<H2>DESCRIPTION' $FILE | cut -d':' -f1`"
-if [ "$CUTLINE" == "" ] ; then
+if [ -z "$CUTLINE" ] ; then
   echo "ERROR: no <H2>DESCRIPTION</H2> present in html file"
   exit
 fi
