84b3d037f453fa7efbbe2d306bfb6206a5456807
Martin Landa <landa.martin@gmail.com>
2011-02-26 20:31:24 +0000

    pythonlib: array.py backported from trunk 	   related r.in/out.bin sync'ed with trunk 	   G_fseek() and G_ftell() added
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@45467 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- include/gis.h
+++ include/gis.h
@@ -53,6 +53,12 @@ static const char *GRASS_copyright __attribute__ ((unused))
 #define FALSE 0
 #endif
 
+#if defined(_FILE_OFFSET_BITS) && _FILE_OFFSET_BITS == 64
+#define PRI_OFF_T	"lld"
+#else
+#define PRI_OFF_T	"ld"
+#endif
+
 #define MAXEDLINES  50
 #define RECORD_LEN  80
 #define NEWLINE     '\n'
