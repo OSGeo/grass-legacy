d55c2a78e9072c86549596867e2d7b3e36494c57
Martin Landa <landa.martin@gmail.com>
2011-03-26 20:20:10 +0000

    wxGUI: use utils.split() instead of shlex.split()
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@45770 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/gmodeler.py
+++ gui/wxpython/gui_modules/gmodeler.py
@@ -41,7 +41,6 @@ This program is free software under the GNU General Public License
 
 import os
 import sys
-import shlex
 import time
 import traceback
 import getpass
@@ -2396,9 +2395,9 @@ class ModelSearchDialog(wx.Dialog):
             list()
         
         try:
-            cmd = shlex.split(str(line))
+            cmd = utils.split(str(line))
         except UnicodeError:
-            cmd = shlex.split(utils.EncodeString((line)))
+            cmd = utils.split(utils.EncodeString((line)))
             
         return cmd
     
