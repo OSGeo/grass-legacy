d9398b8bfad5798711ed74b96cab9cbe427ef624
Martin Landa <landa.martin@gmail.com>
2011-03-27 08:11:28 +0000

    wxGUI: show only bat scripts (merge r45782 from devbr6)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@45783 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/globalvar.py
+++ gui/wxpython/gui_modules/globalvar.py
@@ -148,7 +148,7 @@ def GetGRASSCmds(bin = True, scripts = True, gui_scripts = True):
         
         # add special call for setting vector colors
         cmd.append('vcolors')
-    if scripts:
+    if scripts and sys.platform != "win32":
         cmd = cmd + os.listdir(os.path.join(gisbase, 'scripts')) 
     if gui_scripts:
         os.environ["PATH"] = os.getenv("PATH") + os.pathsep + os.path.join(gisbase, 'etc', 'gui', 'scripts')
@@ -160,7 +160,7 @@ def GetGRASSCmds(bin = True, scripts = True, gui_scripts = True):
             name, ext = os.path.splitext(cmd[idx])
             if ext in (EXT_BIN, EXT_SCT):
                 cmd[idx] = name
-    
+
     return cmd
 
 """@brief Collected GRASS-relared binaries/scripts"""
