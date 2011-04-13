3176ced2af3cef32463502dce12216772754faf8
Martin Landa <landa.martin@gmail.com>
2011-04-13 11:32:26 +0000

    backport ctypes from devbr6
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@45937 15284696-431f-4ddb-bdfa-cd5b030d7da7

---
+++ lib/python/ctypes/__init__.py
@@ -0,0 +1,15 @@
+import date
+import gis
+import gmath
+import proj
+import imagery
+import vector
+import display
+import stats
+import dbmi
+import g3d
+import arraystats
+import cluster
+import trans
+import vedit
+
