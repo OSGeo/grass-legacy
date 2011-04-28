a065ee99ddf1ae41341c8005f610b265337dd285
Martin Landa <landa.martin@gmail.com>
2011-04-28 11:28:38 +0000

    update osgeo4w package scripts        (merge https://trac.osgeo.org/grass/changeset/46122 from relbr64)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@46123 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -1,14 +1,12 @@
 set ICON=%OSGEO4W_ROOT%\apps\grass\grass-@VERSION@\etc\gui\icons\grass.ico
 set BATCH=%OSGEO4W_ROOT%\bin\grass@POSTFIX@.bat
-
-textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@-env.bat 
-textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat 
+textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
 textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@ 
 textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
 xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\wxPython @VERSION@.lnk"   "%BATCH%" "-wxpython" \ "wxPython interface" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\TclTkx @VERSION@.lnk"     "%BATCH%" "-tcltk" \ "Tcl/Tk interface" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\TclTk @VERSION@.lnk"     "%BATCH%" "-tcltk" \ "Tcl/Tk interface" 1 "%ICON%" 
 xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text @VERSION@.lnk"       "%BATCH%" "-text" \ "Text interface" 1 "%ICON%" 
 
 xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (wxpython).lnk" "%BATCH%" "-wxpython" \ "wxPython" 1 "%ICON%" 
