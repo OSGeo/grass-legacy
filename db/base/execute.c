fd8396de47fc2d379af637176ea904754a832c7b
Markus Neteler <neteler@gmail.com>
2011-06-09 07:50:39 +0000

    GUI sections (backport from 6.5)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@46630 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/base/execute.c
+++ db/base/execute.c
@@ -3,9 +3,9 @@
  *
  * MODULE:       db.execute
  * AUTHOR(S):    Radim Blazek <radim.blazek gmail.com> (original contributor)
- *               Huidae Cho <grass4u gmail.com>, Glynn Clements <glynn gclements.plus.com>, Hamish Bowman <hamish_nospam yahoo.com>, Markus Neteler <neteler itc.it>, Stephan Holl
+ *               Huidae Cho <grass4u gmail.com>, Glynn Clements <glynn gclements.plus.com>, Hamish Bowman <hamish_b yahoo.com>, Markus Neteler <neteler itc.it>, Stephan Holl
  * PURPOSE:      process one non-select sql statement
- * COPYRIGHT:    (C) 2002-2006 by the GRASS Development Team
+ * COPYRIGHT:    (C) 2002-2006, 2010 by the GRASS Development Team
  *
  *               This program is free software under the GNU General Public
  *               License (>=v2). Read the file COPYING that comes with GRASS
@@ -111,13 +111,16 @@ static void parse_command_line(int argc, char **argv)
     input = G_define_standard_option(G_OPT_F_INPUT);
     input->required = NO;
     input->description = _("Name of file containing SQL statements");
-
+    input->guisection = _("Query");
+    
     driver = G_define_standard_option(G_OPT_DRIVER);
     driver->options = db_list_drivers();
+    driver->guisection = _("Connection");
     if ((drv = db_get_default_driver_name()))
 	driver->answer = (char *) drv;
 
     database = G_define_standard_option(G_OPT_DATABASE);
+    database->guisection = _("Connection");
     if ((db = db_get_default_database_name()))
 	database->answer = (char *) db;
 
