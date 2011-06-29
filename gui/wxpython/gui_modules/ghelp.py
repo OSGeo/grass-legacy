a795b5d05efe0b4c97fc9e8962ea461e60062241
Martin Landa <landa.martin@gmail.com>
2011-06-29 13:22:44 +0000

    pythonlib: rewrite command_info() using grassTask and processTask (moved from wxGUI)           (merge https://trac.osgeo.org/grass/changeset/46843 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@46849 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/ghelp.py
+++ gui/wxpython/gui_modules/ghelp.py
@@ -34,6 +34,8 @@ except ImportError:
 import wx.lib.flatnotebook as FN
 import  wx.lib.scrolledpanel as scrolled
 
+from grass.script import task as gtask
+
 import menudata
 import gcmd
 import globalvar
@@ -813,7 +815,9 @@ class InstallExtensionWindow(wx.Frame):
         
         self.optionBox = wx.StaticBox(parent = self.panel, id = wx.ID_ANY,
                                       label = " %s " % _("Options"))
-        task = menuform.GUI().ParseInterface(cmd = ['g.extension.py'])
+        
+        task = gtask.parse_interface('g.extension.py')
+        
         for f in task.get_options()['flags']:
             name = f.get('name', '')
             desc = f.get('label', '')
