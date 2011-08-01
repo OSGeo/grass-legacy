cefe5f670a62622d685ace0bfb7bec120586abfc
Martin Landa <landa.martin@gmail.com>
2011-08-01 15:45:36 +0000

    wxGUI: launch ATM when creating new vector map        (merge https://trac.osgeo.org/grass/changeset/47336 & https://trac.osgeo.org/grass/changeset/47337 from trunk)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@47338 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/wxgui.py
+++ gui/wxpython/wxgui.py
@@ -634,18 +634,21 @@ class GMFrame(wx.Frame):
         
     def OnNewVector(self, event):
         """!Create new vector map layer"""
-        name, add = gdialogs.CreateNewVector(self, log = self.goutput,
-                                             cmd = (('v.edit',
-                                                     { 'tool' : 'create' },
-                                                     'map')))
+        dlg = gdialogs.CreateNewVector(self, log = self.goutput,
+                                       cmd = (('v.edit',
+                                               { 'tool' : 'create' },
+                                               'map')))
         
-        if name and add:
+        if not dlg:
+            return
+        
+        name = dlg.GetName(full = True)
+        if name and dlg.IsChecked('add'):
             # add layer to map layer tree
             self.curr_page.maptree.AddLayer(ltype = 'vector',
                                             lname = name,
-                                            lchecked = True,
-                                            lopacity = 1.0,
                                             lcmd = ['d.vect', 'map=%s' % name])
+        dlg.Destroy()
         
     def OnAboutGRASS(self, event):
         """!Display 'About GRASS' dialog"""
@@ -1211,7 +1214,7 @@ class GMFrame(wx.Frame):
                 
         dlg.Destroy()
         
-    def OnShowAttributeTable(self, event):
+    def OnShowAttributeTable(self, event, selection = None):
         """!Show attribute table of the given vector map layer
         """
         if not self.curr_page:
@@ -1248,7 +1251,8 @@ class GMFrame(wx.Frame):
         
         dbmanager = dbm.AttributeManager(parent = self, id = wx.ID_ANY,
                                          size = wx.Size(500, 300),
-                                         item = layer, log = self.goutput)
+                                         item = layer, log = self.goutput,
+                                         selection = selection)
         
         busy.Destroy()
         
