22059857816fa65b1fd489a4b4fee478a32240cb
Markus Neteler <neteler@gmail.com>
2011-09-11 21:24:59 +0000

    since default is defined, required = NO
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@48239 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.kernel/main.c
+++ vector/v.kernel/main.c
@@ -159,7 +159,7 @@ int main(int argc, char **argv)
     node_opt = G_define_option();
     node_opt->key = "node";
     node_opt->type = TYPE_STRING;
-    node_opt->required = YES;
+    node_opt->required = NO;
     node_opt->description = _("Node method");
     node_opt->options = "none,split";
     node_opt->answer = "none";
@@ -170,7 +170,7 @@ int main(int argc, char **argv)
     kernel_opt = G_define_option();
     kernel_opt->key = "kernel";
     kernel_opt->type = TYPE_STRING;
-    kernel_opt->required = YES;
+    kernel_opt->required = NO;
     kernel_opt->description = _("Kernel function");
     kernel_opt->options =
 	"uniform,triangular,epanechnikov,quartic,triweight,gaussian,cosine";
