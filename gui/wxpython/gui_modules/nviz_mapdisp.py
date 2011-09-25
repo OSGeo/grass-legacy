81e7f3922268fa758083a061c6595279879d4ab2
Martin Landa <landa.martin@gmail.com>
2011-09-25 10:00:18 +0000

    wxNviz: use goutput.GetLog()
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@48458 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/nviz_mapdisp.py
+++ gui/wxpython/gui_modules/nviz_mapdisp.py
@@ -98,8 +98,8 @@ class GLWindow(MapWindow, glcanvas.GLCanvas):
         #
         if self.lmgr:
             self.log = self.lmgr.goutput
-            logerr = self.lmgr.goutput.cmd_stderr
-            logmsg = self.lmgr.goutput.cmd_output
+            logerr = self.lmgr.goutput.GetLog(err = True)
+            logmsg = self.lmgr.goutput.GetLog()
         else:
             self.log = logmsg = sys.stdout
             logerr = sys.stderr
