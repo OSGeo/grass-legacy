10505c91a7b9a3e957981808e0baed4f67843523
Martin Landa <landa.martin@gmail.com>
2011-10-09 22:20:49 +0000

    wxGUI/modeler: fix OnModelNew()               (merge r48698 from devbr6)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@48700 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/gmodeler.py
+++ gui/wxpython/gui_modules/gmodeler.py
@@ -904,6 +904,8 @@ class ModelFrame(wx.Frame):
         self.canvas.GetDiagram().DeleteAllShapes()
         self.model.Reset()
         self.canvas.Refresh()
+        self.itemPanel.Update()
+        self.variablePanel.Reset()
         
         # no model file loaded
         self.modelFile = None
@@ -3887,6 +3889,11 @@ class VariablePanel(wx.Panel):
     def Update(self):
         """!Reload list of variables"""
         self.list.OnReload(None)
+        
+    def Reset(self):
+        """!Remove all variables"""
+        self.list.DeleteAllItems()
+        self.parent.GetModel().SetVariables([])
         
 class VariableListCtrl(ModelListCtrl):
     def __init__(self, parent, columns, **kwargs):
