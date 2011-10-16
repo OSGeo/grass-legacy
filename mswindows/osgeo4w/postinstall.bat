1b45d8d03d8651578ebcd0977e8f466dcd1cd2f0
Martin Landa <landa.martin@gmail.com>
2011-10-16 09:44:18 +0000

    winGRASS: clean up support files for osgeo4w
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@48825 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/postinstall.bat
+++ mswindows/osgeo4w/postinstall.bat
@@ -1,14 +1,15 @@
 set ICON=%OSGEO4W_ROOT%\apps\grass\grass-@VERSION@\etc\gui\icons\grass.ico
 set BATCH=%OSGEO4W_ROOT%\bin\grass@POSTFIX@.bat
 textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat
-textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@ 
-textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap 
+textreplace -std -t "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@
+textreplace -std -t "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
 
 mkdir "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\wxPython @VERSION@.lnk"   "%BATCH%" "-wxpython" \ "wxPython interface" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\TclTk @VERSION@.lnk"     "%BATCH%" "-tcltk" \ "Tcl/Tk interface" 1 "%ICON%" 
-xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\Text @VERSION@.lnk"       "%BATCH%" "-text" \ "Text interface" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS GUI.lnk" "%BATCH%" "-wx" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Old TclTk GUI.lnk" "%BATCH%" "-tcltk" \ "Launch GRASS GIS @VERSION@ with the old TclTk GUI" 1 "%ICON%" 
+xxmklink "%OSGEO4W_STARTMENU%\GRASS GIS @VERSION@\GRASS Command Line.lnk" "%BATCH%" "-text" \ "Launch GRASS GIS @VERSION@ in text mode" 1 "%ICON%" 
 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (wxpython).lnk" "%BATCH%" "-wxpython" \ "wxPython" 1 "%ICON%" 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (TclTk).lnk" "%BATCH%" "-tcltk" \ "Tcl/Tk" 1 "%ICON%" 
-xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@ (Text).lnk" "%BATCH%" "-text" \ "Text interface" 1 "%ICON%" 
+xxmklink "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk" "%BATCH%" "-wx" \ "Launch GRASS GIS @VERSION@ with wxGUI" 1 "%ICON%" 
+
+del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.bat.tmpl
+del "%OSGEO4W_ROOT%"\bin\grass@POSTFIX@.tmpl
