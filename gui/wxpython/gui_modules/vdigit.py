9bee7e78c4130ea79b2ef4b8f49e65a42d114d20
Martin Landa <landa.martin@gmail.com>
2011-11-29 10:01:37 +0000

    wxGUI/vdigit: catch also NameError
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@49418 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gui_modules/vdigit.py
+++ gui/wxpython/gui_modules/vdigit.py
@@ -43,7 +43,7 @@ try:
     from wxvdigit  import IVDigit, GV_LINES
     haveVDigit = True
     errorMsg = ''
-except ImportError, err:
+except (ImportError, NameError), err:
     haveVDigit = False
     errorMsg = err
     GV_LINES = -1
