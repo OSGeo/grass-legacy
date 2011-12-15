08089f32083def46249de738381b6baffc94e528
Martin Landa <landa.martin@gmail.com>
2011-12-15 14:27:04 +0000

    wxGUI: optionally delete intermediate data on finish
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@49772 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/gmodeler/frame.py
+++ gui/wxpython/gmodeler/frame.py
@@ -243,6 +243,9 @@ class ModelFrame(wx.Frame):
         
     def OnCmdPrepare(self, event):
         """!Prepare for running command"""
+        if not event.userData:
+            return
+        
         event.onPrepare(item = event.userData['item'],
                         params = event.userData['params'])
         
@@ -329,7 +332,7 @@ class ModelFrame(wx.Frame):
         
         if not rast and not vect and not rast3d:
             GMessage(parent = self,
-                     message = _('Nothing to delete.'))
+                     message = _('No intermediate data to delete.'))
             return
         
         dlg = wx.MessageDialog(parent = self,
