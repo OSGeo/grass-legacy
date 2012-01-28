bf8d9bdcd4763f766ecd9b1d5378c424baae4611
Hamish Bowman <hamish_b@yahoo.com>
2012-01-28 20:52:19 +0000

    minimal fix for #1276
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@50530 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.to.rast/support.c
+++ vector/v.to.rast/support.c
@@ -305,6 +305,7 @@ int update_labels(char *rast_name, char *vector_map, int field,
 	    /* get column type */
 	    if (!label_column) {
 		G_warning(_("Label column was not specified, no labels will be written"));
+		db_close_database_shutdown_driver(Driver);
 		break;
 	    }
 	    else {
