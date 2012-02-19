c5facf3df5480161c6143ee20367ac631a24bb94
Martin Landa <landa.martin@gmail.com>
2012-02-19 14:53:42 +0000

    wingrass: backport from devbr6
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@50874 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/env.bat
+++ mswindows/env.bat
@@ -1,35 +1,17 @@
-set WINGISBASE=%GISBASE%
+rem Environmental variables for GRASS stand-alone installer
 
-rem Directory where your .grassrc6 file will be stored
-set HOME=%USERPROFILE%
-
-rem Name of the wish (Tk) executable
-set GRASS_WISH=wish.exe
-
-rem Path to the shell command
+set GRASS_WISH=%GISBASE%\extrabin\wish.exe
+set GRASS_PYTHON=python
+set GRASS_PROJSHARE=%GISBASE%\proj
+set GRASS_HTML_BROWSER=explorer
 set GRASS_SH=%GISBASE%\msys\bin\sh.exe
 
-rem Set Path to utilities (libraries and binaries) used by GRASS
+set PYTHONHOME=%GISBASE%\Python27
+set GDAL_DATA=%GISBASE%\share\gdal
+set PROJ_LIB=%GISBASE%\proj
+set GEOTIFF_CSV=%GISBASE%\share\epsg_csv
+
 set PATH=%GISBASE%\msys\bin;%PATH%
 set PATH=%GISBASE%\extrabin;%GISBASE%\extralib;%PATH%
 set PATH=%GISBASE%\tcl-tk\bin;%GISBASE%\sqlite\bin;%GISBASE%\gpsbabel;%PATH%
-set PATH=%GISBASE%\bin;%GISBASE%\scripts;%PATH%
-
-rem Set Path to default web browser
-set GRASS_HTML_BROWSER=explorer
-
-rem Path to the proj files (notably the epsg projection list)
-set GRASS_PROJSHARE=%GISBASE%\proj
-
-rem Set GDAL_DATA
-set GDAL_DATA=%GISBASE%\share\gdal
-
-rem Set PROJ_LIB
-set PROJ_LIB=%GISBASE%\proj
-
-rem Set GEOTIFF_CSV
-set GEOTIFF_CSV=%GISBASE%\share\epsg_csv
-
-rem Path to the python directory
-set PYTHONHOME=%GISBASE%\Python27
-if "x%GRASS_PYTHON%" == "x" set GRASS_PYTHON=python
+set PATH=%GISBASE%\bin;%PATH%
