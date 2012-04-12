101ae5fe67fe74c1a68d3568f93e5e3eebb1e762
Martin Landa <landa.martin@gmail.com>
2012-04-12 12:38:13 +0000

    db.execute: note `input=-` for reading standard input
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@51387 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- db/base/execute.c
+++ db/base/execute.c
@@ -114,8 +114,9 @@ static void parse_command_line(int argc, char **argv)
 
     input = G_define_standard_option(G_OPT_F_INPUT);
     input->required = NO;
-    input->description = _("Name of file containing SQL statements");
+    input->label = _("Name of file containing SQL statements");
     input->guisection = _("Query");
+    input->description = _("If not given or '-' read from standard input");
     
     driver = G_define_standard_option(G_OPT_DRIVER);
     driver->options = db_list_drivers();
