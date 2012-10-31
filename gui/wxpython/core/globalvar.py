23700eb5d00dd122ec3643f919ac15e541c3fd07
Martin Landa <landa.martin@gmail.com>
2012-10-31 14:26:11 +0000

    fix SetLanguage() for wingrass
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@53617 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/wxpython/core/globalvar.py
+++ gui/wxpython/core/globalvar.py
@@ -203,20 +203,27 @@ def SetLanguage():
     import locale
     
     language = os.getenv('LANG')
-    if language:
-        language = language.split('.')[0] # Split off ignored .encoding part if present
-        orig_language = language
-        try:
-            locale.setlocale(locale.LC_ALL, language)
-        except:
+    if not language:
+        return
+    
+    language = language.split('.')[0] # Split off ignored .encoding part if present
+    orig_language = language
+    try:
+        locale.setlocale(locale.LC_ALL, language)
+    except locale.Error, e:
+        if sys.platform != 'win32': # Don't try on Windows, it will probably not work
+            sys.stderr.write("Failed to set LC_ALL to %s (%s)\n" % (language, e))
             try:
-                # Locale lang.encoding might be missing. Let's try UTF-8 encoding before giving up
-                # as on Linux systems lang.UTF-8 locales are more common than legacy ISO-8859 ones.
-                language = locale.normalize('%s.UTF-8' % language)
+                # Locale lang.encoding might be missing. Let's try
+                # UTF-8 encoding before giving up as on Linux systems
+                # lang.UTF-8 locales are more common than legacy
+                # ISO-8859 ones.
+                language = locale.normalize('%s.%s' % (language, locale.getdefaultlocale()[1]))
                 locale.setlocale(locale.LC_ALL, language)
-            except:
-                # If we got so far, provided locale is not supported on this system
-                sys.stderr.write("Failed to set LC_ALL to %s\n" % language)
+            except locale.Error, e:
+                # If we got so far, provided locale is not supported
+                # on this system
+                sys.stderr.write("Failed to set LC_ALL to %s (%s)\n" % (language, e))
                 try:
                     default_locale = locale.getdefaultlocale()
                 except:
@@ -226,22 +233,25 @@ def SetLanguage():
                     language = default_locale[0]
                 else:
                     language = 'C'
-        
-        # Set up environment for subprocesses
-        for lc in ('LC_CTYPE', 'LC_MESSAGES', 'LC_TIME', 'LC_COLLATE', 'LC_MONETARY', 'LC_PAPER', 'LC_NAME', 'LC_ADDRESS', 'LC_TELEPHONE', 'LC_MEASUREMENT', 'LC_IDENTIFICATION'):
-            os.environ[lc] = language
-        
-        # Some code in GRASS might not like other decimal separators than .
-        # Other potential sources for problems are: LC_TIME LC_CTYPE
-        locale.setlocale(locale.LC_NUMERIC, 'C')
-        os.environ['LC_NUMERIC'] = 'C'
-        if os.getenv('LC_ALL'):
-            del os.environ['LC_ALL'] # Remove LC_ALL to not override LC_NUMERIC
-        
-        # Even if setting locale has failed, let's set LANG in a hope, that UI will use it
-        # GRASS texts will be in selected language, system messages (i.e. OK, Cancel etc.) - in system default language
-        os.environ['LANGUAGE'] = orig_language
-        os.environ['LANG'] = orig_language
+    
+    # Set up environment for subprocesses
+    for lc in ('LC_CTYPE', 'LC_MESSAGES', 'LC_TIME', 'LC_COLLATE', 'LC_MONETARY', 'LC_PAPER',
+               'LC_NAME', 'LC_ADDRESS', 'LC_TELEPHONE', 'LC_MEASUREMENT', 'LC_IDENTIFICATION'):
+        os.environ[lc] = language
+    
+    # Some code in GRASS might not like other decimal separators than .
+    # Other potential sources for problems are: LC_TIME LC_CTYPE
+    locale.setlocale(locale.LC_NUMERIC, 'C')
+    os.environ['LC_NUMERIC'] = 'C'
+    if os.getenv('LC_ALL'):
+        del os.environ['LC_ALL'] # Remove LC_ALL to not override LC_NUMERIC
+    
+    # Even if setting locale has failed, let's set LANG in a hope,
+    # that UI will use it GRASS texts will be in selected language,
+    # system messages (i.e. OK, Cancel etc.) - in system default
+    # language
+    os.environ['LANGUAGE'] = orig_language
+    os.environ['LANG'] = orig_language
 
 """@brief Collected GRASS-relared binaries/scripts"""
 grassCmd, grassScripts = GetGRASSCommands()
