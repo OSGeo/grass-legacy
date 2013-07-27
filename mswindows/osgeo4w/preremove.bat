1c05ebaa5c28f1c54b5bedc05936b1bc78e36079
Martin Landa <landa.martin@gmail.com>
2013-07-27 15:07:29 +0000

    update preremove.bat file (remove generated __init__.pyc files)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@57296 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- mswindows/osgeo4w/preremove.bat
+++ mswindows/osgeo4w/preremove.bat
@@ -8,3 +8,13 @@ del "%ALLUSERSPROFILE%\Desktop\GRASS GIS @VERSION@.lnk"
 del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@.bat
 del "%OSGEO4W_ROOT%"\bin\@GRASS_EXECUTABLE@
 del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\fontcap
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\dbmgr\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\gcp\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\gui_core\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\location_wizard\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\mapdisp\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\modules\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\nviz\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\psmap\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\vdigit\__init__.pyc
+del "%OSGEO4W_ROOT%"\apps\grass\grass-@VERSION@\etc\wxpython\wxplot\__init__.pyc
