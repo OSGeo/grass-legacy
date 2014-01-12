b55239d3b2dfb240c9680a761f8e9a40e37c4229
Markus Neteler <neteler@gmail.com>
2014-01-12 12:56:22 +0000

    g.extension.py: backport of r58199 (ticket #2138)
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/releasebranch_6_4@58680 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- gui/scripts/g.extension.py
+++ gui/scripts/g.extension.py
@@ -618,14 +618,25 @@ def tidy_citizen():
     if os.path.exists(os.path.join(options['prefix'], 'scripts', options['extension'] + '.py')):
         shutil.move(os.path.join(options['prefix'], 'scripts', options['extension'] + '.py'),
                     os.path.join(options['prefix'], options['extension'] + '.py'))
-    if sys.platform == 'win32' and \
-            os.path.exists(os.path.join(options['prefix'], 'bin', options['extension']) + EXT_SCT):
-        shutil.move(os.path.join(options['prefix'], 'bin', options['extension']) + EXT_SCT,
-                    os.path.join(options['prefix'], options['extension']) + EXT_SCT)
-        # fix script path
-        for line in fileinput.FileInput(os.path.join(options['prefix'], options['extension']) + EXT_SCT,
-                                        inplace = True):
+    if sys.platform == 'win32':
+        is_python_script = False
+        if os.path.exists(os.path.join(options['prefix'], 'bin', options['extension']) + EXT_SCT):
+            shutil.move(os.path.join(options['prefix'], 'bin', options['extension']) + EXT_SCT,
+                        os.path.join(options['prefix'], options['extension']) + EXT_SCT)
+        elif os.path.exists(os.path.join(options['prefix'], 'bin', options['extension']) + '.py' + EXT_SCT):
+            shutil.move(os.path.join(options['prefix'], 'bin', options['extension']) + '.py' + EXT_SCT,
+                        os.path.join(options['prefix'], options['extension']) + EXT_SCT)
+            is_python_script = True
+
+        # fix script path. may be it is better to just write out a new
+        #   windows_launch.bat template manually, since it's just a one-liner.
+        for line in fileinput.FileInput(os.path.join(options['prefix'],
+                                       options['extension']) + EXT_SCT,
+                                       inplace = True):
             line = line.replace("/scripts", "")
+            line = line.replace("%GISBSASE%", options['prefix'])
+            if is_python_script:
+                line = line.replace("GRASS_SH", "GRASS_PYTHON")
             print line
     
     # move man/ into docs/
