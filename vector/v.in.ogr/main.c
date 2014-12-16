274d379ce1c776db96b6611ef4d3e4ecfa4ce591
Markus Metz <markus.metz.giswork@gmail.com>
2014-12-16 08:31:38 +0000

    v.in.ogr: backport https://trac.osgeo.org/grass/changeset/63548
    git-svn-id: https://svn.osgeo.org/grass/grass/branches/develbranch_6@63565 15284696-431f-4ddb-bdfa-cd5b030d7da7

--- vector/v.in.ogr/main.c
+++ vector/v.in.ogr/main.c
@@ -895,6 +895,7 @@ int main(int argc, char *argv[])
 			}
 #endif
 			else if (Ogr_ftype == OFTString ||
+				 Ogr_ftype == OFTStringList ||
 				 Ogr_ftype == OFTIntegerList) {
 			    db_set_string(&strval, (char *)
 					  OGR_F_GetFieldAsString(Ogr_feature,
@@ -920,6 +921,7 @@ int main(int argc, char *argv[])
 			}
 #endif
 			else if (Ogr_ftype == OFTString ||
+				 Ogr_ftype == OFTStringList ||
 				 Ogr_ftype == OFTIntegerList) {
 			    sprintf(buf, ", ''");
 			}
